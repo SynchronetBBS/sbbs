efe0b631197b29771e48f12b3ff67765355597d2
rswindell <>
2000-10-30 09:47:40 +0000

    Added tell() macro for Unix platforms.

--- src/sbbs3/sbbswrap.h
+++ src/sbbs3/sbbswrap.h
@@ -134,6 +134,7 @@ extern "C" {
 	#define mswait(x)			usleep(x*1000)
 	#define _mkdir(dir)			mkdir(dir,0777)
 	#define _rmdir(dir)			rmdir(dir)
+	#define tell(fd)			lseek(fd,0,SEEK_CUR)
 
 	DLLEXPORT void	DLLCALL sbbs_beep(int freq, int dur);
 	DLLEXPORT char*	DLLCALL strupr(char* str);
