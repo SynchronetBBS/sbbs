e801dc083ac24a71639afafd6f54e14c6fcd5206
rswindell <>
2000-10-30 08:55:50 +0000

    glob routines are now only declared ifndef __unix__

--- src/sbbs3/sbbswrap.h
+++ src/sbbs3/sbbswrap.h
@@ -38,48 +38,6 @@
 #ifndef _SBBSWRAP_H
 #define _SBBSWRAP_H
 
-#ifdef _MSC_VER
-	#include "msdirent.h"
-#else
-	#include <dirent.h>	/* POSIX directory functions */
-#endif
-
-#ifdef __unix__
-	#include <glob.h>	/* POSIX.2 directory pattern matching function */
-#else
-
-typedef struct 
-{
-		size_t gl_pathc;			/* Count of paths matched so far */
-		char **gl_pathv;			/* List of matched pathnames. */
-		size_t gl_offs;				/* Slots to reserve in 'gl_pathv'. */
-} glob_t;
-
-/* Bits set in the FLAGS argument to `glob'.  */
-#define GLOB_ERR        (1 << 0)	/* Return on read errors.  */
-#define GLOB_MARK       (1 << 1)	/* Append a slash to each name.  */
-#define GLOB_NOSORT     (1 << 2)	/* Don't sort the names.  */
-#define GLOB_DOOFFS     (1 << 3)	/* Insert PGLOB->gl_offs NULLs.  */
-#define GLOB_NOCHECK    (1 << 4)	/* If nothing matches, return the pattern.  */
-#define GLOB_APPEND     (1 << 5)	/* Append to results of a previous call.  */
-#define GLOB_NOESCAPE   (1 << 6)	/* Backslashes don't quote metacharacters.  */
-#define GLOB_PERIOD     (1 << 7)	/* Leading `.' can be matched by metachars.  */
-#define GLOB_MAGCHAR    (1 << 8)	/* Set in gl_flags if any metachars seen.  */
-#define GLOB_ALTDIRFUNC (1 << 9)	/* Use gl_opendir et al functions.  */
-#define GLOB_BRACE      (1 << 10)	/* Expand "{a,b}" to "a" "b".  */
-#define GLOB_NOMAGIC    (1 << 11)	/* If no magic chars, return the pattern.  */
-#define GLOB_TILDE      (1 << 12)	/* Expand ~user and ~ to home directories. */
-#define GLOB_ONLYDIR    (1 << 13)	/* Match only directories.  */
-#define GLOB_TILDE_CHECK (1 << 14)	/* Like GLOB_TILDE but return an error
-                                      if the user name is not available.  */
-/* Error returns from `glob'.  */
-#define GLOB_NOSPACE    1       /* Ran out of memory.  */
-#define GLOB_ABORTED    2       /* Read error.  */
-#define GLOB_NOMATCH    3       /* No matches found.  */
-#define GLOB_NOSYS      4       /* Not implemented.  */
-
-#endif
-
 #include "gen_defs.h"	/* ulong */
 
 #ifdef DLLEXPORT
@@ -107,6 +65,54 @@ typedef struct
 
 #ifdef __cplusplus
 extern "C" {
+#endif
+
+
+#ifdef _MSC_VER
+	#include "msdirent.h"
+#else
+	#include <dirent.h>	/* POSIX directory functions */
+#endif
+
+#ifdef __unix__
+
+	#include <glob.h>	/* POSIX.2 directory pattern matching function */
+
+#else	/* glob-compatible findfirst/findnext wrapper */
+
+	typedef struct 
+	{
+			size_t gl_pathc;			/* Count of paths matched so far */
+			char **gl_pathv;			/* List of matched pathnames. */
+			size_t gl_offs;				/* Slots to reserve in 'gl_pathv'. */
+	} glob_t;
+
+	/* Bits set in the FLAGS argument to `glob'.  */
+	#define GLOB_ERR        (1 << 0)	/* Return on read errors.  */
+	#define GLOB_MARK       (1 << 1)	/* Append a slash to each name.  */
+	#define GLOB_NOSORT     (1 << 2)	/* Don't sort the names.  */
+	#define GLOB_DOOFFS     (1 << 3)	/* Insert PGLOB->gl_offs NULLs.  */
+	#define GLOB_NOCHECK    (1 << 4)	/* If nothing matches, return the pattern.  */
+	#define GLOB_APPEND     (1 << 5)	/* Append to results of a previous call.  */
+	#define GLOB_NOESCAPE   (1 << 6)	/* Backslashes don't quote metacharacters.  */
+	#define GLOB_PERIOD     (1 << 7)	/* Leading `.' can be matched by metachars.  */
+	#define GLOB_MAGCHAR    (1 << 8)	/* Set in gl_flags if any metachars seen.  */
+	#define GLOB_ALTDIRFUNC (1 << 9)	/* Use gl_opendir et al functions.  */
+	#define GLOB_BRACE      (1 << 10)	/* Expand "{a,b}" to "a" "b".  */
+	#define GLOB_NOMAGIC    (1 << 11)	/* If no magic chars, return the pattern.  */
+	#define GLOB_TILDE      (1 << 12)	/* Expand ~user and ~ to home directories. */
+	#define GLOB_ONLYDIR    (1 << 13)	/* Match only directories.  */
+	#define GLOB_TILDE_CHECK (1 << 14)	/* Like GLOB_TILDE but return an error
+										  if the user name is not available.  */
+	/* Error returns from `glob'.  */
+	#define GLOB_NOSPACE    1       /* Ran out of memory.  */
+	#define GLOB_ABORTED    2       /* Read error.  */
+	#define GLOB_NOMATCH    3       /* No matches found.  */
+	#define GLOB_NOSYS      4       /* Not implemented.  */
+
+	DLLEXPORT int	DLLCALL	glob(const char *pattern, int flags, void* unused, glob_t*);
+	DLLEXPORT void	DLLCALL globfree(glob_t*);
+
 #endif
 
 /***************/
@@ -220,8 +226,6 @@ extern "C" {
 
 
 /* General file system wrappers for all platforms and compilers */
-DLLEXPORT int	DLLCALL	glob(const char *pattern, int flags, void* unused, glob_t*);
-DLLEXPORT void	DLLCALL globfree(glob_t*);
 DLLEXPORT long	DLLCALL fdate(char *filename);
 DLLEXPORT BOOL	DLLCALL	isdir(char *filename);
 DLLEXPORT int	DLLCALL getfattr(char* filename);
