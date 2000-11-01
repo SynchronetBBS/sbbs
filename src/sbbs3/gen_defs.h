45a5f56a5a2d8e4d7716d77fdc681082c6b5e5f1
rswindell <>
2000-11-01 02:28:40 +0000

    Added BEL char-constant macro. Converted some of the standard char-constants to the appropriate C escape sequences.

--- src/sbbs3/gen_defs.h
+++ src/sbbs3/gen_defs.h
@@ -41,13 +41,14 @@
 									/* Control characters */
 #define STX 	0x02				/* Start of text			^B	*/
 #define ETX 	0x03				/* End of text				^C	*/
-#define BS		0x08				/* Back space				^H	*/
-#define TAB 	0x09				/* Horizontal tabulation	^I	*/
-#define LF		0x0a				/* Line feed				^J	*/
+#define BEL		0x07				/* Bell/beep				^G	*/
 #define FF		0x0c				/* Form feed				^L	*/
-#define CR		0x0d				/* Carriage return			^M	*/
 #define ESC 	0x1b				/* Escape					^[	*/
-#define SP		0x20				/* Space						*/
+#define BS		'\b'				/* Back space				^H	*/
+#define TAB 	'\t'				/* Horizontal tabulation	^I	*/
+#define LF		'\n'				/* Line feed				^J	*/
+#define CR		'\r'				/* Carriage return			^M	*/
+#define SP		' '					/* Space						*/
 
 #ifndef MAX_PATH
 	#ifdef MAXPATHLEN
