1af28b8b7f3377c27480a93b162a6440b8c38a61
rswindell <>
2000-11-02 01:14:01 +0000

    Changed format of Remote Spy command message.

--- src/sbbs3/execfunc.cpp
+++ src/sbbs3/execfunc.cpp
@@ -329,7 +329,8 @@ int sbbs_t::exec_function(csi_t *csi)
 				csi->logic=LOGIC_FALSE;
 				return(0);
 			}
-			bprintf("Spying on Node %d, Ctrl-C to Abort...\r\n\r\n",i);
+			bprintf("*** Synchronet Remote Spy on Node %d: Ctrl-C to Abort ***"
+				"\r\n\r\n",i);
 			spy_socket[i-1]=client_socket;
 			ansi_len=0;
 			while(online 
