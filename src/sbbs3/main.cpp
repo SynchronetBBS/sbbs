70484d56c48cf95e424efbfe589e141639e033a9
rswindell <>
2001-05-16 00:30:36 +0000

    Added change directory (to ctrl directory) during initialization.

--- src/sbbs3/main.cpp
+++ src/sbbs3/main.cpp
@@ -2954,6 +2954,9 @@ void DLLCALL bbs_thread(void* arg)
 	lprintf("Initializing on %.24s with options: %lx"
 		,ctime(&t),startup->options);
 
+	if(_chdir(startup->ctrl_dir)!=0)
+		lprintf("!ERROR changing directory to: %s", startup->ctrl_dir);
+
 	/* Initial configuration and load from CNF files */
 	scfg.startup=startup;
     sprintf(scfg.ctrl_dir, "%.*s", (int)sizeof(scfg.ctrl_dir)-1
