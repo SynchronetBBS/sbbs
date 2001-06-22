6620948b7d767e643eea8129345e96fd8ad1ea0e
rswindell <>
2001-06-22 03:23:38 +0000

    Added C++ wrappers for getnodedat and putnodedat.

--- src/sbbs3/ctrl/NodeFormUnit.h
+++ src/sbbs3/ctrl/NodeFormUnit.h
@@ -94,6 +94,9 @@ __published:	// IDE-managed Components
     void __fastcall UserMsgButtonClick(TObject *Sender);
     void __fastcall RefreshMenuItemClick(TObject *Sender);
 private:	// User declarations
+    int __fastcall getnodedat(int node_num, node_t* node, bool lockit);
+    int __fastcall putnodedat(int node_num, node_t* node);
+
 public:		// User declarations
      __fastcall TNodeForm(TComponent* Owner);
      TMainForm* MainForm;
