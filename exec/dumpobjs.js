f4378b486d723046bcd3b8059d4228ef0fa68700
rswindell <>
2001-06-22 02:40:04 +0000

    Initial check-in of JavaScript object dump module.

---
+++ exec/dumpobjs.js
@@ -0,0 +1,53 @@
+// system
+console.clear();
+print("\1hproperties of \1ysystem \1wobject\1n");
+for(i in system)
+	print("system." + i + " = " + system[i]);
+
+// system.stats
+console.clear();
+print("\1hproperties of \1ysystem.stats \1wobject\1n");
+for(i in system.stats)
+	print("system.stats." + i + " = " + system.stats[i]);
+
+// client
+console.clear();
+print("\1hproperties of \1yclient \1wobject\1n");
+for(i in client)
+	print("client." + i + " = " + client[i]);
+
+// client.socket
+console.clear();
+print("\1hproperties of \1yclient.socket \1wobject\1n");
+for(i in client.socket)
+	print("client.socket." + i + " = " + client.socket[i]);
+
+// bbs
+console.clear();
+print("\1hproperties of \1ybbs \1wobject\1n");
+for(i in bbs)
+	print("bbs." + i + " = " + bbs[i]);
+
+// user
+console.clear();
+print("\1hproperties of \1yuser \1wobject\1n");
+for(i in user)
+	print("user." + i + " = " + user[i]);
+
+// user.stats
+console.clear();
+print("\1hproperties of \1yuser.stats \1wobject\1n");
+for(i in user.stats)
+	print("user.stats." + i + " = " + user.stats[i]);
+
+// user.security
+console.clear();
+print("\1hproperties of \1yuser.security \1wobject\1n");
+for(i in user.security)
+	print("user.security." + i + " = " + user.security[i]);
+
+// console
+console.clear();
+print("\1hproperties of \1yconsole \1wobject\1n");
+for(i in console)
+	print("console." + i + " = " + console[i]);
