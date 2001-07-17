aee8da5e3ffcf4cc7807ae5b5caeda6ac6d13233
rswindell <>
2001-07-17 02:17:06 +0000

    Updated for new User class.

--- exec/nodelist.js
+++ exec/nodelist.js
@@ -2,10 +2,12 @@
 
 load("nodedefs.js");
 
+var user = new User(1);
 for(n=0;n<system.node_list.length;n++) {
 	printf("Node %2d ",n+1);
 	if(system.node_list[n].status==NODE_INUSE) {
-		printf("%s ",system.node_list[n].useron_name);
+		user.number=system.node_list[n].useron;
+		printf("%s (%u %s) ", user.alias, user.age, user.gender);
 		printf(NodeAction[system.node_list[n].action],system.node_list[n].aux);
 	} else
 		printf(NodeStatus[system.node_list[n].status],system.node_list[n].aux);
