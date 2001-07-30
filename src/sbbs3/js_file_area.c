6f701b58bf9e57567ec30fc8c29b8ae765ee30e3
rswindell <>
2001-07-30 18:11:04 +0000

    Set lib_list and dir_list property flags to NON-enumerable.

--- src/sbbs3/js_file_area.c
+++ src/sbbs3/js_file_area.c
@@ -70,6 +70,7 @@ JSObject* DLLCALL js_CreateFileAreaObject(JSContext* cx, JSObject* parent, scfg_
 	jsval		val;
 	jsint		index;
 	uint		l,d;
+	JSBool		found;
 
 	areaobj = JS_DefineObject(cx, parent, "file_area", &js_file_area_class, NULL, 0);
 
@@ -83,6 +84,7 @@ JSObject* DLLCALL js_CreateFileAreaObject(JSContext* cx, JSObject* parent, scfg_
 	val=OBJECT_TO_JSVAL(lib_list);
 	if(!JS_SetProperty(cx, areaobj, "lib_list", &val)) 
 		return(NULL);
+	JS_SetPropertyAttributes(cx, areaobj, "lib_list", 0, &found);
 
 	for(l=0;l<cfg->total_libs;l++) {
 
@@ -112,7 +114,7 @@ JSObject* DLLCALL js_CreateFileAreaObject(JSContext* cx, JSObject* parent, scfg_
 		val=OBJECT_TO_JSVAL(dir_list);
 		if(!JS_SetProperty(cx, libobj, "dir_list", &val)) 
 			return(NULL);
-
+		JS_SetPropertyAttributes(cx, libobj, "dir_list", 0, &found);
 
 		for(d=0;d<cfg->total_dirs;d++) {
 			if(cfg->dir[d]->lib!=l)
@@ -144,7 +146,7 @@ JSObject* DLLCALL js_CreateFileAreaObject(JSContext* cx, JSObject* parent, scfg_
 			if(!JS_SetProperty(cx, dirobj, "link", &val))
 				return(NULL);
 
-			if(!JS_GetArrayLength(cx, dir_list, &index))
+			if(!JS_GetArrayLength(cx, dir_list, &index))	/* inexplicable exception here on Jul-6-2001 */
 				return(NULL);
 
 			val=OBJECT_TO_JSVAL(dirobj);
