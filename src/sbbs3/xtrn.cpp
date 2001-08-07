70f032d861e47b5c47958b2dbba209f7e055be85
rswindell <>
2001-08-07 22:29:48 +0000

    NT DOS/FOSSIL driver (DOSXTRN.EXE) now has configurable "polls before yield" value.

--- src/sbbs3/xtrn.cpp
+++ src/sbbs3/xtrn.cpp
@@ -334,7 +334,8 @@ int sbbs_t::external(char* cmdline, long mode, char* startup_dir)
            		i|=SBBSEXEC_MODE_DOS_IN;
 			if(mode&EX_OUTR)
         		i|=SBBSEXEC_MODE_DOS_OUT;
-			sprintf(str," NT %u %u",cfg.node_num,i);
+			sprintf(str," NT %u %u %u"
+				,cfg.node_num,i,startup->xtrn_polls_before_yield);
 			strcat(fullcmdline,str);
 
 			sprintf(str,"sbbsexec_hungup%d",cfg.node_num);
@@ -396,7 +397,8 @@ int sbbs_t::external(char* cmdline, long mode, char* startup_dir)
 			if(mode&EX_OUTR)
         		start.mode|=SBBSEXEC_MODE_DOS_OUT;
 
-			sprintf(str," 95 %u %u",cfg.node_num,start.mode);
+			sprintf(str," 95 %u %u %u"
+				,cfg.node_num,start.mode,startup->xtrn_polls_before_yield);
 			strcat(fullcmdline,str);
 
 			if(!DeviceIoControl(
