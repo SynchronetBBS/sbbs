57b039943c7a6e6ea4d493b52d4cca70a915d2c1
rswindell <>
2001-08-27 16:18:45 +0000

    Now silently ignores 0-byte QWK packets.

--- src/sbbs3/main.cpp
+++ src/sbbs3/main.cpp
@@ -1155,15 +1155,17 @@ void event_thread(void* arg)
 						,j>10 ? ((j-1)/10)+'0' : 'w'
 						,j ? ((j-1)%10)+'0' : 'k');
 					if(fexist(str)) {
-						sbbs->delfiles(sbbs->cfg.temp_dir,ALLFILES);
-						if(sbbs->unpack_qwk(str,i)==false) {
-							char newname[MAX_PATH+1];
-							sprintf(newname,"%s.%lx.bad",str,now);
-							remove(newname);
-							if(rename(str,newname)==0) {
-								char logmsg[MAX_PATH*3];
-								sprintf(logmsg,"%s renamed to %s",str,newname);
-								sbbs->logline("Q!",logmsg);
+						if(flength(str)>0) {	/* silently ignore 0-byte QWK packets */
+							sbbs->delfiles(sbbs->cfg.temp_dir,ALLFILES);
+							if(sbbs->unpack_qwk(str,i)==false) {
+								char newname[MAX_PATH+1];
+								sprintf(newname,"%s.%lx.bad",str,now);
+								remove(newname);
+								if(rename(str,newname)==0) {
+									char logmsg[MAX_PATH*3];
+									sprintf(logmsg,"%s renamed to %s",str,newname);
+									sbbs->logline("Q!",logmsg);
+								}
 							}
 						}
 						remove(str);
