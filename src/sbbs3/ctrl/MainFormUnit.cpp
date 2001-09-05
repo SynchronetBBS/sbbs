a0c222c1dcb3d4882fc1656f83924d26e93813a5
rswindell <>
2001-09-05 16:36:55 +0000

    Added code (that was missing) to save/restore MailRelayPort setting.

--- src/sbbs3/ctrl/MainFormUnit.cpp
+++ src/sbbs3/ctrl/MainFormUnit.cpp
@@ -1233,6 +1233,9 @@ void __fastcall TMainForm::StartupTimerTick(TObject *Sender)
             ,sizeof(mail_startup.relay_server)-1
             ,Registry->ReadString("MailRelayServer").c_str());
 
+    if(Registry->ValueExists("MailRelayPort"))
+    	mail_startup.relay_port=Registry->ReadInteger("MailRelayPort");
+
     if(Registry->ValueExists("MailDNSServer"))
         sprintf(mail_startup.dns_server,"%.*s"
             ,sizeof(mail_startup.dns_server)-1
@@ -1538,6 +1541,7 @@ void __fastcall TMainForm::SaveSettings(TObject* Sender)
     Registry->WriteInteger("MailPOP3Port",mail_startup.pop3_port);
 
     Registry->WriteString("MailRelayServer",mail_startup.relay_server);
+    Registry->WriteInteger("MailRelayPort",mail_startup.relay_port);
     Registry->WriteString("MailDNSServer",mail_startup.dns_server);
 
     Registry->WriteString("MailInboundSound",AnsiString(mail_startup.inbound_sound));
