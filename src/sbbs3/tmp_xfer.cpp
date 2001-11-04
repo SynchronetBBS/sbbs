38523944776890c57ce1daae345b3a20e90bc3cf
rswindell <>
2001-11-04 01:13:02 +0000

    Removed EX_SH from extraction command (no wildcard expansion).

--- src/sbbs3/tmp_xfer.cpp
+++ src/sbbs3/tmp_xfer.cpp
@@ -382,7 +382,7 @@ void sbbs_t::extract(uint dirnum)
 				if(!checkfname(str))
 					break;
 				if((i=external(cmdstr(excmd,path,str,NULL)
-					,EX_INR|EX_OUTL|EX_OUTR|EX_SH))!=0) {
+					,EX_INR|EX_OUTL|EX_OUTR))!=0) {
 					errormsg(WHERE,ERR_EXEC,cmdstr(excmd,path,str,NULL),i);
 					return; }
 				sprintf(tmp,"Extracted %s from %s",str,path);
