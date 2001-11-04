b3d85b44a7023cb6065cf8da464b20887fe7e017
rswindell <>
2001-11-04 03:56:46 +0000

    Added semicolon to the list of chars that trigger auto-shell mode.

--- src/sbbs3/xtrn.cpp
+++ src/sbbs3/xtrn.cpp
@@ -886,7 +886,7 @@ int sbbs_t::external(char* cmdline, long mode, char* startup_dir)
 
 		lprintf("Node %d executing external: %s",cfg.node_num,cmdline);
 
-		if(mode&EX_SH || strcspn(cmdline,"<>|")!=strlen(cmdline)) {
+		if(mode&EX_SH || strcspn(cmdline,"<>|;")!=strlen(cmdline)) {
 			argv[0]=comspec;
 			argv[1]="-c";
 			argv[2]=cmdline;
