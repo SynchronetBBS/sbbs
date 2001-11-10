77baef8418daa36ef64a75db4c65f843b87d1ddb
rswindell <>
2001-11-10 17:24:55 +0000

    A simple finger client.

---
+++ exec/finger.js
@@ -0,0 +1,23 @@
+// finger.js
+
+// A simple finger client
+
+if(argc>0 && argv[0].indexOf('@')!=-1)
+	dest = argv[0];
+else if((dest = prompt("User (user@hostname)"))==null)
+	exit();
+if((hp = dest.indexOf('@'))==-1) {
+	alert("Invalid user");
+	exit();
+}
+host = dest.slice(hp+1);
+sock = new Socket();
+//sock.debug = true;
+if(!sock.connect(host,79)) 
+	alert("Connection to " + host + " failed with error " + sock.last_error);
+else {
+	sock.send(dest.slice(0,hp)+"\r\n");
+	while(bbs.online && sock.is_connected)
+		print(sock.readline());
+}
+sock.close();
