47b778bb7df7e4669f168bec4b4ec9945daa1520
rswindell <>
2002-03-03 20:01:10 +0000

    Changed Unix version of external() to close stdio file descriptors if exec() fails (don't want sbbscon output to go to remote user).

--- src/sbbs3/xtrn.cpp
+++ src/sbbs3/xtrn.cpp
@@ -989,6 +989,9 @@ int sbbs_t::external(char* cmdline, long mode, char* startup_dir)
 		}	
 
 		execvp(argv[0],argv);
+		close(0);	/* close stdin */
+		close(1);	/* close stdout */
+		close(2);	/* close stderr */
 		errormsg(WHERE,ERR_EXEC,argv[0],0);
 		exit(-1);	/* should never get here */
 	}
