7ef488c1a2d1ee7335843dbf7908a32614474222
rswindell <>
2002-03-17 02:26:39 +0000

    Added lock to putnodedat() (in case getnodedat() failed to lock the record).

--- src/sbbs3/putnode.cpp
+++ src/sbbs3/putnode.cpp
@@ -86,6 +86,7 @@ void sbbs_t::putnodedat(uint number, node_t* node)
 	}
 
 	number--;	/* make zero based */
+	lock(nodefile,(long)number*sizeof(node_t),sizeof(node_t));
 	for(attempts=0;attempts<10;attempts++) {
 		lseek(nodefile,(long)number*sizeof(node_t),SEEK_SET);
 		wr=write(nodefile,node,sizeof(node_t));
