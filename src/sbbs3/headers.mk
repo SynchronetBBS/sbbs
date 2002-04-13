2cdb2bf18787f07cb7a174febc58e826512a337a
rswindell <>
2002-04-13 07:41:41 +0000

    Updated headers, added new xpdev headers.

--- src/sbbs3/headers.mk
+++ src/sbbs3/headers.mk
@@ -5,5 +5,26 @@
 
 # $Id$
 
-HEADERS =	sbbs.h sbbsdefs.h sbbswrap.h sbbsinet.h scfgdefs.h gen_defs.h \
-			nodedefs.h smblib.h smbdefs.h text.h
+HEADERS = \
+	ars_defs.h \
+	client.h \
+	cmdshell.h \
+	nodedefs.h \
+	post.h \
+	ringbuf.h \
+	riodefs.h \
+	sbbs.h \
+	sbbsdefs.h \
+	scfgdefs.h \
+	scfglib.h \
+	smbdefs.h \
+	smblib.h \
+	text.h \
+	userdat.h \
+	$(XPDEV)gen_defs.h \
+	$(XPDEV)genwrap.h \
+	$(XPDEV)wrapdll.h \
+	$(XPDEV)dirwrap.h \
+	$(XPDEV)filewrap.h \
+	$(XPDEV)sockwrap.h \
+	$(XPDEV)threadwrap.h
