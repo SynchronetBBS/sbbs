71b89ec299d4bd59fb2e59d8e8410b748893fbf5
rswindell <>
2002-04-25 22:46:42 +0000

    Updated comment header (with license, ptsc header, etc). Changed tag line (removed "Synchronet+" and added platform description).

--- src/sbbs3/sbbsecho.c
+++ src/sbbs3/sbbsecho.c
@@ -1,10 +1,41 @@
-/* SBBSECHO.C */
-
-/* Developed 1990-2000 by Rob Swindell; PO Box 501, Yorba Linda, CA 92885	*/
-
-/* Portions written by Allen Christiansen 1994-1996 						*/
+/* sbbsecho.c */
 
 /* Synchronet FidoNet EchoMail Scanning/Tossing and NetMail Tossing Utility */
+
+/* $Id$ */
+
+/****************************************************************************
+ * @format.tab-size 4		(Plain Text/Source Code File Header)			*
+ * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
+ *																			*
+ * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ *																			*
+ * This program is free software; you can redistribute it and/or			*
+ * modify it under the terms of the GNU General Public License				*
+ * as published by the Free Software Foundation; either version 2			*
+ * of the License, or (at your option) any later version.					*
+ * See the GNU General Public License for more details: gpl.txt or			*
+ * http://www.fsf.org/copyleft/gpl.html										*
+ *																			*
+ * Anonymous FTP access to the most recent released source is available at	*
+ * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
+ *																			*
+ * Anonymous CVS access to the development source and modification history	*
+ * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
+ * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
+ *     (just hit return, no password is necessary)							*
+ * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src		*
+ *																			*
+ * For Synchronet coding style and modification guidelines, see				*
+ * http://www.synchro.net/source.html										*
+ *																			*
+ * You are encouraged to submit any modifications (preferably in Unix diff	*
+ * format) via e-mail to mods@synchro.net									*
+ *																			*
+ * Note: If this box doesn't appear square, then you need to fix your tabs.	*
+ ****************************************************************************/
+
+/* Portions written by Allen Christiansen 1994-1996 						*/
 
 #ifdef _WIN32
 	#include <windows.h>
@@ -3679,7 +3710,7 @@ void export_echomail(char *sub_code,faddr_t addr)
 
 				if(!(scfg.sub[i]->misc&SUB_NOTAG)) {
 					if(!tear) {  /* No previous tear line */
-						sprintf(str,"--- Synchronet+SBBSecho v%s\r",SBBSECHO_VER);
+						sprintf(str,"--- SBBSecho/%s v%s\r",PLATFORM_DESC,SBBSECHO_VER);
 						strcat((char *)fmsgbuf,str); }
 
 					sprintf(str," * Origin: %s (%s)\r"
