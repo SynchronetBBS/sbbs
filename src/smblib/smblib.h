72a07104e60c033f446c47901be86845dbc70efc
rswindell <>
2002-04-25 22:44:15 +0000

    Changed license header to LGPL instead of GPL.

--- src/smblib/smblib.h
+++ src/smblib/smblib.h
@@ -10,12 +10,12 @@
  *																			*
  * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
- * This program is free software; you can redistribute it and/or			*
- * modify it under the terms of the GNU General Public License				*
+ * This library is free software; you can redistribute it and/or			*
+ * modify it under the terms of the GNU Lesser General Public License		*
  * as published by the Free Software Foundation; either version 2			*
  * of the License, or (at your option) any later version.					*
- * See the GNU General Public License for more details: gpl.txt or			*
- * http://www.fsf.org/copyleft/gpl.html										*
+ * See the GNU Lesser General Public License for more details: lgpl.txt or	*
+ * http://www.fsf.org/copyleft/lesser.html									*
  *																			*
  * Anonymous FTP access to the most recent released source is available at	*
  * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
