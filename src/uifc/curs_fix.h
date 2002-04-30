f1e2f2ce6bf592b43ed831df5982ac969f7314c8
rswindell <>
2002-04-30 23:30:19 +0000

    Deuce's fixes for missing curses definitions.

---
+++ src/uifc/curs_fix.h
@@ -0,0 +1,117 @@
+#include <curses.h>
+
+#ifndef	ACS_SBSD
+#define ACS_SBSD	ACS_SBSS
+#endif
+
+#ifndef ACS_DBDS
+#define ACS_DBDS	ACS_SBSS
+#endif
+
+#ifndef ACS_BBDS
+#define	ACS_BBDS	ACS_BBSS
+#endif
+
+#ifndef ACS_BBSD
+#define ACS_BBSD	ACS_BBSS
+#endif
+
+#ifndef ACS_DBDD
+#define	ACS_DBDD	ACS_SBSS
+#endif
+
+#ifndef	ACS_DBDB
+#define	ACS_DBDB	ACS_SBSB
+#endif
+
+#ifndef ACS_BBDD
+#define ACS_BBDD	ACS_BBSS
+#endif
+
+#ifndef ACS_DBBD
+#define ACS_DBBD	ACS_SBBS
+#endif
+
+#ifndef ACS_DBBS
+#define ACS_DBBS	ACS_SBBS
+#endif
+
+#ifndef ACS_SBBD
+#define ACS_SBBD	ACS_SBBS
+#endif
+
+#ifndef ACS_SDSB
+#define ACS_SDSB	ACS_SSSB
+#endif
+
+#ifndef	ACS_DSDB
+#define ACS_DSDB	ACS_SSSB
+#endif
+
+#ifndef ACS_DDBB
+#define	ACS_DDBB	ACS_SSBB
+#endif
+
+#ifndef ACS_BDDB
+#define ACS_BDDB	ACS_BSSB
+#endif
+
+#ifndef ACS_DDBD
+#define ACS_DDBD	ACS_SSBS
+#endif
+
+#ifndef ACS_BDDD
+#define	ACS_BDDD	ACS_BSSS
+#endif
+
+#ifndef ACS_DDDB
+#define ACS_DDDB	ACS_SSSB
+#endif
+
+#ifndef ACS_BDBD
+#define ACS_BDBD	ACS_BSBS
+#endif
+
+#ifndef ACS_DDDD
+#define	ACS_DDDD	ACS_SSSS
+#endif
+
+#ifndef ACS_SDBD
+#define ACS_SDBD	ACS_SSBS
+#endif
+
+#ifndef ACS_DSBS
+#define ACS_DSBS	ACS_SSBS
+#endif
+
+#ifndef ACS_BDSD
+#define ACS_BDSD	ACS_BSSS
+#endif
+
+#ifndef	ACS_BSDS
+#define ACS_BSDS	ACS_BSSS
+#endif
+
+#ifndef	ACS_DSBB
+#define ACS_DSBB	ACS_SSBB
+#endif
+      
+#ifndef ACS_SDBB
+#define ACS_SDBB	ACS_SSBB
+#endif
+
+#ifndef ACS_BDSB
+#define ACS_BDSB	ACS_BSSB
+#endif
+
+#ifndef ACS_BSDB
+#define ACS_BSDB	ACS_BSSB
+#endif
+
+#ifndef ACS_DSDS
+#define	ACS_DSDS	ACS_SSSS
+#endif
+
+#ifndef ACS_SDSD
+#define	ACS_SDSD	ACS_SSSS
+#endif
