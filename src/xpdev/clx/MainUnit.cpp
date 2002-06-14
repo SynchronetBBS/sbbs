a240707c113bc696305133288b7b65f6530481e8
rswindell <>
2002-06-14 22:47:51 +0000

    Create thread suspended.

--- src/xpdev/clx/MainUnit.cpp
+++ src/xpdev/clx/MainUnit.cpp
@@ -10,7 +10,8 @@
 #pragma argsused
 int main(int argc, char* argv[])
 {
-	TTestThread* Thread=new TTestThread(false /* create suspended? */);
+	TTestThread* Thread=new TTestThread(true /* create suspended? */);
+    Thread->Resume();
     Thread->ChildEvent->WaitFor(INFINITE);	/* wait for thread to begin */
     Thread->ChildEvent->ResetEvent();
     for(int i=0;i<10;i++) {
