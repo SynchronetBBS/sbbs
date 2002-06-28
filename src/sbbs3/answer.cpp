5e74ebd2879cd50ac54345fcc270938dd36ae2d8
rswindell <>
2002-06-28 00:21:56 +0000

    Fixed mispelling in comment (whoa!).

--- src/sbbs3/answer.cpp
+++ src/sbbs3/answer.cpp
@@ -105,7 +105,7 @@ bool sbbs_t::answer()
 		/* Disable Telnet Terminal Echo */
 		sprintf(str,"%c%c%c",TELNET_IAC,TELNET_WILL,TELNET_ECHO);
 		putcom(str,3);
-		/* Will supress Go Ahead */
+		/* Will suppress Go Ahead */
 		sprintf(str,"%c%c%c",TELNET_IAC,TELNET_WILL,TELNET_SUP_GA);
 		putcom(str,3);
 	}
