8c53563879b63448a54e1063674d8c1a379b576a
rswindell <>
2002-08-08 07:36:52 +0000

    Fixed a few improper uses of the prep_dir() function.

--- src/sbbs3/load_cfg.c
+++ src/sbbs3/load_cfg.c
@@ -142,9 +142,9 @@ void prep_cfg(scfg_t* cfg)
 	prep_dir(cfg->ctrl_dir, cfg->exec_dir);
 	prep_dir(cfg->ctrl_dir, cfg->text_dir);
 
-	prep_dir(cfg->netmail_dir,cfg->data_dir);
-	prep_dir(cfg->echomail_dir,cfg->data_dir);
-	prep_dir(cfg->fidofile_dir,cfg->data_dir);
+	prep_dir(cfg->ctrl_dir, cfg->netmail_dir);
+	prep_dir(cfg->ctrl_dir, cfg->echomail_dir);
+	prep_dir(cfg->ctrl_dir, cfg->fidofile_dir);
 
 	prep_path(cfg->netmail_sem);
 	prep_path(cfg->echomail_sem);
