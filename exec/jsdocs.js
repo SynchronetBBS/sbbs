2594c0c36c6b3f0bbc7b85a5552edbaa5c6d3c69
rswindell <>
2002-08-30 02:27:09 +0000

    Script to generate object model documentation (eventually in HTML). Currently, it just enumerates the properties of the built-in objects.

---
+++ exec/jsdocs.js
@@ -0,0 +1,29 @@
+// jsdocs.js
+
+// This script will (eventually be used to) generated HTML documentation of the
+// Synchronet JavaScript object model
+
+function document_object(name,obj)
+{
+	var prop_name;
+
+	for(prop in obj) {
+		prop_name=name + "." + prop;
+		printf("%-8s %s\r\n",typeof(obj[prop]),prop_name);
+		if(typeof(obj[prop])=="object") {
+			if(obj[prop].length != undefined)	// array ?
+				document_object(prop_name + "[]",obj[prop][0]);
+			else
+				document_object(prop_name,obj[prop]);
+		}
+	}
+}
+
+document_object("client"	,client);
+document_object("system"	,system);
+document_object("bbs"		,bbs);
+document_object("user"		,user);
+document_object("server"	,server);
+document_object("console"	,console);
+document_object("msg_area"	,msg_area);
+document_object("file_area"	,file_area);
