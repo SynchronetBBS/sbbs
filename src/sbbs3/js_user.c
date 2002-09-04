d031dfeff98260bd3f034c7a58f698c64eb7f1b3
rswindell <>
2002-09-04 09:54:16 +0000

    Replaced missing comma in js_user_functions definition.

--- src/sbbs3/js_user.c
+++ src/sbbs3/js_user.c
@@ -640,7 +640,7 @@ js_chk_ar(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 
 static jsMethodSpec js_user_functions[] = {
 	{"compare_ars",	js_chk_ar,			1,	JSTYPE_BOOLEAN,	"string ars"
-	"Verify user meets access requirements string"
+	,"Verify user meets access requirements string"
 	},		
 	{0}
 };
