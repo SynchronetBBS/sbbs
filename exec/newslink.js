fa47f88ec77e22eae9ff20bab18216c63ec398e9
rswindell <>
2002-09-15 00:26:33 +0000

    Truncates spaces off ends of header lines. Now stores "reply-to" header field (if present) in SMB header. Adds "reply-to" header to exported messages (if present in SMB header).

--- exec/newslink.js
+++ exec/newslink.js
@@ -319,6 +319,8 @@ for(i in area) {
 		if(hdr.newsgroups==undefined)
 			hdr.newsgroups=newsgroup;
 		writeln("Newsgroups: " + hdr.newsgroups);
+		if(hdr.replyto!=undefined)
+			writeln("Reply-To: " + hdr.replyto);
 		if(hdr.reply_id!=undefined)
 			writeln("References: " + hdr.reply_id);
 		writeln("X-Gateway: "
@@ -446,12 +448,14 @@ for(i in area) {
 				continue;
 
 			data=line.slice(sp+1);
-			while(data.charAt(0)==' ')	// skip prepended spaces
+			while(data.charAt(0)==' ')	// trim prepended spaces
 				data=data.slice(1);
+			data=truncsp(data);			// trim trailing spaces
 
 			line=line.substr(0,sp);
-			while(line.charAt(0)==' ')	// skip prepended spaces
+			while(line.charAt(0)==' ')	// trim prepended spaces
 				line=line.slice(1);
+			line=truncsp(line);			// trim trailing spaces
 
 			switch(line.toLowerCase()) {
 				case "to":
@@ -469,6 +473,10 @@ for(i in area) {
 					break;
 				case "from":
 					hdr.from=data;
+					break;
+				case "reply-to"
+					hdr.replyto_net_type=NET_INTERNET;
+					hdr.replyto=data;
 					break;
 				case "date":
 					hdr.date=data;
