ba3b5fe12459c61e5ae375d4a3ee328f9c3f5fc1
rswindell <>
2002-10-25 08:46:57 +0000

    Event script to check free disk space and send warning e-mail to sysop.

---
+++ exec/chkspace.js
@@ -0,0 +1,23 @@
+// chkspace.js
+
+// Requires minimum free disk space (in megabytes) as first argument
+
+// Example: "?chkspace 100"
+
+freespace = system.freediskspace;
+
+if(freespace==-1 || freespace > Number(argv[0])*1024*1024)
+	exit();	// everything's fine
+
+log("!Low disk space: " + freespace + " bytes");
+
+msgbase = new MsgBase("mail");
+
+hdr = { to: 'sysop', to_ext: '1', from: 'chkspace', subject: 'Low disk space notification' }
+
+if(!msgbase.save_msg(hdr, "Only " + freespace + " bytes of free disk space!"))
+	log("!Error " + msgbase.last_error + "saving mail message");
+
+log("E-mailed low disk space notification to sysop");
+
+msgbase.close();
