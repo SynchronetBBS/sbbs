18bf244a7502b21acd764c5cbb3687c8f899a6cb
rswindell <>
2002-10-25 02:07:00 +0000

    Added Continue? questions during new user procedure to give user an "opt-out".

--- src/sbbs3/newuser.cpp
+++ src/sbbs3/newuser.cpp
@@ -38,6 +38,8 @@
 #include "sbbs.h"
 #include "cmdshell.h"
 
+static char* ContinueText = "Continue";
+
 /****************************************************************************/
 /* This function is invoked when a user enters "NEW" at the NN: prompt		*/
 /* Prompts user for personal information and then sends feedback to sysop.  */
@@ -204,6 +206,8 @@ BOOL sbbs_t::newuser()
 					|| trashcan(useron.alias,"name")
 					|| (!(cfg.uq&UQ_ALIASES) && !strchr(useron.alias,SP))) {
 					bputs(text[YouCantUseThatName]);
+					if(!yesno(ContinueText))
+						return(FALSE);
 					continue; 
 				}
 				break; 
@@ -223,6 +227,8 @@ BOOL sbbs_t::newuser()
 					bputs(text[YouCantUseThatName]);
 				else
 					break; 
+				if(!yesno(ContinueText))
+					return(FALSE);
 			} 
 		}
 		else if(cfg.uq&UQ_COMPANY) {
@@ -245,6 +251,8 @@ BOOL sbbs_t::newuser()
 				bputs(text[YouCantUseThatName]);
 			else
 				break; 
+			if(!yesno(ContinueText))
+				return(FALSE);
 		}
 		if(!online) return(FALSE);
 		if(cfg.uq&UQ_ADDRESS)
