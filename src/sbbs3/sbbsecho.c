79528ca031d3fb29dce7874ac3f428835a07ab9c
rswindell <>
2002-11-10 07:38:07 +0000

    Adds tosser ID (TID) when importing messages.

--- src/sbbs3/sbbsecho.c
+++ src/sbbs3/sbbsecho.c
@@ -81,6 +81,7 @@ faddr_t		sys_faddr;
 config_t	cfg;
 scfg_t		scfg;
 char		revision[16];
+char		compiler[32];
 
 BOOL pause_on_exit=FALSE;
 
@@ -2648,6 +2649,12 @@ int fmsgtosmsg(uchar HUGE16 *fbuf, fmsghdr_t fmsghdr, uint user, uint subnum)
 	if(taillen)
 		smb_dfield(&msg,TEXT_TAIL,taillen+2);
 
+	if(smb_get_hfield(&msg,FIDOTID,NULL)==NULL) { /* generate TID */
+		sprintf(str,"SBBSecho %s-%s r%s %s %s"
+			,SBBSECHO_VER,PLATFORM_DESC,revision,__DATE__,compiler);
+		smb_hfield(&msg,FIDOTID,(ushort)strlen(str),str);
+	}
+
 	i=smb_addmsghdr(smbfile,&msg,storage);
 	smb_freemsgmem(&msg);
 	if(i) {
@@ -3564,7 +3571,6 @@ int import_netmail(char *path,fmsghdr_t hdr, FILE *fidomsg)
 void export_echomail(char *sub_code,faddr_t addr)
 {
 	char	str[1025],tear,cr;
-	char	compiler[32];
 	char*	buf=NULL;
 	char*	minus;
 	uchar*	fmsgbuf=NULL;
@@ -3582,8 +3588,6 @@ void export_echomail(char *sub_code,faddr_t addr)
 	areasbbs_t fakearea;
 	addrlist_t msg_seen,msg_path;
     clock_t start_tick=0,export_ticks=0;
-
-	DESCRIBE_COMPILER(compiler);
 
 	memset(&msg_seen,0,sizeof(addrlist_t));
 	memset(&msg_path,0,sizeof(addrlist_t));
@@ -3958,6 +3962,8 @@ int main(int argc, char **argv)
 	memset(&fakearea,0,sizeof(areasbbs_t));
 
 	sscanf("$Revision$" + 11, "%s", revision);
+
+	DESCRIBE_COMPILER(compiler);
 
 	printf("\nSBBSecho v%s-%s (rev %s) - Synchronet FidoNet Packet "
 		"Tosser\n"
