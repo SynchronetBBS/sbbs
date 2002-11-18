78cc6f60ead45861d8b6f564ed1ae989b7f30b01
rswindell <>
2002-11-18 08:09:54 +0000

    No longer reports buffer full errors. Only allow output buffer to fill 50% from VXD before wwiv/telnet expansion.

--- src/sbbs3/xtrn.cpp
+++ src/sbbs3/xtrn.cpp
@@ -705,9 +705,11 @@ int sbbs_t::external(char* cmdline, long mode, char* startup_dir)
 				rd=0;
 				len=sizeof(buf);
 				avail=RingBufFree(&outbuf)/2;	// leave room for wwiv/telnet expansion
+#if 0
 				if(avail==0)
 					lprintf("Node %d !output buffer full (%u bytes)"
 						,cfg.node_num,RingBufFull(&outbuf));
+#endif
 				if(len>avail)
             		len=avail;
 
@@ -782,14 +784,16 @@ int sbbs_t::external(char* cmdline, long mode, char* startup_dir)
         		/* Read from VXD */
 				rd=0;
 				len=sizeof(buf);
-				avail=RingBufFree(&outbuf);
+				avail=RingBufFree(&outbuf)/2;	// leave room for wwiv/telnet expansion
+#if 0
 				if(avail==0) 
 					lprintf("Node %d !output buffer full (%u bytes)"
 						,cfg.node_num,RingBufFull(&outbuf));
+#endif
 
 				if(len>avail)
             		len=avail;
-				if(avail>=RingBufFull(&outbuf)) {
+				if(len) {
 					if(!DeviceIoControl(
 						vxd,					// handle to device of interest
 						SBBSEXEC_IOCTL_READ,	// control code of operation to perform
@@ -1171,8 +1175,10 @@ int sbbs_t::external(char* cmdline, long mode, char* startup_dir)
 
 			avail=RingBufFree(&outbuf)/2;	// Leave room for wwiv/telnet expansion
 			if(avail==0) {
+#if 0
 				lprintf("Node %d !output buffer full (%u bytes)"
 						,cfg.node_num,RingBufFull(&outbuf));
+#endif
 				mswait(1);
 				continue;
 			}
