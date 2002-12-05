620c1e532bc94b3061592d5dc93608cffb8238be
rswindell <>
2002-12-05 08:40:42 +0000

    Deuce mod: added printable char after ESC[99B to fix problem detecting number of rows for Windows 2K/XP telnet client (reports 105 lines without the char).

--- src/sbbs3/answer.cpp
+++ src/sbbs3/answer.cpp
@@ -113,7 +113,7 @@ bool sbbs_t::answer()
 	rioctl(IOFI);		/* flush input buffer */
 	putcom( "\r\n"		/* locate cursor at column 1 */
 			"\x1b[s"	/* save cursor position (necessary for HyperTerm auto-ANSI) */
-    		"\x1b[99B"	/* locate cursor as far down as possible */
+    		"\x1b[99B_"	/* locate cursor as far down as possible */
 			"\x1b[6n"	/* Get cursor position */
 			"\x1b[u"	/* restore cursor position */
 			"\x1b[!_"	/* RIP? */
