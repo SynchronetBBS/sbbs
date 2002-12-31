e4431d2cd9a6c6c899a3fe4dcdf977d8ac6ef050
rswindell <>
2002-12-31 00:50:25 +0000

    Unix version of external() now: - disables Ctrl-C checking in input_thread (sets rio_abortable to false) - sends Telnet "DONT BINARY" command after executing external program - forces text attributes back to lightgray after executing external program

--- src/sbbs3/xtrn.cpp
+++ src/sbbs3/xtrn.cpp
@@ -1051,6 +1051,7 @@ int sbbs_t::external(char* cmdline, long mode, char* startup_dir)
 	ulong	avail;
     ulong	output_len;
 	bool	native=false;			// DOS program by default
+	bool	rio_abortable_save;
 	int		i;
 	int		rd;
 	int		wr;
@@ -1236,6 +1237,12 @@ int sbbs_t::external(char* cmdline, long mode, char* startup_dir)
 	}
 
 	lprintf("Node %d executing external: %s",cfg.node_num,cmdline);
+
+	/* Disable Ctrl-C checking */
+	if(!(mode&EX_OFFLINE)) {
+		rio_abortable_save=rio_abortable;
+		rio_abortable=false;
+	}
 	
 	if(mode&EX_OUTR) {
 		if(!(mode&EX_INR))
@@ -1328,6 +1335,18 @@ int sbbs_t::external(char* cmdline, long mode, char* startup_dir)
 	}
 
 	waitpid(pid, &i, 0); /* Wait for child to terminate */
+
+	if(!(mode&EX_OFFLINE)) {	/* !off-line execution */
+
+		curatr=~0;			// Can't guarantee current attributes
+		attr(LIGHTGRAY);	// Force to "normal"
+
+		rio_abortable=rio_abortable_save;	// Restore abortable state
+
+		/* Got back to Text/NVT mode */
+		send_telnet_cmd(TELNET_DONT,TELNET_BINARY);
+		telnet_mode&=~TELNET_MODE_BIN_RX;
+	}
 
 	pthread_mutex_unlock(&input_thread_mutex);
 
