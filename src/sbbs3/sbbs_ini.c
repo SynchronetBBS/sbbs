d205041d921c8bdabd7019252cf4992aeb6a093f
rswindell <>
2003-01-17 23:54:56 +0000

    Added default service.cfg and mailproc.cfg file names. Added copyright comment header.

--- src/sbbs3/sbbs_ini.c
+++ src/sbbs3/sbbs_ini.c
@@ -1,5 +1,40 @@
 /* sbbs_ini.c */
 
+/* Synchronet console configuration (.ini) file routines */
+
+/* $Id$ */
+
+/****************************************************************************
+ * @format.tab-size 4		(Plain Text/Source Code File Header)			*
+ * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
+ *																			*
+ * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
+ *																			*
+ * This program is free software; you can redistribute it and/or			*
+ * modify it under the terms of the GNU General Public License				*
+ * as published by the Free Software Foundation; either version 2			*
+ * of the License, or (at your option) any later version.					*
+ * See the GNU General Public License for more details: gpl.txt or			*
+ * http://www.fsf.org/copyleft/gpl.html										*
+ *																			*
+ * Anonymous FTP access to the most recent released source is available at	*
+ * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
+ *																			*
+ * Anonymous CVS access to the development source and modification history	*
+ * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
+ * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
+ *     (just hit return, no password is necessary)							*
+ * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src		*
+ *																			*
+ * For Synchronet coding style and modification guidelines, see				*
+ * http://www.synchro.net/source.html										*
+ *																			*
+ * You are encouraged to submit any modifications (preferably in Unix diff	*
+ * format) via e-mail to mods@synchro.net									*
+ *																			*
+ * Note: If this box doesn't appear square, then you need to fix your tabs.	*
+ ****************************************************************************/
+
 #include "sbbs_ini.h"
 
 static const char*	nulstr="";
@@ -272,7 +307,7 @@ void sbbs_read_ini(
 		,iniReadString(fp,section,"OutboundSound",nulstr));
 
 	SAFECOPY(mail->proc_cfg_file
-		,iniReadString(fp,section,"ProcessConfigFile",nulstr));
+		,iniReadString(fp,section,"ProcessConfigFile","mailproc.cfg"));
 
 	mail->options
 		=iniReadBitField(fp,section,"Options",mail_options
@@ -294,7 +329,7 @@ void sbbs_read_ini(
 		,iniReadString(fp,section,"HostName",host_name));
 
 	SAFECOPY(services->cfg_file
-		,iniReadString(fp,section,"ConfigFile",nulstr));
+		,iniReadString(fp,section,"ConfigFile","services.cfg"));
 
 	SAFECOPY(services->answer_sound
 		,iniReadString(fp,section,"AnswerSound",nulstr));
