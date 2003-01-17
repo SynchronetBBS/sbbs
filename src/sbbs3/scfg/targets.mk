5f8924b00507281b40857bd999b853a3360e221d
rswindell <>
2003-01-17 22:31:22 +0000

    Added a MAKEHELP target (to build the makehelp util in the correct output dir).

--- src/sbbs3/scfg/targets.mk
+++ src/sbbs3/scfg/targets.mk
@@ -7,6 +7,7 @@
 # ODIR, SLASH, LIBFILE, EXEFILE, and DELETE must be pre-defined
 
 SCFG	=	$(ODIR)$(SLASH)scfg$(EXEFILE) 
+MAKEHELP=	$(ODIR)$(SLASH)makehelp$(EXEFILE) 
 SCFGHELP=	$(ODIR)$(SLASH)scfghelp.dat
 
 all:	$(ODIR) \
