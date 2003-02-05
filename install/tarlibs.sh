08fdf0ec6e6f282ddd13cea48bcf8b70718fd76b
rswindell <>
2003-02-05 08:22:53 +0000

    Unix shell scripts to create lib and source tarballs.

---
+++ install/tarlibs.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+tar cvzf lib-linux.tgz lib/mozilla/js/linux.*/libjs.a
+tar cvzf lib-freebsd.tgz lib/mozilla/js/freebsd.*/libjs.a
+tar cvzf lib-openbsd.tgz lib/mozilla/js/openbsd.*/libjs.a
+tar cvzf lib-unix.tgz lib/mozilla/js/*/libjs.a
---
+++ install/tarsrc.sh
@@ -0,0 +1,2 @@
+#/bin/sh
+tar -cvzf sbbs_src.tgz -X install/sbbs_src.exclude -T install/sbbs_src.lst
