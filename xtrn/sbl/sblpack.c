295d598a013622162113bd3bce3fe7674d79e85d
rswindell <>
2003-02-28 09:07:56 +0000

    Ported to xpdev.

--- xtrn/sbl/sblpack.c
+++ xtrn/sbl/sblpack.c
@@ -1,18 +1,46 @@
-/* SBLPACK.C */
+/* sblpack.c */
 
-/* Developed 1990-1997 by Rob Swindell; PO Box 501, Yorba Linda, CA 92885 */
-
-/***************************************/
 /* Synchronet BBS List Database Packer */
-/***************************************/
+
+/* $Id$ */
+
+/****************************************************************************
+ * @format.tab-size 4		(Plain Text/Source Code File Header)			*
+ * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
+ *																			*
+ * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ *																			*
+ * This program is free software; you can redistribute it and/or			*
+ * modify it under the terms of the GNU General Public License				*
+ * as published by the Free Software Foundation; either version 2			*
+ * of the License, or (at your option) any later version.					*
+ * See the GNU General Public License for more details: gpl.txt or			*
+ * http://www.fsf.org/copyleft/gpl.html										*
+ *																			*
+ * Anonymous FTP access to the most recent released source is available at	*
+ * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
+ *																			*
+ * Anonymous CVS access to the development source and modification history	*
+ * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
+ * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
+ *     (just hit return, no password is necessary)							*
+ * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout xtrn	*
+ *																			*
+ * For Synchronet coding style and modification guidelines, see				*
+ * http://www.synchro.net/source.html										*
+ *																			*
+ * You are encouraged to submit any modifications (preferably in Unix diff	*
+ * format) via e-mail to mods@synchro.net									*
+ *																			*
+ * Note: If this box doesn't appear square, then you need to fix your tabs.	*
+ ****************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>		/* exit() */
-#include <share.h>
 #include <time.h>
-#include <io.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include "filewrap.h"	/* sopen() */
 #include "xsdkwrap.h"	/* PLATFORM_DESC */
 #include "xsdkdefs.h"
 #include "sbldefs.h"
@@ -25,9 +53,12 @@ int main(void)
 	bbs_t	bbs;
 	ulong	records=0;
 	ulong	deleted=0;
+	char	revision[16];
 
-	printf("\nSBLPACK v1.10/%s  Developed 1995-2001 Rob Swindell\n\n"
-		,PLATFORM_DESC);
+	sscanf("$Revision$", "%*s %s", revision);
+
+	printf("\nSBLPACK %s-%s  Copyright 2003 Rob Swindell\n\n"
+		,revision,PLATFORM_DESC);
 
 	if((file=sopen("sbl.dab",O_RDWR|O_BINARY,SH_DENYNO))==-1) {
 		printf("\n\7Error opening/creating sbl.dab\n");
