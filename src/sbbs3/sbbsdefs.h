9e0772087480646e0415a0fb9244240c91d9f62c
rswindell <>
2003-03-06 03:11:16 +0000

    Increase rev to 'm'.

--- src/sbbs3/sbbsdefs.h
+++ src/sbbs3/sbbsdefs.h
@@ -47,7 +47,7 @@
 /*************/
 
 #define VERSION 	"3.10"  /* Version: Major.minor  */
-#define REVISION	'L'     /* Revision: lowercase letter */
+#define REVISION	'm'     /* Revision: lowercase letter */
 
 #define VERSION_NOTICE		"Synchronet BBS for "PLATFORM_DESC\
 								"  Version " VERSION
