1332f42f07104043a0756139351723d0c5acd855
deuce <>
2003-03-29 04:52:53 +0000

    Add missing include.

--- src/encode/base64.c
+++ src/encode/base64.c
@@ -35,6 +35,7 @@
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
 
+#include <stdlib.h>
 #include <string.h>
 #include "base64.h"
 
