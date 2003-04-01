15d3225eda8551014f2befd873654218153c4467
rswindell <>
2003-04-01 09:38:43 +0000

    getnodedat() now zeroes node struct before reading or erroring-out.

--- src/sbbs3/userdat.c
+++ src/sbbs3/userdat.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -646,9 +646,9 @@ int DLLCALL getnodedat(scfg_t* cfg, uint number, node_t *node, int* fp)
 		|| node==NULL || number<1 || number>cfg->sys_nodes)
 		return(-1);
 
+	memset(node,0,sizeof(node_t));
 	sprintf(str,"%snode.dab",cfg->ctrl_dir);
 	if((file=nopen(str,O_RDWR|O_DENYNONE))==-1) {
-		memset(node,0,sizeof(node_t));
 		if(fp!=NULL)
 			*fp=file;
 		return(errno); 
