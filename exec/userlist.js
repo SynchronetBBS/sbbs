a7d5afaaaae05d52a006f3d91f0aec0ce8074e59
rswindell <>
2003-04-02 00:37:42 +0000

    No longer uses "user" as temporary user object variable name (conflicts with global user object).

--- exec/userlist.js
+++ exec/userlist.js
@@ -1,17 +1,17 @@
 load("sbbsdefs.js");
 
 var lastuser=system.stats.total_users;
-var user = new User(1);
+var u = new User(1);
 
 for(i=1;i<=lastuser;i++) {
-	user.number=i;
-	if(user.settings&USER_DELETED)
+	u.number=i;
+	if(u.settings&USER_DELETED)
 		continue;
 	printf("%d/%d ",i,lastuser);
 	printf("%-30s %-30s %s\r\n"
-		,user.alias
-		,user.location
-		,user.connection
+		,u.alias
+		,u.location
+		,u.connection
 		);
 	if(bbs.sys_status&SS_ABORT)
 		break;
