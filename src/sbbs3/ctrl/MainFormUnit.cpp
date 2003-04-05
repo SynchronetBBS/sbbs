9f5493cfa636465f8c129296192c7a6ac312a624
rswindell <>
2003-04-05 02:51:05 +0000

    Using new total_users function (instead of lastuser()) to get total users stat.

--- src/sbbs3/ctrl/MainFormUnit.cpp
+++ src/sbbs3/ctrl/MainFormUnit.cpp
@@ -1039,10 +1039,6 @@ void __fastcall TMainForm::FtpCloseButtonClick(TObject *Sender)
 	ViewFtpServerExecute(Sender);
 }
 //---------------------------------------------------------------------------
-
-
-#define STAT_DESC_LEN 23
-
 void __fastcall TMainForm::StatsTimerTick(TObject *Sender)
 {
 	char 	str[128];
@@ -1059,7 +1055,7 @@ void __fastcall TMainForm::StatsTimerTick(TObject *Sender)
 	StatsForm->EMailToday->Caption=AnsiString(stats.etoday);
 	StatsForm->TotalFeedback->Caption=AnsiString(getmail(&cfg,1,0));
 	StatsForm->FeedbackToday->Caption=AnsiString(stats.ftoday);
-    StatsForm->TotalUsers->Caption=AnsiString(lastuser(&cfg));
+    StatsForm->TotalUsers->Caption=AnsiString(total_users(&cfg));
     StatsForm->NewUsersToday->Caption=AnsiString(stats.nusers);
     StatsForm->PostsToday->Caption=AnsiString(stats.ptoday);
     StatsForm->UploadedFiles->Caption=AnsiString(stats.uls);
