d2fe390fc277803af2c8664a27ba5f72340e47df
rswindell <>
2003-04-23 08:32:41 +0000

    Added optional temp_dir field.

--- src/sbbs3/ftpsrvr.h
+++ src/sbbs3/ftpsrvr.h
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -78,7 +78,7 @@ typedef struct {
     char	html_index_file[64];
     char	html_index_script[64];
     char	reserved_path8[64];
-    char	reserved_path7[128];
+    char	temp_dir[128];
     char	reserved_path6[128];
     char	reserved_path5[128];
     char	reserved_path4[128];
