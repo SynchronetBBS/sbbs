c39851555df513d39237b6ea57b6ea04cf7f66da
rswindell <>
2003-05-02 21:37:43 +0000

    Removed loop from xp_sem_timedwait.

--- src/xpdev/xpsem.c
+++ src/xpdev/xpsem.c
@@ -249,14 +249,12 @@ xp_sem_timedwait(xp_sem_t *sem, const struct timespec *abs_timeout)
 
 	pthread_mutex_lock(&(*sem)->lock);
 
-	while ((*sem)->count == 0) {
+	if((*sem)->count == 0) {
 		(*sem)->nwaiters++;
-		retval=retval=pthread_cond_timedwait(&(*sem)->gtzero, &(*sem)->lock, abs_timeout);
+		retval=pthread_cond_timedwait(&(*sem)->gtzero, &(*sem)->lock, abs_timeout);
 		(*sem)->nwaiters--;
-		if(retval)
-			break;
 	}
-	if(!retval)
+	if(retval==0)
 		(*sem)->count--;
 
 	pthread_mutex_unlock(&(*sem)->lock);
