459d55e8ad9e52ab544fbc1583eb7e3267ebae80
rswindell <>
2003-05-04 10:39:03 +0000

    Spy form window now clears the terminal when it detects a form-feed character (Ctrl-L).

--- src/sbbs3/ctrl/SpyFormUnit.cpp
+++ src/sbbs3/ctrl/SpyFormUnit.cpp
@@ -6,7 +6,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -60,13 +60,17 @@ __fastcall TSpyForm::TSpyForm(TComponent* Owner)
     ActiveControl=Terminal;
 }
 //---------------------------------------------------------------------------
-int strip_telnet(uchar *buf, int len)
+int __fastcall TSpyForm::strip_telnet(uchar *buf, int len)
 {
     int i;
     int telnet_cmd=0;
     int newlen=0;
 
     for(i=0;i<len;i++) {
+		if(buf[i]==FF) {
+			Terminal->Clear();
+			continue;
+		}
         if(buf[i]==TELNET_IAC || telnet_cmd) {
             if(telnet_cmd==1 && buf[i]==TELNET_IAC) {
                 telnet_cmd=0;   /* escape IAC */
--- src/sbbs3/ctrl/SpyFormUnit.h
+++ src/sbbs3/ctrl/SpyFormUnit.h
@@ -6,7 +6,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -74,6 +74,7 @@ __published:	// IDE-managed Components
     void __fastcall FormMouseUp(TObject *Sender, TMouseButton Button,
           TShiftState Shift, int X, int Y);
 private:	// User declarations
+	int __fastcall strip_telnet(uchar *buf, int len);
 public:		// User declarations
     TEmulVT*    Terminal;
     RingBuf**   inbuf;
