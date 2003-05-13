956d1533cd42fa7034a03f34119ca4a8a6b45a24
rswindell <>
2003-05-13 05:07:40 +0000

    send_telnet_cmd() does nothing on an RLogin connection (RLogin clients do not use or recognize telnet commands).

--- src/sbbs3/main.cpp
+++ src/sbbs3/main.cpp
@@ -909,6 +909,9 @@ static BYTE* telnet_interpret(sbbs_t* sbbs, BYTE* inbuf, int inlen,
 void sbbs_t::send_telnet_cmd(uchar cmd, uchar opt)
 {
 	char buf[16];
+	
+	if(sys_status&SS_RLOGIN)	
+		return; /* RLogin does not use Telnet commands */
 
 	if(cmd<TELNET_WILL) {
 		if(startup->options&BBS_OPT_DEBUG_TELNET)
