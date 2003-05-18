8d92b5a658fc06ae9f682d043b6e21b51a654b28
deuce <>
2003-05-18 07:54:47 +0000

    Initial support for NetBSDs native curses.

--- src/sbbs3/umonitor/chat.c
+++ src/sbbs3/umonitor/chat.c
@@ -218,7 +218,11 @@ int chat(scfg_t *cfg, int nodenum, node_t *node, box_t *boxch, void(*timecallbac
 	FREE(p);
 	lseek(in,0,SEEK_SET);
 	lseek(out,0,SEEK_SET);
+	#ifdef __NetBSD__
+	timeout(100);
+	#else
 	halfdelay(1);
+	#endif
 
 	togglechat(cfg,nodenum,node,TRUE);
 
@@ -279,7 +283,9 @@ int chat(scfg_t *cfg, int nodenum, node_t *node, box_t *boxch, void(*timecallbac
 
 		if((ch=wgetch(swin))) {
 			switch(ch)  {
+				#if ERR!=0
 				case 0:
+				#endif
 				case ERR:
 					ch=0;
 					write(out,&ch,1);
