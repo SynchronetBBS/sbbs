ae226a3291dfde7ce54bc5b8d553b0540f70762d
rswindell <>
2003-06-14 08:41:42 +0000

    Only write the filename (not the entire path) to md5 history file.

--- exec/newslink.js
+++ exec/newslink.js
@@ -493,7 +493,7 @@ for(i in area) {
 					line=line.slice(1);		// Skip prepended dots
 
                 if(flags.indexOf('b')>=0) {        // decode attachments   
-                    if(line.substr(0,6)=="begin ") {   
+                    if(file==undefined && line.substr(0,6)=="begin ") {   
 						 // Parse uuencode header   
 						 arg=line.split(/\s+/);   
 						 arg.splice(0,2);        // strip "begin 666 "   
@@ -568,7 +568,7 @@ for(i in area) {
             }
 			/* Append MD5 to history file */
 			if(md5_file.open("a")) {
-				md5_file.printf("%s %s\n",md5,file.name);
+				md5_file.printf("%s %s\n",md5,file_getname(file.name));
 				md5_file.close();
 			} else
 				printf("!ERROR %d (%s) creating/appending %s\r\n"
