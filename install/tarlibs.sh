a29eab91d02a481032833800d4132cb674c7a2aa
rswindell <>
2003-07-19 22:47:28 +0000

    Updated for .so libraries.

--- install/tarlibs.sh
+++ install/tarlibs.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-tar cvzf lib-linux.tgz lib/mozilla/*/linux.*/*.a
-tar cvzf lib-freebsd.tgz lib/mozilla/*/freebsd.*/*.a
-tar cvzf lib-openbsd.tgz lib/mozilla/*/openbsd.*/*.a
-tar cvzf lib-unix.tgz lib/mozilla/*/*/*.a
+tar cvzf lib-linux.tgz lib/mozilla/*/linux.*/*.so
+tar cvzf lib-freebsd.tgz lib/mozilla/*/freebsd.*/*.so
+tar cvzf lib-openbsd.tgz lib/mozilla/*/openbsd.*/*.so
+tar cvzf lib-unix.tgz lib/mozilla/*/*/*.so
