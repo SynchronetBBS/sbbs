c984d0485081ca67ddcfae6b00e085b5b2ed6ee1
rswindell <>
2003-07-23 11:28:45 +0000

    Expanded description of editfile() method.

--- src/sbbs3/js_console.cpp
+++ src/sbbs3/js_console.cpp
@@ -1201,7 +1201,7 @@ static jsMethodSpec js_console_functions[] = {
 	,JSDOCSTR("print last x lines of file with optional mode")
 	},		
 	{"editfile",		js_editfile,		1, JSTYPE_VOID,		JSDOCSTR("string filename")
-	,JSDOCSTR("edit a text file")
+	,JSDOCSTR("edit/create a text file using the user's preferred message editor")
 	},		
 	{"uselect",			js_uselect,			0, JSTYPE_NUMBER,	JSDOCSTR("[number, string title, string item, string ars]")
 	,JSDOCSTR("user selection menu, call for each item, then with no args to display select menu")
