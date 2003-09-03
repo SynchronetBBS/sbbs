afbfe9e2d1b54a3d9d4b1e64f8911567c66d7be7
deuce <>
2003-09-03 22:20:47 +0000

    NetBSD and OpenBSD can't use the Linux/FreeBSD console ioctl to make noise

--- src/xpdev/genwrap.c
+++ src/xpdev/genwrap.c
@@ -157,7 +157,7 @@ void DLLCALL unix_beep(int freq, int dur)
 	}
 #endif
 
-#if !defined(__GNU__) && !defined(__QNX__)
+#if !defined(__GNU__) && !defined(__QNX__) && !defined(__OpenBSD__) && !defined(NetBSD)
 	if(console_fd == -1) 
   		console_fd = open("/dev/console", O_NOCTTY);
 	
