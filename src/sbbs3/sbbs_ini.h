e4db80e435dd121cc364df1c1095836bfde9f7f7
rswindell <>
2003-09-16 21:53:22 +0000

    Moved ini field string constants from sbbs_ini.c.

--- src/sbbs3/sbbs_ini.h
+++ src/sbbs3/sbbs_ini.h
@@ -45,6 +45,14 @@
 #include "websrvr.h"	/* services_startup_t */
 #include "ini_file.h"
 
+static const char* strJavaScriptMaxBytes		="JavaScriptMaxBytes";
+static const char* strJavaScriptContextStack	="JavaScriptContextStack";
+static const char* strJavaScriptBranchLimit		="JavaScriptBranchLimit";
+static const char* strJavaScriptGcInterval		="JavaScriptGcInterval";
+static const char* strJavaScriptYieldInterval	="JavaScriptYieldInterval";
+static const char* strSemFileCheckFrequency		="SemFileCheckFrequency";
+
+
 #if defined(__cplusplus)
 extern "C" {
 #endif
