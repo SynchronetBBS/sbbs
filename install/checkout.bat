1aec4007e8cbadc40a3a157ee8124c69545a6ab2
rswindell <>
2003-09-19 06:09:33 +0000

    Uses new module aliases to pull all the right directories for build-Win32.

--- install/checkout.bat
+++ install/checkout.bat
@@ -1,13 +1,8 @@
 @echo off
 echo Synchronet source code check-out for Win32
-echo $id: $
+echo $Id$
 setlocal
 set HOME=c:\
 set CVSROOT=:pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs
-cvs co src/uifc
-cvs co src/xpdev
-cvs co src/sbbs3
-cvs co include
-cvs co lib/fltk/win32 
-cvs co lib/mozilla/js/win32.release
-cvs co lib/mozilla/nspr/win32.release
+cvs co src-sbbs3
+cvs co lib-win32 
