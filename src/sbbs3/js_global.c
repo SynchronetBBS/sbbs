8f8ef7d2aa6fd878809585fa1431362321111579
rswindell <>
2003-12-02 02:01:47 +0000

    socket_select() now properly handles undefined values in the socket array.

--- src/sbbs3/js_global.c
+++ src/sbbs3/js_global.c
@@ -2007,23 +2007,21 @@ js_socket_select(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *r
         if(!JS_GetElement(cx, inarray, i, &val))
 			break;
 		sock=js_socket(cx,val);
-		FD_SET(sock,&socket_set);
-		if(sock>maxsock)
-			maxsock=sock;
 		index[i]=sock;
+		if(sock!=INVALID_SOCKET) {
+			FD_SET(sock,&socket_set);
+			if(sock>maxsock)
+				maxsock=sock;
+		}
     }
 
 	if(select(maxsock+1,rd_set,wr_set,NULL,&tv)<0)
-	{
 		lprintf(LOG_DEBUG,"Error in socket_select()  %s (%d)",strerror(errno),errno);
-	}
 
-	for(i=0;i<limit;i++)
-	{
-		if(FD_ISSET(index[i],&socket_set))
-		{
+	for(i=0;i<limit;i++) {
+		if(index[i]!=INVALID_SOCKET && FD_ISSET(index[i],&socket_set)) {
 			val=INT_TO_JSVAL(i);
-    	    if(!JS_SetElement(cx, rarray, len++, &val))
+   			if(!JS_SetElement(cx, rarray, len++, &val))
 				break;
 		}
 	}
