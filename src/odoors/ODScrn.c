1669e30f029331b0ff55869c573b859beea773c1
deuce <>
2004-03-06 12:05:56 +0000

    Revert last change... didn't work

--- src/odoors/ODScrn.c
+++ src/odoors/ODScrn.c
@@ -943,14 +943,17 @@ tODResult ODScrnInitialize(void)
 {
    BOOL bClear = TRUE;
 
-#if defined(ODPLAT_DOS)
+#if defined(ODPLAT_DOS) || defined(ODPLAT_NIX)
    BYTE btDisplayMode;
 
    /* In silent mode, we perform all output in a block of memory that is */
    /* never displayed.                                                   */
-   /* *nix is always in "silent mode"									 */
+   /* *nix is always in "silent mode"                                   
+                         */
+#ifndef ODPLAT_NIX
    if(od_control.od_silent_mode)
    {
+#endif
       /* Allocate memory for screen buffer, using standard pointer type */
       /* for current memory model.                                      */
       pAllocatedBufferMemory = malloc(SCREEN_BUFFER_SIZE);
@@ -963,6 +966,7 @@ tODResult ODScrnInitialize(void)
       /* Set the screen buffer far pointer to point to the allocated */
       /* buffer.                                                     */
       pScrnBuffer = pAllocatedBufferMemory;
+#ifndef ODPLAT_NIX
    }
    else
    {
@@ -1056,8 +1060,9 @@ tODResult ODScrnInitialize(void)
       }
    }
 #endif /* ODPLAT_DOS */
+#endif /* ODPLAT_DOS/NIX */
 
-#if defined(ODPLAT_WIN32) || defined(ODPLAT_NIX)
+#ifdef ODPLAT_WIN32
    /* Allocate memory for screen buffer. */
    pScrnBuffer = malloc(SCREEN_BUFFER_SIZE);
 
@@ -1101,22 +1106,27 @@ tODResult ODScrnInitialize(void)
  */
 void ODScrnShutdown(void)
 {
-#if defined(ODPLAT_WIN32) || defined(ODPLAT_NIX)
+#ifdef ODPLAT_WIN32
    /* Deallocate screen buffer memory. */
    if(pScrnBuffer != NULL)
    {
       free(pScrnBuffer);
       pScrnBuffer = NULL;
    }
-#else /* !(ODPLAT_WIN32 || ODPLAY_NIX) */
+#else /* !ODPLAT_WIN32 */
    /* In silent mode, we must deallocate screen buffer memory. */
-   /* *nix is always in silent mode							   */
+   /* *nix is always in silent mode                                     
+           */
+#ifndef ODPLAT_NIX
    if(od_control.od_silent_mode && pAllocatedBufferMemory != NULL)
    {
+#endif
       free(pAllocatedBufferMemory);
       pAllocatedBufferMemory = NULL;
       pScrnBuffer = NULL;
+#ifndef ODPLAT_NIX
    }
+#endif
 #endif
 }
 
@@ -1723,7 +1733,8 @@ static void ODScrnScrollUpOneLine(void)
  *             pbtBuffer - A pointer to the buffer to copy to. It is the
  *                         caller's responsibility to ensure that this buffer
  *                         is large enough. This buffer must be at least
- *                         2 x (Width of area) x (Height of area) bytes in size.
+ *                         2 x (Width of area) x (Height of area) bytes in size
+.
  *
  *     Return: TRUE on success, or FALSE on failure.
  */
@@ -1961,7 +1972,8 @@ void ODScrnDisplayBuffer(char *pBuffer, INT nCharsToDisplay)
             {
                btCurrentColumn = 0;
 
-               /* If moving cursor down one line advances past end of window. */
+               /* If moving cursor down one line advances past end of window. *
+/
                if(++btCursorRow > btBottomBoundary - btTopBoundary)
                {
                   /* Move cursor back to bottom line of window. */
@@ -2541,3 +2553,4 @@ void ODScrnRemoveMessage(void *pMessageInfo)
    ODScrnEnableCaret(TRUE);
 #endif /* !ODPLAT_WIN32 */
 }
+
