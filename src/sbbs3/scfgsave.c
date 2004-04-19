a3eb5992598d197e86056779ecf89fb881d9bcef
rswindell <>
2004-04-19 16:21:09 +0000

    Bugfix: write_main_cfg() wasn't saving the user and mail backup levels.

--- src/sbbs3/scfgsave.c
+++ src/sbbs3/scfgsave.c
@@ -348,8 +348,11 @@ BOOL DLLCALL write_main_cfg(scfg_t* cfg, int backup_level)
 	for(i=0;i<158;i++)
 		put_int(n,stream);
 	n=(ushort)0xffff;
-	for(i=0;i<256;i++)
+	for(i=0;i<254;i++)
 		put_int(n,stream);
+
+	put_int(cfg->user_backup_level,stream);
+	put_int(cfg->mail_backup_level,stream);
 
 	n=0;
 	for(i=0;i<10;i++) {
