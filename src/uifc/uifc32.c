27c9f2b3bd160aa3d13920ea0e409c732b51d86b
deuce <>
2004-06-03 05:02:37 +0000

    Fix bug in last commit (Was returning on any keypress including some handled internally)

--- src/uifc/uifc32.c
+++ src/uifc/uifc32.c
@@ -1378,6 +1378,8 @@ int ulist(int mode, int left, int top, int width, int *cur, int *bar
 							return((*cur)|MSK_DEL); 
 						}
 						break;
+					default:
+						return(-2-i);
 				} 
 			}
 			else {
@@ -1518,16 +1520,15 @@ int ulist(int mode, int left, int top, int width, int *cur, int *bar
 								api->savdepth--; 
 							}
 							return(-1);
-				} 
-			} 
+						default:
+							return(-2-i);
+			}
 		}
 		else
 			mswait(1);
 		if(mode&WIN_DYN) {
 			save_menu_cur=*cur;
 			save_menu_bar=*bar;
-		}
-		if(mode&WIN_DYN || mode&WIN_EXTKEYS) {
 			return(-2-i);
 	}
 }
