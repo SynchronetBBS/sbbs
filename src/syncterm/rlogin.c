335f4033deea133fe032c1b2681cdbfcfabc5e9c
deuce <>
2004-06-09 21:09:54 +0000

    Windoes Sockets 2 uses an int pointer as the value to SO_RCVTIMEO, not a struct timeval.

--- src/syncterm/rlogin.c
+++ src/syncterm/rlogin.c
@@ -51,7 +51,11 @@ int rlogin_connect(char *addr, int port, char *ruser, char *passwd)
 	char	nil=0;
 	char	*p;
 	unsigned int	neta;
+#ifdef _WIN32
+	int	tv;
+#else
 	struct	timeval	tv;
+#endif
 
 	for(p=addr;*p;p++)
 		if(*p!='.' && !isdigit(*p))
@@ -92,8 +96,12 @@ int rlogin_connect(char *addr, int port, char *ruser, char *passwd)
 		return(-1);
 	}
 
+#ifdef _WIN32
+	tv=100000;
+#else
 	tv.tv_sec=0;
 	tv.tv_usec=100000;
+#endif
 
 	setsockopt(rlogin_socket, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
 
