d39cd3e3a3e77b633b7702a235a9798e1216d3b8
deuce <>
2004-07-05 08:03:27 +0000

    Make new conio modes conditional.

--- src/uifc/uifc32.c
+++ src/uifc/uifc32.c
@@ -44,7 +44,6 @@
     #define mswait(x) delay(x)
 #elif defined(_WIN32)
 	#include <share.h>
-	#include <conio.h>
 	#include <windows.h>
 	#define mswait(x) Sleep(x)
 #endif
@@ -301,36 +300,47 @@ int uifcini32(uifcapi_t* uifcapi)
 		}
 	#endif
 	
-#else
+#endif
     if(api->scrn_len!=0) {
         switch(api->scrn_len) {
+#ifdef C80X14
             case 14:
                 textmode(C80X14);
                 break;
+#endif
+#ifdef C80X21
             case 21:
                 textmode(C80X21);
                 break;
+#endif
             case 25:
                 textmode(C80);
                 break;
+#ifdef C80X28
             case 28:
                 textmode(C80X28);
                 break;
+#endif
+#ifdef C80X43
             case 43:
                 textmode(C80X43);
                 break;
+#endif
+#ifdef C80X50
             case 50:
                 textmode(C80X50);
                 break;
+#endif
+#ifdef C80X60
             case 60:
                 textmode(C80X60);
                 break;
+#endif
             default:
                 textmode(C4350);
                 break;
         }
     }
-#endif
 
     clrscr();
 
