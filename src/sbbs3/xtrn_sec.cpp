948ca396062f652722272c6eec671140f1ebf3f6
rswindell <>
2004-07-16 07:37:50 +0000

    The "Monitor Time Left" toggle option in SCFG (XTRN_CHKTIME) wasn't being propogated to the call to external(), so this optional feature did not work.

--- src/sbbs3/xtrn_sec.cpp
+++ src/sbbs3/xtrn_sec.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -1684,6 +1684,8 @@ bool sbbs_t::exec_xtrn(uint xtrnnum)
 		mode|=EX_SWAP;
 	if(cfg.xtrn[xtrnnum]->misc&XTRN_NATIVE)
 		mode|=EX_NATIVE;
+	if(cfg.xtrn[xtrnnum]->misc&XTRN_CHKTIME)
+		mode|=EX_CHKTIME;
 	if(cfg.xtrn[xtrnnum]->misc&MODUSERDAT) {	 /* Delete MODUSER.DAT */
 		sprintf(str,"%sMODUSER.DAT",dropdir);       /* if for some weird  */
 		remove(str); 								/* reason it's there  */
