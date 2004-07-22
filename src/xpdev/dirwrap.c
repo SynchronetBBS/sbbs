c1fc140800abaf0a35963200061a51893b1d25a5
rswindell <>
2004-07-22 23:26:16 +0000

    Win32 implementation of glob() now functions more like Unix glob() - that is, it doesn't return directory entries beginning with a period (e.g. '.' and '..') unless the GLOB_PERIOD flag is specified.

--- src/xpdev/dirwrap.c
+++ src/xpdev/dirwrap.c
@@ -176,7 +176,8 @@ int	DLLCALL	glob(const char *pattern, int flags, void* unused, glob_t* glob)
 		return(GLOB_NOMATCH);
 
 	do {
-		if(!(flags&GLOB_ONLYDIR) || ff.attrib&_A_SUBDIR) {
+		if((flags&GLOB_PERIOD || ff.name[0]!='.') &&
+			(!(flags&GLOB_ONLYDIR) || ff.attrib&_A_SUBDIR)) {
 			if((new_pathv=realloc(glob->gl_pathv
 				,(glob->gl_pathc+1)*sizeof(char*)))==NULL) {
 				globfree(glob);
@@ -232,7 +233,8 @@ int	DLLCALL	glob(const char *pattern, int flags, void* unused, glob_t* glob)
 
 	ff_handle=_findfirst((char*)pattern,&ff);
 	while(ff_handle!=-1) {
-		if(!(flags&GLOB_ONLYDIR) || ff.attrib&_A_SUBDIR) {
+		if((flags&GLOB_PERIOD || ff.name[0]!='.') &&
+			(!(flags&GLOB_ONLYDIR) || ff.attrib&_A_SUBDIR)) {
 			if((new_pathv=(char**)realloc(glob->gl_pathv
 				,(glob->gl_pathc+1)*sizeof(char*)))==NULL) {
 				globfree(glob);
