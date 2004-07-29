6800ab3ccc9d5acd989645b8cfe3591fc728ae5c
rswindell <>
2004-07-29 23:40:24 +0000

    Line 544 (was "LastFewCallers" header) is unused.

--- src/sbbs3/text.h
+++ src/sbbs3/text.h
@@ -587,7 +587,7 @@ enum {
 	,NodeJoinedPrivateChat
 	,NodeLeftPrivateChat
 	,NoOneHasLoggedOnToday
-	,LastFewCallers
+	,Unused544	/* was LastFewCallers */
 	,LastFewCallersFmt
 	,CallersToday
 	,DoYouMeanThisUserQ
