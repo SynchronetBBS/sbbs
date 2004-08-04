885be866e6153621bdcf6fd5e358c5b084903823
rswindell <>
2004-08-04 04:07:14 +0000

    Functions for creating and parsing tab-delimited data files (very similar to .csv file API).

---
+++ src/xpdev/tab_file.h
@@ -0,0 +1,56 @@
+/* tab_file.h */
+
+/* Functions to deal with tab-delimited files and lists */
+
+/* $Id$ */
+
+/****************************************************************************
+ * @format.tab-size 4		(Plain Text/Source Code File Header)			*
+ * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
+ *																			*
+ * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ *																			*
+ * This library is free software; you can redistribute it and/or			*
+ * modify it under the terms of the GNU Lesser General Public License		*
+ * as published by the Free Software Foundation; either version 2			*
+ * of the License, or (at your option) any later version.					*
+ * See the GNU Lesser General Public License for more details: lgpl.txt or	*
+ * http://www.fsf.org/copyleft/lesser.html									*
+ *																			*
+ * Anonymous FTP access to the most recent released source is available at	*
+ * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
+ *																			*
+ * Anonymous CVS access to the development source and modification history	*
+ * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
+ * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
+ *     (just hit return, no password is necessary)							*
+ * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src		*
+ *																			*
+ * For Synchronet coding style and modification guidelines, see				*
+ * http://www.synchro.net/source.html										*
+ *																			*
+ * You are encouraged to submit any modifications (preferably in Unix diff	*
+ * format) via e-mail to mods@synchro.net									*
+ *																			*
+ * Note: If this box doesn't appear square, then you need to fix your tabs.	*
+ ****************************************************************************/
+
+#ifndef _TAB_FILE_H
+#define _TAB_FILE_H
+
+#include "str_list.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+str_list_t	tabCreateList(str_list_t records[], str_list_t columns /* optional */);
+str_list_t	tabParseLine(char* line);
+str_list_t*	tabParseList(str_list_t list, str_list_t* columns /* optional */);
+str_list_t*	tabReadFile(FILE* fp, str_list_t* columns /* optional */);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif	/* Don't add anything after this line */
