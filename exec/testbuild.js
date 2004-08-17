d04db1e10b3b178f7783a6c8b89c730e800ee1a4
rswindell <>
2004-08-17 22:34:08 +0000

    Builds both debug and release builds. Builds using msvc on Win32. And much more...

--- exec/testbuild.js
+++ exec/testbuild.js
@@ -1,5 +1,7 @@
 /* testbuild.js */
 
+/* JSexec script for periodic Synchronet test builds */
+
 load("sbbsdefs.js");
 
 var date_str = strftime("%b-%d-%y");	/* mmm-dd-yy */
@@ -10,21 +12,31 @@ if(!file_isdir(temp_dir) && !mkdir(temp_dir)) {
 	log(LOG_ERR,"!Failed to create temp directory: " + temp_dir);
 	exit(1);
 }
+log(LOG_INFO,"Using temp directory: " + temp_dir);
 
 putenv("CVSROOT=:pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs");
 
-platform = system.platform.toLowerCase();
+var platform = system.platform.toLowerCase();
+var make = (platform=="win32" ? "make":"gmake");
+var msdev = '"C:\\Program Files\\Microsoft Visual Studio\\Common\\MSDev98\\Bin\\msdev"';
+var build_output = "build_output.txt";
 
 var builds
 	=	[/* sub-dir */		/* cmd-line */
 		["",				"cvs co src-sbbs3"],
-		["",				"cvs co lib-" + platform],
-		["src/sbbs3",		platform=="win32" ? "make RELEASE=1":"gmake"],
-		["src/sbbs3/scfg",	platform=="win32" ? "make RELEASE=1":"gmake"],
+		["",				"cvs co lib-" + platform + ".debug"],
+		["",				"cvs co lib-" + platform + ".release"],
+		["src/sbbs3",		make + " DEBUG=1"],
+		["src/sbbs3",		make + " RELEASE=1"],
+		["src/sbbs3/scfg",	make + " DEBUG=1"],
+		["src/sbbs3/scfg",	make + " RELEASE=1"],
 	];
 
 /* Platform-specific (or non-ported) projects */
 if(platform=="win32") {
+	/* Requires Visual C++ */
+	builds.push(["src/sbbs3",			msdev + " sbbs3.dsw /MAKE ALL /OUT "+ build_output]);
+	/* Requires C++Builder */
 	builds.push(["src/sbbs3/chat",		"build"]);
 	builds.push(["src/sbbs3/ctrl",		"build"]);
 } else {	/* Unix */
@@ -36,31 +48,42 @@ if(platform=="win32") {
 for(i in builds) {
 	var sub_dir = builds[i][0];
 	var build_dir = temp_dir + "/" + sub_dir;
+	var subject = "Build failure in " + sub_dir;
+
+	if(sub_dir.length)
+		log(LOG_INFO, "Build sub-directory: " + sub_dir);
 	if(!chdir(build_dir)) {
-		log(LOG_ERR,"!FAILED to chdir to: " + build_dir);
+		semd_email(subject, log(LOG_ERR,"!FAILED to chdir to: " + build_dir));
 		exit(1);
 	}
 
-	var cmd_line = builds[i][1] + " 2> build_output.txt";
+	var cmd_line = builds[i][1];
+	if(cmd_line.indexOf(build_output)<0)
+		cmd_line += " 2> " + build_output;
+	log(LOG_INFO, "Executing: " + cmd_line);
 	var retval=system.exec(cmd_line);
 	if(retval) {
-		log(LOG_ERR,"!ERROR " + retval + " executing: " + cmd_line + " in " + sub_dir);
-		send_email("build_output.txt", "Build failure in " + sub_dir);
+		log(LOG_ERR,"!ERROR " + retval + " executing: '" + cmd_line + "' in " + sub_dir);
+		send_email(subject, file_contents(build_output));
 		exit(1);
 	}
 }
 
-function send_email(fname, subject)
+function file_contents(fname)
 {
 	var file = new File(fname);
 	if(!file.open("rt")) {
-		log("!ERROR " + errno_str + " opening " + fname);
-		return(false);
+		return log(LOG_ERR, "!ERROR " + errno_str + " opening " + fname);
 	}
 	var msgtxt = lfexpand(file.read(file.length));
 	file.close();
 	delete file;
 
+	return(msgtxt);
+}
+
+function send_email(subject, body)
+{
 	var msgbase = new MsgBase("mail");
 	if(msgbase.open()==false) {
 		log("!ERROR " + msgbase.last_error);
@@ -70,14 +93,14 @@ function send_email(fname, subject)
 	var hdr = { 
 		to: "sysop", 
 		to_ext: 1, 
-		from: "testbuild", 
+		from: "Synchronet testbuild.js", 
 		subject: subject
 	};
 
-	if(!msgbase.save_msg(hdr, msgtxt))
-		log("!ERROR " + msgbase.last_error + "saving mail message");
-
-	log("E-mail sent.");
+	if(!msgbase.save_msg(hdr, body))
+		log(LOG_ERR, "!ERROR " + msgbase.last_error + "saving mail message");
+	else
+		log(LOG_INFO, "E-mail sent.");
 
 	msgbase.close();
 }
