f388a95d3058503f2c58333636c169b47977262b
rswindell <>
2004-08-17 09:43:02 +0000

    Automatically set the source DLL directory based on which ones exist.

--- src/sbbs3/ctrl/makelibs.bat
+++ src/sbbs3/ctrl/makelibs.bat
@@ -1,7 +1,9 @@
 @echo off
 setlocal
-rem set dllsrc=..\bcc.win32.dll.release
-set dllsrc=..\msvc.win32.dll.debug
+if exist ..\bcc.win32.dll.debug 	set dllsrc=..\bcc.win32.dll.debug
+if exist ..\bcc.win32.dll.release 	set dllsrc=..\bcc.win32.dll.release
+if exist ..\msvc.win32.dll.debug	set dllsrc=..\msvc.win32.dll.debug
+if exist ..\msvc.win32.dll.release	set dllsrc=..\msvc.win32.dll.release
 implib sbbs.lib 	%dllsrc%\sbbs.dll
 implib mailsrvr.lib 	%dllsrc%\mailsrvr.dll
 implib ftpsrvr.lib 	%dllsrc%\ftpsrvr.dll
