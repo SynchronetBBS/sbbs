b47c1311b057e4110ef995589e07001d95b2a879
rswindell <>
2004-08-27 23:07:34 +0000

    Debug DLL dirs have higher precedence than release dirs. Echo directory where we're generating the import libraries for.

--- src/sbbs3/ctrl/makelibs.bat
+++ src/sbbs3/ctrl/makelibs.bat
@@ -1,9 +1,10 @@
 @echo off
 setlocal
-if exist ..\bcc.win32.dll.debug 	set dllsrc=..\bcc.win32.dll.debug
 if exist ..\bcc.win32.dll.release 	set dllsrc=..\bcc.win32.dll.release
-if exist ..\msvc.win32.dll.debug	set dllsrc=..\msvc.win32.dll.debug
+if exist ..\bcc.win32.dll.debug 	set dllsrc=..\bcc.win32.dll.debug
 if exist ..\msvc.win32.dll.release	set dllsrc=..\msvc.win32.dll.release
+if exist ..\msvc.win32.dll.debug	set dllsrc=..\msvc.win32.dll.debug
+echo Creating import libraries from %dllsrc%
 implib sbbs.lib 	%dllsrc%\sbbs.dll
 implib mailsrvr.lib 	%dllsrc%\mailsrvr.dll
 implib ftpsrvr.lib 	%dllsrc%\ftpsrvr.dll
