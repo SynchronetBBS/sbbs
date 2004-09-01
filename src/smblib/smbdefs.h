735151fe087fd840d6cfd84ffb292e68d8a8bb83
rswindell <>
2004-09-01 11:10:27 +0000

    Using BOOL macros instead of int's and numeric constants.

--- src/smblib/smbdefs.h
+++ src/smblib/smbdefs.h
@@ -617,7 +617,7 @@ typedef struct {			/* Message base */
 	ulong	retry_time; 	/* Maximum number of seconds to retry opens/locks */
 	ulong	retry_delay;	/* Time-slice yield (milliseconds) while retrying */
 	smbstatus_t status; 	/* Status header record */
-	int		locked;			/* SMB header is locked */
+	BOOL	locked;			/* SMB header is locked */
 	char	shd_buf[SHD_BLOCK_LEN]; 	/* File I/O buffer for header file */
 	char	last_error[MAX_PATH*2];		/* Last error message */
 
