74f3ad600405d37b0044ef50f762af954dbf6adc
rswindell <>
2004-09-10 08:36:46 +0000

    Fixed CVS ID tag. Updated copyright date.

--- src/xpdev/semwrap.h
+++ src/xpdev/semwrap.h
@@ -2,13 +2,13 @@
 
 /* Semaphore-related cross-platform development wrappers */
 
-/* $Id: */
+/* $Id$ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
