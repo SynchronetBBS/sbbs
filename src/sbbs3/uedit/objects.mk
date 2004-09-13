a0dcb7e9b9c0f6ef2f42befa5601fb67e8df0206
deuce <>
2004-09-13 23:07:25 +0000

    New make system!

--- src/sbbs3/uedit/objects.mk
+++ src/sbbs3/uedit/objects.mk
@@ -1,20 +1,14 @@
-OBJS	:=	$(LIBODIR)/filewrap.o \
-			$(LIBODIR)/dirwrap.o \
-			$(LIBODIR)/genwrap.o \
-			$(LIBODIR)/sbbs_ini.o \
-			$(LIBODIR)/ini_file.o \
-			$(LIBODIR)/str_list.o \
-			$(LIBODIR)/scfglib1.o \
-			$(LIBODIR)/scfglib2.o \
-			$(LIBODIR)/nopen.o \
-			$(LIBODIR)/userdat.o \
-			$(LIBODIR)/ars.o \
-			$(LIBODIR)/dat_rec.o \
-			$(LIBODIR)/str_util.o \
-			$(LIBODIR)/crc16.o \
-			$(LIBODIR)/load_cfg.o \
-			$(LIBODIR)/date_str.o \
-			$(LIBODIR)/smblib.o \
-			$(LIBODIR)/getstats.o \
-			$(LIBODIR)/getmail.o \
-			$(LIBODIR)/uedit.o
+OBJS	=	\
+			$(MTOBJODIR)$(DIRSEP)ars$(OFILE) \
+			$(MTOBJODIR)$(DIRSEP)dat_rec$(OFILE) \
+			$(MTOBJODIR)$(DIRSEP)date_str$(OFILE) \
+			$(MTOBJODIR)$(DIRSEP)getmail$(OFILE) \
+			$(MTOBJODIR)$(DIRSEP)getstats$(OFILE) \
+			$(MTOBJODIR)$(DIRSEP)load_cfg$(OFILE) \
+			$(MTOBJODIR)$(DIRSEP)nopen$(OFILE) \
+			$(MTOBJODIR)$(DIRSEP)sbbs_ini$(OFILE) \
+			$(MTOBJODIR)$(DIRSEP)scfglib1$(OFILE) \
+			$(MTOBJODIR)$(DIRSEP)scfglib2$(OFILE) \
+			$(MTOBJODIR)$(DIRSEP)str_util$(OFILE) \
+			$(MTOBJODIR)$(DIRSEP)uedit$(OFILE) \
+			$(MTOBJODIR)$(DIRSEP)userdat$(OFILE)
