fa397c1f0e516317ed9d7ad525be7fc6dccbab95
rswindell <>
2004-09-13 00:27:56 +0000

    Created get_errno(), to allow for a single smb.lib build for both single and multi-threaded applications: the version of xpdev linked with the application will determine the errno implementation (which is unique for multi-threaded apps).

--- src/xpdev/genwrap.c
+++ src/xpdev/genwrap.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2002 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -443,4 +443,13 @@ char* DLLCALL truncnl(char* str)
 	str[c]=0;
 
 	return(str);
+}
+
+/****************************************************************************/
+/* Return errno from the proper C Library implementation					*/
+/* (single/multi-threaded)													*/
+/****************************************************************************/
+int DLLCALL	get_errno(void)
+{
+	return(errno);
 }
