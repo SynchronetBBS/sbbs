820a03b0460076dbdf58509cf9eef5cafc0d9782
deuce <>
2004-09-16 04:46:39 +0000

    Fix bug in last commit

--- src/uifc/targets.mk
+++ src/uifc/targets.mk
@@ -2,7 +2,7 @@ UIFCTEST	=	$(EXEODIR)$(DIRSEP)uifctest$(EXEFILE)
 
 all: lib mtlib test
 
-test: mtlib ciolib-mt xpdevlib-mt $(EXEODIR) $(UIFCTEST)
+test: mtlib ciolib-mt xpdev-mt $(EXEODIR) $(UIFCTEST)
 
 lib: $(OBJODIR) $(LIBODIR) $(UIFCLIB)
 mtlib: $(MTOBJODIR) $(LIBODIR) $(UIFCLIB-MT)
