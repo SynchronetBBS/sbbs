c6cc370bfa40f6b4b42f816e7962efa496a632c6
deuce <>
2004-09-16 19:05:03 +0000

    Use library dependencies correctly...

--- src/syncterm/targets.mk
+++ src/syncterm/targets.mk
@@ -1,1 +1,6 @@
-all: xpdev-mt ciolib-mt uifc-mt $(MTOBJODIR) $(EXEODIR) $(EXEODIR)$(DIRSEP)syncterm$(EXEFILE)
+SYNCTERM	=	$(EXEODIR)$(DIRSEP)syncterm$(EXEFILE)
+
+all: xpdev-mt ciolib-mt uifc-mt $(MTOBJODIR) $(EXEODIR) $(SYNCTERM)
+
+$(SYNCTERM):	$(XPDEV-MT_LIB) $(CIOLIB-MT) $(UIFCLIB-MT)
+
