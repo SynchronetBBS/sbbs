80dbd053ff119de37cc8f8b1e47fe370fcc74884
rswindell <>
2004-09-22 21:27:31 +0000

    Removed "var" keyword from log() priority level definitions.

--- exec/load/sbbsdefs.js
+++ exec/load/sbbsdefs.js
@@ -591,19 +591,18 @@ var FI_USERXFER  	=6			/* User Xfer Download                       */
 var FI_CLOSE 	  	=7			/* Close any open records					*/
 					    		/********************************************/
 
-
 if(this.LOG_EMERG==undefined) {	/* temporary backward compatibility kludge	*/
 	                            /********************************************/   
                                 /* Log "levels" supported by log() function */   
                                 /********************************************/   
-var LOG_EMERG       =0			/* system is unusable                       */   
-var LOG_ALERT       =1			/* action must be taken immediately         */   
-var LOG_CRIT        =2			/* critical conditions                      */   
-var LOG_ERR         =3			/* error conditions                         */   
-var LOG_WARNING     =4			/* warning conditions                       */   
-var LOG_NOTICE      =5			/* normal but significant condition         */   
-var LOG_INFO        =6			/* informational                            */   
-var LOG_DEBUG       =7			/* debug-level messages                     */   
+LOG_EMERG       =0				/* system is unusable                       */   
+LOG_ALERT       =1				/* action must be taken immediately         */   
+LOG_CRIT        =2				/* critical conditions                      */   
+LOG_ERR         =3				/* error conditions                         */   
+LOG_WARNING     =4				/* warning conditions                       */   
+LOG_NOTICE      =5				/* normal but significant condition         */   
+LOG_INFO        =6				/* informational                            */   
+LOG_DEBUG       =7				/* debug-level messages                     */   
                                 /********************************************/ 
 }
 								/* Message attributes */
