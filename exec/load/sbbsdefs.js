c3e56c1367a45a845b2d40628c8050e1e27fff7c
rswindell <>
2004-09-22 20:41:57 +0000

    Define log() priority levels (if not already defined) - for backward compatibility with older sbbs builds.

--- exec/load/sbbsdefs.js
+++ exec/load/sbbsdefs.js
@@ -591,6 +591,21 @@ var FI_USERXFER  	=6			/* User Xfer Download                       */
 var FI_CLOSE 	  	=7			/* Close any open records					*/
 					    		/********************************************/
 
+
+if(this.LOG_EMERG==undefined) {	/* temporary backward compatibility kludge	*/
+	                            /********************************************/   
+                                /* Log "levels" supported by log() function */   
+                                /********************************************/   
+var LOG_EMERG       =0			/* system is unusable                       */   
+var LOG_ALERT       =1			/* action must be taken immediately         */   
+var LOG_CRIT        =2			/* critical conditions                      */   
+var LOG_ERR         =3			/* error conditions                         */   
+var LOG_WARNING     =4			/* warning conditions                       */   
+var LOG_NOTICE      =5			/* normal but significant condition         */   
+var LOG_INFO        =6			/* informational                            */   
+var LOG_DEBUG       =7			/* debug-level messages                     */   
+                                /********************************************/ 
+}
 								/* Message attributes */
 var MSG_PRIVATE 		=(1<<0)
 var MSG_READ			=(1<<1)
