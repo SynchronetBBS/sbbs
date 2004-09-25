37f028eb4e1e4fdcf7194dcae222c367369db63b
rswindell <>
2004-09-25 21:20:15 +0000

    #include <limits.h> for USHRT_MAX definition.

--- src/sbbs3/scfg/scfg.h
+++ src/sbbs3/scfg/scfg.h
@@ -6,7 +6,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2002 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -35,6 +35,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>	/* USHRT_MAX */
 
 #include "uifc.h"
 #include "sbbs.h"
