9ee871f300957b97cada716b06e73202b75922ae
deuce <>
2004-10-13 21:05:38 +0000

    Fix CIO_KEY_F() and CIO_KEY_ALT_F() macros for F11 and F12.

--- src/conio/keys.h
+++ src/conio/keys.h
@@ -35,13 +35,13 @@
 #define CIO_KEY_UP        (72   << 8)
 #define CIO_KEY_END       (0x4f << 8)
 #define CIO_KEY_DOWN      (80   << 8)
-#define CIO_KEY_F(x)      ((0x3a+x) << 8)
+#define CIO_KEY_F(x)      ((x<11)?((0x3a+x) << 8):((0x7a+x) << 8))
 #define CIO_KEY_IC        (0x52 << 8)
 #define CIO_KEY_DC        (0x53 << 8)
 #define CIO_KEY_LEFT      (0x4b << 8)
 #define CIO_KEY_RIGHT     (0x4d << 8)
 #define CIO_KEY_PPAGE     (0x49 << 8)
 #define CIO_KEY_NPAGE     (0x51 << 8)
-#define CIO_KEY_ALT_F(x)      ((0x67+x) << 8)
+#define CIO_KEY_ALT_F(x)      ((x<11)?((0x67+x) << 8):((0x80+x) << 8))
 
 #define CIO_KEY_MOUSE    0x02ff
