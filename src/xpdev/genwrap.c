0decbf85b38d0e02909b67def4fdfd2ecdcc498d
deuce <>
2004-10-14 23:28:55 +0000

    Set numchars to size-1 if it's >= size... NOTICE: This is not "correct" snprintf() behaviour!

--- src/xpdev/genwrap.c
+++ src/xpdev/genwrap.c
@@ -75,6 +75,8 @@ int DLLCALL safe_snprintf(char *dst, size_t size, const char *fmt, ...)
 	va_start(argptr,fmt);
 	numchars= vsnprintf(dst,size,fmt,argptr);
 	va_end(argptr);
+	if(numchars>=size && numchars>0)
+		numchars=size-1;
 	dst[size-1]=0;
 	return(numchars);
 }
