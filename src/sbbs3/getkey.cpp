8bca5c323452e1eeb5f4b6f0d6cf3a7c8a4e9230
rswindell <>
2004-10-20 07:17:56 +0000

    Fixed ASCII spinning cursor bug.

--- src/sbbs3/getkey.cpp
+++ src/sbbs3/getkey.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -86,6 +86,7 @@ char sbbs_t::getkey(long mode)
 					case 3:
 						outchar(BS);
 						outchar('\\');
+						c=0;
 						break;
 				}
 			} else {
