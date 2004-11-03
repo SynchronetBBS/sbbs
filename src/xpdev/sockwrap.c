195154961c600c910b6ab3e232eabcd8e5897124
rswindell <>
2004-11-03 07:29:12 +0000

    Not that it matters, but the proper macro is "ntohs".

--- src/xpdev/sockwrap.c
+++ src/xpdev/sockwrap.c
@@ -241,7 +241,7 @@ int retry_bind(SOCKET s, const struct sockaddr *addr, socklen_t addrlen
 	uint	i;
 
 	if(addr->sa_family==AF_INET)
-		SAFEPRINTF(port_str," to port %u",htons(((SOCKADDR_IN *)(addr))->sin_port)); 
+		SAFEPRINTF(port_str," to port %u",ntohs(((SOCKADDR_IN *)(addr))->sin_port)); 
 	else
 		port_str[0]=0;
 	for(i=0;i<=retries;i++) {
