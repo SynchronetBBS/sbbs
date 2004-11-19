7053dd2033fdf27853109946cb402889af781c7c
rswindell <>
2004-11-19 00:34:59 +0000

    Define control character constants (if not already defined).

--- src/uifc/uifc.h
+++ src/uifc/uifc.h
@@ -225,6 +225,37 @@
 #define DEL     0x7f                /* Delete                   ^BS */
 #endif
 
+#if !CTRL_A
+enum {
+	 CTRL_A=1
+	,CTRL_B
+	,CTRL_C
+	,CTRL_D	
+	,CTRL_E
+	,CTRL_F
+	,CTRL_G
+	,CTRL_H
+	,CTRL_I
+	,CTRL_J
+	,CTRL_K
+	,CTRL_L
+	,CTRL_M
+	,CTRL_N
+	,CTRL_O
+	,CTRL_P
+	,CTRL_Q
+	,CTRL_R
+	,CTRL_S
+	,CTRL_T
+	,CTRL_U
+	,CTRL_V
+	,CTRL_W
+	,CTRL_X
+	,CTRL_Y
+	,CTRL_Z
+};
+#endif
+
 #define CLREOL 256
 
 #ifndef uchar				/* Short-hand for unsigned data types */
