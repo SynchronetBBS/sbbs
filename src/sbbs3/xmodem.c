8c9bf88d2b03b6b2ff00ddfc3212940500b25650
deuce <>
2005-01-13 23:44:29 +0000

    Include stdarg.h for va_lists

--- src/sbbs3/xmodem.c
+++ src/sbbs3/xmodem.c
@@ -35,6 +35,7 @@
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
 
+#include <stdarg.h>		/* va_list */
 #include "sexyz.h"
 #include "genwrap.h"	/* YIELD */
 #include "conwrap.h"	/* kbhit */
