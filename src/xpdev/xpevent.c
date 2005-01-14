c5fd1fdcde5ef2d9b86430f732aecf6a1fd6b225
deuce <>
2005-01-14 19:25:55 +0000

    Redo changes lost to the Great Merge.

--- src/xpdev/xpevent.c
+++ src/xpdev/xpevent.c
@@ -76,7 +76,10 @@ CreateEvent(void *sec, BOOL bManualReset, BOOL bInitialState, void *name)
 BOOL
 SetEvent(xpevent_t event)
 {
-	_EVENT_CHECK_VALIDITY(event)
+	if (event==NULL || (event->magic != EVENT_MAGIC)) {
+		errno = EINVAL;
+		return(FALSE);
+	}
 
 	pthread_mutex_lock(&event->lock);
 
@@ -99,7 +102,10 @@ SetEvent(xpevent_t event)
 BOOL
 ResetEvent(xpevent_t event)
 {
-	_EVENT_CHECK_VALIDITY(event)
+	if (event==NULL || (event->magic != EVENT_MAGIC)) {
+		errno = EINVAL;
+		return(FALSE);
+	}
 
 	pthread_mutex_lock(&event->lock);
 
@@ -113,7 +119,10 @@ ResetEvent(xpevent_t event)
 BOOL
 CloseEvent(xpevent_t event)
 {
-	_EVENT_CHECK_VALIDITY(event)
+	if (event==NULL || (event->magic != EVENT_MAGIC)) {
+		errno = EINVAL;
+		return(FALSE);
+	}
 
 	/* Make sure there are no waiters. */
 	pthread_mutex_lock(&event->lock);
