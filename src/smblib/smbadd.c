a6173bdce9896472622cda819c23f59d0cfbaf6e
rswindell <>
2005-01-15 21:52:52 +0000

    smb_addmsg() no longer fails if it couldn't update the thread linkage (e.g. couldn't find original message).

--- src/smblib/smbadd.c
+++ src/smblib/smbadd.c
@@ -262,33 +262,31 @@ int SMBCALL smb_addmsg(smb_t* smb, smbmsg_t* msg, int storage, long dupechk_hash
 		if(msg->hdr.thread_back) {
 			memset(&remsg,0,sizeof(remsg));
 			remsg.hdr.number=msg->hdr.thread_back;
-			if((retval=smb_getmsgidx(smb, &remsg))!=SMB_SUCCESS)	/* invalid thread origin */
-				break;
+			if(smb_getmsgidx(smb, &remsg)==SMB_SUCCESS	/* valid thread origin */
+				&& smb_lockmsghdr(smb,&remsg)==SMB_SUCCESS) {
 
-			if((retval=smb_lockmsghdr(smb,&remsg))!=SMB_SUCCESS)
-				break;
+				do { /* try */
 
-			if((retval=smb_getmsghdr(smb, &remsg))!=SMB_SUCCESS) {
-				smb_unlockmsghdr(smb, &remsg); 
-				break;
+					if(smb_getmsghdr(smb, &remsg)!=SMB_SUCCESS)
+						break;
+
+					/* Add RFC-822 Reply-ID if original message has RFC Message-ID */
+					if(msg->reply_id==NULL && remsg.id!=NULL
+						&& smb_hfield_str(msg,RFC822REPLYID,remsg.id)!=SMB_SUCCESS)
+						break;
+
+					/* Add FidoNet Reply if original message has FidoNet MSGID */
+					if(msg->ftn_reply==NULL && remsg.ftn_msgid!=NULL
+						&& smb_hfield_str(msg,FIDOREPLYID,remsg.ftn_msgid)!=SMB_SUCCESS)
+						break;
+
+					smb_updatethread(smb, &remsg, msg->hdr.number);
+
+				} while(0); /* finally */
+
+				smb_unlockmsghdr(smb, &remsg);
+				smb_freemsgmem(&remsg);
 			}
-
-			/* Add RFC-822 Reply-ID if original message has RFC Message-ID */
-			if(msg->reply_id==NULL && remsg.id!=NULL
-				&& (retval=smb_hfield_str(msg,RFC822REPLYID,remsg.id))!=SMB_SUCCESS)
-				break;
-
-			/* Add FidoNet Reply if original message has FidoNet MSGID */
-			if(msg->ftn_reply==NULL && remsg.ftn_msgid!=NULL
-				&& (retval=smb_hfield_str(msg,FIDOREPLYID,remsg.ftn_msgid))!=SMB_SUCCESS)
-				break;
-
-			retval=smb_updatethread(smb, &remsg, msg->hdr.number);
-			smb_unlockmsghdr(smb, &remsg);
-			smb_freemsgmem(&remsg);
-
-			if(retval!=SMB_SUCCESS)
-				break;
 		}
 
 		if(smb_addhashes(smb,hashes,/* skip_marked? */FALSE)==SMB_SUCCESS)
