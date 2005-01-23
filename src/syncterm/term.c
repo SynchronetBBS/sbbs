63d8636b32a07b41b1c159c315ba6a3d301d7324
deuce <>
2005-01-23 22:22:45 +0000

    Implement a simple select-to-copy and right-click-to-paste copy/paste thing. Using ciolibs copytext()/getcliptext()

--- src/syncterm/term.c
+++ src/syncterm/term.c
@@ -15,6 +15,84 @@ int backlines=2000;
 
 struct terminal term;
 
+void mousedrag(unsigned char *scrollback)
+{
+	int	key;
+	struct mouse_event mevent;
+	unsigned char *screen;
+	unsigned char *sbuffer;
+	int sbufsize=0;
+	int pos, startpos,endpos, lines;
+	int outpos;
+	char *copybuf;
+
+	sbufsize=term.width*2*term.height;
+	screen=(unsigned char*)malloc(sbufsize);
+	sbuffer=(unsigned char*)malloc(sbufsize);
+	gettext(term.x-1,term.y-1,term.x+term.width-2,term.y+term.height-2,screen);
+	while(1) {
+		key=getch();
+		if(key==0 || key==0xff)
+			key|=getch()<<8;
+		switch(key) {
+			case CIO_KEY_MOUSE:
+				getmouse(&mevent);
+				startpos=((mevent.starty-1)*term.width)+(mevent.startx-1);
+				endpos=((mevent.endy-1)*term.width)+(mevent.endx-1);
+				if(endpos<startpos) {
+					pos=endpos;
+					endpos=startpos;
+					startpos=pos;
+				}
+				switch(mevent.event) {
+					case CIOLIB_BUTTON_1_DRAG_MOVE:
+						memcpy(sbuffer,screen,sbufsize);
+						for(pos=startpos;pos<=endpos;pos++) {
+							if(sbuffer[pos*2+1]&0x70!=0x10)
+								sbuffer[pos*2+1]=sbuffer[pos*2+1]&0x8F|0x10;
+							else
+								sbuffer[pos*2+1]=sbuffer[pos*2+1]&0x8F|0x60;
+							if((sbuffer[pos*2+1]&0x70)>>4 == sbuffer[pos*2+1]&0x0F) {
+								sbuffer[pos*2+1]|=0x60;
+								sbuffer[pos*2+1]&=0xEF;
+							}
+						}
+						puttext(term.x-1,term.y-1,term.x+term.width-2,term.y+term.height-2,sbuffer);
+						break;
+					default:
+						lines=abs(mevent.endy-mevent.starty)+1;
+						copybuf=malloc(endpos-startpos+4+lines*2);
+						outpos=0;
+						for(pos=startpos;pos<=endpos;pos++) {
+							copybuf[outpos++]=screen[pos*2];
+							if((pos+1)%term.width==0) {
+								copybuf[outpos++]='\r';
+								copybuf[outpos++]='\n';
+							}
+						}
+						if((pos+1)%term.width!=0) {
+							copybuf[outpos++]='\r';
+							copybuf[outpos++]='\n';
+						}
+						copybuf[outpos]=0;
+						copytext(copybuf, strlen(copybuf));
+						puttext(term.x-1,term.y-1,term.x+term.width-2,term.y+term.height-2,screen);
+						free(copybuf);
+						free(screen);
+						free(sbuffer);
+						return;
+				}
+				break;
+			default:
+				puttext(term.x-1,term.y-1,term.x+term.width-2,term.y+term.height-2,screen);
+				ungetch(key);
+				free(screen);
+				free(sbuffer);
+				return;
+		}
+	}
+}
+
 void doterm(void)
 {
 	unsigned char ch[2];
@@ -23,7 +101,13 @@ void doterm(void)
 	int	key;
 	int i,j,k;
 	unsigned char *scrollback;
+	char *p;
 
+	ciomouse_setevents(0);
+	ciomouse_addevent(CIOLIB_BUTTON_1_DRAG_START);
+	ciomouse_addevent(CIOLIB_BUTTON_1_DRAG_MOVE);
+	ciomouse_addevent(CIOLIB_BUTTON_1_DRAG_END);
+	ciomouse_addevent(CIOLIB_BUTTON_3_CLICK);
 	scrollback=malloc(term.width*2*backlines);
 	memset(scrollback,0,term.width*2*backlines);
 	cterm_init(term.height,term.width,term.x-1,term.y-1,backlines,scrollback);
@@ -59,8 +143,20 @@ void doterm(void)
 					switch(key) {
 						case CIO_KEY_MOUSE:
 							getmouse(&mevent);
+							switch(mevent.event) {
+								case CIOLIB_BUTTON_1_DRAG_START:
+									mousedrag(scrollback);
+									break;
+								case CIOLIB_BUTTON_3_CLICK:
+									p=getcliptext();
+									if(p!=NULL) {
+										conn_send(p,strlen(p),0);
+										free(p);
+									}
+									break;
+							}
+									
 							break;
-
 						case CIO_KEY_LEFT:
 							conn_send("\033[D",3,0);
 							break;
