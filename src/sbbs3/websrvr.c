83d33d41c1591b16d7fe5bcb591eb80cb75ebe80
rswindell <>
2005-02-08 20:32:01 +0000

    Fix bug in last commit: putuserrec() expects a string for the last argument.

--- src/sbbs3/websrvr.c
+++ src/sbbs3/websrvr.c
@@ -1003,6 +1003,8 @@ static void send_error(http_session_t * session, const char* message)
 
 void http_logon(http_session_t * session, user_t *usr)
 {
+	char	str[128];
+
 	if(usr==NULL)
 		getuserdat(&scfg, &session->user);
 	else
@@ -1025,7 +1027,7 @@ void http_logon(http_session_t * session, user_t *usr)
 		putuserrec(&scfg,session->user.number,U_MODEM,LEN_MODEM,"HTTP");
 		putuserrec(&scfg,session->user.number,U_COMP,LEN_COMP,session->host_name);
 		putuserrec(&scfg,session->user.number,U_NOTE,LEN_NOTE,session->host_ip);
-		putuserrec(&scfg,session->user.number,U_LOGONTIME,0,session->logon_time);
+		putuserrec(&scfg,session->user.number,U_LOGONTIME,0,ultoa(session->logon_time,str,16));
 	}
 	session->client.user=session->username;
 	client_on(session->socket, &session->client, /* update existing client record? */TRUE);
