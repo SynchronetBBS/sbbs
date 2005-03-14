f07958a1f1e2cebd3678ab5ac489ea70af027c89
deuce <>
2005-03-14 09:18:55 +0000

    More reset of status to default "200 OK" into inner request loop... this fixes all kinds of keep-alive related problems.

--- src/sbbs3/websrvr.c
+++ src/sbbs3/websrvr.c
@@ -2934,12 +2934,11 @@ void http_session_thread(void* arg)
 
 	while(!session.finished && server_socket!=INVALID_SOCKET) {
 	    memset(&(session.req), 0, sizeof(session.req));
-		SAFECOPY(session.req.status,"200 OK");
-		session.req.send_location=NO_LOCATION;
 		redirp=NULL;
 		loop_count=0;
 		while((redirp==NULL || session.req.send_location >= MOVED_TEMP)
 				 && !session.finished && server_socket!=INVALID_SOCKET) {
+			SAFECOPY(session.req.status,"200 OK");
 			session.req.send_location=NO_LOCATION;
 			session.req.ld=NULL;
 			if(startup->options&WEB_OPT_HTTP_LOGGING) {
