a230a9ba5262a14d3154a7d9e93c973694d0ef0b
deuce <>
2005-04-06 01:11:00 +0000

    Only WILL to telnet DOs that are supported (or more precicely, that I currently know are supported)
    Allows connections to *nix telnetd.

--- src/syncterm/telnet_io.c
+++ src/syncterm/telnet_io.c
@@ -111,8 +111,18 @@ static BYTE* telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen)
             else if(telnet_cmdlen>=3) {	/* telnet option negotiation */
 				if(command==TELNET_DO || command==TELNET_DONT) {	/* local options */
 					if(telnet_local_option[option]!=command) {
-						telnet_local_option[option]=command;
-						send_telnet_cmd(telnet_opt_ack(command),option);
+						switch(option) {
+							case TELNET_BINARY_TX:
+							case TELNET_ECHO:
+							case TELNET_TERM_TYPE:
+							case TELNET_TERM_SPEED:
+							case TELNET_SUP_GA:
+							case TELNET_NEGOTIATE_WINDOW_SIZE:
+								telnet_local_option[option]=command;
+								send_telnet_cmd(telnet_opt_ack(command),option);
+							default:
+								send_telnet_cmd(telnet_opt_nak(command),option);
+						}
 					}
 
 					if(command==TELNET_DO && option==TELNET_NEGOTIATE_WINDOW_SIZE) {
