da601aac203b348e0f151e6b3e7c5e1047ecbb44
deuce <>
2005-04-07 03:50:34 +0000

    Make Win32 build create gac_bj32.exe as required by the auto-update.

--- src/doors/gac/gac_bj/targets.mk
+++ src/doors/gac/gac_bj/targets.mk
@@ -1,6 +1,8 @@
-GAC_BJ	=	$(EXEODIR)$(DIRSEP)gac_bj$(EXEFILE)
+GAC_BJ	=	$(EXEODIR)$(DIRSEP)gac_bj$(EXE_SUFFIX)$(EXEFILE)
 
 all: xpdev-mt ciolib-mt $(MTOBJODIR) $(EXEODIR) $(GAC_BJ)
 
 $(GAC_BJ):	$(XPDEV-MT_LIB) $(CIOLIB-MT)
+
+release: $(EXEODIR)$(DIRSEP)encrypt$(EXEFILE) ARTPACKS
 
