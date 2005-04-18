71e46e659507a7e8acc74c1f400a2f7e4b4fae78
deuce <>
2005-04-18 09:53:12 +0000

    Bump version to 6.24... it actually WAS 6.23 already... just not bumped properly.

--- src/odoors/OpenDoor.h
+++ src/odoors/OpenDoor.h
@@ -80,7 +80,7 @@
 /* ========================================================================= */
 
 /* OpenDoors API version number. */
-#define OD_VERSION 0x622
+#define OD_VERSION 0x624
 
 #define DIRSEP		'\\'
 #define DIRSEP_STR	"\\"
