d97ebe73a616742736c58f06c48d9a14dbebed5f
rswindell <>
2005-04-20 20:22:29 +0000

    Convert "-> ADDFILES <-" to system.operator.

--- exec/ftp-html.js
+++ exec/ftp-html.js
@@ -371,12 +371,13 @@ if(ftp.file_list.length) {
             var uploader=ftp.file_list[i].uploader;
             if(ftp.file_list[i].settings&FILE_ANON)
                 uploader="Anonymous";
-			else if(!(user.security.restrictions&UFLAG_G)) { /* ! Guest/Anonymous */
+			else {
 				if(uploader == "-> ADDFILES <-")
-					uploader="Sysop".link("mailto:sysop@"+system.inetaddr);
-				else
+					uploader=system.operator;
+				if(!(user.security.restrictions&UFLAG_G)) /* ! Guest/Anonymous */
 					uploader=uploader.link("mailto:" + uploader + "@" + system.inetaddr);
 			}
+
             writeln("<td nowrap>" + dat_font + uploader);
 
             /* download count */
