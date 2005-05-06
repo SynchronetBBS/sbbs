b4490f935c3e1aed596e9c423bb2dcd00c145581
rswindell <>
2005-05-06 08:09:17 +0000

    Fixed memory leak (wasn't calling JS_DestroyIdArray to free ID array returned from JS_Enumerate()).

--- src/sbbs3/js_file.c
+++ src/sbbs3/js_file.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -709,6 +709,8 @@ js_iniSetObject(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rv
 		if(!js_iniSetValue(cx,obj,3,set_argv,rval))
 			break;
 	}
+
+	JS_DestroyIdArray(cx,id_array);
 
     return(JS_TRUE);
 }
