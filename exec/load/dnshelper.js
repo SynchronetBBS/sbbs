d8e83f20d05b603ee5cbcd6ba13676cd4e3c18be
rswindell <>
2005-05-06 08:07:23 +0000

    Greatly simplified.

--- exec/load/dnshelper.js
+++ exec/load/dnshelper.js
@@ -1,3 +1,5 @@
+// $Id$
+
 /*
    dnshelper.js -- quick script to do a DNS resolution and display the result.
 
@@ -5,24 +7,15 @@
    Synchronet load() function. Example:
 
    q = load(true,"dnshelper.js",some_ip_address);
-   var dns_result = 0;
-   while (!dns_result) {
-      dns_result = q.read();
-   }
+   var dns_result;
+   do {
+		dns_result = q.read();
+   while (!dns_result);
+
    log("Got result: " + dns_result);
 
    This is intended for utilization in a situation where blocking is not
    acceptable.  Otherwise simply use resolve_host() as per normal.
 */
 
-if (!argv[0]) {
-	exit();
-}
-
-var resolved_hostname = resolve_host(argv[0]);
-
-if (resolved_hostname)
-	resolved_hostname;
-else
-	false;
-
+resolve_host(argv[0]);
