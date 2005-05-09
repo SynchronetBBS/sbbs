94aea42c0cd67795d38a78700e8a006bd0e95179
rswindell <>
2005-05-09 23:26:15 +0000

    Fix last commit. <sigh>

--- exec/load/dnshelper.js
+++ exec/load/dnshelper.js
@@ -1,22 +1,7 @@
 // $Id$
 
-/*
-   dnshelper.js -- quick script to do a DNS resolution and display the result.
-
-   This is intended to be used from inside another JS script by using the
-   Synchronet load() function. Example:
-
-   q = load(true,"dnshelper.js",some_ip_address);
-   var dns_result;
-   do {
-		dns_result = q.read();
-   while (!dns_result);
-
-   log("Got result: " + dns_result);
-
-   This is intended for utilization in a situation where blocking is not
-   acceptable.  Otherwise simply use resolve_host() as per normal.
-*/
-
-if(resolve_host(argv[0])==null)
+var host;
+if((host=resolve_host(argv[0]))==null)
 	false;	// Work-around for bug in v3.12a (segfault when decoding "null")
+else
+	host;
