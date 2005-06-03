5d9c7a603cf3ca256af1b79759a7ca1fcef68d43
deuce <>
2005-06-03 04:02:46 +0000

    Get the dir and file lists working correctly.  Still much to do, but some real work got done on it.

--- src/uifc/filepick.c
+++ src/uifc/filepick.c
@@ -4,6 +4,13 @@
 
 #include "filepick.h"
 
+enum {
+	 DIR_LIST
+	,FILE_LIST
+	,MASK_FIELD
+	,NAME_FIELD
+	,FIELD_LIST_TERM
+};
 
 int drawfpwindow(uifcapi_t *api)
 {
@@ -79,14 +86,14 @@ int drawfpwindow(uifcapi_t *api)
 		,SCRN_LEFT+width-1,SCRN_TOP+height-1,lbuf);
 	lbuf[0]='\xcc';
 	lbuf[(width-1)*2]='\xb9';
-	lbuf[width+1]='\xcb';
+	lbuf[width-1]='\xcb';
 	puttext(SCRN_LEFT,SCRN_TOP+2,SCRN_LEFT+width-1,SCRN_TOP+2,lbuf);
-	lbuf[width+1]='\xca';
+	lbuf[width-1]='\xca';
 	puttext(SCRN_LEFT,SCRN_TOP+3+listheight
 		,SCRN_LEFT+width-1,SCRN_TOP+3+listheight,lbuf);
 	lbuf[0]='\xba';
 	lbuf[(width-1)*2]='\xba';
-	for(j=2;j<width*2;j+=2)
+	for(j=2;j<(width-1)*2;j+=2)
 		lbuf[j]=' ';
 	puttext(SCRN_LEFT,SCRN_TOP+1,
 		SCRN_LEFT+width-1,SCRN_TOP+1,lbuf);
@@ -96,19 +103,17 @@ int drawfpwindow(uifcapi_t *api)
 		SCRN_LEFT+width-1,SCRN_TOP+height-3,lbuf);
 	puttext(SCRN_LEFT,SCRN_TOP+height-4,
 		SCRN_LEFT+width-1,SCRN_TOP+height-4,lbuf);
-	lbuf[width+1]='\xba';
+	lbuf[width-1]='\xba';
 	for(j=0;j<listheight;j++)
 		puttext(SCRN_LEFT,SCRN_TOP+3+j
 			,SCRN_LEFT+width-1,SCRN_TOP+3+j,lbuf);
-	puttext(SCRN_LEFT,SCRN_TOP+3+listheight
-		,SCRN_LEFT+width-1,SCRN_TOP+3+listheight,lbuf);
 	
 
 	/* Shadow */
 	if(bclr==BLUE) {
 		gettext(SCRN_LEFT+width,SCRN_TOP+1,SCRN_LEFT+width+1
 			,SCRN_TOP+(height-1),shade);
-		for(j=1;j<12;j+=2)
+		for(j=1;j<1024;j+=2)
 			shade[j]=DARKGRAY;
 		puttext(SCRN_LEFT+width,SCRN_TOP+1,SCRN_LEFT+width+1
 			,SCRN_TOP+(height-1),shade);
@@ -121,6 +126,45 @@ int drawfpwindow(uifcapi_t *api)
 	}
 }
 
+void free_opt_list(char **opts)
+{
+	char **p;
+
+	for(p=opts; *p[0]; p++) {
+		free(*p);
+	}
+	free(opts);
+}
+
+char **get_file_opt_list(char **fns, int files, int dirsonly, int root)
+{
+	char **opts;
+	char *opt;
+	char *p;
+	int  i;
+	int  j=0;
+
+	opts=(char **)malloc((files+2)*sizeof(char *));
+	if(opts==NULL)
+		return(NULL);
+	if(dirsonly) {
+		if(!root)
+			opts[j++]=strdup("..");
+	}
+	for(i=0;i<files;i++) {
+		if(isdir(fns[i])) {
+			if(dirsonly)
+				opts[j++]=strdup(getfname(fns[i]));
+		}
+		else {
+			if(!dirsonly)
+				opts[j++]=strdup(getfname(fns[i]));
+		}
+	}
+	opts[j]="";
+	return(opts);
+}
+
 int filepick(uifcapi_t *api, char *title, struct file_pick *fp, char *dir, char *msk, int opts)
 {
 	char	cpath[MAX_PATH+1];
@@ -130,11 +174,21 @@ int filepick(uifcapi_t *api, char *title, struct file_pick *fp, char *dir, char 
 	char	*p1;
 	char	*p2;
 	glob_t	fgl;
-	glob_t	dgl;
-	int		cur=0;
-	int		bar=0;
+	int		dircur=0;
+	int		dirbar=0;
+	int		filecur=0;
+	int		filebar=0;
 	int		listwidth;
 	int	listheight;
+	char	**dir_list;
+	char	**file_list;
+	int		currfield=DIR_LIST;
+	int		i;
+	int		root=0;
+	int		reread=FALSE;
+	int		lbclr;
+
+	lbclr=api->lbclr;
 
 	/* No struct passed */
 	if(fp==NULL)
@@ -166,20 +220,64 @@ int filepick(uifcapi_t *api, char *title, struct file_pick *fp, char *dir, char 
 	else {
 		SAFECOPY(cmsk,msk);
 	}
-	sprintf(cglob,"%s/%s",cpath,cmsk);
-
 	listwidth=SCRN_RIGHT-SCRN_LEFT+1;
 	listwidth-=listwidth%2;
-	listwidth-=2;
+	listwidth-=3;
 	listwidth/=2;
 	/* Draw the file picker itself... */
+	drawfpwindow(api);
 
 	while(1) {
-		drawfpwindow(api);
+		sprintf(cglob,"%s/%s",cpath,cmsk);
+#ifdef __unix__
+		if(cpath[0])
+			root=FALSE;
+		else
+			root=TRUE;
+#else
+#error Need to do something about root paths (in get_file_opt_list() too!)
+#endif
 		if(glob(cglob, 0, NULL, &fgl)!=0)
 			return(-1);
 		api->list_height=api->scrn_len-4-8;
-		api->list(WIN_NOBRDR|WIN_FIXEDHEIGHT,1,3,listwidth,&cur,&bar,NULL,fgl.gl_pathv);
+		dir_list=get_file_opt_list(fgl.gl_pathv, fgl.gl_pathc, TRUE, root);
+		file_list=get_file_opt_list(fgl.gl_pathv, fgl.gl_pathc, FALSE, root);
+		reread=FALSE;
+		dircur=dirbar=filecur=filecur=0;
+		while(!reread) {
+			api->lbclr=api->lclr|(api->bclr<<4);
+			api->list(WIN_NOBRDR|WIN_FIXEDHEIGHT|WIN_IMM|WIN_REDRAW,1,3,listwidth,&dircur,&dirbar,NULL,dir_list);
+			api->list(WIN_NOBRDR|WIN_FIXEDHEIGHT|WIN_IMM|WIN_REDRAW,1+listwidth+1,3,listwidth,&filecur,&filebar,NULL,file_list);
+			api->lbclr=lbclr;
+			switch(currfield) {
+				case DIR_LIST:
+					i=api->list(WIN_NOBRDR|WIN_FIXEDHEIGHT|WIN_EXTKEYS,1,3,listwidth,&dircur,&dirbar,NULL,dir_list);
+					if(i==-2-'\t')
+						currfield++;
+					if(i==0 && !root) {	/* Previous Dir */
+						p1=strrchr(cpath,'/');
+	#ifdef _WIN32
+						p2=strrchr(cpath,'\\');
+						if(p2>p1)
+							p1=p2;
+	#endif
+						*p1=0;
+						reread=TRUE;
+						break;
+					}
+					if(i>=0) {
+						strcat(cpath,"/");
+						strcat(cpath,dir_list[i]);
+						reread=TRUE;
+					}
+					break;
+				case FILE_LIST:
+					api->list(WIN_NOBRDR|WIN_FIXEDHEIGHT|WIN_EXTKEYS,1+listwidth+1,3,listwidth,&filecur,&filebar,NULL,file_list);
+					if(i==-2-'\t')	/* This is only until the text fields are here */
+						currfield--;
+					break;
+			}
+		}
 	}
 
 	return(0);
