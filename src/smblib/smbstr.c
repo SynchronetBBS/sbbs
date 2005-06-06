ddd8d8e6b619d9e55da70896e26d9796362c1396
deuce <>
2005-06-06 22:49:28 +0000

    Fix Intel CC warnings.

--- src/smblib/smbstr.c
+++ src/smblib/smbstr.c
@@ -35,6 +35,7 @@
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
 
+#include <ctype.h>		/* is*() */
 #include <genwrap.h> 		/* stricmp */
 #include "smblib.h"
 
