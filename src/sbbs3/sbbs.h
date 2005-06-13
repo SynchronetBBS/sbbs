6edc9f8e7c51f0d5209a7d8dc941cabed267b168
rswindell <>
2005-06-13 02:33:42 +0000

    Moved nopen.c prototypes into (new file): nopen.h.

--- src/sbbs3/sbbs.h
+++ src/sbbs3/sbbs.h
@@ -137,6 +137,7 @@
 #include "crc16.h"
 #include "crc32.h"
 #include "telnet.h"
+#include "nopen.h"
 
 /* Synchronet Node Instance class definition */
 #ifdef __cplusplus
@@ -584,7 +585,6 @@ public:
 	void	printstatslog(uint node);
 	ulong	logonstats(void);
 	void	logoffstats(void);
-	int		nopen(char *str, int access);
 	int		mv(char *src, char *dest, char copy); /* fast file move/copy function */
 	bool	chksyspass(void);
 	bool	chk_ar(uchar * str, user_t * user); /* checks access requirements */
@@ -1034,12 +1034,6 @@ char *	hexplus(uint num, char *str); 	/* Hex plus for 3 digits up to 9000 */
 uint	hptoi(char *str);
 int		pstrcmp(char **str1, char **str2);  /* Compares pointers to pointers */
 int		strsame(char *str1, char *str2);	/* Compares number of same chars */
-
-/* nopen.c */
-int		nopen(const char* str, int access);
-FILE *	fnopen(int* file, const char* str, int access);
-BOOL	ftouch(const char* fname);
-BOOL	fmutex(const char* fname, const char* text);
 
 /* load_cfg.c */
 BOOL 	md(char *path);
