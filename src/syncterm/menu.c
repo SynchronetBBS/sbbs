3f73b70b102e67de354a282e15c5cc8db6d7971f
deuce <>
2005-06-17 19:02:03 +0000

    Fix bug in last commit (change the value the pointer points to, not the pointer itself)

--- src/syncterm/menu.c
+++ src/syncterm/menu.c
@@ -158,7 +158,7 @@ int syncmenu(struct bbslist *bbs, int *speed)
 					j=get_rate_num(*speed);
 					i=uifc.list(WIN_MID|WIN_SAV,0,0,0,&j,NULL,"Output Rate",rate_names);
 					if(i>=0)
-						speed = rates[i];
+						*speed = rates[i];
 				}
 				ret=5;
 				break;
