a92ba4422b8968b3fb3040aa866210f53815b5a9
rswindell <>
2005-06-17 07:12:22 +0000

    Display the syncterm_version string in the title bar of the main menu.

--- src/syncterm/uifcinit.c
+++ src/syncterm/uifcinit.c
@@ -8,6 +8,8 @@
 
 #include "uifcinit.h"
 
+extern char* syncterm_version;
+
 uifcapi_t uifc; /* User Interface (UIFC) Library API */
 static int uifc_initialized=0;
 
@@ -25,7 +27,7 @@ int	init_uifc(void) {
 	}
 	uifc_initialized=1;
 
-	if(uifc.scrn("SyncTERM")) {
+	if(uifc.scrn(syncterm_version)) {
 		printf(" USCRN (len=%d) failed!\n",uifc.scrn_len+1);
 		uifc_initialized=0;
 		uifc.bail();
