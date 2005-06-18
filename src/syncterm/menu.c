2936c677d18d5f43247524c85ef8b1bccfe25cd9
deuce <>
2005-06-18 21:55:28 +0000

    Add help to the online menu.

--- src/syncterm/menu.c
+++ src/syncterm/menu.c
@@ -133,6 +133,17 @@ int syncmenu(struct bbslist *bbs, int *speed)
 
 	for(ret=0;!ret;) {
 		init_uifc();
+		uifc.helpbuf=	"`Online Menu`\n\n"
+						"~ Scrollback ~         allows to you to view the scrollback buffer\n"
+						"~ Disconnect ~         disconnects the current session and returns to the\n"
+						"                     dialing list\n"
+						"~ Send Login ~         Sends the configured user and password pair separated\n"
+						"                     by a \\r\n"
+						"~ Zmodem Upload ~      Initiates a ZModem upload\n"
+						"~ Zmodem Download ~    Initiates a ZModem download\n"
+						"~ Change Output Rate ~ Changes the speed charaters are output to the screen\n"
+						"~ Change Log Level ~   Changes the minimum log leve for ZModem information\n"
+						"~ Exit ~               Disconnects and closes the Syncterm";
 		i=uifc.list(WIN_MID|WIN_SAV,0,0,0,&opt,NULL,"SyncTERM Online Menu",opts);
 		switch(i) {
 			case -1:	/* Cancel */
@@ -157,6 +168,10 @@ int syncmenu(struct bbslist *bbs, int *speed)
 			case 5:		/* Output rate */
 				if(speed != NULL) {
 					j=get_rate_num(*speed);
+					uifc.helpbuf="`Output Rate`\n\n"
+							"The output rate is the rate in emulated \"bits per second\" to draw incoming\n"
+							"data on the screen.  This rate is a maximum, not guaranteed to be attained\n"
+							"In general, you will only use this option for ANSI animations.";
 					i=uifc.list(WIN_MID|WIN_SAV,0,0,0,&j,NULL,"Output Rate",rate_names);
 					if(i>=0)
 						*speed = rates[i];
@@ -165,6 +180,10 @@ int syncmenu(struct bbslist *bbs, int *speed)
 				break;
 			case 6:		/* Change log level (temporarily) */
 				j=log_level;
+				uifc.helpbuf="`Log Level\n\n"
+						"The log level changes the verbosity of messages shown in the transfer\n"
+						"window.  For the selected log level, messages of that level and those above\n"
+						"it will be displayed.";
 				i=uifc.list(WIN_MID|WIN_SAV,0,0,0,&j,NULL,"Log Level",log_levels);
 				if(i>=0)
 					log_level = j;
