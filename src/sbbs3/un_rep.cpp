9cbd24cb7136c7d28689871a5762cfd66e39b81e
rswindell <>
2005-06-23 08:42:40 +0000

    Delete/ignore the file ATTXREF.DAT if included in .REP packets.

--- src/sbbs3/un_rep.cpp
+++ src/sbbs3/un_rep.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -474,6 +474,9 @@ bool sbbs_t::unpack_rep(char* repfile)
 		if(fexistcase(str))
 			remove(str);
 		sprintf(str,"%s%s.rep",cfg.temp_dir,cfg.sys_id);
+		if(fexistcase(str))
+			remove(str);
+		sprintf(str,"%sATTXREF.DAT",cfg.temp_dir);
 		if(fexistcase(str))
 			remove(str);
 
