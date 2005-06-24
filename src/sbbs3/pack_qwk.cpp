1337b74e758e9dfa12d8951307cf942614346798
rswindell <>
2005-06-24 17:34:44 +0000

    Ignore the "Your Messages Only" scan configuration flag for QWKnet accounts.

--- src/sbbs3/pack_qwk.cpp
+++ src/sbbs3/pack_qwk.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -369,7 +369,7 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 				k=0;
 				if(useron.qwk&QWK_BYSELF)
 					k|=LP_BYSELF;
-				if(!(subscan[usrsub[i][j]].cfg&SUB_CFG_YSCAN))
+				if(useron.rest&FLAG('Q') || !(subscan[usrsub[i][j]].cfg&SUB_CFG_YSCAN))
 					k|=LP_OTHERS;
 				post=loadposts(&posts,usrsub[i][j],subscan[usrsub[i][j]].ptr,k);
 
