9c3107133877ebab0085c97652fc0c34b0d77ca1
rswindell <>
2005-06-25 01:37:02 +0000

    If iniSetDateTime() is used to set a value to 0, set it to "Never" rather than "0x0".

--- src/xpdev/ini_file.c
+++ src/xpdev/ini_file.c
@@ -474,7 +474,9 @@ char* iniSetDateTime(str_list_t* list, const char* section, const char* key
 	char	tstr[32];
 	char*	p;
 
-	if((p=ctime_r(&value,tstr))==NULL)
+	if(value==0)
+		SAFECOPY(str,"Never");
+	else if((p=ctime_r(&value,tstr))==NULL)
 		SAFEPRINTF(str,"0x%lx",value);
 	else if(!include_time)	/* reformat into "Mon DD YYYY" */
 		safe_snprintf(str,sizeof(str),"%.3s %.2s %.4s"		,p+4,p+8,p+20);
