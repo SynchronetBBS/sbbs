598c34ece6e6e875c0d6f9b424b0fdd9163bb9b5
deuce <>
2005-08-03 23:19:19 +0000

    Fix error with the delimiters.

--- exec/gnats_cvs.js
+++ exec/gnats_cvs.js
@@ -17,7 +17,7 @@ if(!gnats.connect())
 
 while((line=readln()) != undefined)
 	logmsg += line+"\n";
-var logmsg='--- End CVS Commit Message ---\n';
+logmsg+='--- End CVS Commit Message ---\n';
 
 auditprs=new Object;
 stateprs=new Object;
