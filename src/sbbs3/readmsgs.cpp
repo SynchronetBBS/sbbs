29f2cf53d05762f7ae754d441869fa7ec44c0229
rswindell <>
2005-08-12 08:38:11 +0000

    Bug-fix: Don't force saved message text/attachment filenames to upper case (for Unix file system compatibilty).

--- src/sbbs3/readmsgs.cpp
+++ src/sbbs3/readmsgs.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -950,7 +950,7 @@ int sbbs_t::scanposts(uint subnum, long mode, char *find)
 								break;
 	*/
 							bputs(text[FileToWriteTo]);
-							if(getstr(str,40,K_LINE|K_UPPER))
+							if(getstr(str,40,K_LINE))
 								msgtotxt(&msg,str,1,1);
 							break;
 						case 'U':   /* User edit */
--- src/sbbs3/writemsg.cpp
+++ src/sbbs3/writemsg.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -254,7 +254,7 @@ bool sbbs_t::writemsg(char *fname, char *top, char *title, long mode, int subnum
 				c=25;
 			bputs(text[SubjectPrompt]); 
 		}
-		if(!getstr(title,c,mode&WM_FILE ? K_LINE|K_UPPER : K_LINE|K_EDIT|K_AUTODEL)
+		if(!getstr(title,c,mode&WM_FILE ? K_LINE : K_LINE|K_EDIT|K_AUTODEL)
 			&& useron_level && useron.logons) {
 			free(buf);
 			return(false); 
