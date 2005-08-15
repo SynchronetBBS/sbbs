aa7d1aa87c112da59d59e7bf7e58580d0147eae3
rswindell <>
2005-08-15 21:28:11 +0000

    Call gettimeleft() in privchat() before displaying the split-chat screen. This fixes a problem with an uninitialized timeleft value being displayed when popped into local sysop chat from the login prompt. PR: 38

--- src/sbbs3/chat.cpp
+++ src/sbbs3/chat.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -788,6 +788,8 @@ void sbbs_t::privchat(bool local)
 			} 
 		}
 	}
+
+	gettimeleft();
 
 	if(getnodedat(cfg.node_num,&thisnode,true)==0) {
 		thisnode.action=action=NODE_PCHT;
