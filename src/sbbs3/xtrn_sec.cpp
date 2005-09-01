07b92049426899a7bd33782275a74c5270ce9220
rswindell <>
2005-09-01 10:09:29 +0000

    Use correct column value in chain.txt (instead of hard-coded '80').

--- src/sbbs3/xtrn_sec.cpp
+++ src/sbbs3/xtrn_sec.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -465,7 +465,7 @@ void sbbs_t::xtrndat(char *name, char *dropdir, uchar type, ulong tleft
 		sprintf(str,"%lu\n%s\n%u\n%lu\n%u\n%u\n%u\n%u\n%u\n"
 			,useron.cdt+useron.freecdt			/* Gold */
 			,unixtodstr(&cfg,useron.laston,tmp)	/* User last on date */
-			,80 								/* User screen width */
+			,cols 								/* User screen width */
 			,rows								/* User screen length */
 			,useron.level						/* User SL */
 			,0									/* Cosysop? */
