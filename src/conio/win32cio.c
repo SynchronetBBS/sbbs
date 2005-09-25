c1e92016ea71d5a9ad9957703c4ab1d97ed62cec
deuce <>
2005-09-25 23:08:04 +0000

    Shotgun approach to fixing the numeric keypad with numlock off.

--- src/conio/win32cio.c
+++ src/conio/win32cio.c
@@ -264,7 +264,9 @@ int win32_keyboardio(int isgetch)
 			case KEY_EVENT:
 				if(input.Event.KeyEvent.bKeyDown) {
 					if((input.Event.KeyEvent.dwControlKeyState & (RIGHT_ALT_PRESSED|LEFT_ALT_PRESSED|RIGHT_CTRL_PRESSED|LEFT_CTRL_PRESSED|ENHANCED_KEY))
-							|| (input.Event.KeyEvent.wVirtualKeyCode >= VK_F1 && input.Event.KeyEvent.wVirtualKeyCode <= VK_F24))
+							|| (input.Event.KeyEvent.wVirtualKeyCode >= VK_F1 && input.Event.KeyEvent.wVirtualKeyCode <= VK_F24)
+							|| !input.Event.KeyEvent.uChar.AsciiChar
+							|| (!(input.Event.KeyEvent.dwControlKeyState & NUMLOCK_ON) && (input.Event.KeyEvent.uChar.AsciiChar >= '0' && input.Event.KeyEvent.uChar.AsciiChar <= '9')))
 						lastch=win32_getchcode(input.Event.KeyEvent.wVirtualKeyCode, input.Event.KeyEvent.dwControlKeyState);
 					else
 						lastch=input.Event.KeyEvent.uChar.AsciiChar;
