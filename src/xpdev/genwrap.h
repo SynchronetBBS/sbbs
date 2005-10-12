a075d94eee59c7a4dca23c843e14dada6de0ffc0
rswindell <>
2005-10-12 19:45:05 +0000

    Fix-up the millisecond clock stuff: MSCLOCKS_PER_SEC is always 1000 (Borland defines CLOCKS_PER_SEC as 1000.0). Added msclock_t definition (currently always the same as clock_t).

--- src/xpdev/genwrap.h
+++ src/xpdev/genwrap.h
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -296,11 +296,12 @@ DLLEXPORT char*		DLLCALL c_unescape_str(char* str);
 DLLEXPORT char		DLLCALL c_unescape_char_ptr(const char* str, char** endptr);
 DLLEXPORT char		DLLCALL c_unescape_char(char ch);
 
-#if !defined(__unix__)
-	#define		msclock()			clock()
-	#define		MSCLOCKS_PER_SEC	CLOCKS_PER_SEC
+/* Millisecond clock */
+typedef		clock_t				msclock_t;
+#define		MSCLOCKS_PER_SEC	1000
+#if defined(_WIN32)
+	#define		msclock()		clock()
 #else
-	#define		MSCLOCKS_PER_SEC	1000
 	clock_t		msclock(void);
 #endif
 
