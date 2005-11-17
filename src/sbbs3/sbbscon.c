b5e4738a1d6d393b181bc4b6f324d0c3fcc3daed
deuce <>
2005-11-17 04:41:35 +0000

    If sigwait() fails due to EINTR, block and log the signal which caused the failure.

--- src/sbbs3/sbbscon.c
+++ src/sbbs3/sbbscon.c
@@ -934,6 +934,13 @@ static void handle_sigs(void)
 	while(1)  {
 		if((i=sigwait(&sigs,&sig))!=0) {   /* wait here until signaled */
 			lprintf(LOG_ERR,"     !sigwait FAILURE (%d)", i);
+			if(i==EINTR) {
+				sigset_t moresigs;
+				lprintf(LOG_ERR,"     Adding signal %d to blocked set",i);
+				memcpy(&moresigs, &sigs, sizeof(moresigs));
+				sigaddset(&sigs, sig);
+				pthread_sigmask(SIG_BLOCK,&moresigs,NULL);
+			}
 			continue;
 		}
 		lprintf(LOG_NOTICE,"     Got signal (%d)", sig);
