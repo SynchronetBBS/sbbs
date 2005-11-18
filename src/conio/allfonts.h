d6b04fdf59840765b497714e4f770f0c884b6344
deuce <>
2005-11-18 21:28:45 +0000

    Generated font data for all fonts which ship with FreeBSD 6.0.

---
+++ src/conio/allfonts.h
@@ -0,0 +1,13 @@
+#ifndef _ALLFONTS_H_
+#define _ALLFONTS_H_
+
+struct conio_font_data_struct {
+        char *eight_by_sixteen;
+        char *eight_by_fourteen;
+        char *eight_by_eight;
+        char *desc;
+};
+
+extern struct conio_font_data_struct conio_fontdata[33];
+
+#endif
