54ef826dbb2c17cac57e355d456485c8fe86546f
rswindell <>
2005-11-19 05:46:44 +0000

    Updated for menulib.js support.

--- exec/menu.js
+++ exec/menu.js
@@ -10,7 +10,7 @@ load("menulib.js");
 var menu_stack = new Array();
 var pop_count = 0;
 
-menu(argv[0]);
+exec_menu(argv[0]);
 
 function isdigit(ch)
 {
@@ -19,7 +19,7 @@ function isdigit(ch)
 	return(val>=ascii('0') && val<=ascii('9'));
 }
 
-function menu(fname)
+function exec_menu(fname)
 {
 	if(!fname) {
 		alert(log(LOG_WARNING,"!No menu file specified"));
@@ -36,6 +36,7 @@ function menu(fname)
 			return;
 		}
 
+	log("Reading menu file" + fname);
 	var menu;
 	if((menu=read_menu(fname))==undefined)
 		return(false);
@@ -51,48 +52,49 @@ function menu(fname)
 		}
 		var cmd_keys="";
 		var cmd_list=new Array();
-		if(hotkey_ext)
-			cmd_keys+=hotkey_ext;
-		for(i in command) {
-			if(command[i].ars && !user.compare_ars(command[i].ars))
+		if(menu.hotkey_ext)
+			cmd_keys+=menu.hotkey_ext;
+		for(i in menu.command) {
+			if(menu.command[i].ars && !user.compare_ars(menu.command[i].ars))
 				continue;
-			cmd_keys+=command[i].key;
-			cmd_list.push(command[i]);
+			cmd_keys+=menu.command[i].key;
+			cmd_list.push(menu.command[i]);
 		}
-
-		if(show_menu || !expert || !(user.settings&USER_EXPERT)) {
-			if(menu_file)	/* static menu */
-				bbs.menu(menu_file);
+		log("menu.show: " + menu.show);
+		
+		if(menu.show || !menu.expert || !(user.settings&USER_EXPERT)) {
+			if(menu.file)	/* static menu */
+				bbs.menu(menu.file);
 			else {			/* dynamic menu */
 				var column=0;
 				for(var i in cmd_list) {
 					if(cmd_list[i].description) {
-						if(column+menu_colwidth>=console.screen_columns)
+						if(column+menu.colwidth>=console.screen_columns)
 							console.crlf(), column=0;
-						var str=format(menu_fmt
-							,menu_reverse ? cmd_list[i].description : cmd_list[i].key.toUpperCase()
-							,menu_reverse ? cmd_list[i].key.toUpperCase() : cmd_list[i].description);
+						var str=format(menu.fmt
+							,menu.reverse ? cmd_list[i].description : cmd_list[i].key.toUpperCase()
+							,menu.reverse ? cmd_list[i].key.toUpperCase() : cmd_list[i].description);
 						console.putmsg(str);
-						console.right(menu_colwidth-console.strlen(str));
-						if(console.strlen(str)>menu_colwidth)
+						console.right(menu.colwidth-console.strlen(str));
+						if(console.strlen(str)>menu.colwidth)
 							column+=console.strlen(str);
 						else
-							column+=menu_colwidth;
+							column+=menu.colwidth;
 					}
 				}
 			}
-			show_menu = false;
+			menu.show = false;
 		}
 		bbs.nodesync();
 		console.aborted=false;
-		if(exec)
-			eval(exec);
+		if(menu.exec)
+			eval(menu.exec);
 
-		if(prompt)
-			console.putmsg(eval(prompt));
+		if(menu.prompt)
+			console.putmsg(eval(menu.prompt));
 
 		var cmd;
-		if(hotkeys)
+		if(menu.hotkeys)
 			console.write(cmd=console.getkey(K_UPPER));
 		else
 			cmd=console.getstr(K_UPPER);
@@ -100,13 +102,15 @@ function menu(fname)
 		if(cmd==undefined)
 			continue;
 
-		if(isdigit(cmd) && num_exec) {
-			if(parseInt(cmd)*10 > maxnum)
-				eval(num_exec);
+		log("menu.num_exec = " + menu.num_exec);
+		log("menu.maxnum = " + menu.maxnum);
+		if(isdigit(cmd) && menu.num_exec) {
+			if(parseInt(cmd)*10 <= menu.maxnum)
+				eval(menu.num_exec);
 			continue;
 		}
 
-		if(hotkeys && cmd.toUpperCase()==hotkey_ext)
+		if(menu.hotkeys && cmd.toUpperCase()==menu.hotkey_ext)
 			cmd+=console.getkey();
 
 		print();
@@ -118,3 +122,5 @@ function menu(fname)
 
 	menu_stack.pop();
 }
+
+log("exiting");
--- exec/menuedit.js
+++ exec/menuedit.js
@@ -3,22 +3,30 @@ load("uifcdefs.js");
 
 main(argc, argv);
 
+function edit_menu(fname)
+{
+	var menu = read_menu(fname);
+	if(!menu)
+		return menu;
+	var options=[];
+	for(var i in menu)
+		options.push(format("%-15s ", i) + menu[i]);
+	uifc.list(WIN_MID,"Options",options);
+}
+
 function main(argc, argv)
 {
-	uifc.init("Synchronet Menu Editor", "ansi");
+	uifc.init("Synchronet Menu Editor");
 	js.on_exit("uifc.bail()");
 
+	if(argc)
+		return edit_menu(argv[0]);
+		
 	while(1) {
 		var menu_list = directory(system.exec_dir + "*.menu");
-		var selection = uifc.list(WIN_MID|WIN_INS|WIN_XTR,"Menu Files",menu_list);
+		var selection = uifc.list(WIN_ORG|WIN_MID|WIN_INS|WIN_XTR,"Menu Files",menu_list);
 		if(selection<0)
 			break;
-		var menu = read_menu(file_getname(menu_list[selection]));
-		if(!menu)
-			continue;
-		var options=[];
-		for(var i in menu)
-			options.push(format("%-25s ", i) + menu[i]);
-		uifc.list(WIN_MID,"Options",options);
+		edit_menu(file_getname(menu_list[selection]));
 	}
 }
