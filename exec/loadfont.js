8e4acc02aa19e42321477509a90d106c3abd1112
deuce <>
2005-11-22 08:18:07 +0000

    Better CTerm detection... both loadfont and pickfont will detect, and neither will redetect.

--- exec/loadfont.js
+++ exec/loadfont.js
@@ -55,50 +55,62 @@ if(firstslot < 32) {
 	exit(1);
 }
 
-// Disable parsed input... we need to do ESC processing ourselves here.
-var oldctrl=console.ctrlkey_passthru;
-console.ctrlkey_passthru=-1;
-
 if(showprogress) {
 	writeln();
 	write("Detecting font support... ");
 }
 
 // Check if it's CTerm and supports font loading...
-write("\x1b[c");
-var response='';
+var ver;
+if(console.terminal.substr(0,6) != 'CTerm;') {
+	// Disable parsed input... we need to do ESC processing ourselves here.
+	var oldctrl=console.ctrlkey_passthru;
+	console.ctrlkey_passthru=-1;
 
-while(1) {
-	var ch=console.inkey(0, 5000);
-	if(ch=="")
-		break;
-	response += ch;
-	if(ch != '\x1b' && ch != '[' && (ch < ' ' || ch > '/') && (ch<'0' || ch > '?'))
-		break;
-}
-// var printable=response;
-// printable=printable.replace(/\x1b/g,"");
-// alert("Response: "+printable);
+	write("\x1b[c");
+	var response='';
 
-if(response.substr(0,21) != "\x1b[=67;84;101;114;109;") {	// Not CTerm
+	while(1) {
+		var ch=console.inkey(0, 5000);
+		if(ch=="")
+			break;
+		response += ch;
+		if(ch != '\x1b' && ch != '[' && (ch < ' ' || ch > '/') && (ch<'0' || ch > '?'))
+			break;
+	}
+	// var printable=response;
+	// printable=printable.replace(/\x1b/g,"");
+	// alert("Response: "+printable);
+
+	if(response.substr(0,21) != "\x1b[=67;84;101;114;109;") {	// Not CTerm
+		console.ctrlkey_passthru=oldctrl;
+		if(showprogress)
+			writeln("Not detected.");
+		exit(0);
+	}
+	if(response.substr(-1) != "c") {	// Not a DA
+		console.ctrlkey_passthru=oldctrl;
+		if(showprogress)
+			writeln("Not detected.");
+		exit(0);
+	}
+	var version=response.substr(21);
+	version=version.replace(/c/,"");
+	ver=version.split(/;/);
+	console.terminal="CTerm;"+ver.join(";");
 	console.ctrlkey_passthru=oldctrl;
-	if(showprogress)
-		writeln("Not detected.");
-	exit(0);
 }
-if(response.substr(-1) != "c") {	// Not a DA
-	console.ctrlkey_passthru=oldctrl;
-	if(showprogress)
-		writeln("Not detected.");
-	exit(0);
+else {
+	ver=console.terminal.substr(6).split(/;/);
+	if(parseInt(ver[0]) < 1 || (parseInt(ver[0])==1 && parseInt(ver[1]) < 61)) {
+		// Too old for dynamic fonts
+		if(showprogress)
+			writeln("Not detected.");
+		exit(0);
+	}
 }
-var version=response.substr(21);
-version=version.replace(/c/,"");
-var ver=version.split(/;/);
-console.terminal="CTerm;"+ver.join(";");
 if(parseInt(ver[0]) < 1 || (parseInt(ver[0])==1 && parseInt(ver[1]) < 61)) {
 	// Too old for dynamic fonts
-	console.ctrlkey_passthru=oldctrl;
 	if(showprogress)
 		writeln("Not detected.");
 	exit(0);
--- exec/pickfont.js
+++ exec/pickfont.js
@@ -3,11 +3,49 @@
 // If nothing passed, changes to font 0 (CP437)
 
 // Check if it's CTerm and supports font loading...
-if(console.terminal.substr(0,6) != 'CTerm;')
-	/* Not CTerm */
-	exit(0);
+var ver;
+if(console.terminal.substr(0,6) != 'CTerm;') {
+	// Disable parsed input... we need to do ESC processing ourselves here.
+	var oldctrl=console.ctrlkey_passthru;
+	console.ctrlkey_passthru=-1;
 
-var ver=console.terminal.substr(6).split(/;/);
+	write("\x1b[c");
+	var response='';
+
+	while(1) {
+		var ch=console.inkey(0, 5000);
+		if(ch=="")
+			break;
+		response += ch;
+		if(ch != '\x1b' && ch != '[' && (ch < ' ' || ch > '/') && (ch<'0' || ch > '?'))
+			break;
+	}
+	// var printable=response;
+	// printable=printable.replace(/\x1b/g,"");
+	// alert("Response: "+printable);
+
+	if(response.substr(0,21) != "\x1b[=67;84;101;114;109;") {	// Not CTerm
+		console.ctrlkey_passthru=oldctrl;
+		exit(0);
+	}
+	if(response.substr(-1) != "c") {	// Not a DA
+		console.ctrlkey_passthru=oldctrl;
+		exit(0);
+	}
+	var version=response.substr(21);
+	version=version.replace(/c/,"");
+	ver=version.split(/;/);
+	console.terminal="CTerm;"+ver.join(";");
+	console.ctrlkey_passthru=oldctrl;
+}
+else {
+	ver=console.terminal.substr(6).split(/;/);
+	if(parseInt(ver[0]) < 1 || (parseInt(ver[0])==1 && parseInt(ver[1]) < 61)) {
+		// Too old for dynamic fonts
+		exit(0);
+	}
+}
+
 if(parseInt(ver[0]) < 1 || (parseInt(ver[0])==1 && parseInt(ver[1]) < 61)) {
 	// Too old for dynamic fonts
 	exit(0);
