12b24cf5b02868a4e3d33ec2fc098ca8158b6f88
deuce <>
2006-01-11 02:24:39 +0000

    Deal with the Solaris lack of SUN_LEN... use sizeof() instead.
    The sizeof acts on THIS:
    /*
     * Definitions for UNIX IPC domain.
     */
    struct  sockaddr_un {
            sa_family_t     sun_family;             /* AF_UNIX */
            char            sun_path[108];          /* path name (gag) */
    };
    
    I second that gag.

--- src/sbbs3/umonitor/spyon.c
+++ src/sbbs3/umonitor/spyon.c
@@ -72,7 +72,11 @@ int spyon(char *sockname)  {
 	
 	spy_name.sun_family=AF_UNIX;
 	SAFECOPY(spy_name.sun_path,sockname);
+#ifdef SUN_LEN
 	spy_len=SUN_LEN(&spy_name);
+#else
+	spy_len=sizeof(struct sockaddr_un);
+#endif
 	if(connect(spy_sock,(struct sockaddr *)&spy_name,spy_len))  {
 		return(SPY_NOCONNECT);
 	}
