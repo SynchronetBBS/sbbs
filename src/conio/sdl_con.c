9cb0a2c4d5b144b373f4c3b7ba837b3d0ccc9314
deuce <>
2006-01-13 07:32:42 +0000

    Do not check the video driver name of symbol loading failed.

--- src/conio/sdl_con.c
+++ src/conio/sdl_con.c
@@ -1380,12 +1380,12 @@ int SDL_main_env(int argc, char **argv, char **env)
 		if(sdl.Init(SDL_INIT_VIDEO))
 			sdl.gotfuncs=FALSE;
 #endif
-	}
-	if(sdl.VideoDriverName(drivername, sizeof(drivername))!=NULL) {
-		/* Unacceptable drivers */
-		if((!strcmp(drivername, "caca")) || (!strcmp(drivername,"aalib")) || (!strcmp(drivername,"dummy"))) {
-			sdl.gotfuncs=FALSE;
-			sdl.Quit();
+		if(sdl.VideoDriverName(drivername, sizeof(drivername))!=NULL) {
+			/* Unacceptable drivers */
+			if((!strcmp(drivername, "caca")) || (!strcmp(drivername,"aalib")) || (!strcmp(drivername,"dummy"))) {
+				sdl.gotfuncs=FALSE;
+				sdl.Quit();
+			}
 		}
 	}
 
