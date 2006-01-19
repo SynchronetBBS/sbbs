996a120eeee05903dbbd299fa2f4323c778f7153
rswindell <>
2006-01-19 18:46:06 +0000

    Fix jashud-reported typo: "forwaded to".

--- src/sbbs3/mailsrvr.c
+++ src/sbbs3/mailsrvr.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -2549,7 +2549,7 @@ static void smtp_thread(void* arg)
 							,timestr(&scfg,(time_t*)&newmsg.hdr.when_imported.time,tmp)
 							,sender,sender_addr);
 						if(!newmsg.idx.to) {	/* Forwarding */
-							strcat(str,"\1mand it was automatically forwaded to: \1h");
+							strcat(str,"\1mand it was automatically forwarded to: \1h");
 							strcat(str,user.netmail);
 							strcat(str,"\1n\r\n");
 						}
