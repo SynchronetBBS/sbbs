a709c349184b1ef11d228accec52c49ac543b8e9
deuce <>
2006-01-30 05:46:05 +0000

    Abuse K_GETSTR to force K_SPIN off. Naughty Deuce.

--- exec/load/lightbar.js
+++ exec/load/lightbar.js
@@ -341,7 +341,7 @@ function Lightbar_getval(current)
 		last_cur=this.current;
 
 		/* Get input */
-		var key=console.getkey(K_UPPER);
+		var key=console.getkey(K_UPPER|K_GETSTR);
 		if(this.hotkeys.indexOf(key)!=-1)
 			return(key);
 		switch(key) {
