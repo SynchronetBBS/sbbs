5311c501631e07d84eeebb11fbcbba779e7a285c
rswindell <>
2006-01-31 23:11:24 +0000

    Default web root dir is now ../web/root (changed from ../web/html).

--- src/sbbs3/websrvr.h
+++ src/sbbs3/websrvr.h
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -145,7 +145,7 @@ static ini_bitdesc_t web_options[] = {
 };
 #endif
 
-#define WEB_DEFAULT_ROOT_DIR		"../web/html"
+#define WEB_DEFAULT_ROOT_DIR		"../web/root"
 #define WEB_DEFAULT_ERROR_DIR		"error"
 #define WEB_DEFAULT_CGI_DIR			"cgi-bin"
 #define WEB_DEFAULT_CGI_CONTENT		"text/plain"
