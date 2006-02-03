51e904a1f0748239e6c2d8f6b8c2468f60828477
rswindell <>
2006-02-03 06:21:37 +0000

    Updated copyright notice to 2006.

--- src/sbbs3/sbbsdefs.h
+++ src/sbbs3/sbbsdefs.h
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -58,8 +58,8 @@
 #define VERSION_NOTICE		"Synchronet BBS for "PLATFORM_DESC\
 								"  Version " VERSION
 #define SYNCHRONET_CRC		0x9BCDD162
-#define COPYRIGHT_NOTICE	"Copyright 2005 Rob Swindell"
-#define COPYRIGHT_CRC		0x9D0B491A
+#define COPYRIGHT_NOTICE	"Copyright 2006 Rob Swindell"
+#define COPYRIGHT_CRC		0xE16A6CC1
 
 #define Y2K_2DIGIT_WINDOW	70
 
