d03d3dfb8a9351b0c2090d84285a49e6f3c8d547
deuce <>
2006-02-13 18:52:55 +0000

    Support a timeout/callback mechanism to allow periodic callbacks from within a lightbar getval() call.

--- exec/load/lightbar.js
+++ exec/load/lightbar.js
@@ -75,6 +75,8 @@ function Lightbar(items)
 	this.rpadding=null;
 	this.hblanks=2;
 	this.hotkeys='';
+	this.callback=undefined;
+	this.timeout=0;
 	if(items==undefined)
 		this.items=new Array();
 	else
@@ -367,7 +369,15 @@ function Lightbar_getval(current)
 		 * This is a hack which triples the time that an ESC takes to be
 		 * procesed.
 		 */
-		var key=console.getkey(K_UPPER|(user.settings&USER_SPIN?K_GETSTR:0));
+		var key=undefined;
+		while(key==undefined || key=='' || key==null) {
+			if(this.callback != undefined)
+				this.callback();
+			if(this.timeout>1)
+				key=console.inkey(K_UPPER|(user.settings&USER_SPIN?K_GETSTR:0),this.timeout);
+			else
+				key=console.getkey(K_UPPER|(user.settings&USER_SPIN?K_GETSTR:0));
+		}
 		if(this.hotkeys.indexOf(key)!=-1) {
 			this.nodraw=false;
 			return(key);
