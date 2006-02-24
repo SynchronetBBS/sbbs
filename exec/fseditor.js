f27cbd7d7604b4f144eb91addde1fd2a47b23339
deuce <>
2006-02-24 22:45:04 +0000

    Basic editing functions and basic wrapping now working.  Only everything else left to do.

--- exec/fseditor.js
+++ exec/fseditor.js
@@ -19,7 +19,7 @@ function Line(attr)
 	this.text='';
 	/* The attributes of this line */
 	this.attr=new Array();
-	for(i=1; i<80; i++)
+	for(i=0; i<80; i++)
 		this.attr[i]=attr;
 	this.canwrap=true;
 	this.draw=Line_draw;
@@ -32,13 +32,29 @@ function Line_draw(ypos)
 	var i;
 
 	console.gotoxy(1,ypos);
-	for(i=1; i<=this.text.length; i++) {
+	for(i=0; i<this.text.length; i++) {
 		console.attributes=this.attr[i];
-		console.write(this.text.substr(i-1,1));
+		console.write(this.text.substr(i,1));
 	}
 	if(i<81) {
 		console.attributes=7;
 		console.cleartoeol();
+	}
+}
+
+/*
+ * Redraws line l if it's currently on the screen
+ */
+function redraw_line(l)
+{
+	if(l>=topline && l<=topline+lines_on_screen) {
+		if(line[l]!=undefined)
+			line[l].draw(edit_top+l-topline);
+		else {
+			console.gotoxy(1,edit_top+l-topline);
+			console.attributes=7;
+			console.cleartoeol();
+		}
 	}
 }
 
@@ -55,29 +71,20 @@ function scroll(count)
 	if(topline<0)
 		topline=0;
 	for(i=0; i<lines_on_screen; i++) {
-		if(line[i+topline]!=undefined)
-			line[i+topline].draw(edit_top+i);
-		else {
-			console.attributes=7;
-			console.cleartoeol();
-		}
+		redraw_line(i+topline);
 	}
 }
 
 /*
- * Adds a new line below the current one.
+ * Adds a new line below line l.
  */
-function add_new_line_below()
+function add_new_line_below(l)
 {
-	line.splice(topline+ypos-edit_top+1,0,new Line(curattr));
+	line.splice(l+1,0,new Line(curattr));
 	/* Scroll lines below down */
+	/* ToDo: Fix this for adding to non-current spot */
 	for(i=ypos-edit_top+1; i<lines_on_screen; i++) {
-		if(line[i+topline]!=undefined)
-			line[i+topline].draw(edit_top+i);
-		else {
-			console.attributes=7;
-			console.cleartoeol();
-		}
+		redraw_line(i+topline);
 	}
 }
 
@@ -146,6 +153,40 @@ function status_line()
 	console.gotoxy(xpos,ypos);
 }
 
+/*
+ * Wraps lines starting at l.  Returns the offset in line# l+1 where
+ * the end of line# l ends.
+ */
+function wrap_line(l)
+{
+	var ret=0;
+	var nline;
+
+	while(line[l].text.length>79) {
+		if(line[l+1]==undefined)
+			add_new_line_below(l);
+		nline=line[l].text.replace(/^(.*)\s+(.*?)$/,function (str, start, end, pos, s) {
+			line[l+1].text=end+" "+line[l+1].text;
+			/* Copy attributes */
+			line[l+1].attr.splice(0,0,line[l+1].attr.slice(line[l].text.length-end.length,end.length).toSource());
+			/* Add an attribute for the space */
+			line[l+1].attr.splice(end.length+1,0,curattr);
+			/* Trim whitespace from end */
+			line[l+1].text=line[l+1].text.replace(/\s+$/,'');
+			if(!ret)
+				ret=end.length+1;
+			return(start);
+		});
+		if(nline != line[l].text) {
+			line[l].text=nline;
+			redraw_line(l);
+		}
+		l++;
+	}
+	redraw_line(l);
+	return(ret);
+}
+
 /* ToDo: Optimize movement... */
 function edit()
 {
@@ -193,14 +234,14 @@ function edit()
 				if(xpos>1) {
 					line[ypos-edit_top+topline].text=line[ypos-edit_top+topline].text.substr(0,xpos-2)
 							+line[ypos-edit_top+topline].text.substr(xpos-1);
-					line[ypos-edit_top+topline].attr.splice(ypos,1);
+					line[ypos-edit_top+topline].attr.splice(xpos-2,1);
 					xpos--;
 				}
 				else {
 					/* ToDo: Backspace onto the previous row and re-wrap... */
 					console.beep();
 				}
-				line[ypos-edit_top+topline].draw(ypos);
+				redraw_line(ypos-edit_top+topline);
 				break;
 			case '\x09':	/* TAB... ToDo expand to spaces */
 				break;
@@ -213,7 +254,7 @@ function edit()
 			case '\x0d':	/* CR */
 				/* ToDo: Break current line at current pos... */
 				if(insert)
-					add_new_line_below();
+					add_new_line_below(ypos-edit_top+topline);
 				next_line();
 				xpos=1;
 				console.gotoxy(xpos,ypos);
@@ -257,15 +298,15 @@ function edit()
 			case '\x7f':	/* DELETE */
 				line[ypos-edit_top+topline].text=line[ypos-edit_top+topline].text.substr(0,xpos-1)
 						+line[ypos-edit_top+topline].text.substr(xpos);
-				line[ypos-edit_top+topline].attr.splice(xpos,1);
-				line[ypos-edit_top+topline].draw(ypos);
+				line[ypos-edit_top+topline].attr.splice(xpos-1,1);
+				redraw_line(ypos-edit_top+topline);
 				break;
 			default:		/* Insert the char */
 				if(insert) {
 					line[ypos-edit_top+topline].text=line[ypos-edit_top+topline].text.substr(0,xpos-1)
 							+key
 							+line[ypos-edit_top+topline].text.substr(xpos-1);
-					line[ypos-edit_top+topline].attr.splice(ypos,0,curattr);
+					line[ypos-edit_top+topline].attr.splice(xpos-1,0,curattr);
 					if(line[ypos-edit_top+topline].text.length > 80) {
 						/* ToDo: Wrap */
 					}
@@ -274,18 +315,20 @@ function edit()
 					line[ypos-edit_top+topline].text=line[ypos-edit_top+topline].text.substr(0,xpos-1)
 							+key
 							+line[ypos-edit_top+topline].text.substr(xpos);
-					line[ypos-edit_top+topline].attr[xpos]=curattr;
+					line[ypos-edit_top+topline].attr[xpos-1]=curattr;
 				}
-				line[ypos-edit_top+topline].draw(ypos);
-				console.gotoxy(xpos,ypos);
 				xpos++;
 				if(xpos>80) {
+					/* Wrap */
 					xpos=1;
-					if(insert)
-						add_new_line_below();
+					xpos=wrap_line(ypos-edit_top+topline);
 					next_line();
-					break;
 				}
+				else {
+					if(line[ypos-edit_top+topline].text.length > 79)
+						wrap_line(ypos-edit_top+topline);
+				}
+				redraw_line(ypos-edit_top+topline);
 				console.gotoxy(xpos,ypos);
 		}
 	}
