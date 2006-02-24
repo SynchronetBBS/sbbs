2f5a9a7c3b87c791a638840dcec2f3f63e1fa07d
deuce <>
2006-02-24 20:03:02 +0000

    Beginnings of a full-screen editor written in JS. *** HORRIBLY BROKEN *** This is being added to CVS because pressing enter in the editor "reliably" crashes Synchronet.
    Yes, this script WILL crashh Synchronet.
    YOU HAVE BEEN WARNED!

---
+++ exec/fseditor.js
@@ -0,0 +1,269 @@
+var xpos=1;
+var ypos=1;
+var topline=0;								/* Index into line[] of line at edit_top */
+var line=new Array();
+var edit_top=1;								/* First line of edit window */
+var edit_bottom=console.screen_rows;		/* Last line of edit window */
+var lines_on_screen=edit_bottom-edit_top+1;	/* Height of edit window */
+var curattr=7;								/* Current attribute */
+var insert=true;							/* Insert mode */
+line[0]=new Line(curattr);
+
+function Line(attr)
+{
+	var i;
+	if(attr==undefined)
+		attr=7;
+
+	/* The actual text of this line */
+	this.text='';
+	/* The attributes of this line */
+	this.attr=new Array();
+	for(i=1; i<80; i++)
+		this.attr[i]=attr;
+	this.canwrap=true;
+	this.draw=Line_draw;
+}
+
+/* Draws the line at given ypos */
+/* ToDo: Optimize with cleartoeol() */
+function Line_draw(ypos)
+{
+	var i;
+
+	console.gotoxy(1,ypos);
+	for(i=1; i<=this.text.length; i++) {
+		console.attributes=this.attr[i];
+		console.write(this.text.substr(i-1,1));
+	}
+	if(i<80) {
+		console.attributes=7;
+		console.cleartoeol();
+	}
+}
+
+/*
+ * Scrolls edit window up count lines.  Allows negative scrolling
+ */
+function scroll(count)
+{
+	var i;
+
+	topline+=count;
+	if(topline+lines_on_screen>=line.length)
+		topline=line.length-lines_on_screen;
+	if(topline<0)
+		topline=0;
+	for(i=0; i<lines_on_screen; i++) {
+		console.gotoxy(1,edit_top+i);
+		if(line[i+topline]!=undefined)
+			line[i+topline].draw();
+		else {
+			console.attributes=7;
+			console.cleartoeol();
+		}
+	}
+}
+
+/*
+ * Adds a new line below the current one.
+ */
+function add_new_line_below()
+{
+	line.splice(topline+ypos-edit_top+1,0,new Line);
+	/* Scroll lines below down */
+	for(i=ypos-edit_top+1; i<lines_on_screen; i++) {
+		console.gotoxy(1,edit_top+i);
+		if(line[i]!=undefined)
+			line[i+topline].draw();
+		else {
+			console.attributes=7;
+			console.cleartoeol();
+		}
+	}
+}
+
+/*
+ * Advances to the next line creating a new line if necessary.
+ */
+function next_line()
+{
+	if(topline+ypos-edit_top>=line.length)
+		line[line.length]=new Line(curattr);
+	ypos++;
+	if(ypos>edit_top+lines_on_screen-1) {
+		scroll(1);		/* Scroll up one line */
+		ypos--;
+	}
+	if(xpos>line[ypos-edit_top+topline].text.length+1)
+		xpos=line[ypos-edit_top+topline].text.length+1;
+}
+
+/*
+ * Moves to the previous line if it's possible to do so.
+ * Will not create another line.
+ */
+function try_prev_line()
+{
+	/* Yes, it's possible */
+	if(ypos-edit_top+topline>0) {
+		ypos--;
+		if(ypos<edit_top) {
+			scroll(-1);		/* Scroll down one line */
+			ypos++;
+		}
+		if(xpos>line[ypos-edit_top+topline].text.length+1)
+			xpos=line[ypos-edit_top+topline].text.length+1;
+	}
+	else
+		console.beep();
+}
+
+/*
+ * Advances to the next line if it's possible to do so.
+ * Will not create another line.
+ */
+function try_next_line()
+{
+	/* Yes, it's possible */
+	if(topline+ypos-edit_top<line.length-1) {
+		ypos++;
+		if(ypos>edit_top+lines_on_screen-1)
+			scroll(1);		/* Scroll up one line */
+		if(xpos>line[ypos-edit_top+topline].text.length+1)
+			xpos=line[ypos-edit_top+topline].text.length+1;
+	}
+	else
+		console.beep();
+}
+
+/* ToDo: Optimize movement... */
+function edit()
+{
+	var key;
+
+	console.gotoxy(xpos,ypos);
+	while(1) {
+		key=console.inkey(1000);
+		if(key=='')
+			continue;
+		switch(key) {
+			/* We're getting all the CTRL keys here... */
+			case '\x00':
+			case '\x01':
+				break;
+			case '\x02':	/* KEY_HOME */
+				xpos=1;
+				console.gotoxy(xpos,ypos);
+				break;
+			case '\x03':
+			case '\x04':
+				break;
+			case '\x05':	/* KEY_END */
+				xpos=line[ypos-edit_top+topline].text.length+1;
+				if(xpos>80)
+					xpos=80;
+				console.gotoxy(xpos,ypos);
+				break;
+			case '\x06':	/* KEY_RIGHT */
+				xpos++;
+				if(xpos>line[ypos-edit_top+topline].text.length+1) {
+					xpos--;
+					console.beep();
+				}
+				if(xpos>80) {
+					xpos=1;
+					try_next_line();
+				}
+				console.gotoxy(xpos,ypos);
+				break;
+			case '\x07':
+				break;
+			case '\x08':	/* Backspace */
+				/* ToDo: Backspace onto the previous row and re-wrap... */
+				if(xpos==0) {
+				}
+			case '\x09':	/* TAB... ToDo expand to spaces */
+				break;
+			case '\x0a':	/* KEY_DOWN */
+				try_next_line();
+				console.gotoxy(xpos,ypos);
+				break;
+			case '\x0b':
+			case '\x0c':
+			case '\x0d':	/* CR */
+				/* ToDo: Break current line at current pos... */
+				if(insert)
+					add_new_line_below();
+				next_line();
+				xpos=1;
+				console.gotoxy(xpos,ypos);
+				break;
+			case '\x0e':
+			case '\x0f':
+			case '\x10':
+			case '\x11':	/* CTRL-Q */
+				return;
+			case '\x12':
+			case '\x13':
+			case '\x14':
+			case '\x15':
+			case '\x16':
+			case '\x17':
+			case '\x18':
+			case '\x19':
+			case '\x1a':
+			case '\x1b':
+			case '\x1c':
+				break;
+			case '\x1d':	/* KEY_LEFT */
+				xpos--;
+				if(xpos<=0) {
+					console.beep();
+					xpos=1;
+				}
+				console.gotoxy(xpos,ypos);
+				break;
+			case '\x1e':	/* KEY_UP */
+				try_prev_line();
+				console.gotoxy(xpos,ypos);
+				break;
+			case '\x1f':
+				break;
+			case '\xf7':	/* DELETE */
+				break;
+			default:		/* Insert the char */
+				if(insert) {
+					line[ypos-edit_top+topline].text=line[ypos-edit_top+topline].text.substr(0,xpos-1)
+							+key
+							+line[ypos-edit_top+topline].text.substr(xpos-1);
+					line[ypos-edit_top+topline].attr.splice(ypos,0,curattr);
+					if(line[ypos-edit_top+topline].text.length > 80) {
+						/* ToDo: Wrap */
+					}
+				}
+				else {
+					line[ypos-edit_top+topline].text=line[ypos-edit_top+topline].text.substr(0,xpos-1)
+							+key
+							+line[ypos-edit_top+topline].text.substr(xpos);
+				}
+				console.gotoxy(1,ypos);
+				line[ypos-edit_top+topline].draw(ypos);
+				console.gotoxy(xpos,ypos);
+				xpos++;
+				if(xpos>80) {
+					xpos=1;
+					if(insert)
+						add_new_line_below();
+					next_line();
+					break;
+				}
+				console.gotoxy(xpos,ypos);
+		}
+	}
+}
+
+var oldpass=console.ctrlkey_passthru;
+console.ctrlkey_passthru=1<<17;
+edit();
+console.ctrlkey_passthru=oldpass;
