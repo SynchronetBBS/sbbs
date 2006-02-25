74a5cb6be944a9ac979575fde5b42b22c620ebee
rswindell <>
2006-02-25 00:11:02 +0000

    Use BEEP() macro instead of xpbeep() function call - this is mapped accordingly in xpbeep.h.

--- src/conio/cterm.c
+++ src/conio/cterm.c
@@ -4,7 +4,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -181,7 +181,7 @@ void playnote(int notenum, int notelen, int dotted)
 	}
 	duration-=pauselen;
 	if(notenum < 72 && notenum >= 0)
-		xpbeep(((double)note_frequency[notenum])/1000,duration);
+		BEEP(((double)note_frequency[notenum])/1000,duration);
 	else
 		SLEEP(duration);
 	SLEEP(pauselen);
