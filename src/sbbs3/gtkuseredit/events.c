be19a965ad047860192c170e92ea70458f44592a
deuce <>
2006-03-12 01:45:54 +0000

    Statistics tab working.

--- src/sbbs3/gtkuseredit/events.c
+++ src/sbbs3/gtkuseredit/events.c
@@ -242,7 +242,176 @@ void load_user(GtkWidget *wiggy, gpointer data)
 		}
 
 	/* Statistics */
-	
+		/* First On */
+		w=glade_xml_get_widget(xml, "eFirstOn");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the first on widget\n");
+		else {
+			unixtodstr(&cfg, user.firston, str);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Last On */
+		w=glade_xml_get_widget(xml, "eLastOn");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the last on widget\n");
+		else {
+			unixtodstr(&cfg, user.laston, str);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* First On */
+		w=glade_xml_get_widget(xml, "eFirstOn");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the first on widget\n");
+		else {
+			unixtodstr(&cfg, user.firston, str);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Total Logons */
+		w=glade_xml_get_widget(xml, "eLogonsTotal");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the total logons widget\n");
+		else {
+			sprintf(str, "%hu", user.logons);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Logons today */
+		w=glade_xml_get_widget(xml, "eLogonsToday");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the logons today widget\n");
+		else {
+			sprintf(str, "%hu", user.ltoday);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Total Posts */
+		w=glade_xml_get_widget(xml, "eTotalPosts");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the total posts widget\n");
+		else {
+			sprintf(str, "%hu", user.posts);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Posts Today */
+		w=glade_xml_get_widget(xml, "ePostsToday");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the posts today widget\n");
+		else {
+			sprintf(str, "%hu", user.ptoday);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Total Uploads */
+		w=glade_xml_get_widget(xml, "eTotalUploads");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the total uploads widget\n");
+		else {
+			sprintf(str, "%hu", user.uls);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Upload Bytes */
+		w=glade_xml_get_widget(xml, "eUploadBytes");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the upload bytes widget\n");
+		else {
+			sprintf(str, "%u", user.ulb);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Total Time On */
+		w=glade_xml_get_widget(xml, "eTotalTimeOn");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the total time on widget\n");
+		else {
+			sprintf(str, "%hu", user.timeon);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Time On Today */
+		w=glade_xml_get_widget(xml, "eTimeOnToday");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the time on today widget\n");
+		else {
+			sprintf(str, "%hu", user.ttoday);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Time On Last Call */
+		w=glade_xml_get_widget(xml, "eTimeOnLastCall");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the last call time on widget\n");
+		else {
+			sprintf(str, "%hu", user.tlast);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Time On Extra */
+		w=glade_xml_get_widget(xml, "eTimeOnExtra");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the extra time on widget\n");
+		else {
+			sprintf(str, "%hu", user.textra);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Total Downloads */
+		w=glade_xml_get_widget(xml, "eDownloadsTotal");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the total downloads widget\n");
+		else {
+			sprintf(str, "%hu", user.dls);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Download Bytes */
+		w=glade_xml_get_widget(xml, "eDownloadsBytes");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the download bytes widget\n");
+		else {
+			sprintf(str, "%u", user.dlb);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Download Leeches */
+		w=glade_xml_get_widget(xml, "eDownloadsLeech");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the downloads leech widget\n");
+		else {
+			sprintf(str, "%hhu", user.leech);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Total Email */
+		w=glade_xml_get_widget(xml, "eEmailTotal");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the total email widget\n");
+		else {
+			sprintf(str, "%hu", user.emails);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Email Today */
+		w=glade_xml_get_widget(xml, "eEmailToday");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the email today widget\n");
+		else {
+			sprintf(str, "%hu", user.etoday);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
+
+		/* Email To Sysop */
+		w=glade_xml_get_widget(xml, "eEmailToSysop");
+		if(w==NULL)
+			fprintf(stderr,"Cannot get the email to sysop widget\n");
+		else {
+			sprintf(str, "%hu", user.fbacks);
+			gtk_entry_set_text(GTK_ENTRY(w),str);
+		}
 }
 
 int update_current_user(int new_user)
@@ -293,4 +462,10 @@ void next_user(GtkWidget *w, gpointer data)
 void last_user(GtkWidget *w, gpointer data)
 {
 	update_current_user(totalusers);
+}
+
+void show_about_box(GtkWidget *w, gpointer data)
+{
+	GladeXML *ab;
+	ab=glade_xml_new("gtkuseredit.glade", "AboutWindow", NULL);
 }
