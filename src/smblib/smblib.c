c2a9b290683f4f47d890a96f6e7ed3f08b816d0d
rswindell <>
2006-04-05 09:30:03 +0000

    Bug-fix in smb_tzutc() - subtract 60 minutes from US timezones with DST in effect.

--- src/smblib/smblib.c
+++ src/smblib/smblib.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -1727,8 +1727,11 @@ int SMBCALL smb_tzutc(short zone)
 		return(zone);
 
 	tz=zone&0xfff;
-	if(zone&(WESTERN_ZONE|US_ZONE))	/* West of UTC? */
+	if(zone&(WESTERN_ZONE|US_ZONE)) {	/* West of UTC? */
+		if(zone&DAYLIGHT)
+			tz-=60;
 		return(-tz);
+	}
 	return(tz);
 }
 
