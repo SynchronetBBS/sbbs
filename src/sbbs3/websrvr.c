3dc291a1478a95c9fd5051026698b0e96918d07e
deuce <>
2006-04-11 03:30:29 +0000

    Support FreeBSDs accf_http(9) kerm option/module designed specifically for HTTP server optimizations.

--- src/sbbs3/websrvr.c
+++ src/sbbs3/websrvr.c
@@ -4387,6 +4387,10 @@ void DLLCALL web_server(void* arg)
 #ifdef ONE_JS_RUNTIME
 	JSRuntime*      js_runtime;
 #endif
+#ifdef SO_ACCEPTFILTER
+	struct accept_filter_arg afa;
+#endif
+
 	startup=(web_startup_t*)arg;
 
 	web_ver();	/* get CVS revision */
@@ -4552,6 +4556,12 @@ void DLLCALL web_server(void* arg)
  *		if(setsockopt(server_socket, IPPROTO_TCP, TCP_NOPUSH, &i, sizeof(i)))
  *			lprintf("Cannot set TCP_NOPUSH socket option");
  */
+
+#ifdef SO_ACCEPTFILTER
+		memset(&afa, 0, sizeof(afa));
+		strcpy(afa.af_name, "httpready");
+		setsockopt(server_socket, SOL_SOCKET, SO_ACCEPTFILTER, &afa, sizeof(afa));
+#endif
 
 		lprintf(LOG_INFO,"%04d Web Server socket opened",server_socket);
 
