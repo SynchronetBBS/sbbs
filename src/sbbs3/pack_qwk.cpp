a14d59eeff5d8b063bf75984340ecb6ff661a511
rswindell <>
2006-04-14 00:54:43 +0000

    Include QWK text files (e.g. HELLO, GOODBYE, BBSNEWS, BLT-*) when prepacking (or packing for FTP download).

--- src/sbbs3/pack_qwk.cpp
+++ src/sbbs3/pack_qwk.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -586,7 +586,7 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 		return(false); 
 	}
 
-	if(!prepack && !(useron.rest&FLAG('Q'))) {      /* Don't include in network */
+	if(!(useron.rest&FLAG('Q'))) {					/* Don't include in network */
 		/***********************/					/* packets */
 		/* Copy QWK Text files */
 		/***********************/
