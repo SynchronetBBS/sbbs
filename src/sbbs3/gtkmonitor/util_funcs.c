1f24476ed41778cf125d7999b878a35d2dc79185
deuce <>
2006-05-09 22:28:43 +0000

    Add a view_stdout() function which displays the stdout output from the passed command to the user... curently hardcoded to use xmess -file -

--- src/sbbs3/gtkmonitor/util_funcs.c
+++ src/sbbs3/gtkmonitor/util_funcs.c
@@ -8,6 +8,24 @@ void run_cmdline(void *cmdline)
 }
 
 /* ToDo: This will need to read the command-line from a config file */
+void view_stdout(char *path, char *filename)
+{
+	static char	cmdline[MAX_PATH*2];
+	char	p[MAX_PATH+1];
+
+	if(path != NULL) {
+		strcpy(p, path);
+		backslash(p);
+	}
+	else
+		p[0]=0;
+
+	strcat(p, filename);
+	sprintf(cmdline, "%s | xmessage -file", p);
+	_beginthread(run_cmdline, 0, cmdline);
+}
+
+/* ToDo: This will need to read the command-line from a config file */
 void view_text_file(char *path, char *filename)
 {
 	static char	cmdline[MAX_PATH*2];
