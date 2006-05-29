42ef3b2f0777dc160c63e18a6b6d3e4222509f5c
deuce <>
2006-05-29 14:28:50 +0000

    Use the new listSemTryWaitBlock() semantics.

--- src/conio/cterm.c
+++ src/conio/cterm.c
@@ -190,7 +190,7 @@ void playnote_thread(void *args)
 	playnote_thread_running=TRUE;
 	while(1) {
 		if(device_open) {
-			if(listSemTryWaitBlock(&notes,5000)) {
+			if(!listSemTryWaitBlock(&notes,5000)) {
 				xptone_close();
 				device_open=FALSE;
 				listSemWait(&notes);
