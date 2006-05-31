d4d775c1bb2c4cebf9681a3757ea9035db1dcf48
rswindell <>
2006-05-31 03:07:54 +0000

    Include type for wsa_error global var (required for MSVC 2005).

--- src/xpdev/sockwrap.h
+++ src/xpdev/sockwrap.h
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -133,7 +133,7 @@ typedef struct {
 
 #define socklen_t		int
 
-static  wsa_error;
+static  int wsa_error;
 #define ERROR_VALUE		((wsa_error=WSAGetLastError())>0 ? wsa_error-WSABASEERR : wsa_error)
 #define sendsocket(s,b,l)	send(s,b,l,0)
 
