9fad417f4f158f2d63e1f8e8f1ad0ba64f57d1d0
deuce <>
2006-08-09 18:19:47 +0000

    Fix bug in last commit.

--- exec/xjs_handler.js
+++ exec/xjs_handler.js
@@ -45,12 +45,13 @@ if(!file_exists(ssjs_filename)) {
 					}
 					else {
 						str=str.replace(/^(.*?)<\?(xjs)?\s+/,
-						function (str, p1, p2, offset, s) {
-							script += "write("+escape_quotes(p1)+");\r\n";
-							in_xjs=true;
-							return '';
-						}
-					);
+							function (str, p1, p2, offset, s) {
+								script += "write("+escape_quotes(p1)+");\r\n";
+								in_xjs=true;
+								return '';
+							}
+						);
+					}
 				}
 			}
 			else {
