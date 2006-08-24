4b007664f86ffc7e3f9bf3f1197a1568dfba9db7
deuce <>
2006-08-24 19:12:31 +0000

    Use console.line_counter to defeaut pause instead.

--- exec/fseditor.js
+++ exec/fseditor.js
@@ -1367,7 +1367,7 @@ function edit(quote_first)
 	redraw_screen();
 	if(quote_first) {
 		if(quote_mode()) {
-			console.gotoxy(1,1);
+			console.line_counter=0;
 			return;
 		}
 	}
@@ -1648,7 +1648,7 @@ function edit(quote_first)
 					xpos=line[ypos].text.length;
 				break;
 			case '\x11':	/* CTRL-Q (XOff) (Quick Abort in SyncEdit) */
-				console.gotoxy(1,1);
+				console.line_counter=0;
 				return;
 			case '\x12':	/* CTRL-R (Quick Redraw in SyncEdit) */
 				redraw_screen();
@@ -1667,7 +1667,7 @@ function edit(quote_first)
 			case '\x15':	/* CTRL-U (Quick Quote in SyncEdit) */
 				if(quote_line.length>0) {
 					if(quote_mode()) {
-						console.gotoxy(1,1);
+						console.line_counter=0;
 						return;
 					}
 				}
@@ -1752,7 +1752,7 @@ function edit(quote_first)
 				var s=make_strings(true,true);
 				f.write(s[0]);
 				f.close();
-				console.gotoxy(1,1);
+				console.line_counter=0;
 				return;
 			case '\x1b':	/* ESC (This should parse extra ANSI sequences) */
 				break;
@@ -1772,7 +1772,7 @@ function edit(quote_first)
 				try_prev_line();
 				break;
 			case '\x1f':	/* CTRL-_ Safe quick-abort*/
-				console.gotoxy(1,1);
+				console.line_counter=0;
 				return;
 			case '\x7f':	/* DELETE */
 				last_xpos=-1;
