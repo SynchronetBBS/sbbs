9a8085eb5616d37e52e96f4e5acb6d5b9cf18839
rswindell <>
2006-10-25 22:08:46 +0000

    Created new directory properties: is_offline, is_upload, and is_sysop, to make it easier to determine if a file directory is one of these special kinds of directories.

--- src/sbbs3/js_file_area.c
+++ src/sbbs3/js_file_area.c
@@ -82,6 +82,9 @@ static char* dir_prop_desc[] = {
 	,"user has sufficient access to download files"
 	,"user is exempt from download credit costs"
 	,"user has operator access to this directory"
+	,"directory is for offline storage <i>(introduced in v3.14)</i>"
+	,"directory is for uploads only <i>(introduced in v3.14)</i>"
+	,"directory is for uploads to sysop only <i>(introduced in v3.14)</i>"
 	,NULL
 };
 #endif
@@ -428,6 +431,18 @@ JSObject* DLLCALL js_CreateFileAreaObject(JSContext* cx, JSObject* parent, scfg_
 			else
 				val=JSVAL_FALSE;
 			if(!JS_SetProperty(cx, dirobj, "is_operator", &val))
+				return(NULL);
+
+			val=BOOLEAN_TO_JSVAL(d==cfg->lib[l]->offline_dir);
+			if(!JS_SetProperty(cx, dirobj, "is_offline", &val))
+				return(NULL);
+
+			val=BOOLEAN_TO_JSVAL(d==cfg->upload_dir);
+			if(!JS_SetProperty(cx, dirobj, "is_upload", &val))
+				return(NULL);
+
+			val=BOOLEAN_TO_JSVAL(d==cfg->sysop_dir);
+			if(!JS_SetProperty(cx, dirobj, "is_sysop", &val))
 				return(NULL);
 
 #ifdef BUILD_JSDOCS
