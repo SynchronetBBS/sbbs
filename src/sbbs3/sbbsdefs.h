3bf46fad415d3befcdd20af50885a54b1d732d54
rswindell <>
2007-01-03 16:59:16 +0000

    Increase rev to 'b'.

--- src/sbbs3/sbbsdefs.h
+++ src/sbbs3/sbbsdefs.h
@@ -51,7 +51,7 @@
 /*************/
 
 #define VERSION 	"3.14"  /* Version: Major.minor  */
-#define REVISION	'a'     /* Revision: lowercase letter */
+#define REVISION	'b'     /* Revision: lowercase letter */
 #define VERSION_NUM	(31400	 + (tolower(REVISION)-'a'))
 #define VERSION_HEX	(0x31400 + (tolower(REVISION)-'a'))
 
