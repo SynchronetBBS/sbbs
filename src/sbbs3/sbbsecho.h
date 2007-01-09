369b0c0c403f026d5b16a65ccccd1cb4eaa56413
rswindell <>
2007-01-09 17:35:55 +0000

    Increment version to 2.12.

--- src/sbbs3/sbbsecho.h
+++ src/sbbs3/sbbsecho.h
@@ -37,7 +37,7 @@
 
 /* Portions written by Allen Christiansen 1994-1996 						*/
 
-#define SBBSECHO_VER "2.11"
+#define SBBSECHO_VER "2.12"
 
 #define IMPORT_NETMAIL  (1L<<0)
 #define IMPORT_PACKETS	(1L<<1)
