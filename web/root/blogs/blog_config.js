063c8b9f9277a54f8c929fab2cfb95e4a732fa41
deuce <>
2007-02-19 06:26:08 +0000

    Start of a blog thinger... here's how it works: 1) Any message which is NOT a reply to another message is a blog post. 2) All replies to that message (and replies to them etc.) are "comments" 3) Blog posts can be filtered by poster, year, month, and day by the URL 	http://mybbs.synchro.net/blogs/Deuce 		Would be my blog entries. 	http://mybbs.synchro.net/blogs/Deuce/2007 		Would be my blog entries for 2007 	http://mybbs.synchro.net/blogs/Deuce/2007/02/18 		My blog entries for Feb. 18th, 2007
    I still haven't figured out the best way to display comment threads...
    Any succestions are welcome.

---
+++ web/root/blogs/blog_config.js
@@ -0,0 +1,53 @@
+// User editable settings...
+
+// Internal code of the message base
+var msg_code="GENERAL";
+
+// Number of posts to show per page.
+var max_posts=16;
+
+// Minimum number of characters in an exerpt
+var min_chars=400;
+
+//
+// Don't change stuff down here.
+//
+
+// User name of the blogger
+var parameters=http_request.path_info.substr(1).split("/");
+var poster=parameters[0];
+var year=parameters[1];
+var month=parameters[2];
+var day=parameters[3];
+var msgid=parameters[4];
+var subject=parameters[5];
+if(poster.indexOf("/")>=0) {
+	poster=poster.substr(0,poster.indexOf("/"));
+}
+
+var pnum=system.matchuser(poster);
+if(pnum==0) {
+	write("<html><head><title>Error</title></head><body>Error getting UserID for "+poster+"!</body></html>");
+	exit(1);
+}
+
+var msgbase = new MsgBase(msg_code);
+if(!msgbase.open()) {
+	write("<html><head><title>Error</title></head><body>Error opening "+msg_code+"!</body></html>");
+	exit(1);
+}
+
+if(msgid != undefined && msgid != "") {
+	if(subject==undefined || subject=="") {
+		var hdr=msgbase.get_msg_header(parseInt(msgid));
+		http_reply.status="301 Moved Permanently";
+		http_reply.header["Location"]="http://"+http_request.host+http_request.virtual_path+poster+"/"+format("%04d",year)+"/"+format("%02d",month)+"/"+format("%02d",day)+"/"+msgid+"/"+clean_subject(hdr.subject);
+		exit(0);
+	}
+}
+
+function clean_subject(sub)
+{
+	sub=sub.replace(/[^a-zA-Z0-9]/g,"_");
+	return(sub);
+}
