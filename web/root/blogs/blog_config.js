0074a46912cb08448d359ce63764f07062a66ed5
deuce <>
2007-02-22 22:58:40 +0000

    More changes... the first item should now be the internal message base code Threaded view uses JavaScript and visibility to make dynamic collapsing threads.
    http://nix.synchro.net/blogs/DOVE-GEN/ for an example "blog" highlighting
    the thread linking issues on my system...
    
    For a good thread view, see:
    http://nix.synchro.net/blogs/DOVE-DEB/Angus%20McLeod/2007/01/10/6872/Quiet___

--- web/root/blogs/blog_config.js
+++ web/root/blogs/blog_config.js
@@ -1,13 +1,13 @@
 // User editable settings...
 
-// Internal code of the message base
-var msg_code="GENERAL";
-
 // Number of posts to show per page.
 var max_posts=16;
 
 // Minimum number of characters in an exerpt
 var min_chars=400;
+
+// Number of pixels per level to indent comments
+var indent_pixels=20;
 
 //
 // Don't change stuff down here.
@@ -15,15 +15,13 @@ var min_chars=400;
 
 // User name of the blogger
 var parameters=http_request.path_info.substr(1).split("/");
-var poster=parameters[0];
-var year=parseInt(parameters[1],10);
-var month=parseInt(parameters[2],10);
-var day=parseInt(parameters[3],10);
-var msgid=parseInt(parameters[4],10);
-var subject=parameters[5];
-if(poster.indexOf("/")>=0) {
-	poster=poster.substr(0,poster.indexOf("/"));
-}
+var msg_code=parameters[0];
+var poster=parameters[1];
+var year=parseInt(parameters[2],10);
+var month=parseInt(parameters[3],10);
+var day=parseInt(parameters[4],10);
+var msgid=parseInt(parameters[5],10);
+var subject=parameters[6];
 
 //var pnum=system.matchuser(poster);
 //if(pnum==0) {
@@ -41,7 +39,7 @@ if(!isNaN(msgid)) {
 	if(subject==undefined || subject=="") {
 		var hdr=msgbase.get_msg_header(parseInt(msgid));
 		http_reply.status="301 Moved Permanently";
-		http_reply.header["Location"]="http://"+http_request.host+http_request.virtual_path+poster+"/"+format("%04d",year)+"/"+format("%02d",month)+"/"+format("%02d",day)+"/"+msgid+"/"+clean_subject(hdr.subject);
+		http_reply.header["Location"]="http://"+http_request.host+http_request.virtual_path+msg_code+"/"+hdr.from+"/"+format("%04d",year)+"/"+format("%02d",month)+"/"+format("%02d",day)+"/"+msgid+"/"+clean_subject(hdr.subject);
 		exit(0);
 	}
 }
