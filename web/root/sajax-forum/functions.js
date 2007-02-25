7bcfc241499409e1ba7799d3b0fdf88ef472c812
deuce <>
2007-02-25 13:38:21 +0000

    Lots of additions... 1)  Indicates if a message is unread according to the new scan pointers 2)  Indicates if any reply in the chain is unread 3)  Clean up the HTML a little 4)  Document the class hierarchy 5)  Add a login box (no, not a pop-up) 6)  Change the cursor to the "processing" cursor while loading stuff 7)  Make the groups a separate script so that they can be reloaded if the     user changes 8)  Move the client-side javascript into a separate .xjs file 9)  Some style sheet cleanups 10) Ensure that some user is used... either "Guest" or the new login
    Things I should be able to do fairly easily...
    
    1) Have the new indication update dynamically so you can tell what you've
       already read
    2) Update the new scan pointer when the user leaves the page
    3) Add a reply button
    4) Add moderation capabilities for the moderator
    5) Add deletion support and a show delete messages option for operators
    6) Also allow users to delete their own posts if allowed
    7) Hook in the users email
    
    What I can't/won't do
    
    1) Work hard enough on the CSS to make it pretty
    2) Make it look correct in IE (see #1)
    3) Replace the current default system with it
    
    #1 and #2 desparately need a web geek.

--- web/root/sajax-forum/functions.js
+++ web/root/sajax-forum/functions.js
@@ -1,3 +1,10 @@
+if(user.number==0)
+	login("Guest");
+if(user.number==0) {
+	write('<html><head><title>No Guest Access</title></head><body>Sorry, this BBS does not offer Guest access.</body></html>');
+	exit(1);
+}
+
 function clean_id(id)
 {
 	return(id.replace(/[^A-Za-z0-9]/g,'_'));
