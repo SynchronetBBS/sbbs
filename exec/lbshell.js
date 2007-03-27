bd1dc63a3e7f107700161b89e946a7e0c5cebacc
rswindell <>
2007-03-27 22:41:27 +0000

    Fixed another typo.

--- exec/lbshell.js
+++ exec/lbshell.js
@@ -1734,7 +1734,6 @@ function show_messagemenu()
  								if(msg_area.grp_list[bbs.curgrp].sub_list[i].scan_cfg&SCAN_CFG_NEW
 									&& !bbs.scan_posts(msg_area.grp_list[bbs.curgrp].sub_list[i].number, SCAN_NEW))
 									break;
- 							}
 							draw_main(true);
 							messagemenu.draw();
 							break;
