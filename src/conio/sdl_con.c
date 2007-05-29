8073b83b03cd398c5453c6433d1da7a4262aac3b
deuce <>
2007-05-29 06:09:56 +0000

    And, of course, the typo in last commit.

--- src/conio/sdl_con.c
+++ src/conio/sdl_con.c
@@ -1621,7 +1621,7 @@ unsigned int sdl_get_char_code(unsigned int keysym, unsigned int mod, unsigned i
 				/* "Extended" syms are always right */
 				if(!unicode)
 					return(expect);
-				if(sdl_keybal[i].key > 255)
+				if(sdl_keyval[i].key > 255)
 					return(expect);
 				/*
 				 * If we don't know that this key can
