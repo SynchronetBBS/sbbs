416d224f78b108221e424e0225c943fe8ea5eabc
deuce <>
2007-05-30 04:33:37 +0000

    Typo.

--- src/conio/ciolib.c
+++ src/conio/ciolib.c
@@ -356,7 +356,7 @@ CIOLIBEXPORT int CIOLIBCALL initciolib(int mode)
 	cio_textinfo.winright=cio_textinfo.screenwidth;
 	cio_textinfo.winbottom=cio_textinfo.screenheight;
 	/* Default C64 is Lt Blue on Black (As per CGTerm) */
-	switch(cio_textmode.currmode) {
+	switch(cio_textinfo.currmode) {
 		case C64_40X25:
 		case C128_40X25:
 		case C128_80X25:
@@ -673,7 +673,7 @@ CIOLIBEXPORT void CIOLIBCALL ciolib_textmode(int mode)
 	cio_textinfo.wintop=1;
 	cio_textinfo.winright=cio_textinfo.screenwidth;
 	cio_textinfo.winbottom=cio_textinfo.screenheight;
-	switch(cio_textmode.currmode) {
+	switch(cio_textinfo.currmode) {
 		case C64_40X25:
 		case C128_40X25:
 		case C128_80X25:
