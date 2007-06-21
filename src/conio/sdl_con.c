7e8ba8083454d2fe5120a0dc26e69d631026e6e3
deuce <>
2007-06-21 03:32:38 +0000

    Fix warnings

--- src/conio/sdl_con.c
+++ src/conio/sdl_con.c
@@ -537,6 +537,7 @@ int sdl_init(int mode)
 	sdl.mutexP(sdl_vstatlock);
 	vstat.vmem=NULL;
 	vstat.scaling=1;
+	vstat.cols=80;		/* Initialize this so sdl_init_mode() is happy */
 	sdl.mutexV(sdl_vstatlock);
 
 	sdl.mutexP(sdl_updlock);
@@ -1819,7 +1820,6 @@ int sdl_video_event_thread(void *data)
 								sdl_ufunc_retval=0;
 								sdl.SemPost(sdl_ufunc_ret);
 								return(0);
-								break;
 							case SDL_USEREVENT_LOADFONT:
 								sdl_ufunc_retval=sdl_load_font((char *)ev.user.data1);
 								FREE_AND_NULL(ev.user.data1);
@@ -2100,6 +2100,7 @@ int sdl_video_event_thread(void *data)
 			}
 		}
 	}
+	return(0);
 }
 
 int sdl_initciolib(int mode)
