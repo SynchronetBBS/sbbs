6b20973ccc59fd7627cd3867ed32ef865f34083a
rswindell <>
2007-06-30 03:57:11 +0000

    dpclean is no longer needed, use 'dpoker -clean' for the clean-up command-line instead (see dpoker.doc for details).

--- xtrn/dpoker/objects.mk
+++ xtrn/dpoker/objects.mk
@@ -2,8 +2,4 @@ OBJS       = $(MTOBJODIR)$(DIRSEP)dpoker$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)xsdk$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)xsdkvars$(OFILE)
 
-DPCLEAN_OBJS = $(MTOBJODIR)$(DIRSEP)dpclean$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)xsdk$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)xsdkvars$(OFILE)
-
 DPCONFIG_OBJS = $(MTOBJODIR)$(DIRSEP)dpconfig$(OFILE)
