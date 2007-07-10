2a95d4cfcb4f8ac6f29f271eb5c3ca6e6c51330c
rswindell <>
2007-07-10 22:26:45 +0000

    Bug-fix to semfile_signal() - wouldn't update timestamp if file already existed (same bug-fix as for ftouch() back in May) - this function is currently unused.

--- src/xpdev/semfile.c
+++ src/xpdev/semfile.c
@@ -6,7 +6,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -136,7 +136,7 @@ BOOL DLLCALL semfile_signal(const char* fname, const char* text)
 		return(FALSE);
 	if(text!=NULL)
 		write(file,text,strlen(text));
-	/* use utime() for force the time-stamp to that of the local system? */
 	close(file);
-	return(TRUE);
+
+	return utime(fname, /* use current date/time: */NULL)==0;
 }
