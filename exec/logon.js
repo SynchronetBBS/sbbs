5b8c2e7a82dfeefb072c41b1be19c4a96b1d200f
deuce <>
2007-07-28 12:51:26 +0000

    Add support for HTML terminals.

--- exec/logon.js
+++ exec/logon.js
@@ -7,6 +7,8 @@
 // @format.tab-size 4, @format.use-tabs true
 
 load("sbbsdefs.js");
+load("text.js");
+load("asc2htmlterm.js");
 
 //Disable spinning cursor at pause prompts
 //bbs.node_settings|=NM_NOPAUSESPIN	
@@ -82,32 +84,105 @@ var random_list = directory(system.text_dir + "menu/random*.*");
 if(random_list.length)
 	bbs.menu(file_getname(random_list[random(random_list.length)]).slice(0,-4));
 
+// If using the HTML shell and has HTML terminal,
+// replace YesNo text.
+if(user.command_shell.search(/HTML/i)!=-1 && user.settings&USER_HTML) {
+	bbs.replace_text(bbs.text(YesNoQuestion),"@EXEC:html_yesno@");
+	bbs.replace_text(bbs.text(NoYesQuestion),"@EXEC:html_yesno@");
+}
+
 console.clear();
 bbs.user_event(EVENT_LOGON);
 
-// Last few callers
-console.aborted=false;
-console.clear();
-logonlst=system.data_dir + "logon.lst"
-if(file_size(logonlst)<1)
-	printf("\1n\1g\1hYou are the first caller of the day!\r\n");
-else {
-	printf("\1n\1g\1hLast few callers:\1n\r\n");
-	console.printtail(logonlst,4,P_NOATCODES);	// args: filename, lines, mode
-}
-console.crlf();
+if(user.settings&USER_HTML) {
+	var buf="\2\2<html><head><title>Welcome status screen</title></head><body bgcolor=\"black\" text=\"#a8a8a8\">";
 
-// Auto-message
-auto_msg=system.data_dir + "msgs/auto.msg"
-if(file_size(auto_msg)>0) {
-	console.printfile(auto_msg,P_NOATCODES);
+	// Last few callers
+	logonlst=system.data_dir + "logon.lst"
+	if(file_size(logonlst)<1)
+		buf += asc2htmlterm("\1n\1g\1hYou are the first caller of the day!\r\n",false,true).replace(/(?:&nbsp;)*<br>/g,'<br>');
+	else {
+		f=new File(logonlst);
+		if(f.open("rb",true,f.length)) {
+			var lastbuf=f.read(f.length);
+			f.close();
+			lastbuf = lastbuf.replace(/^.*((?:[\x00\x09\x0b-\xff]*[\n]){1,4})$/,'$1');
+			buf += asc2htmlterm("\1n\1g\1hLast few callers:\1n\r\n",false,true).replace(/(?:&nbsp;)*<br>/g,'<br>');
+			buf += asc2htmlterm(lastbuf, false, true).replace(/(?:&nbsp;)*<br>/g,'<br>');
+		}
+	}
+	buf += '&nbsp;<br>';
+
+	// Auto-message
+	auto_msg=system.data_dir + "msgs/auto.msg"
+	if(file_size(auto_msg)>0) {
+		f=new File(auto_msg);
+		if(f.open("rb",true,f.length)) {
+			buf += asc2htmlterm(f.read(f.length), false, true, P_NOATCODES).replace(/(?:&nbsp;)*<br>/g,'<br>');
+			f.close();
+		}
+
+		buf += '&nbsp;<br>';
+	}
+
+	if(!(system.settings&SYS_NOSYSINFO)) {
+		buf += asc2htmlterm(format(bbs.text(SiSysName),system.name)
+					+ format(bbs.text(LiUserNumberName),user.number,user.alias)
+					+ format(bbs.text(LiLogonsToday),user.stats.logonstoday
+		            		,user.limits.logons_per_day)
+					+ format(bbs.text(LiTimeonToday),user.stats.timeon_today
+		            		,user.limits.time_per_day+user.security.minutes)
+					+ format(bbs.text(LiMailWaiting),user.mail_waiting)
+				, false, true).replace(/(?:&nbsp;)*<br>/g,'<br>');
+			/*
+			 * Notes:
+			 * 1) We cannot access cfg.sys_char_ar
+			 * 2) logon.cpp and chat.cpp differ... chat.cpp adds useron.exempt&FLAG('C')
+			 */
+			/*
+    		strcpy(str,bbs.text[LiSysopIs]);
+    		if(bbs.startup_options&BBS_OPT_SYSOP_AVAILABLE
+            		|| (cfg.sys_chat_ar[0] && chk_ar(cfg.sys_chat_ar,&useron)))
+            		strcat(str,bbs.text[LiSysopAvailable]);
+    		else
+            		strcat(str,bbs.text[LiSysopNotAvailable]);
+    		format("%s\r\n\r\n",str);
+			*/
+	}
+
+	buf += "<br><a href=\" \">Click here to continue...</a></body>\n\2";
+	/* Disable autopause */
+	var os = bbs.sys_status;
+	bbs.sys_status |= SS_PAUSEOFF;
+	bbs.sys_status &= ~SS_PAUSEON;
+	console.write(buf);
+	bbs.sys_status=os;
+	console.getkey();
+}
+else {
+	// Last few callers
+	console.aborted=false;
+	console.clear();
+	logonlst=system.data_dir + "logon.lst"
+	if(file_size(logonlst)<1)
+		printf("\1n\1g\1hYou are the first caller of the day!\r\n");
+	else {
+		printf("\1n\1g\1hLast few callers:\1n\r\n");
+		console.printtail(logonlst,P_NOATCODES,4);
+	}
 	console.crlf();
+
+	// Auto-message
+	auto_msg=system.data_dir + "msgs/auto.msg"
+	if(file_size(auto_msg)>0) {
+		console.printfile(auto_msg,P_NOATCODES);
+		console.crlf();
+	}
 }
 
 // Automatically set shell to WIPSHELL
 if(user.settings&USER_WIP)
-	user.shell="WIPSHELL";
-
+	user.command_shell="WIPSHELL";
 
 if(user.security.level==99				/* Sysop logging on */
    && !system.matchuser("guest")		/* Guest account does not yet exist */
