976d1a9f491a4a9cbfcfcc02ab89cc21a9f34199
deuce <>
2007-07-28 12:50:40 +0000

    Allow mode bits to be specified in argv[]

--- exec/typeasc.js
+++ exec/typeasc.js
@@ -10,12 +10,36 @@ var f;
 
 var title='';
 var filename='';
+var mode=P_NONE;
 
 for(i in argv) {
-	if(this.f==undefined) {
-		f = new File(file_getcase(argv[i]));
-		filename=file_getcase(argv[i]);
-		filename=filename.replace(/^.*[\\\/]/,'');
+	if(filename=='') {
+		switch(argv) {
+			case '-noatcodes':
+				mode |= P_NOATCODES;
+				break;
+			case '-noabort':
+				mode |= P_NOABORT;
+				break;
+			case '-saveatr':
+				mode |= P_SAVEATR;
+				break;
+			case '-openclose':
+				mode |= P_OPENCLOSE;
+				break;
+			case '-nopause':
+				mode |= P_NOPAUSE;
+				break;
+			case '-nocrlf':
+				mode |= P_NOCRLF;
+				break;
+			default:
+				if(this.f==undefined) {
+					f = new File(file_getcase(argv[i]));
+					filename=file_getcase(argv[i]);
+					filename=filename.replace(/^.*[\\\/]/,'');
+				}
+		}
 	}
 	else {
 		if(title.length)
@@ -25,7 +49,7 @@ for(i in argv) {
 }
 
 if(this.f==undefined) {
-	print("usage: typeasc <filename> [HTML title]");
+	print("usage: typeasc [-noatcodes|-noabort|-saveatr|-openclose|-nopause|-nocrlf] <filename> [HTML title]");
 	exit(1);
 }
 
@@ -43,7 +67,7 @@ if(1 || (user.settings & USER_HTML)) {
 	f.close();
 
 	console.clear(7);
-	buf=asc2htmlterm(buf,true,false);
+	buf=asc2htmlterm(buf,true,false, mode);
 
 	/* Disable autopause */
 	var os = bbs.sys_status;
@@ -53,7 +77,8 @@ if(1 || (user.settings & USER_HTML)) {
 	console.write(buf);
 	bbs.sys_status=os;
 
-	console.getkey();
+	if(!(mode & P_NOPAUSE))
+		console.getkey();
 }
 else
-	console.printfile(f.name);
+	console.printfile(f.name, mode);
