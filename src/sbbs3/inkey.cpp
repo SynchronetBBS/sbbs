ef1c666ad792e7ed3d1ae5abbfcbf8856071d70f
rswindell <>
2007-07-31 19:24:30 +0000

    Bug-fix: added in rev 1.20 (May-2003) - inkey() wasn't resetting the inactivity timer reference value (sbbs_t::timeout) due to accidental argument/member variable name overload.

--- src/sbbs3/inkey.cpp
+++ src/sbbs3/inkey.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -71,7 +71,7 @@ char sbbs_t::inkey(long mode, unsigned long timeout)
 		&& (!(sys_status&SS_USERON) || useron.misc&NO_EXASCII))
 		ch&=0x7f; 
 
-	timeout=time(NULL);
+	this->timeout=time(NULL);
 
 	/* Is this a control key */
 	if(ch<' ') {
