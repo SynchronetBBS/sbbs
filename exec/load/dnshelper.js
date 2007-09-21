19a74f141ae6532e066e9ca73eb9641ed1e03ac3
cyan <>
2007-09-21 02:22:54 +0000

    * No need for the 3.12 workaround anymore

--- exec/load/dnshelper.js
+++ exec/load/dnshelper.js
@@ -1,7 +1,6 @@
 // $Id$
 
 var host;
-if((host=resolve_host(argv[0]))==null)
-	false;	// Work-around for bug in v3.12a (segfault when decoding "null")
-else
-	host;
+host=resolve_host(argv[0]);
+host;
+
