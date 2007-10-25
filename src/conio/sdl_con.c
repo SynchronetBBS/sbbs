666421191e4d26e2a8f285db8b43337cb0bd9ccc
deuce <>
2007-10-25 07:39:37 +0000

    Check both the linked version and the header version before determining the screen size.

--- src/conio/sdl_con.c
+++ src/conio/sdl_con.c
@@ -606,8 +606,11 @@ int sdl_init(int mode)
 	}
 #if (SDL_MAJOR_VERSION > 1) || (SDL_MINOR_VERSION > 2) || (SDL_PATCHLEVEL > 9)
 	if(yuv.enabled) {
-		yuv.screen_width=sdl.initial_videoinfo.current_w;
-		yuv.screen_height=sdl.initial_videoinfo.current_h;
+		SDL_version	linked=sdl.LinkedVersion();
+		if(linked->major > 1 || linked->minor > 2 || linked->patch > 9) {
+			yuv.screen_width=sdl.initial_videoinfo.current_w;
+			yuv.screen_height=sdl.initial_videoinfo.current_h;
+		}
 	}
 #endif
 	sdl_init_mode(3);
