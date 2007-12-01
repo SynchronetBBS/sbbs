a92abf72bf24befd2622e2903ec57dcd28e99f0f
rswindell <>
2007-12-01 18:29:03 +0000

    If the path to the sbbs DLLs directory is passed on the command-line, that is used as the source of the DLLs to create import libraries from. This should solve the problem of sbbsctrl.exe (in sbbs_dev.zip) being linked with debug DLLs, but release DLLs were included in the archive.

--- src/sbbs3/ctrl/makelibs.bat
+++ src/sbbs3/ctrl/makelibs.bat
@@ -4,6 +4,9 @@ if exist ..\bcc.win32.lib.release 	set dllsrc=..\bcc.win32.lib.release
 if exist ..\bcc.win32.lib.debug 	set dllsrc=..\bcc.win32.lib.debug
 if exist ..\msvc.win32.dll.release	set dllsrc=..\msvc.win32.dll.release
 if exist ..\msvc.win32.dll.debug	set dllsrc=..\msvc.win32.dll.debug
+if '%1'=='' goto implib
+set dllsrc=%1
+:implib
 echo Creating import libraries from %dllsrc%
 implib sbbs.lib 	%dllsrc%\sbbs.dll
 implib mailsrvr.lib 	%dllsrc%\mailsrvr.dll
