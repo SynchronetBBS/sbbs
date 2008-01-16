bae0a6cf1a21836dd07544f1b5e4286059eda60c
deuce <>
2008-01-16 04:16:57 +0000

    Also properly parent fido_addr_list

--- src/sbbs3/js_system.c
+++ src/sbbs3/js_system.c
@@ -1732,6 +1732,9 @@ static JSBool js_system_resolve(JSContext *cx, JSObject *obj, jsval id)
 		if((newobj=JS_NewArrayObject(cx, 0, NULL))==NULL)
 			return(JS_FALSE);
 
+		if(!JS_SetParent(cx, newobj, obj))
+			return(JS_FALSE);
+
 		if(!JS_DefineProperty(cx, obj, "fido_addr_list", OBJECT_TO_JSVAL(newobj)
 			, NULL, NULL, JSPROP_ENUMERATE))
 			return(JS_FALSE);
