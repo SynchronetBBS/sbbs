b59d914e80488ad6841a8fdb715ebd40ae3c0f4c
deuce <>
2008-01-21 00:05:05 +0000

    Save/restore hold_update.

--- src/uifc/filepick.c
+++ src/uifc/filepick.c
@@ -316,6 +316,7 @@ int filepick(uifcapi_t *api, char *title, struct file_pick *fp, char *dir, char 
 	int		finished=FALSE;
 	int		retval=0;
 	int		fieldmove;
+	int		oldhu=hold_update;
 
 	height=api->scrn_len-3;
 	width=SCRN_RIGHT-SCRN_LEFT-3;
@@ -639,6 +640,7 @@ int filepick(uifcapi_t *api, char *title, struct file_pick *fp, char *dir, char 
 	}
 
 cleanup:		/* Cleans up allocated variables returns from function */
+	hold_update=oldhu;
 	FREE_AND_NULL(lastpath);
 	FREE_AND_NULL(tmppath);
 	FREE_AND_NULL(tmplastpath);
