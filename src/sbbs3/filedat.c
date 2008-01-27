b0edda4cf9d680627d3a5392c1f5a1a2d889e7f2
rswindell <>
2008-01-27 09:16:53 +0000

    Don't attempt to get the file date/time if the file doesn't exist.

--- src/sbbs3/filedat.c
+++ src/sbbs3/filedat.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -74,8 +74,8 @@ BOOL DLLCALL getfiledat(scfg_t* cfg, file_t* f)
 
 	if(!f->size) {					/* only read disk if this is null */
 			getfilepath(cfg,f,str);
-			f->size=flength(str);
-			f->date=fdate(str);
+			if((f->size=flength(str))>=0)
+				f->date=fdate(str);
 	/*
 			}
 		else {
