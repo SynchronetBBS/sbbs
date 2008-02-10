1245dd053eced7ee572764ac0b404a078ba6e5fb
deuce <>
2008-02-10 05:41:07 +0000

    Some code cleanup and add support for Arrays

--- exec/load/recordfile.js
+++ exec/load/recordfile.js
@@ -1,27 +1,30 @@
-/* TODO: File Locking */
-
 function GetRecordLength(RecordDef)
 {
 	var i;
 	var len=0;
 
-	for(i=0; i<RecordDef.length; i++) {
-		switch(RecordDef[i].type) {
+	function GetTypeLength(fieldtype) {
+		switch(RecordDef[i].fieldtype) {
 			case "Integer":
-				len+=4;
-				break;
+				return(4);
 			case "Date":
-				len+=8;
-				break;
+				return(8);
 			case "Boolean":
-				len+=1;
-				break;
+				return(1);
 			default:
-				var m=RecordDef[i].type.match(/^.*:([0-9]+)$/);
+				var m=fieldtype.match(/^String:([0-9]+)$/);
 				if(m!=null)
-					len+=parseInt(m[1]);
-				break;
+					return(parseInt(m[1]));
+				return(0);
 		}
+	}
+
+	for(i=0; i<RecordDef.length; i++) {
+		var m=RecordDef[i].type.match(/^Array:([0-9]+):(.*)$/);
+		if(m!=null)
+			len += GetTypeLength(m[2])*parseInt(m[1]);
+		else
+			len += GetTypeLength(RecordDef[i].type);
 	}
 	return(len);
 }
@@ -29,16 +32,95 @@ function GetRecordLength(RecordDef)
 function RecordFile(filename, definition)
 {
 	this.file=new File(filename);
+	this.fields=definition;
 	this.RecordLength=GetRecordLength(this.fields);
 	if(!this.file.open(file_exists(this.file.name)?"rb+":"wb+",true,this.RecordLength))
 		return(null);
-	this.fields=definition;
 	this.length=this.file.length/this.RecordLength;
 
 	/* Read a record by index */
 	this.Get=RecordFile_Get;
 	/* Create a new record */
 	this.New=RecordFile_New;
+	/* Read a single field from the current position */
+	this.ReadField=RecordFile_ReadField;
+	/* Write a single field from the current position */
+	this.WriteField=RecordFile_WriteField;
+}
+
+function RecordFile_ReadField(fieldtype)
+{
+	var i;
+	var m=fieldtype.match(/^Array:([0-9]+):(.*)$/);
+
+	if(m!=null) {
+		var ret=new Array();
+		for(i=0; i<parseInt(m[1]); i++)
+			ret.push(this.ReadField(m[2]));
+		return(ret);
+	}
+	else {
+		switch(fieldtype) {
+			case "Integer":
+				return(this.file.readBin(4));
+			case "Date":
+				var tmp=this.file.read(8);
+				return(tmp.replace(/\x00/g,""));
+			case "Boolean":
+				if(file.readBin(1))
+					return(true);
+				return(false);
+			default:
+				var m=fieldtype.match(/^String:([0-9]+)$/);
+				if(m!=null) {
+					var tmp=this.file.read(parseInt(m[1]));
+					return(tmp.replace(/\x00/g,""));
+				}
+				return(null);
+		}
+	}
+}
+
+function RecordFile_WriteField(val, fieldtype)
+{
+	var i;
+	var m=fieldtype.match(/^Array:([0-9]+):(.*)$/);
+
+	if(m!=null) {
+		var ret=new Array();
+		for(i=0; i<parseInt(m[1]); i++)
+			this.WriteField(val[i], m[2]);
+		return(ret);
+	}
+	else {
+		switch(fieldtype) {
+			case "Integer":
+				this.file.writeBin(val,4);
+				break;
+			case "Date":
+				var wr=val.substr(0,8);
+				while(wr.length < 8)
+					wr=wr+"\x00";
+				this.file.write(wr,8);
+				break;
+			case "Boolean":
+				if(val)
+					this.file.writeBin(255,1);
+				else
+					this.file.writeBin(0,1);
+				break;
+			default:
+				var m=fieldtype.match(/^String:([0-9]+)$/);
+				if(m!=null) {
+					var len=parseInt(m[1]);
+					var wr=val.substr(0,len);
+					while(wr.length < len)
+						wr=wr+"\x00";
+					this.file.write(wr,len);
+				}
+				break;
+		}
+	}
 }
 
 function RecordFile_Get(num)
@@ -62,31 +144,8 @@ function RecordFile_Get(num)
 	ret.Record=num;
 
 	this.file.position=ret.Record * this.RecordLength;
-	for(i=0; i<this.fields.length; i++) {
-		switch(this.fields[i].type) {
-			case "Integer":
-				ret[this.fields[i].prop]=this.file.readBin(4);
-				break;
-			case "Date":
-				ret[this.fields[i].prop]=this.file.read(8);
-				ret[this.fields[i].prop].replace(/\x00/g,"");
-				break;
-			case "Boolean":
-				var b=this.file.readBin(1);
-				if(b)
-					ret[this.fields[i].prop]=true;
-				else
-					ret[this.fields[i].prop]=false;
-				break;
-			default:
-				var m=this.fields[i].type.match(/^String:([0-9]+)$/);
-				if(m!=null) {
-					ret[this.fields[i].prop]=this.file.read(parseInt(m[1]));
-					ret[this.fields[i].prop].replace(/\x00/g,"");
-				}
-				break;
-		}
-	}
+	for(i=0; i<this.fields.length; i++)
+		ret[this.fields[i].prop]=this.ReadField(this.fields[i].type);
 
 	return(ret);
 }
@@ -97,7 +156,7 @@ function RecordFile_New()
 	var ret=new Object();
 
 	for(i=0; i<this.fields.length; i++)
-		ret[this.fields[i].prop]=this.fields[i].def;
+		ret[this.fields[i].prop]=eval(this.fields[i].def.toSource());
 
 	ret.parent=this;
 	ret.Put=RecordFileRecord_Put;
@@ -112,72 +171,19 @@ function RecordFile_New()
 function RecordFileRecord_ReInit()
 {
 	for(i=0; i<this.parent.fields.length; i++)
-		this[this.parent.fields[i].prop]=this.parent.fields[i].def;
+		this[this.parent.fields[i].prop]=eval(this.parent.fields[i].def.toSource);
 }
 
 function RecordFileRecord_Put()
 {
 	this.parent.file.position=(this.Record)*this.parent.RecordLength;
-	for(i=0; i<this.parent.fields.length; i++) {
-writeln("Saving "+this.parent.fields[i].prop+" = "+this[this.parent.fields[i].prop]);
-		switch(this.parent.fields[i].type) {
-			case "Integer":
-				this.parent.file.writeBin(this[this.parent.fields[i].prop],4);
-				break;
-			case "Date":
-				var wr=this[this.parent.fields[i].prop].substr(0,8);
-				while(wr.length < 8)
-					wr=wr+"\x00";
-				this.parent.file.write(this[this.parent.fields[i].prop],8);
-				break;
-			case "Boolean":
-				if(this[this.parent.fields[i].prop])
-					this.parent.file.writeBin(255,1);
-				else
-					this.parent.file.writeBin(0,1);
-				break;
-			default:
-				var m=this.parent.fields[i].type.match(/^String:([0-9]+)$/);
-				if(m!=null) {
-					var len=parseInt(m[1]);
-					var wr=this[this.parent.fields[i].prop].substr(0,len);
-						while(wr.length < len)
-						wr=wr+"\x00";
-					this.parent.file.write(this[this.parent.fields[i].prop],len);
-				}
-				break;
-		}
-	}
+	for(i=0; i<this.parent.fields.length; i++)
+		this.parent.WriteField(this[this.parent.fields[i].prop], this.parent.fields[i].type);
 }
 
 function RecordFileRecord_ReLoad(num)
 {
 	this.parent.file.position=(this.Record)*this.parent.RecordLength;
-	for(i=0; i<this.parent.fields.length; i++) {
-		switch(this.parent.fields[i].type) {
-			case "Integer":
-				this[this.parent.fields[i].prop]=this.parent.file.readBin(4);
-				break;
-			case "Date":
-				this[this.parent.fields[i].prop]=this.parent.file.read(8);
-				this[this.parent.fields[i].prop].replace(/\x00/g,"");
-				break;
-			case "Boolean":
-				var b=this.parent.file.readBin(1);
-				if(b)
-					this[this.parent.fields[i].prop]=true;
-				else
-					this[this.parent.fields[i].prop]=false;
-				break;
-			default:
-				var m=this.parent.fields[i].type.match(/^String:([0-9]+)$/);
-				if(m!=null) {
-					this[this.parent.fields[i].prop]=this.parent.file.read(parseInt(m[1]));
-					this[this.parent.fields[i].prop].replace(/\x00/g,"");
-				}
-				break;
-		}
-	}
-
-	return(ret);
+	for(i=0; i<this.parent.fields.length; i++)
+		this[this.parent.fields[i].prop]=this.parent.ReadField(this.parent.fields[i].type);
 }
