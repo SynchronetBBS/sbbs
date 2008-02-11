8f0302130a86f8f4f5e340faa66e82cf4e25933b
deuce <>
2008-02-11 08:36:30 +0000

    Fix warnings.

--- src/conio/cterm.c
+++ src/conio/cterm.c
@@ -473,8 +473,7 @@ void scrollup(void)
 
 void dellines(int lines)
 {
-	char *buf;
-	int i,j,k;
+	int i;
 	int linestomove;
 	int x,y;
 
@@ -511,11 +510,6 @@ void clear2bol(void)
 
 void cterm_clearscreen(char attr)
 {
-	unsigned char *buf;
-	int i;
-	int width,height;
-	struct text_info ti;
-
 	if(cterm.scrollback!=NULL) {
 		cterm.backpos+=cterm.height;
 		if(cterm.backpos>cterm.backlines) {
@@ -1317,7 +1311,6 @@ char *cterm_write(unsigned char *buf, int buflen, char *retbuf, size_t retsize, 
 	struct text_info	ti;
 	int	olddmc;
 	int oldptnm;
-	unsigned char *p;
 
 	oldptnm=puttext_can_move;
 	puttext_can_move=1;
