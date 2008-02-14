171725aaeb4dac972db8536bc39e2067e5e3739f
rswindell <>
2008-02-14 08:06:58 +0000

    Fixed typo in comment.

--- src/sbbs3/scfglib2.c
+++ src/sbbs3/scfglib2.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -39,7 +39,7 @@
 #include "scfglib.h"
 
 /****************************************************************************/
-/* Reads in LIBS.CNF and initializes the associated variables				*/
+/* Reads in FILE.CNF and initializes the associated variables				*/
 /****************************************************************************/
 BOOL read_file_cfg(scfg_t* cfg, char* error)
 {
