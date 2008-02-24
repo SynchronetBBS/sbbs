8b07ddbcc77f50436a1e13989b4fe3da98378ee2
rswindell <>
2008-02-24 18:51:39 +0000

    Work-around for messages with a net_type!=NET_NONE, but no net_addr.

--- src/sbbs3/qwknodes.c
+++ src/sbbs3/qwknodes.c
@@ -6,7 +6,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -319,7 +319,7 @@ int main(int argc, char **argv)
 			}
 			smb_unlockmsghdr(&smb,&msg);
 			if((mode&LOCAL && msg.from_net.type==NET_NONE)
-				|| msg.from_net.type==NET_QWK) {
+				|| (msg.from_net.type==NET_QWK && msg.from_net.addr!=NULL)) {
 				if(msg.from_net.type!=NET_QWK)
 					msg.from_net.addr="";
 				if(cmd&USERS) {
