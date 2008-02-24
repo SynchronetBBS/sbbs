8c672d7ac65defffaecda4715bd908b36ea3f6ce
rswindell <>
2008-02-24 08:06:31 +0000

    Flush stderr stream between headers.

--- src/sbbs3/chksmb.c
+++ src/sbbs3/chksmb.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -277,6 +277,7 @@ int main(int argc, char **argv)
 	for(l=smb.status.header_offset;l<length;l+=size) {
 		size=SHD_BLOCK_LEN;
 		fprintf(stderr,"\r%2lu%%  ",(long)(100.0/((float)length/l)));
+		fflush(stderr);
 		memset(&msg,0,sizeof(msg));
 		msg.idx.offset=l;
 		msgerr=FALSE;
