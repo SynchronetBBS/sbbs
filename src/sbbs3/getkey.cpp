f519e280c5a38d96fe8d83f1fcfcbb6181ec3674
rswindell <>
2008-06-05 05:24:34 +0000

    Fixed bcc32 compile error.

--- src/sbbs3/getkey.cpp
+++ src/sbbs3/getkey.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -301,7 +301,7 @@ char sbbs_t::getkey(long mode)
 /****************************************************************************/
 void sbbs_t::mnemonics(const char *str)
 {
-    char *ctrl_a_codes;
+    const char *ctrl_a_codes;
     long l;
 
 	if(!strchr(str,'~')) {
