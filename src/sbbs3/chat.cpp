e9d58e4fde18b35a6b4f65ae783b9d89bc744bf5
rswindell <>
2008-07-09 01:37:20 +0000

    Bug-fix: C-restricted users could not be pulled into local sysop chat.

--- src/sbbs3/chat.cpp
+++ src/sbbs3/chat.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -728,14 +728,15 @@ void sbbs_t::privchat(bool local)
 	node_t	node;
 	time_t	last_nodechk=0;
 
-	if(useron.rest&FLAG('C')) {
-		bputs(text[R_Chat]);
-		return; 
-	}
-
 	if(local) 
 		n=0;
 	else {
+
+		if(useron.rest&FLAG('C')) {
+			bputs(text[R_Chat]);
+			return; 
+		}
+
 		n=getnodetopage(0,0);
 		if(!n)
 			return;
