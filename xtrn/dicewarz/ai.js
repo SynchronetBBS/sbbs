4f01c1e3dfd23e0bc62abbabbcf517de541c2123
deuce <>
2008-09-22 22:02:13 +0000

    Make Deep Blue fall back to paranoid unless he has his fair share of territories.

--- xtrn/dicewarz/ai.js
+++ xtrn/dicewarz/ai.js
@@ -143,8 +143,13 @@ function	UltraParanoidAICheck(gameNumber, playerNumber, base, target)
 	g=games.gameData[gameNumber];
 	computerPlayer=g.players[playerNumber];
 
+	/* If we don't have our "fair share" of territories, use paranoid attack */
+	if(computerPlayer.territories <= g.playerTerr) {
+		return(ParanoidAICheck(gameNumber, playerNumber, base, target));
+	}
+
 	/* If reserves + expected new dice - used reserves is still greater than seven, use the merely paranoid attack */
-	if(computerPlayer.reserve + computerPlayer.territories - (computerPlayer.AI.moves*7) > 7) {
+	if(computerPlayer.reserve + computerPlayer.territories - (computerPlayer.AI.moves*8) > 7) {
 		return(ParanoidAICheck(gameNumber, playerNumber, base, target));
 	}
 
