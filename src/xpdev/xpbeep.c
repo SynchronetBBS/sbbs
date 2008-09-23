5cf9a426d65d82ff71b16d210c8e920a163ae557
deuce <>
2008-09-23 20:23:05 +0000

    Remove duplicate code

--- src/xpdev/xpbeep.c
+++ src/xpdev/xpbeep.c
@@ -665,8 +665,6 @@ BOOL DLLCALL xp_play_sample(const unsigned char *sample, size_t sample_size, BOO
 	if(handle_type==SOUND_DEVICE_WIN32) {
 		wh.lpData=sample;
 		wh.dwBufferLength=sample_size;
-		wh.lpData=sample;
-		wh.dwBufferLength=sample_size;
 		if(waveOutPrepareHeader(waveOut, &wh, sizeof(wh))==MMSYSERR_NOERROR) {
 			if(waveOutWrite(waveOut, &wh, sizeof(wh))==MMSYSERR_NOERROR) {
 				while(!(wh.dwFlags & WHDR_DONE))
