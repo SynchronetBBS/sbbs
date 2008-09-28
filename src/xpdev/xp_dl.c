7c931fda77bb17923503d449f0f50424dbededbb
deuce <>
2008-09-28 07:32:09 +0000

    Fix Win32 compile bug reported by Toolman (Thanks!)

--- src/xpdev/xp_dl.c
+++ src/xpdev/xp_dl.c
@@ -59,7 +59,7 @@ DLLEXPORT dll_handle DLLCALL xp_dlopen(const char **names, int mode, int major)
 	return(NULL);
 }
 #elif defined(_WIN32)
-xp_dlopen(const char **name, int mode, int major)
+DLLEXPORT dll_handle DLLCALL xp_dlopen(const char **names, int mode, int major)
 {
 	char		fname[MAX_PATH+1];
 	dll_handle	ret=NULL;
