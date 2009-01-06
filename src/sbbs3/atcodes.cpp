cef6de82ba4fee5d7ab60132f80cd1bac62ccc51
rswindell <>
2009-01-06 03:29:25 +0000

    Fix bug with INCLUDE: @-code which caused any @-code starting with "INCLU" to be treated as INCLUDE: - just happened to notice this one.

--- src/sbbs3/atcodes.cpp
+++ src/sbbs3/atcodes.cpp
@@ -612,7 +612,7 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 		return(nulstr);
 	}
 
-	if(!strncmp(sp,"INCLUDE:",5)) {
+	if(!strncmp(sp,"INCLUDE:",8)) {
 		printfile(cmdstr(sp+8,nulstr,nulstr,str),P_NOCRLF|P_SAVEATR);
 		return(nulstr);
 	}
