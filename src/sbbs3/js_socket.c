f5c4e6d5dd9e97ea6b091c384f93207fdb3265f7
rswindell <>
2009-01-09 00:25:57 +0000

    Bug-fix: Socket.sendfile() would leave file open (as of 2 years 8 months ago!).

--- src/sbbs3/js_socket.c
+++ src/sbbs3/js_socket.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -526,6 +526,7 @@ js_sendfile(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 	free(buf);
 #else
 	len = sendfilesocket(p->sock, file, NULL, 0);
+	close(file);
 	if(len > 0) {
 		dbprintf(FALSE, p, "sent %u bytes",len);
 		*rval = JSVAL_TRUE;
