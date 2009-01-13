a1c2cef9bfd3aa6430d8c01cbfb14e938370e06d
rswindell <>
2009-01-13 06:08:20 +0000

    Moved the "insert here" comment in the xtrn_prog_prop_desc array.

--- src/sbbs3/js_xtrn_area.c
+++ src/sbbs3/js_xtrn_area.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -72,8 +72,8 @@ static char* xtrn_prog_prop_desc[] = {
 	,"extra time given to users running this program"
 	,"maximum time allowed in program"
 	,"execution cost (credits to run this program)"
-	,"user has sufficient access to see this program"
 	/* Insert here */
+	,"user has sufficient access to see this program"
 	,"user has sufficient access to run this program"
 	,NULL
 };
