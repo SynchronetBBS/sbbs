bdefdfd75de2b2243edc3341758566be481d318c
rswindell <>
2009-01-20 21:22:40 +0000

    Fix bug where-by a js.on_exit() evaluation string is terminated after its first loop because the termination (e.g. dropped TCP session) flag is set: Disable the auto-termination when evaluting on-exit strings.

--- src/sbbs3/js_internal.c
+++ src/sbbs3/js_internal.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -424,6 +424,8 @@ void DLLCALL js_EvalOnExit(JSContext *cx, JSObject *obj, js_branch_t* branch)
 	char*	p;
 	jsval	rval;
 	JSScript* script;
+
+	branch->auto_terminate=false;
 
 	while((p=strListPop(&branch->exit_func))!=NULL) {
 		if((script=JS_CompileScript(cx, obj, p, strlen(p), NULL, 0))!=NULL) {
