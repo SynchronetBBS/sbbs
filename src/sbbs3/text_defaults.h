f3e2ee0bad26be44dea347b03bd923b245c9991d
deuce <>
2009-01-23 07:29:34 +0000

    Updated... better... not done.

--- src/sbbs3/text_defaults.h
+++ src/sbbs3/text_defaults.h
@@ -9,12 +9,13 @@
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
  * Auto-generated from CTRL\TEXT.DAT										*
-/****************************************************************************
+ ****************************************************************************/
 
 #ifndef _TEXT_DEFAULTS_H_
 #define _TEXT_DEFAULTS_H_
 
 #include "text.h"
-extern const char * const text_defaults[TOTAL_TEXT]
+
+extern const char * const text_defaults[TOTAL_TEXT];
 
 #endif
