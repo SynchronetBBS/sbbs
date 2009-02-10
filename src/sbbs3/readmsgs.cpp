b5eb48d9cd2591c14874acb82e1bfff49786550f
rswindell <>
2009-02-10 07:56:01 +0000

    Strip all ctrl chars and ctrl-a sequences from message buffer before comparing for text searches. The message subject is now converted to uppercase to compare case- insensitively.

--- src/sbbs3/readmsgs.cpp
+++ src/sbbs3/readmsgs.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -346,6 +346,7 @@ int sbbs_t::scanposts(uint subnum, long mode, const char *find)
 {
 	char	str[256],str2[256],do_find=true,mismatches=0
 			,done=0,domsg=1,*buf,*p;
+	char	subj[128];
 	char	find_buf[128];
 	char	tmp[128];
 	int		i;
@@ -574,8 +575,11 @@ int sbbs_t::scanposts(uint subnum, long mode, const char *find)
 							domsg=0;
 					continue; 
 				}
-				strupr((char *)buf);
-				if(!strstr((char *)buf,find) && !strstr(msg.subj,find)) {
+				strupr(buf);
+				strip_ctrl(buf);
+				SAFECOPY(subj,msg.subj);
+				strupr(subj);
+				if(!strstr(buf,find) && !strstr(subj,find)) {
 					free(buf);
 					if(smb.curmsg<smb.msgs-1) 
 						smb.curmsg++;
@@ -1171,7 +1175,8 @@ long sbbs_t::listsub(uint subnum, long mode, long start, const char* search)
 long sbbs_t::searchposts(uint subnum, post_t *post, long start, long posts
 	, const char *search)
 {
-	char	*buf,ch;
+	char*	buf,ch;
+	char	subj[128];
 	long	l,found=0;
 	smbmsg_t msg;
 
@@ -1186,8 +1191,11 @@ long sbbs_t::searchposts(uint subnum, post_t *post, long start, long posts
 			smb_freemsgmem(&msg);
 			continue; 
 		}
-		strupr((char *)buf);
-		if(strstr((char *)buf,search) || strstr(msg.subj,search)) {
+		strupr(buf);
+		strip_ctrl(buf);
+		SAFECOPY(subj,msg.subj);
+		strupr(subj);
+		if(strstr(buf,search) || strstr(subj,search)) {
 			if(!found)
 				CRLF;
 			if(msg.hdr.attr&MSG_DELETE)
