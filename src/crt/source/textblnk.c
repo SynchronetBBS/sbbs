b36da979fd3ac7387b5e749785475a2cb41f0a24
deuce <>
2009-02-10 11:28:46 +0000

    Fix settextblink()

--- src/crt/source/textblnk.c
+++ src/crt/source/textblnk.c
@@ -10,4 +10,5 @@ void settextblink (int cmd) //if cmd==1 => set attribute bit 7 as enable blink
 		flags &= ~CIOLIB_VIDEO_BGBRIGHT;
 	else
 		flags |= CIOLIB_VIDEO_BGBRIGHT;
+	setvideoflags(flags);
  }
