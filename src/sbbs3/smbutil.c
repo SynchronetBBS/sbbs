e225579cdaab4881459565a52820464a15423659
rswindell <>
2009-02-15 12:50:20 +0000

    Removed unused binstr() function (now part of smbdump.c).

--- src/sbbs3/smbutil.c
+++ src/sbbs3/smbutil.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -480,30 +480,6 @@ void listmsgs(ulong start, ulong count)
 		smb_freemsgmem(&msg);
 		l++; 
 	}
-}
-
-char *binstr(uchar *buf, ushort length)
-{
-	static char str[512];
-	char tmp[128];
-	int i;
-
-	str[0]=0;
-	for(i=0;i<length;i++)
-		if(buf[i] && (buf[i]<' ' || buf[i]>=0x7f) 
-			&& buf[i]!='\r' && buf[i]!='\n' && buf[i]!='\t')
-			break;
-	if(i==length)		/* not binary */
-		return((char*)buf);
-	for(i=0;i<length;i++) {
-		sprintf(tmp,"%02X ",buf[i]);
-		strcat(str,tmp); 
-		if(i>=100) {
-			strcat(str,"...");
-			break;
-		}
-	}
-	return(str);
 }
 
 /****************************************************************************/
