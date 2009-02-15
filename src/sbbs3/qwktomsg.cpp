1e5e72412ec7c48f191ed8729ebd7ecfa58613f9
rswindell <>
2009-02-15 12:57:52 +0000

    Bug-fix: when receiving routed QWKnetmail from a node that includes a HEADER.DAT file in it's .rep packets (e.g. running v3.15a), the "NETMAIL" to field would be over-ridden and the mail never routed.

--- src/sbbs3/qwktomsg.cpp
+++ src/sbbs3/qwktomsg.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -38,7 +38,7 @@
 #include "sbbs.h"
 #include "qwk.h"
 
-static void qwk_parse_header_list(smbmsg_t* msg, str_list_t* headers, bool parse_sender_hfields)
+static void qwk_parse_header_list(smbmsg_t* msg, str_list_t* headers, bool parse_sender_hfields, bool parse_recipient_hfields)
 {
 	char*		p;
 	char		zone[32];
@@ -57,14 +57,18 @@ static void qwk_parse_header_list(smbmsg_t* msg, str_list_t* headers, bool parse
 			msg->hdr.when_written.zone=(ushort)strtoul(zone,NULL,16);
 	}
 
-	if((p=iniPopKey(headers,ROOT_SECTION,smb_hfieldtype(hfield_type=RECIPIENT),value))!=NULL)
-		smb_hfield_str(msg,hfield_type,p);		
+	if((p=iniPopKey(headers,ROOT_SECTION,smb_hfieldtype(hfield_type=RECIPIENT),value))!=NULL) {
+		if(parse_recipient_hfields)
+			smb_hfield_str(msg,hfield_type,p);		
+	}
 
 	/* Recipient net address and type */
 	if((p=iniPopKey(headers,ROOT_SECTION,smb_hfieldtype(hfield_type=RECIPIENTNETADDR),value))!=NULL) {
-		net_type=NET_UNKNOWN;
-		smb_hfield_netaddr(msg,hfield_type,p,&net_type);
-		smb_hfield_bin(msg,RECIPIENTNETTYPE,net_type);
+		if(parse_recipient_hfields) {
+			net_type=NET_UNKNOWN;
+			smb_hfield_netaddr(msg,hfield_type,p,&net_type);
+			smb_hfield_bin(msg,RECIPIENTNETTYPE,net_type);
+		}
 	}
 
 	if((p=iniPopKey(headers,ROOT_SECTION,smb_hfieldtype(hfield_type=SUBJECT),value))!=NULL)
@@ -146,6 +150,7 @@ static void qwk_parse_header_list(smbmsg_t* msg, str_list_t* headers, bool parse
 void sbbs_t::qwk_new_msg(smbmsg_t* msg, char* hdrblk, long offset, str_list_t all_headers, bool parse_sender_hfields)
 {
 	char str[128];
+	char to[128];
 	str_list_t	msg_headers;
 
 	smb_freemsgmem(msg);
@@ -156,8 +161,11 @@ void sbbs_t::qwk_new_msg(smbmsg_t* msg, char* hdrblk, long offset, str_list_t al
 	memset(msg,0,sizeof(smbmsg_t));		/* Initialize message header */
 	msg->hdr.version=smb_ver();
 
+	sprintf(to,"%25.25s",(char *)hdrblk+21);     /* To user */
+	truncsp(to);
+
 	if(msg_headers!=NULL)
-		qwk_parse_header_list(msg, &msg_headers, parse_sender_hfields);
+		qwk_parse_header_list(msg, &msg_headers, parse_sender_hfields, stricmp(to,"NETMAIL")!=0);
 
 	/* Parse the QWK message header: */
 	if(msg->hdr.when_written.time==0) {
@@ -174,11 +182,8 @@ void sbbs_t::qwk_new_msg(smbmsg_t* msg, char* hdrblk, long offset, str_list_t al
 		msg->hdr.when_written.time=sane_mktime(&tm);
 	}
 
-	if(msg->to==NULL) {
-		sprintf(str,"%25.25s",(char *)hdrblk+21);     /* To user */
-		truncsp(str);
-		smb_hfield_str(msg,RECIPIENT,str);
-	}
+	if(msg->to==NULL)
+		smb_hfield_str(msg,RECIPIENT,to);
 
 	if(parse_sender_hfields && msg->from==NULL) {
 		sprintf(str,"%25.25s",hdrblk+46);  
@@ -509,3 +514,4 @@ bool sbbs_t::qwk_import_msg(FILE *qwk_fp, char *hdrblk, ulong blocks
 
 	return(success);
 }
+
