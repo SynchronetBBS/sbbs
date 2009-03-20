1530765f4b195cdf61d6558051f3795c979b8274
mcmlxxix <>
2009-03-20 21:45:48 +0000

    Fixed script exit evaluation to remove user file properly. Reworked Init() function to be more simplified

--- exec/load/chateng.js
+++ exec/load/chateng.js
@@ -17,25 +17,45 @@
 	For more advanced chatting, see 
 	ChatEngine this.Init() method
 */
-function Chat(Engine,key)
+function Chat(Engine,control,key)
 {
-	if(this.fullscreen) console.clear();
+	if(!Engine) Exit(0);
+	if(Engine.fullscreen) console.clear();
 	else
 	{
 		console.ctrlkey_passthru="+ACGKLOPQRTUVWXYZ_";
 		bbs.sys_status|=SS_MOFF;
 	}
-	while(1)
+	if(control)
 	{
-		if(!key) key=console.inkey(K_NOCRLF|K_NOSPIN|K_NOECHO,5);
-		Engine.ProcessKey(key);
-		switch(key)
+		while(1)
 		{
-			case '\r':
-				return true;
-			default:
-				key="";
-				break;
+			key=console.inkey(K_NOCRLF|K_NOSPIN|K_NOECHO,5);
+			Engine.ProcessKey(key);
+			switch(key.toUpperCase())
+			{
+				case 'Q':
+					return true;
+				default:
+					key="";
+					break;
+			}
+		}
+	}
+	else
+	{
+		while(1)
+		{
+			if(!key) key=console.inkey(K_NOCRLF|K_NOSPIN|K_NOECHO,5);
+			Engine.ProcessKey(key);
+			switch(key)
+			{
+				case '\r':
+					return true;
+				default:
+					key="";
+					break;
+			}
 		}
 	}
 }
@@ -43,7 +63,8 @@ function Chat(Engine,key)
 load("qengine.js");
 var ChatLog;
 var oldpass=console.ctrlkey_passthru;
-js.on_exit("file_remove(this.queue.user_file.name)");
+var killfile;
+js.on_exit("file_remove(killfile)");
 
 
 function ChatEngine(root,name,log_)
@@ -53,9 +74,8 @@ function ChatEngine(root,name,log_)
 	this.name=		(name?name:"chat");
 	
 	//DEFAULT VALUES FOR FULL SCREEN CHAT MODE WITH NO DEDICATED INPUT LINE
-	this.realtime=	false;
-	this.input_line=false;
 	this.fullscreen=true;
+	this.input_line=false;
 	this.boxed=		false;
 	this.columns=	79;
 	this.rows=		24;
@@ -70,35 +90,18 @@ function ChatEngine(root,name,log_)
 	this.history=	[];
 	this.queue=new DataQueue(this.root,this.name,ChatLog);
 
-	this.Init=function(mode,parameters)
+	this.Init=function(mode,columns,rows,input_line,posx,posy,boxed)
 	{
-		if(mode)
-		{
-			if(!parameters) Quit(100);
-			switch(mode)
-			{
-				case 'F':
-					break;
-				case 'W':		//WINDOW MODE
-					this.columns=	parameters.columns;
-					this.rows=		parameters.rows;
-					this.input_line=parameters.input_line;
-					this.x=			parameters.x;
-					this.y=			parameters.y;
-					this.fullscreen=false;
-					break;
-				case 'S':		//SINGLE LINE MODE
-					this.rows=		1;
-					this.columns=	parameters.columns;
-					this.x=			parameters.x;
-					this.y=			parameters.y;
-					this.input_line={x:this.x,y:this.y,columns:this.columns};
-					this.fullscreen=false;
-					break;
-			}
-			Log("Chat Initialized:");
-			Log("mode: " + (this.fullscreen==true?"fullscreen":(mode=='W'?"window":"single line")));
-		}
+		this.fullscreen=(mode=="FULL"?true:false);
+		if(columns) 	this.columns=columns;
+		if(rows)		this.rows=rows;
+		if(input_line)	this.input_line=input_line;
+		if(posx)		this.x=posx;
+		if(posy)		this.y=posy;
+		if(boxed)		this.boxed=boxed;
+		
+		Log("Chat Initialized: Mode: " + mode);
+		killfile=this.queue.user_file.name;
 		bbs.sys_status |= SS_PAUSEOFF;		
 	}
 	this.Cycle=function()
@@ -241,7 +244,10 @@ function ChatEngine(root,name,log_)
 			console.gotoxy(this.x,this.y);
 			for(msg in this.messages)
 			{
-				console.gotoxy(this.x,this.y+parseInt(msg));
+				//TODO: FIX SINGLE LINE MODE... NOT FUNCTIONAL
+				if(this.rows==1) console.gotoxy(this.x,this.y);
+				
+				else console.gotoxy(this.x,this.y+parseInt(msg));
 				console.putmsg(this.messages[msg],P_SAVEATR);
 				ClearLine(this.columns-console.strlen(strip_ctrl(this.messages[msg])));
 			}
@@ -250,7 +256,6 @@ function ChatEngine(root,name,log_)
 	this.Concat=function(text)
 	{
 		var array=word_wrap(text,this.columns,text.length,false);
-		Log(text);
 		array=array.split(/[\r\n$]+/);
 		for(item in array)
 			if(array[item]!="") this.messages.push(RemoveSpaces(array[item]));
