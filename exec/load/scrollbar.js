3a7e889f1360cbd4722a71d37d77ae64af1d2680
mcmlxxix <>
2009-03-24 21:03:28 +0000

    Store scrollbar index in scrollbar object for faster redrawing and incrementing

--- exec/load/scrollbar.js
+++ exec/load/scrollbar.js
@@ -8,62 +8,67 @@ function Scrollbar(firstx,firsty,lastx,lasty,color)
 	this.first;
 	this.last;
 	this.orientation;
+	this.index;
+	this.length;
 	this.color=color?color:'\1n';
-
+	
 	this.init=function(fx,fy,lx,ly)
 	{
 		this.first={'x':fx,'y':fy};
 		this.last={'x':lx,'y':ly};
 		
-		if(fx==lx) 		this.orientation=1;
-		else if(fy==ly) this.orientation=0;
+		if(fx==lx)
+		{
+			this.orientation=1;
+			this.length=this.last.y-this.first.y-1;
+		}
+		else if(fy==ly) 
+		{
+			this.orientation=0;
+			this.length=this.last.x-this.first.x-1;
+		}
 		else exit(); //if scrollbar orientation is not vertical or horizontal do not continue
 	}
 	this.draw=function(position, total)
 	{
 		if(!this.first || !this.last) return; 		//if no start or end points initialized, do not draw
-		if(!position || position>total) 
+		if(position && total)
 		{
-			return;		//if position is invalid, do not draw
-		}
+			if(position>total) return; 	//invalid position = do not draw
+			var percentage=position/total;
+			this.index=parseInt(percentage*this.length);
+		}		
+		else if(!this.index) return;	//no position supplied and no index = do not draw
 		
-		var percentage=position/total;
-		
-		if(this.orientation==1) this.drawVert(percentage);
-		else this.drawHoriz(percentage);
+		if(this.orientation==1) this.drawVert();
+		else this.drawHoriz();
 	}
-	this.drawVert=function(percentage)
+	this.drawVert=function()
 	{
-		var length=this.last.y-this.first.y-1;
-		if(length==0) return; //who needs a zero length scrollbar?
-		
-		var index=parseInt(percentage*length);
+		if(this.length==0) return; //who needs a zero length scrollbar?
 		
 		console.gotoxy(this.first.x,this.first.y);
 		console.putmsg(this.color + "\1h" + ascii(30));
-		for(i=1;i<=length;i++)
+		for(i=1;i<=this.length;i++)
 		{
 			console.down();
 			console.left();
-			if(i==index) console.putmsg(this.color + '\1h\xDB');
+			if(i==this.index) console.putmsg(this.color + '\1h\xDB');
 			else console.putmsg('\1n' + this.color + '\xB0');
 		}
 		console.down();
 		console.left();
 		console.putmsg(this.color + "\1h" + ascii(31));
 	}
-	this.drawHoriz=function(percentage)
+	this.drawHoriz=function()
 	{
-		var length=this.last.x-this.first.x-1;
-		if(length==0) return; //who needs a zero length scrollbar?
-		
-		var index=parseInt(percentage*length);
+		if(this.length==0) return; //who needs a zero length scrollbar?
 		
 		console.gotoxy(this.first.x,this.first.y);
 		console.putmsg(this.color + "\1h" + ascii(17));
-		for(i=1;i<=length;i++)
+		for(i=1;i<=this.length;i++)
 		{
-			if(i==index) console.putmsg(this.color + '\1h\xDB');
+			if(i==this.index) console.putmsg(this.color + '\1h\xDB');
 			else console.putmsg('\1n' + this.color + '\xB0');
 		}
 		console.putmsg(this.color + "\1h" + ascii(16));
