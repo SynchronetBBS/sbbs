f6e8d476d529205c4c8673ceb5e47c626bb3e956
mcmlxxix <>
2009-05-05 17:16:22 +0000

    cleaned up logfile storage methods. now has a default max log age of 10 days, max number of 10 logs. should go back and clean up old messes created by previously unlimited log amounts, though it may slow down the affected modules on first run (significantly)

--- exec/load/logging.js
+++ exec/load/logging.js
@@ -1,17 +1,65 @@
 function Logger(root, name)
 {
-	this.rootDirectory=root;
-	this.fileName=name;
-	this.logList=directory(this.rootDirectory + this.fileName + "*.log"); 		
-	this.logFile=new File(this.rootDirectory + this.fileName + this.logList.length + ".log");
-	this.logFile.open('w+',false);
+	this.maxlogs=10;
+	this.maxage=10;
+	this.root=root;
+	this.prefix=name;
+	this.logfile;
+	this.Init=function()
+	{
+		var now=time();
+		var loglist=directory(this.root + this.prefix + "*.log"); 	
+		for(l in loglist)
+		{
+			var then=file_date(loglist[l]);
+			var diff=now-then;
+			var daysold=((diff/60)/60)/24;
+			if(daysold>this.maxage)
+			{
+				var remove=loglist[l];
+				file_remove(remove);
+			}
+		}
+		loglist=directory(this.root + this.prefix + "*.log"); 	
+		var numlogs=loglist.length;
+		for(n=0;n<numlogs;n++)
+		{
+			for(m = 0; m < (numlogs-1); m++) 
+			{
+				if(file_date(loglist[m]) < file_date(loglist[m+1])) 
+				{
+					holder = loglist[m+1];
+					loglist[m+1] = loglist[m];
+					loglist[m] = holder;
+				}
+			}
+		}
+		while(loglist.length>=this.maxlogs)
+		{
+			var remove=loglist.shift();
+			file_remove(remove);
+		}
+		var num="01";
+		while(file_exists(this.root+this.prefix+num+".log"))
+		{
+			num++;
+			if(num<10) num="0"+num;
+		}
+		this.logfile=new File(this.root + this.prefix + num + ".log");
+		this.Log("****" + system.datestr() +" - "+ system.timestr());
+	}
+	this.ScreenDump=function()	
+	{
+		console.clear();
+		console.printfile(this.logfile.name);
+		console.pause();
+	}
 	this.Log=function(text)
 	{
-		this.logFile.writeln(text);
+		this.logfile.open(file_exists(this.logfile.name) ? 'r+':'w+');
+		this.logfile.writeln(text);
+		this.logfile.close();
 	}
-	this.ScreenDump=function()	//NOT WORKING, FOR SOME REASON
-	{
-		//console.printfile(this.logFile.name);
-	}
-	this.Log("****" + system.datestr() +" - "+ system.timestr());
+	this.Init();
 }
+
