58073b896ca36b56e6d222e7e4ce12f3df7792b9
rswindell <>
2009-07-14 01:47:58 +0000

    Resolve bcc32 warning.

--- src/sbbs3/js_conio.c
+++ src/sbbs3/js_conio.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -607,7 +607,7 @@ js_conio_setfont(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *r
 {
 	int32	font;
 	int force=JS_FALSE;
-	int fnum=0;
+	int32 fnum=0;
 	jsrefcount	rc;
 	int arg=0;
 
