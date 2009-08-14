9976306ab90a322de337b15e36ed04fe157bbcc1
rswindell <>
2009-08-14 08:56:33 +0000

    Fixed-up support for JavaScriptLoadPath key value.

--- src/sbbs3/sbbs_ini.c
+++ src/sbbs3/sbbs_ini.c
@@ -135,7 +135,8 @@ void sbbs_get_js_settings(
 	js->gc_interval		= iniGetInteger(list,section,strJavaScriptGcInterval	,defaults->gc_interval);
 	js->yield_interval	= iniGetInteger(list,section,strJavaScriptYieldInterval	,defaults->yield_interval);
 
-	iniFreeStringList(js->load_path);
+    if(js->load_path != defaults->load_path)
+    	iniFreeStringList(js->load_path);
 	if((load_path = iniGetStringList(list, section,strJavaScriptLoadPath,",",NULL)) == NULL)
 		load_path = defaults->load_path;
 	js->load_path = load_path;
@@ -158,6 +159,7 @@ BOOL sbbs_set_js_settings(
 			,JAVASCRIPT_BRANCH_LIMIT
 			,JAVASCRIPT_GC_INTERVAL
 			,JAVASCRIPT_YIELD_INTERVAL
+            ,NULL   /* load_path */
 		};
 
 	if(defaults==NULL)
@@ -194,6 +196,11 @@ BOOL sbbs_set_js_settings(
 		iniRemoveValue(lp,section,strJavaScriptYieldInterval);
 	else 
 		failure|=iniSetInteger(lp,section,strJavaScriptYieldInterval,js->yield_interval,style)==NULL;
+
+	if(js->load_path==defaults->load_path)
+		iniRemoveValue(lp,section,strJavaScriptLoadPath);
+	else
+		failure|=iniSetStringList(lp,section,strJavaScriptLoadPath,",",js->load_path,style)==NULL;
 
 	return(!failure);
 }
@@ -237,7 +244,8 @@ static void get_ini_globals(str_list_t list, global_startup_t* global)
 	global->js.branch_limit		= JAVASCRIPT_BRANCH_LIMIT;
 	global->js.gc_interval		= JAVASCRIPT_GC_INTERVAL;
 	global->js.yield_interval	= JAVASCRIPT_YIELD_INTERVAL;
-	global->js.load_path		= strListSplit(NULL, JAVASCRIPT_LOAD_PATH, ",");
+    if(global->js.load_path==NULL)
+    	global->js.load_path	= strListSplit(NULL, JAVASCRIPT_LOAD_PATH, ",");
 
 	/* Read .ini values here */
 	sbbs_get_js_settings(list, section, &global->js, &global->js);
