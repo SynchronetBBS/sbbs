1fd419820a939c204d01946a2df13f1a0371b3ec
rswindell <>
2009-10-05 04:50:57 +0000

    The thread_up(), socket_open(), and client_add() callbacks no longer modify the status bar directly (this is now handled in the UpTimerTick), thus preventing crashes if 2 callbacks are called concurrently.

--- src/sbbs3/ctrl/MainFormUnit.cpp
+++ src/sbbs3/ctrl/MainFormUnit.cpp
@@ -182,10 +182,6 @@ static void thread_up(void* p, BOOL up, BOOL setuid)
 	    threads++;
     else if(threads>0)
     	threads--;
-    sprintf(str,"Threads: %d",threads);
-    AnsiString Str=AnsiString(str);
-    if(MainForm->StatusBar->Panels->Items[0]->Text!=Str)
-		MainForm->StatusBar->Panels->Items[0]->Text=Str;
     ReleaseMutex(mutex);
 }
 
@@ -203,10 +199,6 @@ void socket_open(void* p, BOOL open)
 	    sockets++;
     else if(sockets>0)
     	sockets--;
-    sprintf(str,"Sockets: %d",sockets);
-    AnsiString Str=AnsiString(str);
-    if(MainForm->StatusBar->Panels->Items[1]->Text!=Str)
-		MainForm->StatusBar->Panels->Items[1]->Text=Str;
     ReleaseMutex(mutex);
 }
 
@@ -222,15 +214,6 @@ static void client_add(void* p, BOOL add)
         total_clients++;
     } else if(clients>0)
     	clients--;
-    sprintf(str,"Clients: %d",clients);
-    AnsiString Str=AnsiString(str);
-    if(MainForm->StatusBar->Panels->Items[2]->Text!=Str)
-		MainForm->StatusBar->Panels->Items[2]->Text=Str;
-
-    sprintf(str,"Served: %d",total_clients);
-    Str=AnsiString(str);
-    if(MainForm->StatusBar->Panels->Items[3]->Text!=Str)
-		MainForm->StatusBar->Panels->Items[3]->Text=Str;
 }
 
 static void client_on(void* p, BOOL on, int sock, client_t* client, BOOL update)
@@ -3043,6 +3026,27 @@ void __fastcall TMainForm::UpTimerTick(TObject *Sender)
     AnsiString Str=AnsiString(str);
     if(MainForm->StatusBar->Panels->Items[4]->Text!=Str)
 		MainForm->StatusBar->Panels->Items[4]->Text=Str;
+
+    sprintf(str,"Threads: %d",threads);
+    Str=AnsiString(str);
+    if(MainForm->StatusBar->Panels->Items[0]->Text!=Str)
+		MainForm->StatusBar->Panels->Items[0]->Text=Str;
+
+    sprintf(str,"Sockets: %d",sockets);
+    Str=AnsiString(str);
+    if(MainForm->StatusBar->Panels->Items[1]->Text!=Str)
+		MainForm->StatusBar->Panels->Items[1]->Text=Str;
+
+    sprintf(str,"Clients: %d",clients);
+    Str=AnsiString(str);
+    if(MainForm->StatusBar->Panels->Items[2]->Text!=Str)
+		MainForm->StatusBar->Panels->Items[2]->Text=Str;
+
+    sprintf(str,"Served: %d",total_clients);
+    Str=AnsiString(str);
+    if(MainForm->StatusBar->Panels->Items[3]->Text!=Str)
+		MainForm->StatusBar->Panels->Items[3]->Text=Str;
+
 #if 0
     THeapStatus hp=GetHeapStatus();
     sprintf(str,"Mem Used: %lu bytes",hp.TotalAllocated);
