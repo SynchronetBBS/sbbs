6da9aa927bead43c838a51a35f6c29dfbe2fb279
deuce <>
2009-11-05 04:27:21 +0000

    Numerous fixes... good enough for Thunderbird

--- exec/imapservice.js
+++ exec/imapservice.js
@@ -9,6 +9,7 @@
  */
 
 load("sbbsdefs.js");
+const RFC822HEADER = 0xb0;  // from smbdefs.h
 
 var sepchar="|";
 var debug=true;
@@ -53,6 +54,153 @@ function compNumbers(a,b) {
 	return(a-b);
 }
 
+function send_fetch_response(msgnum, format, uid)
+{
+	var idx=base.get_msg_index(msgnum);
+	var resp='';
+	var sent_uid=false;
+	var i,j;
+	var hdr;
+	var rfc822={};
+	var objtype;
+	var m;
+	var re;
+	var tmp;
+
+	function get_header() {
+		if(hdr == undefined)
+			hdr=base.get_msg_header(msgnum);
+	}
+
+	function get_rfc822_header() {
+		var content_type;
+		var i;
+
+		get_header();
+		if(rfc822.header==undefined) {
+			rfc822.header='';
+			rfc822.header += "To: "+hdr.to+"\r\n";
+			rfc822.header += "Subject: "+hdr.subject+"\r\n";
+			rfc822.header += "Message-ID: "+hdr.id+"\r\n";
+			rfc822.header += "Date: "+hdr.date+"\r\n";
+			if(!hdr.from_net_type || !hdr.from_net_addr)    /* local message */
+				rfc822.header += "From: " + hdr.from + " <" + hdr.from.replace(/ /g,".").toLowerCase() + "@" + system.inetaddr + ">\r\n";
+			else if(!hdr.from_net_addr.length)
+				rfc822.header += "From: "+hdr.from+">\r\n";
+			else if(hdr.from_net_addr.indexOf('@')!=-1)
+				rfc822.header += "From: "+hdr.from+" <"+hdr.from_net_addr+">\r\n";
+			else
+				rfc822.header += "From: "+hdr.from+" <"+hdr.from.replace(/ /g,".").toLowerCase()+"@"+hdr.from_net_addr+">\r\n";
+			if(hdr.field_list!=undefined) {
+				for(i in hdr.field_list) 
+					if(hdr.field_list[i].type==RFC822HEADER) {
+						if(hdr.field_list[i].data.toLowerCase().indexOf("content-type:")==0)
+							content_type = hdr.field_list[i].data;
+						rfc822.header += hdr.field_list[i].data+"\r\n";
+					}
+			}
+			if(content_type==undefined) {
+				/* No content-type specified, so assume IBM code-page 437 (full ex-ASCII) */
+				rfc822.header += "Content-Type: text/plain; charset=IBM437\r\n";
+				rfc822.header += "Content-Transfer-Encoding: 8bit\r\n";
+			}
+		}
+		rfc822.header += "\r\n";
+	}
+
+	function get_rfc822_text() {
+		if(rfc822.text==undefined)
+			rfc822.text=base.get_msg_body(msgnum, true, true, true);
+	}
+
+	function get_rfc822() {
+		get_rfc822_header();
+		get_rfc822_text();
+	}
+
+	function get_rfc822_size() {
+		get_rfc822();
+		rfc822.size=rfc822.header.length+rfc822.text.length;
+	}
+
+	resp=idx.offset + " FETCH (";
+	for(i in format) {
+		if(typeof(format[i])=='object') {
+			// We already handled this I hope...
+			if(objtype == undefined)
+				continue;
+			switch(objtype) {
+				case 'BODY[HEADER.FIELDS':
+				case 'BODY.PEEK[HEADER.FIELDS':
+					tmp='';
+					get_rfc822_header();
+					resp += objtype+" (";
+					for(j in format[i]) {
+						resp+=format[i][j]+" ";
+						re=new RegExp("^("+format[i][j]+":.*)$", "im");
+						m=re.exec(rfc822.header);
+						if(m!=null) {
+							tmp += m[1]+"\r\n";
+						}
+					}
+					tmp += "\r\n";
+					resp=resp.substr(0,resp.length-1)+")] {"+tmp.length+"}\r\n"+tmp+" ";
+					break;
+			}
+			continue;
+		}
+		switch(format[i].toUpperCase()) {
+			case 'FLAGS':
+				resp += "FLAGS () ";
+				break;
+			case 'UID':
+				resp += "UID "+idx.number+" ";
+				sent_uid=true;
+				break;
+			case 'INTERNALDATE':
+				get_header();
+				resp += 'INTERNALDATE '+strftime('"%d-%a-%C%y %H:%M:%S +0000" ', hdr.when_imported_time);
+				break;
+			case 'RFC822.SIZE':
+				get_rfc822_size();
+				resp += "RFC822.SIZE "+rfc822.size+" ";
+				break;
+			case 'RFC822.TEXT':
+			case 'BODY[TEXT]':
+				// Set "Seen" flag
+				// fall-through
+			case 'BODY.PEEK[TEXT]':
+				get_rfc822_text();
+				resp += format[i].replace(/\.PEEK/,"").toUpperCase()+" {"+rfc822.text.length+"}\r\n"+rfc822.text+" ";
+				break;
+			case 'BODY[HEADER]':
+			case 'RFC822.HEADER':
+				// Set "Seen" flag
+				// fall-through
+			case 'BODY.PEEK[HEADER]':
+				get_rfc822_header();
+				resp += format[i].replace(/\.PEEK/,"").toUpperCase()+" {"+rfc822.header.length+"}\r\n"+rfc822.header+" ";
+				break;
+			case 'RFC822':
+			case 'BODY[]':
+				// Set "Seen" flag
+				// fall-through
+			case 'BODY.PEEK[]':
+				get_rfc822();
+				resp += format[i].toUpperCase()+" {"+(rfc822.header.length+rfc822.text.length)+"}\r\n"+rfc822.header+rfc822.text+" ";
+				break;
+			case 'BODY[HEADER.FIELDS':
+			case 'BODY.PEEK[HEADER.FIELDS':
+				objtype=format[i].replace(/\.PEEK/,"").toUpperCase();
+				break;
+		}
+	}
+	if(uid && !sent_uid)
+		resp += "UID "+idx.number+" ";
+	resp = resp.substr(0, resp.length-1)+")";
+	untagged(resp);
+}
+
 /*
  * Parses a data items FETCH parameter for send_fetch_response()
  */
@@ -73,7 +221,7 @@ function parse_data_items(obj)
 				obj=[];
 		}
 	}
-
+	return(obj);
 }
 
 /*
@@ -124,112 +272,116 @@ function parse_seq_set(set, uid) {
 	return(response);
 }
 
-var line;
-function parse_line() {
-	var at_start=true;
-	var	in_quote=false;
-	var paren_depth=0;
-	var string_len;
-	var args=[];
-	var pos;
-
-	function parse_atom() {
-		var ret='';
-
-		while(line.length) {
-			switch(line.charAt(0)) {
-				case ')':
-					return(ret);
-				case ' ':
-					line=line.substr(1);
-					return(ret);
-				default:
-					ret += line.charAt(0);
-					line=line.substr(1);
+function parse_command(line)
+{
+	function execute_line(args) {
+		if(args.length >= 2) {
+			command=args[1].toUpperCase();
+			args.splice(1,1);
+			if(handle_command(command, args, any_state_command_handlers))
+				return;
+			switch(state) {
+				case UnAuthenticated:
+					if(handle_command(command, args, unauthenticated_command_handlers))
+						return;
+					break;
+				case Authenticated:
+					if(handle_command(command, args, authenticated_command_handlers))
+						return;
+					break;
+				case Selected:
+					if(handle_command(command, args, selected_command_handlers))
+						return;
 					break;
 			}
 		}
-		return(ret);
+		tagged(args[0], "BAD", "Bad dog, no cookie.");
 	}
 
-	function parse_string()
-	{
-		var ret='';
+	function parse_line() {
+		var at_start=true;
+		var	in_quote=false;
+		var paren_depth=0;
+		var string_len;
+		var args=[];
+		var pos;
 
-		line=line.replace(/^{([0-9]+)}$/, "$1");
-		client.socket.send("+\r\n");
-		ret=client.socket.recv(parseInt(line));
-		line=client.socket.recvline(1024, 300);
+		function parse_atom() {
+			var ret='';
 
-		return(ret);
-	}
+			while(line.length) {
+				switch(line.charAt(0)) {
+					case ')':
+						return(ret);
+					case ' ':
+						line=line.substr(1);
+						return(ret);
+					default:
+						ret += line.charAt(0);
+						line=line.substr(1);
+						break;
+				}
+			}
+			return(ret);
+		}
 
-	function parse_quotedstring() {
-		var ret='';
+		function parse_string()
+		{
+			var ret='';
 
-		line=line.substr(1);	// Remove leading "
+			line=line.replace(/^{([0-9]+)}$/, "$1");
+			client.socket.send("+ Give me more of that good stuff\r\n");
+			ret=client.socket.recv(parseInt(line));
+			line=client.socket.recvline(1024, 300);
+	
+			return(ret);
+		}
+
+		function parse_quotedstring() {
+			var ret='';
+	
+			line=line.substr(1);	// Remove leading "
+			while(line.length) {
+				switch(line.charAt(0)) {
+					case '"':
+						line=line.substr(1);
+						return(ret);
+					default:
+						ret += line.charAt(0);
+						line=line.substr(1);
+						break;
+				}
+			}
+			return(ret);
+		}
+
 		while(line.length) {
 			switch(line.charAt(0)) {
 				case '"':
+					args.push(parse_quotedstring());
+					break;
+				case ')':
 					line=line.substr(1);
-					return(ret);
+					return(args);
+				case '(':
+					line=line.substr(1);
+					args.push(parse_line());
+					break;
+				case '{':
+					args.push(parse_string());
+					break;
+				case ' ':
+					line=line.substr(1);
+					break;
 				default:
-					ret += line.charAt(0);
-					line=line.substr(1);
+					args.push(parse_atom());
 					break;
 			}
 		}
-		return(ret);
+		return(args);
 	}
 
-	while(line.length) {
-		switch(line.charAt(0)) {
-			case '"':
-				args.push(parse_quotedstring());
-				break;
-			case ')':
-				line=line.substr(1);
-				return(args);
-			case '(':
-				line=line.substr(1);
-				args.push(parse_line());
-				break;
-			case '{':
-				args.push(parse_string());
-				break;
-			case ' ':
-				line=line.substr(1);
-				break;
-			default:
-				args.push(parse_atom());
-				break;
-		}
-	}
-	return(args);
-}
-
-function execute_line(args) {
-	if(args.length >= 2) {
-		command=args[1].toUpperCase();
-		args.splice(1,1);
-		if(handle_command(command, args, any_state_command_handlers))
-			return;
-		switch(state) {
-			case UnAuthenticated:
-				if(handle_command(command, args, unauthenticated_command_handlers))
-					return;
-				break;
-			case Authenticated:
-				if(handle_command(command, args, authenticated_command_handlers))
-					return;
-				break;
-			case Selected:
-				if(handle_command(command, args, selected_command_handlers))
-					return;
-				break;
-		}
-	}
-	tagged(args[0], "BAD", "Bad dog, no cookie.");
+	return(execute_line(parse_line()));
 }
 
 // Global variables
@@ -333,11 +485,6 @@ function sendexists()
 	untagged(base.total_msgs+" EXISTS");
 }
 
-function sendrecent()
-{
-	untagged("0 RECENT");
-}
-
 function sublist(group, match, subscribed)
 {
 	var grp;
@@ -394,7 +541,7 @@ authenticated_command_handlers = {
 			}
 			sendflags(false);
 			untagged(base.total_msgs+" EXISTS");
-			untagged("0 RECENT");
+			untagged(0+" RECENT");
 			untagged("OK [UNSEEN "+(base.total_msgs+1)+"]");
 			sendflags(true);
 			untagged("OK [UIDNEXT "+(base.last_msg+1)+"]");
@@ -416,7 +563,7 @@ authenticated_command_handlers = {
 			}
 			sendflags(false);
 			untagged(base.total_msgs+" EXISTS");
-			untagged("0 RECENT");
+			untagged(0+" RECENT");
 			untagged("OK [UNSEEN "+(base.total_msgs+1)+"]");
 			sendflags(true);
 			untagged("OK [UIDNEXT "+(base.last_msg+1)+"]");
@@ -578,6 +725,13 @@ authenticated_command_handlers = {
 };
 
 selected_command_handlers = {
+	SELECT:{	// Outlook uses SELECT when selected...
+		arguments:1,
+		handler:function(args) {
+			base.close();
+			authenticated_command_handlers.SELECT.handler(args);
+		},
+	},
 	CHECK:{
 		arguments:0,
 		handler:function(args) {
@@ -623,10 +777,10 @@ selected_command_handlers = {
 			var i;
 
 			for(i in seq) {
-				send_fetch_response(seq[i], data_items);
+				send_fetch_response(seq[i], data_items, false);
 			}
-			//tagged(tag, "OK", "There they are!");
-			tagged(tag, "NO", "Can't fetch... I'm useless.");
+			tagged(tag, "OK", "There they are!");
+			//tagged(tag, "NO", "Can't fetch... I'm useless.");
 		},
 	},
 	STORE:{
@@ -649,8 +803,27 @@ selected_command_handlers = {
 		arguments:3,
 		handler:function(args) {
 			var tag=args[0];
+			var cmd=args[1];
+			var seq;
+			var data_items;
+			var i;
 
-			tagged(tag, "NO", "Help, I'm useless.");
+			switch(cmd.toUpperCase()) {
+				case 'FETCH':
+					if(args.length != 4) {
+						tagged(args[0], "BAD", "Bad dog, no UID cookie.");
+						return;
+					}
+					seq=parse_seq_set(args[2],true);
+					data_items=parse_data_items(args[3]);
+					for(i in seq) {
+						send_fetch_response(seq[i], data_items, true);
+					}
+					tagged(tag, "OK", "There they are (with UIDs)!");
+					break;
+				default:
+					tagged(tag, "NO", "Help, I'm useless.");
+			}
 		},
 	}
 };
@@ -661,8 +834,7 @@ while(1) {
 	line=client.socket.recvline(1024, 300);
 	if(line != null) {
 		debug_log("IMAP RECV: "+line);
-		args=parse_line();
-		execute_line(args);
+		parse_command(line);
 	}
 	else {
 		untagged("BYE No lolligaggers here!");
