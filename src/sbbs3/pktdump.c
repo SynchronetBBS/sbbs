254023e71cdb1d9c21a5fb9208af76664ea6795c
rswindell <>
2010-01-27 08:46:16 +0000

    Display the origination point number in the packet header for 2+ packet types.

--- src/sbbs3/pktdump.c
+++ src/sbbs3/pktdump.c
@@ -90,8 +90,10 @@ int pktdump(FILE* fp, const char* fname)
 		&& pkthdr.fill.two_plus.cword&1) {
 		fprintf(stdout,"2+");
 		dest.point=pkthdr.fill.two_plus.destpoint;
-		if(orig.net==0xffff)	/* see FSC-0048 for details */
+		if(pkthdr.fill.two_plus.origpoint!=0 && orig.net==0xffff) {	/* see FSC-0048 for details */
 			orig.net=pkthdr.fill.two_plus.auxnet;
+			orig.point=pkthdr.fill.two_plus.origpoint;
+		}
 	} else if(pkthdr.baud==2) {					/* Type 2.2 Packet Header */
 		fprintf(stdout,"2.2");
 		dest.point=pkthdr.month; 
