62e5f6b3ae211e68d779f13c78314073eeb12e0f
rswindell <>
2010-03-01 07:02:58 +0000

    Bug-fix: when setting the user.command_shell, was setting the user.host_name (computer) property instead.

--- src/sbbs3/js_user.c
+++ src/sbbs3/js_user.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -519,7 +519,7 @@ static JSBool js_user_set(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
 			putuserrec(p->cfg,p->user->number,U_XEDIT,0,str);
 			break;
 		case USER_PROP_SHELL: 	 
-			putuserrec(p->cfg,p->user->number,U_COMP,0,str);
+			putuserrec(p->cfg,p->user->number,U_SHELL,0,str);
 			break;
 		case USER_PROP_MISC:
 			JS_RESUMEREQUEST(cx, rc);
