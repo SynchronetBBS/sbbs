8733f932792a5a4ad1c834c63a2d7a0810fd762a
deuce <>
2010-03-05 02:29:48 +0000

    When a file is growing during transfer, continue to only update once per second.

--- src/syncterm/term.c
+++ src/syncterm/term.c
@@ -355,13 +355,16 @@ void zmodem_progress(void* cbdata, uint32_t current_pos)
 	time_t		l;
 	time_t		t;
 	time_t		now;
-	static time_t last_progress;
+	static time_t last_progress=0;
 	int			old_hold=hold_update;
 	struct zmodem_cbdata *zcb=(struct zmodem_cbdata *)cbdata;
 	zmodem_t*	zm=zcb->zm;
+	BOOL		growing=FALSE;
 
 	now=time(NULL);
-	if(now-last_progress>0 || current_pos >= zm->current_file_size) {
+	if(current_pos > zm->current_file_size)
+		growing=TRUE;
+	if(now != last_progress || (current_pos >= zm->current_file_size && growing==FALSE)) {
 		zmodem_check_abort(cbdata);
 		hold_update = TRUE;
 		window(((trans_ti.screenwidth-TRANSFER_WIN_WIDTH)/2)+2
