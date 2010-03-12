0e517a9e404fe7d76f0bdb6cc4933b3419e86807
rswindell <>
2010-03-12 18:23:24 +0000

    Add support for using wordwrap() in C++ source.

--- src/sbbs3/wordwrap.h
+++ src/sbbs3/wordwrap.h
@@ -4,7 +4,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -34,6 +34,14 @@
 #ifndef _WORDWRAP_H_
 #define _WORDWRAP_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 char* wordwrap(char* inbuf, int len, int oldlen, BOOL handle_quotes);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* Don't add anything after this line */
