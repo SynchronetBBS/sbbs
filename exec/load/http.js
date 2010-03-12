57583467e17914e70f793ec5cc6f2eb7f1657499
deuce <>
2010-03-12 19:51:10 +0000

    Fix generation of request.

--- exec/load/http.js
+++ exec/load/http.js
@@ -36,7 +36,7 @@ function HTTPRequest()
 			throw("Unknown scheme! '"+this.url.scheme+"'");
 		if(this.url.path=='')
 			this.url.path='/';
-		this.request="GET "+url.path+" HTTP/1.0";
+		this.request="GET "+this.url.path+" HTTP/1.0";
 		this.request_headers=[];
 		this.AddDefaultHeaders();
 	};
