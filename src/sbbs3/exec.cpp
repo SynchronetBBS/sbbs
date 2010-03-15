324a19c2850b8078908868eec11513d3b8572242
rswindell <>
2010-03-15 18:42:38 +0000

    Don't pass an uninitialized path to fexistcase().

--- src/sbbs3/exec.cpp
+++ src/sbbs3/exec.cpp
@@ -598,10 +598,11 @@ long sbbs_t::js_execfile(const char *cmd, const char* startup_dir)
 	}
 	fname=cmdline;
 
+	path[0]=0;
 	if(strcspn(fname,"/\\")==strlen(fname)) {
 		if(startup_dir!=NULL && *startup_dir)
 			SAFEPRINTF3(path,"%s%s%s",startup_dir,fname,js_ext(fname));
-		if(!fexistcase(path)) {
+		if(path[0]==0 || !fexistcase(path)) {
 			SAFEPRINTF3(path,"%s%s%s",cfg.mods_dir,fname,js_ext(fname));
 			if(cfg.mods_dir[0]==0 || !fexistcase(path))
 				SAFEPRINTF3(path,"%s%s%s",cfg.exec_dir,fname,js_ext(fname));
