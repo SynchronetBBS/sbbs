32ff65945094889fbe1a1d12838710ef25627b62
rswindell <>
2010-03-15 21:54:53 +0000

    Remove chunk of #if 0'd out code.

--- src/sbbs3/services.c
+++ src/sbbs3/services.c
@@ -654,66 +654,6 @@ js_ErrorReporter(JSContext *cx, const char *message, JSErrorReport *report)
 	JS_RESUMEREQUEST(cx, rc);
 }
 
-#if 0
-
-/* Server Object Properites */
-enum {
-	 SERVER_PROP_TERMINATED
-	,SERVER_PROP_CLIENTS
-};
-
-
-static JSBool js_server_get(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
-{
-    jsint       tiny;
-	service_client_t* client;
-
-	if((client=(service_client_t*)JS_GetContextPrivate(cx))==NULL)
-		return(JS_FALSE);
-
-    tiny = JSVAL_TO_INT(id);
-
-	switch(tiny) {
-		case SERVER_PROP_TERMINATED:
-#if 0
-			lprintf(LOG_DEBUG,"%s client->service->terminated=%d"
-				,client->service->protocol, client->service->terminated);
-#endif
-			*vp = BOOLEAN_TO_JSVAL(client->service->terminated);
-			break;
-		case SERVER_PROP_CLIENTS:
-			*vp = INT_TO_JSVAL(active_clients());
-			break;
-	}
-
-	return(JS_TRUE);
-}
-
-#define SERVER_PROP_FLAGS JSPROP_ENUMERATE|JSPROP_READONLY
-
-static struct JSPropertySpec js_server_properties[] = {
-/*		 name				,tinyid					,flags,				getter,	setter	*/
-
-	{	"terminated"		,SERVER_PROP_TERMINATED	,SERVER_PROP_FLAGS,	NULL,NULL},
-	{	"clients"			,SERVER_PROP_CLIENTS	,SERVER_PROP_FLAGS,	NULL,NULL},
-	{0}
-};
-
-static JSClass js_server_class = {
-         "Server"			/* name			*/
-		,0					/* flags		*/
-        ,JS_PropertyStub	/* addProperty	*/
-		,JS_PropertyStub	/* delProperty	*/
-		,js_server_get		/* getProperty	*/
-		,JS_PropertyStub	/* setProperty	*/
-		,JS_EnumerateStub	/* enumerate	*/
-		,JS_ResolveStub		/* resolve		*/
-		,JS_ConvertStub		/* convert		*/
-		,JS_FinalizeStub	/* finalize		*/
-}; 
-
-#endif
-
 /* Server Methods */
 
 static JSBool
