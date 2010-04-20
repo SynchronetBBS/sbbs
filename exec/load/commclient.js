fb08b578f1af22a163c527347129f2c99406c6ff
mcmlxxix <>
2010-04-20 15:08:36 +0000

    converted to use JSON

--- exec/load/commclient.js
+++ exec/load/commclient.js
@@ -7,252 +7,113 @@
 load("funclib.js");
 load("sbbsdefs.js");
 load("sockdefs.js");
-	
-const normal_scope=		"#";
-const global_scope=		"!";
-const hub_query=			"?";
-const priv_scope=			"%";
-const file_sync=			"@";
-const tries=				5;
+
+const QUERY=				"?";
+const LOCAL=				"&";
+const FILESYNC=			"@";
 const connection_timeout=	5;
+const connection_attempts=	2;
+const connection_interval=	5;
 
 function ServiceConnection(id)
 {
-	this.session_id=			(id?id:"default");
-	this.notices=				[];
-
-	var comminit=new File(system.ctrl_dir + "filesync.ini");
-	comminit.open('r');
-	var module=comminit.iniGetObject(this.session_id);
-	comminit.close();
-	
-	const working_dir=module.working_directory;
+	this.id=				(id?id:"default");
+	this.notices=			[];
+	this.sock=				false;
+	var attempts=0;
+	var last_attempt=0;
 	
 	//hub should point to the local, internal ip address or domain of the computer running commservice.js
 	//port should point to the port the gaming service is set to use in commservice.js
-	const hub=						"localhost";
-	const port=					10088;
+	const hub=					"localhost";
+	const port=				10088;
 	
-	var sock=new Socket();
 	this.init=function()
 	{
-		this.notices.push("Connecting to hub... ");
-		sock.connect(hub,port,connection_timeout);
-		if(sock.is_connected) 
-		{
-			sock.send("&" + this.session_id + "\r\n");
-			return true;
+		if((time()-last_attempt)<connection_interval || attempts>=connection_attempts) {
+			debug("error connecting to hub, exiting program",LOG_WARNING);
+			exit();
 		}
-		this.notices.push("Connection to " + hub + " failed...");
-		return false;
+		this.sock=new Socket();
+		this.sock.connect(hub,port,connection_timeout);
+		if(testSocket(this.sock)) {
+			var hello=new Object();
+			hello.context=LOCAL;
+			hello.alias=user.alias;
+			this.send(hello);
+			attempts=0;
+			last_attempt=0;
+			return true;
+		} else {
+			attempts++;
+			debug("error connecting to hub, attempt " + attempts,LOG_WARNING);
+			last_attempt=time();
+			return false;
+		}
 	}
 	this.getNotices=function()
 	{
-		if(this.notices.length) return this.notices;
-		return false;
+		var notices=[];
+		while(this.notices.length) {
+			notices.push(this.notices.shift());
+		}
+		return notices;
 	}
 	this.receive=function()
 	{
-		var data=0;
-		if(!sock.is_connected)
+		if(!testSocket(this.sock))
 		{
-			if(!this.init()) return -1;
+			if(!this.init()) return false;
 		}
-		if(sock.data_waiting)
+		if(this.sock.data_waiting)
 		{
-			var raw_data=sock.recvline(16384,connection_timeout);
-			data=js.eval(raw_data);
+			var raw_data=this.sock.recvline(1024,connection_timeout);
+			if(raw_data != null) return(js.eval(raw_data));
+			return false;
 		}
-		return data;
 	}
 	this.send=function(data)
 	{
-		if(!sock.is_connected)
+		if(!testSocket(this.sock))
 		{
 			if(!this.init()) return false;
 		}
-		if(!data.scope) 
-		{
-			log(LOG_DEBUG,"scope undefined");
-			return false;
-		}
-		var packet=data.scope + this.session_id + "" + data.toSource() + "\r\n";
-		sock.send(packet);
+		if(!data.id) data.id=this.id;
+		if(!data.system) data.system=system.name;
+		
+		this.sock.send(data.toSource() + "\r\n");
 		return true;
 	}
-	this.recvfile=function(remote_file)
+	this.recvfile=function(files,blocking)
 	{
-		if(!sock.is_connected)
-		{
-			if(!this.init()) return false;
-		}
-		var filesock=new Socket();
-		filesock.connect(hub,port,connection_timeout);
-		if(!filesock.is_connected) { 
-			log(LOG_DEBUG,"error connecting to service");
-			return false;
-		}
-		
-		filesock.send(file_sync + this.session_id + "#send" + file_getname(remote_file) + "\r\n");
-		while(filesock.is_connected)
-		{
-			var data=filesock.recvline(1024,connection_timeout);
-			if(data!=null)
-			{
-				log(LOG_DEBUG,data);
-				data=parse_query(data);
-				var response=data[1];
-				var file=data[2];
-				var date=data[3];
-				
-				switch(response)
-				{
-					case "#askrecv":
-						receive_file(filesock,this.session_id,file,date);
-						break;
-					case "#abort":
-						filesock.close();
-						break;
-					case "#endquery":
-						filesock.close();
-						break;
-					default:
-						filesock.close();
-						log(LOG_DEBUG,"unknown response: " + response);
-						break;
-				}
-			} else {
-				log(LOG_DEBUG,"transfer timed out: " + (file?file:remote_file));
-				filesock.close();
-			}
-		}
+		this.filesync(files,"recv",blocking);
 	}
-	this.sendfile=function(local_file)
+	this.sendfile=function(files,blocking)
 	{
-		if(!sock.is_connected)
-		{
-			if(!this.init()) return false;
-		}
+		this.filesync(files,"send",blocking);
+	}
+	this.filesync=function(filemask,command,blocking)
+	{
+		var q=new Object();
+		q.filemask=filemask;
+		q.command=command;
+		q.blocking=blocking;
+		q.context=FILESYNC;
 		
-		var filesock=new Socket();
-		filesock.connect(hub,port,connection_timeout);
-		if(!filesock.is_connected) { 
-			log(LOG_DEBUG,"error connecting to service");
-			return false;
+		this.send(q);
+		/*
+		if(blocking) {
+			var response=this.receive();
 		}
-		
-		var files=directory(working_dir + file_getname(local_file));
-		for(var f=0;f<files.length && filesock.is_connected;f++)
-		{
-			var filename=file_getname(files[f]);
-			var filedate=file_date(files[f]);
-
-			filesock.send(file_sync + this.session_id + "#askrecv" + filename + "" + filedate + "\r\n");
-			var data=filesock.recvline(1024,connection_timeout);
-			if(data!=null)
-			{
-				data=parse_query(data);
-				var response=data[1];
-				switch(response)
-				{
-					case "#ok":
-						log(LOG_DEBUG,"sending file: " + filename);
-						filesock.sendfile(files[f]);
-						filesock.send("#eof\r\n");
-						log(LOG_DEBUG,"file sent: " + filename);
-						break;
-					case "#skip":
-						log(LOG_DEBUG,"skipping file");
-						break;
-					case "#abort":
-						log(LOG_DEBUG,"aborting query");
-						return false;
-					default:
-						log(LOG_DEBUG,"unknown response: " + response);
-						filesock.send("#abort\r\n");
-						return false;
-				}
-			} else {
-				log(LOG_DEBUG,"query timed out");
-				break;
-			}
-		}
-		filesock.send("#endquery\r\n");
-		filesock.close();
-		return true;
+		*/
 	}
 	this.close=function()
 	{
-		log(LOG_DEBUG,"terminating service connection");
-		sock.close();
+		debug("terminating service connection",LOG_DEBUG);
+		this.sock.close();
 	}
 
-	function compare_dates(local,remote)
-	{
-		//will treat numbers with a difference of 1 or 0 as the same, due to issues with some file systems
-		if(Math.abs(local-remote)<=1) return true;
-		else return false;
-	}
-	function receive_file(socket,session_id,filename,filedate)
-	{
-		filename=working_dir+filename;
-		if(compare_dates(file_date(filename),filedate))
-		{
-			log(LOG_DEBUG,"skipping file: " + file_getname(filename));
-			socket.send(file_sync + session_id + "#skip\r\n");
-			return false;
-		}
-		
-		socket.send(file_sync + session_id + "#ok\r\n");
-		var file=new File(filename + ".tmp");
-		file.open('w',false);
-		log(LOG_DEBUG,"receiving file: " + file_getname(filename));
-		while(socket.is_connected)
-		{
-			var data=socket.recvline(1024,connection_timeout);
-			if(data!=null)
-			{
-				switch(data)
-				{
-					case "#abort":
-						log(LOG_DEBUG,"transfer aborted");
-						file.close();
-						file_remove(file.name);
-						return false;
-					case "#eof":
-						log(LOG_DEBUG,"file received: " + filename);
-						file.close();
-						if(file_exists(filename+".bck")) file_remove(filename+".bck");
-						file_rename(filename,filename+".bck");
-						file_rename(file.name,filename);
-						file_utime(filename,time(),filedate);
-						return true;
-					default:
-						file.writeln(data);
-						break;
-				}
-			} else {
-				log(LOG_DEBUG,"transfer timed out: " +  file_getname(filename));
-				file.close();
-				file_remove(file.name);
-				return false;
-			}
-		}
-		log(LOG_DEBUG,"socket connection error");
-		file.close();
-		file_remove(file.name);
-		return false;
-	}
-	function parse_query(query)
-	{
-		if(query) return query.split("");
-		return false;
-	}
-
-	if(!sock.is_connected)
-	{
-		if(!this.init()) return false;
-	}
+	if(!this.init() && attempts>=connection_attempts) return false;
 }
 
 	
