a74fd9314c614263a0901f5a91f5ead84abfc29d
mcmlxxix <>
2010-05-13 17:12:23 +0000

    Administrative IRC Bot module (functions and commands only)

---
+++ exec/ircbots/admin_functions.js
@@ -0,0 +1,34 @@
+// $Id$
+/*
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details:
+ http://www.gnu.org/licenses/gpl.txt
+
+ Copyright 2010 Randolph E. Sommerfeld <sysop@rrx.ca>
+
+*/
+
+/********** Command Processors. **********/
+this.Server_command=function(srv,cmdline,onick,ouh) {
+	var cmd=IRC_parsecommand(cmdline);
+	switch (cmd[0]) {
+		case "PRIVMSG":
+			if(srv.users[onick.toUpperCase()]) srv.users[onick.toUpperCase()].last_spoke=time();
+		default:
+			break;
+	}
+}
+
+//////////////////// Non-object Functions ////////////////////
+function login_user(usr) {
+	usr.connection = "IRC";
+	usr.logontime = time();
+}
