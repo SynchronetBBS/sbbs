dcb28804cb0887f1719a43b05c6866087481f6bc
cyan <>
2010-06-09 02:08:47 +0000

    * Account for kickers (This update brought to you by the music of the Crystal Method)

--- exec/load/cardlib.js
+++ exec/load/cardlib.js
@@ -118,9 +118,10 @@ function Hunt_Straight(cards,depth) {
 /* This function accepts any number of cards in as a true array */
 function Rank(cards) {
 	var hand=new Object();
-	hand.rank = 0;
+	hand.group = new Object();
+	hand.group[0] = 0; /* Rank - no value */
+	hand.rank = hand.group[0];
 	hand.str = "";
-	
 
 	/* First, count up the cards. */
 	var count = new Object();
@@ -202,11 +203,6 @@ function Rank(cards) {
 	else if (true_array_len(pair_types[2]) == 1)
 		hand.rank |= PAIR;
 
-	/* Consolidate "count" array and reverse. */
-	var consolidated=new Array;
-	for(c in count) consolidated.push(c);
-	hand.high=consolidated.reverse();	
-	
 	if (hand.rank&ROYAL_FLUSH)
 		hand.rank=RF;
 	else if (hand.rank&STRAIGHT_FLUSH)
@@ -228,8 +224,81 @@ function Rank(cards) {
 	else
 		hand.rank=HC;
 
-	if(hand.rank!=ST) hand.str=RANKS[hand.rank];
-	return hand; /* High Card */
+	if(hand.rank!=ST)
+		hand.str=RANKS[hand.rank];
+
+	/* Consolidate "count" array and reverse. 
+	   Only consolidate cards where count=1 */
+	var consolidated=new Array;
+	for(c in count) {
+		if (count[c]==1)
+			consolidated.push(c);
+	}
+	hand.high=consolidated.reverse();	
+	consolidated = consolidated.sort(sortNumber);
+
+	switch(hand.rank) {
+		case HC:
+		case FL: /* Yes, FL really gets treated like HC. */
+			hand.group[1] = consolidated.pop();
+			hand.group[2] = consolidated.pop();
+			hand.group[3] = consolidated.pop();
+			hand.group[4] = consolidated.pop();
+			hand.group[5] = consolidated.pop();
+			break;
+		case PR:
+			hand.group[1] = hunt_count(count, 2);
+			hand.group[2] = consolidated.pop();
+			hand.group[3] = consolidated.pop();
+			hand.group[4] = consolidated.pop();
+			break;
+		case TP:
+			var first_pair = hunt_count(count, 2);
+			delete count[first_pair];
+			var second_pair = hunt_count(count, 2);
+			if (first_pair > second_pair) {
+				hand.group[1] = first_pair;
+				hand.group[2] = second_pair;
+			} else {
+				hand.group[1] = second_pair;
+				hand.group[2] = first_pair;
+			}
+			hand.group[3] = consolidated.pop();
+			break;
+		case TK:
+			hand.group[1] = hunt_count(count, 3);
+			hand.group[2] = consolidated.pop();
+			break;
+		case ST:
+		case SF:
+			hand.group[1] = consolidated.pop();
+			break;
+		case FH:
+			hand.group[1] = hunt_count(count, 3);
+			hand.group[2] = hunt_count(count, 2);
+			hand.group[3] = consolidated.pop();
+			break;
+		case FK:
+			hand.group[1] = hunt_count(count, 4);
+			hand.group[2] = consolidated.pop();
+			break;
+		case RF:
+		default:
+			break;
+	}
+
+	return hand;
+}
+
+function hunt_count(cards, count) {
+	for (c in cards) {
+		if (cards[c] == count)
+			return c;
+	}
+}
+
+function sortNumber(a,b) {
+	return a - b;
 }
 
 function Sort(cards) {
