9b2cc301656cc81e67565a36f42182bdf046f933
rswindell <>
2010-06-15 22:54:35 +0000

    Minor optimization of fnopen() - removed unnecessary calls to strcpy().

--- src/sbbs3/nopen.c
+++ src/sbbs3/nopen.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -74,7 +74,7 @@ int nopen(const char* str, int access)
 /****************************************************************************/
 FILE* fnopen(int* fd, const char* str, int access)
 {
-	char	mode[128];
+	char*	mode;
 	int		file;
 	FILE *	stream;
 
@@ -86,19 +86,19 @@ FILE* fnopen(int* fd, const char* str, int access)
 
     if(access&O_APPEND) {
         if((access&O_RDWR)==O_RDWR)
-            strcpy(mode,"a+");
+            mode="a+";
         else
-            strcpy(mode,"a"); 
+            mode="a"; 
 	} else if(access&(O_TRUNC|O_WRONLY)) {
 		if((access&O_RDWR)==O_RDWR)
-			strcpy(mode,"w+");
+			mode="w+";
 		else
-			strcpy(mode,"w");
+			mode="w";
 	} else {
         if((access&O_RDWR)==O_RDWR)
-            strcpy(mode,"r+");
+            mode="r+";
         else
-            strcpy(mode,"r"); 
+            mode="r"; 
 	}
     stream=fdopen(file,mode);
     if(stream==NULL) {
