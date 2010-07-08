cbedf34ed3be6d45b90160bbad6c7b888ab76677
deuce <>
2010-07-08 02:36:46 +0000

    Attempt to work with bare hostnames

--- exec/ircbots/weather/weather_commands.js
+++ exec/ircbots/weather/weather_commands.js
@@ -25,8 +25,13 @@ Bot_Commands["WEATHER"].command = function (target,onick,ouh,srv,lvl,cmd) {
 	if(cmd.length==2) {
 		try {
 			if(typeof(srv.users[cmd[1].toUpperCase()]) == 'object') {
+				var userhost=srv.users[cmd[1].toUpperCase()].uh.replace(/^.*\@/,'');
+				// If the hostname is not a FQDN, use the server name and replace the first element...
+				if(userhost.indexOf('.')==-1)
+					userhost += (srv.users[cmd[1].toUpperCase()].servername.replace(/^[^\.]+\./,'.'));
+
 				var GeoIP;
-				var geoip_url='http://ipinfodb.com/ip_query2.php?ip='+srv.users[cmd[1].toUpperCase()].uh.replace(/^.*\@/,'')+'&timezone=false';
+				var geoip_url='http://ipinfodb.com/ip_query2.php?ip='+userhost+'&timezone=false';
 
 				GeoIP=new XML((new HTTPRequest().Get(geoip_url)).replace(/<\?.*\?>[\r\n\s]*/,''));
 				if(GeoIP.Location[0].Latitude.length()==1 && GeoIP.Location[0].Longitude.length()==1)
