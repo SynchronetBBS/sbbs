b55962603e903decb23ca25c60f77f3392025380
deuce <>
2010-07-09 19:07:28 +0000

    Support arrays as well (multiple lookups at once) and return a single result for a single non-array host lookup.

--- exec/load/geoip.js
+++ exec/load/geoip.js
@@ -9,43 +9,55 @@ function get_geoip(host, countryonly)
 	var geoip_url;
 	var result;
 	var tmpobj={};
+	var isarray=(typeof(host)=='object' && host instanceof Array);
 
 	/*
 	 * Check if this is an IP address or a hostname...
 	 * (We'll ignore weird encodings though like 12345.12345)
 	 */
-	m=host.match(/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/);
-	if(m!=null) {
-		if(m.length==5) {
-			for(i=1; i<5; i++) {
-				j=parseInt(m[i]);
-				if(j < 0 || j > 255) {
-					ishost=true;
-					break;
-				}
-			}	
+	if(!isarray) {
+		m=host.match(/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/);
+		if(m!=null) {
+			if(m.length==5) {
+				for(i=1; i<5; i++) {
+					j=parseInt(m[i]);
+					if(j < 0 || j > 255) {
+						ishost=true;
+						break;
+					}
+				}	
+			}
 		}
+		else
+			ishost=true;
 	}
-	else
-		ishost=true;
 
 	// Get the best URL
 	if(countryonly) {
-		if(ishost)
-			geoip_url='http://ipinfodb.com/ip_query_country2.php?output=json&ip='+encodeURIComponent(host);
+		if(isarray)
+			geoip_url='http://ipinfodb.com/ip_query2_country.php?output=json&ip='+encodeURIComponent(host.join(','));
+		else if(ishost)
+			geoip_url='http://ipinfodb.com/ip_query2_country.php?output=json&ip='+encodeURIComponent(host);
 		else
-			geoip_url='http://ipinfodb.com/ip_query_country.php?timezone=false&output=json&ip='+encodeURIComponent(host);
+			geoip_url='http://ipinfodb.com/ip_query_country.php?output=json&ip='+encodeURIComponent(host);
 	}
 	else {
-		if(ishost)
+		if(isarray)
+			geoip_url='http://ipinfodb.com/ip_query2.php?output=json&ip='+encodeURIComponent(host.join(','));
+		else if(ishost)
 			geoip_url='http://ipinfodb.com/ip_query2.php?output=json&ip='+encodeURIComponent(host);
 		else
-			geoip_url='http://ipinfodb.com/ip_query.php?timezone=false&output=json&ip='+encodeURIComponent(host);
+			geoip_url='http://ipinfodb.com/ip_query.php?output=json&ip='+encodeURIComponent(host);
 	}
 
 	try {
 		result='ret='+new HTTPRequest().Get(geoip_url);
 		GeoIP=js.eval(result);
+		if(GeoIP.Locations != undefined) {
+			if(isarray)
+				return GeoIP.Locations;
+			return GeoIP.Locations[0];
+		}
 		return GeoIP;
 	}
 	catch(e) {}
