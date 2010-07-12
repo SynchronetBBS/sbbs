fe4721ea1edccbea59c860f258cf50bb13d8b0d8
mcmlxxix <>
2010-07-12 15:08:58 +0000

    added forecast command, moved nick location to weather_functions.js

--- exec/ircbots/weather/weather_commands.js
+++ exec/ircbots/weather/weather_commands.js
@@ -1,31 +1,7 @@
-if(js.global.get_geoip==undefined)
-	js.global.load(js.global, "geoip.js");
-
 Bot_Commands["WEATHER"] = new Bot_Command(0,false,false);
 Bot_Commands["WEATHER"].command = function (target,onick,ouh,srv,lvl,cmd) {
 	var i;
 	var lstr;
-
-	function get_nicklocation(nick)
-	{
-		var ret,geo;
-
-		try {
-			var userhost=srv.users[nick.toUpperCase()].uh.replace(/^.*\@/,'');
-			// If the hostname is not a FQDN, use the server name and replace the first element...
-			if(userhost.indexOf('.')==-1)
-				userhost += (srv.users[cmd[1].toUpperCase()].servername.replace(/^[^\.]+\./,'.'));
-			geo=get_geoip(userhost);
-			ret=geo.Latitude+','+geo.Longitude;
-			if(ret=='0,0') {
-				userhost=srv.users[nick.toUpperCase()].servername
-				geo=get_geoip(userhost);
-				ret=geo.Latitude+','+geo.Longitude;
-			}
-			return ret;
-		}
-		catch(e) {}
-	}
 
 	// Remove empty cmd args
 	for(i=1; i<cmd.length; i++) {
@@ -36,9 +12,9 @@ Bot_Commands["WEATHER"].command = function (target,onick,ouh,srv,lvl,cmd) {
 	}
 
 	if (!cmd[1])
-		lstr=get_nicklocation(onick);
+		lstr=get_nicklocation(srv, onick);
 	if(cmd.length==2)
-		lstr=get_nicklocation(cmd[1]);
+		lstr=get_nicklocation(srv, cmd[1]);
 
 	var query = "";
 
@@ -55,7 +31,7 @@ Bot_Commands["WEATHER"].command = function (target,onick,ouh,srv,lvl,cmd) {
 
 	var weather_url = "http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=" + query;
 	var location_url = "http://api.wunderground.com/auto/wui/geo/GeoLookupXML/index.xml?query=" + query;
-
+	
 	try {
 		var Location = new XML((new HTTPRequest().Get(location_url)).replace(/<\?.*\?>[\r\n\s]*/,''));
 	}
@@ -81,6 +57,89 @@ Bot_Commands["WEATHER"].command = function (target,onick,ouh,srv,lvl,cmd) {
 			if(Weather.display_location.city != Weather.observation_location.city)
 				str += " (Observed at: " + Weather.observation_location.city + ")";
 			str += ' (Provided by Weather Underground, Inc.)';
+			srv.o(target, str);
+			break;
+		default:
+			srv.o(target, "Multiple matches for "+lstr);
+			if(Location.location.length() > 7)
+				srv.o(target, lstr+" matches "+Location.location.length()+" places... listing the first 7.");
+			var outstr = "";
+			var outcnt = 0;
+			for (i in Location.location) {
+				if (outcnt)
+					outstr += "; ";
+				outstr += Location.location[i].name;
+				outcnt++;
+				if (outcnt>=7)
+					break;
+			}
+			srv.o(target, lstr+': '+outstr+'.');
+			break;
+	}
+
+	return true;
+}
+
+Bot_Commands["FORECAST"] = new Bot_Command(0,false,false);
+Bot_Commands["FORECAST"].command = function (target,onick,ouh,srv,lvl,cmd) {
+	var i;
+	var lstr;
+
+	// Remove empty cmd args
+	for(i=1; i<cmd.length; i++) {
+		if(cmd[i].search(/^\s*$/)==0) {
+			cmd.splice(i,1);
+			i--;
+		}
+	}
+
+	if (!cmd[1])
+		lstr=get_nicklocation(srv, onick);
+	if(cmd.length==2)
+		lstr=get_nicklocation(srv, cmd[1]);
+
+	var query = "";
+
+	cmd.shift();
+	if (!lstr) {
+		var usr = new User(system.matchuser(cmd[1]));
+
+		if (typeof(usr)=='object')
+			lstr = usr.location;
+	}
+	if (!lstr)
+		lstr = cmd.join(' ');
+	query = encodeURIComponent(lstr);
+
+	var location_url = "http://api.wunderground.com/auto/wui/geo/GeoLookupXML/index.xml?query=" + query;
+	var forecast_url = "http://api.wunderground.com/auto/wui/geo/ForecastXML/index.xml?query=" + query;
+	
+	try {
+		var Location = new XML((new HTTPRequest().Get(location_url)).replace(/<\?.*\?>[\r\n\s]*/,''));
+	}
+	catch(e) {
+		srv.o(target, "Unable to resolve location "+lstr);
+		return true;
+	}
+
+	switch(Location.location.length()) {
+		case 0:
+			if(Location.nearby_weather_stations.length()==0) {
+				srv.o(target, "Unable to locate "+lstr);
+				break;
+			}
+			// Fall-through
+		case 1:
+			var Forecast = new XML((new HTTPRequest().Get(forecast_url)).replace(/<\?.*\?>[\r\n\s]*/,''));
+
+			var str = Location.city + ", " + Location.state + " -";
+			
+			for(fd in Forecast.txt_forecast.forecastday) {
+				var fcast=Forecast.txt_forecast.forecastday[fd];
+				str += " " + fcast.title + ": " + fcast.fcttext;
+			}
+			str += ' (Provided by Weather Underground, Inc.)';
+			
 			srv.o(target, str);
 			break;
 		default:
---
+++ exec/ircbots/weather/weather_functions.js
@@ -0,0 +1,25 @@
+if(js.global.get_geoip==undefined)
+	js.global.load(js.global, "geoip.js");
+
+function get_nicklocation(srv,nick)
+{
+	var ret,geo;
+
+	try {
+		var userhost=srv.users[nick.toUpperCase()].uh.replace(/^.*\@/,'');
+		// If the hostname is not a FQDN, use the server name and replace the first element...
+		if(userhost.indexOf('.')==-1)
+			userhost += (srv.users[cmd[1].toUpperCase()].servername.replace(/^[^\.]+\./,'.'));
+		geo=get_geoip(userhost);
+		ret=geo.Latitude+','+geo.Longitude;
+		if(ret=='0,0') {
+			userhost=srv.users[nick.toUpperCase()].servername
+			geo=get_geoip(userhost);
+			ret=geo.Latitude+','+geo.Longitude;
+		}
+		return ret;
+	}
+	catch(e) {
+		log("ERROR: " + e);
+	}
+}
