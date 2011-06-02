a074a5fea5fa52e22035c751ed8507f314793c22
deuce <>
2011-06-02 01:42:05 +0000

    Move parse failure test into ten retry loop.

--- exec/load/geoip.js
+++ exec/load/geoip.js
@@ -60,10 +60,10 @@ function get_geoip(host, countryonly)
 	}
 	for(i=0; i<10; i++) {
 		try {
-			do {
-				result='ret='+new HTTPRequest().Get(geoip_url);
-				GeoIP=js.eval(result);
-			} while(GeoIP==undefined);
+			result='ret='+new HTTPRequest().Get(geoip_url);
+			GeoIP=js.eval(result);
+			if(GeoIP==undefined)
+				continue;
 			if(GeoIP.Locations != undefined) {
 				if(isarray)
 					return GeoIP.Locations;
