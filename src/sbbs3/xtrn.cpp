e47b9e3716b83c35247b1dc3b184fcd08344170b
rswindell <>
2011-07-13 11:29:51 +0000

    Fix C++Builder 2010 compile error (apparently DWORD is an int rather than a long?).

--- src/sbbs3/xtrn.cpp
+++ src/sbbs3/xtrn.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -386,7 +386,7 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
     PROCESS_INFORMATION process_info;
 	DWORD	hVM;
 	unsigned long	rd;
-    DWORD	wr;
+    unsigned long	wr;
     unsigned long	len;
     DWORD	avail;
 	unsigned long	dummy;
