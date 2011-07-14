544c39d5d2705e219e942a90cc2ef7f381889f89
rswindell <>
2011-07-14 03:03:48 +0000

    Eliminate duplicate definition of ILLEGAL_FILENAME_CHARS macro. Increment copyright date to 2011.

--- src/sbbs3/ctrl/ConfigWizardUnit.cpp
+++ src/sbbs3/ctrl/ConfigWizardUnit.cpp
@@ -6,7 +6,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -47,7 +47,6 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 
-#define ILLEGAL_FILENAME_CHARS	"\\/|<>+[]:=\";,%"
 #define ILLEGAL_QWKID_CHARS  "*?" ILLEGAL_FILENAME_CHARS
 
 char ctrl_dir[MAX_PATH+1];
