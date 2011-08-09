d110c592b215a0aa4802bd097dc3f4dc739a5f9a
mcmlxxix <>
2011-08-09 13:43:37 +0000

    move Form() object inside formEdit() scope. support recursive form editing

--- exec/load/form.js
+++ exec/load/form.js
@@ -26,6 +26,106 @@ if(js.global.Layout == undefined)
 	
 function formEdit(obj)
 {
+	function Form(obj) 
+	{
+		this.len = 0;
+		this.max = 25;
+		this.index = 0;
+		this.items = [];
+		
+		for(var k in obj) {
+			if(console.strlen(k) > this.len)
+				this.len = console.strlen(k);
+			this.items.push({
+				key:k,
+				value:obj[k],
+				type:typeof obj[k]
+			});
+		}
+		
+		this.box = new Layout_View(
+			printPadded("KEY",this.len+2) + splitPadded("VALUE","ESC",this.max),
+			(console.screen_columns / 2) - ((this.len + 25)/2) -1,
+			(console.screen_rows / 2) - (this.items.length / 2) -2,
+			this.len + this.max + 4,
+			this.items.length + 3
+		);
+		
+		this.box.show_tabs = false;
+		
+		this.edit = function() {
+			var item = this.items[this.index];
+			
+			if(item.type == "object") {
+				item.value = formEdit(item.value);
+				this.draw();
+			}
+			else {
+				var coords = this.coords(this.index);
+				console.attributes = BG_LIGHTGRAY + BLACK;
+				console.gotoxy(coords.x + this.len + 2, coords.y);
+				clearLine(this.max);
+				console.gotoxy(coords.x + this.len + 2, coords.y);
+				
+				var newvalue = console.getstr(item.value.toString(),this.max,K_EDIT|K_AUTODEL);
+				switch(item.type) {
+					case "number":
+						item.value = Number(newvalue);
+						break;
+					case "function":
+						item.value = eval(newvalue);
+						break;
+					case "boolean":
+						item.value = Bool(newvalue);
+						break;
+					case "string":
+						item.value = newvalue;
+						break;
+				}
+				this.drawItem(this.index);
+			}
+		}
+		
+		this.draw = function() {
+			this.box.drawView();
+			
+			for(var i = 0; i < this.items.length; i++) {
+				this.drawItem(i,this.index == i);
+			}
+		}
+		
+		this.object getter = function() {
+			var obj = {};
+			for(var i = 0; i < this.items.length; i++) {
+				var item = this.items[i];
+				obj[item.key] = item.value;
+			}
+			return obj;
+		}
+		
+		this.coords = function(i) {
+			return {
+				x:this.box.x + 1,
+				y:this.box.y + 2 + i,
+			}
+		}
+		
+		this.drawItem = function(i,curr) {
+			var coords = this.coords(i);
+			console.gotoxy(coords);
+			if(curr) 
+				console.putmsg(getColor(layout_settings.lbg) + getColor(layout_settings.lfg) + 
+					printPadded(this.items[i].key,this.len + 2) + "\1w" + 
+					printPadded(this.items[i].value,this.max)
+				);
+			else
+				console.putmsg(getColor(layout_settings.vbg) + getColor(layout_settings.vfg) + 
+					printPadded(this.items[i].key,this.len + 2) + "\1h" +
+					printPadded(this.items[i].value,this.max)
+				);
+		}
+	}
+
 	var form = new Form(obj);
 	var update = false;
 	form.draw();
@@ -64,83 +164,8 @@ function formEdit(obj)
 				break;
 		}
 	}
+	
 	/* just in case?? */
 	return form.object;
 }
 
-function Form(obj) 
-{
-	this.len = 0;
-	this.max = 25;
-	this.index = 0;
-	this.items = [];
-	
-	for(var k in obj) {
-		if(console.strlen(k) > this.len)
-			this.len = console.strlen(k);
-		this.items.push({
-			key:k,
-			value:obj[k]
-		});
-	}
-	
-	this.box = new Layout_View(
-		printPadded("KEY",this.len+2) + splitPadded("VALUE","ESC",this.max),
-		(console.screen_columns / 2) - ((this.len + 25)/2) -1,
-		(console.screen_rows / 2) - (this.items.length / 2) -2,
-		this.len + this.max + 4,
-		this.items.length + 3
-	);
-	
-	this.box.show_tabs = false;
-	
-	this.edit = function() {
-		var item = this.items[this.index];
-		var coords = this.coords(this.index);
-		console.attributes = BG_LIGHTGRAY + BLACK;
-		console.gotoxy(coords.x + this.len + 2, coords.y);
-		clearLine(this.max);
-		console.gotoxy(coords.x + this.len + 2, coords.y);
-		item.value = console.getstr(item.value.toString(),this.max,K_EDIT|K_AUTODEL);
-		this.drawItem(this.index);
-	}
-	
-	this.draw = function() {
-		this.box.drawView();
-		
-		for(var i = 0; i < this.items.length; i++) {
-			this.drawItem(i,this.index == i);
-		}
-	}
-	
-	this.object getter = function() {
-		var obj = {};
-		for(var i = 0; i < this.items.length; i++) {
-			var item = this.items[i];
-			obj[item.key] = item.value;
-		}
-		return obj;
-	}
-	
-	this.coords = function(i) {
-		return {
-			x:this.box.x + 1,
-			y:this.box.y + 2 + i,
-		}
-	}
-	
-	this.drawItem = function(i,curr) {
-		var coords = this.coords(i);
-		console.gotoxy(coords);
-		if(curr) 
-			console.putmsg(getColor(layout_settings.lbg) + getColor(layout_settings.lfg) + 
-				printPadded(this.items[i].key,this.len + 2) + "\1w" + 
-				printPadded(this.items[i].value,this.max)
-			);
-		else
-			console.putmsg(getColor(layout_settings.vbg) + getColor(layout_settings.vfg) + 
-				printPadded(this.items[i].key,this.len + 2) + "\1h" +
-				printPadded(this.items[i].value,this.max)
-			);
-	}
-}
