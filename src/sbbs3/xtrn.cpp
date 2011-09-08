073d4c7eb87c75ab78258da6aee5312806fa3e0c
deuce <>
2011-09-08 02:11:07 +0000

    Move TTYDEFCHARS cefinition to the top of the file because sbbs.h includes termios.h now via comio.h

--- src/sbbs3/xtrn.cpp
+++ src/sbbs3/xtrn.cpp
@@ -35,6 +35,9 @@
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
 
+#ifdef __unix__
+	#define TTYDEFCHARS		// needed for ttydefchars definition
+#endif
 #include "sbbs.h"
 #include "cmdshell.h"
 #include "telnet.h"
@@ -43,8 +46,6 @@
 
 #ifdef __unix__
 	#include <sys/wait.h>	// WEXITSTATUS
-
-	#define TTYDEFCHARS		// needed for ttydefchars definition
 
 #if defined(__FreeBSD__)
 	#include <libutil.h>	// forkpty()
