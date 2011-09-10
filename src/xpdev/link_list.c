629e5cf4e00da219d4f2266b98ae748695423dba
rswindell <>
2011-09-10 01:27:01 +0000

    Fix bug in listLock and listUnlock where pthread_mutex_lock/unlock was being called for non-mutex-protected lists causing exceptions on Windows.

--- src/xpdev/link_list.c
+++ src/xpdev/link_list.c
@@ -239,7 +239,7 @@ BOOL DLLCALL listLock(link_list_t* list)
 	if(list==NULL)
 		return(FALSE);
 #if defined(LINK_LIST_THREADSAFE)
-	if((ret=pthread_mutex_lock(&list->mutex))==0)
+	if((list->flags&LINK_LIST_MUTEX) && (ret=pthread_mutex_lock(&list->mutex))==0)
 #endif
 		list->locks++;
 	return(ret);
@@ -259,7 +259,7 @@ BOOL DLLCALL listUnlock(link_list_t* list)
 	if(list==NULL)
 		return(FALSE);
 #if defined(LINK_LIST_THREADSAFE)
-	if((ret=pthread_mutex_unlock(&list->mutex))==0)
+	if((list->flags&LINK_LIST_MUTEX) && (ret=pthread_mutex_unlock(&list->mutex))==0)
 #endif
 		list->locks--;
 	return(ret);
