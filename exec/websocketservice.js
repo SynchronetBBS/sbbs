fd5c1cb22cb46eefe42471b74c8c7ac1e733a11f
ree <>
2011-09-29 13:43:35 +0000

    Implemented version 7 of the websocket protocol.  This adds support for Firefox 6+, Chrome 13+ and IE 10+

--- exec/websocketservice.js
+++ exec/websocketservice.js
@@ -24,22 +24,28 @@ load("nodedefs.js");
 load("sockdefs.js");
 load("sbbsdefs.js");
 load("ftelnethelper.js");
+load("sha1.js");
 
 // Global constants
-const DEBUG                         = false;
 const TELNET_DATA                   = 0;
 const TELNET_IAC                    = 1;
 const TELNET_SUBNEGOTIATE           = 2;
 const TELNET_SUBNEGOTIATE_IAC       = 3;
 const TELNET_WWDD                   = 4;
-const WEBSOCKET_DATA                = 0;
-const WEBSOCKET_NEED_PACKET_START   = 1;
+const WEBSOCKET_NEED_PACKET_START   = 0;
+const WEBSOCKET_NEED_PAYLOAD_LENGTH = 1;
+const WEBSOCKET_NEED_MASKING_KEY	= 2;
+const WEBSOCKET_DATA                = 3;
 
 // Global variables
+var FFrameMask = [];
+var FFrameOpCode = 0;
+var FFramePayloadLength = 0;
+var FFramePayloadReceived = 0;
 var FServerSocket = null;
 var FTelnetState = TELNET_DATA;
+var FWebSocketHeader = [];
 var FWebSocketState = WEBSOCKET_NEED_PACKET_START;
-var FWebSocketVersion = 0; // The draft/version number of the websocket client
 
 // Main line
 try {
@@ -80,6 +86,8 @@ try {
                     yield();
                 }
             }
+            if (!client.socket.is_connected) log(LOG_DEBUG, 'Client socket no longer connected');
+			if (!FServerSocket.is_connected) log(LOG_DEBUG, 'Server socket no longer connected');
         } else {
             // FServerSocket.connect() failed
             log(LOG_ERR, "Unable to connect to telnet server");
@@ -191,8 +199,11 @@ function GetFromTelnetServer() {
 }
 
 function GetFromWebSocketClient() {
-    switch (FWebSocketVersion) {
+    switch (FWebSocketHeader['Version']) {
         case 0: return GetFromWebSocketClientDraft0();
+		case 7: 
+		case 8: 
+			return GetFromWebSocketClientVersion7();
     }
 }
 
@@ -207,6 +218,12 @@ function GetFromWebSocketClientDraft0() {
         
         // Check what the client packet state is
         switch (FWebSocketState) {
+            case WEBSOCKET_NEED_PACKET_START:
+                // Check for 0x00 to indicate the start of a data packet
+                if (InByte === 0x00) {
+                    FWebSocketState = WEBSOCKET_DATA;
+                }
+                break;
             case WEBSOCKET_DATA:
                 // We're in a data packet, so check for 0xFF, which indicates the data packet is done
                 if (InByte == 0xFF) {
@@ -227,15 +244,69 @@ function GetFromWebSocketClientDraft0() {
                     }
                 }
                 break;
-            case WEBSOCKET_NEED_PACKET_START:
-                // Check for 0x00 to indicate the start of a data packet
-                if (InByte === 0x00) {
-                    FWebSocketState = WEBSOCKET_DATA;
-                }
-                break;
         }
     }
     
+    return Result;
+}
+
+function GetFromWebSocketClientVersion7() {
+	var Result = [];
+	var InByte = 0;
+	var InByte2 = 0;
+	var InByte3 = 0;
+
+    while (client.socket.data_waiting) {
+        // Check what the client packet state is
+        switch (FWebSocketState) {
+            case WEBSOCKET_NEED_PACKET_START:
+				// Next byte will give us the opcode, and also tell is if the message is fragmented
+				FFrameMask = [];
+				FFrameOpCode = client.socket.recvBin(1);
+				FFramePayloadLength = 0;
+				FFramePayloadReceived = 0;
+				FWebSocketState = WEBSOCKET_NEED_PAYLOAD_LENGTH;
+                break;
+            case WEBSOCKET_NEED_PAYLOAD_LENGTH:
+				FFramePayloadLength = (client.socket.recvBin(1) & 0x7F);
+				if (FFramePayloadLength === 126) {
+					FFramePayloadLength = client.socket.recvBin(2);
+				} else if (FFramePayloadLength === 127) {
+					FFramePayloadLength = client.socket.recvBin(8);
+				}
+				FWebSocketState = WEBSOCKET_NEED_MASKING_KEY;
+				break;
+            case WEBSOCKET_NEED_MASKING_KEY:
+				InByte = client.socket.recvBin(4);
+				FFrameMask[0] = (InByte & 0xFF000000) >> 24;
+				FFrameMask[1] = (InByte & 0x00FF0000) >> 16;
+				FFrameMask[2] = (InByte & 0x0000FF00) >> 8;
+				FFrameMask[3] = InByte & 0x000000FF;
+				FWebSocketState = WEBSOCKET_DATA;
+				break;
+            case WEBSOCKET_DATA:
+				InByte = (client.socket.recvBin(1) ^ FFrameMask[FFramePayloadReceived++ % 4]);
+
+				// Check if the byte needs to be UTF-8 decoded
+				if (InByte < 128) {
+					Result.push(InByte);
+				} else if ((InByte > 191) && (InByte < 224)) {
+					// Handle UTF-8 decode
+					InByte2 = (client.socket.recvBin(1) ^ FFrameMask[FFramePayloadReceived++ % 4]);
+					Result.push(((InByte & 31) << 6) | (InByte2 & 63));
+				} else {
+					// Handle UTF-8 decode (should never need this, but included anyway)
+					InByte2 = (client.socket.recvBin(1) ^ FFrameMask[FFramePayloadReceived++ % 4]);
+					InByte3 = (client.socket.recvBin(1) ^ FFrameMask[FFramePayloadReceived++ % 4]);
+					Result.push(((InByte & 15) << 12) | ((InByte2 & 63) << 6) | (InByte3 & 63));
+				}
+
+				// Check if we've received the full payload
+				if (FFramePayloadReceived === FFramePayloadLength) FWebSocketState = WEBSOCKET_NEED_PACKET_START;
+                break;
+        }
+    }
+
     return Result;
 }
 
@@ -251,8 +322,14 @@ function SendToTelnetServer(AData) {
 }
 
 function SendToWebSocketClient(AData) {
-    switch (FWebSocketVersion) {
-        case 0: SendToWebSocketClientDraft0(AData);
+    switch (FWebSocketHeader['Version']) {
+        case 0: 
+			SendToWebSocketClientDraft0(AData); 
+			break;
+		case 7: 
+		case 8: 
+			SendToWebSocketClientVersion7(AData); 
+			break;
     }
 }
 
@@ -275,16 +352,42 @@ function SendToWebSocketClientDraft0(AData) {
     client.socket.sendBin(0xFF, 1);
 }
 
-// TODO Only supports Draft 0, should be updated to handle future drafts
+function SendToWebSocketClientVersion7(AData) {
+	if (AData.length > 0) {
+		var ToSend = [];
+
+    	for (var i = 0; i < AData.length; i++) {
+	        // Check if the byte needs to be UTF-8 encoded
+	        if ((AData[i] & 0xFF) <= 127) {
+				ToSend.push(AData[i]);
+	        } else if (((AData[i] & 0xFF) >= 128) && ((AData[i] & 0xFF) <= 2047)) {
+	            // Handle UTF-8 encode
+	            ToSend.push((AData[i] >> 6) | 192);
+	            ToSend.push((AData[i] & 63) | 128);
+	        } else {
+				log(LOG_ERR, "Byte out of range: " + AData[i]);
+			}
+	    }
+
+		client.socket.sendBin(0x81, 1);
+		if (ToSend.length <= 125) {
+			client.socket.sendBin(ToSend.length, 1);
+		} else if (ToSend.length <= 65535) {
+			client.socket.sendBin(126, 1);
+			client.socket.sendBin(ToSend.length, 2);
+		} else {
+			client.socket.sendBin(127, 1);
+			client.socket.sendBin(ToSend.length, 8);
+		}
+		for (var i = 0; i < ToSend.length; i++) {
+			client.socket.sendBin(ToSend[i] & 0xFF, 1);
+		}
+	}
+}
+
 function ShakeHands() {
-    // Data we need for the reply
-    var Key1 = 0;
-    var Key2 = 0;
-    var Host = "";
-    var Origin = "";
-    var Path = "";
-    var SubProtocol = ""; // Optional
-    
+	FWebSocketHeader['Version'] = 0;
+
     try {
         // Keep reading header data until we get all the data we want
         while (true) {
@@ -295,94 +398,163 @@ function ShakeHands() {
                 return false;
             }
 
-            if (DEBUG) log(LOG_DEBUG, "Handshake Line: " + InLine);
+            log(LOG_DEBUG, "Handshake Line: " + InLine);
             
             // Check for blank line (indicates we have most of the header, and only the last 8 bytes remain
             if (InLine === "") {
-                // Ensure we have all the data we need
-                if ((Key1 !== 0) && (Key2 !== 0) && (Host !== "") && (Origin !== "") && (Path !== "")) {
-                    // Combine Key1, Key2, and the last 8 bytes into a string that we will later hash
-                    var ToHash = ""
-                    ToHash += String.fromCharCode((Key1 & 0xFF000000) >> 24);
-                    ToHash += String.fromCharCode((Key1 & 0x00FF0000) >> 16);
-                    ToHash += String.fromCharCode((Key1 & 0x0000FF00) >> 8);
-                    ToHash += String.fromCharCode((Key1 & 0x000000FF) >> 0);
-                    ToHash += String.fromCharCode((Key2 & 0xFF000000) >> 24);
-                    ToHash += String.fromCharCode((Key2 & 0x00FF0000) >> 16);
-                    ToHash += String.fromCharCode((Key2 & 0x0000FF00) >> 8);
-                    ToHash += String.fromCharCode((Key2 & 0x000000FF) >> 0);				
-                    for (var i = 0; i < 8; i++) {
-                        ToHash += String.fromCharCode(client.socket.recvBin(1));
-                    }
-                    
-                    // Hash the string
-                    var Hashed = md5_calc(ToHash, true);
-
-                    // Setup the handshake response
-                    var Response = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" +
-                                   "Upgrade: WebSocket\r\n" +
-                                   "Connection: Upgrade\r\n" +
-                                   "Sec-WebSocket-Origin: " + Origin + "\r\n" +
-                                   "Sec-WebSocket-Location: ws://" + Host + Path + "\r\n";
-                    if (SubProtocol !== "") Response += "Sec-WebSocket-Protocol: " + SubProtocol + "\r\n";
-                    Response += "\r\n";
-                    
-                    // Loop through the hash string (which is hex encoded) and append the individual bytes to the response
-                    for (var i = 0; i < Hashed.length; i += 2) {
-                        Response += String.fromCharCode(parseInt(Hashed.charAt(i) + Hashed.charAt(i + 1), 16));
-                    }
-                    
-                    // Send the response and return
-                    client.socket.send(Response);
-                    return true;
-                } else {
-                    // We're missing some pice of data, log what we do have
-                    log(LOG_ERR, "Missing some piece of handshake data.  Here's what we have:");
-                    log(LOG_ERR, Key1);
-                    log(LOG_ERR, Key2);
-                    log(LOG_ERR, Host);
-                    log(LOG_ERR, Origin);
-                    log(LOG_ERR, Path);
-                    log(LOG_ERR, SubProtocol);
-                    return false;
-                }
-                break;
+    			switch (FWebSocketHeader['Version']) {
+			        case 0: 
+						return ShakeHandsDraft0();
+					case 7: 
+					case 8: 
+						return ShakeHandsVersion7();
+					default: 
+						//		    TODO If this version does not
+						//          match a version understood by the server, the server MUST
+						//          abort the websocket handshake described in this section and
+						//          instead send an appropriate HTTP error code (such as 426
+						//          Upgrade Required), and a |Sec-WebSocket-Version| header
+						//          indicating the version(s) the server is capable of
+						//          understanding.
+						break;
+			    }
+				break;
+            } else if (InLine.indexOf("Connection:") === 0) {
+                // Example: "Connection: Upgrade"
+                FWebSocketHeader['Connection'] = InLine.replace(/Connection:\s?/i, "");
             } else if (InLine.indexOf("GET") === 0) {
                 // Example: "GET /demo HTTP/1.1"
                 var GET = InLine.split(" ");
-                Path = GET[1];
+                FWebSocketHeader['Path'] = GET[1];
             } else if (InLine.indexOf("Host:") === 0) {
                 // Example: "Host: example.com"
-                Host = InLine.replace(/Host:\s?/i, "");
+                FWebSocketHeader['Host'] = InLine.replace(/Host:\s?/i, "");
             } else if (InLine.indexOf("Origin:") === 0) {
                 // Example: "Origin: http://example.com"
-                Origin = InLine.replace(/Origin:\s?/i, "");
+                FWebSocketHeader['Origin'] = InLine.replace(/Origin:\s?/i, "");
+            } else if (InLine.indexOf("Sec-WebSocket-Key:") === 0) {
+                // Example: "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ=="
+                FWebSocketHeader['Key'] = InLine.replace(/Sec-WebSocket-Key:\s?/i, "");
             } else if (InLine.indexOf("Sec-WebSocket-Key1:") === 0) {
                 // Example: "Sec-WebSocket-Key1: 4 @1  46546xW%0l 1 5"
-                Key1 = CalculateWebSocketKey(InLine.replace(/Sec-WebSocket-Key1:\s?/i, ""));
+                FWebSocketHeader['Key1'] = CalculateWebSocketKey(InLine.replace(/Sec-WebSocket-Key1:\s?/i, ""));
             } else if (InLine.indexOf("Sec-WebSocket-Key2:") === 0) {
                 // Example: "Sec-WebSocket-Key2: 12998 5 Y3 1  .P00"
-                Key2 = CalculateWebSocketKey(InLine.replace(/Sec-WebSocket-Key2:\s?/i, ""));
+                FWebSocketHeader['Key2'] = CalculateWebSocketKey(InLine.replace(/Sec-WebSocket-Key2:\s?/i, ""));
+            } else if (InLine.indexOf("Sec-WebSocket-Origin:") === 0) {
+                // Example: "Sec-WebSocket-Origin: http://example.com"
+                FWebSocketHeader['Origin'] = InLine.replace(/Sec-WebSocket-Origin:\s?/i, "");
             } else if (InLine.indexOf("Sec-WebSocket-Protocol:") === 0) {
                 // Example: "Sec-WebSocket-Protocol: sample"
-                SubProtocol = InLine.replace(/Sec-WebSocket-Protocol:\s?/i, "");
+                FWebSocketHeader['SubProtocol'] = InLine.replace(/Sec-WebSocket-Protocol:\s?/i, "");
             } else if (InLine.indexOf("Sec-WebSocket-Draft") === 0) {
+				// Example: "Sec-WebSocket-Draft: 2"
                 try {
-                    FWebSocketVersion = parseInt(InLine.replace(/Sec-WebSocket-Draft:\s?/i, ""));
+                    FWebSocketHeader['Version'] = parseInt(InLine.replace(/Sec-WebSocket-Draft:\s?/i, ""));
                 } catch (err) {
-                    FWebSocketVersion = 0;
+                    FWebSocketHeader['Version'] = 0;
                 }
             } else if (InLine.indexOf("Sec-WebSocket-Version") === 0) {
+				// Example: "Sec-WebSocket-Version: 8"
                 try {
-                    FWebSocketVersion = parseInt(InLine.replace(/Sec-WebSocket-Version:\s?/i, ""));
+                    FWebSocketHeader['Version'] = parseInt(InLine.replace(/Sec-WebSocket-Version:\s?/i, ""));
                 } catch (err) {
-                    FWebSocketVersion = 0;
+                    FWebSocketHeader['Version'] = 0;
                 }
-            }
+            } else if (InLine.indexOf("Upgrade:") === 0) {
+				// Example: "Upgrade: websocket"
+				FWebSocketHeader['Upgrade'] = InLine.replace(/Upgrade:\s?/i, "");
+			}
         }
     } catch (err) {
         log(LOG_ERR, "ShakeHands() error: " + err.toString());
     }
     
     return false;
+}
+
+function ShakeHandsDraft0() {
+	// Ensure we have all the data we need
+	if (('Key1' in FWebSocketHeader) && ('Key2' in FWebSocketHeader) && ('Host' in FWebSocketHeader) && ('Origin' in FWebSocketHeader !== "") && ('Path' in FWebSocketHeader)) {
+		// Combine Key1, Key2, and the last 8 bytes into a string that we will later hash
+		var ToHash = ""
+		ToHash += String.fromCharCode((FWebSocketHeader['Key1'] & 0xFF000000) >> 24);
+		ToHash += String.fromCharCode((FWebSocketHeader['Key1'] & 0x00FF0000) >> 16);
+		ToHash += String.fromCharCode((FWebSocketHeader['Key1'] & 0x0000FF00) >> 8);
+		ToHash += String.fromCharCode((FWebSocketHeader['Key1'] & 0x000000FF) >> 0);
+		ToHash += String.fromCharCode((FWebSocketHeader['Key2'] & 0xFF000000) >> 24);
+		ToHash += String.fromCharCode((FWebSocketHeader['Key2'] & 0x00FF0000) >> 16);
+		ToHash += String.fromCharCode((FWebSocketHeader['Key2'] & 0x0000FF00) >> 8);
+		ToHash += String.fromCharCode((FWebSocketHeader['Key2'] & 0x000000FF) >> 0);				
+		for (var i = 0; i < 8; i++) {
+			ToHash += String.fromCharCode(client.socket.recvBin(1));
+		}
+		
+		// Hash the string
+		var Hashed = md5_calc(ToHash, true);
+
+		// Setup the handshake response
+		var Response = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" +
+					   "Upgrade: WebSocket\r\n" +
+					   "Connection: Upgrade\r\n" +
+					   "Sec-WebSocket-Origin: " + FWebSocketHeader['Origin'] + "\r\n" +
+					   "Sec-WebSocket-Location: ws://" + FWebSocketHeader['Host'] + FWebSocketHeader['Path'] + "\r\n";
+		if ('SubProtocol' in FWebSocketHeader) Response += "Sec-WebSocket-Protocol: " + FWebSocketHeader['SubProtocol'] + "\r\n";
+		Response += "\r\n";
+		
+		// Loop through the hash string (which is hex encoded) and append the individual bytes to the response
+		for (var i = 0; i < Hashed.length; i += 2) {
+			Response += String.fromCharCode(parseInt(Hashed.charAt(i) + Hashed.charAt(i + 1), 16));
+		}
+		
+		// Send the response and return
+		client.socket.send(Response);
+		return true;
+	} else {
+		// We're missing some pice of data, log what we do have
+		log(LOG_ERR, "Missing some piece of handshake data.  Here's what we have:");
+		for(var x in FWebSocketHeader) { 
+			log(LOG_ERR, x + " => " + FWebSocketHeader[x]); 
+		}
+		return false;
+	}
+}
+
+function ShakeHandsVersion7() {
+	// Ensure we have all the data we need
+	if (('Key' in FWebSocketHeader) && ('Host' in FWebSocketHeader) && ('Origin' in FWebSocketHeader !== "") && ('Path' in FWebSocketHeader)) {
+		var AcceptGUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+		// Combine Key and GUID
+		var ToHash = FWebSocketHeader['Key'] + AcceptGUID;
+		
+		// Hash the string
+		var Hashed = Sha1.hash(ToHash, false);
+
+        // Encode the hash
+        var ToEncode = '';
+        for (var i = 0; i <= 38; i += 2) {
+        	ToEncode += String.fromCharCode(parseInt(Hashed.substr(i, 2), 16));
+		}
+		var Encoded = base64_encode(ToEncode);
+
+		// Setup the handshake response
+		var Response = "HTTP/1.1 101 Switching Protocols\r\n" +
+					   "Upgrade: websocket\r\n" +
+					   "Connection: Upgrade\r\n" +
+					   "Sec-WebSocket-Accept: " + Encoded + "\r\n";
+		if ('SubProtocol' in FWebSocketHeader) Response += "Sec-WebSocket-Protocol: " + FWebSocketHeader['SubProtocol'] + "\r\n";
+		Response += "\r\n";
+		
+		// Send the response and return
+		client.socket.send(Response);
+		return true;
+	} else {
+		// We're missing some pice of data, log what we do have
+		log(LOG_ERR, "Missing some piece of handshake data.  Here's what we have:");
+		for(var x in FWebSocketHeader) { 
+			log(LOG_ERR, x + " => " + FWebSocketHeader[x]); 
+		}
+		return false;
+	}
 }
