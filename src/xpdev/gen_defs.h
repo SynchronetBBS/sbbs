66212ea9c7db8fd851445612762307ac180698b0
rswindell <>
2011-10-16 09:50:47 +0000

    uint must be a typedef (not #defined) to be compatible with Spidermonkey 1.8.5 headers. Fixup indentation (tab expansion) nightmare.

--- src/xpdev/gen_defs.h
+++ src/xpdev/gen_defs.h
@@ -3,36 +3,36 @@
 /* General(ly useful) constant, macro, and type definitions */
 
 /* $Id$ */
-
+																			
 /****************************************************************************
- * @format.tab-size 4           (Plain Text/Source Code File Header)                    *
- * @format.use-tabs true        (see http://www.synchro.net/ptsc_hdr.html)              *
- *                                                                                                                                                      *
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html          *
- *                                                                                                                                                      *
- * This library is free software; you can redistribute it and/or                        *
- * modify it under the terms of the GNU Lesser General Public License           *
- * as published by the Free Software Foundation; either version 2                       *
- * of the License, or (at your option) any later version.                                       *
- * See the GNU Lesser General Public License for more details: lgpl.txt or      *
- * http://www.fsf.org/copyleft/lesser.html                                                                      *
- *                                                                                                                                                      *
- * Anonymous FTP access to the most recent released source is available at      *
- * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net      *
- *                                                                                                                                                      *
- * Anonymous CVS access to the development source and modification history      *
- * is available at cvs.synchro.net:/cvsroot/sbbs, example:                                      *
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login                        *
- *     (just hit return, no password is necessary)                                                      *
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src         *
- *                                                                                                                                                      *
- * For Synchronet coding style and modification guidelines, see                         *
- * http://www.synchro.net/source.html                                                                           *
- *                                                                                                                                                      *
- * You are encouraged to submit any modifications (preferably in Unix diff      *
- * format) via e-mail to mods@synchro.net                                                                       *
- *                                                                                                                                                      *
- * Note: If this box doesn't appear square, then you need to fix your tabs.     *
+ * @format.tab-size 4           (Plain Text/Source Code File Header)        *
+ * @format.use-tabs true        (see http://www.synchro.net/ptsc_hdr.html)  *
+ *                                                                          *
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html      *
+ *                                                                          *
+ * This library is free software; you can redistribute it and/or            *
+ * modify it under the terms of the GNU Lesser General Public License       *
+ * as published by the Free Software Foundation; either version 2           *
+ * of the License, or (at your option) any later version.                   *
+ * See the GNU Lesser General Public License for more details: lgpl.txt or  *
+ * http://www.fsf.org/copyleft/lesser.html                                  *
+ *                                                                          *
+ * Anonymous FTP access to the most recent released source is available at  *
+ * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net  *
+ *                                                                          *
+ * Anonymous CVS access to the development source and modification history  *
+ * is available at cvs.synchro.net:/cvsroot/sbbs, example:                  *
+ * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login            *
+ *     (just hit return, no password is necessary)                          *
+ * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src     *
+ *                                                                          *
+ * For Synchronet coding style and modification guidelines, see             *
+ * http://www.synchro.net/source.html                                       *
+ *                                                                          *
+ * You are encouraged to submit any modifications (preferably in Unix diff  *
+ * format) via e-mail to mods@synchro.net                                   *
+ *                                                                          *
+ * Note: If this box doesn't appear square, then you need to fix your tabs. *
  ****************************************************************************/
 
 #ifndef _GEN_DEFS_H
@@ -69,36 +69,36 @@
 #endif
 #endif
 
-                                                                        /* Control characters */
+                                               /* Control characters */
 #ifndef STX
-#define STX     0x02                            /* Start of text                        ^B      */
+#define STX     0x02                           /* Start of text                 ^B      */
 #endif
 #ifndef ETX
-#define ETX     0x03                            /* End of text                          ^C      */
+#define ETX     0x03                           /* End of text                   ^C      */
 #endif
 #ifndef BEL
-#define BEL             0x07                            /* Bell/beep                            ^G      */
+#define BEL     0x07                            /* Bell/beep                    ^G      */
 #endif
 #ifndef FF
-#define FF              0x0c                            /* Form feed                            ^L      */
+#define FF      0x0c                            /* Form feed                    ^L      */
 #endif
 #ifndef ESC
-#define ESC     0x1b                            /* Escape                                       ^[      */
+#define ESC     0x1b                            /* Escape                       ^[      */
 #endif
 #ifndef DEL
-#define DEL             0x7f                            /* Delete                                       ^BS     */
+#define DEL     0x7f                            /* Delete                       ^BS     */
 #endif
 #ifndef BS
-#define BS              '\b'                            /* Back space                           ^H      */
+#define BS      '\b'                            /* Back space                   ^H      */
 #endif
 #ifndef TAB
 #define TAB     '\t'                            /* Horizontal tabulation        ^I      */
 #endif
 #ifndef LF
-#define LF              '\n'                            /* Line feed                            ^J      */
+#define LF      '\n'                            /* Line feed                    ^J      */
 #endif
 #ifndef CR
-#define CR              '\r'                            /* Carriage return                      ^M      */
+#define CR      '\r'                            /* Carriage return              ^M      */
 #endif
 
 #ifndef CTRL_A
@@ -139,7 +139,7 @@ enum {
 #ifndef __GLIBC__
         #ifndef ushort
         #define ushort  unsigned short
-        #define uint    unsigned int
+        typedef unsigned int uint;			/* Incompatible with Spidermonkey header files when #define'd */
         #define ulong   unsigned long
         #endif
 #endif
@@ -180,20 +180,20 @@ typedef int32_t         time32_t;
 
 #if defined(_WIN32)
 #  if defined(_FILE_OFFSET_BITS) && (_FILE_OFFSET_BITS==64)
-#    define     off_t           int64_t
-#    define PRIdOFF             PRId64
-#    define PRIuOFF             PRIu64
+#    define off_t       int64_t
+#    define PRIdOFF     PRId64
+#    define PRIuOFF     PRIu64
 #  else
-#    define PRIdOFF             "ld"
-#    define PRIuOFF             "lu"
+#    define PRIdOFF     "ld"
+#    define PRIuOFF     "lu"
 #  endif
 #elif defined(__linux__) || defined(__sun__)
 #  if defined(_FILE_OFFSET_BITS) && (_FILE_OFFSET_BITS==64)
-#    define PRIdOFF             PRId64
-#    define PRIuOFF             PRIu64
+#    define PRIdOFF     PRId64
+#    define PRIuOFF     PRIu64
 #  else
-#    define PRIdOFF             PRId32
-#    define PRIuOFF             PRIu32
+#    define PRIdOFF     PRId32
+#    define PRIuOFF     PRIu32
 #  endif
 #else
 #  define PRIdOFF       PRId64
@@ -234,7 +234,7 @@ typedef struct {
 
 typedef struct {
         char*   name;
-        int             value;
+        int     value;
 } named_int_t;
 
 typedef struct {
@@ -278,13 +278,13 @@ typedef struct {
 } named_bool_t;
 
 typedef struct {
-        int             key;
+        int     key;
         char*   value;
 } keyed_string_t;
 
 typedef struct {
-        int             key;
-        int             value;
+        int     key;
+        int     value;
 } keyed_int_t;
 
 
@@ -300,61 +300,61 @@ typedef struct {
 /***********************/
 
 /* Null-Terminate an ASCIIZ char array */
-#define TERMINATE(str)                                  str[sizeof(str)-1]=0
+#define TERMINATE(str)                      str[sizeof(str)-1]=0
 
 /* This is a bound-safe version of strcpy basically - only works with fixed-length arrays */
 #ifdef SAFECOPY_USES_SPRINTF
-#define SAFECOPY(dst,src)                               sprintf(dst,"%.*s",(int)sizeof(dst)-1,src)
+#define SAFECOPY(dst,src)                   sprintf(dst,"%.*s",(int)sizeof(dst)-1,src)
 #else   /* strncpy is faster */
-#define SAFECOPY(dst,src)                               (strncpy(dst,src,sizeof(dst)), TERMINATE(dst))
+#define SAFECOPY(dst,src)                   (strncpy(dst,src,sizeof(dst)), TERMINATE(dst))
 #endif
 
 /* Bound-safe version of sprintf() - only works with fixed-length arrays */
 #if (defined __FreeBSD__) || (defined __NetBSD__) || (defined __OpenBSD__) || (defined(__APPLE__) && defined(__MACH__) && defined(__POWERPC__))
 /* *BSD *nprintf() is already safe */
-#define SAFEPRINTF(dst,fmt,arg)                 snprintf(dst,sizeof(dst),fmt,arg)
-#define SAFEPRINTF2(dst,fmt,a1,a2)              snprintf(dst,sizeof(dst),fmt,a1,a2)
-#define SAFEPRINTF3(dst,fmt,a1,a2,a3)   snprintf(dst,sizeof(dst),fmt,a1,a2,a3)
-#define SAFEPRINTF4(dst,fmt,a1,a2,a3,a4) snprintf(dst,sizeof(dst),fmt,a1,a2,a3,a4)
-#else
-#define SAFEPRINTF(dst,fmt,arg)                 snprintf(dst,sizeof(dst),fmt,arg), TERMINATE(dst)
-#define SAFEPRINTF2(dst,fmt,a1,a2)              snprintf(dst,sizeof(dst),fmt,a1,a2), TERMINATE(dst)
-#define SAFEPRINTF3(dst,fmt,a1,a2,a3)   snprintf(dst,sizeof(dst),fmt,a1,a2,a3), TERMINATE(dst)
-#define SAFEPRINTF4(dst,fmt,a1,a2,a3,a4) snprintf(dst,sizeof(dst),fmt,a1,a2,a3,a4), TERMINATE(dst)
+#define SAFEPRINTF(dst,fmt,arg)             snprintf(dst,sizeof(dst),fmt,arg)
+#define SAFEPRINTF2(dst,fmt,a1,a2)          snprintf(dst,sizeof(dst),fmt,a1,a2)
+#define SAFEPRINTF3(dst,fmt,a1,a2,a3)		snprintf(dst,sizeof(dst),fmt,a1,a2,a3)
+#define SAFEPRINTF4(dst,fmt,a1,a2,a3,a4)	snprintf(dst,sizeof(dst),fmt,a1,a2,a3,a4)
+#else										
+#define SAFEPRINTF(dst,fmt,arg)				snprintf(dst,sizeof(dst),fmt,arg), TERMINATE(dst)
+#define SAFEPRINTF2(dst,fmt,a1,a2)			snprintf(dst,sizeof(dst),fmt,a1,a2), TERMINATE(dst)
+#define SAFEPRINTF3(dst,fmt,a1,a2,a3)		snprintf(dst,sizeof(dst),fmt,a1,a2,a3), TERMINATE(dst)
+#define SAFEPRINTF4(dst,fmt,a1,a2,a3,a4)	snprintf(dst,sizeof(dst),fmt,a1,a2,a3,a4), TERMINATE(dst)
 #endif
 
 /* Replace every occurance of c1 in str with c2, using p as a temporary char pointer */
 #define REPLACE_CHARS(str,c1,c2,p)      for((p)=(str);*(p);(p)++) if(*(p)==(c1)) *(p)=(c2);
 
 /* ASCIIZ char* parsing helper macros */
-#define SKIP_WHITESPACE(p)                      while(*(p) && isspace((unsigned char)*(p)))                     (p)++;
-#define FIND_WHITESPACE(p)                      while(*(p) && !isspace((unsigned char)*(p)))            (p)++;
-#define SKIP_CHAR(p,c)                          while(*(p)==c)                                                                          (p)++;
-#define FIND_CHAR(p,c)                          while(*(p) && *(p)!=c)                                                          (p)++;
-#define SKIP_CHARSET(p,s)                       while(*(p) && strchr(s,*(p))!=NULL)                                     (p)++;
-#define FIND_CHARSET(p,s)                       while(*(p) && strchr(s,*(p))==NULL)                                     (p)++;
-#define SKIP_ALPHA(p)                           while(*(p) && isalpha((unsigned char)*(p)))                     (p)++;
-#define FIND_ALPHA(p)                           while(*(p) && !isalpha((unsigned char)*(p)))            (p)++;
+#define SKIP_WHITESPACE(p)              while(*(p) && isspace((unsigned char)*(p)))                     (p)++;
+#define FIND_WHITESPACE(p)              while(*(p) && !isspace((unsigned char)*(p)))            (p)++;
+#define SKIP_CHAR(p,c)                  while(*(p)==c)                                                                          (p)++;
+#define FIND_CHAR(p,c)                  while(*(p) && *(p)!=c)                                                          (p)++;
+#define SKIP_CHARSET(p,s)               while(*(p) && strchr(s,*(p))!=NULL)                                     (p)++;
+#define FIND_CHARSET(p,s)               while(*(p) && strchr(s,*(p))==NULL)                                     (p)++;
+#define SKIP_ALPHA(p)                   while(*(p) && isalpha((unsigned char)*(p)))                     (p)++;
+#define FIND_ALPHA(p)                   while(*(p) && !isalpha((unsigned char)*(p)))            (p)++;
 #define SKIP_ALPHANUMERIC(p)            while(*(p) && isalnum((unsigned char)*(p)))                     (p)++;
 #define FIND_ALPHANUMERIC(p)            while(*(p) && !isalnum((unsigned char)*(p)))            (p)++;
-#define SKIP_DIGIT(p)                           while(*(p) && isdigit((unsigned char)*(p)))                     (p)++;
-#define FIND_DIGIT(p)                           while(*(p) && !isdigit((unsigned char)*(p)))            (p)++;
-#define SKIP_HEXDIGIT(p)                        while(*(p) && isxdigit((unsigned char)*(p)))            (p)++;
-#define FIND_HEXDIGIT(p)                        while(*(p) && !isxdigit((unsigned char)*(p)))           (p)++;
+#define SKIP_DIGIT(p)                   while(*(p) && isdigit((unsigned char)*(p)))                     (p)++;
+#define FIND_DIGIT(p)                   while(*(p) && !isdigit((unsigned char)*(p)))            (p)++;
+#define SKIP_HEXDIGIT(p)                while(*(p) && isxdigit((unsigned char)*(p)))            (p)++;
+#define FIND_HEXDIGIT(p)                while(*(p) && !isxdigit((unsigned char)*(p)))           (p)++;
 
 /* Variable/buffer initialization (with zeros) */
 #define ZERO_VAR(var)                           memset(&(var),0,sizeof(var))
 #define ZERO_ARRAY(array)                       memset(array,0,sizeof(array))
 
 /****************************************************************************/
-/* MALLOC/FREE Macros for various compilers and environments                            */
-/* MALLOC is used for allocations of 64k or less                                                        */
-/* FREE is used to free buffers allocated with MALLOC                                           */
-/* LMALLOC is used for allocations of possibly larger than 64k                          */
-/* LFREE is used to free buffers allocated with LMALLOC                                         */
-/* REALLOC is used to re-size a previously MALLOCed or LMALLOCed buffer         */
-/* FAR16 is used to create a far (32-bit) pointer in 16-bit compilers           */
-/* HUGE16 is used to create a huge (32-bit) pointer in 16-bit compilers         */
+/* MALLOC/FREE Macros for various compilers and environments                */
+/* MALLOC is used for allocations of 64k or less                            */
+/* FREE is used to free buffers allocated with MALLOC                       */
+/* LMALLOC is used for allocations of possibly larger than 64k              */
+/* LFREE is used to free buffers allocated with LMALLOC                     */
+/* REALLOC is used to re-size a previously MALLOCed or LMALLOCed buffer     */
+/* FAR16 is used to create a far (32-bit) pointer in 16-bit compilers       */
+/* HUGE16 is used to create a huge (32-bit) pointer in 16-bit compilers     */
 /****************************************************************************/
 #if defined(__COMPACT__) || defined(__LARGE__) || defined(__HUGE__)
         #define HUGE16 huge
@@ -368,7 +368,7 @@ typedef struct {
         #elif defined(__WATCOMC__)
                 #define REALLOC realloc
                 #define LMALLOC(x) halloc(x,1)  /* far heap, but slow */
-                #define MALLOC malloc                   /* far heap, but 64k max */
+                #define MALLOC malloc           /* far heap, but 64k max */
                 #define LFREE hfree
                 #define FREE free
         #else   /* Other 16-bit Compiler */
@@ -391,12 +391,12 @@ typedef struct {
 /********************************/
 /* Handy Pointer-freeing Macros */
 /********************************/
-#define FREE_AND_NULL(x)                        if(x!=NULL) { FREE(x); x=NULL; }
+#define FREE_AND_NULL(x)                if(x!=NULL) { FREE(x); x=NULL; }
 #define FREE_LIST_ITEMS(list,i)         if(list!=NULL) {                                \
-                                                                                for(i=0;list[i]!=NULL;i++)      \
-                                                                                        FREE_AND_NULL(list[i]); \
-                                                                        }
-#define FREE_LIST(list,i)                       FREE_LIST_ITEMS(list,i) FREE_AND_NULL(list)
+											for(i=0;list[i]!=NULL;i++)      \
+												FREE_AND_NULL(list[i]); \
+                                        }
+#define FREE_LIST(list,i)               FREE_LIST_ITEMS(list,i) FREE_AND_NULL(list)
 
 /********************************/
 /* Other Pointer-List Macros    */
