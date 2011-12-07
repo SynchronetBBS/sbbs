cad2a19b604b53a934a298d949b4d2cd6116924c
deuce <>
2011-12-07 03:28:28 +0000

    If getaddrinfo() fails, don't free the result pointer.

--- src/syncterm/conn.c
+++ src/syncterm/conn.c
@@ -409,7 +409,7 @@ int conn_socket_connect(struct bbslist *bbs)
 	fd_set			wfd;
 	int				failcode=FAILURE_WHAT_FAILURE;
 	struct addrinfo	hints;
-	struct addrinfo	*res;
+	struct addrinfo	*res=NULL;
 	struct addrinfo	*cur;
 	char			portnum[6];
 
@@ -422,6 +422,7 @@ int conn_socket_connect(struct bbslist *bbs)
 	sprintf(portnum, "%hu", bbs->port);
 	if(getaddrinfo(bbs->addr, portnum, &hints, &res)!=0) {
 		failcode=FAILURE_RESOLVE;
+		res=NULL;
 		goto connect_failed;
 	}
 	uifc.pop(NULL);
@@ -504,7 +505,8 @@ connected:
 	return(sock);
 
 connect_failed:
-	freeaddrinfo(res);
+	if(res)
+		freeaddrinfo(res);
 	{
 		char str[LIST_ADDR_MAX+40];
 
