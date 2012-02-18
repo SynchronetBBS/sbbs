e1bb1925495c1d808fa44478e899ffbadcff78e5
rswindell <>
2012-02-18 03:19:38 +0000

    Enforce sane "maximum" packet and bundle values.

--- src/sbbs3/rechocfg.c
+++ src/sbbs3/rechocfg.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2012 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -622,6 +622,13 @@ void read_echo_cfg()
 		/* printf("Unrecognized line in SBBSECHO.CFG file.\n"); */
 	}
 	fclose(stream);
+
+	/* make sure we have some sane "maximum" size values here: */
+	if(cfg.maxpktsize<1024)
+		cfg.maxpktsize=DFLT_PKT_SIZE;
+	if(cfg.maxbdlsize<1024)
+		cfg.maxbdlsize=DFLT_BDL_SIZE;
+
 	printf("\n");
 }
 
