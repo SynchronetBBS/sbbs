672192a008120e97aa03e6499ebee0864fefd6c8
deuce <>
2012-03-09 07:42:17 +0000

    Add some hackery to allow running from jsexec.

--- exec/json-service.js
+++ exec/json-service.js
@@ -2,6 +2,15 @@ load("event-timer.js");
 load("json-sock.js");
 load("json-db.js");
 
+// Running from jsexec presumably...
+if(js.global.server==undefined) {
+        load("sockdefs.js");
+        server={};
+        server.socket=new Socket(SOCK_STREAM, 'JSONDB');
+        server.socket.bind(10088,'127.0.0.1');
+        server.socket.listen();
+}
+
 /**** SERVICE MODULES
  * 
  * 	main service (socket service)
