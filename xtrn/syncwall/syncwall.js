2c913f7f8dae0c8ea1a57e79761fc9e53552b7aa
mcmlxxix <>
2012-04-16 18:11:07 +0000

    molest echicken's code

--- xtrn/syncwall/syncwall.js
+++ xtrn/syncwall/syncwall.js
@@ -7,6 +7,7 @@ load("json-client.js");
 js.branch_limit = 0; // Who wants to loop forever?
 
 var chDelay = 15; // Milliseconds between characters when loading the buffer
+var chBuffer = 100; // max character history to read at once 
 var server = "bbs.electronicchicken.com"; // IP or hostname of the JSON server
 var port = 10088; // Port that "server" is listening on
 
@@ -31,24 +32,26 @@ var bgColours = [ BG_BLUE, BG_CYAN, BG_GREEN, BG_MAGENTA, BG_RED, BG_BROWN ];
 	the 'syncwall' DB. */
 var ansiClient = new JSONClient(server, port);
 if(!ansiClient.socket.is_connected) exit();
-ansiClient.callback = drawStuff;
-ansiClient.subscribe("syncwall", "canvas." + monthYear);
 
 /*	Establish parent frame 'frame' with subframes 'topBar' and 'bottomBar'.
 	Frames make it nice and easy to output and organize things, and make it
 	easy for us to update the status / "who's drawing" bar (bottomBar) later
 	on. */
-var frame = new Frame(1, 1, 80, 24, BG_BLACK);
-var topBar = new Frame(1, 1, 80, 1, BG_BLUE|WHITE, frame);
-var bottomBar = new Frame(1, 24, 80, 1, BG_BLUE|WHITE, frame);
-topBar.putmsg(".: SyncWall InterBBS Graffiti Wall : Hit [ESC] to Exit :.");
-topBar.gotoxy(69, 1);
-topBar.putmsg("\1h\1bby echicken");
-frame.open();
-frame.cycle();
+var frame, topBar, bottomBar, ansi;
 
-// Create ANSI Editor 'ansi' at x,y coordinates 1,2, 80 columns by 22 rows
-var ansi = new ansiEdit(1, 2, 80, 22, BG_BLACK|LIGHTGRAY);
+function init() {
+	frame = new Frame(1, 1, 80, 24, BG_BLACK);
+	topBar = new Frame(1, 1, 80, 1, BG_BLUE|WHITE, frame);
+	bottomBar = new Frame(1, 24, 80, 1, BG_BLUE|WHITE, frame);
+	ansiClient.callback = drawStuff;
+	ansiClient.subscribe("syncwall", "canvas." + monthYear);
+	topBar.putmsg(".: SyncWall InterBBS Graffiti Wall : Hit [ESC] to Exit :.");
+	topBar.gotoxy(69, 1);
+	topBar.putmsg("\1h\1bby echicken");
+	frame.open();
+	ansi = new ansiEdit(1, 2, 80, 22, BG_BLACK|LIGHTGRAY, frame);
+	frame.cycle();
+}
 
 /*	We previously established that 'drawStuff' would be the callback function
 	for our JSON client 'ansiClient', ie. the function that is called if we do
@@ -88,40 +91,36 @@ function cleanUp() {
 	console.clear();
 }
 
-/*	Buffer in this month's entire canvas drawing history and push it into the
-	ANSI editor one character at a time so that we can watch the progression
-	from a blank slate to whatever it looks like now. */
-/*	The canvas history is an array in the JSON DB (canvas[monthYear].history).
-	We may not be able to read the entire array in one shot, so we will page
-	through it instead, starting with elements 0 - 1000, 'index' & 'endex' */
-var index = 0; 
-var endex = 1000;
-// Find the length of the history array so we'll know where to stop paging
-var canvasLength = ansiClient.read("syncwall", "canvas." + monthYear + ".history.length", 1);
-var canvas = []; // We'll push each partial array onto this one
-if(canvasLength === undefined) {
-	/*	There's no canvas history; we should create a property for this month
-		and create its history array. */
-	ansiClient.write("syncwall", "canvas." + monthYear, { history : [] }, 2);
-} else {
-	// Here's where we page through each 1000 elements in the history array
-	while(index < canvasLength) {
-		/* Use the JSON client's slice method to grab elements 'index' through
-		'endex', then advance 'index' and 'endex' for the next pass through
-		this loop. */
-		canvas.push(ansiClient.slice("syncwall", "canvas." + monthYear + ".history", index, endex, 1));
-		index = index + 1000;
-		endex = index + 1000;
-	}
-	// Loop through each returned array of (up to) 1000 character objects
-	for(var c = 0; c < canvas.length; c++) {
-		for(var cc = 0; cc < canvas[c].length; cc++) {
-			/*	Loop through an array of character objects, calling putCh() on
-				each one, allowing the user to quit if they wish. */
-			putCh(canvas[c][cc]);
-			if(ascii(console.inkey(K_NONE, chDelay)) == 27) {
-				cleanUp();
-				exit();
+// /* buffered realtime history loading (has slight pauses when drawing) */
+function loadHistory() {
+	var index = 0; 
+	var endex = chBuffer;
+	/* Find the length of the history array so we'll know where to stop paging */
+	var canvasLength = ansiClient.read("syncwall", "canvas." + monthYear + ".history.length", 1);
+	if(canvasLength === undefined) {
+		/*	There's no canvas history; we should create a property for this month
+			and create its history array. */
+		ansiClient.write("syncwall", "canvas." + monthYear, { history : [] }, 2);
+	} else {
+		/* Here's where we page through each n elements in the history array */
+		while(index < canvasLength) {
+			/* Use the JSON client's slice method to grab elements 'index' through
+			'endex', then advance 'index' and 'endex' for the next pass through
+			this loop. */
+			var canvas = ansiClient.slice("syncwall", "canvas." + monthYear + ".history", index, endex, 1);
+			index += chBuffer;
+			endex += chBuffer;
+			while(canvas.length > 0) {
+				frame.cycle();
+				putCh(canvas.shift());
+				var cmd = console.inkey(K_NONE, chDelay);
+				if(ascii(cmd) == 27) {
+					cleanUp();
+					exit();
+				}
+				else if(cmd == " ") {
+					// todo: quick load
+				}
 			}
 		}
 	}
@@ -129,33 +128,39 @@ if(canvasLength === undefined) {
 }
 
 // The main loop.  Exit when the user hits 'esc'.
-while(ascii(userInput) != 27) {
-	ansiClient.cycle(); // Check for updates to the JSON DB
-	userInput = console.inkey(K_NONE, 5);
-	if(userInput == "") continue;
-	// If there was user input, pass it to the ANSI editor 'ansi'
-	ch = ansi.cycle(userInput);
-	if(!ch.ch) continue;
-	/*	If a character was drawn, update bottomBar to reflect that this user
-		is drawing, modify lastUser and lastSystem accordingly. */
-	if(lastUser != user.alias || lastSystem != system.name) {
-		bottomBar.attr = BG_BLUE|WHITE;
-		bottomBar.clear();
-		bottomBar.putmsg("You are drawing");
-		bottomBar.cycle();
-		lastUser = user.alias;
-		lastSystem = system.name;
+function main() {
+	while(ascii(userInput) != 27) {
+		ansiClient.cycle(); // Check for updates to the JSON DB
+		userInput = console.inkey(K_NONE, 5);
+		if(userInput == "") continue;
+		// If there was user input, pass it to the ANSI editor 'ansi'
+		ch = ansi.getcmd(userInput);
+		if(!ch.ch) continue;
+		/*	If a character was drawn, update bottomBar to reflect that this user
+			is drawing, modify lastUser and lastSystem accordingly. */
+		if(lastUser != user.alias || lastSystem != system.name) {
+			bottomBar.attr = BG_BLUE|WHITE;
+			bottomBar.clear();
+			bottomBar.putmsg("You are drawing");
+			bottomBar.cycle();
+			lastUser = user.alias;
+			lastSystem = system.name;
+		}
+		/*	Pass the character object 'ch' as returned from ANSI editor 'ansi' up
+			to the JSON DB, with additional properties to show who drew it. */
+		ch.userAlias = user.alias;
+		ch.system = system.name;
+		/*	Clients (like us) watch the 'ch' property of this month's canvas
+			property for updates, which is constantly overwritten. Each character
+			is also pushed onto the history array so that it can be buffered in
+			when this script starts (see above.) */
+		ansiClient.write("syncwall", "canvas." + monthYear + "." + ch.x + "." + ch.y, ch, 2);
+		ansiClient.push("syncwall", "canvas." + monthYear + ".history", ch, 2);
+		frame.cycle();
 	}
-	/*	Pass the character object 'ch' as returned from ANSI editor 'ansi' up
-		to the JSON DB, with additional properties to show who drew it. */
-	ch.userAlias = user.alias;
-	ch.system = system.name;
-	/*	Clients (like us) watch the 'ch' property of this month's canvas
-		property for updates, which is constantly overwritten. Each character
-		is also pushed onto the history array so that it can be buffered in
-		when this script starts (see above.) */
-	ansiClient.write("syncwall", "canvas." + monthYear + ".ch", ch, 2);
-	ansiClient.push("syncwall", "canvas." + monthYear + ".history", ch, 2);
 }
 
+init();
+loadHistory();
+main();
 cleanUp(); // You disgusting slob.
