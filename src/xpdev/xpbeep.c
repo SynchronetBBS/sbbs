b5006ed56e7e1a7ba9899b42503d77f87010d908
deuce <>
2012-04-25 08:07:52 +0000

    Add Haiku to the vast list of things that don't beep via /dev/console.

--- src/xpdev/xpbeep.c
+++ src/xpdev/xpbeep.c
@@ -887,7 +887,7 @@ void DLLCALL unix_beep(int freq, int dur)
 	}
 #endif
 
-#if !defined(__GNU__) && !defined(__QNX__) && !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__APPLE__) && !defined(__CYGWIN__)
+#if !defined(__GNU__) && !defined(__QNX__) && !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__APPLE__) && !defined(__CYGWIN__) && !defined(__HAIKU__)
 	if(console_fd == -1) 
   		console_fd = open("/dev/console", O_NOCTTY);
 	
