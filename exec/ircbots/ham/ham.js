ec43c34278c6549f11e293154a4bb4d958e8becf
deuce <>
2012-04-26 06:54:25 +0000

    Add conditions from N0NBH using "GEO", "HF", and "VHF" commands.

--- exec/ircbots/ham/ham.js
+++ exec/ircbots/ham/ham.js
@@ -1,6 +1,7 @@
 if(!js.global || js.global.HTTPRequest==undefined)
 	load("http.js");
 
+// Comment these...
 //Bot_Commands={};
 //function Bot_Command(x,y,z){}
 //js.global.config_filename='hambot.ini';
@@ -8,8 +9,10 @@ if(!js.global || js.global.HTTPRequest==undefined)
 var last_update=0;
 var update_interval=2;
 var last_wflength=-1;
+var last_solar_update=0;
+var solar_x;
 function main(srv,target)
-{	
+{
 	if((time() - last_update) < update_interval) return;
 
 	var config = new File(system.ctrl_dir + js.global.config_filename);
@@ -59,6 +62,87 @@ Bot_Commands["Z"].command = function (target, onick, ouh, srv, lbl, cmd) {
 		srv.o(target,d.toGMTString());
 	}
 
+	return true;
+}
+
+function update_solar()
+{
+	// MUST NOT request more than every hour or you will get banned!
+	if((time() - last_solar_update) < 60*60) return;
+	var req=new HTTPRequest();
+	var newxml=req.Get('http://www.hamqsl.com/solarxml.php');
+	newxml=newxml.replace(/<\?[^?]*\?>/g,'');
+	solar_x=new XML(newxml).solardata;
+}
+
+function trim(val)
+{
+	var ret=val.toString().replace(/^\s*/,"");
+	ret=ret.replace(/\s*$/,"");
+	return(ret);
+}
+
+Bot_Commands["GEO"] = new Bot_Command(0, false, false);
+Bot_Commands["GEO"].command = function (target, onick, ouh, srv, lbl, cmd) {
+	// Remove empty cmd args
+	for(i=1; i<cmd.length; i++) {
+		if(cmd[i].search(/^\s*$/)==0) {
+			cmd.splice(i,1);
+			i--;
+		}
+	}
+
+	if(cmd.length == 1) {
+		update_solar();
+		srv.o(target,"Solar conditions from "+trim(solar_x.source)+" as of "+trim(solar_x.updated)+":");
+		srv.o(target,"Solar Flux: "+trim(solar_x.solarflux)+", A-Index: "+trim(solar_x.aindex)+", K-Index: "+trim(solar_x.kindex)+"/"+trim(solar_x.kindexnt)+", X-Ray: "+trim(solar_x.xray)+", Sun Spots: "+trim(solar_x.sunspots)+", Helium Line: "+trim(solar_x.heliumline)+", Proton Flux: "+trim(solar_x.protonflux)+", Electron Flux: "+trim(solar_x.electonflux)+", Aurora Activity: "+trim(solar_x.aurora)+"/n="+trim(solar_x.normalization)+", Aurora Latitude: "+trim(solar_x.latdegree)+", Solar Wind: "+trim(solar_x.solarwind)+", Magnetic Field: "+trim(solar_x.magneticfield)+", Geomagnetic Field: "+trim(solar_x.geomagfield)+", Noise Level: "+trim(solar_x.signalnoise)+", F2 Critical Frequency: "+trim(solar_x.fof2));
+	}
+
+	return true;
+}
+
+Bot_Commands["VHF"] = new Bot_Command(0, false, false);
+Bot_Commands["VHF"].command = function (target, onick, ouh, srv, lbl, cmd) {
+	// Remove empty cmd args
+	for(i=1; i<cmd.length; i++) {
+		if(cmd[i].search(/^\s*$/)==0) {
+			cmd.splice(i,1);
+			i--;
+		}
+	}
+
+	if(cmd.length == 1) {
+		update_solar();
+		srv.o(target,"VHF conditions from "+trim(solar_x.source)+" as of "+trim(solar_x.updated)+":");
+		srv.o(target,"Aurora Latitude: "+trim(solar_x.latdegree)+" ("+trim(solar_x.calculatedvhfconditions.phenomenon.(@name=='vhf-aurora'))+")");
+		srv.o(target,"E-Skip North America (2m): "+trim(solar_x.calculatedvhfconditions.phenomenon.(@name=='E-Skip').(@location=='north_america')));
+		srv.o(target,"E-Skip Europe (2m): "+trim(solar_x.calculatedvhfconditions.phenomenon.(@name=='E-Skip').(@location=='europe')));
+		srv.o(target,"E-Skip Europe (4m): "+trim(solar_x.calculatedvhfconditions.phenomenon.(@name=='E-Skip').(@location=='europe_4m')));
+		srv.o(target,"E-Skip Europe (6m): "+trim(solar_x.calculatedvhfconditions.phenomenon.(@name=='E-Skip').(@location=='europe_6m')));
+	}
+	return true;
+}
+
+Bot_Commands["HF"] = new Bot_Command(0, false, false);
+Bot_Commands["HF"].command = function (target, onick, ouh, srv, lbl, cmd) {
+	// Remove empty cmd args
+	for(i=1; i<cmd.length; i++) {
+		if(cmd[i].search(/^\s*$/)==0) {
+			cmd.splice(i,1);
+			i--;
+		}
+	}
+
+	if(cmd.length == 1) {
+		update_solar();
+		srv.o(target,"HF conditions from "+trim(solar_x.source)+" as of "+trim(solar_x.updated)+":");
+		srv.o(target,"Band    | Day    | Night");
+		srv.o(target,format("80m-40m | %-7.7s| %s",solar_x.calculatedconditions.band.(@name == '80m-40m').(@time == 'day'),solar_x.calculatedconditions.band.(@name == '80m-40m').(@time == 'night')));
+		srv.o(target,format("30m-20m | %-7.7s| %s",solar_x.calculatedconditions.band.(@name == '30m-20m').(@time == 'day'),solar_x.calculatedconditions.band.(@name == '30m-20m').(@time == 'night')));
+		srv.o(target,format("17m-15m | %-7.7s| %s",solar_x.calculatedconditions.band.(@name == '17m-15m').(@time == 'day'),solar_x.calculatedconditions.band.(@name == '17m-15m').(@time == 'night')));
+		srv.o(target,format("12m-10m | %-7.7s| %s",solar_x.calculatedconditions.band.(@name == '12m-10m').(@time == 'day'),solar_x.calculatedconditions.band.(@name == '12m-10m').(@time == 'night')));
+		srv.o(target,"Signal Noise Level: "+trim(solar_x.signalnoise));
+	}
 	return true;
 }
 
@@ -257,6 +341,9 @@ Bot_Commands["CALLSIGN"].command = function (target,onick,ouh,srv,lvl,cmd) {
 }
 
 //var dumb={o:function(x,y) {log(y);}};
+//Bot_Commands["GEO"].command(undefined, undefined, undefined,dumb,undefined,['GEO']);
+//Bot_Commands["HF"].command(undefined, undefined, undefined,dumb,undefined,['GEO']);
+//Bot_Commands["VHF"].command(undefined, undefined, undefined,dumb,undefined,['GEO']);
 //Bot_Commands["CALLSIGN"].command(undefined, undefined, undefined,dumb,undefined,['asdf','n0l']);
 //Bot_Commands["CALLSIGN"].command(undefined, undefined, undefined,dumb,undefined,['asdf','kj6pxy']);
 //Bot_Commands["CALLSIGN"].command(undefined, undefined, undefined,dumb,undefined,['asdf','va6rrx']);
