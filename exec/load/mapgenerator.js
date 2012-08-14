c6509384c638e44b4998788635d46c04aef17750
mcmlxxix <>
2012-08-14 21:01:13 +0000

    move terrain generation into TerrainGenerator class. add generate(), poke(), normalize() methods. (perhaps digitalman might rename this file to terraingenerator.js)

--- exec/load/mapgenerator.js
+++ exec/load/mapgenerator.js
@@ -5,27 +5,47 @@
 
 USAGE:
 
-var <variable> = generateLand( 
+var generator = new TerrainGenerator();
+var map = generator.generate( 
 	<width>, 
 	<height>, 
-	<base>, 
-	<hills>, 
-	<min_radius>, 
-	<max_radius>, 
-	<scale> 
+	<hills>
 );
 
 width = the width of the map array
 height = the height of the map array
-base = the minimum elevation of the map
 hills = the number of hills to generate
-max_radius = the largest any single hill can be
-min_radius = the smallest any single hill can be
-scale = the scale of the map (max elevation relative to base)
+
+SETTINGS:
+
+TerrainGenerator.base = the minimum elevation of the map
+TerrainGenerator.max_radius = the largest any single hill can be
+TerrainGenerator.min_radius = the smallest any single hill can be
+TerrainGenerator.scale = the scale of the map (max elevation relative to base)
+
+METHODS:
+
+TerrainGenerator.generate(width,height,hills)
+--must supply width and height 
+--hills determines whether the generator will generate randomized terrain for you
+--if hills is not specified, a "blank slate" will be returned, which 
+  can be manipulated via poke() and normalize()
+
+TerrainGenerator.poke(map,xoff,yoff,direction)
+--must supply a map (2D integer array)
+--xoff, yoff are the position on the map to change elevation (optional)
+--direction is the direction in which to change the elevation (optional)
+--min_radius and max_radius affect the outcome of the "poke"
+
+TerrainGenerator.normalize(map,base,scale)
+--must supply a map (2D integer array)
+--base is the lowest acceptable elevation value (optional)
+--scale is the highest acceptable elevation value (optional)
 
 EXAMPLE:
 
-var land = generateLand(20,10,0,9,10,1,10);
+var generator = new TerrainGenerator();
+var map = generator.generate(20,10,10);
 
 23333344678963322111
 33344444677763332111
@@ -44,89 +64,125 @@ Math.sq = function(num) {
 	return num*num;
 }
 
-/* generate a land object of specified width, height, base elevation */
-function generateLand(width,height,base,scale,hills,min_radius,max_radius) { 
-	var map = [];
+function TerrainGenerator() {
+
+	/* defaults */
+	this.base = 0;
+	this.scale = 9;
+	this.min_radius=5;
+	this.max_radius=15;
 	
-	if(width == undefined || height == undefined)
-		throw("generateLand() requires, at minimum, width and height arguments");
-	if(base == undefined)
-		base = 0;
-	if(scale == undefined)
-		scale = 9;
-	if(hills == undefined)
-		hills = 20;
-	if(min_radius == undefined)
-		min_radius = 1;
-	if(max_radius == undefined)
-		max_radius = 10;
-	
-	/* populate matrix with base elevation values */
-	for(var x=0;x<width;x++) { 
-		map[x] = []; 
-		for(var y=0;y<height;y++) { 
-			map[x][y] = 0; 
-		} 
-	}
-	
-	/* generate specified number of "hills" */
-	for(var h=0;h<hills;h++) {
+	/* generate a land object of specified width, height, base elevation */
+	this.generate = function(width,height,hills) { 
+
+		if(width == undefined || height == undefined)
+			throw("Terrain.generate() requires, at minimum, width and height arguments");
+			
+		/* populate matrix with base elevation values */
+		var map = getMap(width,height,this.base);
 		
-		/* pick a random starting position */
-		var x = random(width);
-		var y = random(height);
+		if(hills > 0) {
+			/* generate specified number of "hills" */
+			for(var h=0;h<hills;h++) 
+				map = this.poke(map);
+			
+			/* normalize elevation values to fit between base elevation and scale */
+			map = this.normalize(map);
+		}
+		
+		return map;
+	}
+
+	/* generate a randomized feature on a map starting 
+	from an optionally specified x,y position in a given direction */
+	this.poke = function(map,xoff,yoff,radius,direction) {
+		/* make a copy of the original map */
+		var copy = copyMap(map);
 		
 		/* pick a hill radius between min and max */
-		var radius = random(max_radius-min_radius)+min_radius;
+		if(!radius)
+			var radius = random(this.max_radius-this.min_radius)+this.min_radius;
+			
+		/* pick a random starting position */
+		if(!xoff)
+			xoff = random(copy.length);
+		if(!yoff)
+			yoff = random(copy[xoff].length);
+			
+		/* default to elevation increase */
+		if(!direction)
+			direction = 1;
 		
-		/* generate hill */
-		generateFeature(map,x,y,radius);
+		for(var y=-radius;y<radius;y++) {
+			var halfRow=Math.round(Math.sqrt(Math.sq(radius)-Math.sq(y)));
+			for(var x=-halfRow;x<halfRow;x++) {     
+				//var h = Math.sq(radius) - (Math.sq(x) + Math.sq(y));
+				if(!direction)
+					direction = 1;
+				if(copy[xoff+x] && copy[xoff+x][yoff+y] >= 0)
+					copy[xoff+x][yoff+y] += direction;
+			}
+		}
+		
+		return copy;
 	}
-	
-	/* normalize elevation values to fit between base elevation and scale */
-	normalize(map,base,scale);
-	
-	return map; 
-}
 
-/* generate a randomized feature (elevation int_change) on a land_object starting 
-from an optionally specified start_position */
-function generateFeature(map,xoff,yoff,radius) {
-	for(var y=-radius;y<radius;y++) {
-		var halfRow=Math.sqrt(Math.sq(radius)-Math.sq(y));
-		for(var x=-halfRow;x<halfRow;x++) {     
-			//var h = Math.sq(radius) - (Math.sq(x) + Math.sq(y));
-			var h = 1;
-			var xpos = Math.floor(x);
-			if(map[xoff+xpos] && map[xoff+xpos][yoff+y] >= 0)
-				map[xoff+xpos][yoff+y] += h;
-			if(map[xoff-xpos] && map[xoff-xpos][yoff+y] >= 0)
-				map[xoff-xpos][yoff+y] += h;
-		}
-	}
-}
-
-/* normalize land within an acceptable range */
-function normalize(map,base,scale) {
-	var min;
-	var max;
+	/* normalize land within a given range */
+	this.normalize = function(map,base,scale) {
 	
-	/* iterate map and find the min and max values */
-	for(var x=0;x<map.length;x++) {
-		for(var y=0;y<map[x].length;y++) {
-			if(!min || map[x][y] < min)
-				min = map[x][y];
-			if(!max || map[x][y] > max)
-				max = map[x][y];
+		/* make a copy of the original map */
+		var copy = copyMap(map);
+		var min=false;
+		var max=false;
+		
+		if(!base)
+			base = this.base;
+		if(!scale)
+			scale = this.scale;
+		
+		/* iterate map and find the min and max values */
+		for(var x=0;x<copy.length;x++) {
+			for(var y=0;y<copy[x].length;y++) {
+				if(min==false || copy[x][y] < min)
+					min = copy[x][y];
+				if(max==false || copy[x][y] > max)
+					max = copy[x][y];
+			}
 		}
+		
+		/* iterate map again and adjust values to fit scale */
+		var range = max - min;
+		for(var x=0;x<copy.length;x++) {
+			for(var y=0;y<copy[x].length;y++) {
+				var a = copy[x][y]-min;
+				var b = (a/range) * scale;
+				var c = b + base;
+				copy[x][y] = c;
+			}
+		}
+		
+		return copy;
 	}
 	
-	/* iterate map again and adjust values to fit scale */
-	var range = max - min;
-	for(var x=0;x<map.length;x++) {
-		for(var y=0;y<map[x].length;y++) {
-			var n = map[x][y]-min;
-			map[x][y] = ((n/range) * scale) + base;
+	/* create a 2d array of given dimensions */
+	function getMap(width,height,base) {
+		var array = [];
+		for(var x=0;x<width;x++) { 
+			array[x] = []; 
+			for(var y=0;y<height;y++) 
+				array[x][y] = base; 
 		}
+		return array;
+	}
+	
+	/* copy a map */
+	function copyMap(map) {
+		var copy = [];
+		for(var x=0;x<map.length;x++) {
+			copy[x] = [];
+			for(var y=0;y<map[x].length;y++) 
+				copy[x][y] = map[x][y];
+		}
+		return copy;
 	}
 }
