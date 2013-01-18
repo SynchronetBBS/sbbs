91978549f131c92433687e0b58e1f57ebd73489b
mcmlxxix <>
2013-01-18 22:09:45 +0000

    updated interbbs docs, default to localhost in server.ini

--- xtrn/maze/maze.js
+++ xtrn/maze/maze.js
@@ -18,8 +18,8 @@ var server_file = new File(root + "server.ini");
 server_file.open('r',true);
 
 //var autoUpdate=server_file.iniGetValue(null,"autoUpdate");
-var serverAddr=server_file.iniGetValue(null,"host");
-var serverPort=server_file.iniGetValue(null,"port");
+var serverAddr=server_file.iniGetValue(null,"host","localhost");
+var serverPort=server_file.iniGetValue(null,"port",10088);
 server_file.close();
 
 var client=new JSONClient(serverAddr,serverPort);
