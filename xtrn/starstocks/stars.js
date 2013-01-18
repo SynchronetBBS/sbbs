b0ec55541388aea8328c1440fb5762d834c5a506
mcmlxxix <>
2013-01-18 22:07:44 +0000

    updated interbbs docs, default to localhost if server.ini missing, default to localhost in server.ini

--- xtrn/starstocks/stars.js
+++ xtrn/starstocks/stars.js
@@ -14,8 +14,8 @@ if(!file_exists(root + "server.ini")) {
 var server_file = new File(root + "server.ini");
 server_file.open('r',true);
 //var autoUpdate=server_file.iniGetValue(null,"autoUpdate");
-var serverAddr=server_file.iniGetValue(null,"host");
-var serverPort=server_file.iniGetValue(null,"port");
+var serverAddr=server_file.iniGetValue(null,"host","localhost");
+var serverPort=server_file.iniGetValue(null,"port",10088);
 server_file.close();
 
 var client=new JSONClient(serverAddr,serverPort);
