b4569e6410f1c4a6be9e97fb38932d8d72a5254e
rswindell <>
2013-02-14 07:24:40 +0000

    No longer word-wrap message tails (tear/tag/origin lines) when displaying messages in the terminal server.

--- src/sbbs3/getmsg.cpp
+++ src/sbbs3/getmsg.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2013 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -186,10 +186,14 @@ void sbbs_t::show_msg(smbmsg_t* msg, long mode)
 
 	show_msghdr(msg);
 
-	if((text=smb_getmsgtxt(&smb,msg,GETMSGTXT_ALL))!=NULL) {
+	if((text=smb_getmsgtxt(&smb,msg,/* body and hfields: */0))!=NULL) {
 		if(!(console&CON_RAW_IN))
 			mode|=P_WORDWRAP;
 		putmsg(text, mode);
+		smb_freemsgtxt(text);
+	}
+	if((text=smb_getmsgtxt(&smb,msg,GETMSGTXT_TAIL_ONLY))!=NULL) {
+		putmsg(text, mode&(~P_WORDWRAP));
 		smb_freemsgtxt(text);
 	}
 }
