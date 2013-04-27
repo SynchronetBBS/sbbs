86b752f1f50ca9e7c3459716b3154a77f2e93628
deuce <>
2013-04-27 02:49:49 +0000

    Unfuck matchuser() by adding space for terminating NULL and extra invalidating char.
    Thanks for the bug reports.

--- src/sbbs3/js_system.c
+++ src/sbbs3/js_system.c
@@ -821,7 +821,7 @@ js_matchuser(JSContext *cx, uintN argc, jsval *arglist)
 	if(argc>1)
 		JS_ValueToBoolean(cx,argv[1],&sysop_alias);
 
-	JSSTRING_TO_ASTRING(cx, js_str, p, (LEN_ALIAS > LEN_NAME) ? LEN_ALIAS:LEN_NAME, NULL);
+	JSSTRING_TO_ASTRING(cx, js_str, p, (LEN_ALIAS > LEN_NAME) ? LEN_ALIAS+2:LEN_NAME+2, NULL);
 	if(p==NULL) {
 		JS_SET_RVAL(cx, arglist, INT_TO_JSVAL(0));
 		return(JS_TRUE);
