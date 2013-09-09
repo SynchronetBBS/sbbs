12ca74e077b022d2d11b26e3a4f6d8477b10b8a2
deuce <>
2013-09-09 06:23:54 +0000

    When running on Deuce's secret IPv6 enabled version of Synchronet, pass an extra paramter to resolve_ip which returns an array of all addresses matching the passed hostname... then loop through them and find the first one that matches a location.
    Should be backward compatible with the Peon version.

--- exec/load/nicklocate.js
+++ exec/load/nicklocate.js
@@ -27,44 +27,48 @@ function get_nicklocation(userhost,srvhost,nick)
 
 	try {
 		var userhost=userhost.replace(/^.*\@/,'');
+		var i;
+		var uha;
 		// If the hostname is not a FQDN, use the server name and replace the first element...
 		if(userhost.indexOf('.')==-1)
 			userhost += (srvhost.replace(/^[^\.]+\./,'.'));
 		// If the address is local, finger the server for the nick and use the From:
-		userhost=resolve_ip(userhost);
-		if(userhost.search(/^10\./) != -1
-				|| userhost.search(/^192\.168\./) != -1
-				|| userhost.search(/^172\.16\./) != -1
-				|| userhost.search(/^172\.17\./) != -1
-				|| userhost.search(/^172\.18\./) != -1
-				|| userhost.search(/^172\.19\./) != -1
-				|| userhost.search(/^172\.2[0-9]\./) != -1
-				|| userhost.search(/^172\.30\./) != -1
-				|| userhost.search(/^172\.31\./) != -1
-				|| userhost.search(/^127\.0\.0\./) != -1
-				|| (userhost.search(/^169\.254\./) != -1
-					&& userhost.search(/^169\.254\.0\./) == -1
-					&& userhost.search(/^169\.254\.255\./) == -1
-					)
-				|| userhost.search(/^fc[0-9a-f][0-9a-f]:/) != -1
-				|| userhost.search(/^fd[0-9a-f][0-9a-f]:/) != -1
-				|| userhost.search(/^fec[0-9a-f]:/) != -1
-				|| userhost.search(/^fed[0-9a-f]:/) != -1
-				|| userhost.search(/^fee[0-9a-f]:/) != -1
-				|| userhost.search(/^fef[0-9a-f]:/) != -1
-				|| userhost.search(/^fe8[0-9a-f]:/) != -1
-				|| userhost.search(/^fe9[0-9a-f]:/) != -1
-				|| userhost.search(/^fea[0-9a-f]:/) != -1
-				|| userhost.search(/^feb[0-9a-f]:/) != -1)
-			userhost=finger_from();
-		geo=get_geoip(userhost);
-		ret=geo.latitude+','+geo.longitude;
-		if(ret=='0,0') {
-			userhost=srvhost;
+		userhost=resolve_ip(userhost, true);
+		if(userhost.constructor==Array)
+			uha=userhost;
+		else
+			userhost=[userhost];
+		for(i in uha) {
+			userhost=uha[i];
+			if(userhost.search(/^10\./) != -1
+					|| userhost.search(/^192\.168\./) != -1
+					|| userhost.search(/^172\.16\./) != -1
+					|| userhost.search(/^172\.17\./) != -1
+					|| userhost.search(/^172\.18\./) != -1
+					|| userhost.search(/^172\.19\./) != -1
+					|| userhost.search(/^172\.2[0-9]\./) != -1
+					|| userhost.search(/^172\.30\./) != -1
+					|| userhost.search(/^172\.31\./) != -1
+					|| userhost.search(/^127\.0\.0\./) != -1
+					|| (userhost.search(/^169\.254\./) != -1
+						&& userhost.search(/^169\.254\.0\./) == -1
+						&& userhost.search(/^169\.254\.255\./) == -1
+						)
+					|| userhost.search(/^fc[0-9a-f][0-9a-f]:/) != -1
+					|| userhost.search(/^fd[0-9a-f][0-9a-f]:/) != -1
+					|| userhost.search(/^fec[0-9a-f]:/) != -1
+					|| userhost.search(/^fed[0-9a-f]:/) != -1
+					|| userhost.search(/^fee[0-9a-f]:/) != -1
+					|| userhost.search(/^fef[0-9a-f]:/) != -1
+					|| userhost.search(/^fe8[0-9a-f]:/) != -1
+					|| userhost.search(/^fe9[0-9a-f]:/) != -1
+					|| userhost.search(/^fea[0-9a-f]:/) != -1
+					|| userhost.search(/^feb[0-9a-f]:/) != -1)
+				userhost=finger_from();
 			geo=get_geoip(userhost);
-			ret=geo.latitude+','+geo.longitude;
+			if(geo != undefined)
+				return geo;
 		}
-		return ret;
 	}
 	catch(e) {
 		log("ERROR: " + e);
