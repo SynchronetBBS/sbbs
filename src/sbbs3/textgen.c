90af71682a053c572cdb0b034f9cabe138c20ea6
deuce <>
2014-02-13 09:09:18 +0000

    include dirwrap.h for MAX_PATH definition.

--- src/sbbs3/textgen.c
+++ src/sbbs3/textgen.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "dirwrap.h"	// MAX_PATH
 #include "gen_defs.h"
 
 /****************************************************************************/
