4173ce48d0ceec0519b0eeb23ef7979398c3d44f
deuce <>
2014-03-15 18:56:28 +0000

    Rewrite the RT_TYPE=RT_UNIQUE stuff to always create/destroy runtimes.
    Use a linked list to track runtimes, thereby allowing an unlimited number
    of them.
    
    These functions are still needed since Spidermonkey crashes if more than
    one runtime is created simultaneously.

--- src/sbbs3/js_rtpool.c
+++ src/sbbs3/js_rtpool.c
@@ -3,6 +3,7 @@
 #include "js_rtpool.h"
 #include <threadwrap.h>		/* Must be included after jsapi.h */
 #include <genwrap.h>		/* SLEEP() */
+#include <link_list.h>
 
 #ifdef DLLCALL
 #undef DLLCALL
@@ -22,29 +23,26 @@
 #define RT_SINGLE	2
 #define RT_TYPE		RT_UNIQUE
 
+#if (RT_TYPE != RT_UNIQUE)
+#if (RT_TYPE == RT_SINGLE)
+#define JSRT_QUEUE_SIZE		1
+#else
+#define JSRT_QUEUE_SIZE		256
+#endif
 struct jsrt_queue {
 	JSRuntime       *rt;
 	int			created;
 #if (RT_TYPE==RT_SHARED)
 	const char*	file;
 	long		line;
-#else
-	int			maxbytes;
-	int			used;
 #endif
 };
 
-#if (RT_TYPE == RT_SINGLE)
-#define JSRT_QUEUE_SIZE		1
-#else
-#define JSRT_QUEUE_SIZE		256
-#endif
 struct jsrt_queue jsrt_queue[JSRT_QUEUE_SIZE];
+#endif
 static pthread_mutex_t		jsrt_mutex;
 static int			initialized=0;
-#if (RT_TYPE == RT_UNIQUE)
-static sem_t			jsrt_sem;
-#endif
+static link_list_t	rt_list;
 
 #define TRIGGER_THREAD_STACK_SIZE	(256*1024)
 static void trigger_thread(void *args)
@@ -53,11 +51,18 @@ static void trigger_thread(void *args)
 
 	SetThreadName("JSRT Trigger");
 	for(;;) {
+		list_node_t *node;
 		pthread_mutex_lock(&jsrt_mutex);
+#if (RT_TYPE==RT_UNIQUE)
+		for(node=listFirstNode(&rt_list); node; node=listNextNode(node)) {
+			JS_TriggerAllOperationCallbacks(node->data);
+		}
+#else
 		for(i=0; i<JSRT_QUEUE_SIZE; i++) {
 			if(jsrt_queue[i].created)
 				JS_TriggerAllOperationCallbacks(jsrt_queue[i].rt);
 		}
+#endif
 		pthread_mutex_unlock(&jsrt_mutex);
 		SLEEP(100);
 	}
@@ -67,11 +72,11 @@ JSRuntime * DLLCALL jsrt_GetNew(int maxbytes, unsigned long timeout, const char 
 {
 #if (RT_TYPE==RT_SINGLE)
 	if(!initialized) {
+		initialized=TRUE;
 		pthread_mutex_init(&jsrt_mutex, NULL);
 		jsrt_queue[0].rt=JS_NewRuntime(128*1024*1024 /* 128 MB total for all scripts? */);
 		jsrt_queue[0].created=1;
 		_beginthread(trigger_thread, TRIGGER_THREAD_STACK_SIZE, NULL);
-		initialized=TRUE;
 	}
 
 	return jsrt_queue[0].rt;
@@ -79,9 +84,9 @@ JSRuntime * DLLCALL jsrt_GetNew(int maxbytes, unsigned long timeout, const char 
 	int	i;
 
 	if(!initialized) {
+		initialized=TRUE;
 		pthread_mutex_init(&jsrt_mutex, NULL);
 		_beginthread(trigger_thread, TRIGGER_THREAD_STACK_SIZE, NULL);
-		initialized=TRUE;
 	}
 
 	pthread_mutex_lock(&jsrt_mutex);
@@ -103,62 +108,32 @@ JSRuntime * DLLCALL jsrt_GetNew(int maxbytes, unsigned long timeout, const char 
 
 	return(NULL);
 #elif (RT_TYPE==RT_UNIQUE)
-	int	i;
-	int	last_unused=-1;
+	JSRuntime *ret;
 
 	if(!initialized) {
-		pthread_mutex_init(&jsrt_mutex, NULL);
-		sem_init(&jsrt_sem, 0, JSRT_QUEUE_SIZE);
-		_beginthread(trigger_thread, TRIGGER_THREAD_STACK_SIZE, NULL);
 		initialized=TRUE;
+		pthread_mutex_init(&jsrt_mutex, NULL);
+		listInit(&rt_list, 0);
+		_beginthread(trigger_thread, TRIGGER_THREAD_STACK_SIZE, NULL);
 	}
-
-	if(sem_trywait_block(&jsrt_sem, timeout)==0) {
-		pthread_mutex_lock(&jsrt_mutex);
-		for(i=0; i<JSRT_QUEUE_SIZE; i++) {
-			if(!jsrt_queue[i].created) {
-				jsrt_queue[i].rt=JS_NewRuntime(maxbytes);
-				if(jsrt_queue[i].rt != NULL) {
-					jsrt_queue[i].maxbytes=maxbytes;
-					jsrt_queue[i].created=1;
-					jsrt_queue[i].used=0;
-				}
-			}
-			if(!jsrt_queue[i].used) {
-				last_unused=i;
-				if(jsrt_queue[i].created && jsrt_queue[i].maxbytes == maxbytes) {
-					jsrt_queue[i].used=1;
-					pthread_mutex_unlock(&jsrt_mutex);
-					return(jsrt_queue[i].rt);
-				}
-			}
-		}
-
-		if(last_unused != -1) {
-			jsrt_queue[last_unused].used=1;
-			pthread_mutex_unlock(&jsrt_mutex);
-			return(jsrt_queue[last_unused].rt);
-		}
-		pthread_mutex_unlock(&jsrt_mutex);
-	}
-
-	return(NULL);
+	pthread_mutex_lock(&jsrt_mutex);
+	ret=JS_NewRuntime(maxbytes);
+	listPushNode(&rt_list, ret);
+	pthread_mutex_unlock(&jsrt_mutex);
+	return ret;
 #endif
 }
 
 void DLLCALL jsrt_Release(JSRuntime *rt)
 {
 #if (RT_TYPE==RT_UNIQUE)
-	int	i;
+	list_node_t	*node;
 
-	for(i=0; i<JSRT_QUEUE_SIZE; i++) {
-		if(rt==jsrt_queue[i].rt) {
-			pthread_mutex_lock(&jsrt_mutex);
-			jsrt_queue[i].used=0;
-			/* TODO: Clear "stuff"? */
-			pthread_mutex_unlock(&jsrt_mutex);
-			sem_post(&jsrt_sem);
-		}
-	}
+	pthread_mutex_lock(&jsrt_mutex);
+	node = listFindNode(&rt_list, rt, 0);
+	if(node)
+		listRemoveNode(&rt_list, node, FALSE);
+	JS_DestroyRuntime(rt);
+	pthread_mutex_unlock(&jsrt_mutex);
 #endif
 }
