813dfbb881a1d5ed2acbc33ae61c7056d5bffaad
deuce <>
2014-04-24 06:22:06 +0000

    Coverity: Unlikely memory leak in unused recvfilesocket() function.

--- src/xpdev/sockwrap.c
+++ src/xpdev/sockwrap.c
@@ -260,9 +260,12 @@ int DLLCALL recvfilesocket(int sock, int file, off_t *offset, off_t count)
 		return(-1);
 	}
 
-	if(offset!=NULL)
-		if(lseek(file,*offset,SEEK_SET)<0)
+	if(offset!=NULL) {
+		if(lseek(file,*offset,SEEK_SET)<0) {
+			free(buf);
 			return(-1);
+		}
+	}
 
 	rd=read(sock,buf,(size_t)count);
 	if(rd!=count) {
