9f52a73abf30175f1efca343b322722d23acc703
rswindell <>
2014-06-10 00:56:20 +0000

    Save the zone_blind threshold value (e.g. '4') when saving the sbbsecho.cfg file. Previously, any threshold set by manually editing the .cfg file would be lost when saving changes using echocfg.

--- src/sbbs3/echocfg.c
+++ src/sbbs3/echocfg.c
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2013 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2014 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -1161,8 +1161,12 @@ int main(int argc, char **argv)
 				}
 				if(!cfg.check_path)
 					fprintf(stream,"NOPATHCHECK\n");
-				if(cfg.zone_blind)
-					fprintf(stream,"ZONE_BLIND\n");
+				if(cfg.zone_blind) {
+					fprintf(stream,"ZONE_BLIND");
+					if(cfg.zone_blind_threshold != 0xffff)
+						fprintf(stream," %u", cfg.zone_blind_threshold);
+					fprintf(stream,"\n");
+				}
 				if(cfg.notify)
 					fprintf(stream,"NOTIFY %u\n",cfg.notify);
 				if(misc&CONVERT_TEAR)
