4c8838ce1f5869969ed353bcfaa5cd389a445ef7
rswindell <>
2014-08-19 01:12:56 +0000

    Added mssing UQ_COLORTERM definition as reported by Nightfox.

--- exec/load/sbbsdefs.js
+++ exec/load/sbbsdefs.js
@@ -361,7 +361,8 @@ var   UQ_XEDIT		=(1<<15);	/* Ask for external editor					*/
 var   UQ_NODEF		=(1<<16);	/* Don't ask for default settings			*/
 var   UQ_NOCOMMAS	=(1<<17);	/* Do not require commas in location		*/
 var   UQ_NONETMAIL	=(1<<18);	/* Don't ask for e-mail/netmail address		*/
-var   UQ_NOUPRLWR	=(1<<19);	/* Don't force upper/lower case strings		*/
+var   UQ_NOUPRLWR   =(1<<19);   /* Don't force upper/lower case strings		*/
+var   UQ_COLORTERM  =(1<<20);   /* Ask if new user has color terminal	    */
 					    		/********************************************/
 
 							    /********************************************/
