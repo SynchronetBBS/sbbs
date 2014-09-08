9e4abed837227f992630b4602b71062902e0225f
echicken <>
2014-09-08 07:26:37 +0000

    New and re-written SyncWall.  Update exec/load/frame.js and exec/load/ansiedit.js before using.  Resolves some annoying display bugs, is generally more gooder and shit.

--- xtrn/syncwall/syncwall.js
+++ xtrn/syncwall/syncwall.js
@@ -1,172 +1,430 @@
-// SyncWall InterBBS Graffiti Wall
-// by echicken -at- bbs.electronicchicken.com
-// $Id$
-
-load("ansiedit.js");
+load("ansiedit.js"); // Loads frame.js, tree.js, funclib.js, sbbsdefs.js
 load("json-client.js");
+load("event-timer.js");
+load("http.js");
 
-js.branch_limit = 0;
+var delay = 10,
+	page = 100,
+	dbName = "SYNCWALL2";
 
-var chDelay = 10; // Milliseconds between characters when loading the buffer
-var chBuffer = 100; // Max character history to read at once 
+var	attr, systemStatus, users, systems, months,
+	frame, titleFrame, editorFrame, statusFrame, promptFrame,
+	editor, jsonClient, timer, timedEvent,
+	lastUser = -1, lastSystem = -1;
 
-var monthYear = strftime("%m%y", time());
+var updateStatus = function(u, s, altUsers, altSystems) {
 
-var server_file = new File(file_cfgname(js.exec_dir, "server.ini"));
-server_file.open('r', true);
-var server = server_file.iniGetValue(null, "host", "localhost");
-var port = server_file.iniGetValue(null, "port", 10088);
-server_file.close();
-
-var ch;
-var lastUser = "";
-var lastSystem = "";
-var userInput = "";
-
-var bgColours = [ 
-	BG_BLUE,
-	BG_CYAN,
-	BG_GREEN,
-	BG_MAGENTA,
-	BG_RED,
-	BG_BROWN
-];
-
-var ansiClient = new JSONClient(server, port);
-if(!ansiClient.socket.is_connected)
-	exit();
-
-var frame, topBar, bottomBar, ansi;
-
-function init() {
-	frame = new Frame(1, 1, 80, 24, BG_BLACK);
-	topBar = new Frame(1, 1, 80, 1, BG_BLUE|WHITE, frame);
-	bottomBar = new Frame(1, 24, 80, 1, BG_BLUE|WHITE, frame);
-	ansiClient.callback = drawStuff;
-	ansiClient.subscribe("syncwall", "canvas." + monthYear + ".ch");
-	topBar.putmsg(".: SyncWall InterBBS Graffiti Wall : Hit [ESC] to Exit :.");
-	topBar.gotoxy(70, 1);
-	topBar.putmsg("\1h\1bby echicken");
-	frame.open();
-	ansi = new ansiEdit(1, 2, 80, 22, BG_BLACK|LIGHTGRAY, frame);
-	frame.cycle();
-}
-
-function drawStuff(update) {
-	if(update.oper.toUpperCase() == "SUBSCRIBE" || update.oper.toUpperCase() == "UNSUBSCRIBE")
+	if(u == lastUser && s == lastSystem)
 		return;
-	var location = update.location.split(".").shift().toUpperCase();
-	if(location != "CANVAS")
-		return;
-	putCh(update.data, false);
-	return;
-}
 
-function putCh(ch, skipMsg) {
-	ch.attr = parseInt(ch.attr);
-	if(ch.x < 1)
-		ch.x = 1;
-	if(ch.y < 1)
-		ch.y = 1;
-	if(ch.x > 80)
-		ch.x = 80;
-	if(ch.y > 21)
-		ch.y = 21;
-	ansi.putChar(ch);
-	if(ch.userAlias != lastUser || ch.system != lastSystem) {
-		var randomAttr = bgColours[Math.floor(Math.random()*bgColours.length)]|WHITE;
-		bottomBar.attr = randomAttr;
-		bottomBar.clear();
-		bottomBar.putmsg(ch.userAlias + "@" + ch.system + " is drawing");
-		if(skipMsg) {
-			bottomBar.gotoxy(60, 1);
-			bottomBar.putmsg("<Hit [SPACE] to skip>");
+	// Derp, how does I scope?  Too late, too much drinkings yesno?
+	var usrs = (typeof altUsers == "undefined") ? users : altUsers;
+	var systms = (typeof altSystems == "undefined") ? systems : altSystems;
+
+	var rbg = [BG_BLUE, BG_CYAN, BG_GREEN, BG_MAGENTA, BG_RED, BG_BROWN];
+	statusFrame.attr = rbg[Math.floor(Math.random()*rbg.length)]|WHITE
+	statusFrame.clear();
+	statusFrame.putmsg(format("%s@%s is drawing", usrs[u], systms[s]));
+	lastUser = u;
+	lastSystem = s;
+	editor.cycle(true);
+	if(typeof timedEvent != "undefined")
+		timedEvent.abort = true;
+	timedEvent = timer.addEvent(
+		5000,
+		false,
+		function() {
+			statusFrame.clear();
+			lastUser = -1;
+			lastSystem = -1;
+			editor.cycle(true);
 		}
-		lastUser = ch.userAlias;
-		lastSystem = ch.system;
+	);
+
+}
+
+var fetchHistory = function(month) {
+
+	try {
+		var historyDir = js.exec_dir + "history";
+		if(!file_exists(historyDir) || !file_isdir(historyDir))
+			mkdir(historyDir);
+		var fn = month.split("/");
+		fn = historyDir + "/" + fn[fn.length - 1];
+		if(!file_exists(fn)) {
+			var http = new HTTPRequest();
+			var body = http.Get(month);
+			var f = new File(fn);
+			f.open("wb");
+			f.write(body);
+			f.close();
+		}
+		loadLocalHistory(fn);
+	} catch(err) {
+		log(LOG_ERR, "SyncWall error: " + err);
 	}
-	return;
+
 }
 
-function cleanUp() {
-	frame.close();
-	ansi.close();
-	ansiClient.unsubscribe("syncwall", "canvas." + monthYear + ".ch");
-	ansiClient.disconnect();
-	console.clear();
-}
-
-function loadHistory() {
-	var index = 0; 
-	var endex = chBuffer;
-	var canvasLength = ansiClient.read("syncwall", "canvas." + monthYear + ".history.length", 1);
-	if(canvasLength === undefined) {
-		ansiClient.write("syncwall", "canvas." + monthYear, { history : [] }, 2);
-	} else {
-		ansi.showUI(false);
-		while(index < canvasLength) {
-			var canvas = ansiClient.slice("syncwall", "canvas." + monthYear + ".history", index, endex, 1);
-			index += chBuffer;
-			endex += chBuffer;
-			while(canvas.length > 0) {
-				if(frame.cycle())
-					console.gotoxy(80, 24);
-				putCh(canvas.shift(), true);
-				var cmd = console.inkey(K_NONE, chDelay);
-				if(ascii(cmd) == 27) {
-					cleanUp();
-					exit();
+var loadLocalHistory = function(fn) {
+	var f = new File(fn);
+	f.open("r");
+	var history = JSON.parse(f.read());
+	f.close();
+	editor.getcmd("\x09");
+	editor.clear();
+	promptFrame.top();
+	promptFrame.putmsg(" Hit <SPACE> to skip");
+	lastUser = -1;
+	lastSystem = -1;
+	for(var h = 0; h < history.SEQUENCE.length; h++) {
+		if(history.SEQUENCE[h].c == "CLEAR") {
+			editor.clear();
+		} else {
+			editor.putChar(
+				{	'x' : history.SEQUENCE[h].x,
+					'y' : history.SEQUENCE[h].y,
+					'ch' : history.SEQUENCE[h].c,
+					'attr' : history.SEQUENCE[h].a
 				}
-				else if(cmd == " ") {
-					var canvas = ansiClient.read("syncwall", "canvas." + monthYear + ".data", 1);
-					ansi.putChar({ x : 1, y: 1, ch : "CLEAR", attr : 0 });
-					for(var c in canvas) {
-						for(var y in canvas[c])
-							ansi.putChar(canvas[c][y]);
-					}
-					index = canvasLength;
-					break;
-				}
+			);
+			updateStatus(
+				history.SEQUENCE[h].u,
+				history.SEQUENCE[h].s,
+				history.USERS,
+				history.SYSTEMS
+			);
+			editor.cycle();
+			frame.cycle();
+			if(console.inkey(K_NONE) == " ") {
+				loadState(history.STATE);
+				break;
 			}
+
+			if(h < history.SEQUENCE.length - 1)
+				mswait(delay);
 		}
-		ansi.showUI(true);
 	}
-	bottomBar.clear();
-	lastUser = "";
-	lastSystem = "";
-	console.gotoxy(80, 24);
+
+	promptFrame.bottom();
+	statusFrame.clear();
+	statusFrame.putmsg("Hit any key to continue");
+	editor.cycle(true);
+	frame.cycle();
+	console.getkey();
+	statusFrame.clear();
+	loadState();
+
 }
 
-function main() {
-	while(ascii(userInput) != 27) {
-		if(frame.cycle())
-			console.gotoxy(80, 24);
-		ansiClient.cycle();
-		userInput = console.inkey(K_NONE, 5);
-		if(userInput == "")
-			continue;
-		ch = ansi.getcmd(userInput);
-		if(!ch.ch)
-			continue;
-		if(lastUser != user.alias || lastSystem != system.name) {
-			bottomBar.attr = BG_BLUE|WHITE;
-			bottomBar.clear();
-			bottomBar.putmsg("You are drawing");
-			lastUser = user.alias;
-			lastSystem = system.name;
+var loadHistory = function() {
+
+	promptFrame.putmsg(" Hit <SPACE> to skip");
+
+	var len = jsonClient.read(dbName, "SEQUENCE.length", 1);
+	pageLoop:
+	for(var i = 0; i < len; i = i + ((len - i > page) ? page : len - i)) {
+		var history = jsonClient.slice(
+			dbName,
+			"SEQUENCE",
+			i,
+			((len - i > page) ? i + page : len),
+			1
+		);
+		for(var h = 0; h < history.length; h++) {
+			if(history[h].c == "CLEAR") {
+				editor.clear();
+			} else {
+				editor.putChar(
+					{	'x' : history[h].x,
+						'y' : history[h].y,
+						'ch' : history[h].c,
+						'attr' : history[h].a
+					}
+				);
+				updateStatus(history[h].u, history[h].s);
+			}
+			editor.cycle();
+			frame.cycle();
+			if(console.inkey(K_NONE) == " ") {
+				loadState();
+				break pageLoop;
+			}
+			// Loading the next 'page' history elements will delay us anyhow.
+			if(h < history.length - 1)
+				mswait(delay);
 		}
-		ch.userAlias = user.alias;
-		ch.system = system.name;
-		if(ch.ch == "CLEAR")
-			ansiClient.write("syncwall", "canvas." + monthYear + ".data", {}, 2);
-		else
-			ansiClient.write("syncwall", "canvas." + monthYear + ".data." + ch.x + "." + ch.y, { x : ch.x, y : ch.y, ch : ch.ch, attr: ch.attr }, 2);
-		ansiClient.write("syncwall", "canvas." + monthYear + ".ch", ch, 2);
-		ansiClient.push("syncwall", "canvas." + monthYear + ".history", ch, 2);
+	}
+
+	promptFrame.bottom();
+	statusFrame.clear();
+	editor.cycle(true);
+	lastUser = -1;
+	lastSystem = -1;
+
+}
+
+var loadState = function(state) {
+	if(typeof state == "undefined")
+		var state = jsonClient.read(dbName, "STATE", 1);
+	editor.clear();
+	for(var y in state) {
+		for(var x in state[y]) {
+			editor.putChar(
+				{	'x' : parseInt(x),
+					'y' : parseInt(y),
+					'ch' : state[y][x].c,
+					'attr' : state[y][x].a
+				}
+			);
+		}
 	}
 }
 
-init();
-loadHistory();
-main();
-cleanUp();
+var sendUpdate = function(ch) {
+
+	jsonClient.write(
+		dbName,
+		"LATEST",
+		{	'x' : ch.x,
+			'y' : ch.y,
+			'c' : ch.ch,
+			'a' : ch.attr,
+			'u' : users.indexOf(user.alias),
+			's' : systems.indexOf(system.name)
+		},
+		2
+	);
+
+}
+
+var processUpdate = function(update) {
+
+	if(update.oper != "WRITE")
+		return;
+
+	var location = update.location.split(".");
+
+	if(location[0] == "USERS") {
+		users.push(update.data[update.data.length - 1]);
+	} else if(location[0] == "SYSTEMS") {
+		systems.push(update.data);
+	} else if(location[0] == "LATEST") {
+		if(update.data.c == "CLEAR") {
+			editor.clear();
+		} else {
+			editor.putChar(
+				{	'x' : update.data.x,
+					'y' : update.data.y,
+					'ch' : update.data.c,
+					'attr' : update.data.a
+				}
+			);
+		}
+		updateStatus(update.data.u, update.data.s);
+	}
+
+}
+
+var initDisplay = function() {
+
+	attr = console.attributes;
+	systemStatus = bbs.sys_status;
+	bbs.sys_status |= SS_MOFF;
+	console.clear(LIGHTGRAY);
+
+}
+
+var initFrames = function() {
+
+	frame = new Frame(
+		1,
+		1,
+		console.screen_columns,
+		console.screen_rows,
+		LIGHTGRAY
+	);
+
+	titleFrame = new Frame(
+		frame.x,
+		frame.y,
+		frame.width,
+		1,
+		BG_BLUE|WHITE,
+		frame
+	);
+	titleFrame.putmsg(
+		"\1h\1w.: SyncWall InterBBS Graffiti Wall :. Hit [ESC] to exit"
+	);
+	titleFrame.gotoxy(titleFrame.width - 10, 1);
+	titleFrame.putmsg("\1h\1bby echicken"); // Aww yeah, bby. :|
+
+	statusFrame = new Frame(
+		frame.x,
+		frame.y + frame.height - 1,
+		frame.width,
+		1,
+		BG_BLUE|WHITE,
+		frame
+	);
+
+	promptFrame = new Frame(
+		statusFrame.width - 20,
+		statusFrame.y,
+		21,
+		1,
+		BG_BLUE|WHITE,
+		statusFrame
+	);
+
+	editorFrame = new Frame(
+		frame.x,
+		titleFrame.y + titleFrame.height,
+		frame.width,
+		frame.height - titleFrame.height - statusFrame.height,
+		LIGHTGRAY,
+		frame
+	);
+
+	frame.open();
+
+}
+
+var initEditor = function() {
+
+	editor = new ANSIEdit(
+		{	'x' : editorFrame.x,
+			'y' : editorFrame.y,
+			'width' : editorFrame.width,
+			'height' : editorFrame.height,
+			'attr' : LIGHTGRAY,
+			'parentFrame' : frame,
+			'menuHeading' : "SyncWall Menu"
+		}
+	);
+
+	editor.open();
+	var historyTree = editor.menu.addTree("View previous month");
+	for(var m in months) {
+		month = months[m].split("/");
+		month = month[month.length - 1].replace(".json", "");
+		month = format(" %s/%s", month.substr(0, 2), month.substr(2));
+		historyTree.addItem(month, fetchHistory, months[m]);
+	}
+
+}
+
+var initUsers = function() {
+
+	users = jsonClient.read(dbName, "USERS", 1);
+	if(!users || users.indexOf(user.alias) < 0) {
+		jsonClient.push(dbName, "USERS", user.alias, 2);
+		users = jsonClient.read(dbName, "USERS", 1);
+	}
+	jsonClient.subscribe(dbName, "USERS");
+
+}
+
+var initSystems = function() {
+
+	systems = jsonClient.read(dbName, "SYSTEMS", 1);
+	if(!systems || systems.indexOf(system.name) < 0) {
+		jsonClient.push(dbName, "SYSTEMS", system.name, 2);
+		systems = jsonClient.read(dbName, "SYSTEMS", 1);
+	}
+	jsonClient.subscribe(dbName, "SYSTEMS");
+
+}
+
+var initJSON = function() {
+
+	if(file_exists(js.exec_dir + "server.ini")) {
+		var f = new File(js.exec_dir + "server.ini");
+		f.open("r");
+		var ini = f.iniGetObject();
+		f.close();
+		var host = ini.host;
+		var port = ini.port;
+	} else {
+		var f = new File(system.ctrl_dir + "services.ini");
+		f.open("r");
+		var ini = f.iniGetObject("JSON");
+		f.close();
+		var host = "localhost";
+		var port = (ini === null) ? 10088 : Number(ini.port);
+	}
+	jsonClient = new JSONClient(host, port);
+	jsonClient.callback = processUpdate;
+	jsonClient.subscribe(dbName, "LATEST");
+	months = jsonClient.read(dbName, "MONTHS", 1);
+	if(!months)
+		months = [];
+
+}
+
+var init = function() {
+
+	timer = new Timer();
+
+	initDisplay();
+	initJSON();
+	initFrames();
+	initEditor();
+	initUsers();
+	initSystems();
+	loadHistory();
+
+}
+
+var cycle = function() {
+
+	frame.cycle();
+	editor.cycle();
+	jsonClient.cycle();
+	timer.cycle();
+
+}
+
+var input = function() {
+
+	var userInput = console.inkey(K_NONE, 5);
+	if(userInput == "")
+		return true;
+	if(ascii(userInput) == 27)
+		return false;
+	var ret = editor.getcmd(userInput);
+	if(ret.ch != false || ascii(ret.ch) == 32) {
+		updateStatus(users.indexOf(user.alias), systems.indexOf(system.name));
+		sendUpdate(ret);
+	}
+	return true;
+
+}
+
+var main = function() {
+
+	while(!js.terminated) {
+		if(!input())
+			break;
+		cycle();
+	}
+
+}
+
+var cleanUp = function() {
+
+	jsonClient.disconnect();
+	editor.close();
+	frame.close();
+	console.clear(attr);
+	bbs.sys_status = systemStatus;
+}
+
+try {
+	init();
+	main();
+	cleanUp();
+	exit();
+} catch(err) {
+	log(LOG_ERR, "SyncWall: " + err);
+}
