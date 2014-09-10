cf28f0e76e49a220d211e8bee0be07b05c860433
echicken <>
2014-09-10 04:03:52 +0000

    The pipe dream continues flowing through the sewing.  Old display bugs gone, maybe some exciting new ones introduced.  Maybe faster, maybe not so much.  Who knows?

--- xtrn/ecreader/ecReader.js
+++ xtrn/ecreader/ecReader.js
@@ -1,468 +1,150 @@
-// ecReader
-// A lightbar, threaded message reader for Synchronet 3.16+
-// echicken -at- bbs.electronicchicken.com, 2012
-
-js.branch_limit = 0;
-console.clear();
-console.putmsg("\1h\1wecReader by echicken, loading message threads...");
-
 load("sbbsdefs.js");
-load("msgutils.js");
 load("frame.js");
-load("tree.js");
-load(js.exec_dir + "msglib.js");
+load("lib.js");
 
-var maxMessages = 0;			// How many messages to load per sub, 0 for all.
-var threaded = true;			// False to default to flat view
-var setPointers = true; 		// False to leave scan pointers unaffected by reading
-var reverseThreads = true;		// True to list threads from most to least recently updated
-var reverseMessages = false;	// True to list messages in a thread from newest to oldest
-var lbg = BG_CYAN;				// Lightbar background
-var lfg = WHITE;				// Foreground colour of highlighted text
-var nfg = LIGHTGRAY;			// Foreground colour of non-highlighted text
-var xfg = LIGHTCYAN;			// Subtree expansion indicator colour
-var tfg = LIGHTCYAN;			// Uh, that line beside subtree items
-var fbg = BG_BLUE;				// Title, Header, Help frame background colour
-var urm = WHITE;				// Unread message foreground colour
-var hfg = "\1h\1w"; 			// Heading text (CTRL-A format, for now)
-var sffg = "\1h\1c";			// Heading sub-field text (CTRL-A format, for now)
-var mfg = "\1n\1w";				// Message text colour (CTRL-A format, for now)
+var frame, chooser, lister, reader, attr, systemStatus, settings;
 
-var messages, tree, frame, titleFrame, columnFrame, treeFrame, helpFrame,
-	messageFrame, headerFrame, bodyFrame, messageBar, promptFrame,
-	promptSubFrame;
+var state = {
+	listing : true,
+	reading : false
+};
 
-var headerTextWidth = console.screen_columns - 9;
-var msgNumberWidth = 9;
-var dateWidth = 17;
-var subjectWidth = Math.round(((console.screen_columns - 2) - (msgNumberWidth + dateWidth)) / 2);
-var nameWidth = Math.round(subjectWidth / 2);
-while(msgNumberWidth + dateWidth + subjectWidth + nameWidth > (console.screen_columns - 2))
-	subjectWidth = subjectWidth - 1;
-
-var formatItem = function(messageNumber, from, to, subject, wwt) {
-	var retval = 
-		format(
-			"%-8s%-" + nameWidth + "s%-" + nameWidth + "s%-" + subjectWidth + "s%s",
-			messageNumber,
-			from.substr(0, nameWidth - 1),
-			to.substr(0, nameWidth - 1),
-			subject.substr(0, subjectWidth - 1),
-			strftime("%m-%d-%Y %H:%I", wwt)
-		);
-	return retval;
-}
-
-var getFlatList = function(code) {
-	var msgBase = new MsgBase(code);
-	var header;
-	var item;
-	try {
-		msgBase.open();
-		var start = (maxMessages == 0) ? msgBase.first_msg : msgBase.last_msg - maxMessages;
-		for(var m = start; m <= msgBase.last_msg; m++) {
-			header = msgBase.get_msg_header(m);
-			if(
-				header === null || header.attr&MSG_DELETE
-				||
-				(!msgBase.hasOwnProperty('cfg') && header.to != user.alias && header.to != user.name && header.to_ext != user.number)
-			) {
-				continue;
-			}
-			messages.push(header);
-		}
-		msgBase.close();
-		if(reverseThreads)
-			messages.reverse();
-		for(var m = 0; m < messages.length; m++) {
-			item = formatItem(messages[m].number, messages[m].from, messages[m].to, messages[m].subject, messages[m].when_written_time);
-			var i = tree.addItem(item, showMessage, messages[m], code);
-			if(code != "mail" && messages[messages.length - 1].number > msg_area.grp_list[bbs.curgrp].sub_list[bbs.cursub].scan_ptr)
-				i.attr = urm;
-			else if(code == "mail" && !(messages[messages.length - 1].attr&MSG_READ))
-				i.attr = urm;
-		}
-	} catch(err) {
-		log(LOG_DEBUG, err);
-	}
-	tree.open();
-	tree.cycle();
-}
-
-var getThreadedList = function(code) {
-	var threads = getMessageThreads(code);
-	var msgBase = new MsgBase(code);
-	var item;
-	
-	try {
-		for(var t in ((!reverseThreads)?threads.thread:threads.order)) {
-			var theThread = (reverseThreads)?threads.thread[threads.order[t]]:threads.thread[t];
-			msgBase.open();
-			var header = msgBase.get_msg_header(theThread.messages[0].number);
-			msgBase.close();
-			messages.push(header);
-			item = formatItem(
-				theThread.messages[0].number,
-				theThread.messages[0].from,
-				theThread.messages[0].to,
-				theThread.messages[0].subject,
-				theThread.messages[0].when_written_time
-			);
-			
-			if(theThread.messages.length == 1)
-				var i = tree.addItem(item, showMessage, messages[messages.length - 1], code);
-			else
-				var i = tree.addTree(item);
-				
-			if(msgBase.hasOwnProperty('cfg') && messages[messages.length - 1].number > msg_area.grp_list[bbs.curgrp].sub_list[bbs.cursub].scan_ptr)
-				i.attr = urm;
-			else if(!msgBase.hasOwnProperty('cfg') && !(messages[messages.length - 1].attr&MSG_READ))
-				i.attr = urm;
-			
-			if(theThread.messages.length == 1)
-				continue;
-			
-			var mm = (reverseMessages) ? theThread.messages.length - 1 : 0;
-			for(var m = 0; m < theThread.messages.length; m++) {
-				msgBase.open();
-				var header = msgBase.get_msg_header(theThread.messages[mm].number);
-				msgBase.close();
-				messages.push(header);
-				item = formatItem(
-					theThread.messages[mm].number,
-					theThread.messages[mm].from,
-					theThread.messages[mm].to,
-					theThread.messages[mm].subject,
-					theThread.messages[mm].when_written_time
-				);
-				var ii = i.addItem(item, showMessage, messages[messages.length - 1], code);
-				if(!msgBase.hasOwnProperty('cfg') && messages[messages.length - 1].number > msg_area.grp_list[bbs.curgrp].sub_list[bbs.cursub].scan_ptr) {
-					ii.attr = urm;
-					i.attr = urm;
-				} else if(msgBase.hasOwnProperty('cfg') && messages[messages.length - 1].attr&MSG_READ == 0) {
-					ii.attr = urm;
-					i.attr = urm;
-				}
-				mm = (reverseMessages) ? mm - 1 : mm + 1;
-			}
-		}
-		tree.open();
-		tree.cycle();
-	} catch(err) {
-		log(LOG_DEBUG, err);
-	}
-}
-
-var getList = function(code) {
-	if(tree.status&1) {
-		tree.close();
-		treeFrame.clear();
-	}
-	tree = new Tree(treeFrame);
-	tree.colors.lbg = lbg;
-	tree.colors.lfg = lfg;
-	tree.colors.fg = nfg;
-	tree.colors.xfg = xfg;
-	tree.colors.tfg = tfg;
-	messages = [];
-	var msgBase = new MsgBase(code);
-	if(msgBase.hasOwnProperty('cfg')) {
-		var mg = msgBase.cfg.grp_name;
-		var ms = msgBase.cfg.name;
-	} else {	
-		var mg = "Private email";
-		var ms = "Private email";
-	}
-	titleFrame.clear();
-	titleFrame.putmsg(hfg + "Message Group: " + sffg + mg);
-	titleFrame.crlf();
-	titleFrame.putmsg(hfg + "    Sub Board: " + sffg + ms);
-	(threaded) ? getThreadedList(code) : getFlatList(code);
-}
-
-var deleteMessage = function(header, code) {
-	var ret = false;
-	var msgBase = new MsgBase(code);
-	msgBase.open();
-	if(
-		((msgBase.hasOwnProperty('cfg') && msg_area.grp_list[bbs.curgrp].sub_list[bbs.cursub].is_operator)
-		||
-		(!msgBase.hasOwnProperty('cfg')))
-		&&
-		prompt("Delete message #" + header.number)
-	)
-		{
-			msgBase.remove_msg(header.number);
-		}
-	msgBase.close();
-	return;
-}
-
-var sendEmail = function() {
-	var ret = true;
-	console.putmsg(hfg + "Send email to: ");
-	var to = console.getstr('', 64, K_LINE);
-	if(to == "")
-		ret = false;
-	else if(system.matchuser(to) > 0)
-		bbs.email(system.matchuser(to, WM_EMAIL));
-	else if(netaddr_type(to) != NET_NONE)
-		bbs.netmail(to, WM_NETMAIL)
-	else
-		ret = false;
-	if(!ret) {
-		console.putmsg(hfg + "Invalid user or netmail/email address.");
-		console.crlf();
-		console.pause();
-	}
-}
-
-var prompt = function(str) {
-	promptFrame.top();
-	promptSubFrame.clear();
-	frame.cycle();
-	console.gotoxy(promptSubFrame.x, promptSubFrame.y + 1);
-	var ret = console.yesno(str);
-	promptFrame.bottom();
-	frame.invalidate();
-	return ret;
-}
-
-var showMessage = function(header, code) {
-	var retval = "REFRESH";
-	var userInput = false;
-	var n = header.number;
-	var currentMessage = messages.indexOf(header);
-
-	messageFrame.open();
-	messageFrame.top();
-	var msgBase = new MsgBase(code);
-	msgBase.open();
-	if(setPointers && msgBase.hasOwnProperty('cfg') && header.number > msg_area.grp_list[bbs.curgrp].sub_list[bbs.cursub].scan_ptr) {
-		msg_area.grp_list[bbs.curgrp].sub_list[bbs.cursub].scan_ptr = header.number;
-	} else if(setPointers && !msgBase.hasOwnProperty('cfg')) {
-		header.attr|=MSG_READ;
-		msgBase.put_msg_header(header.number, header);
-	}
-	var body = msgBase.get_msg_body(header.number);
-	msgBase.close();
-
-	headerFrame.putmsg(
-		format(
-			hfg + "%14s " + sffg + header.subject.substr(0, headerTextWidth) + "\r\n"
-			+ hfg + "%14s " + sffg + header.from.substr(0, headerTextWidth) + "\r\n"
-			+ hfg + "%14s " + sffg + header.to.substr(0, headerTextWidth) + "\r\n"
-			+ hfg + "%14s " + sffg + system.timestr(header.when_written_time),
-			"Subject:", "From:", "To:", "Date:"
-		)
-	);
-	bodyFrame.scrollTo(0, 0);
-	bodyFrame.putmsg(mfg + word_wrap(body));
-	bodyFrame.scrollTo(0, 0);
-
-	while(userInput != "Q") {
-		if(frame.cycle())
-			console.gotoxy(console.screen_columns, console.screen_rows);
-		userInput = console.getkey().toUpperCase();
-		switch(userInput) {
+// Well, parse it enough for our porpoises, anyway...
+var parseCtrlA = function(str) {
+	var arr = str.toUpperCase().split("\\1");
+	var light = (arr.indexOf("H") > arr.indexOf("N"));
+	var attr = 0;
+	for(var a = 0; a < arr.length; a++) {
+		switch(arr[a]) {
+			case "K":
+				attr |= ((light) ? DARKGRAY : BLACK);
+				break;
 			case "R":
-				var f = new File(system.node_dir + "QUOTES.TXT");
-				f.open("w");
-				f.write(body);
-				f.close();
-				frame.invalidate();
-				console.clear();
-				if(msgBase.hasOwnProperty('cfg'))
-					bbs.post_msg(msgBase.cfg.code, WM_QUOTE, header);
-				else if(!msgBase.hasOwnProperty('cfg') && header.from_net_type == NET_NONE)
-					bbs.email(parseInt(header.from_ext), WM_EMAIL|WM_QUOTE, "", header.subject);
-				else if(!msgBase.hasOwnProperty('cfg'))
-					bbs.netmail(header.from_net_addr, WM_NETMAIL|WM_QUOTE, header.subject);
-				frame.draw();
-				retval = header;
-				userInput = "Q";
-				getList((msgBase.hasOwnProperty('cfg'))?msgBase.cfg.code:"mail");
+				attr |= ((light) ? LIGHTRED : RED);
 				break;
-			case "P":
-				if(!threaded && currentMessage < (messages.length - 1)) {
-					currentMessage++;
-					h = messages[currentMessage];
-					retval = h;
-					userInput = "Q";
-				} else if(threaded && currentMessage > 0) {
-					currentMessage = currentMessage - 1;
-					h = messages[currentMessage];
-					retval = h;
-					userInput = "Q";
-				}
+			case "G":
+				attr |= ((light) ? LIGHTGREEN : GREEN);
 				break;
-			case "N":
-				if(!threaded && currentMessage > 0) {
-					currentMessage = currentMessage - 1;
-					h = messages[currentMessage];
-					retval = h;
-					userInput = "Q";
-				} else if(threaded && currentMessage < (messages.length - 1)) {
-					currentMessage++;
-					h = messages[currentMessage];
-					retval = h;
-					userInput = "Q";
-				}
+			case "Y":
+				attr |= ((light) ? YELLOW : BROWN);
 				break;
-			case KEY_UP:
-				bodyFrame.scroll(0, -1);
+			case "B":
+				attr |= ((light) ? LIGHTBLUE : BLUE);
 				break;
-			case KEY_DOWN:
-				bodyFrame.scroll(0, 1);
+			case "M":
+				attr |= ((light) ? LIGHTMAGENTA : MAGENTA);
 				break;
-			case KEY_HOME:
-				bodyFrame.scrollTo(0, 0);
+			case "C":
+				attr |= ((light) ? LIGHTCYAN : CYAN);
 				break;
-			case KEY_END:
-				bodyFrame.end();
-				bodyFrame.scrollTo(0, bodyFrame.data_height - bodyFrame.height);
+			case "W":
+				attr |= ((light) ? WHITE : LIGHTGRAY);
 				break;
-			case "[":
-				if(bodyFrame.offset.y - bodyFrame.height < 0)
-					bodyFrame.scrollTo(0, 0);
-				else
-					bodyFrame.scroll(0, -bodyFrame.height);
+			case "0":
+				attr |= BG_BLACK;
 				break;
-			case "]":
-				if(bodyFrame.offset.y + bodyFrame.height > bodyFrame.data_height)
-					bodyFrame.scrollTo(bodyFrame.data_height - bodyFrame.height);
-				else
-					bodyFrame.scroll(0, bodyFrame.height);
+			case "1":
+				attr |= BG_RED;
 				break;
-			case KEY_DEL:
-				if(deleteMessage(header, code)) {
-					userInput = "Q";
-					retval = "REFRESH";
-				}
+			case "2":
+				attr |= BG_GREEN;
+				break;
+			case "3":
+				attr |= BG_BROWN;
+				break;
+			case "4":
+				attr |= BG_BLUE;
+				break;
+			case "5":
+				attr |= BG_MAGENTA;
+				break;
+			case "6":
+				attr |= BG_CYAN;
+				break;
+			case "7":
+				attr |= BG_LIGHTGRAY;
 				break;
 			default:
 				break;
 		}
 	}
-	messageFrame.clear();
-	headerFrame.clear();
-	bodyFrame.clear();
-	if(retval == "REFRESH")
-		messageFrame.close();
-	return retval;
+	return attr;
+}
+
+var initSettings = function() {
+	var f = new File(js.exec_dir + "settings.ini");
+	f.open("r");
+	settings = f.iniGetObject();
+	f.close();
+	for(var s in settings) {
+		if(typeof settings[s] == "string")
+			settings[s] = parseCtrlA(settings[s]);
+	}
 }
 
 var init = function() {
-	frame = new Frame(1, 1, console.screen_columns, console.screen_rows, BG_BLACK|WHITE);
-	titleFrame = new Frame(1, 1, console.screen_columns, 2, fbg|WHITE, frame);
-	columnFrame = new Frame(1, 3, console.screen_columns, 1, fbg|WHITE, frame);
-	treeFrame = new Frame(1, 4, console.screen_columns, console.screen_rows - 4, BG_BLACK|WHITE, frame);
-	helpFrame = new Frame(1, console.screen_rows, console.screen_columns, 1, fbg|WHITE, frame);
-	messageFrame = new Frame(1, 3, console.screen_columns, console.screen_rows - 3, BG_BLACK|WHITE, frame);
-	headerFrame = new Frame(1, 3, console.screen_columns, 4, fbg|WHITE, messageFrame);
-	bodyFrame = new Frame(1, 7, console.screen_columns, console.screen_rows - 7, BG_BLACK|WHITE, messageFrame);
-	messageBar = new Frame(1, console.screen_rows, console.screen_columns, 1, fbg|WHITE, messageFrame);
-	promptFrame = new Frame(20, Math.round(console.screen_rows / 2) - 3, console.screen_columns - 40, 6, fbg|WHITE, frame);
-	promptSubFrame = new Frame(22, promptFrame.y + 1, promptFrame.width - 4, 4, BG_BLACK|WHITE, promptFrame);
 
-	tree = new Tree(treeFrame);
+	attr = console.attributes;
+	console.clear(LIGHTGRAY);
+
+	systemStatus = bbs.sys_status;
+	bbs.sys_status|=SS_MOFF;
+
+	initSettings();
 	
-	helpFrame.center(
-		hfg + "UP" + sffg + "/" + hfg + "DN  "
-		+ hfg + "[" + sffg + "PgUp/PgDn" + hfg + "]  "
-		+ hfg + "HOME" + sffg + "/" + hfg + "END  "
-		+ hfg + "T" + sffg + "hreaded  "
-		+ hfg + "F" + sffg + "lat  "
-		+ hfg + "A" + sffg + "rea  "
-		+ hfg + "E" + sffg + "mail  "
-		+ hfg + "P" + sffg + "ost  "
-		+ hfg + "DEL" + sffg + "ete  "
-		+ hfg + "Q" + sffg + "uit"
-	);
-
-	messageBar.center(
-		hfg + "UP" + sffg + "/" + hfg + "DN  "
-		+ hfg + "[" + sffg + "PgUp/PgDn" + hfg + "]  "
-		+ hfg + "HOME" + sffg + "/" + hfg + "END  "
-		+ hfg + "R" + sffg + "eply  "
-		+ hfg + "P" + sffg + "revious  "
-		+ hfg + "N" + sffg + "ext  "
-		+ hfg + "DEL" + sffg + "ete  "
-		+ hfg + "Q" + sffg + "uit"
-	);
-
-	columnFrame.putmsg(
-		format(
-			"%-9s%-" + nameWidth + "s%-" + nameWidth + "s%-" + subjectWidth + "s%s",
-			"Msg #", "From", "To", "Subject", "Date"
-		)
+	frame = new Frame(
+		1,
+		1,
+		console.screen_columns,
+		console.screen_rows
 	);
 	frame.open();
-	messageFrame.bottom();
-	headerFrame.bottom();
-	promptFrame.bottom();
-	getList(msg_area.grp_list[bbs.curgrp].sub_list[bbs.cursub].code);
-}
 
-var cleanUp = function() {
-	tree.close();
-	frame.close();
-	frame.delete();
-	console.clear();
+	settings.frame = frame;
+
+	settings.callback = function(sub, header, item) {
+		reader = new Reader(
+			{	'sub' : sub,
+				'msg' : header.number,
+				'frame' : frame,
+				'fhc' : settings.fhc,
+				'fvc' : settings.fvc
+			}
+		);
+		state.reading = true;
+		state.listing = false;
+		log(JSON.stringify(item));
+	}
+
+	lister = new Lister(settings);
+
 }
 
 var main = function() {
-
-	var userInput;
-	var r;
-	var cursub = msg_area.grp_list[bbs.curgrp].sub_list[bbs.cursub].code;
-
-	while(userInput != "Q") {
-
+	while(!js.terminated) {
+		var userInput = console.inkey(K_NONE, 5);
+		if(state.listing) {
+			lister.cycle();
+			if(!lister.getcmd(userInput))
+				break;
+		} else if(state.reading) {
+			reader.cycle();
+			if(!reader.getcmd(userInput)) {
+				state.listing = true;
+				state.reading = false;
+				reader.close();
+			}
+		}
 		if(frame.cycle())
 			console.gotoxy(console.screen_columns, console.screen_rows);
-
-		userInput = console.inkey(K_NONE, 5).toUpperCase();
-		switch(userInput) {
-			case "T":
-				threaded = true;
-				break;
-			case "F":
-				threaded = false;
-				break;
-			case "A":
-				messageAreaSelector(4, 5, console.screen_columns - 6, console.screen_rows - 8, frame);
-				cursub = msg_area.grp_list[bbs.curgrp].sub_list[bbs.cursub].code;
-				break;
-			case "P":
-				frame.invalidate();
-				console.clear();
-				(cursub == "mail")?sendEmail():bbs.post_msg(cursub);
-				frame.draw();
-				break;
-			case "E":
-				cursub = "mail";
-				break;
-			case KEY_DEL:
-				if(!(tree.currentItem instanceof Tree) && deleteMessage(tree.currentItem.args[0], cursub)) {
-					tree.currentTree.deleteItem();
-					getList(cursub);
-				}
-				break;
-			default:
-				if(tree.current === undefined)
-					break;
-				r = tree.getcmd(userInput);
-				if(r != "REFRESH" && !r.hasOwnProperty("number"))
-					continue;
-				while(r.hasOwnProperty("number")) {
-					frame.cycle();
-					r = showMessage(r, cursub);
-				}
-				break;
-		}
-		getList(cursub);
 	}
+}
+
+var cleanUp = function() {
+	lister.close();
+	frame.close();
+	console.clear(attr);
+	bbs.sys_status = systemStatus;
 }
 
 init();
--- xtrn/ecreader/readme.txt
+++ xtrn/ecreader/readme.txt
@@ -1,66 +1,35 @@
-ecReader for Synchronet 3.16+
-echicken -at- bbs.electronicchicken.com, 2012
+ecReader
+A message lister & reader for Synchronet BBS 3.16+
 
-A lightbar message browser/reader/poster for Synchronet BBS, with threaded
-sub-board views and many other features.
+Features:
 
-Disclaimer:
------------
-This script works fine on my system, but it can consume a fair bit of memory.
-Always keep your BBS backed up just in case something happens, like some
-crazy message reader causing your system to slow down and crash, pooping on
-all of your files when it does.
-
-Requirements:
--------------
-An up-to-date Synchronet installation, with recent copies of:
-exec/load/frame.js
-exec/load/tree.js
-exec/load/msgutils.js
+- Lightbar message group & area selector
+- Lightbar flat or threaded message lister
+- Scrollable message reader with message area & thread browsing
+- Operates at a slow, relaxed pace
+- Uses up a good chunk of memory that would otherwise be sitting empty
 
 Installation:
--------------
 
-In SCFG (BBS->Configure in the Synchronet Control Panel on Windows), go to
-External Programs->Online Programs (Doors), and then select the area you wish
-to add ecReader to.  Create a new entry with the following details:
+In 'scfg' (BBS->Configure in Windows), do the following:
 
-¦Name                       ecReader
-¦Internal Code              ECREADER
-¦Start-up Directory         ../xtrn/ecreader
-¦Command Line               ?ecReader.js
-¦Multiple Concurrent Users  Yes
+- Select "External Programs"
+- Select "Online Programs (Doors)"
+- Choose an external programs section
+- Select "Available online programs"
+- Create a new entry as follows:
 
-Leave all other options at their default settings.
+	Online Program Name: ecReader
+	Internal Code: ECREADER
+	Start-up Directory: ../xtrn/ecreader
+	Command Line: ?ecReader.js
+	Multiple Concurrent Users: Yes
 
-You can call this external via javascript with:
-
-bbs.exec_xtrn("ECREADER");
-
-Configuration:
---------------
-You can change the appearance and behavior of ecReader on a system-wide basis
-by editing the following variables at the top of ecReader.js:
-
-var showMail = true;	// Allow access to the private 'mail' sub-board
-var threaded = true;	// False to default to flat view
-var setPointers = true; // False to leave scan pointers unaffected by reading
-var oldestFirst = false;// False to list messages in descending date order
-var lbg = BG_CYAN;		// Lightbar background
-var lfg = WHITE;		// Foreground colour of highlighted text
-var nfg = LIGHTGRAY;	// Foreground colour of non-highlighted text
-var xfg = LIGHTCYAN;	// Subtree expansion indicator colour
-var tfg = LIGHTCYAN;	// Uh, that line beside subtree items
-var fbg = BG_BLUE;		// Title, Header, Help frame background colour
-var urm = WHITE;		// Unread message foreground colour
-var hfg = "\1h\1w"; 	// Heading text (CTRL-A format, for now)
-var sffg = "\1h\1c";	// Heading sub-field text (CTRL-A format, for now)
-var mfg = "\1n\1w";		// Message text colour (CTRL-A format, for now)
-
-I will probably move these settings out into an INI file later on, and allow
-some of them to be configured on a per-user basis.
+All other options can be left at their default values.
 
 Support:
---------
-email: echicken -at- bbs.electronicchicken.com
-  irc: #synchronet on irc.synchro.net
+
+- Post a message to 'echicken' in the 'Synchronet Sysops' sub on DOVE-Net
+- Send an email to echicken -at- bbs.electronicchicken.com
+- Find me in #synchronet on irc.synchro.net
+
