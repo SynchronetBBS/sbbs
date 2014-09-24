9a41bb2b7b092603471d5e971f92bac7a4520983
deuce <>
2014-09-24 00:18:10 +0000

    Fix bug reported by DigitalMan in Win32 console mode where if the screen size is changed so that the new X is larger or smaller than the old X *and* the new Y is different to the old Y in the opposite direction (ie: Old X is smaller and new Y is larger), the screen would not change size, but the internal state would act as though it had.
    Since SetConsoleScreenBufferSize() cannot set the screen buffer smaller than
    the window in either dimension, and SetConsoleWindowInfo() cannot set the window
    larger than the screen buffer size in either direction, all resizing calls would
    fail, but the internal state would still be updated.
    
    This change prevents the size inversion calls from being used, and does not
    update the internal state on failure of any call.

--- src/conio/win32cio.c
+++ src/conio/win32cio.c
@@ -635,18 +635,23 @@ void win32_textmode(int mode)
 		if(vparams[i].mode==mode)
 			modeidx=i;
 	}
-	sz.X=vparams[modeidx].cols;
-	sz.Y=vparams[modeidx].rows;
+	sz.X = cio_textinfo.screenwidth > vparams[modeidx].cols ? cio_textinfo.screenwidth : vparams[modeidx].cols;
+	sz.Y = cio_textinfo.screenheight > vparams[modeidx].rows ? cio_textinfo.screenheight : vparams[modeidx].rows;
 	rc.Left=0;
 	rc.Right=vparams[modeidx].cols-1;
 	rc.Top=0;
 	rc.Bottom=vparams[modeidx].rows-1;
 
-	if((h=GetStdHandle(STD_OUTPUT_HANDLE)) != INVALID_HANDLE_VALUE) {
-		SetConsoleScreenBufferSize(h,sz);
-		SetConsoleWindowInfo(h,TRUE,&rc);
-		SetConsoleScreenBufferSize(h,sz);
-	}
+	if ((h=GetStdHandle(STD_OUTPUT_HANDLE)) == INVALID_HANDLE_VALUE)
+		return;
+	if (!SetConsoleScreenBufferSize(h,sz))
+		return;
+	if (!SetConsoleWindowInfo(h,TRUE,&rc))
+		return;
+	sz.X=vparams[modeidx].cols;
+	sz.Y=vparams[modeidx].rows;
+	if (!SetConsoleScreenBufferSize(h,sz))
+		return;
 
 	cio_textinfo.attribute=7;
 	cio_textinfo.normattr=7;
