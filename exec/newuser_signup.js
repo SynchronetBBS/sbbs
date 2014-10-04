b8b1ab17731af97be687a56c7b2c816d4a987863
deuce <>
2014-10-04 22:17:27 +0000

    Hack hack hack. Set user.number to newuser.number and call user_sync().  login should do this, so if this works, something is amiss.

--- exec/newuser_signup.js
+++ exec/newuser_signup.js
@@ -336,7 +336,8 @@ function create_newuser()
 			newuser.security.password = '';
 			if (bbs.login(newuser.alias, bbs.text(PasswordPrompt))) {
 				user.security.password = tmp;
-
+				user.number = tmp;
+				bbs.user_sync();
 				logline(LOG_INFO, '', "Created user record #"+user.number+": "+user.alias);
 				useron = user;
 			}
