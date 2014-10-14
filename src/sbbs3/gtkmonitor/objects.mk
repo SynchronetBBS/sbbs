35582151630d5cd79ba5814cffbe9807e6b28053
deuce <>
2014-10-14 05:17:59 +0000

    Build gtkmonitor_glade.c too now.

--- src/sbbs3/gtkmonitor/objects.mk
+++ src/sbbs3/gtkmonitor/objects.mk
@@ -1,4 +1,5 @@
 OBJS	:=	\
 			$(MTOBJODIR)$(DIRSEP)gtkmonitor$(OFILE) \
+			$(MTOBJODIR)$(DIRSEP)gtkmonitor_glade$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)util_funcs$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)events$(OFILE) \
