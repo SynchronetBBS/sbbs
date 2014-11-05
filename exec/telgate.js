24ce8fbc419315cdc17e3573e40832548d94d505
rswindell <>
2014-11-05 22:35:53 +0000

    Allow bbs.telnet_gate() flags to be passed on the command-line following the host[:port] argumnet. Example: "?telgate rainmaker.wunderground.com TG_PASSTHRU|TG_ECHO"

--- exec/telgate.js
+++ exec/telgate.js
@@ -9,5 +9,8 @@ load("sbbsdefs.js");
 write("\r\n\1h\1hPress \001yCtrl-]\001w for a control menu anytime.\r\n\r\n");
 console.pause();
 writeln("\001h\001yConnecting to: \001w"+argv[0]+"\001n");
-bbs.telnet_gate(argv[0],TG_PASSTHRU);
+var flags=TG_PASSTHRU;
+if(argc>1)
+	flags=eval(argv[1]);
+bbs.telnet_gate(argv[0],flags);
 console.clear();
