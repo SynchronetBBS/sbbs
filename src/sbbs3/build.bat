5b26b81a7d0aed72f08f5fa519063b0a53ec73c5
rswindell <>
2015-07-15 09:02:26 +0000

    Add the "exit /b 1" trick to force the batch file to return an error (to testbuild.js), I suspect the setlocal call is causing the current error level to not be passed up to the caller. If build.bat failed, testbuild.js would not always detect the build failure.

--- src/sbbs3/build.bat
+++ src/sbbs3/build.bat
@@ -3,4 +3,4 @@ setlocal
 rem *** Requires Microsoft Visual C++ 2013 ***
 call "%VS120COMNTOOLS%\vsvars32.bat"
 msbuild sbbs3.sln %1 %2 %3 %4 %5
-if errorlevel 1 echo. & echo !ERROR(s) occurred
+if errorlevel 1 echo. & echo !ERROR(s) occurred & exit /b 1
