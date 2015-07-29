1d2fc345a7bf8520952c7aee8a9e632deae62c27
deuce <>
2015-07-29 03:52:31 +0000

    Fix bug in last commit.
    -- "he breaks more than he fixes"

--- src/sbbs3/wordwrap.c
+++ src/sbbs3/wordwrap.c
@@ -526,7 +526,7 @@ static char *wrap_paragraphs(struct paragraph *paragraph, int outlen, BOOL handl
 				ws_len = get_ws_len(inp, outcol);
 				word_len = get_word_len(inp+ws_len.bytes, -1);
 				// Do we need to chop a long word?
-				if (word_len.len > (outlen - prefix_cols)))
+				if (word_len.len > (outlen - prefix_cols))
 					word_len = get_word_len(inp + ws_len.bytes, outlen - ws_len.bytes - outcol);
 				if (outcol + ws_len.len + word_len.len > outlen) {
 					inp += ws_len.bytes;
