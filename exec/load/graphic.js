8ace8b75e8fcce51ba102d8923e2f24efd9779bd
rswindell <>
2015-09-01 17:54:15 +0000

    Fxes in parseANSI(): - ESC seqquences were put in the screen buffer - Handle non-destructive backspace chars

--- exec/load/graphic.js
+++ exec/load/graphic.js
@@ -402,6 +402,7 @@ Graphic.prototype.parseANSI = function(lines)
 						std_cmds[cmd](params,this);
 				}
 				line = line.substr(m[0].length);
+                continue;
 			}
 
 			/* set character and attribute */
@@ -418,6 +419,9 @@ Graphic.prototype.parseANSI = function(lines)
                     continue;
                 case '\t':
                     x += 8-(x%8);
+                    continue;
+                case '\b':
+                    if(x) x--;
                     continue;
             }
 
