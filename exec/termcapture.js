66ff5511d3f417e0fc38aa1bf4afac61982c3a3b
rswindell <>
2015-09-04 23:19:16 +0000

    Updated to use the lateset termcapture_lib.js.

--- exec/termcapture.js
+++ exec/termcapture.js
@@ -12,24 +12,35 @@ capture.port = argv[2];
 capture.rows = argv[3];
 capture.timeout = 15;
 
-var lines=capture.capture();
-if(lines==false) {
+var result=capture.capture();
+if(result==false) {
     alert("Capture failure");
     exit();
 }
-print("test");
-print("hello: " + capture.hello.join("\n"));
+print("hello: ");
+for(i in result.hello)
+    print("'" + result.hello[i] + "'");
+
+var file = new File("capture.raw");
+if(!file.open("wb")) {
+    alert("Error opening " + file.name);
+    exit();
+}
+file.write(result.preview.join("\r\n"));
+file.close();
+print(file.name + " written");
+
+load("graphic.js");
+var graphic=new Graphic(80, capture.rows);
+graphic.ANSI = result.preview.join("\r\n");
+graphic.save("capture.bin");
 
 var file = new File("capture.ans");
 if(!file.open("wb")) {
     alert("Error opening " + file.name);
     exit();
 }
-file.write(lines.join("\n"));
+graphic.width--;
+file.write(graphic.ANSI);
 file.close();
 print(file.name + " written");
-
-load("graphic.js");
-var graphic=new Graphic(80, capture.rows);
-graphic.parseANSI(lines);
-graphic.write("capture.bin");
