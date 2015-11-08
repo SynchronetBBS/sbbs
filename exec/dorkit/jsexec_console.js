3132b108a84d020b8542c180d47559e04f478ff1
deuce <>
2015-11-08 07:28:04 +0000

    Add jsexec support.  This is likely problematical on Win32, since you can't select() a file on that OS.  Also, opening the "CON" file may not do what I expect.

---
+++ exec/dorkit/jsexec_console.js
@@ -0,0 +1,40 @@
+/*
+ * Clears the current screen to black and moves to location 1,1
+ * sets the current attribute to 7
+ */
+
+dk.console.remote_io = {
+	clear:function() {
+		this.attr.value=7;
+		this.print("\x0c");
+	},
+
+	/*
+	 * Clears to end of line.
+	 * Not available witout ANSI (???)
+	 */
+	cleareol:function() {
+		if(dk.console.ansi)
+			this.print('\x1b[K');
+	},
+
+	/*
+	 * Moves the cursor to the specified position.
+	 * returns false on error.
+	 * Not available without ANSI
+	 */
+	gotoxy:function(x,y) {
+		if(dk.console.ansi)
+			this.print(format("\x1b[%u;%uH", y, x));
+	},
+
+	/*
+	 * Writes a string unmodified.
+	 */
+	print:function(string) {
+		write(string);
+	},
+};
+
+var input_queue = load(true, "jsexec_input.js");
+js.on_exit("input_queue.write(''); input_queue.poll(0x7fffffff);");
