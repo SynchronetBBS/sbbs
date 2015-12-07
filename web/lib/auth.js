5832e9bce08ddf9d6127f0a5888be6bc223ee441
echicken <echicken@bbs.electronicchicken.com>
2015-12-07 01:19:41 -0500

    Cleanup

--- web/lib/auth.js
+++ web/lib/auth.js
@@ -1,77 +1,75 @@
 load("sbbsdefs.js");
 load(system.exec_dir + "../web/lib/init.js");
 
-var randomString = function(length) {
+function randomString(length) {
 	var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split("");
 	var str = '';
-	for (var i = 0; i < length; i++)
+	for (var i = 0; i < length; i++) {
 		str += chars[Math.floor(Math.random() * chars.length)];
+	}
 	return str;
 }
 
-var getSession = function(un) {
-	var fn = format("%suser/%04d.web", system.data_dir, un);
-	if(!file_exists(fn))
-		return false;
+function getSession(un) {
+	var fn = format('%suser/%04d.web', system.data_dir, un);
+	if (!file_exists(fn)) return false;
 	var f = new File(fn);
-	if(!f.open("r"))
-		return false;
+	if (!f.open('r')) return false;
 	var session = f.iniGetObject();
 	f.close();
 	return session;
 }
 
-var getSessionValue = function(un, key) {
+function getSessionValue(un, key) {
 	var session = getSession(un);
-	if(!session || typeof session[key] == "undefined")
-		return null;
+	if (!session || typeof session[key] === 'undefined') return null;
 	return session[key];
 }
 
-var setSessionValue = function(un, key, value) {
-	var fn = format("%suser/%04d.web", system.data_dir, un);
+function setSessionValue(un, key, value) {
+	var fn = format('%suser/%04d.web', system.data_dir, un);
 	var f = new File(fn);
 	f.open(f.exists ? 'r+' : 'w+');
 	f.iniSetValue(null, key, value);
 	f.close();
 }
 
-var setCookie = function(usr, sessionKey) {
-	if(usr instanceof User && usr.number > 0) {
+function setCookie(usr, sessionKey) {
+	if (usr instanceof User && usr.number > 0) {
 		set_cookie(
 			'synchronet',
 			usr.number + ',' + sessionKey,
 			(time() + settings.timeout),
-			http_request.host.replace(/\:\d*/g, ""),
-			"/"
+			http_request.host.replace(/\:\d*/g, ''),
+			'/'
 		);
 		setSessionValue(usr.number, 'key', sessionKey);
 	}
 }
 
-var validateSession = function(cookies) {
+function validateSession(cookies) {
 
-	for(var c in cookies) {
+	for (var c in cookies) {
 
-		if(cookies[c].search(/^\d+,\w+$/) < 0)
-			continue;
+		if (cookies[c].search(/^\d+,\w+$/) < 0) continue;
 
 		var cookie = cookies[c].split(',');
 
 		try {
 			var usr = new User(cookie[0]);
-			if(usr.number < 1)
-				throw "Invalid user number " + cookie[0] + " in cookie.";
-		} catch(err) {
+			if (usr.number < 1) {
+				throw 'Invalid user number ' + cookie[0] + ' in cookie.';
+			}
+		} catch (err) {
 			log(LOG_DEBUG, err);
 			continue;
 		}
 
 		var session = getSession(usr.number);
-		if(typeof session != "object")
+		if (typeof session !== 'object') continue;
+		if (typeof session.key != 'string' || session.key != cookie[1]) {
 			continue;
-		if(typeof session.key != "string" || session.key != cookie[1])
-			continue;
+		}
 
 		authenticate(usr.alias, usr.security.password);
 		setCookie(usr, session.key);
@@ -81,100 +79,95 @@ var validateSession = function(cookies) {
 
 }
 
-var destroySession = function(cookies) {
+function destroySession(cookies) {
 
-	for(var c in cookies) {
+	for (var c in cookies) {
 
-		if(cookies[c].search(/^\d+,\w+$/) < 0)
-			continue;
+		if (cookies[c].search(/^\d+,\w+$/) < 0)	continue;
 
 		var cookie = cookies[c].split(',');
 
 		try {
 			
 			var usr = new User(cookie[0]);
-			if(usr.number < 1)
-				throw "Invalid user number " + cookie[0] + " in cookie.";
+			if(usr.number < 1) {
+				throw 'Invalid user number ' + cookie[0] + ' in cookie.';
+			}
 
 			var session = getSession(usr.number);
-			if(typeof session != "object")
-				throw "Invalid session for user #" + usr.number;
+			if (typeof session !== 'object') {
+				throw 'Invalid session for user #' + usr.number;
+			}
 
-			if(session.key != cookie[1])
-				throw "Invalid session key for user #" + user.number;
+			if (session.key !== cookie[1]) {
+				throw 'Invalid session key for user #' + user.number;
+			}
 
 			set_cookie(
 				'synchronet',
 				usr.number + ',' + session.key,
 				(time() - settings.timeout),
-				http_request.host.replace(/\:\d*/g, ""),
-				"/"
+				http_request.host.replace(/\:\d*/g, ''),
+				'/'
 			);
 
-			var fn = format("%suser/%04d.web", system.data_dir, usr.number);
+			var fn = format('%suser/%04d.web', system.data_dir, usr.number);
 			file_remove(fn);
 
 			break;
 
-		} catch(err) {
-
-			log("Error destroying session: " + err + ", cookie: " + cookies[c]);
-
+		} catch (err) {
+			log('Error destroying session: ' + err + ', cookie: ' + cookies[c]);
 		}
 
 	}
 
 }
 
-var authenticate = function(alias, password) {
+function authenticate(alias, password) {
 	var un = system.matchuser(alias);
-	if(un < 1)
-		return false;
+	if (un < 1) return false;
 	var usr = new User(un);
-	if(usr.settings&USER_DELETED)
+	if (usr.settings&USER_DELETED) return false;
+	if (usr.security.password.toUpperCase() !== password.toUpperCase()) {
 		return false;
-	if(usr.security.password.toUpperCase() != password.toUpperCase())
-		return false;
+	}
 	login(usr.alias, usr.security.password.toUpperCase());
 	return usr;
 }
 
 // If someone is trying to log in
-if(	typeof http_request.query.username != "undefined"
-	&& 
-	http_request.query.username[0].length <= LEN_ALIAS
-	&&
-	typeof http_request.query.password != "undefined"
-	&&
+if (typeof http_request.query.username !== 'undefined' && 
+	http_request.query.username[0].length <= LEN_ALIAS &&
+	typeof http_request.query.password != 'undefined' &&
 	http_request.query.password[0].length <= LEN_PASS
 ) {
 	var usr = authenticate(
 		http_request.query.username[0],
 		http_request.query.password[0]
 	);
-	if(usr instanceof User)
-		setCookie(usr, randomString(512));
-
+	if (usr instanceof User) setCookie(usr, randomString(512));
 // If they have a cookie
 } else if(
-	typeof http_request.cookie.synchronet != "undefined"
-	&&
-	http_request.cookie.synchronet.some(Function('e', 'return(e.search(/^\\d+,\\w+$/) != -1)'))
+	typeof http_request.cookie.synchronet != 'undefined' &&
+	http_request.cookie.synchronet.some(
+		Function('e', 'return(e.search(/^\\d+,\\w+$/) != -1)')
+	)
 ) {
-
 	// Verify & update their session, or log them out if requested
-	if(typeof http_request.query.logout == "undefined")
+	if (typeof http_request.query.logout === 'undefined') {
 		validateSession(http_request.cookie.synchronet);
-	else
+	} else {
 		destroySession(http_request.cookie.synchronet);
+	}
 }
 
 // If they haven't authenticated as an actual user yet
-if(user.number == 0) {
+if (user.number === 0) {
 
 	// Try to log them in as the guest user
 	var gn = system.matchuser(settings.guest);
-	if(gn > 0) {
+	if (gn > 0) {
 		var gu = new User(gn);
 		login(gu.alias, gu.security.password);
 	} else {
