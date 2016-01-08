0af2d9360f2b44e4dd876bb2ac93715435458cf2
deuce <>
2016-01-08 06:10:17 +0000

    Add circular path detection and file forwarding. This could now be considered "full featured" since there's no more features I currently plan on adding.
    Have at 'er!

--- exec/tickit.js
+++ exec/tickit.js
@@ -3,8 +3,84 @@ load("sbbsdefs.js");
 var inb=[];
 var pktpass = {};
 var files_bbs={};
+var outbound;
 var gcfg;
 var acfg={};
+var is_flo=false;
+const cset='0123456789abcdefghijklmnopqrstuvwxyz0123456789-_';
+
+function basefn_to_num(num)
+{
+	var base = cset.length;
+	var part;
+	var ret=0;
+	var i;
+
+	for (i=0; i<num.length; i++) {
+		ret *= base;
+		ret += cset.indexOf(num[i]);
+	}
+	return ret;
+	
+}
+
+function num_to_basefn(num)
+{
+	var base = cset.length;
+	var part;
+	var ret='';
+
+	while(num) {
+		part = num % base;
+		ret = cset.charAt(part) + ret;
+		num = parseInt((num/base), 10);
+	}
+	return ret;
+}
+
+/*
+ * Returns a filename in the format "ti_XXXXX.tic" where
+ * XXXXX is replaced by an incrementing base-48 number.
+ * This provides 254,803,967 unique filenames.
+ */
+function get_next_tic_filename()
+{
+	var f;
+	var val;
+	var ret;
+
+	// Get previous value or -1 if there is no previous value.
+	f = new File(system.data_dir+'tickit.seq');
+
+	if (f.open("r+b")) {
+		val = f.readBin();
+	}
+	else {
+		if (!f.open("web+")) {
+			log(LOG_ERROR, "Unable to open file "+f.name+"!");
+			return undefined;
+		}
+		val = -1;
+	}
+
+	// Increment by 1...
+	val++;
+
+	// Check for wrap...
+	if (val > basefn_to_num('_____'))
+		val = 0;
+
+	// Write back new value.
+	f.truncate();
+	f.writeBin(val);
+	f.close();
+
+	// Left-pad to five digits.
+	ret = ('00000'+num_to_basefn(val)).substr(-5);
+
+	// Add pre/suf-fix
+	return 'ti_'+ret+'.tic';
+}
 
 function get_pw(node)
 {
@@ -117,6 +193,170 @@ function process_tic(tic)
 	return true;
 }
 
+function add_links(seenbys, list)
+{
+	var l;
+	var i;
+
+	l = list.split(/,/);
+	for (i=0; i<l.length; i++) {
+		if (seenbys[l[i]] !== undefined) {
+			log(LOG_DEBUG, "Node "+l[i]+" has already seen this.");
+			continue;
+		}
+		links[l[i]]='';
+	}
+}
+
+function parse_addr(addr)
+{
+	var m;
+	var ret={};
+
+	m = addr.match(/^([0-9]+):/);
+	if (m !== null)
+		ret.zone = parseInt(m[1], 10);
+
+	m = addr.match(/([0-9]+)\//);
+	if (m !== null)
+		ret.net = parseInt(m[1], 10);
+
+	m = addr.match(/\/([0-9]+)/);
+	if (m !== null)
+		ret.node = parseInt(m[1], 10);
+
+	m = addr.match(/\.([0-9]+)/);
+	if (m !== null)
+		ret.point = parseInt(m[1], 10);
+
+	m = addr.match(/@.+$/);
+	if (m !== null)
+		ret.domain = m[1];
+
+}
+
+function get_zone(addr)
+{
+	var m;
+
+	// Figure out the default zone.
+	m = addr.match(/^([0-9]+):/);
+	if (m===null)
+		return undefined;
+	return parseInt(m[1], 10);
+}
+
+function forward_tic(tic)
+{
+	var seenbys={};
+	var links={};
+	var cfg;
+	var link;
+	var tf;
+	var ff;
+	var bf;
+	var defzone;
+	var addr;
+	var outb;
+	var flobase;
+	var pw;
+	var i;
+
+	defzone = get_zone(system.fido_addr_list[0]);
+	if (defzone === undefined) {
+		log(LOG_ERROR, "Unable to detect default zone!");
+		return false;
+	}
+
+	// Add us to the path...
+	tic.path.push(system.system.fido_addr_list[0]);
+
+	// Populate seenbys from TIC file
+	for (i=0; i<tic.seenby.length; i++)
+		seenbys[tic.seenby[i]]='';
+
+	// Calculate links
+	if (gcfg.links !== undefined)
+		add_links(seenbys, gcfg.links);
+	cfg = acfg[tic.area.toLowerCase()];
+	if (cfg !== undefined && cfg.links !== undefined)
+		add_links(seenbys, cfg.links);
+
+	// Add links to seenbys
+	for (i in links)
+		tic.seenby.push(i);
+
+	// Now, start generating the TIC/FLO files...
+	for (link in links) {
+		if (!is_flo) {
+			log(LOG_ERROR, "TickIT doesn't support non-FLO mailers.");
+			return false;
+		}
+
+		pw = get_pw(link);
+		if (pw===undefined)
+			pw = '';
+
+		// Figure out the outbound dir...
+		addr = parse_addr(link);
+		if (addr.zone === undefined)
+			addr.zone = defzone;
+
+		if (addr.zone === undefined || addr.net === undefined || addr.node === undefined) {
+			log(LOG_ERROR, "Address '"+link+"' is invalid!");
+			continue;
+		}
+
+		outb = outbound.replace(/[\\\/]+$/g, '');;
+		if (addr.zone !== defzone)
+			outb += format(".%03x", addr.zone);
+		outb = file_getcase(outb);
+		backslash(outb);
+
+		// Create TIC file first...
+		tf = new File(outb+get_next_tic_filename());
+		if(!tf.open("wb")) {
+			log(LOG_ERROR, "Unable to create TIC file for "+link+".  He will not get file '"+tic.file+"'!");
+			continue;
+		}
+		tf.write(tic[' forward'].join("\r\n"));
+		tf.write('\r\n');
+		tf.write('From '+system.fido_addr_list[0]+'\r\n');
+		tf.write('To '+link+'\r\n');
+		tf.write('Pw '+pw+'\r\n');
+		for (i=0; i<tic.path.length; i++)
+			tf.write('Path '+tic.path[i]+'\r\n');
+		for (i=0; i<tic.seenby.length; i++)
+			tf.write('Path '+tic.seenby[i]+'\r\n');
+		tf.close;
+
+		// Create bsy file...
+		flobase = outb+format("%04x%04x", addr.net, addr.node);
+		bf = new File(flobase+'.bsy');
+		while (!bf.open('web+')) {
+			// TODO: This waits forever...
+			log(LOG_WARNING, "Waiting for BSY file '"+bf.name+"'...");
+			mswait(1000);
+		}
+
+		// Append to FLO file...
+		ff = new File(flobase+'.flo');
+		if (!ff.open('ab+')) {
+			log(LOG_ERROR, "Unable to append to '"+ff.name+"' for "+link+".  He will not get file '"+tic.file+"'!");
+			bf.close();
+			bf.remove();
+			continue;
+		}
+		ff.writeln(tic.full_path);
+		ff.writeln('^'+tf.name);
+		ff.close();
+		bf.close();
+		bg.remove();
+	}
+
+	return true;
+}
+
 function parse_ticfile(fname)
 {
 	var crc32;
@@ -128,6 +368,7 @@ function parse_ticfile(fname)
 	var outtic=[];
 	var f=new File(fname);
 	var dir = fullpath(fname).replace(/([\/\\])[^\/\\]*$/,'$1');
+	var i;
 
 	log(LOG_INFO, "Parsing "+fname);
 	if (!f.open("r")) {
@@ -149,8 +390,18 @@ function parse_ticfile(fname)
 					tic[key] = val;
 					break;
 				// Multiple values
-				case 'path':
 				case 'seenby':
+					tic[key].push(val);
+					break;
+
+				case 'path':
+					// Circular path detection...
+					for (i=0; i<system.fido_addr_list.length; i++) {
+						if (val === system.fido_addr_list[i]) {
+							log(LOG_ERROR, "Circular path detected on address "+val+"!");
+							return false;
+						}
+					}
 					tic[key].push(val);
 					break;
 
@@ -223,6 +474,8 @@ function parse_ticfile(fname)
 	if (!match_pw(tic.from, tic.pw))
 		return false;
 
+	tic[' forward'] = outtic;
+
 	return tic;
 }
 
@@ -244,6 +497,14 @@ function parse_ecfg()
 		m = line.match(/^\s*pktpwd\s+(.*?)\s+(.*)\s*$/i);
 		if (m !== null) {
 			pktpass[m[1].toUpperCase()] = m[2].toUpperCase();
+		}
+		m = line.match(/^\s*outbound\s+(.*?)\s*$/i);
+		if (m !== null) {
+			outbound = m[1];
+		}
+		m = line.match(/^\s*flo_mailer\s*$/i);
+		if (m !== null) {
+			is_flo = true;
 		}
 	}
 	ecfg.close();
@@ -332,8 +593,11 @@ function main() {
 		for (j=0; j<ticfiles.length; j++) {
 			tic = parse_ticfile(ticfiles[j]);
 			if (tic !== false) {
-				if (process_tic(tic))
+				if (process_tic(tic)) {
 					processed++;
+
+					forward_tic(tic);
+				}
 			}
 		}
 	}
