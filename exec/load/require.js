70d054ded89ffb26772ced2be69a1d1129a7e7a8
deuce <>
2016-04-23 01:55:14 +0000

    If load()ing the specified file doesn't defined the required symbol, throw and exception.

--- exec/load/require.js
+++ exec/load/require.js
@@ -2,6 +2,8 @@ js.global.require_module_file = argv[1];
 if (eval('typeof('+argv[2]+') !== "undefined" ? true : false'))
 	js.global.require_module_file = 'dummy.js';
 load(js.global.require_module_file);
+if (eval('typeof('+argv[2]+') == "undefined"'))
+	throw("ERROR: load()ing "+argv[1]+" didn't define symbol \""+argv[2]+"\"");
 if (argv[0] === 'undefined') {
 	Object.defineProperties(this, {
 		"argv": {
