7377a4c009cd9c56b1efb828f55b055c4ef8dda0
deuce <>
2016-05-13 01:36:28 +0000

    Handle new send() return value in the same manner.  This can be fixed more correctly now though.

--- exec/load/binkp.js
+++ exec/load/binkp.js
@@ -848,7 +848,8 @@ BinkP.prototype.sendCmd = function(cmd, data)
 	var len = data.length+1;
 	len |= 0x8000;
 	// We'll send it all in one go to avoid sending small packets...
-	if (!this.sock.send(this.send_buf(ascii((len & 0xff00)>>8) + ascii(len & 0xff) + ascii(cmd) + data)))
+	var sstr = this.send_buf(ascii((len & 0xff00)>>8) + ascii(len & 0xff) + ascii(cmd) + data;
+	if (this.sock.send(sstr) !== sstr.length))
 		return false;
 	switch(cmd) {
 		case this.command.M_EOB:
@@ -878,7 +879,8 @@ BinkP.prototype.sendData = function(data)
 	if (this.debug)
 		log(LOG_DEBUG, "Sending "+data.length+" bytes of data");
 	// We'll send it all in one go to avoid sending small packets...
-	if (!this.sock.send(this.send_buf(ascii((len & 0xff00)>>8) + ascii(len & 0xff) + data)))
+	var sstr = this.send_buf(ascii((len & 0xff00)>>8) + ascii(len & 0xff) + data);
+	if (!this.sock.send(sstr) != sstr.length)
 		return false;
 	return true;
 };
