644d1f35100c8513d0b9c5bf9ad1d4dbf1ce2e5b
rswindell <>
2016-11-16 08:01:46 +0000

    Added the 'V' (vote) restriction to the default "guest" account created by this script. If you have already have a Guest account on your BBS, you might want to add this restriction to it.

--- exec/makeguest.js
+++ exec/makeguest.js
@@ -33,6 +33,7 @@ guest.security.restrictions|=UFLAG_M;	// can't send network mail
 guest.security.restrictions|=UFLAG_W;	// can't write to the auto-message
 guest.security.restrictions|=UFLAG_R;	// can't remove files
 guest.security.restrictions|=UFLAG_C;	// can't chat
+guest.security.restrictions|=UFLAG_V;	// can't vote
 guest.security.exemptions|=UFLAG_G;		// multiple simultaneous logins
 guest.security.exemptions|=UFLAG_L;		// unlimited logons per day
 guest.security.exemptions|=UFLAG_T;		// unlimited time online
