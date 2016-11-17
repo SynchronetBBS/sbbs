6e7bf7685ed6ba15770fe1d7491334e8d675df4a
rswindell <>
2016-11-17 23:54:01 +0000

    Fix getuserdat() bug introduced in rev 1.164: when failing to read a user record (e.g. the user number is invalid), the user number should be 0 after returning. This would cause, for example, sbbs_t::login() to accept a login string with an invalid usernumber (e.g. "12345") and do some strange things. The same side-effect was missing in the (new) fgetuserdat().

--- src/sbbs3/userdat.c
+++ src/sbbs3/userdat.c
@@ -418,6 +418,7 @@ int DLLCALL getuserdat(scfg_t* cfg, user_t *user)
 	memset(userdat, 0, sizeof(userdat));
 	if((retval = readuserdat(cfg, user->number, userdat, file)) != 0) {
 		close(file);
+		user->number = 0;
 		return retval;
 	}
 	retval = parseuserdat(cfg, userdat, user);
@@ -431,9 +432,14 @@ int DLLCALL fgetuserdat(scfg_t* cfg, user_t *user, int file)
 	int		retval;
 	char	userdat[U_LEN+1];
 
+	if(!VALID_CFG(cfg) || user==NULL || user->number < 1)
+		return(-1); 
+
 	memset(userdat, 0, sizeof(userdat));
-	if((retval = readuserdat(cfg, user->number, userdat, file)) != 0)
+	if((retval = readuserdat(cfg, user->number, userdat, file)) != 0) {
+		user->number = 0;
 		return retval;
+	}
 	return parseuserdat(cfg, userdat, user);
 }
 
