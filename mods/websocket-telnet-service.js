b5b0867376434bf05ab8e0a074e462694905f960
echicken <echicken@bbs.electronicchicken.com>
2016-12-19 15:32:23 -0500

    Debug logging stuff.

--- mods/websocket-telnet-service.js
+++ mods/websocket-telnet-service.js
@@ -235,8 +235,9 @@ try {
     }
 
     var wss = new WebSocketProxy(client);
+    log(LOG_DEBUG, 'Connecting to ' + telnet_addr + ':' + ini.TelnetPort);
     var telnet = new TelnetClient(telnet_addr, ini.TelnetPort);
- 
+
 	while (client.socket.is_connected && telnet.connected) {
 
     	wss.cycle();
