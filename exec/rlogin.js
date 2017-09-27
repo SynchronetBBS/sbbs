b505a6e3b55da32dc8357e84f74f0180b397c1b3
rswindell <>
2017-09-27 00:59:59 +0000

    Allow TG_* flags to be passed on the command-line (similar to telgate.js).

--- exec/rlogin.js
+++ exec/rlogin.js
@@ -10,6 +10,9 @@ load("sbbsdefs.js");
 
 write("\r\n\001h\1hPress \001yCtrl-]\001w for a control menu anytime.\r\n\r\n");
 console.pause();
-writeln("\001h\001yConnecting to: \001w"+argv[0]+"\001n");
-bbs.telnet_gate(argv[0],TG_RLOGIN);
+writeln("\001h\001yConnecting to: \001w" + argv[0] + "\001n");
+var flags = TG_RLOGIN;
+if (argc > 1)
+    flags = eval(argv[1]);
+bbs.telnet_gate(argv[0], flags);
 console.clear();
