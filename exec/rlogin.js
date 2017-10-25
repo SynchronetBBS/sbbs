9ba11ef967db2b30248dbf141607fc0eba068abc
rswindell <>
2017-10-25 08:59:15 +0000

    Use bbs.rlogin_gate() instead of bbs.telnet_gate(), forces the TG_RLOGIN mode flag to be set. Fix for DesotoFireflite.

--- exec/rlogin.js
+++ exec/rlogin.js
@@ -11,8 +11,8 @@ load("sbbsdefs.js");
 write("\r\n\001h\1hPress \001yCtrl-]\001w for a control menu anytime.\r\n\r\n");
 console.pause();
 writeln("\001h\001yConnecting to: \001w" + argv[0] + "\001n");
-var flags = TG_RLOGIN;
+var flags = 0;
 if (argc > 1)
     flags = eval(argv[1]);
-bbs.telnet_gate(argv[0], flags);
+bbs.rlogin_gate(argv[0], flags);
 console.clear();
