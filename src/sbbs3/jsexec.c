d058e6a48900b87eadc8a115a4f4419ea2484a8f
rswindell <>
2017-11-13 19:50:14 +0000

    Fix inadvertent commit of changes to jsexec.c

--- src/sbbs3/jsexec.c
+++ src/sbbs3/jsexec.c
@@ -298,8 +298,8 @@ static int do_bail(int code)
 	if(code)
 		fprintf(statfp,"\nReturning error code: %d\n",code);
 #ifdef __unix__
-//	if(isatty(fileno(stdin)))
-//		tcsetattr(STDIN_FILENO, TCSANOW, &orig_term);
+	if(isatty(fileno(stdin)))
+		tcsetattr(STDIN_FILENO, TCSANOW, &orig_term);
 #endif
 	return(code);
 }
@@ -1089,10 +1089,10 @@ int main(int argc, char **argv, char** environ)
 #ifdef __unix__
 		struct termios term;
 
-//		tcgetattr(fileno(stdin), &orig_term);
-//		term = orig_term;
-//		raw_input(&term);
-//		tcsetattr(fileno(stdin), TCSANOW, &term);
+		tcgetattr(fileno(stdin), &orig_term);
+		term = orig_term;
+		raw_input(&term);
+		tcsetattr(fileno(stdin), TCSANOW, &term);
 #else
 	//	This completely disabled console input on Windows:
 	//	SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), 0);
