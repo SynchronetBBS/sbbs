b94cbddc9d7a2e01a85b308e34b196b61462fe14
rswindell <>
2017-11-17 10:47:49 +0000

    Lower the CPU utilization of umonitor considerably by sleeping 100ms when there are no key presses.

--- src/sbbs3/umonitor/umonitor.c
+++ src/sbbs3/umonitor/umonitor.c
@@ -1011,8 +1011,10 @@ int main(int argc, char** argv)  {
 		j=uifc.list(WIN_L2R|WIN_ESC|WIN_ACT|WIN_DYN,0,5,70,&main_dflt,&main_bar
 			,title,mopt);
 
-		if(j == -2)
+		if(j == -2) {
+			SLEEP(100);
 			continue;
+		}
 
 		last_semfile_check = time(NULL);
 
