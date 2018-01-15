6702f252d436f60dffe4320dee7cb27ff8d1ee33
rswindell <>
2018-01-15 00:31:28 +0000

    New Ctrl-A code: Ctrl-A? (or \1?): Conditional blank-line - this code will print a blank line if the previously printed line was not blank

--- src/sbbs3/con_out.cpp
+++ src/sbbs3/con_out.cpp
@@ -237,8 +237,16 @@ void sbbs_t::outchar(char ch)
 		} 
 	}
 	if(!outchar_esc) {
-		if((uchar)ch>=' ')
+		if((uchar)ch>=' ') {
 			column++;
+			if(column >= cols) {	// assume terminal has/will auto-line-wrap
+				lncntr++;
+				lbuflen = 0;
+				tos = 0;
+				lastlinelen = column;
+				column = 0;
+			}
+		}
 		else if(ch=='\r') {
 			lastlinelen = column;
 			column=0;
@@ -253,12 +261,11 @@ void sbbs_t::outchar(char ch)
 				column++;
 		}
 	}
-	if(ch==LF || column>=cols) {
+	if(ch==LF) {
 		if(lncntr || lastlinelen)
 			lncntr++;
 		lbuflen=0;
 		tos=0;
-		lastlinelen = column;
 		column=0;
 	} else if(ch==FF) {
 		lncntr=0;
@@ -515,6 +522,12 @@ void sbbs_t::ctrl_a(char x)
 			break;
 		case '/':	/* Conditional new-line */
 			if(column > 0)
+				CRLF;
+			break;
+		case '?':	/* Conditional blank-line */
+			if(column > 0)
+				CRLF;
+			if(lastlinelen)
 				CRLF;
 			break;
 		case '[':   /* Carriage return */
