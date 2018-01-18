cd40224cdc683b45fc7b6bd7118be21c431b6dbc
rswindell <>
2018-01-18 01:21:58 +0000

    Fix GCC warnings about not checking return value of cryptlib calls.

--- src/sbbs3/ver.cpp
+++ src/sbbs3/ver.cpp
@@ -1,4 +1,5 @@
 /* ver.cpp */
+// vi: tabstop=4
 
 /* Synchronet version display */
 
@@ -110,9 +111,11 @@ void sbbs_t::ver()
 #ifdef USE_CRYPTLIB
 	{
 		int cl_major=0, cl_minor=0, cl_step=0;
-		cryptGetAttribute(CRYPT_UNUSED, CRYPT_OPTION_INFO_MAJORVERSION, &cl_major);
-		cryptGetAttribute(CRYPT_UNUSED, CRYPT_OPTION_INFO_MINORVERSION, &cl_minor);
-		cryptGetAttribute(CRYPT_UNUSED, CRYPT_OPTION_INFO_STEPPING, &cl_step);
+		int result;
+		result = cryptGetAttribute(CRYPT_UNUSED, CRYPT_OPTION_INFO_MAJORVERSION, &cl_major);
+		result = cryptGetAttribute(CRYPT_UNUSED, CRYPT_OPTION_INFO_MINORVERSION, &cl_minor);
+		result = cryptGetAttribute(CRYPT_UNUSED, CRYPT_OPTION_INFO_STEPPING, &cl_step);
+		(void)result;
 		sprintf(str + strlen(str), "  cryptlib %u.%u.%u (%u)", cl_major, cl_minor, cl_step, CRYPTLIB_VERSION);
 	}
 #endif
