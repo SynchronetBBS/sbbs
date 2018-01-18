47513c2649f5f11b8450f1f5e8b1d07abb5df46d
rswindell <>
2018-01-18 01:12:19 +0000

    Include cryptlib run-time and build-time version numbers in version detail output.

--- src/sbbs3/ver.cpp
+++ src/sbbs3/ver.cpp
@@ -8,7 +8,7 @@
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -105,7 +105,18 @@ void sbbs_t::ver()
 	}
 #endif
 
-	center(socklib_version(str,SOCKLIB_DESC));
+	socklib_version(str,SOCKLIB_DESC);
+
+#ifdef USE_CRYPTLIB
+	{
+		int cl_major=0, cl_minor=0, cl_step=0;
+		cryptGetAttribute(CRYPT_UNUSED, CRYPT_OPTION_INFO_MAJORVERSION, &cl_major);
+		cryptGetAttribute(CRYPT_UNUSED, CRYPT_OPTION_INFO_MINORVERSION, &cl_minor);
+		cryptGetAttribute(CRYPT_UNUSED, CRYPT_OPTION_INFO_STEPPING, &cl_step);
+		sprintf(str + strlen(str), "  cryptlib %u.%u.%u (%u)", cl_major, cl_minor, cl_step, CRYPTLIB_VERSION);
+	}
+#endif
+	center(str);
 	CRLF;
 
 	center(os_version(str));
