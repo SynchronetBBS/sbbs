63590b2ca277d8b76c682ade79068dbbb2d67a69
deuce <>
2018-01-29 23:14:47 +0000

    Add cl34 to the list of DLL names to try loading.

--- src/syncterm/st_crypt.c
+++ src/syncterm/st_crypt.c
@@ -46,7 +46,7 @@ struct crypt_funcs cl;
 int init_crypt(void)
 {
 	dll_handle	cryptlib;
-	const char *libnames[]={ "cl", "cl33", "cl32", NULL };
+	const char *libnames[]={ "cl", "cl34", "cl33", "cl32", NULL };
 
 	if(crypt_loaded)
 		return(0);
