a5c5b4b6be23cd58c263391a95f98ffa85cbdfa6
rswindell <>
2018-02-02 12:34:41 +0000

    Added ext_mode (CTerm) 'save_all' and 'restore_all' verbs.

--- exec/load/ansiterm_lib.js
+++ exec/load/ansiterm_lib.js
@@ -157,7 +157,10 @@ var ext_mode = {
 	set: 	function(mode) 	{ return format("\x1b[?%uh", defs.ext_mode[mode]); },
 	clear:	function(mode) 	{ return format("\x1b[?%ul", defs.ext_mode[mode]); },
 	save:	function(mode)	{ return format("\x1b[?%us", defs.ext_mode[mode]); },
-	restore: function(mode) { return format("\x1b[?%uu", defs.ext_mode[mode]); }
+	restore: function(mode) { return format("\x1b[?%uu", defs.ext_mode[mode]); },
+	save_all:		function()	{ return "\x1b[?s"; },
+	restore_all:	function()	{ return "\x1b[?u"; }
+
 }
 
 var speed = {
