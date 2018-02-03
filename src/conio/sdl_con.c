95ac053513aeefd966d4eb4d78de02687c78145e
deuce <>
2018-02-03 08:27:54 +0000

    Always call SDL_SetColors() from sdl_set_colours()... that's kinda the point of the function.

--- src/conio/sdl_con.c
+++ src/conio/sdl_con.c
@@ -923,7 +923,6 @@ int sdl_setup_colours(SDL_Surface *surf)
 			sdl_co[i].g=dac_default[i].green;
 			sdl_co[i].b=dac_default[i].blue;
 		}
-		sdl.SetColors(surf, sdl_co, 0, sizeof(dac_default)/sizeof(struct dac_colors));
 
 		for(i=0; i<(sizeof(dac_default)/sizeof(struct dac_colors)); i++) {
 			sdl_dac_default[i]=sdl.MapRGB(win->format, sdl_co[i].r, sdl_co[i].g, sdl_co[i].b);
@@ -931,6 +930,7 @@ int sdl_setup_colours(SDL_Surface *surf)
 
 		sdl_dac_defaultsz = newsz;
 	}
+	sdl.SetColors(surf, sdl_co, 0, sdl_dac_defaultsz);
 
 	return(ret);
 }
