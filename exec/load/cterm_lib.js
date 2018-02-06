1e3f462d04af0c988b61ef2fda315e0c7f5c3089
rswindell <>
2018-02-06 11:18:21 +0000

    Only enable the background-bright-intensity option if we did *not* activate one of the blink fonts.

--- exec/load/cterm_lib.js
+++ exec/load/cterm_lib.js
@@ -326,7 +326,8 @@ function xbin_draw(image, xpos, ypos, fg_color, bg_color, delay, cycle)
 		ansiterm.send("ext_mode", "set", "no_bright_intensity");
 	if(image.flags&xbin.FLAG_NONBLINK) {
 		ansiterm.send("ext_mode", "set", "no_blink");
-		ansiterm.send("ext_mode", "set", "bg_bright_intensity");
+		if(!(console.status&(CON_BLINK_FONT|CON_HBLINK_FONT)))
+			ansiterm.send("ext_mode", "set", "bg_bright_intensity");
 	}
 
 	if(image.palette && this.supports_palettes()) {
