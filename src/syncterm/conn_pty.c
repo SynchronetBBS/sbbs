25462be6c7d1c634c34aef989c88d3533d021fe3
deuce <>
2018-02-10 07:10:34 +0000

    Update termios and termcap entries, and say the pty speed is 115200.
    The pty speed apparently defaults to 50 baud on some systems, and some
    vi versions will reduce the number of displayed lines for slow terminals.

--- src/syncterm/conn_pty.c
+++ src/syncterm/conn_pty.c
@@ -398,6 +398,7 @@ int pty_connect(struct bbslist *bbs)
 	ts.c_lflag = TTYDEF_LFLAG;
 	ts.c_cflag = TTYDEF_CFLAG;
 	memcpy(ts.c_cc,ttydefchars,sizeof(ts.c_cc));
+	cfsetspeed(&ts, 115200);
 
 	/* Horrible way to determine the screen size */
 	textmode(screen_to_ciolib(bbs->screen_mode));
@@ -427,24 +428,26 @@ int pty_connect(struct bbslist *bbs)
 	case 0:		/* Child */
 		setenv("TERM",settings.TERM,1);
 		termcap=xp_asprintf("syncterm|SyncTERM"
-				":am:mi:ms:ut"
-				":Co#8:co#%d:it#8:li#%d:pa#64"
-				":@7=\\E[K:AB=\\E[4%%dm:AF=\\E[3%%dm:AL=\\E[%%dL:DC=\\E[%%dP"
-				":DL=\\E[%%dM:DO=\\E[%%dB:F1=\\E[23~:F2=\\E[24~:IC=\\E[%%d@"
-				":LE=\\E[%%dD:RA=\\E[7l:RI=\\E[%%dC:SA=\\E[?7h:SF=\\E[%%dS"
-				":SR=\\E[%%dT:UP=\\E[%%dA:cs=\\E[%%i%%d;%%dr"
-				":ac=-\\030.^Y0\\333`\\004a\\260f\\370g\\361h\\261i\\025j\\331k\\277l\\332m\\300n\\305q\\304t\\303u\\264v\\301w\\302x\\263y\\363z\\362~\\371"
-				":al=\\E[L:bl=^G:bt=\\E[Z:cb=\\E[1K:cd=\\E[J:ce=\\E[K:cl=\\E[2J"
-				":cm=\\E[%%i%%d;%%dH:cr=^M:dc=\\E[P:dl=\\E[M:do=^J:ec=\\E[%%dX:ei="
-				":ho=\\E[H:ic=\\E[@:im="
-				":is=\\E[?7h\\E[?25h\\E[?31l\\E[?32l\\E[?33l\\E[*r\\E[ D\\E[0m\\E[?s"
-				":k1=\\EOP:k2=\\EOQ:k3=\\EOR:k4=\\EOS:k5=\\EOt:k6=\\E[17~"
-				":k7=\\E[18~:k8=\\E[19~:k9=\\E[20~:k;=\\E[21~:kD=\\177:kI=\\E[@"
-				":kd=\\E[B:kN=\\E[U:kP=\\E[V:kb=^H:kh=\\E[H:kl=\\E[D:kr=\\E[C"
-				":ku=\\E[A:le=\\E[D:mb=\\E[5m:md=\\E[1m:me=\\E[0m:nd=\\E[C"
-				":nw=^M^J:rc=\\E[u"
-				":sc=\\E[s:sf=\\E[S:so=\\E[0;1;7m:sr=\\E[T:up=\\E[A:ve=\\E[?25h"
-				":vi=\\E[?25l:",ws.ws_col,ws.ws_row);
+			":co#%d:li#%d:ND:am:cc:da:mi:ms:ut"
+			":Co#256:pa#65525:it#8"
+			":@7=\\E[K:AB=\\E[48;5;%%dm:AF=\\E[38;5;%%dm:AL=\\E[%%dL:DL=\\E[%%dM"
+			":DO=\\E[%%dB:F1=\\E[23~:F2=\\E[24~:IC=\\E[%%d@"
+			":ic=\\E[@"
+			":LE=\\E[%%dD:RA=\\E[7l:RI=\\E[%%dC:SA=\\E[?7h:SF=\\E[%%dS"
+			":SR=\\E[%%dT:UP=\\E[%%dA"
+			":ac=-\\030.^Y0\\333`\\004a\\260f\\370g\\361h\\261i\\025j\\331k\\277l\\332m\\300n\\305q\\304t\\303u\\264v\\301w\\302x\\263y\\363z\\362~\\371"
+			":al=\\E[L:bl=^G:bt=\\E[Z:cb=\\E[1K:cd=\\E[J:ce=\\E[K"
+			":ch=\\E[%%i%%dG:cl=\\E[2J:cm=\\E[%%i%%d;%%dH:cr=^M:cs=\\E[%%i%%d;%%dr"
+			":cv=\\E[%%i%%dH:dc=\\E[P:dl=\\E[M:do=^J:ec=\\E[%%dX:ho=\\E[H"
+			":k1=\\EOP:k2=\\EOQ:k3=\\EOR:k4=\\EOS:k5=\\EOt:k6=\\E[17~"
+			":k7=\\E[18~:k8=\\E[19~:k9=\\E[20~:k;=\\E[21~:kD=\\177:kI=\\E[@"
+			":kN=\\E[U:kP=\\E[V:kb=^H:kd=\\E[B:kh=\\E[H:kl=\\E[D:kr=\\E[C"
+			":ku=\\E[A:le=\\E[D:ll=\\E[255H:mb=\\E[5m:md=\\E[1m:me=\\E[0m"
+			":nd=\\E[C:nw=^M^J:DC=\\E[%%dP"
+			":r1=\\E[?7h\\E[?25h\\E[?31l\\E[?32l\\E[?33l\\E[*r\\E[ D\\E[0m\\E[?s"
+			":rc=\\E[u"
+			":sc=\\E[s:sf=\\E[S:so=\\E[0;1;7m:sr=\\E[T:ta=^I:up=\\E[A"
+			":ve=\\E[?25h:vi=\\E[?25l:",ws.ws_col,ws.ws_row);
 		setenv("TERMCAP",termcap,1);
 		xp_asprintf_free(termcap);
 		termcap=xp_asprintf("%d",ws.ws_col);
