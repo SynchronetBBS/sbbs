c38948a4b22a4c1362c11e8916ebc51bb7708cbb
deuce <>
2018-02-13 20:11:49 +0000

    Fix gettext() on 16+ colour displays.

--- src/conio/curs_cio.c
+++ src/conio/curs_cio.c
@@ -575,7 +575,10 @@ int curs_gettext(int sx, int sy, int ex, int ey, void *fillbuf)
 				attrib |= 128;
 			}
 			colour=PAIR_NUMBER(attr&A_COLOR)-1;
-			colour=((colour&56)<<1)|(colour&7);
+			if (COLORS >= 16)
+				colour=colour&0x7f;
+			else
+				colour=((colour&56)<<1)|(colour&7);
 			fill[fillpos++]=colour|attrib;
 		}
 	}
