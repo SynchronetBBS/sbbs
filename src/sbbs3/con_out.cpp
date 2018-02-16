d3661e197ab9b15bef5a0978ca61198a0cdf6dc6
rswindell <>
2018-02-16 09:01:01 +0000

    Remove unused variable.

--- src/sbbs3/con_out.cpp
+++ src/sbbs3/con_out.cpp
@@ -177,8 +177,6 @@ long sbbs_t::term_supports(long cmp_flags)
 /****************************************************************************/
 void sbbs_t::outchar(char ch)
 {
-	int		i;
-
 	/*
 	 * outchar_esc values:
 	 * 0: No sequence
