d1ef562405da02b08c9624f90fc717323b68fbf2
rswindell <>
2018-02-20 00:24:09 +0000

    A simple script to clean all the relevant sbbs3 build output directories. Use 'cleanall.sh RELEASE=1' to clean the release output dirs. The deafult is the DEBUG output dirs otherwise. This possibly could be made a 'clean' target of the Makefile already in this directory, but I don't want to deal with recursive make right now.

---
+++ src/cleanall.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+make -C conio clean $@
+make -C sbbs3 clean $@
+make -C sbbs3/scfg clean $@
+make -C sbbs3/uedit clean $@
+make -C sbbs3/umonitor clean $@
+make -C smblib clean $@
+make -C uifc clean $@
+make -C xpdev clean $@
