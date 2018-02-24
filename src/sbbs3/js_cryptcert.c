b4a9e450ecc386d6eb94f5faaa56dbb9801ffb77
rswindell <>
2018-02-24 21:58:38 +0000

    Resolve compile warnings from msvc2013.

--- src/sbbs3/js_cryptcert.c
+++ src/sbbs3/js_cryptcert.c
@@ -362,7 +362,7 @@ js_get_attribute_time(JSContext *cx, uintN argc, jsval *arglist)
 		js_cryptcert_error(cx, p->cert, status);
 		return JS_FALSE;
 	}
-	msec = val;
+	msec = (jsdouble)val;
 	msec *= 1000;
 	dobj = JS_NewDateObjectMsec(cx, msec);
 	if(dobj==NULL)
@@ -496,7 +496,7 @@ js_set_attribute_time(JSContext *cx, uintN argc, jsval *arglist)
 			return JS_FALSE;
 		}
 	}
-	val = sec;
+	val = (time_t)sec;
 
 	if ((p=(struct js_cryptcert_private_data *)JS_GetPrivate(cx,obj))==NULL) {
 		JS_ReportError(cx, getprivate_failure, WHERE);
@@ -631,7 +631,7 @@ js_cryptcert_attrtime_get(JSContext *cx, jsval *vp, CRYPT_CERTIFICATE cert, CRYP
 		js_cryptcert_error(cx, cert, status);
 		return JS_FALSE;
 	}
-	msec = t;
+	msec = (jsdouble)t;
 	msec *= 1000;
 	dobj = JS_NewDateObjectMsec(cx, msec);
 	if(dobj==NULL)
@@ -1932,7 +1932,7 @@ js_cryptcert_attrtime_set(JSContext *cx, jsval *vp, CRYPT_CERTIFICATE cert, CRYP
 			return JS_FALSE;
 		}
 	}
-	t = sec;
+	t = (time_t)sec;
 
 	status = cryptSetAttributeString(cert, type, &t, sizeof(t));
 	if (cryptStatusError(status)) {
