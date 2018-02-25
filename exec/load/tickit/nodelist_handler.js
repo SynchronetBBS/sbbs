dc791c2a97777e7988ad302c791257e6f28c3c77
deuce <>
2018-02-25 07:55:08 +0000

    !

--- exec/load/tickit/nodelist_handler.js
+++ exec/load/tickit/nodelist_handler.js
@@ -121,7 +121,7 @@ function Handle_TIC(tic, ctx, arg)
 	}
 
 	log(LOG_DEBUG, "Copying "+fl[0]+" to "+ctx.FIDO.FTNDomains.nodeListFN[cfg.domain]+" from "+tic.file);
-	if (file_copy(fl[0], ctx.FIDO.FTNDomains.nodeListFN[cfg.domain]))
+	if (!file_copy(fl[0], ctx.FIDO.FTNDomains.nodeListFN[cfg.domain]))
 		log(LOG_ERR, "Failed to copy  "+fl[0]+" to "+ctx.FIDO.FTNDomains.nodeListFN[cfg.domain]+" from "+tic.file);
 
 	// Return false so it is still moved into the appropriate dir
