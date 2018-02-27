e2d64c7b95331a01347e2f634cef849511f420d8
deuce <>
2018-02-27 08:03:26 +0000

    Revert commit during update breakage.

--- src/sbbs3/prntfile.cpp
+++ src/sbbs3/prntfile.cpp
@@ -228,4 +228,6 @@ bool sbbs_t::menu_exists(const char *code)
 		return fexistcase(menu_file) ? true : false;
 
 	backslash(menu_dir);
-	SAFEPRINTF3(path, "%smenu/%s%s.asc", cfg.text_dir, menu_di
+	SAFEPRINTF3(path, "%smenu/%s%s.asc", cfg.text_dir, menu_dir, code);
+	return fexistcase(path) ? true : false;
+}
