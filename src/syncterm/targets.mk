4762ac1a374ae7ace278da3a783f480208b9582c
deuce <>
2018-02-28 06:43:30 +0000

    SyncTERM does not depend on smblib.

--- src/syncterm/targets.mk
+++ src/syncterm/targets.mk
@@ -2,5 +2,5 @@ SYNCTERM	=	$(EXEODIR)$(DIRSEP)syncterm$(EXEFILE)
 
 all: xpdev-mt ciolib-mt uifc-mt $(MTOBJODIR) $(EXEODIR) $(SYNCTERM)
 
-$(SYNCTERM):	$(XPDEV-MT_LIB) $(CIOLIB-MT) $(UIFCLIB-MT) $(SMBLIB)
+$(SYNCTERM):	$(XPDEV-MT_LIB) $(CIOLIB-MT) $(UIFCLIB-MT)
 
