b52e31d58ff9176a6b9b1b3a4035c442860af5db
deuce <>
2018-03-05 05:55:49 +0000

    Add AUTH, PBSZ, PROT, and CCC to HELP.  Also, add CCC to FEAT

--- src/sbbs3/ftpsrvr.c
+++ src/sbbs3/ftpsrvr.c
@@ -2755,7 +2755,7 @@ static void ctrl_thread(void* arg)
 			sockprintf(sock,sess," QUIT    REIN    PORT    PASV    LIST    NLST    NOOP    HELP");
 			sockprintf(sock,sess," SIZE    MDTM    RETR    STOR    REST    ALLO    ABOR    SYST");
 			sockprintf(sock,sess," TYPE    STRU    MODE    SITE    RNFR*   RNTO*   DELE*   DESC#");
-			sockprintf(sock,sess," FEAT#   OPTS#   EPRT    EPSV");
+			sockprintf(sock,sess," FEAT#   OPTS#   EPRT    EPSV    AUTH#   PBSZ#   PROT#   CCC#");
 			sockprintf(sock,sess,"214 Direct comments to sysop@%s.",scfg.sys_inetaddr);
 			continue;
 		}
@@ -2768,6 +2768,7 @@ static void ctrl_thread(void* arg)
 			sockprintf(sock,sess," AUTH TLS");
 			sockprintf(sock,sess," PBSZ");
 			sockprintf(sock,sess," PROT");
+			sockprintf(sock,sess," CCC");
 			sockprintf(sock,sess,"211 End");
 			continue;
 		}
