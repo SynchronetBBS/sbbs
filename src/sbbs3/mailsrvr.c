a656cfdd8d1a24f172885882df0d573a758cebeb
deuce <>
2018-03-10 09:20:26 +0000

    Mail server is guilty too.

--- src/sbbs3/mailsrvr.c
+++ src/sbbs3/mailsrvr.c
@@ -147,7 +147,7 @@ typedef struct {
 #define GCES(status, server, sock, sess, action) do {                             \
 	char *GCES_estr;                                                               \
 	int GCES_level;                                                                 \
-	get_crypt_error_string(status, sess, &GCES_estr, "flushing data", &GCES_level);  \
+	get_crypt_error_string(status, sess, &GCES_estr, action, &GCES_level);  \
 	if (GCES_estr) {                                                                  \
 		lprintf(GCES_level, "%04d %s%s", sock, server, GCES_estr);                     \
 		free_crypt_attrstr(GCES_estr);                                                  \
@@ -157,7 +157,7 @@ typedef struct {
 #define GCESH(status, server, sock, host, sess, action) do {                      \
 	char *GCES_estr;                                                               \
 	int GCES_level;                                                                 \
-	get_crypt_error_string(status, sess, &GCES_estr, "flushing data", &GCES_level);  \
+	get_crypt_error_string(status, sess, &GCES_estr, action, &GCES_level);  \
 	if (GCES_estr) {                                                                  \
 		lprintf(GCES_level, "%04d %s [%s] %s", sock, server, host, GCES_estr);         \
 		free_crypt_attrstr(GCES_estr);                                                  \
