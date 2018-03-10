c410b29ec8ddcb2ff9abe29274f4e05313d4a25f
rswindell <>
2018-03-10 04:29:37 +0000

    Elminated the redundnat "XXX Server listening" log messages (the multisock stuff already logs this for each interface and port).

--- src/sbbs3/main.cpp
+++ src/sbbs3/main.cpp
@@ -4915,14 +4915,11 @@ void DLLCALL bbs_thread(void* arg)
 	 */
 	xpms_add_list(ts_set, PF_UNSPEC, SOCK_STREAM, 0, startup->telnet_interfaces, startup->telnet_port, "Telnet Server", sock_cb, startup->seteuid, &telnet_cb);
 
-	lprintf(LOG_INFO,"Telnet Server listening");
-
 	if(startup->options&BBS_OPT_ALLOW_RLOGIN) {
 		/* open a socket and wait for a client */
 		rlogin_cb.protocol="rlogin";
 		rlogin_cb.startup=startup;
 		xpms_add_list(ts_set, PF_UNSPEC, SOCK_STREAM, 0, startup->rlogin_interfaces, startup->rlogin_port, "RLogin Server", sock_cb, startup->seteuid, &rlogin_cb);
-		lprintf(LOG_INFO,"RLogin Server listening");
 	}
 
 #ifdef USE_CRYPTLIB
@@ -4973,7 +4970,6 @@ void DLLCALL bbs_thread(void* arg)
 		ssh_cb.protocol="ssh";
 		ssh_cb.startup=startup;
 		xpms_add_list(ts_set, PF_UNSPEC, SOCK_STREAM, 0, startup->ssh_interfaces, startup->ssh_port, "SSH Server", sock_cb, startup->seteuid, &ssh_cb);
-		lprintf(LOG_INFO,"SSH Server listening");
 	}
 NO_SSH:
 #endif
