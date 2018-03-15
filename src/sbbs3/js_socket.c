e482ef1412ba8ddd87d1a23d9eaac0bdf625d265
deuce <>
2018-03-15 22:07:19 +0000

    Add missing else in last commit.

--- src/sbbs3/js_socket.c
+++ src/sbbs3/js_socket.c
@@ -171,7 +171,8 @@ static ptrdiff_t js_socket_recv(js_socket_private_t *p, void *buf, size_t len, i
 			tv.tv_sec = timeout;
 			if(select(p->sock+1,&socket_set,NULL,NULL,&tv)==1)
 				ret = recv(p->sock, buf, len, flags);
-			ret = 0;
+			else
+				ret = 0;
 		}
 		else {
 			status = cryptPopData(p->session, buf, len, &copied);
