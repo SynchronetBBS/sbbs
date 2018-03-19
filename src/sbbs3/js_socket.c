e0f415e14dac4a243f6456ad94055558b702c321
deuce <>
2018-03-19 21:45:56 +0000

    If select() returns an error, return it rather than forcing the return value to zero.

--- src/sbbs3/js_socket.c
+++ src/sbbs3/js_socket.c
@@ -238,10 +238,8 @@ static ptrdiff_t js_socket_recv(js_socket_private_t *p, void *buf, size_t len, i
 			FD_ZERO(&socket_set);
 			FD_SET(p->sock,&socket_set);
 			tv.tv_sec = timeout;
-			if(select(p->sock+1,&socket_set,NULL,NULL,&tv)==1)
+			if((ret = select(p->sock+1,&socket_set,NULL,NULL,&tv))==1)
 				ret = recv(p->sock, buf, len, flags);
-			else
-				ret = 0;
 		}
 		else {
 			status = cryptPopData(p->session, buf, len, &copied);
