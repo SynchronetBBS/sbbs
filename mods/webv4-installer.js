1ee2c2c0bc82dc3f344c3980dd9dc48612bf76fa
echicken <echicken@bbs.electronicchicken.com>
2018-03-31 16:51:10 -0400

    Extract zip to temp dir, move files around, clean up after yourself, don't clobber custom pages or sidebar modules. More prompts, including an 'enable ftelnet' option that isn't actually used for anything yet. Update sbbs.ini RootDirectory and ErrorDirectory last of all; should trigger recycle. Install path is system.exec_dir + ../webv4. Rather than move stock components out of the way, we'll just make a new space.  People who installed to, say, system.exec_dir + ../web will need to migrate content; instructions forthcoming.

--- mods/webv4-installer.js
+++ mods/webv4-installer.js
@@ -54,19 +54,71 @@ function get_setting(text, value) {
     return (i == '' ? value : i);
 }
 
+function confirm_setting(text, value) {
+    if (!value) {
+        return !deny(text + ' [' + value + ']');
+    } else {
+        return confirm(text + ' [' + value + ']');
+    }
+}
+
 function get_settings(modopts) {
-    writeln('---');
+    write('\r\n---\r\n\r\n');
+    write('Configuration - press enter to accept default/current value.\r\n\r\n');
     modopts.guest = get_setting('Guest user alias', modopts.guest);
-    modopts.ftelnet_splash = get_setting('Path to ftelnet background .ans', modopts.ftelnet_splash);
+    modopts.ftelnet = confirm_setting('Enable fTelnet', modopts.ftelnet);
+    if (modopts.ftelnet) modopts.ftelnet_splash = get_setting('Path to ftelnet background .ans', modopts.ftelnet_splash);
+    modopts.user_registration = confirm_setting('Allow new user registration via the web', modopts.user_registration);
+    write('\r\n\r\n---\r\n\r\n');
     return modopts;
+}
+
+function copy_dir_contents(src, dest, overwrite) {
+    src = backslash(fullpath(src));
+    dest = backslash(fullpath(dest));
+    const delim = src.substr(-1);
+    if (!file_isdir(dest)) mkdir(dest);
+    directory(src + '*').forEach(
+    	function (e) {
+    		e = fullpath(e);
+    		if (file_isdir(e)) {
+    			const path = e.split(delim);
+    			var df = dest + path[path.length - 2];
+    			if (!file_isdir(df)) mkdir(df);
+    			copy_dir_contents(e, df, overwrite);
+    		} else {
+    			var df = dest + file_getname(e);
+    			if (!file_exists(df) || overwrite) {
+					file_copy(e, dest + file_getname(e));
+    			}
+    		}
+    	}
+    );
+}
+
+// yikes
+function remove_dir(dir) {
+    dir = backslash(fullpath(dir));
+    directory(dir + '*').forEach(
+    	function (e) {
+    		if (file_isdir(e)) {
+    			remove_dir(e);
+    		} else {
+	    		file_remove(e);
+	    	}
+    	}
+    );
+    rmdir(dir);
 }
 
 const zip_url = 'https://codeload.github.com/echicken/synchronet-web-v4/zip/master';
 const download_target = system.temp_dir + 'webv4.zip';
-const extract_dir = fullpath(system.exec_dir + '../web');
-const install_dir = fullpath(extract_dir + '/synchronet-web-v4-master');
-const root_directory = fullpath(system.exec_dir + '../web/synchronet-web-v4-master/root');
+const extract_dir = fullpath(system.temp_dir);
+const temp_dir = fullpath(extract_dir + '/synchronet-web-v4-master');
+const install_dir = fullpath(system.exec_dir + '../webv4');
+const root_directory = fullpath(install_dir + '/root');
 const error_directory = fullpath(root_directory + '/errors');
+
 var modopts_web = get_modopts_ini();
 if (!modopts_web) {
     modopts_web = {
@@ -77,6 +129,7 @@ if (!modopts_web) {
     	minimum_password_length : 6,
     	maximum_telegram_length : 800,
     	web_directory : install_dir,
+        ftelnet : true,
     	ftelnet_splash : '../text/synch.ans',
     	keyboard_navigation : false,
     	vote_functions : true,
@@ -90,23 +143,42 @@ if (!modopts_web) {
     };
 }
 
-writeln('Downloading ' + zip_url + ' to ' + download_target + ' ...');
+write('\r\n---\r\n\r\n');
+writeln('ecwebv4 installer/updater');
+writeln('https://github.com/echicken/synchronet-web-v4');
+write('\r\nIt is strongly recommended that you back up your BBS before proceeding.\r\n\r\n');
+if (deny('Proceed with installation/update')) {
+    writeln('Install/update aborted.  Exiting.');
+    exit();
+}
+write('\r\n\r\n---\r\n\r\n');
+
+writeln('Downloading ' + zip_url);
 if (!download(zip_url, download_target)) {
     writeln('Download of ' + zip_url + ' failed. Exiting.');
     exit();
 }
 
-writeln('Extracting ' + download_target + ' to ' + extract_dir + ' ...');
+writeln('Extracting ' + download_target);
 if (!extract(download_target, extract_dir)) {
     writeln('Extraction of ' + download_target + ' failed. Exiting.');
     exit();
 }
 
-writeln('Updating sbbs.ini ...');
-if (!update_sbbs_ini(root_directory, error_directory)) {
-    writeln('Failed to update sbbs.ini. Exiting.');
-    exit();
-}
+writeln('Copying files ...');
+copy_dir_contents(temp_dir + '/mods', system.mods_dir, true);
+copy_dir_contents(temp_dir + '/text', system.text_dir, true);
+copy_dir_contents(temp_dir + '/web', install_dir, true);
+copy_dir_contents(temp_dir + '/web/pages/.examples', install_dir + '/pages', false);
+copy_dir_contents(temp_dir + '/web/pages/.examples', install_dir + '/pages/.examples', true);
+copy_dir_contents(temp_dir + '/web/sidebar/.examples', install_dir + '/sidebar', false);
+copy_dir_contents(temp_dir + '/web/sidebar/.examples', install_dir + '/sidebar/.examples', true);
+
+writeln('Cleaning up ...');
+remove_dir(temp_dir + '/web/pages/.examples');
+remove_dir(temp_dir + '/web/sidebar/.examples');
+remove_dir(temp_dir);
+file_remove(download_target);
 
 modopts_web = get_settings(modopts_web);
 
@@ -114,4 +186,17 @@ writeln('Updating modopts.ini ...');
 if (!update_modopts_ini(modopts_web)) {
     writeln('Failed to update modopts.ini. Exiting.');
     exit();
+}
+
+writeln('Updating sbbs.ini ...');
+if (!update_sbbs_ini(root_directory, error_directory)) {
+    writeln('Failed to update sbbs.ini. Exiting.');
+    exit();
+} else {
+    write('\r\n---\r\n\r\n');
+    writeln('Install/update complete.');
+    writeln('Changes will take effect once your BBS has been restarted.');
+    writeln('For additional configuration and customization steps,');
+    writeln('visit https://github.com/echicken/synchronet-web-v4');
+    write('\r\n\r\n---\r\n\r\n');
 }
