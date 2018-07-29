3dffa7e6a2867095dc00c80d2a2f92028e689103
rswindell <>
2018-07-29 05:01:42 +0000

    Store a history (of 10) destination netmail addresses and pass them to console.getstr() so they can be recalled with the up/down arrow keys.

--- exec/email_sec.js
+++ exec/email_sec.js
@@ -9,6 +9,8 @@ load("sbbsdefs.js");
 var text = load({}, "text.js");
 var userprops = load({}, "userprops.js");
 
+const NetmailAddressHistoryLength = 10;
+
 while(bbs.online) {
 	if(!(user.settings & USER_EXPERT))
 		bbs.menu("e-mail");
@@ -55,12 +57,16 @@ while(bbs.online) {
 			if((netmail&NMAIL_FILE) && !console.noyes("Attach a file"))
 				wm_mode = WM_FILE;
 			console.print(bbs.text(text.EnterNetMailAddress));
-			var addr = userprops.get("netmail sent", "address");
-			addr = console.getstr(addr, 60, K_LINE|K_EDIT);
+			var addr_list = userprops.get("netmail sent", "address", []);
+			var addr = console.getstr(60, K_LINE, addr_list);
 			if(!addr || console.aborted)
 				break;
 			if(bbs.netmail(addr, wm_mode)) {
-				userprops.set("netmail sent", "address", addr);
+				if(addr_list.indexOf(addr) == -1)
+					addr_list.unshift(addr);
+				if(addr_list.length > NetmailAddressHistoryLength)
+					addr_list.length = NetmailAddressHistoryLength;
+				userprops.set("netmail sent", "address", addr_list);
 				userprops.set("netmail sent", "localtime", new Date().toString());
 			}
 			break;
