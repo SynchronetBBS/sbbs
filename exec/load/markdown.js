cbcb787e697f084c44c824e33b231e90230cdab3
echicken <>
2018-09-27 22:21:59 +0000

    Numbered list fixes.

--- exec/load/markdown.js
+++ exec/load/markdown.js
@@ -270,11 +270,11 @@ Markdown.prototype.render_line_console = function (line) {
       lt = 'ol';
     }
     if (match[1].length > this.state.list_level) {
-      this.state.list_level++;
+      this.state.list_level = match[1].length;
       if (lt == 'ol') this.state.list_stack[this.state.list_level] = 0;
     } else if (match[1].length < this.state.list_level) {
       if (lt == 'ol') this.state.list_stack.splice(this.state.list_level, 1);
-      this.state.list_level--;
+      this.state.list_level = match[1].length;
     } else if (lt == 'ol') {
       if (typeof this.state.list_stack[this.state.list_level] != 'number') {
         this.state.list_stack[this.state.list_level] = 0;
