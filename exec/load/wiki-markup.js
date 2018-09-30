0303c7e90da06c40e19401033cdad24dc7bd6e94
echicken <>
2018-09-30 03:52:02 +0000

    Pass columns (console or Frame) to word_wrap for adequate girthiness.

--- exec/load/wiki-markup.js
+++ exec/load/wiki-markup.js
@@ -284,7 +284,7 @@ WikiMarkup.prototype.render_line_console = function (line) {
 
   var match;
   const self = this;
-  var ret = word_wrap(this.render_text_console(line));
+  var ret = word_wrap(this.render_text_console(line), this.columns);
 
   // Ordered and unordered lists
   match = ret.match(/^(\s*)(\*|-)\s+(.+)$/m);
