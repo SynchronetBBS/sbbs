96c68f5ea3da0eecbc5375c4c6623864e34c98ab
rswindell <>
2018-10-04 03:42:20 +0000

    Added new @-codes: MSG_SUMMARY and MSG_TAGS. MSG_SUMMARY is currently never populated by anything. MSG_TAGS is a new (optional) thing.

--- src/sbbs3/atcodes.cpp
+++ src/sbbs3/atcodes.cpp
@@ -1029,6 +1029,10 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 	}
 	if(!strcmp(sp,"MSG_SUBJECT") && current_msg!=NULL)
 		return(current_msg->subj==NULL ? nulstr : current_msg->subj);
+	if(!strcmp(sp,"MSG_SUMMARY") && current_msg!=NULL)
+		return(current_msg->summary==NULL ? nulstr : current_msg->summary);
+	if(!strcmp(sp,"MSG_TAGS") && current_msg!=NULL)
+		return(current_msg->tags==NULL ? nulstr : current_msg->tags);
 	if(!strcmp(sp,"MSG_DATE") && current_msg!=NULL)
 		return(timestr(current_msg->hdr.when_written.time));
 	if(!strcmp(sp,"MSG_AGE") && current_msg!=NULL)
