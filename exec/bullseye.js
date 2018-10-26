129506c9ed66271911d62823af6194825f342bf3
rswindell <>
2018-10-26 02:12:50 +0000

    Shortened the prompt to fit nicely on 40-column display. Also, using mnemonics() for better dumb terminal support.

--- exec/bullseye.js
+++ exec/bullseye.js
@@ -53,7 +53,7 @@ while((str=file.readln())!=null) {
 menu:
 while(1) {
 	bbs.menu("../bullseye");
-	write("\001n\r\n\001b\001hEnter number of bulletin to view or press (\001wENTER\001b) to continue: \001w");
+	console.mnemonics("\r\nEnter number of bulletin or [~Quit]: ");
 	b=console.getnum(total);
 	if(b<1)
 		exit(0);
