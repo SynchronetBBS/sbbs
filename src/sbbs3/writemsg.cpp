33bae8b66165d744d1b313113f198ba135217923
rswindell <>
2018-11-09 03:19:14 +0000

    Store the draft message creation time stamp (just informational for now) and log a message when a draft message is saved.

--- src/sbbs3/writemsg.cpp
+++ src/sbbs3/writemsg.cpp
@@ -517,8 +517,11 @@ bool sbbs_t::writemsg(const char *fname, const char *top, char *subj, long mode,
 
 		checkline();
 		if(!online)	 { // save draft due to disconnection
-			mv(msgtmp, draft, /* copy: */true);
-			user_set_property(&cfg, useron.number, draft_desc, "subject", subj);
+			if(mv(msgtmp, draft, /* copy: */true) == 0) {
+				user_set_property(&cfg, useron.number, draft_desc, "subject", subj);
+				user_set_time_property(&cfg, useron.number, draft_desc, "created", time(NULL));
+				lprintf(LOG_NOTICE, "Draft message saved: %s", draft);
+			}
 		}
 
 		if(result != EXIT_SUCCESS || !fexistcase(msgtmp) || !online
