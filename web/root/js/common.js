e1d9deeeaf503b828cc69966b6dcd4569933cf1c
echicken <echicken@bbs.electronicchicken.com>
2019-01-06 00:21:03 -0500

    Moved event subscription/handling stuff from index.xjs to common.js. Tidied up common.js, removed telegram polling, switched to telegram event subscription and handler.

--- web/root/js/common.js
+++ web/root/js/common.js
@@ -1,45 +1,36 @@
 // How often to check for unread mail, new telegrams (milliseconds)
 var updateInterval = 60000;
+const _sbbs_events = {};
 
 function login(evt) {
-	if ($('#input-username').val() === '' ||
-		$('#input-password').val() === ''
-	) {
+	if ($('#input-username').val() == '' || $('#input-password').val() == '') {
 		return;
 	}
 	if (typeof evt !== 'undefined') evt.preventDefault();
-	$.ajax(
-		{	'url' : './api/auth.ssjs',
-			'method' : 'POST',
-			'data' : {
-				username : $('#input-username').val(),
-				password : $('#input-password').val()
-			}
+	$.ajax({
+        url: './api/auth.ssjs',
+		method: 'POST',
+		data: {
+			username : $('#input-username').val(),
+			password : $('#input-password').val()
 		}
-	).done(
-		function (data) {
-			if (data.authenticated) {
-				window.location.reload(true);
-			} else {
-				$('#login-form').append(
-					'<p class="text-danger">Login failed</p>'
-				);
-			}
+	}).done(function (data) {
+		if (data.authenticated) {
+			window.location.reload(true);
+		} else {
+			$('#login-form').append('<p class="text-danger">Login failed</p>');
 		}
-	);
+	});
 }
 
 function logout() {
-	$.ajax(
-		{	'url' : './api/auth.ssjs',
-			'method' : 'GET',
-			'data' : {
-				'logout' : true
-			}
-		}
-	).done(
-		function (data) { if (!data.authenticated) window.location.href = '/'; }
-	);
+	$.ajax({
+        url: './api/auth.ssjs',
+		method: 'GET',
+		data: { logout: true }
+	}).done(function (data) {
+        if (!data.authenticated) window.location.href = '/';
+    });
 }
 
 function scrollUp() {
@@ -49,15 +40,10 @@ function scrollUp() {
 }
 
 function getMailUnreadCount() {
-	$.getJSON(
-		'./api/forum.ssjs?call=get-mail-unread-count',
-		function (data) {
-			$('#badge-unread-mail').text(data.count < 1 ? '' : data.count);
-			$('#badge-unread-mail-inner').text(
-				data.count < 1 ? '' : data.count
-			);
-		}
-	);
+	$.getJSON('./api/forum.ssjs?call=get-mail-unread-count', function (data) {
+		$('#badge-unread-mail').text(data.count < 1 ? '' : data.count);
+		$('#badge-unread-mail-inner').text(data.count < 1 ? '' : data.count);
+	});
 }
 
 function sendTelegram(alias) {
@@ -67,39 +53,15 @@ function sendTelegram(alias) {
 		'placeholder="My message" name="telegram" id="telegram">'
 	);
 	$('#popUpModalActionButton').show();
-	$('#popUpModalActionButton').click(
-		function () {
-			$.getJSON(
-				'./api/system.ssjs?call=send-telegram&user=' +
-				alias + '&telegram=' + $('#telegram').val(),
-				function(data) {}
-			);
-			$('#popUpModal').modal('hide');
-		}
-	);
+	$('#popUpModalActionButton').click(function () {
+		$.getJSON(
+			'./api/system.ssjs?call=send-telegram&user=' +
+			alias + '&telegram=' + $('#telegram').val(),
+			function(data) {}
+		);
+        $('#popUpModal').modal('hide');
+	});
 	$('#popUpModal').modal('show');
-}
-
-function getTelegram() {
-	$.getJSON(
-		'./api/system.ssjs?call=get-telegram',
-		function (data) {
-			if (typeof data.telegram === 'undefined' ||
-				data.telegram === null
-			) {
-				return;
-			}
-			var tg = data.telegram.replace(
-				/\1./g, ''
-			).replace(
-				/\r?\n/g, '<br>'
-			);
-			$('#popUpModalTitle').html('New telegram(s) received');
-			$('#popUpModalBody').append(tg);
-			$('#popUpModalActionButton').hide();
-			$('#popUpModal').modal('show');
-		}
-	);
 }
 
 window.onload =	function () {
@@ -108,31 +70,43 @@ window.onload =	function () {
 	$('#button-login').click(login);
 	$('#form-login').submit(login);
 
-	$('#popUpModal').on(
-		'hidden.bs.modal',
-		function (e) {
-			$('#popUpModalActionButton').off('click');
-			$('#popUpModalTitle').empty();
-			$('#popUpModalBody').empty();
-		}
-	);
-	$("#popUpModalCloseButton").click(
-		function () {
-			$('#popUpModal').modal('hide');
-		}
-	);
+	$('#popUpModal').on('hidden.bs.modal', function (e) {
+		$('#popUpModalActionButton').off('click');
+		$('#popUpModalTitle').empty();
+		$('#popUpModalBody').empty();
+	});
+	$("#popUpModalCloseButton").click(function () {
+		$('#popUpModal').modal('hide');
+	});
 
 	setTimeout(scrollUp, 25);
 	window.onhashchange = scrollUp;
 
 	if ($('#button-logout').length > 0) {
 
-        getMailUnreadCount();
-        setInterval(getMailUnreadCount, updateInterval);
+        // Write backing event module
+        // Switch to event subscription & callback
+		getMailUnreadCount();
+		setInterval(getMailUnreadCount, updateInterval);
 
-        getTelegram();
-        setInterval(getTelegram, updateInterval);
+        _sbbs_events.telegram = function (e) {
+            const tg = JSON.parse(e.data).replace(/\1./g, '').replace(
+                /\r?\n/g, '<br>'
+            );
+            $('#popUpModalTitle').html('New telegram(s) received');
+            $('#popUpModalBody').append(tg);
+            $('#popUpModalActionButton').hide();
+            $('#popUpModal').modal('show');
+        }
 
-    }
+	}
+
+    const _evtqs = Object.keys(_sbbs_events).reduce(function (a, c, i) {
+        return a + (i == 0 ? '?' : '&') + 'subscribe=' + c; }, ''
+    );
+    const _es = new EventSource('/api/events.ssjs' + _evtqs);
+    Object.keys(_sbbs_events).forEach(function (e) {
+        _es.addEventListener(e, _sbbs_events[e]);
+    });
 
 }
