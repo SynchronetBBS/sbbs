73281a6cc688c575101ac72b08dd17691d85451a
echicken <echicken@bbs.electronicchicken.com>
2019-01-06 00:53:46 -0500

    Emit an event if mail count is 0 but last mail count was > 0, to clear the indicator ir mail was picked up elsewhere.

--- web/lib/events/mail.js
+++ web/lib/events/mail.js
@@ -2,13 +2,17 @@ load(settings.web_lib + 'forum.js');
 
 var last_run = 0;
 var frequency = 30;
+var last_count = 0;
 
 function cycle() {
     if (user.number < 1 || user.alias == settings.guest) return;
     if (time() - last_run <= frequency) return;
     last_run = time();
     const count = user.stats.mail_waiting;
-    if (count > 0) emit({ event: 'mail', data: JSON.stringify({ count: count })});
+    if (count > 0 || (count == 0 && last_count > 0)) {
+        emit({ event: 'mail', data: JSON.stringify({ count: count })});
+    }
+    last_count = count;
 }
 
 this;
