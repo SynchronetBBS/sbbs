61b761a251ad3c1926abb41923f39247df6702c8
rswindell <>
2019-01-11 01:11:17 +0000

    Added Synchronet/ftelnet's WS/WSS port definitions. Added more comments and use 'var' when defining a new variable (though 'const' might be better here, don't want to risk that change atm).

--- exec/load/portdefs.js
+++ exec/load/portdefs.js
@@ -9,43 +9,45 @@
 // Duplicates port numbers for service name aliases are included
 // (e.g. both "nttp" and "news")
 
-standard_service_port = {
-	"systat":	11,
-	"users":	11,
-	"qotd":		17,
-	"msp":		18,	// Message send protocol
-	"ftp-data":	20,
-	"ftp":		21,
-	"ssh": 		22,
-	"telnet": 	23,
-	"smtp":		25,
-	"mail":		25,
-	"gopher":	70,
-	"finger":	79,
-	"http":		80,
-	"pop3":		110,
-    "auth":     113,
-    "ident":    113,
-	"nntp":		119,
-	"news":		119,
-	"imap":		143,
-	"imap2":	143,
-	"https":	443,
-	"submissions":	465,	// SMTPS
-	"login":	513,
-	"rlogin":	513,
-	"talk":		517,
-	"ntalk":	518,
-	"nntps":	563,
+var standard_service_port = {
+	"systat":		11,		// Active Users
+	"users":		11,		// Active Users
+	"qotd":			17,		// Quote Of The Day
+	"msp":			18,		// Message send protocol
+	"ftp-data":		20,
+	"ftp":			21,
+	"ssh": 			22,
+	"telnet": 		23,
+	"smtp":			25,
+	"mail":			25,
+	"gopher":		70,
+	"finger":		79,
+	"http":			80,
+	"pop3":			110,
+	"auth":			113,
+	"ident":		113,
+	"nntp":			119,
+	"news":			119,
+	"imap":			143,
+	"imap2":		143,
+	"https":		443,	// HTTP/TLS
+	"submissions":	465,	// SMTP/TLS
+	"login":		513,
+	"rlogin":		513,
+	"talk":			517,
+	"ntalk":		518,
+	"nntps":		563,	// NNTP/TLS
 	"submission":	587,	// SMTP
-	"ftps":		990,
-	"telnets":	992,
-	"imaps":	993,
-	"ircs":		994,
-	"pop3s":	995,
-	"hotline":	5500,
-	"irc":		6667,
-	"http-alt":	8080,
-	"json":		10088,
-	"binkp":	24554,
+	"ftps":			990,	// FTP/TLS
+	"telnets":		992,	// Telnet/TLS
+	"imaps":		993,	// IMAP/TLS
+	"ircs":			994,	// IRC/TLS
+	"pop3s":		995,	// POP3/TLS
+	"ws":			1123,	// SBBS
+	"hotline":		5500,
+	"irc":			6667,
+	"http-alt":		8080,
+	"json":			10088,	// SBBS
+	"wss":			11235,	// SBBS
+	"binkp":		24554,
 };
