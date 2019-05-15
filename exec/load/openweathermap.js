0dc9c4af842dc0a6c8e863a7d8c961f2144a2512
mcmlxxix <>
2019-05-15 16:40:39 +0000

    throw exception on no api key found in modopts.ini

--- exec/load/openweathermap.js
+++ exec/load/openweathermap.js
@@ -18,6 +18,7 @@ function OpenWeatherMap() {
 	if (!this.settings.rate_window) this.settings.rate_window = 60; // Seconds
     if (!this.settings.rate_limit) this.settings.rate_limit = 60; // Requests per window
     if (!this.settings.data_refresh) this.settings.data_refresh = 7200; // Seconds
+	if (!this.settings.api_key) throw("no openweathermap API key found in modopts.ini");
     this.rate_file = new File(system.temp_dir + 'openweathermap_rate.json');
 }
 
