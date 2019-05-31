0816018f04404274e69e5e4318654e06fc8e7e6f
deuce <>
2019-05-31 19:16:29 +0000

    Remove exit() hack.  Now that we have random_exit.patch in cryptlib, we don't need it.
    This should fix OS X (and other systems) doing weird things when they
    exit SyncTERM.  We were forcibly preventing atexit() handlers from running,
    and pretty much everything installed some of them.

--- src/syncterm/st_crypt.c
+++ src/syncterm/st_crypt.c
@@ -24,23 +24,6 @@ void exit_crypt()
 
 #else
 
-#ifdef __unix__
-/*
- * cryptlib calls fork() to gather entropy.
- * It then calls exit().
- * This calls the atexit() handlers.
- * SDL_Exit is in there...
- * SDL doesn't exist in the forked child.
- * This causes the child to spin FOREVER
- * Eating your CPU.
- * So, we will break exit(3).
- */
-void exit(int code)
-{
-	_exit(code);
-}
-#endif
-
 struct crypt_funcs cl;
 
 int init_crypt(void)
