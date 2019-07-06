d7e6975a4b68c9cf4525f16fa300e91aa8e58fdb
rswindell <>
2019-07-06 08:39:39 +0000

    Added USER_UTF8 user 'settings' flag. And renamed ICE_COLOR to USER_ICE_COLOR, to be consistent.

--- exec/load/userdefs.js
+++ exec/load/userdefs.js
@@ -30,7 +30,8 @@ const USER_HTML		    = (1<<23);	// Using Deuce's HTML terminal (*cough*)
 const USER_NOPAUSESPIN  = (1<<24);	// No spinning cursor at pause prompt		
 const USER_PETSCII      = (1<<26);	// Commodore PET (e.g. C64) terminal		
 const USER_SWAP_DELETE  = (1<<27);	// Swap the DEL and backspace keys			
-const ICE_COLOR         = (1<<28);
+const USER_ICE_COLOR    = (1<<28);	// Bright background color support
+const USER_UTF8 		= (1<<29);	// UTF-8 terminal
 
 //**********************************************************************
 // user.qwk_settings 						
