cb5609dc5c207452d1754cf11ca668d5bc8de35e
deuce <>
2019-07-09 22:17:57 +0000

    Implement DECRQSS for SCR.

--- src/conio/cterm.c
+++ src/conio/cterm.c
@@ -1539,16 +1539,16 @@ static void save_extended_colour_seq(struct cterminal *cterm, int fg, struct esc
 
 	switch (count) {
 		case 1:
-			*str = asprintf("%s", seq->param[seqoff]);
+			asprintf(str, "%s", seq->param[seqoff]);
 			break;
 		case 2:
-			*str = asprintf("%s;%s", seq->param[seqoff], seq->param[seqoff+1]);
+			asprintf(str, "%s;%s", seq->param[seqoff], seq->param[seqoff+1]);
 			break;
 		case 3:
-			*str = asprintf("%s;%s;%s", seq->param[seqoff], seq->param[seqoff+1], seq->param[seqoff+2]);
+			asprintf(str, "%s;%s;%s", seq->param[seqoff], seq->param[seqoff+1], seq->param[seqoff+2]);
 			break;
 		case 5:
-			*str = asprintf("%s;%s;%s;%s;%s", seq->param[seqoff], seq->param[seqoff+1], seq->param[seqoff+2], seq->param[seqoff+3], seq->param[seqoff+4]);
+			asprintf(str, "%s;%s;%s;%s;%s", seq->param[seqoff], seq->param[seqoff+1], seq->param[seqoff+2], seq->param[seqoff+3], seq->param[seqoff+4]);
 			break;
 	}
 }
@@ -1863,7 +1863,7 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 									strcpy(tmp, "\x1b[=2");
 									if(cterm->extattr & CTERM_EXTATTR_ORIGINMODE)
 										strcat(tmp, ";6");
-									if (cterm->extattr & CTERM_EXTATTR_AUTOWRAP);
+									if (cterm->extattr & CTERM_EXTATTR_AUTOWRAP)
 										strcat(tmp, ";7");
 									if(cterm->cursor == _NORMALCURSOR)
 										strcat(tmp, ";25");
@@ -2891,6 +2891,67 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 								switch (cterm->strbuf[2]) {
 									case 'm':
 										if (cterm->strbuf[3] == 0) {
+											strcpy(tmp, "\x1b" "1$r0");
+											if (cterm->attr & 8)
+												strcat(tmp, ";1");
+											if (cterm->attr & 128)
+												strcat(tmp, ";5");
+											if (cterm->fg_tc_str == NULL) {
+												switch (cterm->attr & 7) {
+													case 0:
+														strcat(tmp, ";30");
+														break;
+													case 1:
+														strcat(tmp, ";34");
+														break;
+													case 2:
+														strcat(tmp, ";32");
+														break;
+													case 3:
+														strcat(tmp, ";36");
+														break;
+													case 4:
+														strcat(tmp, ";31");
+														break;
+													case 5:
+														strcat(tmp, ";35");
+														break;
+													case 6:
+														strcat(tmp, ";33");
+														break;
+												}
+											}
+											if (cterm->bg_tc_str == NULL) {
+												switch ((cterm->attr >> 4) & 7) {
+													case 1:
+														strcat(tmp, ";44");
+														break;
+													case 2:
+														strcat(tmp, ";42");
+														break;
+													case 3:
+														strcat(tmp, ";46");
+														break;
+													case 4:
+														strcat(tmp, ";41");
+														break;
+													case 5:
+														strcat(tmp, ";45");
+														break;
+													case 6:
+														strcat(tmp, ";43");
+														break;
+													case 7:
+														strcat(tmp, ";47");
+														break;
+												}
+											}
+											if (cterm->fg_tc_str)
+												strcat(tmp, cterm->fg_tc_str);
+											if (cterm->bg_tc_str)
+												strcat(tmp, cterm->bg_tc_str);
+											if(strlen(retbuf)+strlen(tmp) < retsize)
+												strcat(retbuf, tmp);
 										}
 										break;
 									default:
@@ -2898,7 +2959,7 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 										if(retbuf!=NULL) {
 											strcpy(tmp,"\x1b[0n");
 											if(strlen(retbuf)+7 < retsize)
-												strcat(retbuf, "\x1bP1$r\x1b_");
+												strcat(retbuf, "\x1bP0$r\x1b_");
 										}
 								}
 							}
