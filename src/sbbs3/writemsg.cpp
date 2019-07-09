afc18c789bfaef0dfc83a017adbb6566c02c1f50
rswindell <>
2019-07-09 05:27:31 +0000

    Use enum values from (new file) cp437defs.h

--- src/sbbs3/writemsg.cpp
+++ src/sbbs3/writemsg.cpp
@@ -38,6 +38,7 @@
 #include "wordwrap.h"
 #include "utf8.h"
 #include "unicode.h"
+#include "cp437defs.h"
 
 #define MAX_LINES		10000
 #define MAX_LINE_LEN	(cols - 1)
@@ -112,9 +113,9 @@ bool sbbs_t::quotemsg(smb_t* smb, smbmsg_t* msg, bool tails)
 			else {
 				utf8_normalize_str(buf);
 				utf8_replace_chars(buf, unicode_to_cp437
-					,/* unsupported char: */'\xA8' /* Inverted question mark */
+					,/* unsupported char: */CP437_CHAR_INVERTED_QUESTION_MARK
 					,/* unsupported zero-width ch: */0
-					,/* decode error char: */ '\xAD' /* inverted exclamation mark */);
+					,/* decode error char: */CP437_CHAR_INVERTED_EXCLAMATION_MARK);
 			}
 		}
 		if(!useron_xedit || (useron_xedit && (cfg.xedit[useron_xedit-1]->misc&QUOTEWRAP))) {
