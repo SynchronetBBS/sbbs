5c0d7905ef9d3a4c8507fee0fd8d78c269dadc38
rswindell <>
2019-07-15 00:36:41 +0000

    Fix CVS Id keyword usage. Use require() rather than load() for sbbsdefs.js.

--- exec/emailval.js
+++ exec/emailval.js
@@ -1,5 +1,6 @@
+// $Id$
 /*******************************************************************************
-$ Id: $
+Originally based on:
 FILE: emailval.js v0.2
 BY  : Michael J. Ryan (http://tracker1.info/)
 ON  : 2005-02-14
@@ -68,7 +69,7 @@ var cPrevalText = "telvalcode";
 var cValCodeLen = 16;
 
 //include SBBS Definition constants
-load("sbbsdefs.js"); 
+require("sbbsdefs.js", 'NET_NONE'); 
 
 //gets the validation code in use, if any, otherwise sets/returns a new code.
 function GetValidationCode() {
