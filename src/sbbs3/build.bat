72fdd6d9bab870c6d2956d6dc7265746866810d3
rswindell <>
2019-07-18 03:27:50 +0000

    Migrate to MSVC++ 2019 w/PlatformToolset v141_xp: Visual Studio 2017 - Windows XP (v141_xp) toolset

--- src/sbbs3/build.bat
+++ src/sbbs3/build.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-rem *** Requires Microsoft Visual C++ 2013 ***
-call "%VS120COMNTOOLS%\vsvars32.bat"
-msbuild sbbs3.sln %1 %2 %3 %4 %5
+rem *** Requires Microsoft Visual C++ 2019 ***
+call "%VS160COMNTOOLS%\vsvars32.bat"
+msbuild sbbs3.sln /p:Platform="Win32" %1 %2 %3 %4 %5
 if errorlevel 1 echo. & echo !ERROR(s) occurred & exit /b 1
