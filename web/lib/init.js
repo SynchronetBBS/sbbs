e737463f476a5da9fc675a2037fe71011786e752
echicken <echicken@bbs.electronicchicken.com>
2019-07-19 01:30:50 -0400

    Regarding that 'if it exists' thing, check that it's an Array and that it has length.

--- web/lib/init.js
+++ web/lib/init.js
@@ -86,12 +86,12 @@ const Req = {
     },
     // First instance of query parameter p, or undefined
     get_param: function (p) {
-        if (Array.isArray(http_request.query[p])) {
+        if (Array.isArray(http_request.query[p]) && http_request.query[p].length) {
             return http_request.query[p][0];
         }
     },
     write_param: function (p) {
-        if (Array.isArray(http_request.query[p])) {
+        if (Array.isArray(http_request.query[p]) && http_request.query[p].length) {
             write(http_request.query[p][0]);
         }
     }
