82acf06349f183b0bada688ef3fc1b7ca73e9162
rswindell <>
2019-07-19 02:28:29 +0000

    New batch file name for MSVC++ 2019.

--- src/sbbs3/build.bat
+++ src/sbbs3/build.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 rem *** Requires Microsoft Visual C++ 2019 ***
-call "%VS160COMNTOOLS%\vsvars32.bat"
+call "%VS160COMNTOOLS%\VsMSBuildCmd.bat"
 msbuild sbbs3.sln /p:Platform="Win32" %1 %2 %3 %4 %5
 if errorlevel 1 echo. & echo !ERROR(s) occurred & exit /b 1
