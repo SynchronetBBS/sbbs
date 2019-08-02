db69abbac1811f1b6617d2f8b8e7c4239ced92d0
rswindell <>
2019-08-02 22:20:37 +0000

    Remove unused variable in session_check()

--- src/sbbs3/websrvr.c
+++ src/sbbs3/websrvr.c
@@ -598,7 +598,6 @@ static BOOL session_check(http_session_t *session, BOOL *rd, BOOL *wr, unsigned 
 	BOOL	ret = FALSE;
 	BOOL	lcl_rd;
 	BOOL	*rd_ptr = rd?rd:&lcl_rd;
-	char	buf;
 
 	if (session->is_tls) {
 		if(wr)
