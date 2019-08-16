7a0e924219ec989f54ea3c1701f56e27268d151a
echicken <echicken@bbs.electronicchicken.com>
2019-08-16 10:23:36 -0400

    Some tidying up of old var usage and unused things. For txt sidebar modules, avoid outputting <pre> tags unless the file gots some stuff in it.

--- web/lib/sidebar.js
+++ web/lib/sidebar.js
@@ -1,24 +1,22 @@
 function getFileContents(file) {
-	var ret = '';
-	var f = new File(file);
-	if (f.open('r')) {
-		ret = f.read();
-		f.close();
-	}
+	const f = new File(file);
+	if (!f.open('r')) return '';
+	const ret = f.read();
+	f.close();
 	return ret;
 }
 
 function getSidebarModules() {
-	return directory(settings.web_sidebar + '*').filter(
-		function (e, i, a) { return (!file_isdir(e)); }
-	);
+	return directory(settings.web_sidebar + '*').filter(function (e) {
+		return (!file_isdir(e));
+	});
 }
 
 function getSidebarModule(module) {
 
 	var ret = '';
 	if (!file_exists(module)) return ret;
-	var ext = file_getext(module).toUpperCase();
+	const ext = file_getext(module).toUpperCase();
 
 	switch (ext) {
 		case '.SSJS':
@@ -36,7 +34,8 @@ function getSidebarModule(module) {
 			ret = getFileContents(module);
 			break;
 		case '.TXT':
-			ret = '<pre>' + getFileContents(module) + '</pre>';
+			const tfc = getFileContents(module);
+			if (tfc.length) ret = '<pre>' + tfc + '</pre>';
 			break;
 		default:
 			break;
@@ -47,16 +46,14 @@ function getSidebarModule(module) {
 }
 
 function writeSidebarModules() {
-	var modules = getSidebarModules();
+	const modules = getSidebarModules();
 	write('<ul class="list-group">');
-	modules.forEach(
-		function (module) {
-			if (module.search(/\.xjs\.ssjs$/i) >= 0) return;
-			write('<li class="list-group-item sidebar">');
-			var str = getSidebarModule(module);
-			if (str !== '') write(str);
-			write('</li>');
-		}
-	);
+	modules.forEach(function (module) {
+		if (module.search(/\.xjs\.ssjs$/i) >= 0) return;
+		write('<li class="list-group-item sidebar">');
+		const str = getSidebarModule(module);
+		if (str.length) write(str);
+		write('</li>');
+	});
 	write('</ul>');
 }
