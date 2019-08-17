5f7719826fb2b28fabdb4245be7572b6ba1d1c0b
rswindell <>
2019-08-17 06:22:20 +0000

    If a message section HEADERS.DAT has the "utf8" field set to "true", set the MSG_HFIELDS_UTF8 flag in the auxattr header field to indicate UTF-8 encoding of message header fields.

--- src/sbbs3/qwktomsg.cpp
+++ src/sbbs3/qwktomsg.cpp
@@ -54,6 +54,10 @@ static bool qwk_parse_header_list(sbbs_t* sbbs, ulong confnum, smbmsg_t* msg, st
 			return false;
 		}
 	}
+	if((p=iniPopKey(headers,ROOT_SECTION,"utf8",value))!=NULL) {
+		if(stricmp(value,"true") == 0)
+			msg->hdr.auxattr |= MSG_HFIELDS_UTF8;
+	}
 
 	if((p=iniPopKey(headers,ROOT_SECTION,"WhenWritten",value))!=NULL) {
 		xpDateTime_t dt=isoDateTimeStr_parse(p);
