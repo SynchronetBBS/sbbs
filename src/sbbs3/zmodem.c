fba33a157410f7f29a5f14ca5b9431a9e70bc7bc
rswindell <>
2019-08-25 03:05:34 +0000

    Fix a 14-year old bug with ZMODEM-send. If the receiver already has the file named in the ZFILE frame, they will usually compute the local CRC value and request the sender to compute and send its CRC of the file it wants to send. If the CRCs match, the receiver will send a ZSKIP frame next ("skip this file, I already have it"). However, when I implemented the ZCRC frame support in zmodem_send_file(), I added the ZCRC frame check *after* the check to see if its a ZSKIP frame, when in fact, they will normally come in the other order (ZCRC, then ZSKIP). This would result in multiple ZCRC request/response, then ZSKIP requests that were ignored by the sender (who just send a ZFILE frame again). Simple fix: check for ZCRC frame before ZSKIP frame.

--- src/sbbs3/zmodem.c
+++ src/sbbs3/zmodem.c
@@ -191,7 +191,7 @@ static char* frame_desc(int frame)
 				break;
 		}
 	} else
-		sprintf(str,"%d",frame);
+		return chr(frame);
 	return(str);
 }
 
@@ -1826,12 +1826,6 @@ BOOL zmodem_send_file(zmodem_t* zm, char* fname, FILE* fp, BOOL request_init, ti
 		lprintf(zm,LOG_INFO,"type : %d",type);
 #endif
 
-		if(type == ZSKIP) {
-			zm->file_skipped=TRUE;
-			lprintf(zm,LOG_WARNING,"File skipped by receiver");
-			return(TRUE);
-		}
-
 		if(type == ZCRC) {
 			if(zm->crc_request==0)
 				lprintf(zm,LOG_NOTICE,"Receiver requested CRC of entire file");
@@ -1840,6 +1834,12 @@ BOOL zmodem_send_file(zmodem_t* zm, char* fname, FILE* fp, BOOL request_init, ti
 					,zm->crc_request);
 			zmodem_send_pos_header(zm,ZCRC,fcrc32(fp,zm->crc_request),TRUE);
 			type = zmodem_recv_header(zm);
+		}
+
+		if(type == ZSKIP) {
+			zm->file_skipped=TRUE;
+			lprintf(zm,LOG_WARNING,"File skipped by receiver");
+			return(TRUE);
 		}
 
 		if(type == ZRPOS)
