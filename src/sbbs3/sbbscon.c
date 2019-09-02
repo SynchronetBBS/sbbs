7f05c37f860a444519791151be0bdb53ff556adc
rswindell <>
2019-09-02 00:52:44 +0000

    Leave the node file (node.dab) open.

--- src/sbbs3/sbbscon.c
+++ src/sbbs3/sbbscon.c
@@ -1363,7 +1363,7 @@ int main(int argc, char** argv)
 {
 	int		i;
 	int		n;
-	int		file;
+	int		nodefile = -1;
 	char	ch;
 	char*	p;
 	char*	arg;
@@ -2190,7 +2190,7 @@ int main(int argc, char** argv)
 					printf("\n");
 					count=0;
 					for(i=1;i<=scfg.sys_nodes;i++) {
-						getnodedat(&scfg,i,&node, /* lockit: */FALSE, NULL /* file */);
+						getnodedat(&scfg,i,&node, /* lockit: */FALSE, &nodefile);
 						if(ch=='w' && node.status!=NODE_INUSE && node.status!=NODE_QUIET)
 							continue;
 						printnodedat(&scfg, i,&node);
@@ -2210,8 +2210,7 @@ int main(int argc, char** argv)
 						break;
 					fflush(stdin);
 					printf("\n");
-					CLOSE_OPEN_FILE(file);
-					if((i=getnodedat(&scfg,n,&node, /* lockit: */TRUE, &file))!=0) {
+					if((i=getnodedat(&scfg,n,&node, /* lockit: */TRUE, &nodefile))!=0) {
 						printf("!Error %d getting node %d data\n",i,n);
 						break;
 					}
@@ -2226,7 +2225,7 @@ int main(int argc, char** argv)
 							node.misc^=NODE_INTR;
 							break;
 					}
-					putnodedat(&scfg,n,&node,/* closeit: */TRUE, file);
+					putnodedat(&scfg,n,&node,/* closeit: */FALSE, nodefile);
 					printnodedat(&scfg,n,&node);
 #ifdef __unix__
 	                _echo_off(); /* turn off echoing - failsafe */
