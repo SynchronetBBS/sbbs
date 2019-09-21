da0f2a1afc8bada7a3400866ce6800a726635a5b
rswindell <>
2019-09-21 11:10:09 +0000

    Make center() less destructive: Use cursor-right movement rather than spaces for left-margin. Don't do the clear-to-EOL thing added in previous commit.

--- src/sbbs3/con_out.cpp
+++ src/sbbs3/con_out.cpp
@@ -749,19 +749,17 @@ void sbbs_t::inc_column(int count)
 void sbbs_t::center(char *instr, unsigned int columns)
 {
 	char str[256];
-	int i,j;
+	size_t len;
 
 	if(columns < 1)
 		columns = cols;
 
 	SAFECOPY(str,instr);
 	truncsp(str);
-	j=bstrlen(str);
-	if(j < columns)
-		for(i=0;i<(columns-j)/2;i++)
-			outchar(' ');
+	len = bstrlen(str);
+	if(len < columns)
+		cursor_right((columns - len) / 2);
 	bputs(str);
-	cleartoeol();
 	newline();
 }
 
