a82499e20c91fc43ee0466ecba6b08b0225c3a75
rswindell <>
2019-10-08 08:30:08 +0000

    Added 'install' and 'symlinks' targets.

--- src/sbbs3/gtkuseredit/targets.mk
+++ src/sbbs3/gtkuseredit/targets.mk
@@ -2,4 +2,14 @@ GTKUSEREDIT	=	$(EXEODIR)$(DIRSEP)gtkuseredit$(EXEFILE)
 
 all: xpdev-mt smblib $(MTOBJODIR) $(EXEODIR) $(GTKUSEREDIT)
 
+ifdef SBBSEXEC
+.PHONY: install
+install: all
+        install $(EXEODIR)/* $(SBBSEXEC)
+
+.PHONY: symlinks
+symlinks: all
+        ln -sfr $(EXEODIR)/* $(SBBSEXEC)
+endif
+
 $(GTKUSEREDIT):	$(XPDEV-MT_LIB) $(SMBLIB)
--- src/sbbs3/gtkuserlist/targets.mk
+++ src/sbbs3/gtkuserlist/targets.mk
@@ -2,4 +2,14 @@ GTKUSERLIST	=	$(EXEODIR)$(DIRSEP)gtkuserlist$(EXEFILE)
 
 all: xpdev-mt $(MTOBJODIR) $(EXEODIR) $(GTKUSERLIST)
 
+ifdef SBBSEXEC
+.PHONY: install
+install: all
+        install $(EXEODIR)/* $(SBBSEXEC)
+
+.PHONY: symlinks
+symlinks: all
+        ln -sfr $(EXEODIR)/* $(SBBSEXEC)
+endif
+
 $(GTKUSERLIST):	$(XPDEV-MT_LIB) $(SMBLIB)
