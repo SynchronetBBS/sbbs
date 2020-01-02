3bea8651e8677dcbf5f562037f010a75c3786038
deuce <>
2020-01-02 21:19:21 +0000

    Add pause after each move.

--- xtrn/knk/knk.js
+++ xtrn/knk/knk.js
@@ -1598,6 +1598,7 @@ function play_game()
 		turn_order[turn].drawscreen(month);
 		dk.console.println("");
 		winner=turn_order[turn].move(month, turn_order[1-turn]);
+		dk.console.pause();
 		if(winner===false) {
 			turn_order[turn].produce();
 			turn++;
