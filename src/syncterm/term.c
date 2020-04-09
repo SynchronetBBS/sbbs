02aeaebb9b46d70ddb0cae8503155fadadd82ee2
deuce <>
2020-04-09 05:48:11 +0000

    Furping da derpin

--- src/syncterm/term.c
+++ src/syncterm/term.c
@@ -2347,7 +2347,7 @@ static int fill_mevent(char *buf, size_t bufsz, struct mouse_event *me, struct m
 	}
 	if (button < 0)
 		return 0;
-	if ((button > 11)
+	if (button > 11)
 		return 0;
 	if (button > 7)
 		button += 128;
