68a36bbf48f10e0ec07116d18035b192f925be61
deuce <>
2020-04-13 23:39:10 +0000

    Ciolib has taken ALT-Up/ALT-Down... switch speed change to Ctrl.

--- src/syncterm/term.c
+++ src/syncterm/term.c
@@ -2865,7 +2865,7 @@ BOOL doterm(struct bbslist *bbs)
 					gotoxy(i,j);
 					key = 0;
 					break;
-				case 0x9800:	/* ALT-Up */
+				case 0x8d00:	/* CTRL-Up */
 					if(bbs->conn_type != CONN_TYPE_SERIAL) {
 						if(speed)
 							speed=rates[get_rate_num(speed)+1];
@@ -2874,7 +2874,7 @@ BOOL doterm(struct bbslist *bbs)
 						key = 0;
 					}
 					break;
-				case 0xa000:	/* ALT-Down */
+				case 0x9100:	/* CTRL-Down */
 					if(bbs->conn_type != CONN_TYPE_SERIAL) {
 						i=get_rate_num(speed);
 						if(i==0)
