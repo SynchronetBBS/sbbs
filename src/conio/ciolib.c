425a20f8b2f63fe2d4713ed9a96334b7fa2d5130
deuce <>
2020-04-17 21:43:41 +0000

    Use font 1 to detect the codepage, not font 0 (the default font).

--- src/conio/ciolib.c
+++ src/conio/ciolib.c
@@ -1872,7 +1872,7 @@ CIOLIBEXPORT void CIOLIBCALL ciolib_setwinposition(int x, int y)
 
 CIOLIBEXPORT enum ciolib_codepage CIOLIBCALL ciolib_getcodepage(void)
 {
-	int font = ciolib_getfont(0);
+	int font = ciolib_getfont(1);
 
 	if (font < 0)
 		return CIOLIB_CP437;
