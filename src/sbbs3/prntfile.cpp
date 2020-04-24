0b6132f9d1adf2116cd07df2105d9964c1099dba
rswindell <>
2020-04-24 05:33:38 +0000

    Fix bug reported by Nelgin in printfile(), when called with-OUT the P_OPENCLOSE mode flag, an EOF @-code would not stop the output since it calls putmsg() for *each* line of the file and was not checking the return value of putmsg() for an abnormal/early-termination indicator.

--- src/sbbs3/prntfile.cpp
+++ src/sbbs3/prntfile.cpp
@@ -138,7 +138,8 @@ bool sbbs_t::printfile(const char* fname, long mode, long org_cols)
 				break;
 			if((mode&P_UTF8) && !term_supports(UTF8))
 				utf8_normalize_str(buf);
-			putmsg(buf, mode|P_SAVEATR, org_cols);
+			if(putmsg(buf, mode|P_SAVEATR, org_cols) != '\0') // early-EOF?
+				break;
 		}
 		free(buf);
 		fclose(stream);
