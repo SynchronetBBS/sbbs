f29d55b90377a1ae7b1eed8008474cdeeb8adc2d
rswindell <>
2020-04-27 08:11:33 +0000

    We do need the CLREOL on CR console hack while display message headers apparently, so just enable for that period. Without this, returning from the viewmsg help screen produces interesting effects. Trim trailing white-space off search strings.

--- exec/msglist.js
+++ exec/msglist.js
@@ -399,12 +399,12 @@ function view_msg(msgbase, msg, lines, total_msgs, grp_name, sub_name)
 	while(!js.terminated) {
 		if(show_hdr) {
 			console.home();
-//			console.status |= CON_CR_CLREOL;
+			console.status |= CON_CR_CLREOL;
 			bbs.show_msg_header(msg
 				, property_value(msg, 'subject')
 				, property_value(msg, 'from')
 				, msg.forward_path || msg.to_list || msg.to);
-//			console.status &= ~CON_CR_CLREOL;
+			console.status &= ~CON_CR_CLREOL;
 			hdr_len = console.line_counter;
 			if(console.term_supports(USER_ANSI))
 				show_hdr = false;
@@ -1154,7 +1154,7 @@ function list_msgs(msgbase, list, current, preview, grp_name, sub_name)
 			{
 				console.clearline();
 				console.print("\x01n\x01y\x01hFind: ");
-				var search = console.getstr(60,K_LINE|K_UPPER|K_NOCRLF);
+				var search = console.getstr(60,K_LINE|K_UPPER|K_NOCRLF|K_TRIM);
 				console.clearline(LIGHTGRAY);
 				if(search && search.length) {
 					console.print("Searching \x01i...\x01n");
