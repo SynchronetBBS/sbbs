50c3e1cf055c01a1b9447aeba503ed6ef3d5dbd6
mcmlxxix <>
2020-06-04 16:37:48 +0000

    remove backgroundlog reference. add try/catch blocks in service methods to prevent exceptions from stopping service

--- xtrn/maze/service.js
+++ xtrn/maze/service.js
@@ -1,7 +1,6 @@
 var root = argv[0];
 js.branch_limit = 0;
 
-load("backgroundlog.js");
 load("json-client.js");
 load("event-timer.js");
 load("funclib.js");
@@ -82,150 +81,218 @@ function processUpdate(update) {
 
 /* handle a player disconnection */
 function handleDisco(update) {
-	for each(var game in data.games) {
-		if(!game.players) {
-			deleteGame(game.gameNumber);
-			continue;
-		}
-		var pcount = countMembers(game.players);
-		if(pcount == 0) 
-			deleteGame(game.gameNumber);
-		else if(game.players[update.data.nick]) {
-			if(pcount == 1)
+	try {
+		for each(var game in data.games) {
+			if(!game.players) {
 				deleteGame(game.gameNumber);
-			else 
-				deletePlayer(game.gameNumber,update.data.nick);
+				continue;
+			}
+			var pcount = countMembers(game.players);
+			if(pcount == 0) {
+				deleteGame(game.gameNumber);
+			}
+			else if(game.players[update.data.nick]) {
+				if(pcount == 1)
+					deleteGame(game.gameNumber);
+				else 
+					deletePlayer(game.gameNumber,update.data.nick);
+			}
 		}
+	}
+	catch(e) {
+		log(LOG_ERROR,e);
 	}
 }
 
 /* delete a game */
 function deleteGame(gameNumber) {
-	log(LOG_DEBUG,"removing empty game #" + gameNumber);
-	client.remove(game_id,"games." + gameNumber,2);
-	if(data.timers[gameNumber])
-		data.timers[gameNumber].abort = true;
-	delete data.games[gameNumber];
+	try {
+		log(LOG_DEBUG,"removing empty game #" + gameNumber);
+		client.remove(game_id,"games." + gameNumber,2);
+		if(data.timers[gameNumber]) {
+			data.timers[gameNumber].abort = true;
+		}
+		delete data.games[gameNumber];
+	}
+	catch(e) {
+		log(LOG_ERROR,e);
+	}
 }
 
 /* delete a player */
 function deletePlayer(gameNumber,playerName) {
-	log(LOG_DEBUG,"removing player from game " + gameNumber);
-	client.remove(game_id,"games." + gameNumber + ".players." + playerName,2)
-	delete data.games[gameNumber].players[playerName];
+	try {
+		log(LOG_DEBUG,"removing player from game " + gameNumber);
+		client.remove(game_id,"games." + gameNumber + ".players." + playerName,2)
+		delete data.games[gameNumber].players[playerName];
+	}
+	catch(e) {
+		log(LOG_ERROR,e);
+	}
 }
 
 /* ensure there arent too few or too many players in a game */
 function validateGamePlayers(update,gameNumber,playerName) {
-	var game = data.games[gameNumber];
-	if(!game.players) {
-		deleteGame(gameNumber);
-		return;
-	}
-	var pcount = countMembers(game.players);
-	if(pcount == 0) 
-		deleteGame(gameNumber);
-	else if(pcount > settings.max_players)
-		deletePlayer(gameNumber,playerName);
-	else if(pcount >= settings.min_players) {
-		var ready = getReady(game);
-		if(ready && game.status == status.WAITING)
-			startTimer(game);
-		else if(!ready && game.status == status.RACING && pcount == 1)
+	try {
+		var game = data.games[gameNumber];
+		if(!game.players) {
+			deleteGame(gameNumber);
+			return;
+		}
+		var pcount = countMembers(game.players);
+		if(pcount == 0) {
+			deleteGame(gameNumber);
+		}
+		else if(pcount > settings.max_players) {
 			deletePlayer(gameNumber,playerName);
+		}
+		else if(pcount >= settings.min_players) {
+			var ready = getReady(game);
+			if(ready && game.status == status.WAITING)
+				startTimer(game);
+			else if(!ready && game.status == status.RACING && pcount == 1)
+				deletePlayer(gameNumber,playerName);
+		}
+	}
+	catch(e) {
+		log(LOG_ERROR,e);
 	}
 }
 
 /* check the status of all games (after an update) */
 function updateGames() {
-	for each(var game in data.games) 
-		if(game.status == status.STARTING && !getReady(game)) 
-			stopTimer(game);
+	try {
+		for each(var game in data.games) {
+			if(game.status == status.STARTING && !getReady(game)) {
+				stopTimer(game);
+			}
+		}
+	}
+	catch(e) {
+		log(LOG_ERROR,e);
+	}
 }
 
 /* stop the countdown */
 function stopTimer(game) {
-	if(data.timers[game.gameNumber])
-		data.timers[game.gameNumber].abort = true;
-	game.status = status.WAITING;
-	client.write("mazerace","games." + game.gameNumber + ".status",status.WAITING,2);
+	try {
+		if(data.timers[game.gameNumber]) {
+			data.timers[game.gameNumber].abort = true;
+		}
+		game.status = status.WAITING;
+		client.write("mazerace","games." + game.gameNumber + ".status",status.WAITING,2);
+	}
+	catch(e) {
+		log(LOG_ERROR,e);
+	}
 }
 
 /* start the countdown */
 function startTimer(game) {
-	data.timers[game.gameNumber] = timer.addEvent(settings.start_delay,false,startGame,[game]);
-	game.status = status.STARTING;
-	client.write("mazerace","games." + game.gameNumber + ".status",status.STARTING,2);
+	try {
+		data.timers[game.gameNumber] = timer.addEvent(settings.start_delay,false,startGame,[game]);
+		game.status = status.STARTING;
+		client.write("mazerace","games." + game.gameNumber + ".status",status.STARTING,2);
+	}
+	catch(e) {
+		log(LOG_ERROR,e);
+	}
 }
 
 /* check a game for player readiness */
 function getReady(game) {
-	for each(var p in game.players) {
-		if(p.ready == false)
-			return false;
+	try {
+		for each(var p in game.players) {
+			if(p.ready == false) {
+				return false;
+			}
+		}
+		return true;
 	}
-	return true;
+	catch(e) {
+		log(LOG_ERROR,e);
+	}
 }
 
 /* generate a maze and start the race */
 function startGame(game) {
-	/* verify that the game is still ready */
-	if(game.status == status.STARTING) {
-		log(LOG_DEBUG,"starting race: " + game.gameNumber);
-		client.lock("mazerace","games." + game.gameNumber + ".status",2);
-		game.status = status.RACING;
-		createMaze(game);
-		client.write("mazerace","games." + game.gameNumber + ".status",status.RACING);
-		client.unlock("mazerace","games." + game.gameNumber + ".status");
+	try {
+		/* verify that the game is still ready */
+		if(game.status == status.STARTING) {
+			log(LOG_DEBUG,"starting race: " + game.gameNumber);
+			client.lock("mazerace","games." + game.gameNumber + ".status",2);
+			game.status = status.RACING;
+			createMaze(game);
+			client.write("mazerace","games." + game.gameNumber + ".status",status.RACING);
+			client.unlock("mazerace","games." + game.gameNumber + ".status");
+		}
+	}
+	catch(e) {
+		log(LOG_ERROR,e);
 	}
 }
 
 /* generate a maze */
 function createMaze(game) {
-	client.lock("mazerace","mazes." + game.gameNumber,2);
-	
-	var maze = generateMaze(settings.columns,settings.rows);
-	var start = getRandomCorner(maze,0,E);
-	var finish = getRandomCorner(maze,settings.columns-1,W);
-	var race = new Race(game.gameNumber,game.players,game.damage,game.fog,maze,start,finish);
-	
-	data.mazes[game.gameNumber] = race;
-	client.write("mazerace","mazes." + game.gameNumber,race);
-	client.unlock("mazerace","mazes." + game.gameNumber);
+	try {
+		client.lock("mazerace","mazes." + game.gameNumber,2);
+		
+		var maze = generateMaze(settings.columns,settings.rows);
+		var start = getRandomCorner(maze,0,E);
+		var finish = getRandomCorner(maze,settings.columns-1,W);
+		var race = new Race(game.gameNumber,game.players,game.damage,game.fog,maze,start,finish);
+		
+		data.mazes[game.gameNumber] = race;
+		client.write("mazerace","mazes." + game.gameNumber,race);
+		client.unlock("mazerace","mazes." + game.gameNumber);
+	}
+	catch(e) {
+		log(LOG_ERROR,e);
+	}
 }
 
 /* locate start and finish points */
 function getRandomCorner(maze,col,side)	{
-	var count = 0;
-	var row = random(settings.rows);
-	while(count < settings.rows) {
-		if((maze[row][col]&side) && (maze[row][col]&S || maze[row][col]&N)) 
-			break;
-		count++;
-		row++;
-		if(row == settings.rows)
-			row = 0;
+	try {
+		var count = 0;
+		var row = random(settings.rows);
+		while(count < settings.rows) {
+			if((maze[row][col]&side) && (maze[row][col]&S || maze[row][col]&N)) 
+				break;
+			count++;
+			row++;
+			if(row == settings.rows)
+				row = 0;
+		}
+		return {
+			x:col,
+			y:row
+		};
 	}
-	return {
-		x:col,
-		y:row
-	};
+	catch(e) {
+		log(LOG_ERROR,e);
+	}
 }
 
 /* initialize service */
 function init() {
-	js.branch_limit=0;
-	client.subscribe(game_id,"games");
-	client.subscribe(game_id,"players");
-	client.write(game_id,"settings",settings,2);
-	client.callback = processUpdate;
-	if(!data.games)
-		data.games = {};
-	if(!data.mazes)
-		data.mazes = {};
-	if(!data.players)
-		data.players = {};
-	log(LOG_INFO,"Maze Race background service initialized");
+	try {
+		js.branch_limit=0;
+		client.subscribe(game_id,"games");
+		client.subscribe(game_id,"players");
+		client.write(game_id,"settings",settings,2);
+		client.callback = processUpdate;
+		if(!data.games)
+			data.games = {};
+		if(!data.mazes)
+			data.mazes = {};
+		if(!data.players)
+			data.players = {};
+		log(LOG_INFO,"Maze Race background service initialized");
+	}
+	catch(e) {
+		log(LOG_ERROR,e);
+	}
 }
 
 /* main loop */
