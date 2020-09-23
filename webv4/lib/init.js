8a2ff406f4d577b66f596b122d8eb723833e76b4
echicken <echicken@bbs.electronicchicken.com>
2020-09-23 16:16:53 -0400

    Add a web_mods_sidebar property to settings

--- webv4/lib/init.js
+++ webv4/lib/init.js
@@ -20,6 +20,7 @@ settings.web_lib = backslash(settings.web_directory + 'lib/');
 settings.web_components = backslash(settings.web_directory + 'components/');
 settings.web_pages = backslash(fullpath(settings.web_root + '../pages'));
 settings.web_sidebar = backslash(fullpath(settings.web_root + '../sidebar'));
+settings.web_mods_sidebar = backslash(fullpath(settings.web_mods + 'sidebar/'));
 settings.web_mods = backslash(fullpath(settings.web_directory + 'mods/'));
 settings.web_mods_pages = backslash(fullpath(settings.web_mods + 'pages/'));
 
