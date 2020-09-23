SynchronetBBS/sbbs

commit 3eb83ec5e7b10343aa5b3b04e93cf83b81343b2a

Don't allow invalid netmail-forwarding addresses
Don't prompt a new user to forward email to their netmail address if they
provided an invalid netmail address (not supported by the system).

If a sysop has an invalid netmail address setup for forwarding, don't try to
forward email (or new user feedback) to that address.

Re-ordered the new user terminal questions a little bit (ask for the backspace
key first, to get earlier manual PETSCII detection). I'm not sure why I was
enabling AUTOTERM along with PETSCII before. Removed that as PETSCII cannot
be auto-detected.

src/sbbs3/useredit.cpp
@@ -1082,11 +1082,9 @@ void sbbs_t::maindflts(user_t* user)
 				if(sys_status&SS_ABORT)
 					break;
 				putuserrec(&cfg,user->number,U_NETMAIL,LEN_NETMAIL,user->netmail); 
-				if(user->netmail[0] == 0 || noyes(text[ForwardMailQ]))
-					user->misc&=~NETMAIL;
-				else {
-					user->misc|=NETMAIL;
-				}
+				user->misc &= ~NETMAIL;
+				if(is_supported_netmail_addr(&cfg, user->netmail) && !noyes(text[ForwardMailQ]))
+					user->misc |= NETMAIL;
 				putuserrec(&cfg,user->number,U_MISC,8,ultoa(user->misc,str,16));
 				break;
 			case 'C':

src/sbbs3/email.cpp
@@ -91,11 +91,15 @@ bool sbbs_t::email(int usernumber, const char *top, const char *subj, long mode,
 		bputs(text[UnknownUser]);
 		return(false); 
 	}
-	if((l&NETMAIL) && (cfg.sys_misc&SM_FWDTONET) && !(mode & WM_NOFWD)) {
+	if((l&NETMAIL) && (cfg.sys_misc&SM_FWDTONET) && !(mode & WM_NOFWD) && !(useron.rest&FLAG('M'))) {
 		getuserrec(&cfg,usernumber,U_NETMAIL,LEN_NETMAIL,str);
-		bprintf(text[UserNetMail],str);
-		if((mode & WM_FORCEFWD) || text[ForwardMailQ][0]==0 || yesno(text[ForwardMailQ])) /* Forward to netmail address */
-			return(netmail(str, subj, mode, resmb, remsg));
+		if(is_supported_netmail_addr(&cfg, str)) {
+			bprintf(text[UserNetMail],str);
+			if((mode & WM_FORCEFWD) || yesno(text[ForwardMailQ])) /* Forward to netmail address */
+				return(netmail(str, subj, mode, resmb, remsg));
+		} else {
+			bprintf(text[InvalidNetMailAddr], str);
+		}
 	}
 	if(sys_status&SS_ABORT) {
 		bputs(text[Aborted]);

src/sbbs3/netmail.cpp
@@ -1428,3 +1428,23 @@ bool sbbs_t::qnetmail(const char *into, const char *subj, long mode, smb_t* resm
 	logline("EN",str);
 	return(true);
 }
+
+extern "C" BOOL is_supported_netmail_addr(scfg_t* cfg, const char* addr)
+{
+	switch (smb_netaddr_type(addr)) {
+		case NET_FIDO:
+			return INT_TO_BOOL(cfg->total_faddrs && (cfg->netmail_misc&NMAIL_ALLOW));
+		case NET_INTERNET:
+			return INT_TO_BOOL(cfg->inetmail_misc&NMAIL_ALLOW);
+		case NET_QWK:
+		{
+			char fulladdr[256] = "";
+			const char* p = strchr(addr, '@');
+			if(p == NULL)
+				return FALSE;
+			qwk_route(cfg, p + 1, fulladdr, sizeof(fulladdr)-1);
+			return fulladdr[0] != 0;
+		}
+	}
+	return FALSE;
+}

src/sbbs3/newuser.cpp
@@ -152,7 +152,26 @@ BOOL sbbs_t::newuser()
 		} else
 			useron.misc&=~AUTOTERM;
 
-		if(!(useron.misc&AUTOTERM)) {
+		while(text[HitYourBackspaceKey][0] && !(useron.misc&(PETSCII|SWAP_DELETE)) && online) {
+			bputs(text[HitYourBackspaceKey]);
+			uchar key = getkey(K_NONE);
+			bprintf(text[CharacterReceivedFmt], key, key);
+			if(key == '\b')
+				break;
+			if(key == DEL) {
+				if(text[SwapDeleteKeyQ][0] == 0 || yesno(text[SwapDeleteKeyQ]))
+					useron.misc |= SWAP_DELETE;
+			}
+			else if(key == PETSCII_DELETE)
+				useron.misc |= (PETSCII|COLOR);
+			else {
+				bprintf(text[InvalidBackspaceKeyFmt], key, key);
+				if(text[ContinueQ][0] && !yesno(text[ContinueQ]))
+					return FALSE;
+			}
+		}
+
+		if(!(useron.misc&(AUTOTERM|PETSCII))) {
 			if(text[AnsiTerminalQ][0] && yesno(text[AnsiTerminalQ]))
 				useron.misc|=ANSI; 
 			else
@@ -172,25 +191,6 @@ BOOL sbbs_t::newuser()
 		}
 		else
 			useron.rows = TERM_ROWS_DEFAULT;
-
-		while(text[HitYourBackspaceKey][0] && !(useron.misc&(PETSCII|SWAP_DELETE)) && online) {
-			bputs(text[HitYourBackspaceKey]);
-			uchar key = getkey(K_NONE);
-			bprintf(text[CharacterReceivedFmt], key, key);
-			if(key == '\b')
-				break;
-			if(key == DEL) {
-				if(text[SwapDeleteKeyQ][0] == 0 || yesno(text[SwapDeleteKeyQ]))
-					useron.misc |= SWAP_DELETE;
-			}
-			else if(key == PETSCII_DELETE)
-				useron.misc |= (AUTOTERM|PETSCII|COLOR);
-			else {
-				bprintf(text[InvalidBackspaceKeyFmt], key, key);
-				if(text[ContinueQ][0] && !yesno(text[ContinueQ]))
-					return FALSE;
-			}
-		}
 
 		if(useron.misc&PETSCII) {
 			autoterm |= PETSCII;
@@ -328,10 +328,10 @@ BOOL sbbs_t::newuser()
 				&& !trashcan(useron.netmail,"email"))
 				break;
 		}
-		if(useron.netmail[0] && cfg.sys_misc&SM_FWDTONET && text[ForwardMailQ][0] && yesno(text[ForwardMailQ]))
+		useron.misc&=~NETMAIL;
+		if((cfg.sys_misc&SM_FWDTONET) && is_supported_netmail_addr(&cfg, useron.netmail) && yesno(text[ForwardMailQ]))
 			useron.misc|=NETMAIL;
-		else 
-			useron.misc&=~NETMAIL;
+
 		if(text[UserInfoCorrectQ][0]==0 || yesno(text[UserInfoCorrectQ]))
 			break; 
 	}

src/sbbs3/sbbs.h
@@ -1294,6 +1294,9 @@ extern "C" {
 	/* qwk.cpp */
 	DLLEXPORT int		qwk_route(scfg_t*, const char *inaddr, char *fulladdr, size_t maxlen);
 
+	/* netmail.cpp */
+	DLLEXPORT BOOL		is_supported_netmail_addr(scfg_t*, const char* addr);
+
 	/* con_out.cpp */
 	unsigned char		cp437_to_petscii(unsigned char);
 

commit 9d0d09f99590f8faeb2cc12660a0fea3d4bf86fb

Allow forward-to-netmail option to be toggled off by user
Bug introduced in commit 3eb83ec5e7b (Sept-11):
By calling putuserrec() before modifying user->misc and then calling
noyes(), we're giving an opportunity for the low-level node sync code to
read the modified "useron" back from the database, thus losing the change
we just made to user->misc. Instead, move the putuserrec() call to the end
of the case statement. Another option would have been to turn off the NETMAIL
flag before the first call to putuserrec().

Bug reported by Nugax (BYTEXCHG)

src/sbbs3/useredit.cpp
@@ -1081,11 +1081,11 @@ void sbbs_t::maindflts(user_t* user)
 				getstr(user->netmail,LEN_NETMAIL,K_LINE|K_EDIT|K_AUTODEL|K_TRIM);
 				if(sys_status&SS_ABORT)
 					break;
-				putuserrec(&cfg,user->number,U_NETMAIL,LEN_NETMAIL,user->netmail); 
 				user->misc &= ~NETMAIL;
 				if(is_supported_netmail_addr(&cfg, user->netmail) && !noyes(text[ForwardMailQ]))
 					user->misc |= NETMAIL;
 				putuserrec(&cfg,user->number,U_MISC,8,ultoa(user->misc,str,16));
+				putuserrec(&cfg,user->number,U_NETMAIL,LEN_NETMAIL,user->netmail); 
 				break;
 			case 'C':
 				user->misc^=CLRSCRN;