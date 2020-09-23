76210c7a057519b27e55521604d769745386b742
echicken <echicken@bbs.electronicchicken.com>
2020-09-23 16:29:52 -0400

    Reordered declarations for clarity.

--- webv4/lib/init.js
+++ webv4/lib/init.js
@@ -20,9 +20,9 @@ settings.web_lib = backslash(settings.web_directory + 'lib/');
 settings.web_components = backslash(settings.web_directory + 'components/');
 settings.web_pages = backslash(fullpath(settings.web_root + '../pages'));
 settings.web_sidebar = backslash(fullpath(settings.web_root + '../sidebar'));
-settings.web_mods_sidebar = backslash(fullpath(settings.web_mods + 'sidebar/'));
 settings.web_mods = backslash(fullpath(settings.web_directory + 'mods/'));
 settings.web_mods_pages = backslash(fullpath(settings.web_mods + 'pages/'));
+settings.web_mods_sidebar = backslash(fullpath(settings.web_mods + 'sidebar/'));
 
 var defaults = {
   guest : {
