SynchronetBBS/sbbs

commit 97bade44ebad5b431485e93c82dcc8d63b642624

Notify local recipients of locally-posted messages addressed to them.
While SBBSecho would notify local users of imported messages addressed to
them (both EchoMail and NetMail) and QWK-imported messages recently gained
the ability to notify local recipients of messages addressed to them, other
methods of posting messages (e.g. just using the normal terminal server
method, e.g. the 'P' command) or using any JavaScript (e.g. web) methods of
posting messages lacked recipient notifications. So that's now fixed.
And for JavaScript-saved local email messages too. I don't *think* this will
result in any duplicate notifications of received messages, but let me know
if you see excess/duplicate notifications.
Anonymously posted messages are excluded.
Messages posted to yourself are excluded (who does that?).

src/sbbs3/postmsg.cpp
@@ -336,6 +336,20 @@ bool sbbs_t::postmsg(uint subnum, long wm_mode, smb_t* resmb, smbmsg_t* remsg)
 		,cfg.grp[cfg.sub[subnum]->grp]->sname,cfg.sub[subnum]->lname);
 	logline("P+",str);
 
+	if(!(msgattr & MSG_ANONYMOUS)) {
+		if(cfg.sub[subnum]->misc&SUB_NAME)
+			i = userdatdupe(0, U_NAME, LEN_NAME, touser);
+		else
+			i = matchuser(&cfg, touser, TRUE /* sysop_alias */);
+		if(i > 0 && i != useron.number) {
+			SAFEPRINTF4(str, text[MsgPostedToYouVia]
+				,cfg.sub[subnum]->misc&SUB_NAME ? useron.name : useron.alias
+				,connection
+				,cfg.grp[cfg.sub[subnum]->grp]->sname,cfg.sub[subnum]->lname);
+			putsmsg(&cfg, i, str);
+		}
+	}
+
 	signal_sub_sem(&cfg,subnum);
 
 	user_event(EVENT_POST);
@@ -477,6 +491,32 @@ extern "C" int DLLCALL savemsg(scfg_t* cfg, smb_t* smb, smbmsg_t* msg, client_t*
 				ftouch(cmdstr(cfg,NULL,cfg->netmail_sem,nulstr,nulstr,NULL));
 		} else
 			signal_sub_sem(cfg,smb->subnum);
+
+		if(msg->to_net.type == NET_NONE && !(msg->hdr.attr & MSG_ANONYMOUS) && cfg->text != NULL) {
+			if(msg->to_ext != NULL)
+				i = atoi(msg->to_ext);
+			else if(smb->subnum != INVALID_SUB && (cfg->sub[smb->subnum]->misc & SUB_NAME))
+				i = userdatdupe(cfg, 0, U_NAME, LEN_NAME, msg->to, /* del: */FALSE, /* next: */FALSE, NULL, NULL);
+			else
+				i = matchuser(cfg, msg->to, TRUE /* sysop_alias */);
+			if(i > 0 && (client == NULL || i != client->usernum)) {
+				char str[256];
+				if(smb->subnum == INVALID_SUB) {
+					safe_snprintf(str, sizeof(str), cfg->text[UserSentYouMail], msg->from);
+					putsmsg(cfg, i, str);
+				} else {
+					char fido_buf[64];
+					const char* via = smb_netaddrstr(&msg->from_net, fido_buf);
+					if(via == NULL)
+						via = (client == NULL) ? "" : client->protocol;
+					safe_snprintf(str, sizeof(str), cfg->text[MsgPostedToYouVia]
+						,msg->from
+						,via
+						,cfg->grp[cfg->sub[smb->subnum]->grp]->sname,cfg->sub[smb->subnum]->lname);
+					putsmsg(cfg, i, str);
+				}
+			}
+		}
 	}
 	return(i);
 }

commit c8193766c6b75abb18a2c257acc03a1fa89585f8

Fix savemsg() bug introduced in 97bade44eb (Sep-30), returning false failure
'i' is the return value of savemg(), used to determine success or failure
(and what kind of failure) - so we can't use that for the usernumber of
the recipient.

src/sbbs3/postmsg.cpp
@@ -493,17 +493,18 @@ extern "C" int DLLCALL savemsg(scfg_t* cfg, smb_t* smb, smbmsg_t* msg, client_t*
 			signal_sub_sem(cfg,smb->subnum);
 
 		if(msg->to_net.type == NET_NONE && !(msg->hdr.attr & MSG_ANONYMOUS) && cfg->text != NULL) {
+			int usernum = 0;
 			if(msg->to_ext != NULL)
-				i = atoi(msg->to_ext);
+				usernum = atoi(msg->to_ext);
 			else if(smb->subnum != INVALID_SUB && (cfg->sub[smb->subnum]->misc & SUB_NAME))
-				i = userdatdupe(cfg, 0, U_NAME, LEN_NAME, msg->to, /* del: */FALSE, /* next: */FALSE, NULL, NULL);
+				usernum = userdatdupe(cfg, 0, U_NAME, LEN_NAME, msg->to, /* del: */FALSE, /* next: */FALSE, NULL, NULL);
 			else
-				i = matchuser(cfg, msg->to, TRUE /* sysop_alias */);
-			if(i > 0 && (client == NULL || i != (int)client->usernum)) {
+				usernum = matchuser(cfg, msg->to, TRUE /* sysop_alias */);
+			if(usernum > 0 && (client == NULL || usernum != (int)client->usernum)) {
 				char str[256];
 				if(smb->subnum == INVALID_SUB) {
 					safe_snprintf(str, sizeof(str), cfg->text[UserSentYouMail], msg->from);
-					putsmsg(cfg, i, str);
+					putsmsg(cfg, usernum, str);
 				} else {
 					char fido_buf[64];
 					const char* via = smb_netaddrstr(&msg->from_net, fido_buf);
@@ -513,7 +514,7 @@ extern "C" int DLLCALL savemsg(scfg_t* cfg, smb_t* smb, smbmsg_t* msg, client_t*
 						,msg->from
 						,via
 						,cfg->grp[cfg->sub[smb->subnum]->grp]->sname,cfg->sub[smb->subnum]->lname);
-					putsmsg(cfg, i, str);
+					putsmsg(cfg, usernum, str);
 				}
 			}
 		}