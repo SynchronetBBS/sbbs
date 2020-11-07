62c21cfe85b12403bbd101eb12c3c629489c3dc2
Michael Long <mlong@bizjournals.com>
2020-11-07 00:10:17 -0500

    tweak pre/post execution code, fix cnf

--- src/sbbs2/xtrn_ovl.c
+++ src/sbbs2/xtrn_ovl.c
@@ -1275,7 +1275,10 @@ void exec_xtrn(uint xtrnnum)
     node_t node;
 	time_t start,end;
 
-if(cfg.xtrnprogpre_mod[0] != '\xff') {
+if(cfg.xtrnprogpre_mod[0] == '\xff') {
+	SAFEPRINTF2(str, "prextrn %s", xtrn[xtrnnum]->code); 
+}
+if((cfg.xtrnprogpre_mod[0] != '\xff') && (cfg.xtrnprogpre_mod[0] != '\0')) {
 	SAFEPRINTF2(str, "%s %s", cfg.xtrnprogpre_mod,xtrn[xtrnnum]->code);
 	exec_bin(str, &main_csi);
 }
@@ -1438,10 +1441,13 @@ if(xtrn[xtrnnum]->misc&MODUSERDAT) {	/* Modify user data */
 	moduserdat(xtrnnum);
 	statusline(); }
 
-if(cfg.xtrnprogpost_mod[0] != '\0') {
-    SAFEPRINTF2(str, "%s %s", cfg.xtrnprogpost_mod,xtrn[xtrnnum]->code);
-    exec_bin(str, &main_csi);
- }
+if(cfg.xtrnprogpost_mod[0] == '\xff') {
+	SAFEPRINTF2(str, "postxtrn %s", xtrn[xtrnnum]->code); 
+}
+if((cfg.xtrnprogpost_mod[0] != '\xff') && (cfg.xtrnprogpost_mod[0] != '\0')) {
+	SAFEPRINTF2(str, "%s %s", cfg.xtrnprogpost_mod,xtrn[xtrnnum]->code);
+	exec_bin(str, &main_csi);
+}
 
 getnodedat(node_num,&thisnode,1);
 thisnode.aux=0; /* aux is 0, only if at menu */
