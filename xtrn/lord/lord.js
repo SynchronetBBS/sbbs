94078c0ec72156b1363c91a4fb9390e7ac7ca101
Rob Swindell <rob@synchro.net>
2020-11-19 20:56:16 -0800

    Go back to the rev 1.37 behavior (don't confirm RESET when run via JSexec)
    ... but do it do it correctly. This allows for automated game-reset, e.g.
    via cron job or timed event.

--- xtrn/lord/lord.js
+++ xtrn/lord/lord.js
@@ -16765,7 +16765,7 @@ function cmdline()
 			settings.menu_dir = argv[i];
 		}
 		else if (argv[i].toUpperCase() === 'RESET') {
-			if(!deny("Reset LORD")) {
+			if(!js.global.bbs || !deny("Reset LORD")) {
 				file_remove(gamedir('state.bin'));
 				file_remove(gamedir('player.bin'));
 			}
