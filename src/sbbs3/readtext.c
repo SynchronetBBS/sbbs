15938054e540a0a29c7b6171006636260a017536
Rob Swindell <rob@synchro.net>
2020-11-24 12:05:32 -0800

    readtext.c no longer dependent on sbbs.h

--- src/sbbs3/readtext.c
+++ src/sbbs3/readtext.c
@@ -1,6 +1,29 @@
-#include "sbbs.h"
+/* Read a string (item) from ctrl/text.dat */
+
+/****************************************************************************
+ * @format.tab-size 4		(Plain Text/Source Code File Header)			*
+ * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
+ *																			*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ *																			*
+ * This program is free software; you can redistribute it and/or			*
+ * modify it under the terms of the GNU General Public License				*
+ * as published by the Free Software Foundation; either version 2			*
+ * of the License, or (at your option) any later version.					*
+ * See the GNU General Public License for more details: gpl.txt or			*
+ * http://www.fsf.org/copyleft/gpl.html										*
+ *																			*
+ * For Synchronet coding style and modification guidelines, see				*
+ * http://www.synchro.net/source.html										*
+ *																			*
+ * Note: If this box doesn't appear square, then you need to fix your tabs.	*
+ ****************************************************************************/
+
+#include "sbbsdefs.h"
+#include "str_util.h"
 #include "text_defaults.h"	/* text_defaults */
 
+int lprintf(int level, const char *fmt, ...);       /* log output */
 /****************************************************************************/
 /* Reads special TEXT.DAT printf style text lines, splicing multiple lines, */
 /* replacing escaped characters, and allocating the memory					*/
