858ab1587bcceba18d8dc63d229b149030e87b07
Rob Swindell <rob@synchro.net>
2020-11-25 14:07:32 -0800

    Add SYSAVAILYN @-code, same as SYSAVAIL but expands to Yes or No instead. - for Nelgin

--- src/sbbs3/atcodes.cpp
+++ src/sbbs3/atcodes.cpp
@@ -459,6 +459,9 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen, long* pmode, bool
 	if(strcmp(sp, "SYSAVAIL") == 0)
 		return text[sysop_available(&cfg) ? LiSysopAvailable : LiSysopNotAvailable];
 
+	if(strcmp(sp, "SYSAVAILYN") == 0)
+		return text[sysop_available(&cfg) ? Yes : No];
+
 	if(!strcmp(sp,"LOCATION"))
 		return(cfg.sys_location);
 
