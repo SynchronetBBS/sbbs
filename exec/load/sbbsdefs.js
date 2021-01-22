71388a19c2b56bf4c763907a5509b2229e7c76f2
Rob Swindell <rob@synchro.net>
2021-01-21 18:01:35 -0800

    Add K_NUL definition.

--- exec/load/sbbsdefs.js
+++ exec/load/sbbsdefs.js
@@ -204,6 +204,7 @@ var   K_NOSPIN      =(1<<21);	/* Do not honor user's spinning cursor		*/
 var   K_ANSI_CPR	=(1<<22);	/* ANSI Cursor Position Report expected		*/
 var   K_TRIM        =(1<<23);   /* Trim white-space from both ends of str   */
 var   K_CTRLKEYS	=(1<<24);	/* No control-key handling/eating in inkey()*/
+var   K_NUL         =(1<<25);   /* Return null instead of "" upon timeout   */
 					    		/********************************************/
 
 						    	/********************************************/
