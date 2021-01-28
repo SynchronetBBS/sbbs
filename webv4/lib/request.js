b125b22fdf45b6071c743f1755ee52923feb763f
echicken <echicken@bbs.electronicchicken.com>
2021-01-28 13:46:25 -0500

    has_params

--- webv4/lib/request.js
+++ webv4/lib/request.js
@@ -1,18 +1,24 @@
 // Helpers for http_request
 var Request = {
-    // Query parameter p exists and first instance is of optional type t
-    has_param: function (p) {
-      return (Array.isArray(http_request.query[p]) && http_request.query[p].length);
-    },
-    // First instance of query parameter p, or undefined
-    get_param: function (p) {
-      if (Array.isArray(http_request.query[p]) && http_request.query[p].length) {
-          return http_request.query[p][0];
-      }
-    },
-    write_param: function (p) {
-      if (Array.isArray(http_request.query[p]) && http_request.query[p].length) {
-          write(http_request.query[p][0]);
-      }
-    }
+	// Query parameter p exists
+	has_param: function (p) {
+		return (Array.isArray(http_request.query[p]) && http_request.query[p].length);
+	},
+    // All query parameters in Array[String] p exist
+	has_params: function (p) {
+		return p.every(function (e) {
+			return (Array.isArray(http_request.query[e]) && http_request.query[e].length);
+		});
+	},
+	// First instance of query parameter p, or undefined
+	get_param: function (p) {
+		if (Array.isArray(http_request.query[p]) && http_request.query[p].length) {
+			return http_request.query[p][0];
+		}
+	},
+	write_param: function (p) {
+		if (Array.isArray(http_request.query[p]) && http_request.query[p].length) {
+			write(http_request.query[p][0]);
+		}
+	}
 };
