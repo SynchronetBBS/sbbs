3fe9759b54cda8495ebf811cd28f6dac95d3c018
Deucе <shurd@sasktel.net>
2021-02-21 01:44:54 -0500

    FSF has moved. :(

--- src/doors/top/output.c
+++ src/doors/top/output.c
@@ -14,7 +14,7 @@ OUTPUT.C     Screen output function.
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 This module contains TOP's custom screen output function, which is capable
 of processing PubColour codes and language tokens.  It is also capable of
--- src/doors/top/screens.c
+++ src/doors/top/screens.c
@@ -14,7 +14,7 @@ SCREENS.C    Large displays and screen showing functions.
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 This module contains functions to display external screens.  It also contains
 many of TOP's large displays, such as the opening and closing credits.
