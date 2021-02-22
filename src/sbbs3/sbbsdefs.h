1d317681e4e8dd13ee1bae4d065f02a1f7a53f7c
Rob Swindell <rob@synchro.net>
2021-02-21 19:08:49 -0800

    Define ERR_MKDIR - needed by previous commit

--- src/sbbs3/sbbsdefs.h
+++ src/sbbs3/sbbsdefs.h
@@ -297,6 +297,7 @@ typedef struct js_callback {
 #define ERR_CHK		"checking"		/* checking */
 #define ERR_LEN		"checking length"
 #define ERR_EXEC	"executing"		/* executing */
+#define ERR_MKDIR	"making directory"
 #define ERR_CHDIR	"changing directory"
 #define ERR_CREATE	"creating" 		/* creating */
 #define ERR_LOCK	"locking"		/* locking */
