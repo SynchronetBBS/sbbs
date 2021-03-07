2f44c3aec1f04232a914c1deef28e29785980bc8
echicken <echicken@bbs.electronicchicken.com>
2021-03-07 06:14:39 +0000

    No functional change; just some reformatting so I can go to sleep tonight without cringing myself to death. "echicken authored 6 years ago" ... yikes, I can practically smell the whisky coming off of this file.

--- exec/load/menu-command-helpers.js
+++ exec/load/menu-command-helpers.js
@@ -8,105 +8,78 @@ load("text.js");
 // select file group & directory
 
 // I'm not sure about the handling of bbs.curgrp vs. msg_area.grp_list index here
-var selectMessageGroup = function() {
+function selectMessageGroup() {
 
 	console.putmsg(bbs.text(CfgGrpLstHdr));
-	for(var g = 0; g < msg_area.grp_list.length; g++) {
-		console.putmsg(
-			format(
-				((g == bbs.curgrp) ? " * " : "   ") + bbs.text(CfgGrpLstFmt),
-				g + 1,
-				msg_area.grp_list[g].description
-			)
-		);
+	for (var g = 0; g < msg_area.grp_list.length; g++) {
+		console.putmsg(format(
+			((g == bbs.curgrp) ? " * " : "   ") + bbs.text(CfgGrpLstFmt),
+			g + 1, msg_area.grp_list[g].description
+		));
 	}
-	console.mnemonics(
-		format(
-			bbs.text(JoinWhichGrp),
-			bbs.curgrp + 1
-		)
-	);
+	console.mnemonics(format(bbs.text(JoinWhichGrp), bbs.curgrp + 1));
 	var g = console.getkeys("Q", msg_area.grp_list.length);
-	if(g != "Q" && g != "")
-		bbs.curgrp = parseInt(g) - 1;
+	if (g != "Q" && g != "") bbs.curgrp = parseInt(g) - 1;
 
 }
 
 // Not sure about the handling of bbs.cursb vs sub_list index here
-var selectMessageArea = function() {
+function selectMessageArea() {
 
 	console.putmsg(format(bbs.text(SubLstHdr), msg_area.sub[bbs.cursub_code].grp_name));
-	for(var s = 0; s < msg_area.grp_list[bbs.curgrp].sub_list.length; s++) {
+	for (var s = 0; s < msg_area.grp_list[bbs.curgrp].sub_list.length; s++) {
 		var mb = new MsgBase(msg_area.grp_list[bbs.curgrp].sub_list[s].code);
 		mb.open();
 		var tm = mb.total_msgs;
 		mb.close();
-		console.putmsg(
-			format(
-				((s == bbs.cursub) ? " * " : "   ") + bbs.text(SubLstFmt),
-				s + 1,
-				msg_area.grp_list[bbs.curgrp].sub_list[s].description,
-				"",
-				tm
-			)
-		);
+		console.putmsg(format(
+			((s == bbs.cursub) ? " * " : "   ") + bbs.text(SubLstFmt),
+			s + 1, msg_area.grp_list[bbs.curgrp].sub_list[s].description, "", tm
+		));
 	}
-	console.mnemonics(
-		format(
-			bbs.text(JoinWhichSub),
-			bbs.cursub + 1
-		)
-	);
+	console.mnemonics(format(bbs.text(JoinWhichSub), bbs.cursub + 1));
 	var s = console.getkeys("Q", msg_area.grp_list[bbs.curgrp].sub_list.length);
-	if(s != "Q" && s != "")
-		bbs.cursub = parseInt(s) - 1;
+	if (s != "Q" && s != "") bbs.cursub = parseInt(s) - 1;
 
 }
 
-var selectGroupAndArea = function() {
+function selectGroupAndArea() {
 	selectMessageGroup();
 	selectMessageArea();
 }
 
-var scanSubs = function() {
-
+function scanSubs() {
 	console.putmsg(bbs.text(MessageScan));
 	console.crlf();
 	var youOnly = (console.noyes("To you only")) ? 0 : SCAN_TOYOU;
 	bbs.scan_subs(SCAN_NEW|youOnly);
-
 }
 
-var sendMail = function() {
-
+function sendMail() {
 	console.putmsg(bbs.text(Email));
 	console.crlf();
 	var nameOrNumber = console.getstr("", 30, K_EDIT|K_LINE);
-	if(isNaN(parseInt(nameOrNumber)))
-		nameOrNumber = system.matchuser(nameOrNumber);
-	if(nameOrNumber < 1) {
+	if (isNaN(parseInt(nameOrNumber))) nameOrNumber = system.matchuser(nameOrNumber);
+	if (nameOrNumber < 1) {
 		console.crlf();
 		console.putmsg(bbs.text(Aborted));
 		return;
 	}
 	bbs.email(nameOrNumber, WM_EMAIL);
-	
 }
 
-var sendNetMail = function() {
-
+function sendNetMail() {
 	console.putmsg("Netmail/email address:");
 	console.crlf();
 	var name = console.getstr("", console.screen_columns - 1, K_EDIT|K_LINE);
 	bbs.netmail(name, WM_NONE);
-	
 }
 
-var findMessages = function() {
+function findMessages() {
 
-	var findInSub = function(sub) {
+	function findInSub(sub) {
 		subs++;
-		if(subjectsOnly) {
+		if (subjectsOnly) {
 			bbs.list_msgs(sub, SCAN_FIND, text);
 			return true;
 		} else {
@@ -114,40 +87,33 @@ var findMessages = function() {
 		}
 	}
 
-	var findInGroup = function(grp) {
-		for(var sub in msg_area.grp_list[grp].sub_list) {
+	function findInGroup(grp) {
+		for (var sub in msg_area.grp_list[grp].sub_list) {
 			var ret = findInSub(msg_area.grp_list[grp].sub_list[sub].code);
-			if(!ret)
-				break;
+			if (!ret) break;
 		}
-		return (typeof ret == "undefined") ? true : ret;
+		return ret === undefined ? true : ret;
 	}
 
-	var findInAll = function() {
-		for(var grp in msg_area.grp_list) {
+	function findInAll() {
+		for (var grp in msg_area.grp_list) {
 			var ret = findInGroup(grp);
-			if(!ret)
-				break;
+			if (!ret) break;
 		}
-		return (typeof ret == "undefined") ? true : ret;
+		return ret === undefined ? true : ret;
 	}
 
-	var main = function() {
+	function main() {
 		console.mnemonics(bbs.text(SubGroupOrAll));
 		var sga = console.getkeys("SGA");
 		console.putmsg(bbs.text(SearchStringPrompt));
 		console.crlf();
-		text = console.getstr(
-			"",
-			console.screen_columns - 1,
-			K_EDIT|K_LINE|K_UPPER
-		);
-		if(text == "")
-			return false;
+		text = console.getstr("", console.screen_columns - 1, K_EDIT|K_LINE|K_UPPER);
+		if (text == "") return false;
 		console.crlf();
 		subjectsOnly = console.yesno(bbs.text(DisplaySubjectsOnlyQ));
 		var ret = true;
-		switch(sga) {
+		switch (sga) {
 			case "S":
 				ret = findInSub(bbs.cursub_code);
 				break;
@@ -163,7 +129,7 @@ var findMessages = function() {
 		return ret;
 	}
 
-	var complete = function(ret) {
+	function complete(ret) {
 		console.putmsg(
 			bbs.text(MessageScan) +
 			((ret) ? format(bbs.text(MessageScanComplete), subs) : bbs.text(MessageScanAborted))
@@ -178,15 +144,13 @@ var findMessages = function() {
 
 }
 
-var findUser = function() {
-
+function findUser() {
 	console.putmsg("Full or partial username:");
 	console.crlf();
 	var name = console.getstr("", 30, K_EDIT|K_LINE);
-	if(name == "") {
+	if (name == "") {
 		console.putmsg(bbs.text(Aborted));
 		return;
 	}
 	bbs.finduser(name);
-
 }
