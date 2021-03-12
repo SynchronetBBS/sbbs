cbdd6bfbbc4abf1421936d5ed78c002cb92a2b27
echicken <echicken@bbs.electronicchicken.com>
2021-03-12 18:10:52 +0000

    Run fetched query parameters through decodeURIComponent. camelCase method names.

--- webv4/lib/request.js
+++ webv4/lib/request.js
@@ -1,26 +1,31 @@
 // Helpers for http_request
 var request = {
 	// Query parameter p exists
-	has_param: function (p) {
+	hasParam: function (p) {
 		return (Array.isArray(http_request.query[p]) && http_request.query[p].length);
 	},
     // All query parameters in Array[String] p exist
-	has_params: function (p) {
+	hasParams: function (p) {
 		return p.every(function (e) {
 			return (Array.isArray(http_request.query[e]) && http_request.query[e].length);
 		});
 	},
 	// First instance of query parameter p, or undefined
-	get_param: function (p) {
-		if (Array.isArray(http_request.query[p]) && http_request.query[p].length) {
-			return http_request.query[p][0];
-		}
+	getParam: function (p) {
+		if (this.hasParam(p)) return decodeURIComponent(http_request.query[p][0]);
 	},
-	write_param: function (p) {
-		if (Array.isArray(http_request.query[p]) && http_request.query[p].length) {
-			write(http_request.query[p][0]);
-		}
-	}
+	getParamMulti: function (p) {
+		return p.reduce((function (a, c) {
+			if (this.hasParam(c)) a[c] = http_request.query[c].map(decodeURIComponent);
+			return a;
+		}).bind(this), {});
+	},
+	writeParam: function (p) {
+		if (this.hasParam(p)) write(http_request.query[p][0]);
+	},
+	getCookie: function () {
+		return http_request.cookie.synchronet;
+	},
 };
 
 request;
