d2023d9488a03b547297a93127e8a1bf5a5dcb55
echicken <echicken@bbs.electronicchicken.com>
2021-03-13 05:17:03 +0000

    Never mind custom scope. Allow passing args to target script.

--- webv4/lib/components.js
+++ webv4/lib/components.js
@@ -2,17 +2,19 @@
 require('xjs.js', 'xjs_compile');
 
 var components = {
-    load: function loadComponent(fn, scope) {
+    load: function loadComponent(fn, args) {
         const cdir = backslash(fullpath(settings.web_mods + 'components'));
+        var sf;
         if (file_isdir(cdir) && file_exists(cdir + fn)) {
-            js.exec(xjs_compile(cdir + fn), scope || new function () {});
-            return true;
+            sf = xjs_compile(cdir + fn);
+        } else if (file_exists(settings.web_components + fn)) {
+            sf = xjs_compile(settings.web_components + fn);
+        } else {
+            return false;
         }
-        if (file_exists(settings.web_components + fn)) {
-            js.exec(xjs_compile(settings.web_components + fn), scope || new function () {});
-            return true;
-        }
-        return false;
+        js.exec.apply(null, [].concat(sf, new function () {}, args));
+        return true;
+
 	}
 }
 
