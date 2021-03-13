SynchronetBBS/sbbs

commit 0c441424bc6217cf2ac16e0fb09fb26b0d5badb5

Address Coverity-reported issues
Hopefully not introducing any bugs in the process.

src/sbbs3/writemsg.cpp
@@ -78,7 +78,7 @@ bool sbbs_t::quotemsg(smb_t* smb, smbmsg_t* msg, bool tails)
 		useron_xedit = 0;
 
 	quotes_fname(useron_xedit,fname,sizeof(fname));
-	removecase(fname);
+	(void)removecase(fname);
 
 	if((fp=fopen(fname,"wb"))==NULL) {
 		errormsg(WHERE,ERR_OPEN,fname,0);
@@ -300,9 +300,9 @@ bool sbbs_t::writemsg(const char *fname, const char *top, char *subj, long mode,
 		mode|=WM_NOTOP;
 
 	msg_tmp_fname(useron_xedit, msgtmp, sizeof(msgtmp));
-	removecase(msgtmp);
+	(void)removecase(msgtmp);
 	SAFEPRINTF(tagfile,"%seditor.tag",cfg.temp_dir);
-	removecase(tagfile);
+	(void)removecase(tagfile);
 	SAFEPRINTF(draft_desc, "draft.%s.msg", subnum >= cfg.total_subs ? "mail" : cfg.sub[subnum]->code);
 	SAFEPRINTF3(draft, "%suser/%04u.%s", cfg.data_dir, useron.number, draft_desc);
 
@@ -311,7 +311,7 @@ bool sbbs_t::writemsg(const char *fname, const char *top, char *subj, long mode,
 		if(mv(draft, msgtmp, /* copy: */true) == 0) {
 			lprintf(LOG_NOTICE, "draft message restored: %s (%lu bytes)", draft, (ulong)flength(msgtmp));
 			draft_restored = true;
-			removecase(quotes_fname(useron_xedit, str, sizeof(str)));
+			(void)removecase(quotes_fname(useron_xedit, str, sizeof(str)));
 		} else
 			lprintf(LOG_ERR, "ERROR %d (%s) restoring draft message: %s", errno, strerror(errno), draft);
 	}
@@ -456,7 +456,7 @@ bool sbbs_t::writemsg(const char *fname, const char *top, char *subj, long mode,
 		} 
 	}
 	else {
-		removecase(quotes_fname(useron_xedit, str, sizeof(str)));
+		(void)removecase(quotes_fname(useron_xedit, str, sizeof(str)));
 	}
 
 	if(!online || sys_status&SS_ABORT) {
@@ -575,7 +575,7 @@ bool sbbs_t::writemsg(const char *fname, const char *top, char *subj, long mode,
 
 		if(!draft_restored) {
 			if(!linesquoted)
-				removecase(msgtmp);
+				(void)removecase(msgtmp);
 			else {
 				qlen=(long)flength(msgtmp);
 				qtime=(long)fdate(msgtmp); 
@@ -678,7 +678,7 @@ bool sbbs_t::writemsg(const char *fname, const char *top, char *subj, long mode,
 
 	now=time(NULL);
 	bputs(text[Saving]);
-	removecase(fname);
+	(void)removecase(fname);
 	if((stream=fnopen(NULL,fname,O_WRONLY|O_CREAT|O_TRUNC))==NULL) {
 		errormsg(WHERE,ERR_OPEN,fname,O_WRONLY|O_CREAT|O_TRUNC);
 		free(buf);
@@ -736,7 +736,7 @@ bool sbbs_t::writemsg(const char *fname, const char *top, char *subj, long mode,
 		}
 	}
 
-	remove(draft);
+	(void)remove(draft);
 	fclose(stream);
 	free((char *)buf);
 	bprintf(text[SavedNBytes],l,lines);
@@ -781,10 +781,10 @@ void sbbs_t::editor_inf(int xeditnum, const char *to, const char* from, const ch
 	xeditnum--;
 
 	SAFEPRINTF(path,"%sresult.ed",cfg.node_dir);
-	removecase(path);
+	(void)removecase(path);
 	if(cfg.xedit[xeditnum]->misc&QUICKBBS) {
 		SAFEPRINTF2(path,"%s%s",cfg.node_dir, cfg.xedit[xeditnum]->misc&XTRN_LWRCASE ? "msginf":"MSGINF");
-		removecase(path);
+		(void)removecase(path);
 		if((fp=fopen(path,"wb"))==NULL) {
 			errormsg(WHERE,ERR_OPEN,path,O_WRONLY|O_CREAT|O_TRUNC);
 			return; 
@@ -804,7 +804,7 @@ void sbbs_t::editor_inf(int xeditnum, const char *to, const char* from, const ch
 	}
 	else {
 		SAFEPRINTF2(path,"%s%s",cfg.node_dir,cfg.xedit[xeditnum]->misc&XTRN_LWRCASE ? "editor.inf" : "EDITOR.INF");
-		removecase(path);
+		(void)removecase(path);
 		if((fp=fopen(path,"wb"))==NULL) {
 			errormsg(WHERE,ERR_OPEN,path,O_WRONLY|O_CREAT|O_TRUNC);
 			return; 
@@ -1123,7 +1123,7 @@ ulong sbbs_t::msgeditor(char *buf, const char *top, char *title)
 				upload:
 				char	fname[MAX_PATH + 1];
 				SAFEPRINTF(fname, "%sUPLOAD.MSG", cfg.temp_dir);
-				removecase(fname);
+				(void)removecase(fname);
 				if(!recvfile(fname, prot, /* autohang: */false)) {
 					bprintf(text[FileNotReceived], "File");
 					continue;
@@ -1210,7 +1210,7 @@ bool sbbs_t::editfile(char *fname, bool msg)
 	else
 		maxlines=MAX_LINES;
 	quotes_fname(useron_xedit, path, sizeof(path));
-	removecase(path);
+	(void)removecase(path);
 
 	if(useron_xedit) {
 
@@ -1218,7 +1218,8 @@ bool sbbs_t::editfile(char *fname, bool msg)
 
 		msg_tmp_fname(useron_xedit, msgtmp, sizeof(msgtmp));
 		if(stricmp(msgtmp,path)) {
-			removecase(msgtmp);
+			if(removecase(msgtmp) != 0)
+				errormsg(WHERE, ERR_REMOVE, msgtmp, 0);
 			if(fexistcase(path))
 				CopyFile(path, msgtmp, /* failIfExists: */FALSE);
 		}
@@ -1246,8 +1247,8 @@ bool sbbs_t::editfile(char *fname, bool msg)
 		rioctl(IOSM|PAUSE|ABORT); 
 		return true; 
 	}
-	if((buf=(char *)malloc(maxlines*MAX_LINE_LEN))==NULL) {
-		errormsg(WHERE,ERR_ALLOC,nulstr,maxlines*MAX_LINE_LEN);
+	if((buf=(char *)malloc((maxlines*MAX_LINE_LEN) + 1))==NULL) {
+		errormsg(WHERE,ERR_ALLOC,nulstr, (maxlines*MAX_LINE_LEN) + 1);
 		return false; 
 	}
 	if((file=nopen(fname,O_RDONLY))!=-1) {
@@ -1589,7 +1590,8 @@ bool sbbs_t::editmsg(smb_t* smb, smbmsg_t *msg)
 		return false;
 
 	msg_tmp_fname(useron.xedit, msgtmp, sizeof(msgtmp));
-	removecase(msgtmp);
+	if(removecase(msgtmp) != 0)
+		return false;
 	msgtotxt(smb, msg, msgtmp, /* header: */false, /* mode: */GETMSGTXT_ALL);
 	if(!editfile(msgtmp, /* msg: */true))
 		return false;

commit 5b044471b7f5dd69840fcb044fcf5a6379c56d77

Fix error removing INPUT.MSG introduced in Commit 0c441424
remove() is expected to fail in this scenario sometimes depending
on how writemsg() is used - in the error case, the user was trying
to edit his .plan file with the ;plan command:
term Node 10 <Deuce> !ERROR 11 (Resource temporarily unavailable) in writemsg.cpp line 1232 (editfile) removing "/sbbs/node10/temp/INPUT.MSG" access=0

Thanks for the bug report!

src/sbbs3/writemsg.cpp
@@ -1228,8 +1228,7 @@ bool sbbs_t::editfile(char *fname, bool msg)
 
 		msg_tmp_fname(useron_xedit, msgtmp, sizeof(msgtmp));
 		if(stricmp(msgtmp,path)) {
-			if(removecase(msgtmp) != 0)
-				errormsg(WHERE, ERR_REMOVE, msgtmp, 0);
+			(void)removecase(msgtmp);
 			if(fexistcase(path))
 				CopyFile(path, msgtmp, /* failIfExists: */FALSE);
 		}