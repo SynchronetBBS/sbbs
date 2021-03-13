077b904208e72e3a6e1844311f92427d89231821
echicken <echicken@bbs.electronicchicken.com>
2021-03-13 03:48:34 +0000

    Comments. Return empty arrays for multis if no values. Reuse own methods.

--- webv4/lib/request.js
+++ webv4/lib/request.js
@@ -14,14 +14,27 @@ var request = {
 	getParam: function (p) {
 		if (this.hasParam(p)) return decodeURIComponent(http_request.query[p][0]);
 	},
+	// Return an array of all values supplied for query param 'p'
+	// eg. ?a=a&a=b&a=c&a=d would yield [ 'a', 'b', 'c', 'd' ]
 	getParamMulti: function (p) {
+		if (this.hasParam(p)) return http_request.query[p].map(decodeURIComponent);
+		return [];
+	},
+	// As above but returns an object with param names as keys, with array properties
+	// eg. ?a=a&b=c&a=b&b=d would yield { a: [ 'a', 'b' ], b: [ 'c', 'd' ] }
+	getParamsMulti: function (p) {
 		return p.reduce((function (a, c) {
-			if (this.hasParam(c)) a[c] = http_request.query[c].map(decodeURIComponent);
+			if (this.hasParam(c)) {
+				a[c] = http_request.query[c].map(decodeURIComponent);
+			} else {
+				a[c] = [];
+			}
 			return a;
 		}).bind(this), {});
 	},
+	// Print the value of parameter 'p' if it exists, otherwise do nothing
 	writeParam: function (p) {
-		if (this.hasParam(p)) write(http_request.query[p][0]);
+		if (this.hasParam(p)) write(this.getParam(p));
 	},
 	getCookie: function () {
 		return http_request.cookie.synchronet;
