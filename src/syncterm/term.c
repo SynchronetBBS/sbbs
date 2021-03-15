197ef5abde737abffd67d0e82898c40f7e26669d
Deucе <shurd@sasktel.net>
2021-03-15 01:57:07 -0400

    In XModem/YModem, when total_files reaches zero, stop downloading.

--- src/syncterm/term.c
+++ src/syncterm/term.c
@@ -1838,7 +1838,8 @@ void xmodem_download(struct bbslist *bbs, long mode, char *path)
 			break;
 		if((cps=(unsigned)(file_bytes/t))==0)
 			cps=1;
-		total_files--;
+		if (--total_files <= 0)
+			break;
 		total_bytes-=file_bytes;
 		if(total_files>1 && total_bytes)
 			lprintf(LOG_INFO,"Remaining - Time: %lu:%02lu  Files: %u  KBytes: %"PRId64
