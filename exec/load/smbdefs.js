b876d5e18e967c84e556cee8ada17e7b4c1be052
Rob Swindell <rob@synchro.net>
2021-03-18 21:00:24 -0700

    Define SMB_TAGS_MAXLEN for maximum total tags length per message

--- exec/load/smbdefs.js
+++ exec/load/smbdefs.js
@@ -118,6 +118,7 @@ const SMB_SUMMARY			= 0x61; 	// Not currently used for messages
 const SMB_COMMENT 			= 0x62; 	// Appear in message text, before body
 const SMB_TAGS				= 0x69; 	// Tags (ala hashtags) for a message
 const SMB_TAG_DELIMITER		= ' ';		// Tags are space-separated
+const SMB_TAG_MAXLEN		= 60;		// Tags strings are limited to this total length
 const SMB_POLL_ANSWER		= 0xe0;		// One poll answer (the subject is the question)
 
 const FIDOCTRL     			= 0xa0;
