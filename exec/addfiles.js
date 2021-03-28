7fde4d1cf2d932b73ec6c25fe4a3e9760ca8c09a
Rob Swindell <rob@synchro.net>
2021-03-27 21:54:06 -0700

    Support filenames with ! and # in them and lots of other improvements

--- exec/addfiles.js
+++ exec/addfiles.js
@@ -13,10 +13,45 @@ const default_excludes = [
 
 if(argv.indexOf("-help") >= 0 || argv.indexOf("-?") >= 0) {
 	print("usage: [-options] [dir-code] [listfile]");
+	print("options:");
+	print("-lib=<name>     add files in all directories of specified lib");
+	print("-from=<name>    specify uploader's user name");
+	print("-ex=<filename>  add to excluded filename list");
+	print("                (default: " + default_excludes.join(',') + ")");
+	print("-diz            always extract/use description in archive");
+	print("-update         update existing file entries");
+	print("-date[=fmt]     include today's date in description");
+	print("-fdate[=fmt]    include file's date in description");
+	print("-adate[=fmt]    include newest archived file date in description");
+	print("                (fmt = optional strftime date/time format string)");
+	print("-v              increase verbosity of output");
+	print("-debug          enable debug output");
 	exit(0);
 }
 
+function datestr(t)
+{
+	if(date_fmt)
+		return strftime(date_fmt, t);
+	return system.datestr(t);
+}
+
+function archive_date(file)
+{
+	try {
+		var list = Archive(file).directory;
+	} catch(e) {
+		return file_date(file);
+	}
+	var t = 0;
+	for(var i = 0; i < list.length; i++)
+		t = Math.max(list[i].time, t);
+	return t;
+}
+
+var uploader;
 var listfile;
+var date_fmt;
 var options = {};
 var exclude = [];
 var dir_list = [];
@@ -26,6 +61,35 @@ for(var i = 0; i < argc; i++) {
 	if(arg[0] == '-') {
 		if(arg.indexOf("-ex=") == 0) {
 			exclude.push(arg.slice(4).toUpperCase());
+			continue;
+		}
+		if(arg.indexOf("-lib=") == 0) {
+			var lib = arg.slice(5);
+			if(!file_area.lib[lib]) {
+				alert("Library not found: " + lib);
+				exit(1);
+			}
+			for(var j = 0; j < file_area.lib[lib].dir_list.length; j++)
+				dir_list.push(file_area.lib[lib].dir_list[j].code);
+			continue;
+		}
+		if(arg.indexOf("-from=") == 0) {
+			uploader = arg.slice(6);
+			continue;
+		}
+		if(arg.indexOf("-date=") == 0) {
+			date_fmt = arg.slice(6);
+			options.date = true;
+			continue;
+		}
+		if(arg.indexOf("-fdate=") == 0) {
+			date_fmt = arg.slice(7);
+			options.fdate = true;
+			continue;
+		}
+		if(arg.indexOf("-adate=") == 0) {
+			date_fmt = arg.slice(7);
+			options.adate = true;
 			continue;
 		}
 		if(arg == '-' || arg == '-all') {
@@ -64,6 +128,7 @@ if(!dir_list.length) {
 }
 
 var added = 0;
+var updated = 0;
 for(var d = 0; d < dir_list.length; d++) {
 	
 	var code = dir_list[d];
@@ -119,6 +184,7 @@ for(var d = 0; d < dir_list.length; d++) {
 	
 	for(var i = 0; i < file_list.length; i++) {
 		var file = file_list[i];
+		file.from = uploader;
 		if(options.debug)
 			print(JSON.stringify(file, null, 4));
 		else if(verbosity)
@@ -128,7 +194,11 @@ for(var d = 0; d < dir_list.length; d++) {
 				print("excluded (ignored)");
 			continue;
 		}
-		if(name_list.indexOf(file.name.toUpperCase()) >= 0) {
+		file.extdesc = lfexpand(file.extdesc);
+		if(verbosity > 1)
+			print(JSON.stringify(file));
+		var exists = name_list.indexOf(file.name.toUpperCase()) >= 0;
+		if(exists && !options.update) {
 			if(verbosity)
 				print("already added");
 			continue;
@@ -138,27 +208,52 @@ for(var d = 0; d < dir_list.length; d++) {
 			alert("does not exist: " + path);
 			continue;
 		}
-		//if(!(file_area.dir[code].settings & DIR_FREE)) ??
+		if(options.date)
+			file.desc = datestr(time()) + " " + file.desc;
+		else if(options.fdate)
+			file.desc = datestr(file_date(path)) + " " + file.desc;
+		else if(options.adate)
+			file.desc = datestr(archive_date(path)) + " " + file.desc;
 		file.cost = file_size(path);
-		// Add file here:
-		if(!filebase.add_file(file)) {
-			alert("Error " + filebase.last_error + " adding " + file.name);
+		if(exists) {
+			var hash = filebase.hash_file(file.name);
+			if(hash) {
+				file.size = hash.size;
+				file.crc16 = hash.crc16;
+				file.crc32 = hash.crc32;
+				file.md5 = hash.md5;
+				file.sha1 = hash.sha1;
+			}
+			if(!filebase.update_file(file.name, file, options.diz)) {
+				alert("Error " + filebase.last_error + " updating " + file.name);
+			} else {
+				print("Updated " + file.name);
+				updated++;
+			}
 		} else {
-			print("Added " + file.name);
-			added++;
+			// Add file here:
+			if(!filebase.add_file(file, options.diz)) {
+				alert("Error " + filebase.last_error + " adding " + file.name);
+			} else {
+				print("Added " + file.name);
+				added++;
+			}
 		}
 	}
 	
 	filebase.close();
 }
 print(added + " files added");
+if(updated)
+	print(updated + " files updated");
 
 function parse_file_list(lines)
 {
 	var file_list = [];
 	for(var i = 0; i < lines.length; i++) {
 		var line = lines[i];
-		var match = line.match(/(^[\w]+[\w\-\.]*)\W+[^A-Za-z]*(.*)/);
+		var match = line.match(/(^[\w]+[\w\-\!\#\.]*)\W+[^A-Za-z]*(.*)/);
+//		print('fname line match: ' + JSON.stringify(match));
 		if(match && match.length > 1) {
 			var file = { name: match[1], desc: match[2] };
 			if(file.desc && file.desc.length > LEN_FDESC)
@@ -166,15 +261,21 @@ function parse_file_list(lines)
 			file_list.push(file);
 			continue;
 		}
-		match = line.match(/\W+\|\W+(.*)/);
-		if(!match)
+		match = line.match(/\W+\|\s+(.*)/);
+		if(!match) {
+			if(verbosity)
+				alert("Ignoring line: " + line);
 			continue;
-		print('match: ' + JSON.stringify(match));
+		}
+//		print('match: ' + JSON.stringify(match));
 		if(match && match.length > 1 && file_list.length) {
 			var file = file_list[file_list.length - 1];
 			if(!file.extdesc)
 				file.extdesc = file.desc + "\n";
 			file.extdesc += match[1] + "\n";
+			var combined = file.desc + " " + match[1].trim();
+			if(combined.length <= LEN_FDESC)
+				file.desc = combined;
 		}
 	}
 	return file_list;
