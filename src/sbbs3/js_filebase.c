SynchronetBBS/sbbs

commit d79b390aab22d77ccc67d5d63a2c8e841e35fefe

Alt paths aren't supported/used any longer
Fixes CID 330973

src/sbbs3/js_filebase.c
@@ -468,8 +468,6 @@ js_hash_file(JSContext *cx, uintN argc, jsval *arglist)
 		SAFECOPY(path, filename);
 	else {
 		file.name = filename;
-		// read index record, if it exists (for altpath)
-		smb_findfile(&p->smb, filename, &file);
 		getfilepath(scfg, &file, path);
 	}
 	off_t size = flength(path);

commit 2c218bdb89cf00325d7d8ab5d7bffa418d4bc583

Fix hash() method broken in commit d79b390a - file needs the dir num

src/sbbs3/js_filebase.c
@@ -468,6 +468,7 @@ js_hash_file(JSContext *cx, uintN argc, jsval *arglist)
 	if(getfname(filename) != filename)
 		SAFECOPY(path, filename);
 	else {
+		file.dir = p->smb.dirnum;
 		file.name = filename;
 		getfilepath(scfg, &file, path);
 	}