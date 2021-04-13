0cceee5a40ee05876d5e9054497816a78fc265ef
Rob Swindell <rob@synchro.net>
2021-04-13 01:05:21 -0700

    Don't pass uninitialized string to scanposts()
    CID 319108

--- src/sbbs3/scansubs.cpp
+++ src/sbbs3/scansubs.cpp
@@ -1,7 +1,5 @@
 /* Synchronet message database scanning routines */
 
-/* $Id: scansubs.cpp,v 1.30 2019/05/02 19:37:04 rswindell Exp $ */
-
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
@@ -15,20 +13,8 @@
  * See the GNU General Public License for more details: gpl.txt or			*
  * http://www.fsf.org/copyleft/gpl.html										*
  *																			*
- * Anonymous FTP access to the most recent released source is available at	*
- * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
- *																			*
- * Anonymous CVS access to the development source and modification history	*
- * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
- *     (just hit return, no password is necessary)							*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src		*
- *																			*
  * For Synchronet coding style and modification guidelines, see				*
  * http://www.synchro.net/source.html										*
- *																			*
- * You are encouraged to submit any modifications (preferably in Unix diff	*
- * format) via e-mail to mods@synchro.net									*
  *																			*
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
@@ -40,7 +26,7 @@
 /****************************************************************************/
 void sbbs_t::scansubs(long mode)
 {
-	char	ch,str[256];
+	char	ch,str[256] = "";
 	char 	tmp[512];
 	uint	i=0,found=0;
 	ulong	subs_scanned=0;
