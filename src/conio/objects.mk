7080b0c428dc54fecc750f254e68eb5c4006dc7f
Deucе <shurd@sasktel.net>
2021-05-14 19:14:15 -0400

    scale.o and xbr.o are (basically) required by bitmap now.

--- src/conio/objects.mk
+++ src/conio/objects.mk
@@ -4,6 +4,5 @@ OBJS	=	$(MTOBJODIR)$(DIRSEP)ansi_cio$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)vidmodes$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)allfonts$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)mouse$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)scale$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)syncicon64$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)utf8_codepages$(OFILE)
