9b392346debd80a0aa30587cb693bb7c10feb7c1
Deucе <shurd@sasktel.net>
2021-05-19 19:07:24 -0400

    Implement upload/download support and use cache dir

--- src/syncterm/term.h
+++ src/syncterm/term.h
@@ -16,12 +16,19 @@ struct terminal {
 	int nostatus;
 };
 
+#define XMODEM_128B		(1<<10)	/* Use 128 byte block size (ick!) */
+
 extern struct terminal term;
 extern struct cterminal	*cterm;
 extern int log_level;
 
+void zmodem_upload(struct bbslist *bbs, FILE *fp, char *path);
+void xmodem_upload(struct bbslist *bbs, FILE *fp, char *path, long mode, int lastch);
+void xmodem_download(struct bbslist *bbs, long mode, char *path);
+void zmodem_download(struct bbslist *bbs);
 BOOL doterm(struct bbslist *);
 void mousedrag(struct vmem_cell *scrollback);
 void get_cterm_size(int *cols, int *rows, int ns);
+int get_cache_fn_base(struct bbslist *bbs, char *fn, size_t fnsz);
 
 #endif
