0b8517e11ce52702b4c56e2926990ae456dc23f6
Rob Swindell <rob@synchro.net>
2022-01-23 02:07:22 -0800

    comReadBuf() supports an infinite timeout (0)

--- src/comio/comio.c
+++ src/comio/comio.c
@@ -1,8 +1,4 @@
-/* comio.c */
-
 /* Synchronet Serial Communications I/O Library Common Functions */
-
-/* $Id: comio.c,v 1.4 2020/06/26 19:33:08 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -17,20 +13,8 @@
  * See the GNU Lesser General Public License for more details: lgpl.txt or	*
  * http://www.fsf.org/copyleft/lesser.html									*
  *																			*
- * Anonymous FTP access to the most recent released source is available at	*
- * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
- *																			*
- * Anonymous CVS access to the development source and modification history	*
- * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
- *     (just hit return, no password is necessary)							*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src		*
- *																			*
  * For Synchronet coding style and modification guidelines, see				*
  * http://www.synchro.net/source.html										*
- *																			*
- * You are encouraged to submit any modifications (preferably in Unix diff	*
- * format) via e-mail to mods@synchro.net									*
  *																			*
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
@@ -46,7 +30,7 @@ size_t comReadBuf(COM_HANDLE handle, char* buf, size_t buflen, const char* termi
 
 	while(len < buflen) {
 		if(!comReadByte(handle, &ch)) {
-			if(msclock()-start >= timeout)
+			if(timeout > 0 && msclock()-start >= timeout)
 				break;
 			YIELD();
 			continue;
