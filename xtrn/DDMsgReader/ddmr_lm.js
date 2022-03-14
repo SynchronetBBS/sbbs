4d0a7e96dcac1e3487fdc1c466119c4ffdd666c4
Eric Oulashin <eric.oulashin@gmail.com>
2022-03-14 16:21:16 -0700

    Simplified the loadable module script (called argv.join(" ") to generate the arguments string)

--- xtrn/DDMsgReader/ddmr_lm.js
+++ xtrn/DDMsgReader/ddmr_lm.js
@@ -2,14 +2,5 @@
 // Message Reader in a different path
 var msgReaderPath = "../xtrn/DDMsgReader";
 
-// Build a command line string and then run DDMsgReader
-var argsStr = "";
-for (var i = 0; i < argv.length; ++i)
-{
-	argsStr += argv[i];
-	if (i < argv.length - 1)
-		argsStr += " ";
-}
-
 // Run Digital Distortion Message Reader
-bbs.exec("?" + msgReaderPath + "/DDMsgReader.js " + argsStr);
+bbs.exec("?" + msgReaderPath + "/DDMsgReader.js " + argv.join(" "));
