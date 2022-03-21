939d7e6e39380b33070a14a48d66af023434d877
Rob Swindell <rob@synchro.net>
2022-03-20 21:16:19 -0700

    Add options to sort directories by file size (ascending or descending)
    Size is indexed, so might as well sort by it. This does have an issue with files >= 4GB in length however, so I'm looking at that next.

--- src/sbbs3/sbbsdefs.h
+++ src/sbbs3/sbbsdefs.h
@@ -296,6 +296,8 @@ enum file_sort {                    /* Values for dir[x].sort */
 	,FILE_SORT_NAME_DC  = 5         /* Sort by filename, descending (case-sensitive) */
     ,FILE_SORT_DATE_A   = 2         /* Sort by upload date, ascending */
     ,FILE_SORT_DATE_D   = 3         /* Sort by upload date, descending */
+	,FILE_SORT_SIZE_A	= 6			/* Sort by file size, ascending */
+	,FILE_SORT_SIZE_D	= 7			/* Sort by file size, descending */
 	,FILE_SORT_NATURAL  = FILE_SORT_DATE_A
 };
 
