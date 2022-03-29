ac0420b8014b899c40690ba6dca5392e084ece9d
Rob Swindell <rob@synchro.net>
2022-03-29 01:14:01 -0700

    Pretty much a total rewrite of the csts.* file parsing stuff
    Hunting down and destroying some super old SBBS code now. :-)

--- src/sbbs3/slog.c
+++ src/sbbs3/slog.c
@@ -1,19 +1,28 @@
-/* SLOG.C */
+/* Statistics Log Viewer */
 
-/* Developed 1990-1997 by Rob Swindell; PO Box 501, Yorba Linda, CA 92885 */
+/****************************************************************************
+ * @format.tab-size 4		(Plain Text/Source Code File Header)			*
+ * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
+ *																			*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ *																			*
+ * This program is free software; you can redistribute it and/or			*
+ * modify it under the terms of the GNU General Public License				*
+ * as published by the Free Software Foundation; either version 2			*
+ * of the License, or (at your option) any later version.					*
+ * See the GNU General Public License for more details: gpl.txt or			*
+ * http://www.fsf.org/copyleft/gpl.html										*
+ *																			*
+ * For Synchronet coding style and modification guidelines, see				*
+ * http://www.synchro.net/source.html										*
+ *																			*
+ * Note: If this box doesn't appear square, then you need to fix your tabs.	*
+ ****************************************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <time.h>
-#include <errno.h>
+#include <stdbool.h>
 
-#include "sbbsdefs.h"
-#include "nopen.h"
-#include "dirwrap.h"
-#include "conwrap.h"
+#include "sbbs.h"
+#include "dat_file.h"
 
 /****************************************************************************/
 /* Lists system statistics for everyday the bbs has been running.           */
@@ -22,102 +31,121 @@
 int main(int argc, char **argv)
 {
 	char str[256],dir[256]={""},*p;
-    uchar *buf;
-	int i,file,pause=0,lncntr=0;
+	FILE* fp;
+	int i, lncntr=0, rows=20;
+	uint days = 0;
+	uint count = 0;
+	bool pause = false;
     time_t timestamp;
-    long l;
-    off_t   length;
-    ulong	logons,
-            timeon,
-            posts,
-            emails,
-            fbacks,
-            ulb,
-            uls,
-            dlb,
-            dls;
-	time_t	yesterday;
-	struct tm *curdate;
+	stats_t stats = {0};
+	totals_t total = {0};
+	bool totals = false;
+	time_t yesterday;
+	struct tm* tm;
 
+	ZERO_VAR(total);
+	printf("\nSynchronet System/Node Statistics Log Viewer v2.0\n\n");
 
-printf("\nSynchronet System/Node Statistics Log Viewer v1.02\n\n");
+	for(i=1;i<argc;i++) {
+		if(stricmp(argv[i],"/P") == 0 || strncmp(argv[i], "-p", 2) == 0) {
+			pause = true;
+			if(IS_DIGIT(argv[i][2]))
+				rows = atoi(argv[i] + 2);
+		}
+		else if(strcmp(argv[i],"-t") == 0)
+			totals = true;
+		else if(strncmp(argv[i], "-d", 2) == 0)
+			days = atoi(argv[i] + 2);
+		else if(isdir(argv[i]))
+			SAFECOPY(dir, argv[i]);
+		else {
+			fprintf(stderr, "usage: %s [-opt [...]] [path]\n"
+				"\n"
+				" opts:\n"
+				"\t-p[rows]  enable screen pause prompt\n"
+				"\t-d<days>  specify maximum number of days\n"
+				"\t-t        display totals\n"
+				,argv[0]
+			);
+			return EXIT_SUCCESS;
+		}
+	}
+	if(!dir[0]) {
+		p=getenv("SBBSCTRL");
+		if(p == NULL)
+			p = SBBSCTRL_DEFAULT;
+		SAFECOPY(dir,p);
+	}
 
-for(i=1;i<argc;i++)
-	if(!stricmp(argv[i],"/P"))
-		pause=1;
-	else
-		SAFECOPY(dir,argv[1]);
-if(!dir[0]) {
-	p=getenv("SBBSCTRL");
-	if(p!=NULL)
-		SAFECOPY(dir,p); }
+	backslash(dir);
 
-backslash(dir);
+	SAFEPRINTF(str,"%scsts.tab",dir);
+	if(!fexistcase(str)) {
+		fprintf(stderr, "%s does not exist\n",str);
+		return EXIT_FAILURE;
+	}
+	if((fp = fopen(str, "rb")) == NULL) {
+		fprintf(stderr, "Error %d opening %s\n", errno, str);
+		return EXIT_FAILURE;
+	}
 
-SAFEPRINTF(str,"%scsts.dab",dir);
-if(!fexistcase(str)) {
-	printf("%s does not exist\r\n",str);
-	return(1); }
-if((file=nopen(str,O_RDONLY))==-1) {
-	printf("Error opening %s\r\n",str);
-	return(1); }
-length=filelength(file);
-if(length<40) {
-    close(file);
-	return(1); }
-if((buf=malloc((size_t)length))==0) {
-    close(file);
-	printf("error allocating %lu bytes\r\n",(ulong)length);
-	return(1); }
-read(file,buf,(uint)length);
-close(file);
-l=(long)(length-4);
-while(l>-1L) {
-    fbacks=buf[l]|((long)buf[l+1]<<8)|((long)buf[l+2]<<16)
-        |((long)buf[l+3]<<24);
-    l-=4;
-    emails=buf[l]|((long)buf[l+1]<<8)|((long)buf[l+2]<<16)
-        |((long)buf[l+3]<<24);
-    l-=4;
-    posts=buf[l]|((long)buf[l+1]<<8)|((long)buf[l+2]<<16)
-        |((long)buf[l+3]<<24);
-    l-=4;
-    dlb=buf[l]|((long)buf[l+1]<<8)|((long)buf[l+2]<<16)
-        |((long)buf[l+3]<<24);
-    l-=4;
-    dls=buf[l]|((long)buf[l+1]<<8)|((long)buf[l+2]<<16)
-        |((long)buf[l+3]<<24);
-    l-=4;
-    ulb=buf[l]|((long)buf[l+1]<<8)|((long)buf[l+2]<<16)
-        |((long)buf[l+3]<<24);
-    l-=4;
-    uls=buf[l]|((long)buf[l+1]<<8)|((long)buf[l+2]<<16)
-        |((long)buf[l+3]<<24);
-    l-=4;
-    timeon=buf[l]|((long)buf[l+1]<<8)|((long)buf[l+2]<<16)
-        |((long)buf[l+3]<<24);
-    l-=4;
-    logons=buf[l]|((long)buf[l+1]<<8)|((long)buf[l+2]<<16)
-        |((long)buf[l+3]<<24);
-    l-=4;
-    timestamp=buf[l]|((long)buf[l+1]<<8)|((long)buf[l+2]<<16)
-        |((long)buf[l+3]<<24);
-    l-=4;
-	yesterday=timestamp-(24*60*60);	/* 1 day less than stamp */
-	curdate=localtime(&yesterday);
-	printf("%2.2d/%2.2d/%2.2d T:%5lu   L:%3lu   P:%3lu   "
-		"E:%3lu   F:%3lu   U:%6luk %3lu  D:%6luk %3lu\n"
-		,curdate->tm_mon+1,curdate->tm_mday,curdate->tm_year%100,timeon,logons,posts,emails
-		,fbacks,ulb/1024,uls,dlb/1024,dls);
-	lncntr++;
-	if(pause && lncntr>=20) {
-		printf("More (Y/n) ? ");
-		fflush(stdout);
-		int ch = getchar();
-		if(ch == CTRL_C || toupper(ch) == 'N')
+	str_list_t columns = NULL;
+	str_list_t* records = tabReadFile(fp, &columns);
+	fclose(fp);
+
+	long l;
+	COUNT_LIST_ITEMS(records, l);
+	for(--l; l >= 0; --l) {
+		parse_cstats(records[l], &stats);
+		timestamp = isoDateTime_to_time(strtoul(records[l][CSTATS_DATE], NULL, 10), 0);
+		yesterday = timestamp-(24*60*60);	/* 1 day less than stamp */
+		tm = localtime(&yesterday);
+		printf("%02d/%02d/%02d", tm->tm_year % 100, tm->tm_mon + 1, tm->tm_mday);
+		printf(" T:%4lu  L:%3lu  P:%3lu  E:%3lu  F:%3lu"
+			,stats.ttoday, stats.ltoday, stats.ptoday, stats.etoday, stats.ftoday);
+		printf("  U:%5s %5lu"
+			,byte_estimate_to_str(stats.ulb, str, sizeof(str), 1, 0), stats.uls);
+		printf("  D:%5s %5lu"
+			,byte_estimate_to_str(stats.dlb, str, sizeof(str), 1, 0), stats.dls);
+		printf("  N:%2lu\n", stats.nusers);
+		total.timeon += stats.ttoday;
+		total.logons += stats.ltoday;
+		total.posts += stats.ptoday;
+		total.email += stats.etoday;
+		total.fbacks += stats.ftoday;
+		total.uls += stats.uls;
+		total.ulb += stats.ulb;
+		total.dls += stats.dls;
+		total.dlb += stats.dlb;
+		total.nusers += stats.nusers;
+		lncntr++;
+		count++;
+		if(days && count >= days)
 			break;
-		printf("\r");
-		lncntr=0; } }
-free(buf);
-return(0);
+		if(pause && lncntr>=rows) {
+			printf("More (Y/n) ? ");
+			fflush(stdout);
+			int ch = getchar();
+			if(ch == CTRL_C || toupper(ch) == 'N')
+				break;
+			printf("\r");
+			lncntr=0;		}
+	}
+
+	if(totals) {
+		printf("\n");
+		printf("%10s\n", "===TOTAL===");
+		char sep = ',';
+		printf("%10s : %13s\n", columns[CSTATS_TIMEON], minutes_to_str(total.timeon, str, sizeof(str)));
+		printf("%10s : %13s\n", columns[CSTATS_LOGONS], u32toac(total.logons, str, sep));
+		printf("%10s : %13s\n", columns[CSTATS_POSTS], u32toac(total.posts, str, sep));
+		printf("%10s : %13s\n", "Email Sent", u32toac(total.email, str, sep));
+		printf("%10s : %13s\n", "Feedbacks", u32toac(total.fbacks, str, sep));
+		printf("%10s : %13s\n", "New Users", u32toac(total.nusers, str, sep));
+		printf("%10s : %7s bytes", "Uploads", byte_estimate_to_str(total.ulb, str, sizeof(str), 1, 1));
+		printf(" in %s files\n", u32toac(total.uls, str, sep));
+		printf("%10s : %7s bytes", "Downloads", byte_estimate_to_str(total.dlb, str, sizeof(str), 1, 1));
+		printf(" in %s files\n", u32toac(total.dls, str, sep));
+	}
+	return EXIT_SUCCESS;
 }
