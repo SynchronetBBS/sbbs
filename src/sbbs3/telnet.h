66e922ad5031a7c2809d28df91b8e8c85761bb21
Rob Swindell <rob@synchro.net>
2022-06-05 15:49:07 -0700

    Add telnet_opt_enabled() function that returns true for WILL or DO opt status
    An option is enabled/supported regardless of which end makes the request, so
    the status can be WILL (an ACK to a remote request) or DO (an ACK to a local
    request) when an option is enabled.

--- src/sbbs3/telnet.h
+++ src/sbbs3/telnet.h
@@ -1,8 +1,4 @@
-/* telnet.h */
-
 /* Synchronet telnet-related constants and function prototypes */
-
-/* $Id: telnet.h,v 1.21 2019/08/24 19:37:11 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -17,20 +13,8 @@
  * See the GNU General Public License for more details: gpl.txt or			*
  * http://www.fsf.org/copyleft/gpl.html										*
  *																			*
- * Anonymous FTP access to the most recent released source is available at	*
- * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
- *																			*
- * Anonymous CVS access to the development source and modification history	*
- * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
- *     (just hit return, no password is necessary)							*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src		*
- *																			*
  * For Synchronet coding style and modification guidelines, see				*
  * http://www.synchro.net/source.html										*
- *																			*
- * You are encouraged to submit any modifications (preferably in Unix diff	*
- * format) via e-mail to mods@synchro.net									*
  *																			*
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
@@ -39,6 +23,7 @@
 #define _TELNET_H
 
 #include "gen_defs.h"	/* uchar */
+#include <stdbool.h>
 
 /* commands */
 
@@ -165,6 +150,7 @@ DLLEXPORT const char* telnet_cmd_desc(uchar cmd);
 DLLEXPORT const char* telnet_opt_desc(uchar opt);
 DLLEXPORT		uchar telnet_opt_ack(uchar cmd);
 DLLEXPORT		uchar telnet_opt_nak(uchar cmd);
+DLLEXPORT		bool  telnet_opt_enabled(uchar value);
 DLLEXPORT size_t telnet_expand(const uchar* inbuf, size_t inlen, uchar* outbuf, size_t outlen
 								,BOOL expand_cr, uchar** result);
 
