SynchronetBBS/sbbs

commit 440ccc805e6745e513ce57d7d9de041a920809fe

Add iniSortSections() which can sort a read ini list and optionally keys
This also exposed problems with the find_section() utility function: when the "found" section was actually empty and the next immediate line in the ini file was the beginning of a new section, the "keys" indexed would actually be the next section. Now fixed.

src/xpdev/ini_file.c
@@ -178,17 +178,29 @@ static size_t find_section_index(str_list_t list, const char* section)
 	return(i);
 }
 
+static size_t section_start(str_list_t list, size_t index)
+{
+	char* p = list[index];
+	if(p != NULL) {
+		SKIP_WHITESPACE(p);
+		if(*p == INI_OPEN_SECTION_CHAR) // A new section starts immediately?
+			return strListCount(list);
+	}
+	return index;
+}
+
 static size_t find_section(str_list_t list, const char* section)
 {
 	size_t	i;
+	char* p;
 
 	if(section==ROOT_SECTION)
-		return(0);
+		return section_start(list, 0);
 
 	i=find_section_index(list,section);
 	if(list[i]!=NULL)
 		i++;
-	return(i);
+	return section_start(list, i);
 }
 
 static char* key_name(char* p, char** vp, BOOL literals_supported)
@@ -428,6 +440,43 @@ BOOL iniRemoveSections(str_list_t* list, const char* prefix)
 	strListFree(&sections);
 
 	return(TRUE);
+}
+
+// This sorts comments too, so should not be used on human created/edited files
+BOOL iniSortSections(str_list_t* list, BOOL sort_keys)
+{
+	size_t i;
+	str_list_t root_keys;
+	str_list_t new_list;
+	str_list_t keys;
+	str_list_t section_list = iniGetSectionList(*list, /* prefix */NULL);
+
+	root_keys = iniGetSection(*list, ROOT_SECTION);
+	if(section_list == NULL && root_keys == NULL)
+		return TRUE;
+
+	if(sort_keys)
+		strListSortAlphaCase(root_keys);
+
+	if(section_list != NULL)
+		strListSortAlphaCase(section_list);
+	new_list = strListInit();
+	if(new_list == NULL) {
+		strListFree(&section_list);
+		return FALSE;
+	}
+	strListAppendList(&new_list, root_keys);
+	strListFree(&root_keys);
+	for(i = 0; section_list != NULL && section_list[i] != NULL; i++) {
+		keys = iniGetSection(*list, section_list[i]);
+		if(sort_keys)
+			strListSortAlphaCase(keys);
+		iniAppendSectionWithKeys(&new_list, section_list[i], keys, /* ini_style_t */NULL);
+		strListFree(&keys);
+	}
+	strListFree(list);
+	*list = new_list;
+	return TRUE;
 }
 
 BOOL iniRenameSection(str_list_t* list, const char* section, const char* newname)
@@ -2410,10 +2459,14 @@ void main(int argc, char** argv)
 	str_list_t	list;
 
 	for(i=1;i<argc;i++) {
-		if((fp=iniOpenFile(argv[i],FALSE)) == NULL)
+		if((fp=iniOpenFile(argv[i],FALSE)) == NULL) {
+			perror(argv[i]);
 			continue;
+		}
 		if((list=iniReadFile(fp)) != NULL) {
-			printf("%s\n",iniGetString(list," test | bogus ","key","default",str));
+			iniSortSections(&list, TRUE);
+			for(size_t j = 0; list[j] != NULL; j++)
+				printf("%s\n", list[j]);
 			strListFree(&list);
 		}
 		fclose(fp);

src/xpdev/ini_file.h
@@ -276,6 +276,7 @@ DLLEXPORT BOOL 			iniRemoveValue(str_list_t*, const char* section, const char* k
 DLLEXPORT BOOL 			iniRemoveSection(str_list_t*, const char* section);
 DLLEXPORT BOOL 			iniRemoveSections(str_list_t*, const char* prefix);
 DLLEXPORT BOOL 			iniRenameSection(str_list_t*, const char* section, const char* newname);
+DLLEXPORT BOOL 			iniSortSections(str_list_t*, BOOL sort_keys);
 
 DLLEXPORT BOOL 			iniHasInclude(const str_list_t);
 

commit 09b4a1dbb882c4860b4b945360922d46c4b97b0d

Fix issue with iniSet* functions and root section
Bug introduced in commit 440ccc80: iniSet* functions would write key/values to the end of the list when the root section (NULL) was specified, rather than the beginning of the list.
The root section always starts at the beginning of the list/file, so I'm not sure why section_start() was doing anything other than returning 0 for the root section. It's possible this now breaks iniSortSections() in some way, so that should be retested with a file with a root section and named sections.

src/xpdev/ini_file.c
@@ -195,7 +195,7 @@ static size_t find_section(str_list_t list, const char* section)
 	size_t	i;
 
 	if(section==ROOT_SECTION)
-		return section_start(list, 0);
+		return 0;
 
 	i=find_section_index(list,section);
 	if(list[i]!=NULL)