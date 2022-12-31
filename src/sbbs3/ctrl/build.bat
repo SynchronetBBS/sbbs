6c064579c3b4b2e1449ce65bfab47c463b19700c
Rob Swindell <rob@synchro.net>
2022-12-31 02:21:25 -0800

    Don't call makelibs.bat from build.bat
    This seems to stop the CI build and I haven't found any other solution.
    So just copy/paste the contents of makelibs.bat into this batch file. <sigh>

--- src/sbbs3/ctrl/build.bat
+++ src/sbbs3/ctrl/build.bat
@@ -1,6 +1,15 @@
 @echo off
 setlocal
-call makelibs.bat
+if exist ..\bcc.win32.lib.release 	set dllsrc=..\bcc.win32.lib.release
+if exist ..\bcc.win32.lib.debug 	set dllsrc=..\bcc.win32.lib.debug
+if exist ..\msvc.win32.dll.release	set dllsrc=..\msvc.win32.dll.release
+if exist ..\msvc.win32.dll.debug	set dllsrc=..\msvc.win32.dll.debug
+echo Creating import libraries from %dllsrc%
+coff2omf %dllsrc%\sbbs.lib sbbs.lib
+coff2omf %dllsrc%\mailsrvr.lib 	mailsrvr.lib
+coff2omf %dllsrc%\ftpsrvr.lib 	ftpsrvr.lib
+coff2omf %dllsrc%\websrvr.lib 	websrvr.lib
+coff2omf %dllsrc%\services.lib	services.lib
 bpr2mak sbbsctrl.bpr
 echo.
 make %1 %2 %3 %4 %5 %6 -f sbbsctrl.mak
