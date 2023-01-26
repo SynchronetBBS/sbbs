ee3ac7f18d914b17d3eb5b2541ce3eb7d8ac1f3f
Rob Swindell (ChromeOS) <rob@synchro.net>
2023-01-25 23:20:30 -0800

    Resolve GCC-Linux build warnings

--- xtrn/sdk/xsdk.c
+++ xtrn/sdk/xsdk.c
@@ -1,8 +1,4 @@
-/* xsdk.c */
-
 /* Synchronet External Program Software Development Kit	*/
-
-/* $Id: xsdk.c,v 1.41 2020/09/20 18:57:00 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -17,20 +13,8 @@
  * See the GNU Lesser General Public License for more details: lgpl.txt or	*
  * http://www.fsf.org/copyleft/lesser.html									*
  *																			*
- * Anonymous FTP access to the most recent released source is available at	*
- * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
- *																			*
- * Anonymous CVS access to the development source and modification history	*
- * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
- *     (just hit return, no password is necessary)							*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout xtrn	*
- *																			*
  * For Synchronet coding style and modification guidelines, see				*
  * http://www.synchro.net/source.html										*
- *																			*
- * You are encouraged to submit any modifications (preferably in Unix diff	*
- * format) via e-mail to mods@synchro.net									*
  *																			*
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
@@ -217,6 +201,8 @@
 \****************************************************************************/
 
 #include "xsdk.h"
+#include "conwrap.h"
+#include "threadwrap.h"
 
 #ifdef _WINSOCKAPI_
 WSADATA WSAData;		// WinSock data
--- xtrn/sdk/xsdk.h
+++ xtrn/sdk/xsdk.h
@@ -1,14 +1,10 @@
-/* xsdk.h */
-
 /* Synchronet XSDK function prototypes */
-
-/* $Id: xsdk.h,v 1.24 2009/01/01 12:45:59 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -17,24 +13,11 @@
  * See the GNU Lesser General Public License for more details: lgpl.txt or	*
  * http://www.fsf.org/copyleft/lesser.html									*
  *																			*
- * Anonymous FTP access to the most recent released source is available at	*
- * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
- *																			*
- * Anonymous CVS access to the development source and modification history	*
- * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
- *     (just hit return, no password is necessary)							*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout xtrn	*
- *																			*
  * For Synchronet coding style and modification guidelines, see				*
  * http://www.synchro.net/source.html										*
  *																			*
- * You are encouraged to submit any modifications (preferably in Unix diff	*
- * format) via e-mail to mods@synchro.net									*
- *																			*
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
-
 
 #ifndef _XSDK_H
 #define _XSDK_H
@@ -84,6 +67,8 @@
 #ifndef USE_XPDEV
 #include "xsdkwrap.h"
 #endif
+int kbhit(void);
+int getch(void);
 #include "xsdkvars.c"
 
 #ifdef __cplusplus
