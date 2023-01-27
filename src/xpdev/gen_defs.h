bb163f65c28034102c1ab46a68dd4d4b0d1d9852
Rob Swindell (on ChromeOS) <rob@synchro.net>
2023-01-26 23:28:30 -0800

    Revert "Make SAFECOPY() even safer by not passing NULL destination to strncpy()"
    This reverts commit 13d44e3b78d13205463e28759f67abea1e263aa1.

--- src/xpdev/gen_defs.h
+++ src/xpdev/gen_defs.h
@@ -430,7 +430,11 @@ typedef struct {
 #define TERMINATE(str)                      str[sizeof(str)-1]=0
 
 /* This is a bound-safe version of strcpy basically - only works with fixed-length arrays */
-#define SAFECOPY(dst,src)                   (strncpy(dst,(src)==NULL?"(null)":(src),sizeof(dst)), TERMINATE(dst))
+#ifdef SAFECOPY_USES_SPRINTF
+#define SAFECOPY(dst,src)                   sprintf(dst,"%.*s",(int)sizeof(dst)-1,src)
+#else   /* strncpy is faster */
+#define SAFECOPY(dst,src)                   (strncpy(dst,src,sizeof(dst)), TERMINATE(dst))
+#endif
 
 #define SAFECAT(dst, src) do { \
 	if(strlen((char*)(dst)) + strlen((char*)(src)) < sizeof(dst)) { \
