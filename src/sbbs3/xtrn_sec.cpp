SynchronetBBS/sbbs

commit 73e5dd9c39fd059a7c63b56a89b67ad7ea4bb1ad

The 3rd great MQTT data scheme update (sorry Nelgin)
- Most published messages (besides log entries) have a timestamp (in ISO8601 format) prepended and tab-separated
- The order and number of elements in client messages (list and activities) has been updated, now includes user number
- Server client lists are now published to .../SERVER/client/list
- Server client activities (connect, disconnect, update) are now published to .../SERVER/client/action/#
- Server client count is now published to .../SERVER/client (with the maximum client count, if applicable)
- Server states are now just represented by name (e.g. initializing, ready, stopping, stopped) and not number
- BBS errors are logged to sbbs/BBS/action/error/LEVEL (where LEVEL is the log level name, e.g. "critical" or "error')
- All server hack-attempts, SPAM attempts, logins, logouts, uploads, downloads, are published to sbbs/BBS/action/ACTION/*
- Chat pages are published to sbbs/BBS/action/page/node/#
- New users (on the terminal server) are published to sbbs/BBS/action/newuser
- Posted messages and executed external programs (on the terminal server) are published to sbbs/BBS/action/ACTION/CODE topic
- The event thread started/stopped status is published to .../SERVER/event

Yeah, the wiki will get updated soon to reflect/document all these changes

src/sbbs3/xtrn_sec.cpp
@@ -1255,7 +1255,7 @@ const char* sbbs_t::xtrn_dropdir(const xtrn_t* xtrn, char* buf, size_t maxlen)
 /****************************************************************************/
 /* This function handles configured external program execution. 			*/
 /****************************************************************************/
-bool sbbs_t::exec_xtrn(uint xtrnnum)
+bool sbbs_t::exec_xtrn(uint xtrnnum, bool user_event)
 {
 	char str[256],path[MAX_PATH+1],dropdir[MAX_PATH+1],name[32],c;
 	uint i;
@@ -1372,7 +1372,7 @@ bool sbbs_t::exec_xtrn(uint xtrnnum)
 	xtrndat(name,dropdir,cfg.xtrn[xtrnnum]->type,tleft,cfg.xtrn[xtrnnum]->misc);
 	if(!online)
 		return(false);
-	SAFEPRINTF(str, "running external program: %s", cfg.xtrn[xtrnnum]->name);
+	snprintf(str, sizeof(str), "running external %s: %s", cfg.xtrn[xtrnnum]->name, user_event ? "user event" : "program");
 	logline("X-",str);
 	if(cfg.xtrn[xtrnnum]->cmd[0]!='*' && logfile_fp!=NULL) {
 		fclose(logfile_fp);
@@ -1418,6 +1418,12 @@ bool sbbs_t::exec_xtrn(uint xtrnnum)
 	}
 
 	start=time(NULL);
+
+	char topic[128];
+	snprintf(topic, sizeof(topic), "exec/%s", cfg.xtrn[xtrnnum]->code);
+	snprintf(str, sizeof(str), "%u\t%s", useron.number, useron.alias);
+	mqtt_pub_timestamped_msg(mqtt, TOPIC_BBS_ACTION, topic, start, str);
+
 	external(cmdstr(cfg.xtrn[xtrnnum]->cmd, drop_file, startup_dir, NULL, mode)
 		,mode
 		,cfg.xtrn[xtrnnum]->path);
@@ -1487,7 +1493,7 @@ bool sbbs_t::user_event(user_event_t event)
 			|| !chk_ar(cfg.xtrn[i]->run_ar,&useron,&client)
 			|| !chk_ar(cfg.xtrnsec[cfg.xtrn[i]->sec]->ar,&useron,&client))
 			continue;
-		success=exec_xtrn(i); 
+		success=exec_xtrn(i, /* user_event: */true); 
 	}
 
 	return(success);

src/sbbs3/chat.cpp
@@ -697,6 +697,10 @@ bool sbbs_t::sysop_page(void)
 			sprintf(str, "%s paged you to chat", useron.alias);
 			notify(&cfg, 1, str, NULL);
 			ftouch(syspage_semfile);
+			char topic[128];
+			SAFEPRINTF(topic, "page/node/%u", cfg.node_num);
+			snprintf(str, sizeof(str), "%u\t%s", useron.number, useron.alias);
+			mqtt_pub_timestamped_msg(mqtt, TOPIC_BBS_ACTION, topic, time(NULL), str);
 		}
 		for(i=0;i<cfg.total_pages;i++)
 			if(chk_ar(cfg.page[i]->ar,&useron,&client))

src/sbbs3/download.cpp
@@ -46,6 +46,7 @@ void sbbs_t::downloadedfile(file_t* f)
 	logline("D-",str);
 
 	user_downloaded_file(&cfg, &useron, &client, f->dir, f->name, length);
+	mqtt_file_download(mqtt, &useron, f, length, &client);
 
 	user_event(EVENT_DOWNLOAD);
 }

src/sbbs3/ftpsrvr.c
@@ -820,13 +820,15 @@ static void send_thread(void* arg)
 								,getfname(xfer.filename)
 								,prefix
 								,username,tmp); 
-						putsmsg(&scfg,uploader.number,str); 
+						putsmsg(&scfg,uploader.number,str);
 					}
 				}
 			}
 			if(!xfer.tmpfile && !xfer.delfile && !(scfg.dir[f.dir]->misc&DIR_NOSTAT))
 				inc_download_stats(&scfg, 1, (ulong)total);
-		}	
+
+			mqtt_file_download(&mqtt, xfer.user, &f, total, xfer.client);
+		}
 
 		if(xfer.credits) {
 			user_downloaded(&scfg, xfer.user, 1, total);
@@ -1104,6 +1106,8 @@ static void receive_thread(void* arg)
 			}
 			if(!(scfg.dir[f.dir]->misc&DIR_NOSTAT))
 				inc_upload_stats(&scfg, 1, (ulong)total);
+
+			mqtt_file_upload(&mqtt, xfer.user, &f, total, xfer.client);
 		}
 		/* Send ACK */
 		sockprintf(xfer.ctrl_sock,sess,"226 Upload complete (%lu cps).",cps);
@@ -2561,6 +2565,7 @@ static void ctrl_thread(void* arg)
 			SAFECOPY(user.ipaddr,host_ip);
 			user.logontime=(time32_t)logintime;
 			putuserdat(&scfg, &user);
+			mqtt_user_login(&mqtt, &client);
 
 #ifdef _WIN32
 			if(startup->sound.login[0] && !sound_muted(&scfg)) 
@@ -4808,6 +4813,7 @@ static void ctrl_thread(void* arg)
 		/* Update User Statistics */
 		if(!logoutuserdat(&scfg, &user, time(NULL), logintime))
 			lprintf(LOG_ERR,"%04d <%s> !ERROR in logoutuserdat", sock, user.alias);
+		mqtt_user_logout(&mqtt, &client, logintime);
 		lprintf(LOG_INFO,"%04d <%s> logged off", sock, user.alias);
 #ifdef _WIN32
 		if(startup->sound.logout[0] && !sound_muted(&scfg)) 

src/sbbs3/logfile.cpp
@@ -32,7 +32,7 @@ extern "C" BOOL hacklog(scfg_t* cfg, struct mqtt* mqtt, const char* prot, const 
 	char	fname[MAX_PATH+1];
 	FILE*	fp;
 	char	ip[INET6_ADDRSTRLEN];
-	time32_t now=time32(NULL);
+	time_t	now = time(NULL);
 
 	SAFEPRINTF(fname, "%shack.log", cfg->logs_dir);
 
@@ -43,7 +43,7 @@ extern "C" BOOL hacklog(scfg_t* cfg, struct mqtt* mqtt, const char* prot, const 
 	fprintf(fp,"SUSPECTED %s HACK ATTEMPT for user '%s' on %.24s%sUsing port %u at %s [%s]%s"
 		,prot
 		,user
-		,timestr(cfg,now,tstr)
+		,timestr(cfg, (time32_t)now, tstr)
 		,log_line_ending
 		,inet_addrport(addr)
 		,host
@@ -56,11 +56,13 @@ extern "C" BOOL hacklog(scfg_t* cfg, struct mqtt* mqtt, const char* prot, const 
 	fcloselog(fp);
 
 	if(mqtt != NULL) {
+		char topic[128];
 		char str[1024];
 		if(text == NULL)
 			text= "";
-		snprintf(str, sizeof(str), "%s\t%s\t%u\t%s\t%s\t%s", prot, user, inet_addrport(addr), host, ip, text);
-		mqtt_pub_strval(mqtt, TOPIC_BBS, "hack", str);
+		snprintf(str, sizeof(str), "%s\t%u\t%s\t%s\t%s", user, inet_addrport(addr), host, ip, text);
+		snprintf(topic, sizeof(topic), "hack/%s", prot);
+		mqtt_pub_timestamped_msg(mqtt, TOPIC_BBS_ACTION, topic, now, str);
 	}
 
 	return true;
@@ -79,7 +81,7 @@ extern "C" BOOL spamlog(scfg_t* cfg, struct mqtt* mqtt, char* prot, char* action
 	char	tstr[64];
 	char	fname[MAX_PATH+1];
 	FILE*	fp;
-	time32_t now=time32(NULL);
+	time_t	now = time(NULL);
 
 	SAFEPRINTF(fname, "%sspam.log", cfg->logs_dir);
 
@@ -97,7 +99,7 @@ extern "C" BOOL spamlog(scfg_t* cfg, struct mqtt* mqtt, char* prot, char* action
 	fprintf(fp, "SUSPECTED %s SPAM %s on %.24s%sHost: %s [%s]%sFrom: %.128s %s%s"
 		,prot
 		,action
-		,timestr(cfg,now,tstr)
+		,timestr(cfg, (time32_t)now, tstr)
 		,log_line_ending
 		,host
 		,ip_addr
@@ -113,10 +115,12 @@ extern "C" BOOL spamlog(scfg_t* cfg, struct mqtt* mqtt, char* prot, char* action
 
 	if(mqtt != NULL) {
 		char str[1024];
+		char topic[128];
 		if(reason == NULL)
 			reason = (char*)"";
-		snprintf(str, sizeof(str), "%s\t%s\t%s\t%s\t%s\t%s\t%s", prot, action, host, ip_addr, from, to_user, reason);
-		mqtt_pub_strval(mqtt, TOPIC_BBS, "spam", str);
+		snprintf(str, sizeof(str), "%s\t%s\t%s\t%s\t%s\t%s", prot, host, ip_addr, from, to_user, reason);
+		snprintf(topic, sizeof(topic), "spam/%s", action);
+		mqtt_pub_timestamped_msg(mqtt, TOPIC_BBS_ACTION, topic, now, str);
 	}
 
 	return true;

src/sbbs3/logon.cpp
@@ -431,6 +431,8 @@ bool sbbs_t::logon()
 	getmsgptrs();
 	sys_status|=SS_USERON;          /* moved from further down */
 
+	mqtt_user_login(mqtt, &client);
+
 	if(useron.rest&FLAG('Q')) {
 		safe_snprintf(str, sizeof(str), "(%04u)  %-25s  QWK Network Connection"
 			,useron.number,useron.alias);

src/sbbs3/logout.cpp
@@ -153,6 +153,8 @@ void sbbs_t::logout()
 		PlaySound(startup->sound.logout, NULL, SND_ASYNC|SND_FILENAME);
 #endif
 
+	mqtt_user_logout(mqtt, &client, logontime);
+
 	lprintf(LOG_DEBUG, "logout completed");
 }
 

src/sbbs3/mailsrvr.c
@@ -1366,6 +1366,8 @@ static void pop3_thread(void* arg)
 		if(startup->sound.login[0] && !sound_muted(&scfg)) 
 			PlaySound(startup->sound.login, NULL, SND_ASYNC|SND_FILENAME);
 #endif
+		mqtt_user_login(&mqtt, &client);
+
 		SAFEPRINTF(smb.file,"%smail",scfg.data_dir);
 		if(smb_islocked(&smb)) {
 			lprintf(LOG_WARNING,"%04d %s <%s> !MAIL BASE LOCKED: %s",socket, client.protocol, user.alias, smb.last_error);
@@ -1737,6 +1739,7 @@ static void pop3_thread(void* arg)
 #endif
 			if(!logoutuserdat(&scfg,&user,time(NULL),client.time))
 				lprintf(LOG_ERR,"%04d %s <%s> !ERROR in logoutuserdat", socket, client.protocol, user.alias);
+			mqtt_user_logout(&mqtt, &client, client.time);
 		}
 
 	} while(0);

commit 553e9229faa73c9f16dfed5d0d74a74a3ef55331

Fix "running external" log message format mistake
Introduced in commit 73e5dd9c, reported by Codefenix

src/sbbs3/xtrn_sec.cpp
@@ -1372,7 +1372,9 @@ bool sbbs_t::exec_xtrn(uint xtrnnum, bool user_event)
 	xtrndat(name,dropdir,cfg.xtrn[xtrnnum]->type,tleft,cfg.xtrn[xtrnnum]->misc);
 	if(!online)
 		return(false);
-	snprintf(str, sizeof(str), "running external %s: %s", cfg.xtrn[xtrnnum]->name, user_event ? "user event" : "program");
+	snprintf(str, sizeof(str), "running external %s: %s"
+		,user_event ? "user event" : "program"
+		,cfg.xtrn[xtrnnum]->name);
 	logline("X-",str);
 	if(cfg.xtrn[xtrnnum]->cmd[0]!='*' && logfile_fp!=NULL) {
 		fclose(logfile_fp);