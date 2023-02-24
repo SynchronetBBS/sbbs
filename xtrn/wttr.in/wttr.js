d5d582e07b2d4f852bb37409141d8bf82f50defe
echicken <echicken@bbs.electronicchicken.com>
2023-02-24 07:17:27 +0000

    Command line arguments no longer supported, use modopts instead.
    See readme.txt for details on modopts.d/wttr.ini.

--- xtrn/wttr.in/wttr.js
+++ xtrn/wttr.in/wttr.js
@@ -1,25 +1,8 @@
 require('sbbsdefs.js', 'P_UTF8');
 const wttr = load({}, js.exec_dir + 'wttr-lib.js');
 
-function parseArgs() {
-	const ret = {
-		qs: 'https://wttr.in/?AFn',
-		ttl: 0, // Seconds
-	};
-	for (var n = 0; n < argc; n++) {
-		const arg = parseInt(argv[n], 10);
-		if (isNaN(arg)) {
-			ret.qs = arg;
-		} else {
-			ret.ttl = arg;
-		}
-	}
-	return ret;
-}
-
 function main() {
-	const args = parseArgs();
-	const ansi = wttr.getWeather(args.qs, args.ttl);
+	const ansi = wttr.getWeather();
 	const attr = console.attributes;
 	console.clear(BG_BLACK|LIGHTGRAY);
 	console.putmsg(ansi, P_UTF8);
