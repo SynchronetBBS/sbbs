ab33ab91b58690a845fab95f750c080ece9a12ce
echicken <echicken@bbs.electronicchicken.com>
2023-02-24 16:21:30 +0000

    Allow command line arguments to override modopts values.
    Parameter names are -- prefixed, with a space between name and
    value. Quotes etc. around values are not handled; use escapes.
    We can get fancier with this later if it's necessary.
    
    Example:
    
    ?wttr.js --fallback_location Toronto --cache_ttl 0

--- xtrn/wttr.in/wttr-lib.js
+++ xtrn/wttr.in/wttr-lib.js
@@ -2,21 +2,36 @@ require('http.js', 'HTTPRequest');
 const locator = load({}, js.exec_dir + 'locator.js');
 const xterm = load({}, js.exec_dir + 'xterm-colors.js');
 
-function loadSettings() {
-	const settings = load({}, 'modopts.js', 'wttr.in') || {};
-	if (settings.base_url === undefined) {
-		settings.base_url = 'https://wttr.in/';
-	} else if (settings.base_url.search(/\/$/) < 0) {
-		settings.base_url += '/';
+function parseArgs(settings, argv) {
+	if (argv.length < 2) return settings;
+	settings.fallback_ip = '';
+	const keys = Object.keys(settings);
+	for (var k = 0; k < keys.length && argv.length >= 2; k++) {
+		const a = argv.shift().replace(/^--/, '');
+		const v = argv.shift();
+		if (settings[a] === undefined) log(LOG_WARNING, 'wttr.in: Invalid parameter ' + a);
+		if (typeof settings[a] === 'number') {
+			const n = parseInt(v, 10);
+			if (isNaN(n)) throw new Error(a + ' must be a number, got ' + (typeof v) + ' v');
+			settings[a] = n;
+		} else {
+			settings[a] = v;
+		}
 	}
+	if (settings.fallback_ip === '') delete settings.fallback_ip;
+	return settings;
+}
+
+function loadSettings(argv) {
+	var settings = load({}, 'modopts.js', 'wttr.in') || {};
+	if (settings.base_url === undefined) settings.base_url = 'https://wttr.in/';
 	if (settings.units === undefined) settings.units = '';
 	if (settings.view === undefined) settings.view = 'AFn';
 	if (settings.cache_ttl === undefined) settings.cache_ttl = 3600;
-	if (settings.fallback_location === undefined) {
-		settings.fallback_location = '';
-	} else {
-		settings.fallback_location = settings.fallback_location.replace(/\s/g, '+');
-	}
+	if (settings.fallback_location === undefined) settings.fallback_location = '';
+	settings = parseArgs(settings, argv);
+	if (settings.base_url.search(/\/$/) < 0) settings.base_url += '/';
+	settings.fallback_location = settings.fallback_location.replace(/\s/g, '+');
 	return settings;
 }
 
@@ -63,8 +78,8 @@ function getURL(settings, addr) {
 	return url;
 }
 
-function getWeather() {
-	const settings = loadSettings();
+function getWeather(argv) {
+	const settings = loadSettings(argv);
 	const addr = locator.getAddress() || settings.fallback_ip;
 	const url = getURL(settings, addr);
 	if (settings.cache_ttl > 0) {
