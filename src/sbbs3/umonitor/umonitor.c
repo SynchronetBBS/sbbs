354b2c11b05b0e0c38091936e0ea8fe414e7d2bb
Rob Swindell (on ChromeOS) <rob@synchro.net>
2023-03-26 13:00:59 -0700

    Check the return value of chdir() - fixes CID 451056

--- src/sbbs3/umonitor/umonitor.c
+++ src/sbbs3/umonitor/umonitor.c
@@ -869,7 +869,10 @@ int main(int argc, char** argv)  {
 	if(fp!=NULL)
 		fclose(fp);
 
-	chdir(bbs_startup.ctrl_dir);
+	if(chdir(bbs_startup.ctrl_dir) != 0) {
+		printf("Error %d changing directory to: %s\n", errno, bbs_startup.ctrl_dir);
+		exit(1);
+	}
 
 	/* Read .cfg files here */
 	memset(&cfg,0,sizeof(cfg));
