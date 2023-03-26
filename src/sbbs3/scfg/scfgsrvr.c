1f06fc852479868b808aa24c433b4d9f55dfba1c
Rob Swindell (on ChromeOS) <rob@synchro.net>
2023-03-25 18:42:15 -0700

    Add and use sbbs_free_ini() to free allocated portions of startup structures
    Remove values, not keys, when setting a key with a global default.
    
    Use UInt16 functions for getting/setting TCP ports (this fixes an issue with
    the FTP PasvHigh port being set to -1 (instead of 65535).
    
    Use iniGetUInteger instead of iniGetShortInt for most other key values.
    
    First sbbs.ini save support in SCFG, still experimental.

--- src/sbbs3/scfg/scfgsrvr.c
+++ src/sbbs3/scfg/scfgsrvr.c
@@ -32,12 +32,17 @@ static const char* threshold(uint val)
 	return str;
 }
 
-static const char* duration(uint val)
+static const char* duration(uint val, bool verbose)
 {
 	static char str[128];
 	if(val == 0)
 		return strDisabled;
-	return duration_to_vstr(val, str, sizeof(str));
+	return verbose ? duration_to_vstr(val, str, sizeof(str)) : duration_to_str(val, str, sizeof(str));;
+}
+
+static const char* vduration(uint val)
+{
+	return duration(val, true);
 }
 
 static const char* maximum(uint val)
@@ -54,6 +59,7 @@ static void global_cfg(global_startup_t* startup)
 	static int cur;
 	char str[256];
 	char tmp[256];
+	uint32_t ip4_addr;
 
 	while(1) {
 		int i = 0;
@@ -62,7 +68,7 @@ static void global_cfg(global_startup_t* startup)
 		sprintf(opt[i++], "%-40s%s", "Network Interfaces (IPv4/6)", strListCombine(startup->interfaces, tmp, sizeof(tmp), ", "));
 		sprintf(opt[i++], "%-40s%s", "Outbound Interface (IPv4)", IPv4AddressToStr(startup->outgoing4.s_addr, tmp, sizeof(tmp)));
 		sprintf(opt[i++], "%-40s%s", "Bind Retry Count", threshold(startup->bind_retry_count));
-		sprintf(opt[i++], "%-40s%s", "Bind Retry Delay", duration(startup->bind_retry_delay));
+		sprintf(opt[i++], "%-40s%s", "Bind Retry Delay", vduration(startup->bind_retry_delay));
 		sprintf(opt[i++], "%-40s%u ms", "Failed Login Delay", startup->login_attempt.delay);
 		sprintf(opt[i++], "%-40s%u ms", "Failed Login Throttle", startup->login_attempt.throttle);
 		sprintf(opt[i++], "%-40s%s", "Failed Login Hack Log Threshold", threshold(startup->login_attempt.hack_threshold));
@@ -75,7 +81,7 @@ static void global_cfg(global_startup_t* startup)
 		sprintf(opt[i++], "%-40s%u ticks", "JavaScript GC Interval ", startup->js.gc_interval);
 		sprintf(opt[i++], "%-40s%u ticks", "JavaScript Yield Interval", startup->js.yield_interval);
 		sprintf(opt[i++], "%-40s%s", "JavaScript Load Path", startup->js.load_path);
-		sprintf(opt[i++], "%-40s%s", "Semaphore File Check Interval", duration(startup->sem_chk_freq));
+		sprintf(opt[i++], "%-40s%s", "Semaphore File Check Interval", vduration(startup->sem_chk_freq));
 		opt[i][0] = '\0';
 
 		uifc.helpbuf=
@@ -104,16 +110,49 @@ static void global_cfg(global_startup_t* startup)
 				break;
 			case 2:
 				strListCombine(startup->interfaces, str, sizeof(str), ", ");
-				if(uifc.input(WIN_MID|WIN_SAV, 0, 0, "", str, sizeof(str)-1, K_NONE|K_EDIT) >= 0) {
+				if(uifc.input(WIN_MID|WIN_SAV, 0, 0, "Network Interfaces", str, sizeof(str)-1, K_EDIT) >= 0) {
 					strListFree(&startup->interfaces);
 					strListSplitCopy(&startup->interfaces, str, ", ");
 					uifc.changes = true;
 				}
 				break;
 			case 3:
-				IPv4AddressToStr(startup->outgoing4.s_addr, str, sizeof(str));
-				if(uifc.input(WIN_MID|WIN_SAV, 0, 0, "Outbound Network Interface", str, sizeof(str)-1, K_NONE|K_EDIT) > 0)
-					startup->outgoing4.s_addr = parseIPv4Address(str);
+				IPv4AddressToStr(ip4_addr = startup->outgoing4.s_addr, str, sizeof(str));
+				if(uifc.input(WIN_MID|WIN_SAV, 0, 0, "Outbound Network Interface", str, sizeof(str)-1, K_EDIT) > 0) {
+					if((ip4_addr = parseIPv4Address(str)) != startup->outgoing4.s_addr) {
+						startup->outgoing4.s_addr = ip4_addr;
+						uifc.changes = true;
+					}
+				}
+				break;
+			case 4:
+				SAFECOPY(str, threshold(startup->bind_retry_count));
+				if(uifc.input(WIN_MID|WIN_SAV, 0, 0, "Port Bind Retry Count", str, 6, K_EDIT) > 0) {
+					if(atoi(str) != startup->bind_retry_count) {
+						startup->bind_retry_count = atoi(str);
+						uifc.changes = true;
+					}
+				}
+				break;
+			case 5:
+				SAFECOPY(str, duration(startup->bind_retry_delay, false));
+				if(uifc.input(WIN_MID|WIN_SAV, 0, 0, "Port Bind Retry Delay", str, 6, K_EDIT) > 0) {
+					uint dur = parse_duration(str);
+					if(dur != startup->bind_retry_delay) {
+						startup->bind_retry_delay = dur;
+						uifc.changes = true;
+					}
+				}
+				break;
+			case 6:
+				SAFEPRINTF(str, "%u", startup->login_attempt.delay);
+				if(uifc.input(WIN_MID|WIN_SAV, 0, 0, "Millisecond Delay After Failed Login Attempts", str, 6, K_NUMBER|K_EDIT) > 0) {
+					uint dur = atoi(str);
+					if(dur != startup->login_attempt.delay) {
+						startup->login_attempt.delay = dur;
+						uifc.changes = true;
+					}
+				}
 				break;
 		}
 	}
@@ -134,7 +173,7 @@ static void termsrvr_cfg(BOOL* enabled, bbs_startup_t* startup)
 		sprintf(opt[i++], "%-30s%s", "SSH Support", startup->options & BBS_OPT_ALLOW_SSH ? "Yes" : "No");
 		sprintf(opt[i++], "%-30s%s", "SSH Interfaces", startup->options & BBS_OPT_ALLOW_SSH ? strListCombine(startup->ssh_interfaces, tmp, sizeof(tmp), ", ") : "N/A");
 		sprintf(opt[i++], "%-30s%u", "SSH Port", startup->ssh_port);
-		sprintf(opt[i++], "%-30s%s", "SSH Connect Timeout", startup->options & BBS_OPT_ALLOW_SSH ? duration(startup->ssh_connect_timeout) : "N/A");
+		sprintf(opt[i++], "%-30s%s", "SSH Connect Timeout", startup->options & BBS_OPT_ALLOW_SSH ? vduration(startup->ssh_connect_timeout) : "N/A");
 		sprintf(opt[i++], "%-30s%s", "Telnet Support", startup->options & BBS_OPT_NO_TELNET ? "No" : "Yes");
 		sprintf(opt[i++], "%-30s%s", "Telnet Interfaces", startup->options & BBS_OPT_NO_TELNET ? "N/A" : strListCombine(startup->telnet_interfaces, tmp, sizeof(tmp), ", "));
 		sprintf(opt[i++], "%-30s%u", "Telnet Port", startup->telnet_port);
@@ -146,9 +185,9 @@ static void termsrvr_cfg(BOOL* enabled, bbs_startup_t* startup)
 		sprintf(opt[i++], "%-30s%u", "40 Column PETSCII Port", startup->pet40_port);
 		sprintf(opt[i++], "%-30s%u", "80 Column PETSCII Port", startup->pet80_port);
 		sprintf(opt[i++], "%-30s%s", "Max Concurrent Connections", maximum(startup->max_concurrent_connections));
-		sprintf(opt[i++], "%-30s%s", "Max Login Inactivity", duration(startup->max_login_inactivity));
-		sprintf(opt[i++], "%-30s%s", "Max New User Inactivity", duration(startup->max_newuser_inactivity));
-		sprintf(opt[i++], "%-30s%s", "Max User Inactivity", duration(startup->max_session_inactivity));
+		sprintf(opt[i++], "%-30s%s", "Max Login Inactivity", vduration(startup->max_login_inactivity));
+		sprintf(opt[i++], "%-30s%s", "Max New User Inactivity", vduration(startup->max_newuser_inactivity));
+		sprintf(opt[i++], "%-30s%s", "Max User Inactivity", vduration(startup->max_session_inactivity));
 		sprintf(opt[i++], "%-30s%u ms", "Output Buffer Drain Timeout", startup->outbuf_drain_timeout);
 		sprintf(opt[i++], "%-30s%s", "Execute Timed Events", startup->options & BBS_OPT_NO_EVENTS ? "No" : "Yes");
 		sprintf(opt[i++], "%-30s%s", "Execute QWK-relatd Events", startup->options & BBS_OPT_NO_EVENTS ? "N/A" : startup->options & BBS_OPT_NO_QWK_EVENTS ? "No" : "Yes");
@@ -203,7 +242,7 @@ static void websrvr_cfg(BOOL* enabled, web_startup_t* startup)
 		sprintf(opt[i++], "%-30s%s", "Virtual Host Support", startup->options & WEB_OPT_VIRTUAL_HOSTS ? "Yes" : "No");
 		sprintf(opt[i++], "%-30s%s", "Access Logging", startup->options & WEB_OPT_HTTP_LOGGING ? startup->logfile_base : strDisabled);
 		sprintf(opt[i++], "%-30s%s", "Max Clients", maximum(startup->max_clients));
-		sprintf(opt[i++], "%-30s%s", "Max Inactivity", duration(startup->max_inactivity));
+		sprintf(opt[i++], "%-30s%s", "Max Inactivity", vduration(startup->max_inactivity));
 		sprintf(opt[i++], "%-30s%s", "Filebase Index Script", startup->file_index_script);
 		sprintf(opt[i++], "%-30s%s", "Filebase VPath Prefix", startup->file_vpath_prefix);
 		sprintf(opt[i++], "%-30s%s", "Filebase VPath for VHosts", startup->file_vpath_for_vhosts ? "Yes" : "No");
@@ -216,7 +255,7 @@ static void websrvr_cfg(BOOL* enabled, web_startup_t* startup)
 			sprintf(opt[i++], "%-30s%s", "CGI Directory", startup->cgi_dir);
 			sprintf(opt[i++], "%-30s%s", "CGI File Extensions", strListCombine(startup->cgi_ext, tmp, sizeof(tmp), ", "));
 			sprintf(opt[i++], "%-30s%s", "CGI Default Content-Type", startup->default_cgi_content);
-			sprintf(opt[i++], "%-30s%s", "CGI Max Inactivity", duration(startup->max_cgi_inactivity));
+			sprintf(opt[i++], "%-30s%s", "CGI Max Inactivity", vduration(startup->max_cgi_inactivity));
 		}
 		if(!*enabled)
 			i = 1;
@@ -255,9 +294,9 @@ static void ftpsrvr_cfg(BOOL* enabled, ftp_startup_t* startup)
 		sprintf(opt[i++], "%-30s%u - %u", "Passive Port Range", startup->pasv_port_low, startup->pasv_port_high);
 		sprintf(opt[i++], "%-30s%s", "Auto-generate Index File", startup->options & FTP_OPT_INDEX_FILE ? startup->index_file_name : strDisabled);
 		sprintf(opt[i++], "%-30s%s", "QWK Message Packet Transfers", startup->options & FTP_OPT_ALLOW_QWK ? "Yes" : "No");
-		sprintf(opt[i++], "%-30s%s", "QWK Message Packet Timeout", startup->options & FTP_OPT_ALLOW_QWK ? duration(startup->qwk_timeout) : "N/A");
+		sprintf(opt[i++], "%-30s%s", "QWK Message Packet Timeout", startup->options & FTP_OPT_ALLOW_QWK ? vduration(startup->qwk_timeout) : "N/A");
 		sprintf(opt[i++], "%-30s%s", "Max Clients", maximum(startup->max_clients));
-		sprintf(opt[i++], "%-30s%s", "Max Inactivity", duration(startup->max_inactivity));
+		sprintf(opt[i++], "%-30s%s", "Max Inactivity", vduration(startup->max_inactivity));
 		sprintf(opt[i++], "%-30s%s", "Max Concurrent Connections", maximum(startup->max_concurrent_connections));
 		sprintf(opt[i++], "%-30s%s", "Sysop Filesystem Access", startup->options & FTP_OPT_NO_LOCAL_FSYS ? "Yes" : "No");
 		sprintf(opt[i++], "%-30s%s", "Allow Bounce Transfers", startup->options & FTP_OPT_ALLOW_BOUNCE ? "Yes" : "No");
@@ -305,7 +344,7 @@ static void mailsrvr_cfg(BOOL* enabled, mail_startup_t* startup)
 		sprintf(opt[i++], "%-30s%s", "POP3/TLS Support", startup->options & MAIL_OPT_TLS_POP3 ? "Yes" : "No");
 		sprintf(opt[i++], "%-30s%u", "POP3/TLS Port", startup->pop3s_port);
 		sprintf(opt[i++], "%-30s%s", "Max Clients", maximum(startup->max_clients));
-		sprintf(opt[i++], "%-30s%s", "Max Inactivity", duration(startup->max_inactivity));
+		sprintf(opt[i++], "%-30s%s", "Max Inactivity", vduration(startup->max_inactivity));
 		sprintf(opt[i++], "%-30s%s", "Max Concurrent Connections", maximum(startup->max_concurrent_connections));
 		sprintf(opt[i++], "%-30s%s", "Max Recipients Per Message", maximum(startup->max_recipients));
 		sprintf(opt[i++], "%-30s%s", "Max Messages Waiting", maximum(startup->max_msgs_waiting));
@@ -345,8 +384,8 @@ static void mailsrvr_cfg(BOOL* enabled, mail_startup_t* startup)
 				p = "None";
 			sprintf(opt[i++], "%-30s%s", "SendMail Relay Auth", applicable ? p : "N/A");
 			sprintf(opt[i++], "%-30s%u", "SendMail Max Attempts", startup->max_delivery_attempts);
-			sprintf(opt[i++], "%-30s%s", "SendMail Rescan Interval", duration(startup->rescan_frequency));
-			sprintf(opt[i++], "%-30s%s", "SendMail Connect Timeout", duration(startup->connect_timeout));
+			sprintf(opt[i++], "%-30s%s", "SendMail Rescan Interval", vduration(startup->rescan_frequency));
+			sprintf(opt[i++], "%-30s%s", "SendMail Connect Timeout", vduration(startup->connect_timeout));
 		}
 		if(!*enabled)
 			i = 1;
@@ -411,7 +450,7 @@ void server_cfg(void)
 	services_startup_t services_startup = {0};
 
 	sbbs_get_ini_fname(cfg.filename, cfg.ctrl_dir);
-	FILE* fp = fopen(cfg.filename, "r");
+	FILE* fp = iniOpenFile(cfg.filename, /* for_modify? */false);
 	if(fp == NULL) {
 		uifc.msgf("Error opening %s", cfg.filename);
 		return;
@@ -431,7 +470,7 @@ void server_cfg(void)
 		,&run_services
 		,&services_startup
 		);
-	fclose(fp);
+	iniCloseFile(fp);
 
 	while(1) {
 		int i = 0;
@@ -474,6 +513,39 @@ void server_cfg(void)
 				services_cfg(&run_services, &services_startup);
 				break;
 			default:
+				i = save_changes(WIN_MID);
+				if(i < 0)
+					continue;
+				if(i == 0) {
+					fp = iniOpenFile(cfg.filename, /* for_modify? */true);
+					if(fp == NULL)
+						uifc.msgf("Error opening %s", cfg.filename);
+					else {
+						if(!sbbs_write_ini(
+							 fp
+							,&cfg
+							,&global_startup
+							,run_bbs
+							,&bbs_startup
+							,run_ftp
+							,&ftp_startup
+							,run_web
+							,&web_startup
+							,run_mail
+							,&mail_startup
+							,run_services
+							,&services_startup
+							))
+							uifc.msgf("Error writing %s", cfg.filename);
+						iniCloseFile(fp);
+					}
+				}
+				sbbs_free_ini(&global_startup
+					,&bbs_startup
+					,&ftp_startup
+					,&web_startup
+					,&mail_startup
+					,&services_startup);
 				return;
 		}
 	}
