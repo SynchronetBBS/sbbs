80a94b2af0d1ad6853ba2c9aca733981e469d383
Rob Swindell (on Windows) <rob@synchro.net>
2023-08-09 18:23:50 -0700

    Changes in comments only: copyright header block, typo

--- src/sbbs3/nodedefs.h
+++ src/sbbs3/nodedefs.h
@@ -1,8 +1,4 @@
-/* nodedefs.h */
-
 /* Synchronet node information structure and constant definitions */
-
-/* $Id: nodedefs.h,v 1.19 2020/08/01 22:04:03 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -17,20 +13,8 @@
  * See the GNU General Public License for more details: gpl.txt or			*
  * http://www.fsf.org/copyleft/gpl.html										*
  *																			*
- * Anonymous FTP access to the most recent released source is available at	*
- * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
- *																			*
- * Anonymous CVS access to the development source and modification history	*
- * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
- *     (just hit return, no password is necessary)							*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src		*
- *																			*
  * For Synchronet coding style and modification guidelines, see				*
  * http://www.synchro.net/source.html										*
- *																			*
- * You are encouraged to submit any modifications (preferably in Unix diff	*
- * format) via e-mail to mods@synchro.net									*
  *																			*
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
@@ -124,7 +108,7 @@ typedef struct {					/* Node information kept in node.dab */
 #define NODE_CONNECTION_SSH				(USHRT_MAX-2)
 #define NODE_CONNECTION_RAW				(USHRT_MAX-3)
 				misc,					/* Miscellaneous bits for node */
-				aux;					/* Auxillary word for node */
+				aux;					/* Auxiliary word for node */
     uint32_t   extaux;					/* Extended aux dword for node */
 	} node_t;
 
