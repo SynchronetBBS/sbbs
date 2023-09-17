c35835f5d847480864c067cb674af4af4f62c026
Rob Swindell (on Windows) <rob@synchro.net>
2023-09-16 23:23:47 -0700

    The first argument to sbbs_t::uselect() is actually a boolean

--- src/sbbs3/sbbs.h
+++ src/sbbs3/sbbs.h
@@ -958,7 +958,7 @@ public:
 	bool	random_menu(const char *code, int mode = 0, JSObject* obj = NULL);
 	bool	menu_exists(const char *code, const char* ext=NULL, char* realpath=NULL);
 
-	int		uselect(int add, uint n, const char *title, const char *item, const uchar *ar);
+	int		uselect(bool add, uint n, const char *title, const char *item, const uchar *ar);
 	uint	uselect_total = 0, uselect_num[500]{};
 
 	int		mselect(const char *title, str_list_t list, unsigned max_selections, const char* item_fmt, const char* selected_str, const char* unselected_str, const char* prompt_fmt);
