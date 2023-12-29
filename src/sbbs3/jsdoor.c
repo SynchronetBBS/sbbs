8eda65ac676f285a871fe90de5c2cc4fb0ad7fbe
Rob Swindell (on Debian Linux) <rob@synchro.net>
2023-12-28 21:12:34 -0800

    Fix jsdoor build for recent MQTT JS integration

--- src/sbbs3/jsdoor.c
+++ src/sbbs3/jsdoor.c
@@ -1,8 +1,4 @@
-/* jsdoor.c */
-
 /* Execute a BBS JavaScript module from the command-line */
-
-/* $Id: jsdoor.c,v 1.8 2019/08/20 23:14:30 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -17,20 +13,8 @@
  * See the GNU General Public License for more details: gpl.txt or			*
  * http://www.fsf.org/copyleft/gpl.html										*
  *																			*
- * Anonymous FTP access to the most recent released source is available at	*
- * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
- *																			*
- * Anonymous CVS access to the development source and modification history	*
- * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
- *     (just hit return, no password is necessary)							*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src		*
- *																			*
  * For Synchronet coding style and modification guidelines, see				*
  * http://www.synchro.net/source.html										*
- *																			*
- * You are encouraged to submit any modifications (preferably in Unix diff	*
- * format) via e-mail to mods@synchro.net									*
  *																			*
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
@@ -244,6 +228,7 @@ BOOL DLLCALL js_CreateCommonObjects(JSContext* js_cx
 										,CRYPT_CONTEXT session		/* client */
 										,js_server_props_t* props	/* server */
 										,JSObject** glob
+										,struct mqtt* mqtt
 										)
 {
 	BOOL	success=FALSE;
@@ -257,7 +242,7 @@ BOOL DLLCALL js_CreateCommonObjects(JSContext* js_cx
 
 	do {
 		/* System Object */
-		if(js_CreateSystemObject(js_cx, *glob, &scfg, uptime, host_name, socklib_desc)==NULL)
+		if(js_CreateSystemObject(js_cx, *glob, &scfg, uptime, host_name, socklib_desc, mqtt)==NULL)
 			break;
 
 		/* Internal JS Object */
