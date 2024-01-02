SynchronetBBS/sbbs

commit e631451e211617b35fd525061868358f9b9fb588

Don't complain about lack of comma in new user's location
... unless a comma is actually required.

src/sbbs3/newuser.cpp
@@ -264,11 +264,13 @@ BOOL sbbs_t::newuser()
 		if(!online) return(FALSE);
 		while((cfg.uq&UQ_LOCATION) && online && text[EnterYourCityState][0]) {
 			bputs(text[EnterYourCityState]);
-			if(getstr(useron.location,LEN_LOCATION,kmode)
-				&& ((cfg.uq&UQ_NOCOMMAS) || strchr(useron.location,',')))
+			if(getstr(useron.location,LEN_LOCATION,kmode) < 1)
+				continue;
+			if((cfg.uq&UQ_NOCOMMAS) && strchr(useron.location,',') == NULL) {
+				bputs(text[CommaInLocationRequired]);
+				useron.location[0]=0;
+			} else
 				break;
-			bputs(text[CommaInLocationRequired]);
-			useron.location[0]=0; 
 		}
 		if(cfg.uq&UQ_ADDRESS)
 			while(online && text[EnterYourZipCode][0]) {

commit 3c84eb4e434b90583566f24f9fcf69eeea3a5ce3

Fix inverted logic for comma-requirement in new user location
This was broken one year ago in Commit e631451e

Thanks for the report (just now) about this bug Max!

src/sbbs3/newuser.cpp
@@ -236,7 +236,7 @@ BOOL sbbs_t::newuser()
 			bputs(text[EnterYourCityState]);
 			if(getstr(useron.location,LEN_LOCATION,kmode) < 1)
 				continue;
-			if((cfg.uq&UQ_NOCOMMAS) && strchr(useron.location,',') == NULL) {
+			if(!(cfg.uq&UQ_NOCOMMAS) && strchr(useron.location,',') == NULL) {
 				bputs(text[CommaInLocationRequired]);
 				useron.location[0]=0;
 			} else