3f9faa658e17215322f7f4a04d0d187645080f70
Deucе <shurd@sasktel.net>
2024-01-18 03:21:00 -0500

    Add rwlock wrappers.
    Recursive read locks, non-recursive write locks.

---
+++ src/xpdev/rwlockwrap.c
@@ -0,0 +1,132 @@
+#include "rwlockwrap.h"
+
+#ifdef _WIN32
+
+bool
+rwlock_init(rwlock_t *lock)
+{
+	InitializeCriticalSection(&lock->lk);
+	InitializeCriticalSection(&lock->wlk);
+	readers = 0;
+	writers = 0;
+	writers_waiting = 0;
+	writer = (DWORD)-1;
+}
+
+bool
+rwlock_rdlock(rwlock_t *lock)
+{
+	DWORD obj;
+
+	EnterCriticalSection(&lock->lk);
+	while(lock->writers || lock->writers_waiting) {
+		LeaveCriticalSection(&lock->lk);
+		// Wait for current writer to release
+		EnterCriticalSection(&lock->wlk);
+		EnterCriticalSection(&lock->lk);
+		if (lock->writers || lock->writers_waiting) {
+			LeaveCriticalSection(&lock->lk);
+			LeaveCriticalSection(&lock->wlk);
+			/*
+			 * Just in case thread scheduling is weird on
+			 * Win32, allow time for a writer to grab wlk
+			 */
+			Sleep(1);
+			EnterCriticalSection(&lock->lk);
+			continue;
+		}
+		else {
+			lock->readers++;
+			LeaveCriticalSection(&lock->lk);
+			LeaveCriticalSection(&lock->wlk);
+			return true;
+		}
+	}
+	lock->readers++;
+	LeaveCriticalSection(&lock->lk);
+	return true;
+}
+
+bool
+rwlock_tryrdlock(rwlock_t *lock)
+{
+	bool ret = false;
+
+	EnterCriticalSection(&lock->lk);
+	if (lock->writers == 0 && lock->writers_waiting == 0) {
+		lock->readers++;
+		ret = true;
+	}
+	LeaveCriticalSection(&lock->lk);
+	return ret;
+}
+
+bool
+rwlock_wrlock(rwlock_t *lock)
+{
+	EnterCriticalSection(&lock->lk);
+	lock->writers_waiting++;
+	LeaveCriticalSection(&lock->lk);
+	EnterCriticalSection(&lock->wlk);
+	EnterCriticalSection(&lock->lk);
+	// No recursion
+	if (lock->writers == 0) {
+		lock->writers_waiting--;
+		lock->writers++;
+		lock->writer = GetCurrentThreadId();
+		LeaveCriticalSection(&lock->lk);
+		// Keep holding wlk
+		return true;
+	}
+	LeaveCriticalSection(&lock->lk);
+	LeaveCriticalSection(&lock->wlk);
+	return false;
+}
+
+bool
+rwlock_trywrlock(rwlock_t *lock)
+{
+	if (TryEnterCriticalSection(&lock->wlk)) {
+		EnterCriticalSection(&lock->lk);
+		// Prevent recursing on writer locks
+		if (lock->writers == 0) {
+			lock->writers++;
+			lock->writer = GetCurrentThreadId();
+			LeaveCriticalSection(&lock->lk);
+			return true;
+		}
+		LeaveCriticalSection(&lock->lk);
+		LeaveCriticalSection(&lock->wlk);
+		return false;
+	}
+	return false;
+}
+
+bool
+rwlock_unlock(rwlock_t *lock)
+{
+	bool ret = false;
+	EnterCriticalSection(&lock->lk);
+	if (lock->writers) {
+		if (lock->writer == GetCurrentThreadId()) {
+			lock->writers--;
+			LeaveCriticalSection(&lock->lk);
+			LeaveCriticalSection(&lock->wlk);
+			return true;
+		}
+		LeaveCriticalSection(&lock->lk);
+		return false;
+	}
+	lock->readers--;
+	return true;
+}
+
+#elif defined(__unix__)
+
+// All macros...
+
+#else
+
+#error no rwlock wrapper for this platform
+
+#endif
--- src/xpdev/rwlockwrap.h
+++ src/xpdev/rwlockwrap.h
@@ -1,9 +1,40 @@
 #ifndef RWLOCKWRAP_H
-
 #define RWLOCKWRAP_H
-#ifdef UNIX
+
+#include <stdbool.h>
+
+#if defined(__unix__)
+
 #include <pthread.h>
-#include <time.h>
+typedef pthread_rwlock_t rwlock_t;
+
+#define rwlock_init(lock) (pthread_rwlock_init(lock, NULL) == 0)
+#define rwlock_rdlock(lock) (pthread_rwlock_rdlock(lock) == 0)
+#define rwlock_tryrdlock(lock) (pthread_rwlock_tryrdlock(lock) == 0)
+#define rwlock_wrlock(lock) (pthread_rwlock_wrlock(lock) == 0)
+#define rwlock_trywrlock(lock) (pthread_rwlock_trywrlock(lock) == 0)
+#define rwlock_unlock(lock) (pthread_rwlock_unlock(lock) == 0)
+
+#elif defined(_WIN32)
+
+#include "threadwrap.h"
+
+typedef struct {
+	CRITICAL_SECTION lk;       // Protects access to all elements
+	CRITICAL_SECTION wlk;      // Locked by an active writer
+	unsigned readers;
+	unsigned writers;
+	unsigned writers_waiting;
+	DWORD writer;
+} rwlock_t;
+
+bool rwlock_init(rwlock_t *lock);
+bool rwlock_rdlock(rwlock_t *lock);
+bool rwlock_tryrdlock(rwlock_t *lock);
+bool rwlock_wrlock(rwlock_t *lock);
+bool rwlock_trywrlock(rwlock_t *lock);
+bool rwlock_unlock(rwlock_t *lock);
+
 #else
 #error Not implemented
 #endif
