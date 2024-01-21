73b3e0d1ceaf6face929bb0c6bd129e608e4c2b7
Rob Swindell (on Windows 11) <rob@synchro.net>
2024-01-21 01:15:16 -0800

    Comment/header block clean-up (e.g. remove CVS references)

--- src/comio/comio_nix.c
+++ src/comio/comio_nix.c
@@ -1,8 +1,4 @@
-/* comio_nix.c */
-
 /* Synchronet Serial Communications I/O Library Functions for *nix */
-
-/* $Id: comio_nix.c,v 1.19 2020/06/27 08:23:39 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4       (Plain Text/Source Code File Header)            *
@@ -17,20 +13,8 @@
  * See the GNU Lesser General Public License for more details: lgpl.txt or  *
  * http://www.fsf.org/copyleft/lesser.html                                  *
  *                                                                          *
- * Anonymous FTP access to the most recent released source is available at  *
- * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net  *
- *                                                                          *
- * Anonymous CVS access to the development source and modification history  *
- * is available at cvs.synchro.net:/cvsroot/sbbs, example:                  *
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login            *
- *     (just hit return, no password is necessary)                          *
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src     *
- *                                                                          *
  * For Synchronet coding style and modification guidelines, see             *
  * http://www.synchro.net/source.html                                       *
- *                                                                          *
- * You are encouraged to submit any modifications (preferably in Unix diff  *
- * format) via e-mail to mods@synchro.net                                   *
  *                                                                          *
  * Note: If this box doesn't appear square, then you need to fix your tabs. *
  ****************************************************************************/
