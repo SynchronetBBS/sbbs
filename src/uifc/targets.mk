41793daec220253da092c7cac0e39455e5e94d76
Deucе <shurd@sasktel.net>
2024-02-06 10:59:17 -0500

    Remove single-threaded library support
    It didn't work anyway.

--- src/uifc/targets.mk
+++ src/uifc/targets.mk
@@ -2,11 +2,10 @@ UIFCTEST	=	$(EXEODIR)$(DIRSEP)uifctest$(EXEFILE)
 UIFCLIB_BUILD	=	$(LIBODIR)$(DIRSEP)$(LIBPREFIX)uifc$(LIBFILE)
 UIFCLIB-MT_BUILD	=	$(LIBODIR)$(DIRSEP)$(LIBPREFIX)uifc_mt$(LIBFILE)
 
-all: lib mtlib test
+all: mtlib test
 
 test: xpdev-mt ciolib-mt mtlib $(EXEODIR) $(UIFCTEST)
 
-lib: $(OBJODIR) $(LIBODIR) $(UIFCLIB_BUILD)
 mtlib: $(MTOBJODIR) $(LIBODIR) $(UIFCLIB-MT_BUILD)
 
 # Library dependencies.
