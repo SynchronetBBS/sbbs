b5b5ea67dc910331b2120645496df6ba039dff3e
Deucе <shurd@sasktel.net>
2024-02-07 20:15:59 -0500

    Fix backslash alignment when 8-position tabs are used.

--- src/sftp/sftp_pkt.c
+++ src/sftp/sftp_pkt.c
@@ -283,12 +283,12 @@ sftp_tx_pkt_reset(sftp_tx_pkt_t *pktp)
 	return true;
 }
 
-#define APPEND_FUNC_BODY(var)                            \
-	if (!grow_tx(pktp, sizeof(var)))                  \
-		return false;                              \
-	sftp_tx_pkt_t pkt = *pktp;                          \
+#define APPEND_FUNC_BODY(var)                                                                     \
+	if (!grow_tx(pktp, sizeof(var)))                                                           \
+		return false;                                                                       \
+	sftp_tx_pkt_t pkt = *pktp;                                                                   \
 	memcpy(&((uint8_t *)pkt)[pkt->used + offsetof(struct sftp_tx_pkt, type)], &var, sizeof(var)); \
-	pkt->used += sizeof(var);                             \
+	pkt->used += sizeof(var);                                                                      \
 	return true
 
 bool
