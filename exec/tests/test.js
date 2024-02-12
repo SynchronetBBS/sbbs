3d61b1297eb4c1080c82be073d77c7c8c0009add
Deucе <shurd@sasktel.net>
2024-02-12 12:41:20 -0500

    More fixins to get error message.

--- exec/tests/test.js
+++ exec/tests/test.js
@@ -79,14 +79,14 @@ function run_tests(location, obj)
 				chdir(js.exec_dir);
 				if (result instanceof Error) {
 					tfailed = true;
-					fail_msg = e.toString();
+					fail_msg = result.toString();
 					log("Caught: "+result);
 				}
 			}
 			catch(e) {
 				tfailed = true;
 				fail_msg = e.toString();
-				log("Caught: "+e);
+				log("Caught: "+fail_msg);
 			}
 			if (tfailed) {
 				stdout.writeln("FAILED!");
