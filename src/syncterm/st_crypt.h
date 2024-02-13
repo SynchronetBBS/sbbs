99b1034bbe1717d7df324bb6ae4a8c8254246486
Deucе <shurd@sasktel.net>
2024-02-12 23:06:59 -0500

    Use C_RET for cryptlib symbols.
    Given how patched up cryptlib is at this point, this is all likely
    to go away and just use the static lib in the nearish future.

--- src/syncterm/st_crypt.h
+++ src/syncterm/st_crypt.h
@@ -12,50 +12,50 @@
  #endif
 
 struct crypt_funcs {
-	int (*PopData)(C_IN CRYPT_HANDLE envelope, C_OUT void C_PTR buffer,
+	C_RET (*PopData)(C_IN CRYPT_HANDLE envelope, C_OUT void C_PTR buffer,
 	    C_IN int length, C_OUT int C_PTR bytesCopied);
-	int (*PushData)(C_IN CRYPT_HANDLE envelope, C_IN void C_PTR buffer,
+	C_RET (*PushData)(C_IN CRYPT_HANDLE envelope, C_IN void C_PTR buffer,
 	    C_IN int length, C_OUT int C_PTR bytesCopied);
-	int (*FlushData)(C_IN CRYPT_HANDLE envelope);
-	int (*Init)(void);
-	int (*End)(void);
-	int (*CreateSession)(C_OUT CRYPT_SESSION C_PTR session,
+	C_RET (*FlushData)(C_IN CRYPT_HANDLE envelope);
+	C_RET (*Init)(void);
+	C_RET (*End)(void);
+	C_RET (*CreateSession)(C_OUT CRYPT_SESSION C_PTR session,
 	    C_IN CRYPT_USER                            cryptUser,
 	    C_IN CRYPT_SESSION_TYPE                    formatType);
-	int (*GetAttribute)(C_IN CRYPT_HANDLE cryptHandle,
+	C_RET (*GetAttribute)(C_IN CRYPT_HANDLE cryptHandle,
 	    C_IN CRYPT_ATTRIBUTE_TYPE         attributeType,
 	    C_OUT int C_PTR                   value);
-	int (*GetAttributeString)(C_IN CRYPT_HANDLE cryptHandle,
+	C_RET (*GetAttributeString)(C_IN CRYPT_HANDLE cryptHandle,
 	    C_IN CRYPT_ATTRIBUTE_TYPE               attributeType,
 	    C_OUT void C_PTR                        value,
 	    C_OUT int C_PTR                         valueLength);
-	int (*SetAttribute)(C_IN CRYPT_HANDLE cryptHandle,
+	C_RET (*SetAttribute)(C_IN CRYPT_HANDLE cryptHandle,
 	    C_IN CRYPT_ATTRIBUTE_TYPE         attributeType,
 	    C_IN int                          value);
-	int (*SetAttributeString)(C_IN CRYPT_HANDLE cryptHandle,
+	C_RET (*SetAttributeString)(C_IN CRYPT_HANDLE cryptHandle,
 	    C_IN CRYPT_ATTRIBUTE_TYPE attributeType,
 	    C_IN void C_PTR value, C_IN int valueLength);
-	int (*DestroySession)(C_IN CRYPT_SESSION session);
-	int (*AddRandom)(C_IN void C_PTR randomData, C_IN int randomDataLength);
-	int (*DeleteAttribute)(C_IN CRYPT_HANDLE cryptHandle,
+	C_RET (*DestroySession)(C_IN CRYPT_SESSION session);
+	C_RET (*AddRandom)(C_IN void C_PTR randomData, C_IN int randomDataLength);
+	C_RET (*DeleteAttribute)(C_IN CRYPT_HANDLE cryptHandle,
 	    C_IN CRYPT_ATTRIBUTE_TYPE            attributeType);
-	int (*KeysetOpen)(C_OUT CRYPT_KEYSET C_PTR keyset,
+	C_RET (*KeysetOpen)(C_OUT CRYPT_KEYSET C_PTR keyset,
             C_IN CRYPT_USER cryptUser,
             C_IN CRYPT_KEYSET_TYPE keysetType,
             C_IN C_STR name, C_IN CRYPT_KEYOPT_TYPE options);
-        int (*KeysetClose)(C_IN CRYPT_KEYSET keyset);
-        int (*GenerateKey)(C_IN CRYPT_CONTEXT cryptContext);
-	int (*AddPrivateKey)(C_IN CRYPT_KEYSET keyset,
+        C_RET (*KeysetClose)(C_IN CRYPT_KEYSET keyset);
+        C_RET (*GenerateKey)(C_IN CRYPT_CONTEXT cryptContext);
+	C_RET (*AddPrivateKey)(C_IN CRYPT_KEYSET keyset,
 	    C_IN CRYPT_HANDLE cryptKey,
 	    C_IN C_STR password );
-	int (*GetPrivateKey)(C_IN CRYPT_KEYSET keyset,
+	C_RET (*GetPrivateKey)(C_IN CRYPT_KEYSET keyset,
 	    C_OUT CRYPT_CONTEXT C_PTR cryptContext,
 	    C_IN CRYPT_KEYID_TYPE keyIDtype,
 	    C_IN C_STR keyID, C_IN_OPT C_STR password );
-	int (*CreateContext)(C_OUT CRYPT_CONTEXT C_PTR cryptContext,
+	C_RET (*CreateContext)(C_OUT CRYPT_CONTEXT C_PTR cryptContext,
 	    C_IN CRYPT_USER cryptUser,
 	    C_IN CRYPT_ALGO_TYPE cryptAlgo);
-	int (*DestroyContext)(C_IN CRYPT_CONTEXT cryptContext);
+	C_RET (*DestroyContext)(C_IN CRYPT_CONTEXT cryptContext);
 };
 
 #endif // ifndef WITHOUT_CRYPTLIB
