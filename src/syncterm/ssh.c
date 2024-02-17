3bd030183f67beaa4bb6e884616001eb61fec757
Deucе <shurd@sasktel.net>
2024-02-17 15:01:56 -0500

    Move sftp_state inside ssh_mutex.
    This won't matter now since only one thread ever touches it, but
    when cool stuff happens, it could.

--- src/syncterm/ssh.c
+++ src/syncterm/ssh.c
@@ -457,11 +457,13 @@ key_not_present(sftp_filehandle_t f, const char *priv)
 				bufsz += 4096;
 			}
 			if (!sftpc_read(sftp_state, f, off, (bufsz - bufpos > 1024) ? 1024 : bufsz - bufpos, &r)) {
+				free(buf);
+				pthread_mutex_lock(&ssh_mutex);
 				if (sftp_state->err_code == SSH_FX_EOF) {
-					free(buf);
+					pthread_mutex_unlock(&ssh_mutex);
 					return true;
 				}
-				free(buf);
+				pthread_mutex_unlock(&ssh_mutex);
 				return false;
 			}
 			memcpy(&buf[bufpos], r->c_str, r->len);
