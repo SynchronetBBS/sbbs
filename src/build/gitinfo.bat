8cdf2800305271eb5defeb65f2a711cb9eae64fb
Rob Swindell (on Debian Linux) <rob@synchro.net>
2024-03-06 18:25:43 -0800

    We need to double-up the percents in batch lingo

--- src/build/gitinfo.bat
+++ src/build/gitinfo.bat
@@ -1,5 +1,5 @@
 @git log -1 HEAD --format="#define GIT_HASH \"%%h\"" > git_hash.h
-@git log -1 HEAD --format="#define GIT_DATE \"%cd\"" '--date=format-local:%b %e %Y %H:%M' >> git_hash.h
+@git log -1 HEAD --format="#define GIT_DATE \"%%cd\"" '--date=format-local:%%b %%e %%Y %%H:%%M' >> git_hash.h
 @echo #define GIT_BRANCH ^"| tr -d "\r\n" > git_branch.h
 @git rev-parse --abbrev-ref HEAD | tr -d "\n" >> git_branch.h
 @echo ^" >> git_branch.h
