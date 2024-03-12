52624e1a4681d4cd3136d36e413d84b978b1a5cf
Rob Swindell (on Debian Linux) <rob@synchro.net>
2024-03-11 18:18:31 -0700

    This file isn't used/useful (no CVS repo anymore, per Nelgin's wishes)

--- install/checkout.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Synchronet source code check-out for Win32
-echo $Id: checkout.bat,v 1.5 2011/10/27 01:23:59 rswindell Exp $
-setlocal
-set HOME=c:\
-set CVSROOT=:pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs
-cvs co src-sbbs3
-cvs co lib-win32
