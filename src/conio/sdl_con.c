dcb4df6f5a4f55623b0f8ed6edc40baedc020324
Deucе <shurd@sasktel.net>
2024-03-17 17:30:43 -0400

    Revert last change reaper is explicitly disabled here.

--- src/conio/sdl_con.c
+++ src/conio/sdl_con.c
@@ -296,10 +296,6 @@ void exit_sdl_con(void)
 	ciolib_reaper = 0;
 	if (sdl_init_good)
 		sdl_user_func_ret(SDL_USEREVENT_QUIT);
-	else {
-		if (ciolib_reaper)
-			exit(0);
-	}
 }
 
 void sdl_copytext(const char *text, size_t buflen)
