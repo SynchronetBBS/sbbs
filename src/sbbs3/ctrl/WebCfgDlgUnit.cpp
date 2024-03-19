c39969851574acf97816cef961c5417c2aeae45f
Rob Swindell (on Windows 11) <rob@synchro.net>
2024-03-18 20:52:44 -0700

    Bugfix: Log->Base Filename edit box wasn't populated or read
    ... so the [web] HttpLogFile setting in sbbs.ini couldn't be set using this
    dialog. Been broken all this time.

--- src/sbbs3/ctrl/WebCfgDlgUnit.cpp
+++ src/sbbs3/ctrl/WebCfgDlgUnit.cpp
@@ -1,7 +1,5 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: WebCfgDlgUnit.cpp,v 1.9 2019/01/12 23:48:32 rswindell Exp $ */
-
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
@@ -15,20 +13,8 @@
  * See the GNU General Public License for more details: gpl.txt or			*
  * http://www.fsf.org/copyleft/gpl.html										*
  *																			*
- * Anonymous FTP access to the most recent released source is available at	*
- * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
- *																			*
- * Anonymous CVS access to the development source and modification history	*
- * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
- *     (just hit return, no password is necessary)							*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src		*
- *																			*
  * For Synchronet coding style and modification guidelines, see				*
  * http://www.synchro.net/source.html										*
- *																			*
- * You are encouraged to submit any modifications (preferably in Unix diff	*
- * format) via e-mail to mods@synchro.net									*
  *																			*
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
@@ -108,6 +94,7 @@ void __fastcall TWebCfgDlg::FormShow(TObject *Sender)
 	DebugRxCheckBox->Checked=MainForm->web_startup.options&WEB_OPT_DEBUG_RX;
     AccessLogCheckBox->Checked=MainForm->web_startup.options&WEB_OPT_HTTP_LOGGING;
     AccessLogCheckBoxClick(Sender);
+	LogBaseNameEdit->Text = AnsiString(MainForm->web_startup.logfile_base);
 
     VirtualHostsCheckBox->Checked=MainForm->web_startup.options&WEB_OPT_VIRTUAL_HOSTS;
 
@@ -150,6 +137,8 @@ void __fastcall TWebCfgDlg::OKBtnClick(TObject *Sender)
         ,CGIDirEdit->Text.c_str());
     SAFECOPY(MainForm->web_startup.ssjs_ext
         ,ServerSideJsExtEdit->Text.c_str());
+	SAFECOPY(MainForm->web_startup.logfile_base
+		,LogBaseNameEdit->Text.c_str());
 
     SAFECOPY(MainForm->web_startup.default_cgi_content
         ,CGIContentEdit->Text.c_str());
