13ca484a712ab44d789d92c8edb6933c0d97f0a0
Deucе <shurd@sasktel.net>
2024-03-22 14:17:41 -0400

    Use strlcpy() for SAFECOPY() and XSAFECOPY() macros.
    While we're here, Update strlcpy() to slightly more optimal
    FreeBSD-13.3 version.

--- src/xpdev/genwrap.c
+++ src/xpdev/genwrap.c
@@ -69,17 +69,26 @@ int safe_snprintf(char *dst, size_t size, const char *fmt, ...)
 #ifdef NEEDS_STRLCPY
 size_t strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t i;
+        const char *osrc = src;
+        size_t nleft = dsize;
 
-	if(size < 1)
-		return 0;
+        /* Copy as many bytes as will fit. */
+        if (nleft != 0) {
+                while (--nleft != 0) {
+                        if ((*dst++ = *src++) == '\0')
+                                break;
+                }
+        }
 
-	for(i = 0; src[i] != '\0'; i++) {
-		if(i < (size - 1))
-			*(dst++) = src[i];
-	}
-	*dst = 0;
-	return i;
+        /* Not enough room in dst, add NUL and traverse rest of src. */
+        if (nleft == 0) {
+                if (dsize != 0)
+                        *dst = '\0';            /* NUL-terminate dst */
+                while (*src++)
+                        ;
+        }
+
+        return(src - osrc - 1); /* count does not include NUL */
 }
 
 size_t
