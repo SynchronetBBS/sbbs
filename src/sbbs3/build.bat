8e1a9aec1f28125f680b33a7f76593e954fec2a9
Rob Swindell (on Windows 11) <rob@synchro.net>
2024-04-27 17:03:13 -0700

    Suppress the XP deprecation warning, as suggested by Yigael

--- src/sbbs3/build.bat
+++ src/sbbs3/build.bat
@@ -2,5 +2,5 @@
 setlocal
 rem *** Requires Microsoft Visual C++ 2022 ***
 call "%VS170COMNTOOLS%\VsMSBuildCmd.bat"
-msbuild sbbs3.sln /p:Platform="Win32" %*
+msbuild sbbs3.sln /p:Platform="Win32" /p:XPDeprecationWarning=false %*
 if errorlevel 1 echo. & echo !ERROR(s) occurred & exit /b 1
