09f5d705afae2851d5146c358a0c8e0ca62c854a
Rob Swindell (on Debian Linux) <rob@synchro.net>
2024-05-27 23:16:29 -0700

    Use the telnet gateway proxy (not pass-through) by default
    Pass-through only really does anything useful if the BBS user's client is a
    Telnet client and it responds as the server expects. Since the user's
    Telnet client (when using one) has already negotiated Telnet options, they
    won't ACK options that the server expects them to, so this doesn't really
    work. The proxy fixes that issue.

--- exec/telgate.js
+++ exec/telgate.js
@@ -7,7 +7,7 @@ load("sbbsdefs.js");
 write("\r\n\1h\1hPress \001yCtrl-]\001w for a control menu anytime.\r\n\r\n");
 console.pause();
 writeln("\001h\001yConnecting to: \001w"+argv[0]+"\001n");
-var flags=TG_PASSTHRU;
+var flags=TG_NONE;
 if(argc>1)
 	flags=eval(argv[1]);
 bbs.telnet_gate(argv[0],flags);
--- exec/unixgate.js
+++ exec/unixgate.js
@@ -9,5 +9,5 @@ load("sbbsdefs.js");
 write("\r\n\1h\1hPress \001yCtrl-]\001w for a control menu anytime.\r\n\r\n");
 console.pause();
 writeln("\001h\001yConnecting to: \001w"+argv[0]+"\001n");
-bbs.telnet_gate(argv[0],TG_PASSTHRU|TG_NOTERMTYPE);
+bbs.telnet_gate(argv[0],TG_NOTERMTYPE);
 console.clear();
