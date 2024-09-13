SynchronetBBS/sbbs

commit 29a35642160a7f4148134ce3d72b5636af902ea1

Blocked IP addresses in ip-silent.can weren't filtered from QWK and REP pkts
The "from_ip" header field of QWK messages is checked against blocked IP
addresses, but was only checking against IP addresses from ip.can, not any
addresses listed in ip-silent.can. These 2 list files are now merged together
for the purposes of filtering during QWK/REP packet import.

src/sbbs3/un_qwk.cpp
@@ -137,6 +137,9 @@ bool sbbs_t::unpack_qwk(char *packet,uint hubnum)
 	msg_filters.host_can = trashcan_list(&cfg,"host");
 	msg_filters.subject_can = trashcan_list(&cfg,"subject");
 	msg_filters.twit_list = list_of_twits(&cfg);
+	str_list_t ip_silent_list = trashcan_list(&cfg, "ip-silent");
+	strListMerge(&msg_filters.ip_can, ip_silent_list);
+	strListFree(&ip_silent_list);
 
 	for(l=QWK_BLOCK_LEN;l<size;l+=blocks*QWK_BLOCK_LEN) {
 		if(terminated) {

src/sbbs3/un_rep.cpp
@@ -172,8 +172,10 @@ bool sbbs_t::unpack_rep(char* repfile)
 	msg_filters.ip_can = trashcan_list(&cfg,"ip");
 	msg_filters.host_can = trashcan_list(&cfg,"host");
 	msg_filters.subject_can = trashcan_list(&cfg,"subject");
-
 	msg_filters.twit_list = list_of_twits(&cfg);
+	str_list_t ip_silent_list = trashcan_list(&cfg, "ip-silent");
+	strListMerge(&msg_filters.ip_can, ip_silent_list);
+	strListFree(&ip_silent_list);
 
 	now=time(NULL);
 	for(l=QWK_BLOCK_LEN;l<size;l+=blocks*QWK_BLOCK_LEN) {

commit aff554a44aa48a1e1565f45038893e6d24940b92

Fix crash due to use after free or double-free
... bug introduced in commit 29a35642160a7f4.

strListMerge() doesn't realloc the strings in the list, so we don't want to
free the strings in this list here.

This is likely the cause of the crash Keyop eluded to in #synchronet.

src/sbbs3/un_qwk.cpp
@@ -139,7 +139,7 @@ bool sbbs_t::unpack_qwk(char *packet,uint hubnum)
 	msg_filters.twit_list = list_of_twits(&cfg);
 	str_list_t ip_silent_list = trashcan_list(&cfg, "ip-silent");
 	strListMerge(&msg_filters.ip_can, ip_silent_list);
-	strListFree(&ip_silent_list);
+	free(ip_silent_list);
 
 	for(l=QWK_BLOCK_LEN;l<size;l+=blocks*QWK_BLOCK_LEN) {
 		if(terminated) {

src/sbbs3/un_rep.cpp
@@ -175,7 +175,7 @@ bool sbbs_t::unpack_rep(char* repfile)
 	msg_filters.twit_list = list_of_twits(&cfg);
 	str_list_t ip_silent_list = trashcan_list(&cfg, "ip-silent");
 	strListMerge(&msg_filters.ip_can, ip_silent_list);
-	strListFree(&ip_silent_list);
+	free(ip_silent_list);
 
 	now=time(NULL);
 	for(l=QWK_BLOCK_LEN;l<size;l+=blocks*QWK_BLOCK_LEN) {