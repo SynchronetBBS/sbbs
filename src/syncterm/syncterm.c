065fb8e33280059a1acccd563e633535cac5b636
Deucе <shurd@sasktel.net>
2024-09-25 18:50:42 -0400

    And this is RC1

--- src/syncterm/syncterm.c
+++ src/syncterm/syncterm.c
@@ -83,7 +83,7 @@ enum {
 #include "uifcinit.h"
 #include "window.h"
 
-const char *syncterm_version = "SyncTERM 1.2b"
+const char *syncterm_version = "SyncTERM 1.2rc1"
 
 // #define ALPHA
 #ifdef _DEBUG
