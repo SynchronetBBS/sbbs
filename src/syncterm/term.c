SynchronetBBS/sbbs

commit dcf8c35f9b053a8b4d498cbf4a63ee2b8dba6774

Resolve GCC 12.2 warnings
warning: format ‘%s’ expects argument of type ‘char *’, but argument 4 has type ‘void *’
warning: ‘sprintf’ writing a terminating nul past the end of the destination

src/syncterm/term.c
@@ -2230,7 +2230,7 @@ capture_control(struct bbslist *bbs)
 							if ((tm = localtime(&t)) != NULL) { // The null-terminator
                                                                                             // overwrites the first
                                                                                             // byte of filesize
-								sprintf(sauce.date, "%04u%02u%02u",
+								snprintf(sauce.date, sizeof(sauce.date), "%04u%02u%02u",
 								    1900 + tm->tm_year, 1 + tm->tm_mon, tm->tm_mday);
 							}
 							sauce.filesize = LE_INT32(ftell(fp)); // LE
@@ -2949,7 +2949,7 @@ draw_ppm_str_handler(char *str, size_t slen, char *fn, void *apcd)
 	}
 
 	if (mask != NULL) {
-		if (asprintf(&maskfn, "%s%s", fn, mask) < 0)
+		if (asprintf(&maskfn, "%s%s", fn, (char*)mask) < 0)
 			goto done;
 	}
 
@@ -3231,7 +3231,7 @@ paste_pixmap(char *str, size_t slen, char *fn, void *apcd)
 	}
 
 	if (mask != NULL) {
-		if (asprintf(&maskfn, "%s%s", fn, mask) < 0)
+		if (asprintf(&maskfn, "%s%s", fn, (char*)mask) < 0)
 			goto done;
 	}
 

commit 93a8c94862d8c7614646041343b8d2af6a1a0b20

Fix bug generating SAUCE date introduced by commit dcf8c35f9b
Using snprintf() prevents the last character of the date from
being put into the string.

src/syncterm/term.c
@@ -2273,7 +2273,10 @@ capture_control(struct bbslist *bbs)
 							if ((tm = localtime(&t)) != NULL) { // The null-terminator
                                                                                             // overwrites the first
                                                                                             // byte of filesize
-								snprintf(sauce.date, sizeof(sauce.date), "%04u%02u%02u",
+								// We can't use snprintf() here because snprintf()
+								// Is guaranteed to terminate, so the last digit
+								// would always be truncated.
+								sprintf(sauce.date, sizeof(sauce.date), "%04u%02u%02u",
 								    1900 + tm->tm_year, 1 + tm->tm_mon, tm->tm_mday);
 							}
 							sauce.filesize = LE_INT32(ftell(fp)); // LE