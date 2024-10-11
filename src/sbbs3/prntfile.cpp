SynchronetBBS/sbbs

commit d02fc1a29efd23dd9380c2f7a37a890da223ce07

Address Coverity-reported issues
Hopefully not introducing any bugs in the process.

src/sbbs3/prntfile.cpp
@@ -1,9 +1,4 @@
-/* prntfile.cpp */
-// vi: tabstop=4
-
 /* Synchronet file print/display routines */
-
-/* $Id: prntfile.cpp,v 1.47 2020/05/26 03:07:05 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -18,20 +13,8 @@
  * See the GNU General Public License for more details: gpl.txt or			*
  * http://www.fsf.org/copyleft/gpl.html										*
  *																			*
- * Anonymous FTP access to the most recent released source is available at	*
- * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
- *																			*
- * Anonymous CVS access to the development source and modification history	*
- * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
- *     (just hit return, no password is necessary)							*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src		*
- *																			*
  * For Synchronet coding style and modification guidelines, see				*
  * http://www.synchro.net/source.html										*
- *																			*
- * You are encouraged to submit any modifications (preferably in Unix diff	*
- * format) via e-mail to mods@synchro.net									*
  *																			*
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
@@ -285,7 +268,8 @@ bool sbbs_t::menu(const char *code, long mode, JSObject* obj)
 			}
 			if(menu_exists(code, next, path))
 				break;
-			menu_exists(code, last, path);
+			if(!menu_exists(code, last, path))
+				return false;
 		} while(0);
 	}
 

commit b656e19ff168e27583a33c268874345ffcea6d4d

Report error when menu() can't find a display file and P_NOERROR not used
This issue was introduced in commit d02fc1a2 (3 years ago), where menu() would
silenty fail (just return false) if no display file matching the supported
menu/display file types/extensions could be found.

Previous to commit d02fc1a2, we would log a NOTICE-level message and display
a "File not found" message to the user (what sbbs_t::printfile() does), but
since d02fc1a2, we no longer even call printfile() when the file doesn't
exist, so no error was logged or message displayed to the user.

Of course, when using the P_NOERROR mode flag, silent failure is expected and
that's still what happens in that case.

src/sbbs3/prntfile.cpp
@@ -282,8 +282,11 @@ bool sbbs_t::menu(const char *code, int mode, JSObject* obj)
 			}
 			if(menu_exists(code, next, path))
 				break;
-			if(!menu_exists(code, last, path))
+			if(!menu_exists(code, last, path)) {
+				if(!(mode & P_NOERROR))
+					errormsg(WHERE, ERR_CHK, path);
 				return false;
+			}
 		} while(0);
 	}
 