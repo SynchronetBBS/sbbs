2934f43bbf5128b917c4b0e765c85573b18e18b5
Deucе <shurd@sasktel.net>
2024-10-15 00:58:34 -0400

    More Haiku stuff

--- src/syncterm/updvers.sh
+++ src/syncterm/updvers.sh
@@ -17,21 +17,25 @@ a)
 	PATCHSTR=alpha
 	PATCHSEQ=0
 	ISREL=0
+	HAIKU_PRE=~$PATCH
 	;;
 b)
 	PATCHSTR=beta
 	PATCHSEQ=1
 	ISREL=0
+	HAIKU_PRE=~$PATCH
 	;;
 rc*)
 	PATCHSTR=${PATCH}
 	PATCHSEQ=$((1+${PATCH##rc}))
 	ISREL=0
+	HAIKU_PRE=~$PATCH
 	;;
 "")
 	PATCHSTR=release
 	PATCHSEQ=0
 	ISREL=1
+	HAIKU_PRE=
 	;;
 *)
 	echo Invalid patch value
@@ -52,27 +56,38 @@ echo "ISREL:    ${ISREL}"
 echo "Do you need to bump revision in ../conio/cterm.c?"
 
 # CMakeLists.txt
+echo Updating CMakeLists.txt
 perl -pi -e "s/(CPACK_PACKAGE_VERSION_MAJOR )[0-9]+/\$1.'${MAJOR}'/ge" CMakeLists.txt
 perl -pi -e "s/(CPACK_PACKAGE_VERSION_MINOR )[0-9]+/\$1.'${MINOR}'/ge" CMakeLists.txt
 perl -pi -e "s/(CPACK_PACKAGE_VERSION_PATCH )[^)]+/\$1.'${PATCHSTR}'/ge" CMakeLists.txt
 perl -pi -e "s/(CPACK_PACKAGE_VERSION )[^)]+/\$1.'${VERSTR}'/ge" CMakeLists.txt
 
 # Info.plist
+echo Updating Info.plist
 perl -pi -e "s|(<key>CFBundleShortVersionString</key>.+?<string>)[^<]+(?=</string>)|\$1.'${VERSTR}'|ges" Info.plist
 perl -pi -e "s|(<key>CFBundleVersion</key>.+?<string>)[^<]+(?=</string>)|\$1.'${NUMERIC}'|ges" Info.plist
 
 # Manual.txt
+echo Updating Manual.txt
 perl -pi -e "s/(?<=SyncTERM v)[0-9.a-z]+/${VERSTR}/g" Manual.txt
 
 # dpkg-control.in
+echo Updating dpkg-control.in
 perl -pi -e "s/(?<=^Version: ).*$/${VERSTR}/g" dpkg-control.in
 
 # syncterm.c
+echo Updating syncterm.c
 perl -pi -e "s/(?<=^const char \\*syncterm_version = \"SyncTERM )[^\"]+/${VERSTR}/g" syncterm.c
 
 # syncterm.rc
+echo Updating syncterm.rc
 perl -pi -e "s/^(\s+(PRODUCTVERSION|FILEVERSION)\s+).*$/\$1.'${MAJOR},${MINOR},${ISREL},${PATCHSEQ}'/e" syncterm.rc
 perl -pi -e "s/^(\s+(VALUE \"(File|Product)Version\",)\s+\").*$/\$1.'${VERSTR}\\0\"'/e" syncterm.rc
 
 # syncterm.spec
+echo Updating syncterm.spec
 perl -pi -e "s/(?<=^Version: ).*$/${VERSTR}/g" dpkg-control.in
+
+# PackageInfo.in
+echo Updating PackageInfo.in
+perl -pi -e "s/^(version\s+).*$/\$1.'${NUMERIC}${HAIKU_PRE}-1'/e" PackageInfo.in
