18e8720ff83812a02d9b47974d3abfc85c4a3d9b
Deucе <shurd@sasktel.net>
2024-11-03 03:39:51 -0500

    Make Return send #, use CTRL-Return for return
    Not tested on Windows.

--- src/syncterm/term.c
+++ src/syncterm/term.c
@@ -4419,7 +4419,14 @@ doterm(struct bbslist *bbs)
 						ch[0] = '_';
 						conn_send(ch, 1, 0);
 						break;
-					// TODO: Can send control chars instead...
+					case 10:
+						ch[0] = '\r';
+						conn_send(ch, 1, 0);
+						break;
+					case 13:
+						ch[0] = '_';
+						conn_send(ch, 1, 0);
+						break;
 					case 8:
 					case CIO_KEY_DC:
 					case CIO_KEY_LEFT:
