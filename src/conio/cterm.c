348f1a7e91c6fbab5b81c9e9d1d5fd639ab5a1a4
Deucе <shurd@sasktel.net>
2024-12-29 01:19:29 -0500

    Bump the CTerm revision.
    Commands are added.

--- src/conio/cterm.c
+++ src/conio/cterm.c
@@ -4823,7 +4823,7 @@ cterm_reset(struct cterminal *cterm)
 
 struct cterminal* cterm_init(int height, int width, int xpos, int ypos, int backlines, int backcols, struct vmem_cell *scrollback, int emulation)
 {
-	char	*revision="$Revision: 1.317 $";
+	char	*revision="$Revision: 1.318 $";
 	char *in;
 	char	*out;
 	struct cterminal *cterm;
