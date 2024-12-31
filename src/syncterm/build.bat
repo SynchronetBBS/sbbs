5c821372e50f2df5910a0162f72e96187fac6f0b
Deucе <shurd@sasktel.net>
2024-12-31 05:59:16 -0500

    Nope.
    I guess just because it works on my computer doesn't mean it will
    work on someone else's. :(

--- src/syncterm/build.bat
+++ src/syncterm/build.bat
@@ -2,5 +2,5 @@
 setlocal
 rem *** Requires Microsoft Visual C++ 2022 ***
 call "%VS170COMNTOOLS%\VsMSBuildCmd.bat"
-msbuild syncterm.sln /p:Platform="Win32" %* -t:Clean;Compile
+msbuild syncterm.sln /p:Platform="Win32" %*
 if errorlevel 1 echo. & echo !ERROR(s) occurred & exit /b 1
