86f5b9877b168751b841c3bc1e48e2d81debd32d
Deucе <shurd@sasktel.net>
2025-01-05 16:55:36 -0500

    Include windows.h in conn_pty.h too to ensure it's right.

--- src/syncterm/conn_conpty.h
+++ src/syncterm/conn_conpty.h
@@ -1,6 +1,11 @@
 #ifndef CONN_CONPTY_H
 #define CONN_CONPTY_H
 #ifdef _WIN32
+
+// We need to #include windows.h for NTDDI_VERSION to be rightified.
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
 #if NTDDI_VERSION >= 0x0A000006
 
 #define HAS_CONPTY
