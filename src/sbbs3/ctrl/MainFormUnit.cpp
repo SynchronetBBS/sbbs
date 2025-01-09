SynchronetBBS/sbbs

commit 6edc027042d7abe829652206ee01c1c02e193a70

Fix a couple remaining references to MAIN.CNF

src/sbbs3/ctrl/MainFormUnit.cpp
@@ -1966,7 +1966,7 @@ void __fastcall TMainForm::StartupTimerTick(TObject *Sender)
     fclose(fp);
 
     AnsiString CtrlDirectory = AnsiString(global.ctrl_dir);
-    if(!FileExists(CtrlDirectory+"MAIN.CNF")) {
+    if(!FileExists(CtrlDirectory + "main.ini")) {
 		Application->CreateForm(__classid(TCtrlPathDialog), &CtrlPathDialog);
     	if(CtrlPathDialog->ShowModal()!=mrOk) {
         	Application->Terminate();
@@ -1975,7 +1975,7 @@ void __fastcall TMainForm::StartupTimerTick(TObject *Sender)
         CtrlDirectory=CtrlPathDialog->Edit->Text;
         delete CtrlPathDialog;
     }
-    if(CtrlDirectory.UpperCase().AnsiPos("MAIN.CNF"))
+    if(CtrlDirectory.UpperCase().AnsiPos("main.ini"))
 		CtrlDirectory.SetLength(CtrlDirectory.Length()-8);
     SAFECOPY(global.ctrl_dir,CtrlDirectory.c_str());
     memset(&cfg,0,sizeof(cfg));

src/sbbs3/ctrl/CtrlPathDialogUnit.dfm
@@ -50,7 +50,7 @@ object CtrlPathDialog: TCtrlPathDialog
     Width = 320
     Height = 24
     TabOrder = 2
-    Text = 'c:\sbbs\ctrl\main.cnf'
+    Text = 'c:\sbbs\ctrl\main.ini'
   end
   object BrowseButton: TButton
     Left = 25
@@ -62,8 +62,8 @@ object CtrlPathDialog: TCtrlPathDialog
     OnClick = BrowseButtonClick
   end
   object OpenDialog: TOpenDialog
-    FileName = 'main.cnf'
-    Filter = 'Synchronet Configuration File|main.cnf'
+    FileName = 'main.ini'
+    Filter = 'Synchronet Configuration File|main.ini'
     Left = 424
     Top = 88
   end

commit 57d4c080e28191752cb2ed0bd3594a1409831a3f

Fix the removal of "main.ini" filename from CtrlDir path, if supplied
Converting the path to UPPER case and then comparing against a lowercase
string would never work.

Bug introduced in commit 6edc027042d7, reported by xbit (XBITBBS)

src/sbbs3/ctrl/MainFormUnit.cpp
@@ -1894,7 +1894,7 @@ void __fastcall TMainForm::StartupTimerTick(TObject *Sender)
         CtrlDirectory=CtrlPathDialog->Edit->Text;
         delete CtrlPathDialog;
     }
-    if(CtrlDirectory.UpperCase().AnsiPos("main.ini"))
+    if(CtrlDirectory.LowerCase().AnsiPos("main.ini"))
 		CtrlDirectory.SetLength(CtrlDirectory.Length()-8);
     SAFECOPY(global.ctrl_dir,CtrlDirectory.c_str());
     memset(&cfg,0,sizeof(cfg));