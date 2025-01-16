fea42451f4ac8d927b0cb99b299fdcb4e2866a8b
Rob Swindell (on Debian Linux) <rob@synchro.net>
2025-01-16 01:31:38 -0800

    A very basic initial set of tests for format()

---
+++ exec/tests/global/format.js
@@ -0,0 +1,27 @@
+const args = [ 1, 1.1, true, "one", -1, 1e9 ];
+
+const test = {
+	"%s":		"1",
+	"%d":		"1",
+	"%i":		"1",
+	"%u":		"1",
+	"%02d":		"01",
+	"%02x":		"01",
+	"%02u":		"01",
+	"%02i":		"01",
+	"%d %.2f":	"1 1.10",
+	"%d %d %d":	"1 1 1",
+	"%4$s":		"one",
+	"%4$.2s":	"on",
+	"%4$5s":	"  one",
+	"%4$-5s":	"one  ",
+	"%5$d":		"-1",
+	"%5$x":		"ffffffff",
+	"%6$u":		"1000000000",
+};
+
+for (var i in test) {
+	var result = format.apply(null, [i].concat(args));
+	if (result != test[i])
+		throw new Error("format(" + i + ") = '" + result + "' instead of '" + test[i] + "'");
+}
