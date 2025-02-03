SynchronetBBS/sbbs

commit d116f36227ec89a7f9257647ba5c138201fb06c5

Overhaul how custom Node actions and status strings are handled
Custom NodeStatus and NodeAction/Activity strings are now displayed pretty
much everywhere possible (except the node utility, since it doesn't read
any configuration files). This fixes issue #618, finally.

If you have custom (non-blank) NodeAction* strings in your text.dat/text.ini
file, or you change these strings during runtime (e.g. using bbs.replace_text)
this will impact you:
- do not include the user name or any other fields normally included in
  printed node status in your string - just the customized node action and
  really, you should only use/override these strings for dynamically changing
  node status (i.e. you're using NodeActionCustom and you're setting different
  custom strings depending on current node activity). Consider using the new
  NodeActivity* text.dat strings instead if your customized string doesn't
  change during run-time.
- don't use the %s/%u/%d specifiers any more - use @-codes instead, if needed

If you want static/permanent node action strings, set new new NodeActivity*
strings in your text.dat or (preferrbaly) text.ini file instead.
This means the node actions can now be localized for other languagues!

I'm still using the node.exb file (I did consider alternatives, e.g.
node.ini or node*/activity.asc), but decided to postpone such a change.
The node.exb file is still used, but the strings represent just the custom
(extended) node *activity* and not the entire node status string.

Use of Ctrl-A codes needs to be considered/accounted-for (currently isn't).

src/sbbs3/userdat.c
@@ -1403,14 +1403,158 @@ char* getnodeext(scfg_t* cfg, int num, char* buf)
 	return buf;
 }
 
+char* node_vstatus(scfg_t* cfg, node_t* node, char* str, size_t size)
+{
+	char	tmp[128];
+
+    switch(node->status) {
+        case NODE_WFC:
+			return cfg->text != NULL ? cfg->text[NodeStatusWaitingForCall] : "Waiting for connection";
+        case NODE_OFFLINE:
+			return cfg->text != NULL ? cfg->text[NodeStatusOffline] : "Offline";
+        case NODE_NETTING:	/* Obsolete */
+            return "Networking";
+        case NODE_LOGON:
+			return cfg->text != NULL ? cfg->text[NodeStatusLogon] : "At login prompt";
+		case NODE_LOGOUT:
+			snprintf(str, sizeof str, cfg->text != NULL ? cfg->text[NodeStatusLogout] : "Logging out %s", username(cfg,node->useron,tmp));
+			return str;
+        case NODE_EVENT_WAITING:
+            return cfg->text != NULL ? cfg->text[NodeStatusEventWaiting] : "Waiting for all nodes to become inactive";
+        case NODE_EVENT_LIMBO:
+            snprintf(str, size, cfg->text != NULL ? cfg->text[NodeStatusEventLimbo] : "Waiting for node %d to finish external event"
+                ,node->aux);
+			break;
+        case NODE_EVENT_RUNNING:
+            return cfg->text != NULL ? cfg->text[NodeStatusEventRunning] : "Running external event";
+        case NODE_NEWUSER:
+            return cfg->text != NULL ? cfg->text[NodeStatusNewUser] : "New user applying for access";
+		case NODE_QUIET:
+		case NODE_INUSE:
+			return "In use";
+			break;
+		default:
+			snprintf(str, size, "Unknown status %u", node->status);
+			break;
+	}
+	return str;
+}
+
+char* node_activity(scfg_t* cfg, node_t* node, char* str, size_t size, int num)
+{
+	int xtrnnum;
+	int gurunum;
+	user_t	user = {0};
+
+	if(node->misc & NODE_EXT) {
+		getnodeext(cfg, num, str); // note assuming sizeof str is >= 128
+		return str;
+	}
+
+	switch(node->action) {
+		case NODE_MAIN:
+			return cfg->text != NULL ? cfg->text[NodeActivityMainMenu] : "at main menu";
+		case NODE_RMSG:
+			return cfg->text != NULL ? cfg->text[NodeActivityReadingMsgs] : "reading messages";
+		case NODE_RMAL:
+			return cfg->text != NULL ? cfg->text[NodeActivityReadingMail] : "reading mail";
+		case NODE_RSML:
+			return cfg->text != NULL ? cfg->text[NodeActivityReadingSentMail]: "reading sent mail";
+		case NODE_RTXT:
+			return cfg->text != NULL ? cfg->text[NodeActivityReadingTextFiles] : "reading text files";
+		case NODE_PMSG:
+			return cfg->text != NULL ? cfg->text[NodeActivityPostingMsg] : "posting message";
+		case NODE_SMAL:
+			return cfg->text != NULL ? cfg->text[NodeActivitySendingMail] : "sending mail";
+		case NODE_AMSG:
+			return cfg->text != NULL ? cfg->text[NodeActivityAutoMsg] : "posting auto-message";
+		case NODE_XTRN:
+			if(node->aux == 0)
+				return cfg->text != NULL ? cfg->text[NodeActivityXtrnMenu] : "at external program menu";
+			user.number = node->useron;
+			getuserdat(cfg, &user);
+			xtrnnum = getxtrnnum(cfg, user.curxtrn);
+			if(is_valid_xtrnnum(cfg, xtrnnum))
+				snprintf(str, size, "%s %s"
+					,cfg->text != NULL ? cfg->text[NodeActivityRunningXtrn] : "running"
+					,cfg->xtrn[xtrnnum]->name);
+			else if(*user.curxtrn != '\0')
+				snprintf(str, size, "%s external program %s"
+					,cfg->text != NULL ? cfg->text[NodeActivityRunningXtrn] : "running"
+					,user.curxtrn);
+			else
+				snprintf(str, size, "%s external program #%d"
+					,cfg->text != NULL ? cfg->text[NodeActivityRunningXtrn] : "running"
+					,node->aux);
+			break;
+		case NODE_DFLT:
+			return cfg->text != NULL ? cfg->text[NodeActivitySettings] : "changing defaults";
+		case NODE_XFER:
+			return cfg->text != NULL ? cfg->text[NodeActivityFileMenu] : "at transfer menu";
+		case NODE_RFSD:
+			snprintf(str, size,cfg->text != NULL ? cfg->text[NodeActivityRetrievingFile] : "retrieving from device #%d", node->aux);
+			break;
+		case NODE_DLNG:
+			return cfg->text != NULL ? cfg->text[NodeActivityDownloadingFile] : "downloading";
+		case NODE_ULNG:
+			return cfg->text != NULL ? cfg->text[NodeActivityUploadingFile] : "uploading";
+		case NODE_BXFR:
+			return cfg->text != NULL ? cfg->text[NodeActivityBiXferFile] : "transferring bidirectional";
+		case NODE_LFIL:
+			return cfg->text != NULL ? cfg->text[NodeActivityListingFiles] : "listing files";
+		case NODE_LOGN:
+			return cfg->text != NULL ? cfg->text[NodeActivityLoggingOn] : "logging on";
+		case NODE_LCHT:
+			snprintf(str, size, cfg->text != NULL ? cfg->text[NodeActivityLocalChat] : "chatting with %s", cfg->sys_op);
+			break;
+		case NODE_MCHT:
+			if(node->aux != 0)
+				snprintf(str, sizeof str
+					,cfg->text != NULL ? cfg->text[NodeActivityChatChannel] : "in multinode chat channel %d"
+					,node->aux & 0xff);
+			else
+				return cfg->text != NULL ? cfg->text[NodeActivityGlobalChat] : "in multinode global chat channel";
+			break;
+		case NODE_PAGE:
+			snprintf(str, size, cfg->text != NULL ? cfg->text[NodeActivityPagingNode] : "paging node %u for private chat", node->aux);
+			break;
+		case NODE_PCHT:
+			if(node->aux == 0)
+				snprintf(str, size
+					,cfg->text != NULL ? cfg->text[NodeActivityLocalChat] : "in local chat with %s"
+					,cfg->sys_op);
+			else
+				snprintf(str, size
+					,cfg->text != NULL ? cfg->text[NodeActivityNodeChat] : "in private chat with node %u"
+					,node->aux);
+			break;
+		case NODE_GCHT:
+			gurunum = node->aux;
+			if(gurunum >= cfg->total_gurus)
+				gurunum = 0;
+			snprintf(str, size, cfg->text != NULL ? cfg->text[NodeActivityLocalChat] : "chatting with %s", cfg->guru[gurunum]->name);
+			break;
+		case NODE_CHAT:
+			return cfg->text != NULL ? cfg->text[NodeActivityChatMenu] : "in chat section";
+		case NODE_TQWK:
+			return cfg->text != NULL ? cfg->text[NodeActivityQWKmenu] : "transferring QWK packet";
+		case NODE_SYSP:
+			return cfg->text != NULL ? cfg->text[NodeActivitySysop] : "performing sysop activities";
+		case NODE_CUSTOM:
+			return cfg->text != NULL ? cfg->text[NodeActivityCustom] : "performing custom action";
+		default:
+			snprintf(str, size, "unknown user action %d", node->action);
+			break;
+	}
+	return str;
+}
+
 char* nodestatus(scfg_t* cfg, node_t* node, char* buf, size_t buflen, int num)
 {
 	char	str[256];
 	char	tmp[128];
 	char*	mer;
 	int		hour;
-	int		xtrnnum;
-	user_t	user = {0};
 
 	if(node==NULL) {
 		strncpy(buf,"(null)",buflen);
@@ -1420,158 +1564,25 @@ char* nodestatus(scfg_t* cfg, node_t* node, char* buf, size_t buflen, int num)
 	str[0]=0;
     switch(node->status) {
         case NODE_WFC:
-            SAFECOPY(str,"Waiting for connection");
-            break;
         case NODE_OFFLINE:
-            strcpy(str,"Offline");
-            break;
-        case NODE_NETTING:	/* Obsolete */
-            SAFECOPY(str,"Networking");
-            break;
-        case NODE_LOGON:
-            SAFEPRINTF(str,"At login prompt %s"
-				,node_connection_desc(node->connection, tmp));
-            break;
+        case NODE_NETTING:
 		case NODE_LOGOUT:
-			SAFEPRINTF(str,"Logging out %s", username(cfg,node->useron,tmp));
-			break;
         case NODE_EVENT_WAITING:
-            SAFECOPY(str,"Waiting for all nodes to become inactive");
-            break;
         case NODE_EVENT_LIMBO:
-            SAFEPRINTF(str,"Waiting for node %d to finish external event"
-                ,node->aux);
-            break;
         case NODE_EVENT_RUNNING:
-            SAFECOPY(str,"Running external event");
-            break;
+			SAFECOPY(str, node_vstatus(cfg, node, tmp, sizeof tmp));
+			break;
+        case NODE_LOGON:
         case NODE_NEWUSER:
-            SAFEPRINTF(str,"New user applying for access %s"
-				,node_connection_desc(node->connection, tmp));
+			SAFECOPY(str, node_vstatus(cfg, node, tmp, sizeof tmp));
+			SAFECAT(str, " ");
+			SAFECAT(str, node_connection_desc(node->connection, tmp));
             break;
         case NODE_QUIET:
         case NODE_INUSE:
-			if(node->misc & NODE_EXT) {
-				getnodeext(cfg, num, str);
-				break;
-			}
             username(cfg,node->useron,str);
-            strcat(str," ");
-            switch(node->action) {
-                case NODE_MAIN:
-                    strcat(str,"at main menu");
-                    break;
-                case NODE_RMSG:
-                    strcat(str,"reading messages");
-                    break;
-                case NODE_RMAL:
-                    strcat(str,"reading mail");
-                    break;
-                case NODE_RSML:
-                    strcat(str,"reading sent mail");
-                    break;
-                case NODE_RTXT:
-                    strcat(str,"reading text files");
-                    break;
-                case NODE_PMSG:
-                    strcat(str,"posting message");
-                    break;
-                case NODE_SMAL:
-                    strcat(str,"sending mail");
-                    break;
-                case NODE_AMSG:
-                    strcat(str,"posting auto-message");
-                    break;
-                case NODE_XTRN:
-                    if(node->aux == 0) {
-                        strcat(str,"at external program menu");
-						break;
-					}
-					user.number = node->useron;
-					getuserdat(cfg, &user);
-					xtrnnum = getxtrnnum(cfg, user.curxtrn);
-					if(is_valid_xtrnnum(cfg, xtrnnum))
-						sprintf(str+strlen(str),"running %s"
-							,cfg->xtrn[xtrnnum]->name);
-					else if(*user.curxtrn != '\0')
-						sprintf(str+strlen(str),"running external program %s"
-							,user.curxtrn);
-					else
-						sprintf(str+strlen(str),"running external program #%d"
-							,node->aux);
-                    break;
-                case NODE_DFLT:
-                    strcat(str,"changing defaults");
-                    break;
-                case NODE_XFER:
-                    strcat(str,"at transfer menu");
-                    break;
-                case NODE_RFSD:
-                    sprintf(str+strlen(str),"retrieving from device #%d",node->aux);
-                    break;
-                case NODE_DLNG:
-                    strcat(str,"downloading");
-                    break;
-                case NODE_ULNG:
-                    strcat(str,"uploading");
-                    break;
-                case NODE_BXFR:
-                    strcat(str,"transferring bidirectional");
-                    break;
-                case NODE_LFIL:
-                    strcat(str,"listing files");
-                    break;
-                case NODE_LOGN:
-                    strcat(str,"logging on");
-                    break;
-                case NODE_LCHT:
-                    strcat(str,"in local chat with sysop");
-                    break;
-                case NODE_MCHT:
-                    if(node->aux) {
-                        sprintf(str+strlen(str),"in multinode chat channel %d"
-                            ,node->aux&0xff);
-                        if(node->aux&0x1f00) { /* password */
-                            strcat(str,"* ");
-                            unpackchatpass(str+strlen(str),node);
-                        }
-                    }
-                    else
-                        strcat(str,"in multinode global chat channel");
-                    break;
-                case NODE_PAGE:
-                    sprintf(str+strlen(str)
-						,"paging node %u for private chat",node->aux);
-                    break;
-                case NODE_PCHT:
-                    if(node->aux==0)
-                        sprintf(str+strlen(str)
-							,"in local chat with %s"
-							,cfg->sys_op);
-                    else
-                        sprintf(str+strlen(str)
-							,"in private chat with node %u"
-                            ,node->aux);
-                    break;
-                case NODE_GCHT:
-                    strcat(str,"chatting with The Guru");
-                    break;
-                case NODE_CHAT:
-                    strcat(str,"in chat section");
-                    break;
-                case NODE_TQWK:
-                    strcat(str,"transferring QWK packet");
-                    break;
-                case NODE_SYSP:
-                    strcat(str,"performing sysop activities");
-                    break;
-				case NODE_CUSTOM:
-					SAFECAT(str, "performing custom action");
-					break;
-                default:
-                    sprintf(str+strlen(str),"%d",node->action);
-                    break;
-			}
+            SAFECAT(str," ");
+			SAFECAT(str, node_activity(cfg, node, tmp, sizeof tmp, num));
 			sprintf(str+strlen(str)," %s",node_connection_desc(node->connection, tmp));
             if(node->action==NODE_DLNG) {
                 if((node->aux/60)>=12) {

ctrl/text.dat
@@ -851,31 +851,31 @@
 "Directory %u"                                          693 NoAccessDir
 "\1n\1hNode Status\r\n\1c"\                                694 NodeLstHdr
 	"\xc4\xc4\xc4\xc4 \xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\xc4\r\n"
-""                                                      695 NodeActionMain
-""                                                      696 NodeActionReadMsgs
-""                                                      697 NodeActionReadMail
-""                                                      698 NodeActionSendMail
-""                                                      699 NodeActionReadTxt
-""                                                      700 NodeActionReadSentMail
-""                                                      701 NodeActionPostMsg
+""                                                      695 NodeActionMainMenu
+""                                                      696 NodeActionReadingMsgs
+""                                                      697 NodeActionReadingMail
+""                                                      698 NodeActionSendingMail
+""                                                      699 NodeActionReadingTextFiles
+""                                                      700 NodeActionReadingSentMail
+""                                                      701 NodeActionPostingMsg
 ""                                                      702 NodeActionAutoMsg
-""                                                      703 NodeActionXtrn
-""                                                      704 NodeActionDefaults
-""                                                      705 NodeActionXfer
-""                                                      706 NodeActionDLing
-""                                                      707 NodeActionULing
-""                                                      708 NodeActionBiXfer
-""                                                      709 NodeActionListFiles
+""                                                      703 NodeActionXtrnMenu
+""                                                      704 NodeActionSettings
+""                                                      705 NodeActionFileMenu
+""                                                      706 NodeActionDownloadingFile
+""                                                      707 NodeActionUploadingFile
+""                                                      708 NodeActionBiXferFile
+""                                                      709 NodeActionListingFiles
 ""                                                      710 NodeActionLoggingOn
 ""                                                      711 NodeActionLocalChat
-""				       	                712 NodeActionMultiChat
+""				       	                                712 NodeActionMultiChat
 ""                                                      713 NodeActionGuruChat
-""                                                      714 NodeActionChatSec
-""                                                      715 NodeActionSysopAct
-""                                                      716 NodeActionQWK
+""                                                      714 NodeActionChatMenu
+""                                                      715 NodeActionSysop
+""                                                      716 NodeActionQWKmenu
 ""                                                      717 NodeActionPrivateChat
-""                                                      718 NodeActionPaging
-""                                                      719 NodeActionRetrieving
+""                                                      718 NodeActionPagingNode
+""                                                      719 NodeActionRetrievingFile
 ""                                                      720 NodeActionCustom
 "View signature"                                        721 ViewSignatureQ
 "Delete signature"                                      722 DeleteSignatureQ
@@ -1082,3 +1082,30 @@
 "Nov"                                                      895 Nov
 "Dec"                                                      896 Dec
 "\7@ALIAS@ paged you to chat from node @NODE@\7"           897 SysopPageNotification
+"at main menu"                                             898 NodeActivityMainMenu
+"reading messages"                                         899 NodeActivityReadingMsgs
+"reading mail"                                             900 NodeActivityReadingMail
+"sending mail"                                             901 NodeActivitySendingMail
+"reading text files"                                       902 NodeActivityReadingTextFiles
+"reading sent mail"                                        903 NodeActivityReadingSentMail
+"posting message"                                          904 NodeActivityPostingMsg
+"posting auto-message"                                     905 NodeActivityAutoMsg
+"at external program menu"                                 906 NodeActivityXtrnMenu
+"running"                                                  907 NodeActivityRunningXtrn
+"changing settings"                                        908 NodeActivitySettings
+"at file menu"                                             909 NodeActivityFileMenu
+"downloading files"                                        910 NodeActivityDownloadingFile
+"uploading files"                                          911 NodeActivityUploadingFile
+"transferring files bidirectional"                         912 NodeActivityBiXferFile
+"listing files"                                            913 NodeActivityListingFiles
+"logging on"                                               914 NodeActivityLoggingOn
+"chatting with %s"                                         915 NodeActivityLocalChat
+"in multinode chat channel %d"                             916 NodeActivityChatChannel
+"in multinode global chat channel"                         917 NodeActivityGlobalChat
+"in chat section"                                          918 NodeActivityChatMenu
+"performing sysop activity"                                919 NodeActivitySysop
+"transferring QWK packet"                                  920 NodeActivityQWKmenu
+"chatting with node %u"                                    921 NodeActivityNodeChat
+"paging node %u for private chat"                          922 NodeActivityPagingNode
+"retrieving file from device #%d"                          923 NodeActivityRetrievingFile
+"performing custom activity"                               924 NodeActivityCustom

exec/load/presence_lib.js
@@ -133,17 +133,6 @@ function user_age_and_gender(user, options)
 	return output;
 }
 
-function extended_status(num)
-{
-	var f = new File(system.ctrl_dir + "node.exb");
-	if(!f.open("rb"))
-		return "!error " + f.error + " opening " + f.name;
-	f.position = num * 128;
-	var str = f.read(128);
-	f.close();
-	return truncsp(str);
-}
-
 // Returns a string describing the node status, suitable for printing on a single line
 //
 // num is zero-based
@@ -169,14 +158,10 @@ function node_status(node, is_sysop, options, num)
 	switch(node_status) {
 		case NODE_QUIET:
 			if(!is_sysop)
-				return format(NodeStatus[NODE_WFC],node.aux);
+				return node.vstatus || format(NodeStatus[NODE_WFC],node.aux);
 			/* Fall-through */
 		case NODE_INUSE:
 		{
-			if(misc&NODE_EXT) {
-				output += extended_status(num);
-				break;
-			}
 			var user = new User(node.useron);
 
 			if (!options.exclude_username) {
@@ -191,22 +176,26 @@ function node_status(node, is_sysop, options, num)
 				output += options.status_prefix;
 			output += user_age_and_gender(user, options);
 			output += " ";
-			switch(node.action) {
-				case NODE_PCHT:
-					if(node.aux == 0)
-						output += NodeAction[NODE_LCHT];
-					else
+			if(node.activity)
+				output += node.activity;
+			else {
+				switch(node.action) {
+					case NODE_PCHT:
+						if(node.aux == 0)
+							output += NodeAction[NODE_LCHT];
+						else
+							output += format(NodeAction[node.action], node.aux);
+						break;
+					case NODE_XTRN:
+						if(node.aux)
+							output += "running " + xtrn_name(node.aux);
+						else
+							output += NodeAction[node.action];
+						break;
+					default:
 						output += format(NodeAction[node.action], node.aux);
-					break;
-				case NODE_XTRN:
-					if(node.aux)
-						output += "running " + xtrn_name(node.aux);
-					else
-						output += NodeAction[node.action];
-					break;
-				default:
-					output += format(NodeAction[node.action], node.aux);
-					break;
+						break;
+				}
 			}
 			if (!options.exclude_connection) {
 				if(options.connection_prefix)
@@ -217,11 +206,11 @@ function node_status(node, is_sysop, options, num)
 		}
 		case NODE_LOGON:
 		case NODE_NEWUSER:
-			output += format(NodeStatus[node_status], node.aux);
+			output += node.vstatus || format(NodeStatus[node_status], node.aux);
 			output += node_connection_desc(node);
 			break;
 		case NODE_LOGOUT:
-			output += NodeStatus[node_status];
+			output += node.vstatus || NodeStatus[node_status];
 
 			if(options.username_prefix)
 				output += options.username_prefix;
@@ -231,7 +220,7 @@ function node_status(node, is_sysop, options, num)
 				output += system.username(node.useron);
 			break;
 		default:
-			output += format(NodeStatus[node_status], node.aux);
+			output += node.vstatus || format(NodeStatus[node_status], node.aux);
 			break;
 	}
 

exec/load/text.js
@@ -701,31 +701,31 @@ var NoAccessSub=691;
 var NoAccessLib=692;
 var NoAccessDir=693;
 var NodeLstHdr=694;
-var NodeActionMain=695;
-var NodeActionReadMsgs=696;
-var NodeActionReadMail=697;
-var NodeActionSendMail=698;
-var NodeActionReadTxt=699;
-var NodeActionReadSentMail=700;
-var NodeActionPostMsg=701;
+var NodeActionMainMenu=695;
+var NodeActionReadingMsgs=696;
+var NodeActionReadingMail=697;
+var NodeActionSendingMail=698;
+var NodeActionReadingTextFiles=699;
+var NodeActionReadingSentMail=700;
+var NodeActionPostingMsg=701;
 var NodeActionAutoMsg=702;
-var NodeActionXtrn=703;
-var NodeActionDefaults=704;
-var NodeActionXfer=705;
-var NodeActionDLing=706;
-var NodeActionULing=707;
-var NodeActionBiXfer=708;
-var NodeActionListFiles=709;
+var NodeActionXtrnMenu=703;
+var NodeActionSettings=704;
+var NodeActionFileMenu=705;
+var NodeActionDownloadingFile=706;
+var NodeActionUploadingFile=707;
+var NodeActionBiXferFile=708;
+var NodeActionListingFiles=709;
 var NodeActionLoggingOn=710;
 var NodeActionLocalChat=711;
 var NodeActionMultiChat=712;
 var NodeActionGuruChat=713;
-var NodeActionChatSec=714;
-var NodeActionSysopAct=715;
-var NodeActionQWK=716;
+var NodeActionChatMenu=714;
+var NodeActionSysop=715;
+var NodeActionQWKmenu=716;
 var NodeActionPrivateChat=717;
-var NodeActionPaging=718;
-var NodeActionRetrieving=719;
+var NodeActionPagingNode=718;
+var NodeActionRetrievingFile=719;
 var NodeActionCustom=720;
 var ViewSignatureQ=721;
 var DeleteSignatureQ=722;
@@ -904,7 +904,34 @@ var Oct=894;
 var Nov=895;
 var Dec=896;
 var SysopPageNotification=897;
+var NodeActivityMainMenu=898;
+var NodeActivityReadingMsgs=899;
+var NodeActivityReadingMail=900;
+var NodeActivitySendingMail=901;
+var NodeActivityReadingTextFiles=902;
+var NodeActivityReadingSentMail=903;
+var NodeActivityPostingMsg=904;
+var NodeActivityAutoMsg=905;
+var NodeActivityXtrnMenu=906;
+var NodeActivityRunningXtrn=907;
+var NodeActivitySettings=908;
+var NodeActivityFileMenu=909;
+var NodeActivityDownloadingFile=910;
+var NodeActivityUploadingFile=911;
+var NodeActivityBiXferFile=912;
+var NodeActivityListingFiles=913;
+var NodeActivityLoggingOn=914;
+var NodeActivityLocalChat=915;
+var NodeActivityChatChannel=916;
+var NodeActivityGlobalChat=917;
+var NodeActivityChatMenu=918;
+var NodeActivitySysop=919;
+var NodeActivityQWKmenu=920;
+var NodeActivityNodeChat=921;
+var NodeActivityPagingNode=922;
+var NodeActivityRetrievingFile=923;
+var NodeActivityCustom=924;
 
-var TOTAL_TEXT=898;
+var TOTAL_TEXT=925;
 
 this;

src/sbbs3/ctrl/MainFormUnit.cpp
@@ -1905,7 +1905,7 @@ void __fastcall TMainForm::StartupTimerTick(TObject *Sender)
 	SAFECOPY(error,UNKNOWN_LOAD_ERROR);
 
    	StatusBar->Panels->Items[STATUSBAR_LAST_PANEL]->Text="Loading configuration...";
-	if(!load_cfg(&cfg, /* text: */NULL, /* prep: */TRUE, /* node: */FALSE, error, sizeof(error))) {
+	if(!load_cfg(&cfg, text, /* prep: */TRUE, /* node: */FALSE, error, sizeof(error))) {
     	Application->MessageBox(error,"ERROR Loading Configuration"
 	        ,MB_OK|MB_ICONEXCLAMATION);
         Application->Terminate();
@@ -3060,7 +3060,7 @@ void __fastcall TMainForm::reload_config(void)
 	char error[256];
 	SAFECOPY(error,UNKNOWN_LOAD_ERROR);
    	StatusBar->Panels->Items[STATUSBAR_LAST_PANEL]->Text="Reloading configuration...";
-	if(!load_cfg(&cfg, /* text: */NULL, /* prep: */TRUE, /* node: */FALSE, error, sizeof(error))) {
+	if(!load_cfg(&cfg, text, /* prep: */TRUE, /* node: */FALSE, error, sizeof(error))) {
     	Application->MessageBox(error,"ERROR Re-loading Configuration"
 	        ,MB_OK|MB_ICONEXCLAMATION);
         Application->Terminate();

commit 1677e74966d6449e108ec464d43baf81d49682c1

Fix ERROR 2 (...) in putnode.cpp ... opening "node.exb"
Need the O_CREAT mode flag.

Broken with commit d116f3622: I started using opennodeext() (created 5 years
ago with commit 710bb23c1bd) rather than nopen(), and I didn't realize that
opennodeext() was not including O_CREAT.

Once the ctrl/node.exb was created (by anything), this error wouldn't happen.
And it'd only be an issues for systems with text.dat's that included custom
node status (NodeAction*) strings. Good catch Nelgin!

src/sbbs3/userdat.c
@@ -1267,7 +1267,7 @@ int opennodeext(scfg_t* cfg)
 		return -1;
 
 	SAFEPRINTF(fname, "%snode.exb", cfg->ctrl_dir);
-	return nopen(fname, O_RDWR | O_DENYNONE);
+	return nopen(fname, O_CREAT | O_RDWR | O_DENYNONE);
 }
 
 /****************************************************************************/