SynchronetBBS/sbbs

commit 29b93c3297c7d46af898d1505c73fb8cf0633ca9

Don't use deprecated functions: inet_addr() and gethostbyname()

src/sbbs3/mailsrvr.c
@@ -1003,7 +1003,8 @@ static in_addr_t resolve_ip(const char *inaddr)
 	char*    p;
 	char*    addr;
 	char     buf[128];
-	HOSTENT* host;
+	struct addrinfo* res;
+	in_addr_t ipa = INADDR_NONE;
 
 	SAFECOPY(buf, inaddr);
 	addr = buf;
@@ -1019,15 +1020,15 @@ static in_addr_t resolve_ip(const char *inaddr)
 		if (*p != '.' && !IS_DIGIT(*p))
 			break;
 	if (!(*p))
-		return inet_addr(addr);
+		return parseIPv4Address(addr);
 
-	if ((host = gethostbyname(inaddr)) == NULL)
+	if (getaddrinfo(addr, NULL, NULL, &res) != 0)
 		return INADDR_NONE;
 
-	if (host->h_addr_list[0] == NULL)
-		return INADDR_NONE;
-
-	return *((in_addr_t*)host->h_addr_list[0]);
+	if (res->ai_family == AF_INET)
+		ipa = ((struct sockaddr_in*)res->ai_addr)->sin_addr.s_addr;
+	freeaddrinfo(res);
+	return ipa;
 }
 
 /****************************************************************************/
@@ -2788,7 +2789,7 @@ static int chk_received_hdr(SOCKET socket, const char* prot, const char *buf, IN
 			strncpy(ip, p, 16);
 			ip[15] = 0;
 			addr.in.sin_family = AF_INET;
-			addr.in.sin_addr.s_addr = inet_addr(ip);
+			addr.in.sin_addr.s_addr = parseIPv4Address(ip);
 			lprintf(LOG_DEBUG, "%04d %s DNSBL checking received header address %s [%s]", socket, prot, host_name, ip);
 		}
 

src/sbbs3/main.cpp
@@ -458,8 +458,9 @@ int close_socket(SOCKET sock)
 /* TODO: IPv6 */
 in_addr_t resolve_ip(char *addr)
 {
-	HOSTENT* host;
 	char*    p;
+	struct addrinfo* res;
+	in_addr_t ipa = INADDR_NONE;
 
 	if (*addr == 0)
 		return INADDR_NONE;
@@ -468,12 +469,15 @@ in_addr_t resolve_ip(char *addr)
 		if (*p != '.' && !IS_DIGIT(*p))
 			break;
 	if (!(*p))
-		return inet_addr(addr);
-	if ((host = gethostbyname(addr)) == NULL)
+		return parseIPv4Address(addr);
+
+	if (getaddrinfo(addr, NULL, NULL, &res) != 0)
 		return INADDR_NONE;
-	if (host->h_addr_list[0] == NULL)
-		return INADDR_NONE;
-	return *((in_addr_t*)host->h_addr_list[0]);
+
+	if (res->ai_family == AF_INET)
+		ipa = ((struct sockaddr_in*)res->ai_addr)->sin_addr.s_addr;
+	freeaddrinfo(res);
+	return ipa;
 }
 
 } /* extern "C" */
@@ -5921,8 +5925,10 @@ NO_SSH:
 				result = connect(new_node->passthru_socket, (struct sockaddr *)&tmp_addr, tmp_addr_len);
 
 				if (result != 0) {
+					char tmp[16];
 					lprintf(LOG_ERR, "Node %d !ERROR %d (%d) connecting to passthru socket: %s port %u"
-					        , new_node->cfg.node_num, result, SOCKET_ERRNO, inet_ntoa(tmp_addr.sin_addr), htons(tmp_addr.sin_port));
+					        , new_node->cfg.node_num, result, SOCKET_ERRNO
+					        , inet_ntop(AF_INET, &tmp_addr.sin_addr.s_addr, tmp, sizeof tmp), htons(tmp_addr.sin_port));
 					close_socket(new_node->passthru_socket);
 					new_node->passthru_socket = INVALID_SOCKET;
 					close_socket(tmp_sock);

commit db8bb221954baab52c765db2aade2643e457cbca

parseIPv4Address() and IPv4AddresstoStr() now return/accept network byte order
Since that's the most used/assumed byte order for IP addresses, let's make
that the default and eliminate a lot of unnecessary byte-swapping.

This fixes the issue reported by Keyop in IRC, introduced in commit 29b93c32

src/sbbs3/execnet.cpp
@@ -52,7 +52,7 @@ int sbbs_t::exec_net(csi_t* csi)
 					SOCKADDR_IN addr;
 
 					memset(&addr, 0, sizeof(addr));
-					addr.sin_addr.s_addr = htonl(startup->outgoing4.s_addr);
+					addr.sin_addr.s_addr = startup->outgoing4.s_addr;
 					addr.sin_family = AF_INET;
 
 					if ((i = bind(sock, (struct sockaddr *) &addr, sizeof (addr))) != 0) {
@@ -538,7 +538,7 @@ SOCKET sbbs_t::ftp_data_sock(csi_t* csi, SOCKET ctrl_sock, SOCKADDR_IN* addr)
 	}
 
 	memset(addr, 0, sizeof(SOCKADDR_IN));
-	addr->sin_addr.s_addr = htonl(startup->outgoing4.s_addr);
+	addr->sin_addr.s_addr = startup->outgoing4.s_addr;
 	addr->sin_family = AF_INET;
 
 	if (bind(data_sock, (struct sockaddr *)addr, xp_sockaddr_len(addr)) != 0) {

src/sbbs3/mailsrvr.c
@@ -5308,7 +5308,7 @@ static BOOL sendmail_open_socket(SOCKET *sock, CRYPT_SESSION *session)
 	}
 
 	memset(&addr, 0, sizeof(addr));
-	addr.sin_addr.s_addr = htonl(startup->outgoing4.s_addr);
+	addr.sin_addr.s_addr = startup->outgoing4.s_addr;
 	addr.sin_family = AF_INET;
 
 	i = bind(*sock, (struct sockaddr *)&addr, sizeof(addr));

src/sbbs3/telgate.cpp
@@ -273,7 +273,7 @@ bool sbbs_t::telnet_gate(char* destaddr, uint mode, unsigned timeout, str_list_t
 	}
 
 	memset(&addr, 0, sizeof(addr));
-	addr.sin_addr.s_addr = htonl(startup->outgoing4.s_addr);
+	addr.sin_addr.s_addr = startup->outgoing4.s_addr;
 	addr.sin_family = AF_INET;
 
 	if ((i = bind(remote_socket, (struct sockaddr *) &addr, sizeof (addr))) != 0) {

src/xpdev/netwrap.c
@@ -110,6 +110,7 @@ void freeNameServerList(str_list_t list)
 }
 
 // If the input is invalid, INADDR_NONE (usually -1) is returned
+// The return value is in *network* (not host) byte order
 uint32_t parseIPv4Address(const char* value)
 {
 	uint32_t result = 0;
@@ -123,7 +124,7 @@ uint32_t parseIPv4Address(const char* value)
 	if (inet_pton(AF_INET, value, &result) != 1)
 		result = INADDR_NONE;
 #endif
-	return ntohl(result);
+	return result;
 }
 
 struct in6_addr parseIPv6Address(const char* value)
@@ -149,6 +150,7 @@ struct in6_addr parseIPv6Address(const char* value)
 	return ret;
 }
 
+// 'addr' must be in network byte order
 const char* IPv4AddressToStr(uint32_t addr, char* dest, size_t size)
 {
 #if defined _WIN32
@@ -156,7 +158,7 @@ const char* IPv4AddressToStr(uint32_t addr, char* dest, size_t size)
 	WSADATA     wsaData;
 	SOCKADDR_IN sockaddr = {0};
 	sockaddr.sin_family = AF_INET;
-	sockaddr.sin_addr.s_addr = htonl(addr);
+	sockaddr.sin_addr.s_addr = addr;
 
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return NULL;
@@ -167,7 +169,7 @@ const char* IPv4AddressToStr(uint32_t addr, char* dest, size_t size)
 	return dest;
 #else
 	struct in_addr in_addr;
-	in_addr.s_addr = htonl(addr);
+	in_addr.s_addr = addr;
 	return inet_ntop(AF_INET, &in_addr, dest, size);
 #endif
 }