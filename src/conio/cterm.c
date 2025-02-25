SynchronetBBS/sbbs

commit cc07b354ec1912896a6bb5460eea3abae2ff5e09

Fix defaults for variable length sequences
Only SGR was impacted, but this explicitly sets the rest of the
defaults to zero.

Closes ticket 193.

src/conio/cterm.c
@@ -2636,9 +2636,9 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 						case 'c':
 							/* SyncTERM Device Attributes */
 							if (seq->param_str[0] == '<' && parse_parameters(seq)) {
-								seq_default(seq, 0, 0);
 								tmp[0] = 0;
 								for (i=0; i<seq->param_count; i++) {
+									seq_default(seq, i, 0);
 									switch (seq->param_int[i]) {
 										case 0:		/* Advanced features */
 											strcpy(tmp, "\x1b[<0");
@@ -2669,6 +2669,7 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 								updfg = (oldfg == cterm->fg_color);
 								updbg = (oldbg == cterm->bg_color);
 								for (i=0; i<seq->param_count; i++) {
+									seq_default(seq, i, 0);
 									switch(seq->param_int[i]) {
 										case 6:
 											clear_lcf(cterm);
@@ -2752,6 +2753,7 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 							}
 							else if(seq->param_str[0] == '=' && parse_parameters(seq)) {
 								for (i=0; i<seq->param_count; i++) {
+									seq_default(seq, i, 0);
 									switch(seq->param_int[i]) {
 										case 4:
 											cterm->last_column_flag |= CTERM_LCF_ENABLED;
@@ -2773,6 +2775,7 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 								updfg = (oldfg == cterm->fg_color);
 								updbg = (oldbg == cterm->bg_color);
 								for (i=0; i<seq->param_count; i++) {
+									seq_default(seq, i, 0);
 									switch(seq->param_int[i]) {
 										case 6:
 											clear_lcf(cterm);
@@ -2857,6 +2860,7 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 							}
 							else if(seq->param_str[0] == '=' && parse_parameters(seq)) {
 								for (i=0; i<seq->param_count; i++) {
+									seq_default(seq, i, 0);
 									switch(seq->param_int[i]) {
 										case 4:
 											if ((cterm->last_column_flag & CTERM_LCF_FORCED) == 0)
@@ -3028,6 +3032,7 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 								}
 								else {
 									for (i=0; i<seq->param_count; i++) {
+										seq_default(seq, i, 0);
 										switch(seq->param_int[i]) {
 											case 6:
 												cterm->saved_mode_mask |= CTERM_SAVEMODE_ORIGIN;
@@ -3239,6 +3244,7 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 								}
 								else {
 									for (i=0; i<seq->param_count; i++) {
+										seq_default(seq, i, 0);
 										switch(seq->param_int[i]) {
 											case 6:
 												if(cterm->saved_mode_mask & CTERM_SAVEMODE_ORIGIN) {
@@ -3982,10 +3988,10 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 						case 'l':	/* TODO? Reset Mode */
 							break;
 						case 'm':	/* Select Graphic Rendition */
-							seq_default(seq, 0, 0);
 							gettextinfo(&ti);
 							flags = getvideoflags();
 							for (i=0; i < seq->param_count; i++) {
+								seq_default(seq, i, 0);
 								switch(seq->param_int[i]) {
 									case 0:
 										set_negative(cterm, false);

commit cbe604acdd0303d821bb87707a9a5532fb2a2853

CSI < c only takes one manditory argument with default
Fixes bug introduced in cc07b354 where the command without any
parameters would not return anything.

src/conio/cterm.c
@@ -2636,28 +2636,26 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 						case 'c':
 							/* SyncTERM Device Attributes */
 							if (seq->param_str[0] == '<' && parse_parameters(seq)) {
+								seq_default(seq, 0, 0);
 								tmp[0] = 0;
-								for (i=0; i<seq->param_count; i++) {
-									seq_default(seq, i, 0);
-									switch (seq->param_int[i]) {
-										case 0:		/* Advanced features */
-											strcpy(tmp, "\x1b[<0");
-											if (cio_api.options & CONIO_OPT_LOADABLE_FONTS)
-												strcat(tmp, ";1");
-											if (cio_api.options & CONIO_OPT_BRIGHT_BACKGROUND)
-												strcat(tmp, ";2");
-											if (cio_api.options & CONIO_OPT_PALETTE_SETTING)
-												strcat(tmp, ";3");
-											if (cio_api.options & CONIO_OPT_SET_PIXEL)
-												strcat(tmp, ";4");
-											if (cio_api.options & CONIO_OPT_FONT_SELECT)
-												strcat(tmp, ";5");
-											if (cio_api.options & CONIO_OPT_EXTENDED_PALETTE)
-												strcat(tmp, ";6");
-											if (cio_api.mouse)
-												strcat(tmp, ";7");
-											strcat(tmp, "c");
-									}
+								switch (seq->param_int[0]) {
+									case 0:		/* Advanced features */
+										strcpy(tmp, "\x1b[<0");
+										if (cio_api.options & CONIO_OPT_LOADABLE_FONTS)
+											strcat(tmp, ";1");
+										if (cio_api.options & CONIO_OPT_BRIGHT_BACKGROUND)
+											strcat(tmp, ";2");
+										if (cio_api.options & CONIO_OPT_PALETTE_SETTING)
+											strcat(tmp, ";3");
+										if (cio_api.options & CONIO_OPT_SET_PIXEL)
+											strcat(tmp, ";4");
+										if (cio_api.options & CONIO_OPT_FONT_SELECT)
+											strcat(tmp, ";5");
+										if (cio_api.options & CONIO_OPT_EXTENDED_PALETTE)
+											strcat(tmp, ";6");
+										if (cio_api.mouse)
+											strcat(tmp, ";7");
+										strcat(tmp, "c");
 								}
 								if(retbuf && *tmp && strlen(retbuf) + strlen(tmp) < retsize)
 									strcat(retbuf, tmp);