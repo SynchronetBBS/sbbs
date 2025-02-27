8ca6d2f8a172e520770941542730fef34214335e
Deucе <shurd@sasktel.net>
2025-02-27 13:54:32 -0500

    Don't bother checking for specific autoterm flags
    If there's anything set in autoterm, don't wait for telnet
    negotiation.

--- src/sbbs3/answer.cpp
+++ src/sbbs3/answer.cpp
@@ -548,7 +548,7 @@ bool sbbs_t::answer()
 	if (!term_output_disabled) {
 		// Grab telnet terminal if negotiated already
 		if (!(telnet_mode & TELNET_MODE_OFF)) {
-			if (!(autoterm & (PETSCII | ANSI))) {
+			if (autoterm == 0) {
 				unsigned loops = 0;
 				// Wait up to 2s more for telnet term type
 				// TODO: Any way to detect if the remote send a zero-length type?
