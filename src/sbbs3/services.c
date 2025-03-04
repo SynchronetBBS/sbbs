SynchronetBBS/sbbs

commit 88e8688baac4a83830fdd06509af4b940a65eccf

Pass an optional 'terminated' (bool pointer) to the multi-sock bind functions
This allows a server that's being terminated to stop the port-bind delay/retry
loops and terminate more quickly in cases of port binding failures.

src/sbbs3/services.c
@@ -59,7 +59,7 @@ static const char*         server_abbrev = "srvc";
 static services_startup_t* startup = NULL;
 static scfg_t              scfg;
 static char*               text[TOTAL_TEXT];
-static volatile bool       terminated = false;
+static bool                terminated = false;
 static time_t              uptime = 0;
 static ulong               served = 0;
 static volatile uint32_t   client_highwater = 0;
@@ -2086,7 +2086,7 @@ void services_thread(void* arg)
 				return;
 			}
 			xpms_add_list(service[i].set, PF_UNSPEC, (service[i].options & SERVICE_OPT_UDP) ? SOCK_DGRAM : SOCK_STREAM
-			              , IPPROTO_IP, service[i].interfaces, service[i].port, service[i].protocol
+			              , IPPROTO_IP, service[i].interfaces, service[i].port, service[i].protocol, &terminated
 			              , (service[i].options & SERVICE_OPT_UDP) ? service_udp_sock_cb : open_socket_cb, startup->seteuid, &service[i]);
 			total_sockets += service[i].set->sock_count;
 		}

src/sbbs3/ftpsrvr.c
@@ -5297,7 +5297,7 @@ void ftp_server(void* arg)
 		/*
 		 * Add interfaces
 		 */
-		xpms_add_list(ftp_set, PF_UNSPEC, SOCK_STREAM, 0, startup->interfaces, startup->port, "FTP Server", ftp_open_socket_cb, startup->seteuid, NULL);
+		xpms_add_list(ftp_set, PF_UNSPEC, SOCK_STREAM, 0, startup->interfaces, startup->port, "FTP Server", &terminate_server, ftp_open_socket_cb, startup->seteuid, NULL);
 
 		/* Setup recycle/shutdown semaphore file lists */
 		shutdown_semfiles = semfile_list_init(scfg.ctrl_dir, "shutdown", server_abbrev);

src/sbbs3/js_socket.c
@@ -3326,7 +3326,7 @@ js_listening_socket_constructor(JSContext *cx, uintN argc, jsval *arglist)
 		JSVALUE_TO_MSTRING(cx, argv[0], interface, NULL);
 		HANDLE_PENDING(cx, interface);
 		rc = JS_SUSPENDREQUEST(cx);
-		if (!xpms_add(set, domain, type, proto, interface, port, protocol, ls_cb, NULL, &cb)) {
+		if (!xpms_add(set, domain, type, proto, interface, port, protocol, NULL, ls_cb, NULL, &cb)) {
 			JS_RESUMEREQUEST(cx, rc);
 			JS_ReportError(cx, "Unable to add host to socket set");
 			goto fail;
@@ -3346,7 +3346,7 @@ js_listening_socket_constructor(JSContext *cx, uintN argc, jsval *arglist)
 			JSVALUE_TO_MSTRING(cx, v, interface, NULL);
 			HANDLE_PENDING(cx, interface);
 			rc = JS_SUSPENDREQUEST(cx);
-			if (!xpms_add(set, domain, type, proto, interface, port, protocol, ls_cb, NULL, &cb)) {
+			if (!xpms_add(set, domain, type, proto, interface, port, protocol, NULL, ls_cb, NULL, &cb)) {
 				free(interface);
 				JS_RESUMEREQUEST(cx, rc);
 				JS_ReportError(cx, "Unable to add host to socket set");

src/sbbs3/mailsrvr.c
@@ -86,7 +86,7 @@ static protected_uint32_t thread_count;
 static volatile uint32_t  client_highwater = 0;
 static volatile int       active_sendmail = 0;
 static volatile BOOL      sendmail_running = FALSE;
-static volatile BOOL      terminate_server = FALSE;
+static bool               terminate_server = FALSE;
 static volatile BOOL      terminate_sendmail = FALSE;
 static sem_t              sendmail_wakeup_sem;
 static volatile time_t    uptime;
@@ -6290,30 +6290,30 @@ void mail_server(void* arg)
 		}
 		terminated = FALSE;
 		if (!xpms_add_list(mail_set, PF_UNSPEC, SOCK_STREAM, 0, startup->interfaces
-		                   , startup->smtp_port, "SMTP Transfer Agent", mail_open_socket, startup->seteuid, (void*)servprot_smtp))
+		                   , startup->smtp_port, "SMTP Transfer Agent", &terminate_server, mail_open_socket, startup->seteuid, (void*)servprot_smtp))
 			lprintf(LOG_INFO, "SMTP No extra interfaces listening");
 
 		if (startup->options & MAIL_OPT_USE_SUBMISSION_PORT) {
 			xpms_add_list(mail_set, PF_UNSPEC, SOCK_STREAM, 0, startup->interfaces
-			              , startup->submission_port, "SMTP Submission Agent", mail_open_socket, startup->seteuid, (void*)servprot_submission);
+			              , startup->submission_port, "SMTP Submission Agent", &terminate_server, mail_open_socket, startup->seteuid, (void*)servprot_submission);
 		}
 
 		if (startup->options & MAIL_OPT_TLS_SUBMISSION) {
 			xpms_add_list(mail_set, PF_UNSPEC, SOCK_STREAM, 0, startup->interfaces, startup->submissions_port
-			              , "SMTPS Submission Agent", mail_open_socket, startup->seteuid, (void*)servprot_submissions);
+			              , "SMTPS Submission Agent", &terminate_server, mail_open_socket, startup->seteuid, (void*)servprot_submissions);
 		}
 
 		if (startup->options & MAIL_OPT_ALLOW_POP3) {
 			/* open a socket and wait for a client */
 			if (!xpms_add_list(mail_set, PF_UNSPEC, SOCK_STREAM, 0, startup->pop3_interfaces, startup->pop3_port
-			                   , "POP3 Server", mail_open_socket, startup->seteuid, (void*)servprot_pop3))
+			                   , "POP3 Server", &terminate_server, mail_open_socket, startup->seteuid, (void*)servprot_pop3))
 				lprintf(LOG_INFO, "POP3 No extra interfaces listening");
 		}
 
 		if (startup->options & MAIL_OPT_TLS_POP3) {
 			/* open a socket and wait for a client */
 			if (!xpms_add_list(mail_set, PF_UNSPEC, SOCK_STREAM, 0, startup->pop3_interfaces
-			                   , startup->pop3s_port, "POP3S Server", mail_open_socket, startup->seteuid, (void*)servprot_pop3s))
+			                   , startup->pop3s_port, "POP3S Server", &terminate_server, mail_open_socket, startup->seteuid, (void*)servprot_pop3s))
 				lprintf(LOG_INFO, "POP3S No extra interfaces listening");
 		}
 

src/sbbs3/main.cpp
@@ -5230,14 +5230,14 @@ void bbs_thread(void* arg)
 			/*
 			 * Add interfaces
 			 */
-			xpms_add_list(ts_set, PF_UNSPEC, SOCK_STREAM, 0, startup->telnet_interfaces, startup->telnet_port, "Telnet Server", sock_cb, startup->seteuid, &telnet_cb);
+			xpms_add_list(ts_set, PF_UNSPEC, SOCK_STREAM, 0, startup->telnet_interfaces, startup->telnet_port, "Telnet Server", &terminate_server, sock_cb, startup->seteuid, &telnet_cb);
 		}
 
 		if (startup->options & BBS_OPT_ALLOW_RLOGIN) {
 			/* open a socket and wait for a client */
 			rlogin_cb.protocol = "rlogin";
 			rlogin_cb.startup = startup;
-			xpms_add_list(ts_set, PF_UNSPEC, SOCK_STREAM, 0, startup->rlogin_interfaces, startup->rlogin_port, "RLogin Server", sock_cb, startup->seteuid, &rlogin_cb);
+			xpms_add_list(ts_set, PF_UNSPEC, SOCK_STREAM, 0, startup->rlogin_interfaces, startup->rlogin_port, "RLogin Server", &terminate_server, sock_cb, startup->seteuid, &rlogin_cb);
 		}
 
 #ifdef USE_CRYPTLIB
@@ -5290,7 +5290,7 @@ void bbs_thread(void* arg)
 			/* open a socket and wait for a client */
 			ssh_cb.protocol = "ssh";
 			ssh_cb.startup = startup;
-			xpms_add_list(ts_set, PF_UNSPEC, SOCK_STREAM, 0, startup->ssh_interfaces, startup->ssh_port, "SSH Server", sock_cb, startup->seteuid, &ssh_cb);
+			xpms_add_list(ts_set, PF_UNSPEC, SOCK_STREAM, 0, startup->ssh_interfaces, startup->ssh_port, "SSH Server", &terminate_server, sock_cb, startup->seteuid, &ssh_cb);
 		}
 NO_SSH:
 #endif
@@ -5357,7 +5357,7 @@ NO_SSH:
 				continue;
 			}
 			else  {
-				if (xpms_add(ts_set, PF_UNIX, SOCK_STREAM, 0, str, 0, "Spy Socket", sock_cb, NULL, &uspy_cb[i - 1]))
+				if (xpms_add(ts_set, PF_UNIX, SOCK_STREAM, 0, str, 0, "Spy Socket", NULL, sock_cb, NULL, &uspy_cb[i - 1]))
 					lprintf(LOG_INFO, "Node %d local spy using socket %s", i, str);
 				else
 					lprintf(LOG_ERR, "Node %d !ERROR %d (%s) creating local spy socket %s"

src/sbbs3/websrvr.c
@@ -109,7 +109,7 @@ static protected_uint32_t active_clients;
 static protected_uint32_t thread_count;
 static volatile uint32_t  client_highwater = 0;
 static volatile uint32_t  con_conn_highwater = 0;
-static volatile bool      terminate_server = false;
+static bool               terminate_server = false;
 static volatile bool      terminate_js = false;
 static volatile bool      terminate_http_logging_thread = false;
 static struct xpms_set *  ws_set = NULL;
@@ -7436,12 +7436,12 @@ void web_server(void* arg)
 		 */
 
 		if (!(startup->options & WEB_OPT_NO_HTTP))
-			xpms_add_list(ws_set, PF_UNSPEC, SOCK_STREAM, 0, startup->interfaces, startup->port, "Web Server", open_socket, startup->seteuid, NULL);
+			xpms_add_list(ws_set, PF_UNSPEC, SOCK_STREAM, 0, startup->interfaces, startup->port, "Web Server", &terminate_server, open_socket, startup->seteuid, NULL);
 		if (startup->options & WEB_OPT_ALLOW_TLS) {
 			if (!ssl_sync(&scfg, lprintf))
 				lprintf(LOG_CRIT, "!ssl_sync() failure trying to enable TLS support");
 			else
-				xpms_add_list(ws_set, PF_UNSPEC, SOCK_STREAM, 0, startup->tls_interfaces, startup->tls_port, "Secure Web Server", open_socket, startup->seteuid, "TLS");
+				xpms_add_list(ws_set, PF_UNSPEC, SOCK_STREAM, 0, startup->tls_interfaces, startup->tls_port, "Secure Web Server", &terminate_server, open_socket, startup->seteuid, "TLS");
 		}
 
 		listInit(&log_list, /* flags */ LINK_LIST_MUTEX | LINK_LIST_SEMAPHORE);

src/xpdev/multisock.c
@@ -49,7 +49,7 @@ void xpms_destroy(struct xpms_set *xpms_set, void (*sock_destroy)(SOCKET, void *
 }
 
 bool xpms_add(struct xpms_set *xpms_set, int domain, int type,
-              int protocol, const char *addr, uint16_t port, const char *prot,
+              int protocol, const char *addr, uint16_t port, const char *prot, bool* terminated,
               void (*sock_init)(SOCKET, void *), bool (*bind_init)(bool), void *cbdata)
 {
 	struct xpms_sockdef *new_socks;
@@ -136,7 +136,7 @@ bool xpms_add(struct xpms_set *xpms_set, int domain, int type,
 			if (port < IPPORT_RESERVED && port > 0)
 				bind_init(false);
 		}
-		if (retry_bind(xpms_set->socks[xpms_set->sock_count].sock, cur->ai_addr, cur->ai_addrlen, xpms_set->retries, xpms_set->wait_secs, prot, xpms_set->lprintf) == -1) {
+		if (retry_bind(xpms_set->socks[xpms_set->sock_count].sock, cur->ai_addr, cur->ai_addrlen, xpms_set->retries, xpms_set->wait_secs, prot, terminated, xpms_set->lprintf) == -1) {
 			closesocket(xpms_set->socks[xpms_set->sock_count].sock);
 			FREE_AND_NULL(xpms_set->socks[xpms_set->sock_count].address);
 			FREE_AND_NULL(xpms_set->socks[xpms_set->sock_count].prot);
@@ -178,7 +178,7 @@ bool xpms_add(struct xpms_set *xpms_set, int domain, int type,
 }
 
 bool xpms_add_list(struct xpms_set *xpms_set, int domain, int type,
-                   int protocol, str_list_t list, uint16_t default_port, const char *prot,
+                   int protocol, str_list_t list, uint16_t default_port, const char *prot, bool* terminated,
                    void (*sock_init)(SOCKET, void *), bool (*bind_init)(bool), void *cbdata)
 {
 	char **iface;
@@ -214,7 +214,7 @@ bool xpms_add_list(struct xpms_set *xpms_set, int domain, int type,
 		}
 		if (xpms_set->lprintf)
 			xpms_set->lprintf(LOG_INFO, "%s listening on socket %s port %hu", prot, host_str, port);
-		if (xpms_add(xpms_set, domain, type, protocol, host_str, port, prot, sock_init, bind_init, cbdata))
+		if (xpms_add(xpms_set, domain, type, protocol, host_str, port, prot, terminated, sock_init, bind_init, cbdata))
 			one_good = true;
 		free(host);
 	}
@@ -222,7 +222,7 @@ bool xpms_add_list(struct xpms_set *xpms_set, int domain, int type,
 }
 
 bool xpms_add_chararray_list(struct xpms_set *xpms_set, int domain, int type,
-                             int protocol, const char *list, uint16_t default_port, const char *prot,
+                             int protocol, const char *list, uint16_t default_port, const char *prot, bool* terminated,
                              void (*sock_init)(SOCKET, void *), bool (*bind_init)(bool), void *cbdata)
 {
 	str_list_t slist;
@@ -231,7 +231,7 @@ bool xpms_add_chararray_list(struct xpms_set *xpms_set, int domain, int type,
 	slist = strListSplitCopy(NULL, list, ", \t\r\n");
 	if (slist == NULL)
 		return false;
-	ret = xpms_add_list(xpms_set, domain, type, protocol, slist, default_port, prot,
+	ret = xpms_add_list(xpms_set, domain, type, protocol, slist, default_port, prot, terminated,
 	                    sock_init, bind_init, cbdata);
 	strListFree(&slist);
 	return ret;

src/xpdev/multisock.h
@@ -39,15 +39,15 @@ DLLEXPORT struct xpms_set* xpms_create(unsigned int retries, unsigned int wait_s
 	int (*lprintf)(int level, const char *fmt, ...));
 DLLEXPORT void xpms_destroy(struct xpms_set *xpms_set, void (*sock_destroy)(SOCKET, void *), void *cbdata);
 DLLEXPORT bool xpms_add(struct xpms_set *xpms_set, int domain, int type,
-	int protocol, const char *addr, uint16_t port, const char *prot, 
+	int protocol, const char *addr, uint16_t port, const char *prot, bool* terminated,
 	void (*sock_init)(SOCKET, void *), bool(*bind_init)(bool), void *cbdata);
 DLLEXPORT bool xpms_add_list(struct xpms_set *xpms_set, int domain, int type,
-	int protocol, str_list_t list, uint16_t default_port, const char *prot, 
+	int protocol, str_list_t list, uint16_t default_port, const char *prot, bool* terminated,
 	void (*sock_init)(SOCKET, void *), bool(*bind_init)(bool), void *cbdata);
 DLLEXPORT bool xpms_add_chararray_list(struct xpms_set *xpms_set, int domain, int type,
-	int protocol, const char *list, uint16_t default_port, const char *prot,
+	int protocol, const char *list, uint16_t default_port, const char *prot, bool* terminated,
 	void (*sock_init)(SOCKET, void *), bool(*bind_init)(bool), void *cbdata);
-DLLEXPORT SOCKET xpms_accept(struct xpms_set *, union xp_sockaddr * addr, 
+DLLEXPORT SOCKET xpms_accept(struct xpms_set *, union xp_sockaddr * addr,
 	socklen_t * addrlen, unsigned int timeout, uint32_t flags, void **cb_data);
 
 #ifdef __cplusplus

commit f06ac1e44385de61bb98ccb55f60252bef2a5378

Fix restart of services after a stop
Bug introduced in commit 88e8688baac4a83830fdd06509af4b940a65eccf

src/sbbs3/services.c
@@ -1973,6 +1973,7 @@ void services_thread(void* arg)
 	served = 0;
 	startup->recycle_now = false;
 	startup->shutdown_now = false;
+	terminated = false;
 
 	SetThreadName("sbbs/services");
 