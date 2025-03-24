64a6a0d5057d56d3337f28894e433186b7296928
Rob Swindell <rob@synchro.net>
2025-03-24 14:36:11 -0700

    Add more detail to the file open failure exception

--- exec/load/recordfile.js
+++ exec/load/recordfile.js
@@ -71,7 +71,7 @@ function RecordFile(filename, definition)
 	this.RecordLength=getRecordLength(this.fields);
 	// A vbuf of a record length prevents more than one record from being in the buffer.
 	if(!this.file.open(file_exists(this.file.name)?"rb+":"wb+",true,this.RecordLength)) {
-		throw new Error("Failed to open file "+this.file.name);
+		throw new Error(format("%d (%s) opening '%s'", this.file.error, strerror(this.file.error), this.file.name));
 	}
 	Object.defineProperty(this, 'length', {
 		enumerable: true,
