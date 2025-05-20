SynchronetBBS/sbbs

commit 91ea951d35e0941f99c2560df76cace79176e1d1

Break scan loop on abort (Ctrl-C)
Fixes issue #926

src/sbbs3/readmsgs.cpp
@@ -771,8 +771,11 @@ int sbbs_t::scanposts(int subnum, int mode, const char *find)
 				}
 			}
 		}
-		else
+		else {
+			if (sys_status & SS_ABORT)
+				break;
 			domsg = 1;
+		}
 		if (mode & SCAN_CONT) {
 			if (smb.curmsg < smb.msgs - 1)
 				smb.curmsg++;

commit 6c98e2878d226ccc502fa2640cefbe0f413bcc0e

Fix issue introduced in commit 91ea951d with Ctrl-C aborting read msgs prompt
When hitting Ctrl-C at the PostTo prompt (for example), the read msgs prompt
shouldn't be aborted, so clear the abort flag after handling any command key.

src/sbbs3/readmsgs.cpp
@@ -1632,6 +1632,7 @@ int sbbs_t::scanposts(int subnum, int mode, const char *find)
 				domsg = 0;
 				break;
 		}
+		clearabort();
 	}
 	if (msg.total_hfields)
 		smb_freemsgmem(&msg);