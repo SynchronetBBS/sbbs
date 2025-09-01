30c5ca214d2f2f6cc163613034b92f194cc7f2b0
Rob Swindell (on Debian Linux) <rob@synchro.net>
2025-08-31 19:04:54 -0700

    Add support for -t (testing) and -v (verbose) options
    A bit of a refactor on the command-line parsing and output function usage.

--- exec/purgefiles.js
+++ exec/purgefiles.js
@@ -1,25 +1,44 @@
-// purgefiles.js
+// Utility for deleting old files (e.g. log files) based on age/date
 
 const default_max_age=30;	// days
 
-argn=0;
-if((max_age=parseInt(argv[argn]))>0)
-	argn++;
-else
-	max_age=default_max_age;
+var testing = false;
+var verbose = false;
+var max_age = default_max_age;
+var pattern;
 
-if(argc==argn) {
-	printf("\nusage: purgfiles.js [max_age_in_days] <directory/pattern>\n");
-	printf("\ndefault max_age_in_days = %lu\n",default_max_age);
+for (var argn = 0; argn < argc; ++argn) {
+	if (argv[argn] == '-t')
+		testing = true;
+	else if (argv[argn] == '-v')
+		verbose = true;
+	else if (parseInt(argv[argn]) > 0)
+		max_age = parseInt(argv[argn]);
+	else if (argv[argn][0] == '-')
+		break;
+	else
+		pattern = argv[argn];
+}
+
+if(!pattern) {
+	print("\nusage: purgfiles.js [max_age_in_days] [-options] <directory/pattern>");
+	print();
+	print("default max_age_in_days = " + default_max_age);
+	print();
+	print("options:");
+	print(" -t  testing only (no file deletion)");
+	print(" -v  verbose output");
 	exit();
 }
 
-
-printf("Getting directory of %s", argv[argn]);
-files = directory(argv[argn]);
+printf("Getting directory of %s", pattern);
+files = directory(pattern);
 printf("\n%lu files\n",files.length);
+if (verbose)
+	print(JSON.stringify(files, null, 4));
 
-printf("\nPurging files aged more than %lu days\n",max_age);
+if (files.length < 1)
+	exit(0);
 purged_files=0;
 now = time();
 for(i in files) {
@@ -28,6 +47,8 @@ for(i in files) {
 	if(age <= max_age)
 		continue;
 	print(system.datestr(t) + " " + files[i]);
+	if (testing)
+		continue;
 	if(!file_remove(files[i])) {
 		log(LOG_ERR,"!Error " + errno + " removing " + files[i]);
 		continue;
