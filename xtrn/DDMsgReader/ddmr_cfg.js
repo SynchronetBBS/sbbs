08b7ee6b0a3fed43509b1f356017b2b27e276b3f
Eric Oulashin <nightfox@synchro.net>
2025-09-07 17:38:25 -0700

    DDMsgReader fix: Fix: When listing messages in lightbar mode in reverse order, deleting (with DEL) and toggling messages (with the spacebar) now work on the correct message. Reported by m1ndsurf3r.

--- xtrn/DDMsgReader/ddmr_cfg.js
+++ xtrn/DDMsgReader/ddmr_cfg.js
@@ -17,7 +17,7 @@ require("sbbsdefs.js", "P_NONE");
 require("uifcdefs.js", "UIFC_INMSG");
 
 
-if (!uifc.init("DigDist. Message Reader 1.97d Configurator"))
+if (!uifc.init("DigDist. Message Reader 1.97e Configurator"))
 {
 	print("Failed to initialize uifc");
 	exit(1);
