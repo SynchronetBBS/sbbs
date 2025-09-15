SynchronetBBS/sbbs

commit 661f5084fca81a01e499b97c7d9dcd4cd59a22f2

Support exceptions for saving log info of web, ftp, and services users
To reduce thrash on the userbase, it's now possible to specify which users
of some servers (web, ftp, and services) will have their user record
updated for every login. Normally, every user's login info (IP address,
hostname, protocol, date/time) is updated for every login to every
server. But for user accounts that are expected to have frequent logins
(e.g. Guest), this can be a source of excessive opening/locking/reading and
writing acitvity on the user base (data/user.tab file). So, if needed, a
sysop can limit which users login-info is updated for every login by setting
LoginInfoSave to an ARS value (e.g. "NOT GUEST") in the [Web], [Ftp], or
[Services] section of their ctrl/sbbs.ini file.

This is a mitigation specifically for Vertrauen where multiple instances of
SBBS share data files on a single Samba server and high "guest" activity leads
to occasional bursts of errors locking, reading, or writing the user.tab file.
This is likely not a feature that any other sysop will need or want to use.

src/sbbs3/userdat.c
@@ -3201,17 +3201,24 @@ bool user_adjust_minutes(scfg_t* cfg, user_t* user, long amount)
 }
 
 /****************************************************************************/
+/* 'cfg' and 'user' arguments may not NOT be NULL							*/
+/* 'client' and 'save_ars' arguments may be NULL							*/
+/* 'use_prot' indicates whether to copy the client->protocol value to user	*/
+/* When save_ars is not NULL, specifies users to not save login values for	*/
+/* Returns 0 (USER_SUCCESS) on success										*/
 /****************************************************************************/
-int loginuserdat(scfg_t* cfg, user_t* user, const char* protocol, const char* hostname, const char* ipaddr, time_t logontime)
+int loginuserdat(scfg_t* cfg, user_t* user, client_t* client, bool use_prot, char* save_ars)
 {
-	if (protocol != NULL)
-		SAFECOPY(user->connection, protocol);
-	if (hostname != NULL)
-		SAFECOPY(user->comp, hostname);
-	if (ipaddr != NULL)
-		SAFECOPY(user->ipaddr, ipaddr);
-	user->logontime = (time32_t)logontime;
+	if (client != NULL) {
+		if (use_prot)
+			SAFECOPY(user->connection, client->protocol);
+		SAFECOPY(user->comp, client->host);
+		SAFECOPY(user->ipaddr, client->addr);
+	}
+	user->logontime = time32(NULL);
 
+	if (save_ars != NULL && !chk_ars(cfg, save_ars, user, client))
+		return USER_SUCCESS;
 	return putuserdat(cfg, user);
 }
 

src/sbbs3/ftpsrvr.c
@@ -2672,7 +2672,7 @@ static void ctrl_thread(void* arg)
 			/* Adjust User Total Logons/Logons Today */
 			user.logons++;
 			user.ltoday++;
-			if ((result = loginuserdat(&scfg, &user, "FTP", host_name, host_ip, logintime)) != 0)
+			if ((result = loginuserdat(&scfg, &user, &client, /* use_protocol: */true, startup->login_info_save)) != 0)
 				lprintf(LOG_ERR, "%04d [%s] <%s> !Error %d (errno %d %s) writing user data for user #%d"
 				        , sock, host_ip, user.alias, result, errno, safe_strerror(errno, error, sizeof error), user.number);
 			mqtt_user_login(&mqtt, &client);

src/sbbs3/ftpsrvr.h
@@ -45,6 +45,7 @@ typedef struct {
 	int64_t max_fsize;          /* Maximum file size accepted for upload (0=unlimited) */
 	uint max_concurrent_connections;
 	char index_file_name[64];
+	char login_info_save[INI_MAX_VALUE_LEN];
 
 } ftp_startup_t;
 

src/sbbs3/logon.cpp
@@ -449,7 +449,7 @@ bool sbbs_t::logon()
 		return false;
 	}
 	useron.logons++;
-	i = loginuserdat(&cfg, &useron, connection, client_name, client_ipaddr, logontime);
+	i = loginuserdat(&cfg, &useron, &client, /* use_prot: */true, /* save_ars: */NULL);
 	if (i != USER_SUCCESS)
 		lprintf(LOG_ERR, "%s !Error %d writing user data for user #%d", __FUNCTION__, i, useron.number);
 	getmsgptrs();

src/sbbs3/mailsrvr.c
@@ -1389,7 +1389,7 @@ static bool pop3_client_thread(pop3_t* pop3)
 			listAddNodeData(&current_logins, client.addr, strlen(client.addr) + 1, socket, LAST_NODE);
 		}
 
-		loginuserdat(&scfg, &user, /* protocol: */NULL, host_name, host_ip, time(NULL));
+		loginuserdat(&scfg, &user, &client, /* use_prot: */false, /* save_ars: */NULL);
 
 		/* Update client display */
 		SAFECOPY(client.user, user.alias);
@@ -4317,7 +4317,7 @@ static bool smtp_client_thread(smtp_t* smtp)
 			client.usernum = relay_user.number;
 			client_on(socket, &client, TRUE /* update */);
 
-			loginuserdat(&scfg, &relay_user, client.protocol, host_name, host_ip, time(NULL));
+			loginuserdat(&scfg, &relay_user, &client, /* use_prot: */true, /* save_ars: */NULL);
 
 			lprintf(LOG_INFO, "%04d %-5s %s <%s> logged-in using %s authentication (%u emails sent today)"
 			        , socket, client.protocol, client_id, relay_user.alias, auth_login ? "LOGIN" : "PLAIN"
@@ -4425,7 +4425,7 @@ static bool smtp_client_thread(smtp_t* smtp)
 			client.usernum = relay_user.number;
 			client_on(socket, &client, TRUE /* update */);
 
-			loginuserdat(&scfg, &relay_user, client.protocol, host_name, host_ip, time(NULL));
+			loginuserdat(&scfg, &relay_user, &client, /* use_prot: */true, /* save_ars: */NULL);
 
 			lprintf(LOG_INFO, "%04d %-5s %s <%s> logged-in using CRAM-MD5 authentication (%u emails sent today)"
 			        , socket, client.protocol, client_id, relay_user.alias, relay_user.etoday);

src/sbbs3/sbbs_ini.c
@@ -63,6 +63,7 @@ static const char* strLoginAttemptTempBanThreshold = "LoginAttemptTempBanThresho
 static const char* strLoginAttemptTempBanDuration = "LoginAttemptTempBanDuration";
 static const char* strLoginAttemptFilterThreshold = "LoginAttemptFilterThreshold";
 static const char* strLoginAttemptFilterDuration = "LoginAttemptFilterDuration";
+static const char* strLoginInfoSave = "LoginInfoSave";
 static const char* strLoginRequirements = "LoginRequirements";
 static const char* strArchiveRequirements = "ArchiveRequirements";
 static const char* strJavaScriptMaxBytes       = "JavaScriptMaxBytes";
@@ -589,6 +590,8 @@ void sbbs_read_ini(
 
 		SAFECOPY(ftp->login_ars
 		         , iniGetString(list, section, strLoginRequirements, global->login_ars, value));
+		SAFECOPY(ftp->login_info_save
+				 , iniGetString(list, section, strLoginInfoSave, "", value));
 
 		ftp->log_level
 		    = iniGetLogLevel(list, section, strLogLevel, global->log_level);
@@ -738,6 +741,8 @@ void sbbs_read_ini(
 
 		SAFECOPY(services->login_ars
 		         , iniGetString(list, section, strLoginRequirements, global->login_ars, value));
+		SAFECOPY(services->login_info_save
+		         , iniGetString(list, section, strLoginInfoSave, "", value));
 
 		SAFECOPY(services->services_ini
 		         , iniGetString(list, section, strIniFileName, "services.ini", value));
@@ -789,6 +794,8 @@ void sbbs_read_ini(
 
 		SAFECOPY(web->login_ars
 		         , iniGetString(list, section, strLoginRequirements, global->login_ars, value));
+		SAFECOPY(web->login_info_save
+		         , iniGetString(list, section, strLoginInfoSave, "", value));
 
 		SAFECOPY(web->root_dir
 		         , iniGetString(list, section, "RootDirectory", WEB_DEFAULT_ROOT_DIR, value));
@@ -1109,6 +1116,8 @@ bool sbbs_write_ini(
 				iniRemoveKey(lp, section, strLoginRequirements);
 			else if (!iniSetString(lp, section, strLoginRequirements, ftp->login_ars, &style))
 				break;
+			if (!iniSetString(lp, section, strLoginInfoSave, ftp->login_info_save, &style))
+				break;
 
 			if (!iniSetString(lp, section, "IndexFileName", ftp->index_file_name, &style))
 				break;
@@ -1317,6 +1326,8 @@ bool sbbs_write_ini(
 				iniRemoveKey(lp, section, strLoginRequirements);
 			else if (!iniSetString(lp, section, strLoginRequirements, services->login_ars, &style))
 				break;
+			if (!iniSetString(lp, section, strLoginInfoSave, services->login_info_save, &style))
+				break;
 
 			if (!sbbs_set_sound_settings(lp, section, &services->sound, &global->sound, &style))
 				break;
@@ -1397,6 +1408,8 @@ bool sbbs_write_ini(
 			if (stricmp(web->login_ars, global->login_ars) == 0)
 				iniRemoveKey(lp, section, strLoginRequirements);
 			else if (!iniSetString(lp, section, strLoginRequirements, web->login_ars, &style))
+				break;
+			if (!iniSetString(lp, section, strLoginInfoSave, web->login_info_save, &style))
 				break;
 
 			if (!iniSetString(lp, section, "RootDirectory", web->root_dir, &style))

src/sbbs3/services.c
@@ -483,19 +483,15 @@ js_login(JSContext *cx, uintN argc, jsval *arglist)
 		JS_ValueToBoolean(cx, argv[2], &inc_logons);
 
 	rc = JS_SUSPENDREQUEST(cx);
-	if (client->client != NULL) {
-		SAFECOPY(client->user.ipaddr, client->client->addr);
-		SAFECOPY(client->user.comp, client->client->host);
-		if (!(client->service->options & SERVICE_OPT_NO_USER_PROT))
-			SAFECOPY(client->user.connection, client->service->protocol);
-	}
 
 	if (inc_logons) {
 		client->user.logons++;
 		client->user.ltoday++;
 	}
 
-	int result = putuserdat(&scfg, &client->user);
+	int result = loginuserdat(&scfg, &client->user, client->client
+	                          , !(client->service->options & SERVICE_OPT_NO_USER_PROT)
+	                          , startup->login_info_save);
 	if (result != 0) {
 		errprintf(LOG_ERR, WHERE, "%04d %s !Error %d writing user data for user #%d"
 		          , client->socket, client->service->protocol

src/sbbs3/services.h
@@ -29,6 +29,7 @@ typedef struct {
 	struct in6_addr outgoing6;
 	str_list_t interfaces;
 	char services_ini[128];     // services.ini filename
+	char login_info_save[INI_MAX_VALUE_LEN];
 
 	/* JavaScript operating parameters */
 	js_startup_t js;

commit 869dac47e01978f4e7f20fb5fdd3e5703f410299

Fix inverted "GUEST" ARS keyword logic bug
introduced in commit 661f5084fca81a01

Fixes issue #975

src/sbbs3/userdat.c
@@ -2273,7 +2273,7 @@ static bool ar_exp(scfg_t* cfg, uchar **ptrptr, user_t* user, client_t* client)
 					result = !not;
 				break;
 			case AR_GUEST:
-				if (user_is_guest(user))
+				if (!user_is_guest(user))
 					result = not;
 				else
 					result = !not;