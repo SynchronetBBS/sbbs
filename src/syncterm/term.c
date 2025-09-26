b5052ce242827c2c19e1bf4872314c0c7a71a777
Deucе <shurd@sasktel.net>
2025-09-26 02:59:08 -0400

    Remove telstar detection and slowing down
    All Glass TTY nodes have been updated to fix the issue now.

--- src/syncterm/term.c
+++ src/syncterm/term.c
@@ -68,8 +68,6 @@ static struct text_info trans_ti;    // Holds the screen and window size from be
 static struct text_info transw_ti;   // Holds the screen and transfer window
 static struct text_info progress_ti; // Holds the screen and progress window
 static struct text_info log_ti;      // Holds the screen and log info
-static bool is_telstar;              // If Telstar is detected (first string is date as follows: \x0c\x1e\x1e\x1e20250920T0706Z\x0c\x1e\x1e)
-                                     // We will delay 200ms between frames when downloading
 
 static struct ciolib_pixels *pixmap_buffer[2];
 static struct ciolib_mask *mask_buffer;
@@ -1141,7 +1139,7 @@ cet_frame_recv_byte(void *ptr, unsigned timeout)
 		free(sp->orig_screen);
 		sp->orig_screen = NULL;
 		sp->recv_byte = recv_byte_ms;
-		return -1;
+		return sp->recv_byte(ptr, timeout);
 	}
 	uint8_t ret = sp->orig_screen[sp->orig_screen_pos];
 	sp->orig_screen_pos += 2;
@@ -1150,17 +1148,6 @@ cet_frame_recv_byte(void *ptr, unsigned timeout)
 
 #define CET_TS_TIMEOUT_SEC 5
 #define CET_TS_TIMEOUT_MS (CET_TS_TIMEOUT_SEC * 1000)
-/*
- * The original Telesoftware spec required waiting for 2 seconds after
- * the last byte is received to detect an end of frame, which would
- * really slow down transfers, and isn't actually necessary.
- * 
- * However, Telstar 2.0 has a requirement that received # characters be
- * at least 200ms after the previous received character to work around
- * an issue with TNCs (Amateur Packet Radio Systems), so we basically
- * need at least a 200ms delay or transfers won't work reliably.
- */
-#define CET_TS_FRAME_TIMEOUT_MS (is_telstar ? 200 : 1)
 #define CET_TS_RETRIES 3
 
 static void
@@ -1186,7 +1173,7 @@ cet_telesoftware_progress(struct cet_ts_state *sp)
 			cps = 1;           /* cps so far */
 		double fps = ((double)sp->frame_num) / t;
 		if (fps <= 0.0)
-			fps = 0.001 / CET_TS_FRAME_TIMEOUT_MS;           /* fps so far (default to the fastest we can go) */
+			fps = DBL_MIN;     /* Avoid division by zero and denormals */
 		time_t l = (time_t)(sp->frame_count / fps); /* total transfer est time */
 		if (t >= l)
 			l = 0;
@@ -1253,16 +1240,6 @@ cet_append_fb(struct cet_ts_state *sp, int byte)
 	sp->frame_buffer[sp->fb_pos++] = byte;
 }
 
-static void
-cet_telesoftware_end_of_frame(struct cet_ts_state *sp)
-{
-	// Original spec says to wait until two seconds have passed without a character.
-	int byte;
-	while ((byte = sp->recv_byte(sp, CET_TS_FRAME_TIMEOUT_MS)) != -1) {
-		cet_append_fb(sp, byte);
-	}
-}
-
 static struct cet_ts_block *
 cet_telesoftware_try_get_block(struct cet_ts_state *sp)
 {
@@ -1321,7 +1298,7 @@ cet_telesoftware_try_get_block(struct cet_ts_state *sp)
 		cet_append_fb(sp, b);
 		if (got_start && (b > 127 || b < 32)) {
 			free(ret);
-			lprintf(LOG_ERR, "Received illegal byte 0x%02x (high bit set)", b);
+			lprintf(LOG_ERR, "Received illegal byte 0x%02x (%s)", b, b < 32 ? "control character" : "high bit set");
 			return NULL;
 		}
 		if (got_escape) {
@@ -1528,12 +1505,8 @@ cet_telesoftware_get_block(struct cet_ts_state *sp)
 	struct cet_ts_block *ret = NULL;
 	while ((!sp->aborted) && ret == NULL) {
 		ret = cet_telesoftware_try_get_block(sp);
-		if (ret) {
-			if (ret->block_num == ret->block_cnt)
-				cet_telesoftware_end_of_frame(sp);
+		if (ret)
 			return ret;
-		}
-		cet_telesoftware_end_of_frame(sp);
 		if (retries == 0)
 			break;
 		retries--;
@@ -4803,64 +4776,6 @@ finish_scrollback(void)
 	}
 }
 
-/*
- * Telstar sends the current data as metadata at the start of a connection.
- * This allows systems without an RTC so set their clock. This *also* allows
- * is to detect Telstar-2 and slow down Tele4software downloads.
- */
-static bool
-check_if_telstar(const char buf[22], size_t sz)
-{
-	if (sz != 22)
-		return false;
-	if (buf[0] != '\x0c')
-		return false;
-	if (buf[1] != '\x1e')
-		return false;
-	if (buf[2] != '\x1e')
-		return false;
-	if (buf[3] != '\x1e')
-		return false;
-	// Will only work until 29:69 on the 39th day of the 19th month of 9999
-	if (buf[4] < '2' || buf[4] > '9')
-		return false;
-	if (buf[5] < '0' || buf[5] > '9')
-		return false;
-	if (buf[6] < '0' || buf[6] > '9')
-		return false;
-	if (buf[7] < '0' || buf[7] > '9')
-		return false;
-	if (buf[8] < '0' || buf[8] > '1')
-		return false;
-	if (buf[9] < '0' || buf[9] > '9')
-		return false;
-	if (buf[10] < '0' || buf[10] > '3')
-		return false;
-	if (buf[11] < '0' || buf[11] > '9')
-		return false;
-	if (buf[12] != 'T')
-		return false;
-	if (buf[13] < '0' || buf[13] > '2')
-		return false;
-	if (buf[14] < '0' || buf[14] > '9')
-		return false;
-	if (buf[15] < '0' || buf[15] > '6')
-		return false;
-	if (buf[16] < '0' || buf[16] > '9')
-		return false;
-	if (buf[17] != 'Z')
-		return false;
-	if (buf[18] != '\x0c')
-		return false;
-	if (buf[19] != '\x1e')
-		return false;
-	if (buf[20] != '\x1e')
-		return false;
-	if (buf[21] != '\x1e')
-		return false;
-	return true;
-}
-
 bool
 doterm(struct bbslist *bbs)
 {
@@ -4913,9 +4828,6 @@ doterm(struct bbslist *bbs)
 	struct mouse_state ms = {0};
 	int                speedwatch = 0;
 	bool atascii_inverse = false;
-	is_telstar = false;
-	char telstar_buffer[22];
-	size_t telstar_buffer_offset = 0;
 
 	normalize_entry(bbs);
 	freepixels(pixmap_buffer[0]);
@@ -5117,12 +5029,6 @@ doterm(struct bbslist *bbs)
 									remain = 1;
 								speedwatch = 0;
 								break;
-						}
-						if (telstar_buffer_offset < sizeof(telstar_buffer)) {
-							telstar_buffer[telstar_buffer_offset++] = inch;
-							if (telstar_buffer_offset == sizeof(telstar_buffer)) {
-								is_telstar = check_if_telstar(telstar_buffer, telstar_buffer_offset);
-							}
 						}
 						if ((inch == zrqinit[zrqlen]) || (inch == zrinit[zrqlen])) {
 							zrqbuf[zrqlen] = inch;
