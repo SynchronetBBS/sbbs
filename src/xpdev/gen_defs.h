a136e0d08cd0da404a519b01b103378e71e6c2d7
Deucе <shurd@sasktel.net>
2025-10-10 15:45:32 -0400

    Both inttypes.h and stdint.h were in C++11

--- src/xpdev/gen_defs.h
+++ src/xpdev/gen_defs.h
@@ -22,14 +22,10 @@
 #ifndef _GEN_DEFS_H
 #define _GEN_DEFS_H
 
-#if (__STDC_VERSION__ >= 199901L) || (__cplusplus >= 201703L)
+#if (__STDC_VERSION__ >= 199901L) || (__cplusplus >= 201103L)
 # ifndef HAS_INTTYPES_H
 #  define HAS_INTTYPES_H
 # endif
-# ifndef HAS_STDINT_H
-#  define HAS_STDINT_H
-# endif
-#elif __cplusplus >= 201103
 # ifndef HAS_STDINT_H
 #  define HAS_STDINT_H
 # endif
