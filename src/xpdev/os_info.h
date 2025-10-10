4ee81da6b1094ff586d4614021fdcd964ef50ad2
Deucе <shurd@sasktel.net>
2025-10-10 06:59:25 -0400

    C++ needs C code to submit to it's will

--- src/xpdev/os_info.h
+++ src/xpdev/os_info.h
@@ -8,8 +8,16 @@
 	#define OS_CMD_SHELL_ENV_VAR	"COMSPEC"
 #endif
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 DLLEXPORT char*		os_version(char *str, size_t);
 DLLEXPORT char*		os_cpuarch(char *str, size_t);
 DLLEXPORT char*		os_cmdshell(void);
+
+#if defined(__cplusplus)
+}
+#endif
 
 #endif
