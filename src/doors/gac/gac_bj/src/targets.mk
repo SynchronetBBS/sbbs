5953a8a2277dd0fa3688238c89cede2a69ea2736
Deucе <shurd@sasktel.net>
2025-10-13 09:31:39 -0400

    Build .art files as part of build.

--- src/doors/gac/gac_bj/src/targets.mk
+++ src/doors/gac/gac_bj/src/targets.mk
@@ -1,10 +1,6 @@
 GAC_BJ	=	$(EXEODIR)$(DIRSEP)gac_bj$(EXE_SUFFIX)$(EXEFILE)
-ENCRYPT =	$(EXEODIR)$(DIRSEP)encrypt$(EXE_SUFFIX)$(EXEFILE)
 GAC_BJ_DST =	..$(DIRSEP)gac_bj.$(machine)$(EXEFILE)
-ENCRYPT_DST =	..$(DIRSEP)encrypt.$(machine)$(EXEFILE)
 
-all: xpdev-mt ciolib-mt $(MTOBJODIR) $(EXEODIR) $(GAC_BJ) release
+all: xpdev-mt ciolib-mt $(MTOBJODIR) $(EXEODIR) $(GAC_BJ)
 
 $(GAC_BJ):	$(XPDEV-MT_LIB)
-
-release: $(ENCRYPT)
