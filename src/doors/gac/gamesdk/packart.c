9f5732affda1ef9e783abcff069498b6f38f8ee8
Deucе <shurd@sasktel.net>
2025-10-13 09:30:26 -0400

    Add pack/unpack utility for .art files

---
+++ src/doors/gac/gamesdk/packart.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+
+#include "dirwrap.h"
+#include "genwrap.h"
+
+static char line[261];
+
+static void
+usage(const char *argv0)
+{
+	printf("Usage: %s out.file file1.type file2.type...\n\n"
+	     "out.file must not exist, or an error occurs.\n\n"
+	     "at least line fileX.type file is required\n\n"
+	     "Packs fileX.type into out.file.\n", argv0);
+	exit(1);
+}
+
+static void HelpEncrypt(void)
+{
+	char *curp;
+	char tmp;
+
+	if (strlen(line) & 1)
+		strlcat(line, " ", sizeof(line));
+	strrev(line);
+	/*
+	 * The swab() that was used did not work properly when src and dst
+	 * overlapped, only the first two pairs got swapped.
+	 */
+	if (line[0] && line[1]) {
+		tmp = line[0];
+		line[0] = line[1];
+		line[1] = tmp;
+	}
+	if (line[2] && line[3]) {
+		tmp = line[2];
+		line[2] = line[3];
+		line[3] = tmp;
+	}
+
+	curp = &line[0];
+	for (curp = line; *curp; curp++) {
+		if (*curp > 0 )
+			*curp -= 128;
+		else
+			*curp += 128;
+	}
+
+	return;
+}
+
+int
+main(int argc, char **argv)
+{
+	FILE *outfile = NULL;
+	FILE *infile = NULL;
+	int i;
+	int ret = 1;
+	char *p;
+
+	if (argc < 3)
+		usage(argv[0]);
+	if (fexist(argv[1])) {
+		fprintf(stderr, "%s already exists\n", argv[1]);
+		usage(argv[0]);
+	}
+	for (i = 2; i < argc; i++) {
+		if (!fexist(argv[i])) {
+			fprintf(stderr, "%s does not exist\n", argv[i]);
+			usage(argv[0]);
+		}
+	}
+	outfile = fopen(argv[1], "wb");
+	if (outfile == NULL) {
+		fprintf(stderr, "Error %d opening %s\n", errno, argv[1]);
+		goto exit;
+	}
+
+	for (i = 2; i < argc; i++) {
+		infile = fopen(argv[i], "rb");
+		if (infile == NULL) {
+			fprintf(stderr, "Error %d opening %s\n", errno, argv[1]);
+			goto exit;
+		}
+		fputs("@#", outfile);
+		strlcpy(line, argv[i], sizeof(line));
+		p = strrchr(line, '.');
+		if (p)
+			*p = 0;
+		strupr(line);
+		HelpEncrypt();
+		fputs(line, outfile);
+		fputs("\r\n", outfile);
+		while (fscanf(infile, "%[^\r\n]\r\n", line) == 1) {
+			HelpEncrypt();
+			fputs(line, outfile);
+			fputs("\r\n", outfile);
+		}
+		fclose(infile);
+		infile = NULL;
+	}
+	fputs("@#", outfile);
+	memset(line, 0, sizeof(line));
+	HelpEncrypt();
+	fputs(line, outfile);
+	fputs("\r\n", outfile);
+	
+	ret = 0;
+
+exit:
+	if (infile)
+		fclose(infile);
+	if (outfile)
+		fclose(outfile);
+	return ret;
+}
---
+++ src/doors/gac/gamesdk/unpackart.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "dirwrap.h"
+#include "genwrap.h"
+
+static char line[261];
+
+static void
+usage(const char *argv0)
+{
+	printf("Usage: %s in_ext.file\n\n"
+	     "File in in.file must not exist, or an error occurs.\n\n"
+	     "Unpacks files from in_ext.file into NAME.ext.\n", argv0);
+	exit(1);
+}
+
+static void
+HelpDecrypt(char *line)
+{
+	char *curp;
+	char tmp;
+
+	curp = &line[0];
+	while (curp[0] != '\0')
+	{
+
+				if (curp[0] > 0 )
+						curp[0] -= 128;
+				else
+						curp[0] += 128; //should be 127
+		curp++;
+
+	}
+
+	/*
+	 * The swab() that was used did not work properly when src and dst
+	 * overlapped, only the first two pairs got swapped.
+	 */
+	if (line[0] && line[1]) {
+		tmp = line[0];
+		line[0] = line[1];
+		line[1] = tmp;
+	}
+	if (line[2] && line[3]) {
+		tmp = line[2];
+		line[2] = line[3];
+		line[3] = tmp;
+	}
+	//swab(line, line, sizeof(line));
+	//strcpy(line, strrev(line));
+	strrev(line);
+
+	// if there are blanks on the end remove them...
+
+	while (curp[strlen(curp)-1] == ' ')
+	{
+		curp[strlen(curp)-1] = '\0';
+	}
+
+	return;
+}
+
+int
+main(int argc, char **argv)
+{
+	FILE *outfile = NULL;
+	FILE *infile = NULL;
+	int ret = 1;
+	char *p;
+	char *ext = NULL;
+	char *dupfname = NULL;
+
+	if (argc != 2)
+		usage(argv[0]);
+	infile = fopen(argv[1], "rb");
+	if (infile == NULL) {
+		fprintf(stderr, "Error %d opening %s\n", errno, argv[1]);
+		goto exit;
+	}
+	dupfname = strdup(argv[1]);
+	p = getfname(dupfname);
+	if (p) {
+		ext = getfext(p);
+		if (ext) {
+			*ext = 0;
+			ext = strrchr(p, '_');
+			if (ext) {
+				ext++;
+			}
+			else {
+				FREE_AND_NULL(dupfname);
+				ext = NULL;
+			}
+		}
+		else {
+			FREE_AND_NULL(dupfname);
+			ext = NULL;
+		}
+	}
+	else
+		FREE_AND_NULL(dupfname);
+	if (!ext) {
+		fputs("Unable to get extension, using .out\n", stderr);
+		ext = ".out";
+	}
+
+	while (fscanf(infile, "%[^\r\n]\r\r", line) == 1) {
+		if (line[0] == '@' && line[1] == '#') {
+			if (outfile)
+				fclose(outfile);
+			HelpDecrypt(&line[2]);
+			strlcat(line, ext, sizeof(line));
+			if (strlen(line) < 2)
+				fprintf(stderr, "Skipping: %02x, %02x (%s)\n", line[0], line[1], line);
+			if (fexist(line)) {
+				fprintf(stderr, "%s already exists, skipping\n", line);
+			}
+			else {
+				outfile = fopen(line, "wb");
+				if (outfile == NULL) {
+					fprintf(stderr, "Error %d opening %s\n", errno, line);
+				}
+			}
+		}
+		else {
+			if (outfile) {
+				HelpDecrypt(line);
+				fputs(line, outfile);
+				fputs("\r\n", outfile);
+			}
+		}
+	}
+	
+	ret = 0;
+
+exit:
+	free(dupfname);
+	if (infile)
+		fclose(infile);
+	if (outfile)
+		fclose(outfile);
+	return ret;
+}
