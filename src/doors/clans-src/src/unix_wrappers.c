8b6a71df37d2c7e44d7dede008f42fcd94992064
Deucе <shurd@sasktel.net>
2025-10-15 12:08:17 -0400

    Whoops, missed one.

--- src/doors/clans-src/src/unix_wrappers.c
+++ src/doors/clans-src/src/unix_wrappers.c
@@ -123,7 +123,8 @@ findnext(struct ffblk *fblk)
 			(*fblk).ff_fdate=0;
 			(*fblk).ff_fsize=file_stat.st_size;
 
-			if (file_stat.st_mode&S_IFDIR)(*fblk).ff_attrib=FA_DIREC;
+			if (S_ISDIR(file_stat.st_mode))
+				(*fblk).ff_attrib=FA_DIREC;
 			if (strrchr(dp->d_name,'/')!=NULL)
 				strcpy((*fblk).ff_name,strrchr(dp->d_name,'/')+1);
 			else
