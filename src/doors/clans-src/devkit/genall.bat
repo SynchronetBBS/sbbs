a10d1069b6e3d217b39fe6649053be16d62f7bcd
Deucе <shurd@sasktel.net>
2025-10-15 23:28:38 -0400

    Put the devkit files in devkit dir
    These are only the files that aren't duplicates of one in the data
    dir.

---
+++ src/doors/clans-src/devkit/genall.bat
@@ -0,0 +1,13 @@
+langcomp strings.txt > TMP.FIL
+mcomp eventmon.txt event.mon >> TMP.FIL
+mcomp monsters.txt output.mon >> TMP.FIL
+mspells spells.txt spells.dat >> TMP.FIL
+mitems items.txt items.dat >> TMP.FIL
+mclass >>TMP.FIL
+makenpc npcs.txt clans.npc >>TMP.FIL
+ecomp pray.evt pray.e >>TMP.FIL
+ecomp eva.txt eva.e >> TMP.FIL
+ecomp quests.evt quests.e >> TMP.FIL
+ecomp church.evt church.e >> TMP.FIL
+ecomp secret.evt secret.e >> TMP.FIL
+makepak clans.pak pak.lst >> TMP.FIL
