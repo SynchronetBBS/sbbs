9219bd10198f9d163b033d23c68601db829b85bd
Deucе <shurd@sasktel.net>
2025-10-18 18:17:58 -0400

    Add wrappers for Windows and !Windows

---
+++ src/doors/clans-src/src/wrapper.cmd
@@ -0,0 +1,3 @@
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set PLAT=.win32.x86.|| set PLAT=.win64.amd64.
+IF "%CLANS_DEBUG%"=="" set TYPE=opt|| set TYPE=debug
+%~dpn0%PLAT%%TYPE%.exe
