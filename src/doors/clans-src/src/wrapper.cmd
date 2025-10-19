a3d18d7b263413521fbb6d9e51969c6d37a64ced
Deucе <shurd@sasktel.net>
2025-10-19 02:27:37 -0400

    Make the variables local

--- src/doors/clans-src/src/wrapper.cmd
+++ src/doors/clans-src/src/wrapper.cmd
@@ -1,3 +1,5 @@
+setlocal
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set PLAT=.win32.x86.|| set PLAT=.win64.amd64.
 IF "%CLANS_DEBUG%"=="" set TYPE=opt|| set TYPE=debug
 %~dpn0%PLAT%%TYPE%.exe %*
+endlocal
