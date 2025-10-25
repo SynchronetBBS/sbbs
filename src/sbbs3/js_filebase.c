SynchronetBBS/sbbs

commit 3949a52dfb4b5af715253dc457ba0d5643e3e74d

Move 'is_path' argument after code_or_path argument in Msg|FileBase ctors
This makes the change in commit 93b4d946cc12ad15f15773afa42f4cee988b5d2f
backward compatible with previous versions of SBBS: scripts  that add a second
(true) argument when calling these constructors will still work with older
versions of sbbs.

Added argument count verification too: a minimum of one argument is expected
to these constructors, so throw the proper exception in those cases.

src/sbbs3/js_filebase.c
@@ -1815,6 +1815,9 @@ js_filebase_constructor(JSContext *cx, uintN argc, jsval *arglist)
 	private_t* p;
 	scfg_t*    scfg;
 
+	if (js_argcIsInsufficient(cx, argc, 1))
+		return JS_FALSE;
+
 	scfg = JS_GetRuntimePrivate(JS_GetRuntime(cx));
 
 	obj = JS_NewObject(cx, &js_filebase_class, NULL, NULL);
@@ -1828,11 +1831,8 @@ js_filebase_constructor(JSContext *cx, uintN argc, jsval *arglist)
 	p->smb.retry_time = scfg->smb_retry_time;
 
 	int argn = 0;
-	if (JSVAL_IS_BOOLEAN(argv[argn])) {
-		is_path = JSVAL_TO_BOOLEAN(argv[argn]);
-		++argn;
-	}
 	JSVALUE_TO_STRBUF(cx, argv[argn], base, sizeof base, NULL);
+	++argn;
 	if (JS_IsExceptionPending(cx)) {
 		free(p);
 		return JS_FALSE;
@@ -1841,6 +1841,10 @@ js_filebase_constructor(JSContext *cx, uintN argc, jsval *arglist)
 		JS_ReportError(cx, "Invalid 'path_or_code' parameter");
 		free(p);
 		return JS_FALSE;
+	}
+	if (argn < argc && JSVAL_IS_BOOLEAN(argv[argn])) {
+		is_path = JSVAL_TO_BOOLEAN(argv[argn]);
+		++argn;
 	}
 
 	if (!JS_SetPrivate(cx, obj, p)) {
@@ -1852,8 +1856,8 @@ js_filebase_constructor(JSContext *cx, uintN argc, jsval *arglist)
 #ifdef BUILD_JSDOCS
 	js_DescribeSyncObject(cx, obj, "Class used for accessing file databases", 31900);
 	js_DescribeSyncConstructor(cx, obj, "To create a new FileBase object: "
-	                           "<tt>var filebase = new FileBase([<i>bool</i> is_path=false,] <i>string</i> path_or_code)</tt><br>"
-	                           "where <i>path_or_code</i> is a directory internal code, or ...<br>"
+	                           "<tt>var filebase = new FileBase(<i>string</i> code_or_path, [<i>bool</i> is_path=false])</tt><br>"
+	                           "where <i>code_or_path</i> is a file directory internal code, or ...<br>"
 	                           "when <i>is_path</i> is <tt>true</tt>, the string parameter specifies the path to a file base (not an internal code) - new behavior in v3.21."
 	                           );
 	js_CreateArrayOfStrings(cx, obj, "_property_desc_list", filebase_prop_desc, JSPROP_READONLY);

src/sbbs3/js_msgbase.c
@@ -3350,6 +3350,9 @@ js_msgbase_constructor(JSContext *cx, uintN argc, jsval *arglist)
 	private_t* p;
 	scfg_t*    scfg;
 
+	if (js_argcIsInsufficient(cx, argc, 1))
+		return JS_FALSE;
+
 	scfg = JS_GetRuntimePrivate(JS_GetRuntime(cx));
 
 	obj = JS_NewObject(cx, &js_msgbase_class, NULL, NULL);
@@ -3363,11 +3366,8 @@ js_msgbase_constructor(JSContext *cx, uintN argc, jsval *arglist)
 	p->smb.retry_time = scfg->smb_retry_time;
 
 	int argn = 0;
-	if (JSVAL_IS_BOOLEAN(argv[argn])) {
-		is_path = JSVAL_TO_BOOLEAN(argv[argn]);
-		++argn;
-	}
 	JSVALUE_TO_STRBUF(cx, argv[argn], base, sizeof base, NULL);
+	++argn;
 	if (JS_IsExceptionPending(cx)) {
 		free(p);
 		return JS_FALSE;
@@ -3378,6 +3378,10 @@ js_msgbase_constructor(JSContext *cx, uintN argc, jsval *arglist)
 		return JS_FALSE;
 	}
 
+	if (argn < argc && JSVAL_IS_BOOLEAN(argv[argn])) {
+		is_path = JSVAL_TO_BOOLEAN(argv[argn]);
+		++argn;
+	}
 	p->debug = JS_FALSE;
 
 	if (!JS_SetPrivate(cx, obj, p)) {
@@ -3389,8 +3393,8 @@ js_msgbase_constructor(JSContext *cx, uintN argc, jsval *arglist)
 #ifdef BUILD_JSDOCS
 	js_DescribeSyncObject(cx, obj, "Class used for accessing message bases", 310);
 	js_DescribeSyncConstructor(cx, obj, "To create a new MsgBase object: "
-	                           "<tt>var msgbase = new MsgBase([<i>bool</i> is_path=false,] <i>string</i> path_or_code)</tt><br>"
-	                           "where <i>code</i> is a sub-board internal code, or <tt>'mail'</tt> for the e-mail message base.<br>"
+	                           "<tt>var msgbase = new MsgBase(<i>string</i> code_or_path, [<i>bool</i> is_path=false])</tt><br>"
+	                           "where <i>code_or_path</i> is a sub-board internal code, or <tt>'mail'</tt> for the e-mail message base.<br>"
 	                           "When <i>is_path</i> is <tt>true</tt>, the string parameter specifies the path to a message base (not an internal code) - new behavior in v3.21."
 	                           "<p>"
 	                           "The MsgBase retrieval methods that accept a <tt>by_offset</tt> argument as their optional first boolean argument "

commit 4c0f23ba492237d2d1288ff145cdf5ad282e1b2b

Allow FileBase() to be invoked without arguments, filelist.js assumes you can
Commit 3949a52dfb4 broke filelist.js (for non-JSON output)

src/sbbs3/js_filebase.c
@@ -1815,9 +1815,6 @@ js_filebase_constructor(JSContext *cx, uintN argc, jsval *arglist)
 	private_t* p;
 	scfg_t*    scfg;
 
-	if (js_argcIsInsufficient(cx, argc, 1))
-		return JS_FALSE;
-
 	scfg = JS_GetRuntimePrivate(JS_GetRuntime(cx));
 
 	obj = JS_NewObject(cx, &js_filebase_class, NULL, NULL);
@@ -1830,21 +1827,23 @@ js_filebase_constructor(JSContext *cx, uintN argc, jsval *arglist)
 	memset(p, 0, sizeof(private_t));
 	p->smb.retry_time = scfg->smb_retry_time;
 
-	uintN argn = 0;
-	JSVALUE_TO_STRBUF(cx, argv[argn], base, sizeof base, NULL);
-	++argn;
-	if (JS_IsExceptionPending(cx)) {
-		free(p);
-		return JS_FALSE;
-	}
-	if (*base == '\0') {
-		JS_ReportError(cx, "Invalid 'path_or_code' parameter");
-		free(p);
-		return JS_FALSE;
-	}
-	if (argn < argc && JSVAL_IS_BOOLEAN(argv[argn])) {
-		is_path = JSVAL_TO_BOOLEAN(argv[argn]);
+	if(argc >= 1) {
+		uintN argn = 0;
+		JSVALUE_TO_STRBUF(cx, argv[argn], base, sizeof base, NULL);
 		++argn;
+		if (JS_IsExceptionPending(cx)) {
+			free(p);
+			return JS_FALSE;
+		}
+		if (*base == '\0') {
+			JS_ReportError(cx, "Invalid 'path_or_code' parameter");
+			free(p);
+			return JS_FALSE;
+		}
+		if (argn < argc && JSVAL_IS_BOOLEAN(argv[argn])) {
+			is_path = JSVAL_TO_BOOLEAN(argv[argn]);
+			++argn;
+		}
 	}
 
 	if (!JS_SetPrivate(cx, obj, p)) {
@@ -1863,14 +1862,16 @@ js_filebase_constructor(JSContext *cx, uintN argc, jsval *arglist)
 	js_CreateArrayOfStrings(cx, obj, "_property_desc_list", filebase_prop_desc, JSPROP_READONLY);
 #endif
 
-	if (is_path) {
-		p->smb.dirnum = INVALID_DIR;
-		SAFECOPY(p->smb.file, base);
-	} else if (dirnum_is_valid(scfg, p->smb.dirnum = getdirnum(scfg, base))) {
-		safe_snprintf(p->smb.file, sizeof(p->smb.file), "%s%s"
-		              , scfg->dir[p->smb.dirnum]->data_dir, scfg->dir[p->smb.dirnum]->code);
-	} else { /* unknown code */
-		JS_ReportError(cx, "Unrecognized file area (directory) internal code: '%s'", base);
+	if(argc >= 1) {
+		if (is_path) {
+			p->smb.dirnum = INVALID_DIR;
+			SAFECOPY(p->smb.file, base);
+		} else if (dirnum_is_valid(scfg, p->smb.dirnum = getdirnum(scfg, base))) {
+			safe_snprintf(p->smb.file, sizeof(p->smb.file), "%s%s"
+						  , scfg->dir[p->smb.dirnum]->data_dir, scfg->dir[p->smb.dirnum]->code);
+		} else { /* unknown code */
+			JS_ReportError(cx, "Unrecognized file area (directory) internal code: '%s'", base);
+		}
 	}
 
 	return JS_TRUE;