SynchronetBBS/sbbs

commit 9bf86ab10c206945417c45d98f096c758a1ecf7f

Store original "references" header field as an RFC822HEADER in SMB
If an original references header field was supplied with the message, include
*that* in the header (since it should contain all the IDs of the replied-to
message thread) rather than just the message's reply_id (a single message
ID) when sending via NNTP. This maintains more NNTP post integrity when
sending back out via NNTP.

Not sure why we were storing received "references" in the hdr.references
returned by parse_news_header(), we don't seem to be using it anywere. Maybe
copied from 822header.js or mailproc_util.js? Seems like cruft, so remove it.

Also, don't ever send an "In-Reply-To" heder field via NNTP (it's not valid
/used for NNTP articles, per RFC 5536).

exec/load/newsutil.js
@@ -32,8 +32,6 @@ function write_news_header(hdr,writeln)
 		writeln("Newsgroups: " + hdr.newsgroups);
 	if(hdr.replyto!=undefined)
 		writeln("Reply-To: " + hdr.replyto);
-	if(hdr.reply_id!=undefined)
-		writeln("References: " + hdr.reply_id);
 
 	/* FidoNet header fields */
 	if(hdr.ftn_area!=undefined)
@@ -53,6 +51,7 @@ function write_news_header(hdr,writeln)
 
 	var content_type;
 	var user_agent;
+	var references;
 
 	if(hdr.field_list!=undefined) {
 		for(var i in hdr.field_list) {
@@ -61,6 +60,8 @@ function write_news_header(hdr,writeln)
 					content_type = hdr.field_list[i].data;
 				if(hdr.field_list[i].data.toLowerCase().indexOf("user-agent:")==0)
 					user_agent = hdr.field_list[i].data;
+				if(hdr.field_list[i].data.toLowerCase().indexOf("references:")==0)
+					references = hdr.field_list[i].data;
 				writeln(hdr.field_list[i].data);
 			} else if(hdr.field_list[i].type==FIDOCTRL) {
 				writeln("X-FTN-Kludge: " + hdr.field_list[i].data);
@@ -72,6 +73,8 @@ function write_news_header(hdr,writeln)
 		}
 	}
 
+	if(references === undefined && hdr.reply_id !== undefined)
+		writeln("References: " + hdr.reply_id);
 	if(user_agent === undefined && hdr.editor !== undefined)
 		writeln("User-Agent: " + hdr.editor);
 
@@ -151,9 +154,6 @@ function parse_news_header(hdr, line)
 			hdr.replyto_net_type=NET_INTERNET;
 			hdr.replyto=data;
 			break;
-		case "in-reply-to":
-			hdr.reply_id=data;
-			break;
 		case "date":
 			hdr.date=data;
 			break;
@@ -162,11 +162,6 @@ function parse_news_header(hdr, line)
 			break;
 		case "message-id":
 			hdr.id=data;
-			break;
-		case "references":
-			hdr.references=data;
-			if(!hdr.reply_id && data.length)
-				hdr.reply_id=data.match(/<[^\<]*>$/);
 			break;
 		case "user-agent":
 			hdr.editor=data;
@@ -203,6 +198,10 @@ function parse_news_header(hdr, line)
 			hdr.control=data;
 			break;
 
+		case "references":
+			if(!hdr.reply_id && data.length)
+				hdr.reply_id = data.match(/<[^\<]*>$/);
+			// fall-through
 		default:
 			if(hdr.field_list==undefined)
 				hdr.field_list=[];

commit faa6f9fb839ad90a6836828ae05e03b9104f4e61

Don't make reply_id an array when parsing "References" header field
Bug introduced in commit 9bf86ab10c206945417c45d98f096c758a1ecf7f

Reported and root-caused by Carlos Navarro (2:341/234.1) - thank you!

exec/load/newsutil.js
@@ -235,8 +235,11 @@ function parse_news_header(hdr, line)
 			break;
 
 		case "references":
-			if(!hdr.reply_id && data.length)
-				hdr.reply_id = data.match(/<[^\<]*>$/);
+			if(!hdr.reply_id && data.length) {
+				var match = data.match(/<[^\<]*>$/);
+				if(match !== null && match.length)
+					hdr.reply_id = match[0];
+			}
 			// fall-through
 		default:
 			if(hdr.field_list==undefined)