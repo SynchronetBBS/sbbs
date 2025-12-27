SynchronetBBS/sbbs

commit 95596962fd27cb2dc5debe1d172afafdf6333cc0

Replace sprintf() calls with snprintf()
Addresses Clang warnings about deprecation

src/sbbs3/js_console.cpp
@@ -810,7 +810,7 @@ js_getstr(JSContext *cx, uintN argc, jsval *arglist)
 			free(p);
 			return JS_FALSE;
 		}
-		sprintf(p,"%.*s",(int)maxlen,p2);
+		strlcpy(p, p2, maxlen);
 		free(p2);
 	}
 

src/sbbs3/atcodes.cpp
@@ -491,7 +491,7 @@ const char* sbbs_t::atcode(const char* sp, char* str, size_t maxlen, int* pmode,
 			up = now-uptime;
 		char   days[64]="";
 		if((up/(24*60*60))>=2) {
-	        sprintf(days,"%u days ",(uint)(up/(24L*60L*60L)));
+	        snprintf(days, sizeof days, "%u days ",(uint)(up/(24L*60L*60L)));
 			up%=(24*60*60);
 		}
 		safe_snprintf(str,maxlen,"%s%u:%02u"
@@ -509,7 +509,7 @@ const char* sbbs_t::atcode(const char* sp, char* str, size_t maxlen, int* pmode,
 	}
 
 	if(!strcmp(sp,"SOCKET_LIB"))
-		return(socklib_version(str,SOCKLIB_DESC));
+		return(socklib_version(str, maxlen, SOCKLIB_DESC));
 
 	if(!strcmp(sp,"MSG_LIB")) {
 		safe_snprintf(str,maxlen,"SMBLIB %s",smb_lib_ver());

src/sbbs3/chat.cpp
@@ -62,7 +62,7 @@ void sbbs_t::multinodechat(int channel)
 	bprintf(text[WelcomeToChannelN],channel,cfg.chan[channel-1]->name);
 	if(cfg.chan[channel-1]->misc&CHAN_GURU && cfg.chan[channel-1]->guru<cfg.total_gurus
 		&& chk_ar(cfg.guru[cfg.chan[channel-1]->guru]->ar,&useron,&client)) {
-		sprintf(str,"%s%s.dat",cfg.ctrl_dir,cfg.guru[cfg.chan[channel-1]->guru]->code);
+		snprintf(str, sizeof str, "%s%s.dat",cfg.ctrl_dir,cfg.guru[cfg.chan[channel-1]->guru]->code);
 		if((file=nopen(str,O_RDONLY))==-1) {
 			errormsg(WHERE,ERR_OPEN,str,O_RDONLY);
 			return;
@@ -128,7 +128,7 @@ void sbbs_t::multinodechat(int channel)
 				if(j==usrs) {
 					getnodedat(preusr[i], &node);
 					if(node.misc&NODE_ANON)
-						sprintf(str,"%.80s",text[UNKNOWN_USER]);
+						snprintf(str, sizeof str, "%.80s",text[UNKNOWN_USER]);
 					else
 						username(&cfg,node.useron,str);
 					bprintf(text[NodeLeftMultiChat]
@@ -150,7 +150,7 @@ void sbbs_t::multinodechat(int channel)
 				if(j==preusrs) {
 					getnodedat(usr[i], &node);
 					if(node.misc&NODE_ANON)
-						sprintf(str,"%.80s",text[UNKNOWN_USER]);
+						snprintf(str, sizeof str, "%.80s",text[UNKNOWN_USER]);
 					else
 						username(&cfg,node.useron,str);
 					bprintf(text[NodeJoinedMultiChat]
@@ -217,7 +217,7 @@ void sbbs_t::multinodechat(int channel)
 						&& cfg.chan[savch-1]->guru<cfg.total_gurus
 						&& chk_ar(cfg.guru[cfg.chan[savch-1]->guru]->ar,&useron,&client
 						)) {
-						sprintf(str,"%s%s.dat",cfg.ctrl_dir
+						snprintf(str, sizeof str, "%s%s.dat",cfg.ctrl_dir
 							,cfg.guru[cfg.chan[savch-1]->guru]->code);
 						if((file=nopen(str,O_RDONLY))==-1) {
 							errormsg(WHERE,ERR_OPEN,str,O_RDONLY);
@@ -341,7 +341,7 @@ void sbbs_t::multinodechat(int channel)
 						if(!getstr(line,66,K_LINE|K_MSG))
 							break;
 
-						sprintf(buf,text[ChatLineFmt]
+						snprintf(buf, sizeof buf, text[ChatLineFmt]
 							,thisnode.misc&NODE_ANON
 							? text[AnonUserChatHandle]
 							: useron.handle
@@ -405,12 +405,12 @@ void sbbs_t::multinodechat(int channel)
 					if(!getstr(line,66,K_WRAP|K_MSG|K_CHAT))
 						break;
 					if(j) {
-						sprintf(str,text[ChatLineFmt]
+						snprintf(str, sizeof str, text[ChatLineFmt]
 							,thisnode.misc&NODE_ANON
 							? text[AnonUserChatHandle]
 							: useron.handle
 							,cfg.node_num,':',nulstr);
-						sprintf(tmp,"%*s",(int)bstrlen(str),nulstr);
+						snprintf(tmp, sizeof tmp, "%*s",(int)bstrlen(str),nulstr);
 						SAFECAT(pgraph,tmp);
 					}
 					SAFECAT(pgraph,line);
@@ -425,10 +425,10 @@ void sbbs_t::multinodechat(int channel)
 							if(cfg.chatact[i]->actset
 								!=cfg.chan[channel-1]->actset)
 								continue;
-							sprintf(str,"%s ",cfg.chatact[i]->cmd);
+							snprintf(str, sizeof str, "%s ",cfg.chatact[i]->cmd);
 							if(!strnicmp(str,pgraph,strlen(str)))
 								break;
-							sprintf(str,"%.*s"
+							snprintf(str, sizeof str, "%.*s"
 								,LEN_CHATACTCMD+2,pgraph);
 							str[strlen(str)-2]=0;
 							if(!stricmp(cfg.chatact[i]->cmd,str))
@@ -474,7 +474,7 @@ void sbbs_t::multinodechat(int channel)
 
 							if(usrs && j<usrs) {
 								/* Display to dest user */
-								sprintf(buf,cfg.chatact[i]->out
+								snprintf(buf, sizeof buf, cfg.chatact[i]->out
 									,thisnode.misc&NODE_ANON
 									? text[UNKNOWN_USER] : useron.alias
 									,"you");
@@ -484,7 +484,7 @@ void sbbs_t::multinodechat(int channel)
 
 
 							/* Display to all other users */
-							sprintf(buf,cfg.chatact[i]->out
+							snprintf(buf, sizeof buf, cfg.chatact[i]->out
 								,thisnode.misc&NODE_ANON
 								? text[UNKNOWN_USER] : useron.alias
 								,str);
@@ -504,7 +504,7 @@ void sbbs_t::multinodechat(int channel)
 						}
 					}
 
-					sprintf(buf,text[ChatLineFmt]
+					snprintf(buf, sizeof buf, text[ChatLineFmt]
 						,thisnode.misc&NODE_ANON
 						? text[AnonUserChatHandle]
 						: useron.handle
@@ -560,7 +560,7 @@ bool sbbs_t::guru_page(void)
 		if(i<0)
 			return(false);
 	}
-	sprintf(path,"%s%s.dat",cfg.ctrl_dir,cfg.guru[i]->code);
+	snprintf(path, sizeof path, "%s%s.dat",cfg.ctrl_dir,cfg.guru[i]->code);
 	if((file=nopen(path,O_RDONLY))==-1) {
 		errormsg(WHERE,ERR_OPEN,path,O_RDONLY);
 		return(false);
@@ -730,11 +730,11 @@ void sbbs_t::privchat(bool forced, int node_num)
 				bprintf(text[PagingUser]
 					,node.misc&NODE_ANON ? text[UNKNOWN_USER] : username(&cfg,node.useron,tmp)
 					,node.misc&NODE_ANON ? 0 : node.useron);
-				sprintf(str,text[NodePChatPageMsg]
+				snprintf(str, sizeof str, text[NodePChatPageMsg]
 					,cfg.node_num,thisnode.misc&NODE_ANON
 						? text[UNKNOWN_USER] : useron.alias);
 				putnmsg(n,str);
-				sprintf(str,"paged %s on node %d to private chat"
+				snprintf(str, sizeof str, "paged %s on node %d to private chat"
 					,username(&cfg,node.useron,tmp),n);
 				logline("C",str);
 			}
@@ -801,16 +801,16 @@ void sbbs_t::privchat(bool forced, int node_num)
 			bputs(text[WelcomeToPrivateChat]);
 	}
 
-	sprintf(outpath,"%schat.dab",cfg.node_dir);
+	snprintf(outpath, sizeof outpath, "%schat.dab",cfg.node_dir);
 	if((out=sopen(outpath,O_RDWR|O_CREAT|O_BINARY,SH_DENYNO,DEFFILEMODE))==-1) {
 		errormsg(WHERE,ERR_OPEN,outpath,O_RDWR|O_DENYNONE|O_CREAT);
 		return;
 	}
 
 	if(forced && n == 0)
-		sprintf(inpath,"%slchat.dab",cfg.node_dir);
+		snprintf(inpath, sizeof inpath, "%slchat.dab",cfg.node_dir);
 	else
-		sprintf(inpath,"%schat.dab",cfg.node_path[n-1]);
+		snprintf(inpath, sizeof inpath, "%schat.dab",cfg.node_path[n-1]);
 	if(!fexist(inpath))		/* Wait while it's created for the first time */
 		mswait(2000);
 	if((in=sopen(inpath,O_RDWR|O_CREAT|O_BINARY,SH_DENYNO,DEFFILEMODE))==-1) {
@@ -1191,7 +1191,7 @@ int sbbs_t::getnodetopage(int all, int telegram)
 	}
 
 	if(!lastnodemsguser[0])
-		sprintf(lastnodemsguser,"%u",lastnodemsg);
+		snprintf(lastnodemsguser, sizeof lastnodemsguser, "%u",lastnodemsg);
 
 	if(!j && !telegram) {
 		bputs(text[NoOtherActiveNodes]);
@@ -1199,9 +1199,9 @@ int sbbs_t::getnodetopage(int all, int telegram)
 	}
 
 	if(all)
-		sprintf(str,text[NodeToSendMsgTo],lastnodemsg);
+		snprintf(str, sizeof str, text[NodeToSendMsgTo],lastnodemsg);
 	else
-		sprintf(str,text[NodeToPrivateChat],lastnodemsg);
+		snprintf(str, sizeof str, text[NodeToPrivateChat],lastnodemsg);
 	mnemonics(str);
 
 	SAFECOPY(str,lastnodemsguser);
@@ -1354,7 +1354,7 @@ void sbbs_t::nodemsg()
 				now=time(NULL);
 				bprintf(text[SendingTelegramToUser]
 					,username(&cfg,usernumber,tmp),usernumber);
-				sprintf(buf,text[TelegramFmt]
+				snprintf(buf, sizeof buf, text[TelegramFmt]
 					,thisnode.misc&NODE_ANON ? text[UNKNOWN_USER] : useron.alias
 					,timestr(now));
 				i=0;
@@ -1379,7 +1379,7 @@ void sbbs_t::nodemsg()
 					break;
 				}
 				putsmsg(usernumber,buf);
-				sprintf(str,"sent telegram to %s #%u"
+				snprintf(str, sizeof str, "sent telegram to %s #%u"
 					,username(&cfg,usernumber,tmp),usernumber);
 				logline("C",str);
 				logline(nulstr,logbuf);
@@ -1405,14 +1405,14 @@ void sbbs_t::nodemsg()
 						bputs(text[NodeMsgPrompt]);
 						if(!getstr(line,69,K_LINE))
 							break;
-						sprintf(buf,text[NodeMsgFmt],cfg.node_num
+						snprintf(buf, sizeof buf, text[NodeMsgFmt],cfg.node_num
 							,thisnode.misc&NODE_ANON
 								? text[UNKNOWN_USER] : useron.alias,line);
 						putnmsg(i,buf);
 						if(!(node.misc&NODE_ANON))
 							bprintf(text[MsgSentToUser],"Message"
 								,username(&cfg,usernumber,tmp),usernumber);
-						sprintf(str,"sent message to %s on node %d:"
+						snprintf(str, sizeof str, "sent message to %s on node %d:"
 							,username(&cfg,usernumber,tmp),i);
 						logline("C",str);
 						logline(nulstr,line);
@@ -1422,7 +1422,7 @@ void sbbs_t::nodemsg()
 					bputs(text[NodeMsgPrompt]);
 					if(!getstr(line,70,K_LINE))
 						break;
-					sprintf(buf,text[AllNodeMsgFmt],cfg.node_num
+					snprintf(buf, sizeof buf, text[AllNodeMsgFmt],cfg.node_num
 						,thisnode.misc&NODE_ANON
 							? text[UNKNOWN_USER] : useron.alias,line);
 					for(i=1;i<=cfg.sys_nodes;i++) {
@@ -1613,7 +1613,7 @@ void sbbs_t::guruchat(char* line, char* gurubuf, int gurunum, char* last_answer)
 							SAFECAT(theanswer,cfg.sys_id);
 							break;
 						case 'J':
-							sprintf(tmp,"%u",tm.tm_mday);
+							snprintf(tmp, sizeof tmp, "%u",tm.tm_mday);
 							break;
 						case 'L':
                     		if(sys_status&SS_USERON) {
@@ -1656,7 +1656,7 @@ void sbbs_t::guruchat(char* line, char* gurubuf, int gurunum, char* last_answer)
 							SAFECAT(theanswer,cfg.sys_name);
 							break;
 						case 'T':
-							sprintf(tmp,"%u:%02u",tm.tm_hour>12 ? tm.tm_hour-12
+							snprintf(tmp, sizeof tmp, "%u:%02u",tm.tm_hour>12 ? tm.tm_hour-12
 								: tm.tm_hour,tm.tm_min);
 							SAFECAT(theanswer,tmp);
 							break;
@@ -1671,7 +1671,7 @@ void sbbs_t::guruchat(char* line, char* gurubuf, int gurunum, char* last_answer)
 							SAFECAT(theanswer,weekday[tm.tm_wday]);
 							break;
 						case 'Y':   /* Current year */
-							sprintf(tmp,"%u",1900+tm.tm_year);
+							snprintf(tmp, sizeof tmp, "%u",1900+tm.tm_year);
 							SAFECAT(theanswer,tmp);
 							break;
 						case 'Z':
@@ -1747,7 +1747,7 @@ void sbbs_t::guruchat(char* line, char* gurubuf, int gurunum, char* last_answer)
 					,cfg.sys_nodes+1,':',theanswer);
 			}
 			CRLF;
-			sprintf(str,"%sguru.log",cfg.logs_dir);
+			snprintf(str, sizeof str, "%sguru.log",cfg.logs_dir);
 			if((fp = fopenlog(&cfg, str)) == NULL)
 				errormsg(WHERE,ERR_OPEN,str,O_WRONLY|O_CREAT|O_APPEND);
 			else {

src/sbbs3/con_hi.cpp
@@ -75,7 +75,7 @@ int sbbs_t::uselect(bool add, uint n, const char *title, const char *item, const
 			break;
 	if(u==uselect_total)
 		u=0;
-	sprintf(str,text[SelectItemWhich],u+1);
+	snprintf(str, sizeof str, text[SelectItemWhich],u+1);
 	mnemonics(str);
 	i=getnum(uselect_total);
 	t=uselect_total;

src/sbbs3/con_out.cpp
@@ -351,7 +351,7 @@ size_t sbbs_t::print_utf8_as_cp437(const char* str, size_t len)
 		outchar(CP437_INVERTED_QUESTION_MARK);
 		char seq[32] = "";
 		for(size_t i = 0; i < len; i++)
-			sprintf(seq + strlen(seq), "%02X ", (uchar)*(str + i));
+			snprintf(seq + strlen(seq), 4, "%02X ", (uchar)*(str + i));
 		lprintf(LOG_DEBUG, "Unsupported UTF-8 sequence: %s (U+%X)", seq, codepoint);
 	}
 	return len;

src/sbbs3/exec.cpp
@@ -1467,7 +1467,7 @@ int sbbs_t::exec(csi_t *csi)
 					csi->logic=LOGIC_FALSE; 
 				}
 				else {
-					sprintf(csi->str,"%lu",l);
+					snprintf(csi->str, 128, "%lu",l);
 					csi->logic=LOGIC_TRUE; 
 				}
 				return(0);
@@ -1657,7 +1657,7 @@ int sbbs_t::exec(csi_t *csi)
 						i=matchuser(&cfg,csi->str,TRUE /*sysop_alias*/);
 						if(i && i!=useron.number)
 							break;
-						sprintf(useron.alias,"%.*s",LEN_ALIAS,csi->str);
+						snprintf(useron.alias, sizeof useron.alias, "%.*s",LEN_ALIAS,csi->str);
 						putuserstr(useron.number, USER_ALIAS, useron.alias);
 						putusername(&cfg,useron.number,useron.alias);
 						csi->logic=LOGIC_TRUE;
@@ -1668,7 +1668,7 @@ int sbbs_t::exec(csi_t *csi)
 						if(cfg.uq&UQ_DUPREAL
 							&& finduserstr(useron.number, USER_NAME, csi->str))
 							break;
-						sprintf(useron.name,"%.*s",LEN_NAME,csi->str);
+						snprintf(useron.name, sizeof useron.name, "%.*s",LEN_NAME,csi->str);
 						putuserstr(useron.number, USER_NAME
 							,useron.name);
 						csi->logic=LOGIC_TRUE;
@@ -1679,7 +1679,7 @@ int sbbs_t::exec(csi_t *csi)
 						if(cfg.uq&UQ_DUPHAND
 							&& finduserstr(useron.number, USER_HANDLE, csi->str))
 							break;
-						sprintf(useron.handle,"%.*s",LEN_HANDLE,csi->str);
+						snprintf(useron.handle, sizeof useron.handle, "%.*s",LEN_HANDLE,csi->str);
 						putuserstr(useron.number, USER_HANDLE
 							,useron.handle);
 						csi->logic=LOGIC_TRUE;
@@ -1691,31 +1691,31 @@ int sbbs_t::exec(csi_t *csi)
 						csi->logic=LOGIC_TRUE;
 						break;
 					case USER_STRING_NOTE:
-						sprintf(useron.note,"%.*s",LEN_NOTE,csi->str);
+						snprintf(useron.note, sizeof useron.note, "%.*s",LEN_NOTE,csi->str);
 						putuserstr(useron.number, USER_NOTE
 							,useron.note);
 						csi->logic=LOGIC_TRUE;
 						break;
 					case USER_STRING_ADDRESS:
-						sprintf(useron.address,"%.*s",LEN_ADDRESS,csi->str);
+						snprintf(useron.address, sizeof useron.address, "%.*s",LEN_ADDRESS,csi->str);
 						putuserstr(useron.number, USER_ADDRESS
 							,useron.address);
 						csi->logic=LOGIC_TRUE;
 						break;
 					case USER_STRING_LOCATION:
-						sprintf(useron.location,"%.*s",LEN_LOCATION,csi->str);
+						snprintf(useron.location, sizeof useron.location, "%.*s",LEN_LOCATION,csi->str);
 						putuserstr(useron.number, USER_LOCATION
 							,useron.location);
 						csi->logic=LOGIC_TRUE;
 						break;
 					case USER_STRING_ZIPCODE:
-						sprintf(useron.zipcode,"%.*s",LEN_ZIPCODE,csi->str);
+						snprintf(useron.zipcode, sizeof useron.zipcode, "%.*s",LEN_ZIPCODE,csi->str);
 						putuserstr(useron.number, USER_ZIPCODE
 							,useron.zipcode);
 						csi->logic=LOGIC_TRUE;
 						break;
 					case USER_STRING_PASSWORD:
-						sprintf(useron.pass,"%.*s",LEN_PASS,csi->str);
+						snprintf(useron.pass, sizeof useron.pass, "%.*s",LEN_PASS,csi->str);
 						putuserstr(useron.number, USER_PASS
 							,useron.pass);
 						csi->logic=LOGIC_TRUE;
@@ -1723,7 +1723,7 @@ int sbbs_t::exec(csi_t *csi)
 					case USER_STRING_BIRTHDAY:
 						if(!getage(&cfg,csi->str))
 							break;
-						sprintf(useron.birth,"%.*s",LEN_BIRTH,csi->str);
+						snprintf(useron.birth, sizeof useron.birth, "%.*s",LEN_BIRTH,csi->str);
 						putuserstr(useron.number, USER_BIRTH
 							,useron.birth);
 						csi->logic=LOGIC_TRUE;
@@ -1731,31 +1731,31 @@ int sbbs_t::exec(csi_t *csi)
 					case USER_STRING_PHONE:
 						if(trashcan(csi->str,"phone"))
 							break;
-						sprintf(useron.phone,"%.*s",LEN_PHONE,csi->str);
+						snprintf(useron.phone, sizeof useron.phone, "%.*s",LEN_PHONE,csi->str);
 						putuserstr(useron.number, USER_PHONE
 							,useron.phone);
 						csi->logic=LOGIC_TRUE;
 						break;
 					case USER_STRING_MODEM:
-						sprintf(useron.modem,"%.*s",LEN_MODEM,csi->str);
+						snprintf(useron.modem, sizeof useron.modem, "%.*s",LEN_MODEM,csi->str);
 						putuserstr(useron.number, USER_CONNECTION
 							,useron.phone);
 						csi->logic=LOGIC_TRUE;
 						break;
 					case USER_STRING_IPADDR:
-						sprintf(useron.ipaddr,"%.*s",LEN_IPADDR,csi->str);
+						snprintf(useron.ipaddr, sizeof useron.ipaddr, "%.*s",LEN_IPADDR,csi->str);
 						putuserstr(useron.number, USER_IPADDR
 							,useron.phone);
 						csi->logic=LOGIC_TRUE;
 						break;
 					case USER_STRING_COMMENT:
-						sprintf(useron.comment,"%.*s",LEN_COMMENT,csi->str);
+						snprintf(useron.comment, sizeof useron.comment, "%.*s",LEN_COMMENT,csi->str);
 						putuserstr(useron.number, USER_COMMENT
 							,useron.comment);
 						csi->logic=LOGIC_TRUE;
 						break;
 					case USER_STRING_NETMAIL:
-						sprintf(useron.netmail,"%.*s",LEN_NETMAIL,csi->str);
+						snprintf(useron.netmail, sizeof useron.netmail, "%.*s",LEN_NETMAIL,csi->str);
 						putuserstr(useron.number, USER_NETMAIL
 							,useron.netmail);
 						csi->logic=LOGIC_TRUE;

src/sbbs3/execfile.cpp
@@ -51,7 +51,7 @@ int sbbs_t::exec_file(csi_t *csi)
 								,i+1,cfg.lib[usrlib[i]]->lname); 
 						} 
 					}
-					sprintf(str,text[JoinWhichLib],curlib+1);
+					snprintf(str, sizeof str, text[JoinWhichLib],curlib+1);
 					mnemonics(str);
 					j=getnum(usrlibs);
 					clear_hotspots();
@@ -62,14 +62,14 @@ int sbbs_t::exec_file(csi_t *csi)
 					else
 						j--; 
 				}
-				sprintf(str,"dirs%u",usrlib[j]+1);
+				snprintf(str, sizeof str, "dirs%u",usrlib[j]+1);
 				if(!menu(str, P_NOERROR)) {
 					CLS;
 					bprintf(text[DirLstHdr], cfg.lib[usrlib[j]]->lname);
 					for(i=0;i<usrdirs[j] && !msgabort();i++) {
 						if(i==curdir[j]) outchar('*');
 						else outchar(' ');
-						sprintf(str,text[DirLstFmt],i+1
+						snprintf(str, sizeof str, text[DirLstFmt],i+1
 							,cfg.dir[usrdir[j][i]]->lname,nulstr
 							,getfiles(&cfg,usrdir[j][i]));
 						if(i<9) outchar(' ');
@@ -78,7 +78,7 @@ int sbbs_t::exec_file(csi_t *csi)
 						bputs(str); 
 					} 
 				}
-				sprintf(str,text[JoinWhichDir],curdir[j]+1);
+				snprintf(str, sizeof str, text[JoinWhichDir],curdir[j]+1);
 				mnemonics(str);
 				i=getnum(usrdirs[j]);
 				clear_hotspots();
@@ -207,7 +207,7 @@ int sbbs_t::exec_file(csi_t *csi)
 
 		case CS_FILE_SHOW_DIRECTORIES:
 			if(!usrlibs) return(0);
-			sprintf(str,"dirs%u",usrlib[curlib]+1);
+			snprintf(str, sizeof str, "dirs%u",usrlib[curlib]+1);
 			if(menu(str, P_NOERROR)) {
 				return(0); 
 			}
@@ -216,7 +216,7 @@ int sbbs_t::exec_file(csi_t *csi)
 			for(i=0;i<usrdirs[curlib] && !msgabort();i++) {
 				if(i==curdir[curlib]) outchar('*');
 				else outchar(' ');
-				sprintf(str,text[DirLstFmt],i+1
+				snprintf(str, sizeof str, text[DirLstFmt],i+1
 					,cfg.dir[usrdir[curlib][i]]->lname,nulstr
 					,getfiles(&cfg,usrdir[curlib][i]));
 				if(i<9) outchar(' ');

src/sbbs3/execfunc.cpp
@@ -220,7 +220,7 @@ int sbbs_t::exec_function(csi_t *csi)
 				return(0);
 			if(localtime_r(&now,&tm)==NULL)
 				return(0);
-			sprintf(str,"%slogs/%2.2d%2.2d%2.2d.log", cfg.logs_dir
+			snprintf(str, sizeof str, "%slogs/%2.2d%2.2d%2.2d.log", cfg.logs_dir
 				,tm.tm_mon+1,tm.tm_mday,TM_YEAR(tm.tm_year));
 			printfile(str,0);
 			return(0);
@@ -230,7 +230,7 @@ int sbbs_t::exec_function(csi_t *csi)
 			now-=(ulong)60L*24L*60L;
 			if(localtime_r(&now,&tm)==NULL)
 				return(0);
-			sprintf(str,"%slogs/%2.2d%2.2d%2.2d.log",cfg.logs_dir
+			snprintf(str, sizeof str, "%slogs/%2.2d%2.2d%2.2d.log",cfg.logs_dir
 				,tm.tm_mon+1,tm.tm_mday,TM_YEAR(tm.tm_year));
 			printfile(str,0);
 			return(0);
@@ -246,7 +246,7 @@ int sbbs_t::exec_function(csi_t *csi)
 		case CS_SHOW_MEM:
 			return(0);
 		case CS_ERROR_LOG:
-			sprintf(str,"%serror.log", cfg.logs_dir);
+			snprintf(str, sizeof str, "%serror.log", cfg.logs_dir);
 			if(fexist(str)) {
 				bputs(text[ErrorLogHdr]);
 				printfile(str,0);
@@ -292,7 +292,7 @@ int sbbs_t::exec_function(csi_t *csi)
 				editfile(str);
 			return(0);
 		case CS_GURU_LOG:
-			sprintf(str,"%sguru.log", cfg.logs_dir);
+			snprintf(str, sizeof str, "%sguru.log", cfg.logs_dir);
 			if(fexist(str)) {
 				printfile(str,0);
 				CRLF;

commit 5047306f0dd5ade95bdd781273365c988ec4a2b6

Fix off-by-one bug in edit string length copy in console.getstr() method
Bug introduced in commit 95596962fd27cb2d

As noted in strlcpy man page: "a byte for the NUL should be included in size."

src/sbbs3/js_console.cpp
@@ -819,7 +819,7 @@ js_getstr(JSContext *cx, uintN argc, jsval *arglist)
 			free(p);
 			return JS_FALSE;
 		}
-		strlcpy(p, p2, maxlen);
+		strlcpy(p, p2, maxlen + 1);
 		free(p2);
 	}
 