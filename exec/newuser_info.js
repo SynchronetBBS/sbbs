c9bae24626ec374ad36753138e4a9532d8a8fee7
Rob Swindell (on Debian Linux) <rob@synchro.net>
2026-01-03 00:14:57 -0800

    Display a helpful message if no external editors are available to the user

--- exec/newuser_info.js
+++ exec/newuser_info.js
@@ -2,15 +2,19 @@
 
 require("sbbsdefs.js", "P_NOABORT");
 require("text.js", "UseExternalEditorQ");
+require("gettext.js", "gettext");
 
 bbs.menu("../sbbs", P_NOABORT | P_NOERROR);
 bbs.menu("../system", P_NOABORT | P_NOERROR);
 bbs.menu("../newuser", P_NOABORT | P_NOERROR);
 
 if (Object.getOwnPropertyNames(xtrn_area.editor).length && (system.newuser_questions & UQ_XEDIT) && bbs.text(UseExternalEditorQ)) {
-	if (console.yesno(bbs.text(UseExternalEditorQ)))
-		bbs.select_editor();
-	else
+	if (console.yesno(bbs.text(UseExternalEditorQ))) {
+		if (!bbs.select_editor()) {
+			console.print(gettext("Sorry, no external editors are available to you"))
+			console.newline();
+		}
+	} else
 		user.xedit = 0;
 }
 
