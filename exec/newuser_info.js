3379f85c37078512a27c7a06d5b7d7379c81f2e9
Rob Swindell (on Debian Linux) <rob@synchro.net>
2026-01-03 02:12:47 -0800

    It's user.editor, not user.xedit (doh!)
    Clear screen before editor prompt, use strict mode

--- exec/newuser_info.js
+++ exec/newuser_info.js
@@ -1,21 +1,25 @@
 // New User Information/help module
+// for Synchronet v3.21+
 
 require("sbbsdefs.js", "P_NOABORT");
 require("text.js", "UseExternalEditorQ");
 require("gettext.js", "gettext");
+
+"use strict";
 
 bbs.menu("../sbbs", P_NOABORT | P_NOERROR);
 bbs.menu("../system", P_NOABORT | P_NOERROR);
 bbs.menu("../newuser", P_NOABORT | P_NOERROR);
 
 if (Object.getOwnPropertyNames(xtrn_area.editor).length && (system.newuser_questions & UQ_XEDIT) && bbs.text(UseExternalEditorQ)) {
+	console.clear();
 	if (console.yesno(bbs.text(UseExternalEditorQ))) {
 		if (!bbs.select_editor()) {
 			console.print(gettext("Sorry, no external editors are available to you"))
 			console.newline();
 		}
 	} else
-		user.xedit = 0;
+		user.editor = "";
 }
 
 if ((system.newuser_questions & UQ_CMDSHELL) && bbs.text(CommandShellHeading)) {
