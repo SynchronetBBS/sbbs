SynchronetBBS/sbbs

commit ff46f601780b4e933404ebb3879d88f49f16dd6d

Also use a timeout if we have nothing to send.
Fixes an infinite loop where there is nothing to send because the
remote does not have the address you are calling to deliver a message
for.

For example if W6RAY just joined the BBSDev FTN, and was assigned
885:1/99, and a netmail was created welcoming the BBS to the network,
but they didn't add the new address to the AKA list in scfg, binkit
would sit using 100% CPU.

Now it sets the timeout, so sits at 0% CPU for 120s then fails.

This is still likely wrong, but at least it's wrong in a much less
harmful way.

exec/load/binkp.js
@@ -706,7 +706,7 @@ BinkP.prototype.session = function()
 			if (this.senteob)
 				cur_timeout = this.timeout;
 		}
-		if (this.sending !== undefined && this.sending.waitingForGet !== undefined && this.sending.waitingForGet)
+		if (this.sending === undefined || (this.sending.waitingForGet !== undefined && this.sending.waitingForGet))
 			cur_timeout = this.timeout;
 		pkt = this.recvFrame(cur_timeout);
 		if (pkt !== undefined && pkt !== this.partialFrame && pkt !== null) {

commit 92ecc21be9b5c3cd825fa9b1cdd83e3e93d37bb2

Fix two-minute timeout when answering and no files
Regression introduced in ff46f601, where the answering system would
not send an M_EOB and instead perform a timeout.

exec/load/binkp.js
@@ -706,8 +706,10 @@ BinkP.prototype.session = function()
 			if (this.senteob)
 				cur_timeout = this.timeout;
 		}
-		if ((this.sending === undefined && this.tx_queue.length === 0) || (this.sending !== undefined && this.sending.waitingForGet !== undefined && this.sending.waitingForGet))
-			cur_timeout = this.timeout;
+		if ((this.sending === undefined && this.tx_queue.length === 0) || (this.sending !== undefined && this.sending.waitingForGet !== undefined && this.sending.waitingForGet)) {
+			if (this.tx_queue.length !== 0 || this.senteob !== 0)
+				cur_timeout = this.timeout;
+		}
 		pkt = this.recvFrame(cur_timeout);
 		if (pkt !== undefined && pkt !== this.partialFrame && pkt !== null) {
 			last = Date.now();