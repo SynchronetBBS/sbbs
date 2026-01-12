817699d96e81bd70dd1e06a63db210dc3a14f55d
Rob Swindell (on Windows 11) <rob@synchro.net>
2026-01-11 19:32:21 -0800

    Log length of "broken ANSI sequence"
    to help get to the bottom of "Sent broken ANSI sequence ''" log messages

--- src/sbbs3/ansi_terminal.cpp
+++ src/sbbs3/ansi_terminal.cpp
@@ -855,7 +855,9 @@ bool ANSI_Terminal::parse_output(char ich) {
 			ansiParser.reset();
 			return true;
 		case ansiState_broken:
-			sbbs->lprintf(LOG_WARNING, "Sent broken ANSI sequence '%s'", ansiParser.ansi_sequence.c_str());
+			sbbs->lprintf(LOG_WARNING, "Sent %u-character broken ANSI sequence '%s'"
+				, static_cast<uint>(ansiParser.ansi_sequence.length())
+				, ansiParser.ansi_sequence.c_str());
 			ansiParser.reset();
 			return true;
 		case ansiState_none:
