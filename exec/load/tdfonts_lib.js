ea8b688370f19178be6c0c696c498f77e9fcfd0a
Rob Swindell (on Debian Linux) <rob@synchro.net>
2026-01-29 19:53:52 -0800

    Fix bug with Ctrl-A output, switching from high to low intensity fg-colors
    I was wondering why the ANSI output looked different/better than the Ctrl-A
    output. This was the bug.

--- exec/load/tdfonts_lib.js
+++ exec/load/tdfonts_lib.js
@@ -290,7 +290,7 @@ function printcolor(color) {
 	var out = "";
 
     if (this.opt == undefined || !opt.ansi) {
-		var fgcolors = ["K", "B", "G", "C", "R", "M", "Y", "W", "HK", "HB", "HG", "HC", "HR", "HM", "HY", "HW"]; // Normal/Bright
+		var fgcolors = ["-K", "-B", "-G", "-C", "-R", "-M", "-Y", "-W", "HK", "HB", "HG", "HC", "HR", "HM", "HY", "HW"]; // Normal/Bright
 		var bgcolors = ["0", "4", "2", "6", "1", "5", "3", "7"]; // Backgrounds (normal only for 8 colors)
 		out += "\x01" + fgcolors[fg].split("").join("\x01");
 		out += "\x01" + bgcolors[bg];
