SynchronetBBS/sbbs

commit 29213fde9f24d62b7e20bbfaa5a1cb1365748809

printfile() reads and uses columns from SAUCE records and supports file seeking
The seeking is actually the bigger change here, and long over due. When viewing
a larger file and using the default (line at a time) mode of printfile(), we
also support a P_SEEK mode flag that will allow the user to seek/jump around
in the viewed file using home/end/page up/down, arrow keys (and mouse
clickable). I'd like to extend this feature to putmsg() (and thus printfile
when used with the P_OPENCLOSE mode), but for now, this still a big thing.
Mouse scrolling would be cool too (but not done). This doesn't do any fancy
ANSI window scrolling, just redisplays all the new lines from the top of the
screen after each seek. Adding a text-find feature wouldn't be too hard either.

The SAUCE feature is pretty cool in that if you now display a file that has
a valid SAUCE record that specifies a column width, the display output will
automatically be taylored to suit that file (e.g. set the display width and
enabel the P_WRAP print mode flag). That should help solve a lot of problems
some sysops have with display files (usually ANSI artwork) that uses 80 columns
and does undesireable things on wider terminals. Of course, the file has to
have the valid SAUCE record (e.g. added by the ANSI editor).

src/sbbs3/prntfile.cpp
@@ -22,6 +22,7 @@
 #include "sbbs.h"
 #include "utf8.h"
 #include "petdefs.h"
+#include "sauce.h"
 
 #ifndef PRINTFILE_MAX_LINE_LEN
 #define PRINTFILE_MAX_LINE_LEN (8 * 1024)
@@ -106,6 +107,15 @@ bool sbbs_t::printfile(const char* inpath, int mode, int org_cols, JSObject* obj
 		return true;
 	}
 
+	struct sauce_charinfo sauce{};
+	if (sauce_fread_charinfo(stream, /* type */nullptr, &sauce)) {
+		mode |= P_CPM_EOF;
+		if (org_cols == 0 && sauce.width > TERM_COLS_MIN) {
+			org_cols = sauce.width;
+			mode |= P_WRAP;
+		}
+	}
+
 	lprintf(LOG_DEBUG, "Printing file: %s", fpath);
 	if (!(mode & P_NOCRLF) && term->row > 0 && !rip) {
 		term->newline();
@@ -134,11 +144,29 @@ bool sbbs_t::printfile(const char* inpath, int mode, int org_cols, JSObject* obj
 		uint             org_line_delay = line_delay;
 		uint             tmpatr = curatr;
 		uint             orgcon = console;
+
 		attr_sp = 0;    /* clear any saved attributes */
+		off_t* offset = nullptr;
+		size_t line=0;
+		size_t lines=0;
+		char key = 0;
+		int kmode = K_UPPER;
+		if ((sys_status & SS_USERON) && !(useron.misc & (NOPAUSESPIN)) && cfg.spinning_pause_prompt)
+			kmode |= K_SPIN;
+
+		if (!pause_enabled())
+			mode &= ~P_SEEK;
+
+		if (mode & P_SEEK) {
+			mode |= P_NOPAUSE;
+			if (length > (int)term->cols)
+				length = (int)term->cols;
+		}
 		if (!(mode & P_SAVEATR))
 			attr(LIGHTGRAY);
 		if (mode & P_NOPAUSE)
 			sys_status |= SS_PAUSEOFF;
+
 		if (length > PRINTFILE_MAX_LINE_LEN)
 			length = PRINTFILE_MAX_LINE_LEN;
 		if ((buf = (char*)malloc(length + 1L)) == NULL) {
@@ -149,18 +177,101 @@ bool sbbs_t::printfile(const char* inpath, int mode, int org_cols, JSObject* obj
 
 		ansiParser.reset();
 		while (!feof(stream) && !msgabort()) {
+			off_t o = ftello(stream);
 			if (fgets(buf, length + 1, stream) == NULL)
 				break;
+			truncsp(buf);
+			if ((mode & P_SEEK) && line == lines) {
+				++lines;
+				if ((offset = static_cast<off_t*>(realloc_or_free(offset, lines * sizeof *offset))) == nullptr) {
+					errormsg(WHERE, ERR_ALLOC, fpath, lines * sizeof *offset);
+					break;
+				}
+				offset[line] = o;
+			}
 			if ((mode & P_UTF8) && (term->charset() != CHARSET_UTF8))
 				utf8_normalize_str(buf);
 			if (putmsgfrag(buf, mode, org_cols, obj) != '\0') // early-EOF?
 				break;
+			if (*buf == '\0' || term->column)
+				term->newline();
+			if ((mode & P_SEEK) && (term->lncntr == term->rows - 1 || key == TERM_KEY_DOWN)) {
+				term->lncntr = 0;
+				int curatr = term->curatr;
+				bputs(text[SeekPrompt]);
+				auto nextline = line;
+				key = getkey(kmode);
+				if (key == no_key() || key == quit_key())
+					sys_status |= SS_ABORT;
+				attr(curatr);
+				term->carriage_return();
+				term->cleartoeol();
+				switch (key) {
+					case TERM_KEY_HOME:
+						nextline = 0;
+						break;
+					case TERM_KEY_UP:
+						if (line <= term->rows - 1)
+							nextline = 0;
+						else
+							nextline = line - (term->rows - 1);
+						break;
+					case TERM_KEY_PAGEUP:
+						if (line <= ((term->rows - 1) * 2) - 1)
+							nextline = 0;
+						else
+							nextline = line - (((term->rows - 1) * 2) - 1);
+						break;
+					case TERM_KEY_END:
+					{
+						bputs(text[SeekingFile]);
+						fseeko(stream, offset[lines - 1], SEEK_SET);
+						if (fgets(buf, length + 1, stream) == NULL)
+							break;
+						off_t lastline = lines - 1;
+						while (!feof(stream) && !msgabort()) {
+							o = ftello(stream);
+							if (fgets(buf, length + 1, stream) == NULL)
+								break;
+							++lastline;
+							if (lastline >= lines) {
+								++lines;
+								if ((offset = static_cast<off_t*>(realloc_or_free(offset, lines * sizeof *offset))) == nullptr) {
+									errormsg(WHERE, ERR_ALLOC, fpath, lines * sizeof *offset);
+									break;
+								}
+								offset[lastline] = o;
+							}
+						}
+						bputs(text[SeekingFileDone]);
+						if (lines <= term->rows -1)
+							nextline = 0;
+						else
+							nextline = lines - (term->rows -1);
+						break;
+					}
+					case TERM_KEY_PAGEDN:
+						if (feof(stream))
+							continue;
+						// Fall-through
+					default:
+					case TERM_KEY_DOWN:
+						nextline = line + 1;
+						break;
+				}
+				if (nextline != line + 1 && nextline < lines)
+					fseeko(stream, offset[nextline], 0);
+				line = nextline;
+			}
+			else
+				++line;
 		}
 		if (ansiParser.current_state() != ansiState_none)
 			lprintf(LOG_DEBUG, "Incomplete ANSI stripped from end");
 		memcpy(rainbow, cfg.rainbow, sizeof rainbow);
 		free(buf);
 		fclose(stream);
+		free(offset);
 		if (!(mode & P_SAVEATR)) {
 			console = orgcon;
 			attr(tmpatr);

ctrl/text.dat
@@ -1129,3 +1129,12 @@
 "Read your unread mail now\1\\"                            939 ReadYourUnreadMailNowQ
 "\1n\1c(\1h%s\1n\1c bytes in \1h%u\1n\1c files today)"     940 UserDownloadsToday
 "\1n\1h\1?Wide (> 80 column) output mode is now %s.\r\n"   941 WideModeIsNow
+"\1n\1hSeeking @ELLIPSIS@\1n"                              942 SeekingFile
+"\1n\1[\1>"                                                862 SeekingFileDone
+"\1n\1r\1h[\1wSeek: \1r"\                                  863 SeekPrompt
+	"@~Home~\\x02@\1w/\1r"\
+	"@~End~\\x05@\1w/\1r"\
+	"@~PgUp~\\x10@\1w/\1r"\
+	"@~PgDn~\\x0e@\1w/\1r"\
+	"@~Up~\\x1e@\1w/\1r"\
+	"@~Dn~\\x0a@\1r] "

exec/load/sbbsdefs.js
@@ -194,29 +194,28 @@ var   K_WRAP = K_WORDWRAP;
 						    	/********************************************/
     							/* Bits in 'mode' for putmsg and printfile  */
 							    /********************************************/
-var   P_NONE		=0;			/* No special behavior						*/
-var   P_NOABORT  	=(1<<0);	/* Disallows abortion of a message          */
-var   P_SAVEATR		=(1<<1);	/* Save the new current attributes after	*/
-					    		/* msg has printed							*/
-var   P_NOATCODES	=(1<<2);	/* Don't allow @ codes                      */
-var   P_OPENCLOSE	=(1<<3);	/* Open and close the file					*/
-var   P_NOPAUSE		=(1<<4);	/* Disable screen pause						*/
-var   P_HTML		=(1<<5);	/* Message is HTML							*/
-var   P_NOCRLF		=(1<<6);	/* Don't prepend a CRLF	in printfile()		*/
-var   P_WORDWRAP	=(1<<7);	/* Word-wrap long lines for user's terminal	*/
-var   P_CPM_EOF		=(1<<8);	/* Treat Ctrl-Z as End-of-file				*/
-var   P_TRUNCATE    =(1<<9);    /* Truncate (don't display) long lines      */
-var   P_NOERROR     =(1<<10);   /* Don't report error if file doesn't exist */
-var   P_PETSCII     =(1<<11);   /* Message is native PETSCII                */
-var   P_WRAP        =(1<<12);   /* Wrap/split long-lines, ungracefully      */
-var   P_UTF8        =(1<<13);	/* Message is UTF-8 encoded                 */
-var   P_AUTO_UTF8	=(1<<14);	/* Message may be UTF-8, auto-detect		*/
-var   P_NOXATTRS	=(1<<15);	/* No "Extra Attribute Codes" supported		*/
-var   P_MARKUP		=(1<<16);	/* Support StyleCodes/Rich/StructuredText	*/
-var   P_HIDEMARKS	=(1<<17);	/* Hide the mark-up tags					*/
-var   P_REMOTE		=(1<<18);	/* Only print when online == ON_REMOTE		*/
-var   P_INDENT		=(1<<19);	/* Indent lines to current cursor column	*/
-var   P_ATCODES		=(1<<20);	/* Trusted @-codes in formatted string		*/
+var   P_NONE		=0;			// No special behavior
+var   P_NOABORT  	=(1<<0);	// Disallows abortion of a message
+var   P_SAVEATR		=(1<<1);	// Save the new current attributes after msg has printed
+var   P_NOATCODES	=(1<<2);	// Don't allow @ codes
+var   P_OPENCLOSE	=(1<<3);	// Open and close the file (for smaller files)
+var   P_NOPAUSE		=(1<<4);	// Disable screen pause
+var   P_SEEK 		=(1<<5);	// Support jumping around via Home/End/PgUp/Dn, etc.
+var   P_NOCRLF		=(1<<6);	// Don't prepend a CRLF	in printfile()
+var   P_WORDWRAP	=(1<<7);	// Word-wrap long lines for user's terminal
+var   P_CPM_EOF		=(1<<8);	// Treat Ctrl-Z as End-of-file
+var   P_TRUNCATE    =(1<<9);    // Truncate (don't display) long lines
+var   P_NOERROR     =(1<<10);   // Don't report error if file doesn't exist
+var   P_PETSCII     =(1<<11);   // Message is native PETSCII
+var   P_WRAP        =(1<<12);   // Wrap/split long-lines, ungracefully
+var   P_UTF8        =(1<<13);	// Message is UTF-8 encoded
+var   P_AUTO_UTF8	=(1<<14);	// Message may be UTF-8, auto-detect
+var   P_NOXATTRS	=(1<<15);	// No "Extra Attribute Codes" supported
+var   P_MARKUP		=(1<<16);	// Support StyleCodes/Rich/StructuredText
+var   P_HIDEMARKS	=(1<<17);	// Hide the mark-up tags
+var   P_REMOTE		=(1<<18);	// Only print when online == ON_REMOTE
+var   P_INDENT		=(1<<19);	// Indent lines to current cursor column
+var   P_ATCODES		=(1<<20);	// Trusted @-codes in formatted string
 var   P_MODS        =(1<<21);   // Display from mods/text dir, if file is there
 var   P_CENTER      =(1<<22);   // Center the output based on widest line
 var   P_80COLS      =(1<<23);   // Format the output for 80-column display

exec/str_cmds.js
@@ -73,7 +73,7 @@ function str_cmds(str)
 			var errlog=system.logs_dir+"error.log";
 			if(file_exists(errlog)) {
 				write(bbs.text(bbs.text.ErrorLogHdr));
-				console.printfile(errlog);
+				console.printfile(errlog, P_NOATCODES | P_SEEK);
 				console.aborted = false;
 				if(!console.noyes(bbs.text(bbs.text.DeleteErrorLogQ)))
 					file_remove(errlog);
@@ -98,7 +98,7 @@ function str_cmds(str)
 		help["GURU"] = "Display and optionally clear current guru log";
 		if(str=="GURU") {
 			if(file_exists(system.logs_dir+"guru.log")) {
-				console.printfile(system.logs_dir+"guru.log");
+				console.printfile(system.logs_dir+"guru.log", P_NOATCODES | P_SEEK);
 				console.crlf();
 				if(!console.noyes(bbs.text(bbs.text.DeleteGuruLogQ)))
 					file_remove(system.logs_dir+"guru.log");
@@ -119,8 +119,10 @@ function str_cmds(str)
 				var pmode = (word == "CAT") ? P_NOATCODES : P_CPM_EOF;
 				if(word == "TYPE")
 					pmode |= P_OPENCLOSE;
-				log("printing " + str);
-				log(console.printfile(get_filename(str), pmode));
+				else
+					pmode |= P_SEEK;
+				log("printing " + (str = get_filename(str)));
+				console.printfile(str, pmode);
 				return;
 			}
 		}
@@ -155,7 +157,8 @@ function str_cmds(str)
 		if(word=="EDIT") {
 			if(bbs.check_syspass()) {
 				str=str.substr(4);
-				console.editfile(get_filename(str));
+				log("editing " + (str = get_filename(str)));
+				console.editfile(str);
 			}
 		}
 
@@ -163,7 +166,7 @@ function str_cmds(str)
 		if(str=="LOG") {
 			if(bbs.check_syspass()) {
 				str=system.logs_dir+strftime("logs/%m%d%y.log",time());
-				console.printfile(str);
+				console.printfile(str, P_NOATCODES);
 			}
 			return;
 		}
@@ -172,7 +175,7 @@ function str_cmds(str)
 		if(str=="YLOG") {
 			if(bbs.check_syspass()) {
 				str=system.logs_dir+strftime("logs/%m%d%y.log",time()-24*60*60);
-				console.printfile(str);
+				console.printfile(str, P_NOATCODES);
 			}
 			return;
 		}
@@ -706,14 +709,14 @@ function str_cmds(str)
 			var plan=format("%suser/%04d.plan",system.data_dir,user.number);
 			if(file_exists(plan)) {
 				if(console.yesno("Display current .plan"))
-					console.printfile(plan);
+					console.printfile(plan, P_NOATCODES);
 				if(!console.noyes("Delete current .plan"))
 					file_remove(plan);
 			}
 			if(console.yesno("Edit/Create .plan")) {
 				console.editfile(plan);
 				if(file_exists(plan))
-					console.printfile(plan);
+					console.printfile(plan, P_NOATCODES);
 			}
 		}
 
@@ -726,7 +729,7 @@ function str_cmds(str)
 			userSigFilename += ".sig";
 			if (file_exists(userSigFilename)) {
 				if (console.yesno(bbs.text(bbs.text.ViewSignatureQ)))
-					console.printfile(userSigFilename);
+					console.printfile(userSigFilename, P_NOATCODES);
 			}
 			if (console.yesno(bbs.text(bbs.text.CreateEditSignatureQ)))
 				console.editfile(userSigFilename);

src/sbbs3/sbbsdefs.h
@@ -677,7 +677,7 @@ typedef enum {                       // Values for xtrn_t.event
 #define P_NOATCODES (1 << 2)      // Don't allow @ codes
 #define P_OPENCLOSE (1 << 3)      // Open and close the file
 #define P_NOPAUSE   (1 << 4)      // Disable screen pause
-#define P_HTML      (1 << 5)      // Unused - Message is HTML
+#define P_SEEK      (1 << 5)      // Support jumping around via Home/End/PgUp/Dn, etc.
 #define P_NOCRLF    (1 << 6)      // Don't prepend a CRLF	in printfile()
 #define P_WORDWRAP  (1 << 7)      // Word-wrap long lines for user's terminal
 #define P_CPM_EOF   (1 << 8)      // Ignore Ctrl-Z chars (CPM End-of-File)

src/sbbs3/text.h
@@ -958,6 +958,9 @@ enum text {
 	,ReadYourUnreadMailNowQ
 	,UserDownloadsToday
 	,WideModeIsNow
+	,SeekingFile
+	,SeekingFileDone
+	,SeekPrompt
 
 	,TOTAL_TEXT
 };

src/sbbs3/text_defaults.c
@@ -1513,4 +1513,10 @@ const char * const text_defaults[TOTAL_TEXT]={
 		"\x66\x69\x6c\x65\x73\x20\x74\x6f\x64\x61\x79\x29" // 940 UserDownloadsToday
 	,"\x01\x6e\x01\x68\x01\x3f\x57\x69\x64\x65\x20\x28\x3e\x20\x38\x30\x20\x63\x6f\x6c\x75\x6d\x6e\x29\x20\x6f\x75\x74\x70\x75\x74\x20"
 		"\x6d\x6f\x64\x65\x20\x69\x73\x20\x6e\x6f\x77\x20\x25\x73\x2e\x0d\x0a" // 941 WideModeIsNow
+	,"\x53\x65\x65\x6b\x69\x6e\x67\x20\x40\x45\x4c\x4c\x49\x50\x53\x49\x53\x40\x01\x6e" // 942 SeekingFile
+	,"\x01\x5b\x01\x3e" // 862 SeekingFileDone
+	,"\x01\x6e\x01\x72\x01\x68\x5b\x53\x65\x65\x6b\x01\x6d\x3a\x20\x01\x77\x40\x7e\x48\x6f\x6d\x65\x7e\x5c\x78\x30\x32\x40\x01\x72\x2f"
+		"\x01\x77\x40\x7e\x45\x6e\x64\x7e\x5c\x78\x30\x35\x40\x01\x72\x2f\x01\x77\x40\x7e\x50\x67\x55\x70\x7e\x5c\x78\x31\x30\x40\x01\x72"
+		"\x2f\x01\x77\x40\x7e\x50\x67\x44\x6e\x7e\x5c\x78\x30\x65\x40\x01\x72\x2f\x01\x77\x40\x7e\x55\x70\x7e\x5c\x78\x31\x65\x40\x01\x72"
+		"\x2f\x01\x77\x40\x7e\x44\x6e\x7e\x5c\x78\x30\x61\x40\x01\x72\x5d\x20" // 863 SeekPrompt
 };

src/sbbs3/text_id.c
@@ -942,4 +942,7 @@ const char* const text_id[]={
 	,"ReadYourUnreadMailNowQ"
 	,"UserDownloadsToday"
 	,"WideModeIsNow"
+	,"SeekingFile"
+	,"SeekingFileDone"
+	,"SeekPrompt"
 };

commit 1bea7cc6aa25b656c070fd578e3a40a8ff7611e0

Use fread() instead of read() to printfile (in P_OPENCLOSE mode)
After reading a file's SAUCE record and seeking around (with fseek), a read()
(on the file *descriptor*, not the stream) will fail, but only on *nix.
Use fread() instead of read() to fix this.

This fixes issue #1060, bug recently introduced in commit 29213fde9f24d62b7e20
(reading/using SAUCE records in display files). Thanks Keyop! I had a few of
these errors recently logged as well but chalked it up to something
ephemeral and not reproducible. It was 100% reproducible.

src/sbbs3/prntfile.cpp
@@ -161,7 +161,7 @@ bool sbbs_t::printfile(const char* inpath, int mode, int org_cols, JSObject* obj
 			errormsg(WHERE, ERR_ALLOC, fpath, length + 1L);
 			return false;
 		}
-		l = read(file, buf, length);
+		l = fread(buf, 1, length, stream);
 		fclose(stream);
 		if (l != length)
 			errormsg(WHERE, ERR_READ, fpath, length);