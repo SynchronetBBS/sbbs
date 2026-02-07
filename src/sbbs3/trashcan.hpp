e3348e0ec32797d461ab9874aece5a2f31b67de9
Rob Swindell (on Windows 11) <rob@synchro.net>
2026-02-06 16:41:51 -0800

    Limit the rate of trashcan file date/time checks (by default, once per 10s)
    Maybe make that interval configurable somewhere at some point

--- src/sbbs3/trashcan.hpp
+++ src/sbbs3/trashcan.hpp
@@ -23,6 +23,7 @@
 #define TRASH_HPP_
 #include "trash.h"
 #include <mutex>
+#include <time.h>
 
 class trashCan {
 	public:
@@ -32,24 +33,33 @@ class trashCan {
 		~trashCan() {
 			strListFree(&list);
 		}
-		time_t time{};
+		time_t timestamp{};
 		unsigned int read_count{};
+		unsigned int total_found{};
+		time_t min_interval{ 10 }; // seconds
 		bool listed(const char* str1, const char* str2 = nullptr, struct trash* details = nullptr) {
 			const std::lock_guard<std::mutex> lock(mutex);
-			time_t latest = fdate(fname);
-			if (latest > time) {
-				strListFree(&list);
-				list = findstr_list(fname);
-				time = latest;
-				++read_count;
+			time_t now = time(NULL);
+			if ((now - lastftime_check) >= min_interval) {
+				lastftime_check = now;
+				time_t latest = fdate(fname);
+				if (latest > timestamp) {
+					strListFree(&list);
+					list = findstr_list(fname);
+					timestamp = latest;
+					++read_count;
+				}
 			}
 			bool result = trash_in_list(str1, str2, list, details);
+			if (result)
+				++total_found;
 			return result;
 		}
 	private:
 		char fname[MAX_PATH + 1];
 		str_list_t list{};
 		std::mutex mutex;
+		time_t lastftime_check{};
 };
 
 #endif  /* Don't add anything after this line */
