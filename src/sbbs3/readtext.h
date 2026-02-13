cadd5fa22ba3517c951e323d8396b393eaf25ca9
Rob Swindell (on Windows 11) <rob@synchro.net>
2026-02-12 16:39:36 -0800

    Whoops, for this file in previous to last commit

---
+++ src/sbbs3/readtext.h
@@ -0,0 +1,38 @@
+/* Read a string (item) from ctrl/text.dat */
+
+/****************************************************************************
+ * @format.tab-size 4		(Plain Text/Source Code File Header)			*
+ * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
+ *																			*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ *																			*
+ * This program is free software; you can redistribute it and/or			*
+ * modify it under the terms of the GNU General Public License				*
+ * as published by the Free Software Foundation; either version 2			*
+ * of the License, or (at your option) any later version.					*
+ * See the GNU General Public License for more details: gpl.txt or			*
+ * http://www.fsf.org/copyleft/gpl.html										*
+ *																			*
+ * For Synchronet coding style and modification guidelines, see				*
+ * http://www.synchro.net/source.html										*
+ *																			*
+ * Note: If this box doesn't appear square, then you need to fix your tabs.	*
+ ****************************************************************************/
+
+#ifndef READTEXT_H_
+#define READTEXT_H_
+
+#include <stdio.h>
+#include "gen_defs.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char *readtext(int *line, FILE *stream, int dflt, named_string_t** substr_list);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // READTEXT_H_
