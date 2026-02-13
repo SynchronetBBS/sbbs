b7ce76d7e1ac865c9f3c0cdb5b22d50f751a359f
Rob Swindell (on Windows 11) <rob@synchro.net>
2026-02-13 00:21:08 -0800

    Remove obsolete option: MUTE (not used by the mailserver)

--- src/sbbs3/mailsrvr.h
+++ src/sbbs3/mailsrvr.h
@@ -133,7 +133,6 @@ static struct init_field mail_init_fields[] = {
 #define MAIL_OPT_KILL_READ_SPAM         (1 << 28) /* Set the KILLREAD flag on SPAM msgs */
 #define MAIL_OPT_TLS_SUBMISSION         (1 << 29) /* Listen on the TLS "MSA" service port for mail submissions */
 #define MAIL_OPT_TLS_POP3               (1 << 30) /* POP3S */
-#define MAIL_OPT_MUTE                   (1U << 31)
 
 #define MAIL_OPT_RELAY_AUTH_MASK        (MAIL_OPT_RELAY_AUTH_PLAIN | MAIL_OPT_RELAY_AUTH_LOGIN | MAIL_OPT_RELAY_AUTH_CRAM_MD5)
 
@@ -174,7 +173,6 @@ static ini_bitdesc_t mail_options[] = {
 	{ MAIL_OPT_KILL_READ_SPAM, "KILL_READ_SPAM"       },
 	{ MAIL_OPT_TLS_SUBMISSION, "TLS_SUBMISSION"       },
 	{ MAIL_OPT_TLS_POP3, "TLS_POP3"             },
-	{ MAIL_OPT_MUTE, "MUTE"                 },
 	/* terminator */
 	{ 0, NULL                   }
 };
