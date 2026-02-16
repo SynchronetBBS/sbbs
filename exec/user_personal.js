b7011df40d13ebc5f7a949e38bc6a00c42ba7606
Rob Swindell (on Debian Linux) <rob@synchro.net>
2026-02-15 23:48:47 -0800

    Make all the text strings options
    Fix issue #1071

--- exec/user_personal.js
+++ exec/user_personal.js
@@ -17,8 +17,14 @@ require("sbbsdefs.js", 'SYS_PWEDIT');
 require("gettext.js", 'gettext');
 var prompts = bbs.mods.prompts || load(bbs.mods.prompts = {}, "user_info_prompts.js");
 var options = load("modopts.js", "user_personal");
-if(!options)
+if (!options)
 	options = {};
+if (!options.option_fmt)
+	options.option_fmt = "\x01u[\x01U%c\x01u] ";
+if (!options.option_val)
+	options.option_val = ": \x01U%s";
+if (!options.option_date)
+	options.option_date = " (%s: \x01U%s\x01u)";
 var ssh_support = server.options & (1<< 12); // BBS_OPT_ALLOW_SSH
 
 prompts.operation = "";
@@ -27,28 +33,31 @@ while(bbs.online && !js.terminated) {
 	var keys = 'Q\r';
 	console.aborted = false;	
 	console.clear();
-	console.print("\x01U" + gettext("Personal Information") + " " + gettext("for") + format(" \x01U%s #%u:", user.alias, user.number));
+	console.print((options.header_prefix || "\x01U")
+		+ gettext("Personal Information")
+		+ " " + gettext("for")
+		+ format(options.user_fmt || " \x01U%s #%u:", user.alias, user.number));
 	console.newline(2);
 	if (options.alias === true) {
-		console.print("\x01u[\x01UA\x01u] " + gettext("Alias") + format(": \x01U%s", user.alias));
+		console.print(format(options.option_fmt, 'A') + gettext("Alias") + format(options.option_val, user.alias));
 		keys += 'A';
 		console.add_hotspot('A');
 		console.newline();
 	}
 	if (options.name === true) {
-		console.print("\x01u[\x01UR\x01u] " + gettext("Real Name") + format(": \x01U%s", user.name));
+		console.print(format(options.option_fmt, 'R') + gettext("Real Name") + format(options.option_val, user.name));
 		keys += 'R';
 		console.add_hotspot('R');
 		console.newline();
 	}
 	if (options.handle === true) {
-		console.print("\x01u[\x01UH\x01u] " + gettext("Handle") + format(": \x01U%s", user.handle));
+		console.print(format(options.option_fmt, 'H') + gettext("Handle") + format(options.option_val, user.handle));
 		keys += 'H';
 		console.add_hotspot('H');
 		console.newline();
 	}
 	if (options.phone === true) {
-		console.print("\x01u[\x01UN\x01u] " + gettext("Phone Number") +  format(": \x01U%s", user.phone));
+		console.print(format(options.option_fmt, 'N') + gettext("Phone Number") +  format(options.option_val, user.phone));
 		keys += 'N';
 		console.add_hotspot('N');
 		console.newline();
@@ -57,37 +66,37 @@ while(bbs.online && !js.terminated) {
 		var location = (system.newuser_questions & UQ_ADDRESS) ? user.address : "";
 		if (location) location += " ";
 		location += user.location;
-		console.print("\x01u[\x01UL\x01u] " + gettext("Location") + format(": \x01U%s", location));
+		console.print(format(options.option_fmt, 'L') + gettext("Location") + format(options.option_val, location));
 		keys += 'L';
 		console.add_hotspot('L');
 		console.newline();
 	}
 	if (options.gender === true) {
-		console.print("\x01u[\x01UG\x01u] " + gettext("Gender") + format(": \x01U%s", user.gender));
+		console.print(format(options.option_fmt, 'G') + gettext("Gender") + format(options.option_val, user.gender));
 		keys += 'G';
 		console.add_hotspot('G');
 		console.newline();
 	}
 	if (system.settings & SYS_PWEDIT) {
-		console.print("\x01u[\x01UP\x01u] " + gettext("Password")
-			+ format(" (%s: \x01U%s\x01u)", gettext("last changed"), system.datestr(user.security.password_date)));
+		console.print(format(options.option_fmt, 'P') + gettext("Password")
+			+ format(options.option_date, gettext("last changed"), system.datestr(user.security.password_date)));
 		keys += 'P';
 		console.add_hotspot('P');
 		console.newline();
 	}
 	if (ssh_support) {
-		console.print("\x01u[\x01US\x01u] " + gettext("SSH Keys")
-			+ format(" (%s: \x01U%s\x01u)", gettext("last changed"), system.datestr(file_date(prompts.ssh_keys_filename(user)))));
+		console.print(format(options.option_fmt, 'S') + gettext("SSH Keys")
+			+ format(options.option_date, gettext("last changed"), system.datestr(file_date(prompts.ssh_keys_filename(user)))));
 		keys += 'S';
 		console.add_hotspot('S');
 		console.newline();	
 	}
-	console.print("\x01u[\x01UM\x01u] " + gettext("Sign-off")
-		+ format(" (%s: \x01U%s\x01u)", gettext("last changed"), system.datestr(file_date(prompts.sig_filename(user)))));
+	console.print(format(options.option_fmt, 'M') + gettext("Sign-off")
+		+ format(options.option_date, gettext("last changed"), system.datestr(file_date(prompts.sig_filename(user)))));
 	keys += 'M';
 	console.add_hotspot('M');
 	console.newline(2);	
-	console.putmsg("\x01n\x01h\x01u@Which@ or [\x01UQ\x01u] to @Quit@: \x01U", P_SAVEATR);
+	console.putmsg(options.prompt || "\x01n\x01h\x01u@Which@ or [\x01UQ\x01u] to @Quit@: \x01U", P_SAVEATR);
 	console.add_hotspot('Q');
 
 	switch(console.getkeys(keys, K_UPPER)) {
