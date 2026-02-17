e7c97c9edc95dc29684656de93ea27add1b040c3
Rob Swindell (on Debian Linux) <rob@synchro.net>
2026-02-16 16:29:11 -0800

    More customizeable text via modopts
    for Nelgin

--- exec/user_personal.js
+++ exec/user_personal.js
@@ -11,7 +11,17 @@
 
 // Customizable look via modopts.ini [user_personal]
 // header_prefix
+// header
 // user_fmt
+// text_alias
+// text_name
+// text_handle
+// text_phone
+// text_location
+// text_gender
+// text_password
+// text_ssh_keys
+// text_signature
 // option_with_val_fmt
 // option_with_date_fmt
 // prompt
@@ -29,7 +39,7 @@ if (!options)
 if (!options.option_with_val_fmt)
 	options.option_with_val_fmt = "\x01u[\x01U%c\x01u] %s: \x01U%s";
 if (!options.option_with_date_fmt)
-	options.option_with_date_fmt = "\x01u[\x01U%c\x01u] %s (%s: \x01U%s\x01u)";
+	options.option_with_date_fmt = "\x01u[\x01U%c\x01u] %-9s (%s: \x01U%s\x01u)";
 var ssh_support = server.options & (1<< 12); // BBS_OPT_ALLOW_SSH
 
 prompts.operation = "";
@@ -39,30 +49,29 @@ while(bbs.online && !js.terminated) {
 	console.aborted = false;	
 	console.clear();
 	console.print((options.header_prefix || "\x01U")
-		+ gettext("Personal Information")
-		+ " " + gettext("for")
+		+ (options.header || (gettext("Personal Information") + " " + gettext("for")))
 		+ format(options.user_fmt || " \x01U%s #%u:", user.alias, user.number));
 	console.newline(2);
 	if (options.alias === true) {
-		console.print(format(options.option_with_val_fmt, 'A', gettext("Alias"), user.alias));
+		console.print(format(options.option_with_val_fmt, 'A', options.text_alias || gettext("Alias"), user.alias));
 		keys += 'A';
 		console.add_hotspot('A');
 		console.newline();
 	}
 	if (options.name === true) {
-		console.print(format(options.option_with_val_fmt, 'R', gettext("Real Name"), user.name));
+		console.print(format(options.option_with_val_fmt, 'R', options.text_name ||gettext("Real Name"), user.name));
 		keys += 'R';
 		console.add_hotspot('R');
 		console.newline();
 	}
 	if (options.handle === true) {
-		console.print(format(options.option_with_val_fmt, 'H', gettext("Handle"), user.handle));
+		console.print(format(options.option_with_val_fmt, 'H', options.text_handle ||gettext("Handle"), user.handle));
 		keys += 'H';
 		console.add_hotspot('H');
 		console.newline();
 	}
 	if (options.phone === true) {
-		console.print(format(options.option_with_val_fmt, 'N', gettext("Phone Number"), user.phone));
+		console.print(format(options.option_with_val_fmt, 'N', options.text_phone ||gettext("Phone Number"), user.phone));
 		keys += 'N';
 		console.add_hotspot('N');
 		console.newline();
@@ -71,32 +80,32 @@ while(bbs.online && !js.terminated) {
 		var location = (system.newuser_questions & UQ_ADDRESS) ? user.address : "";
 		if (location) location += " ";
 		location += user.location;
-		console.print(format(options.option_with_val_fmt, 'L', gettext("Location"), location));
+		console.print(format(options.option_with_val_fmt, 'L', options.text_location || gettext("Location"), location));
 		keys += 'L';
 		console.add_hotspot('L');
 		console.newline();
 	}
 	if (options.gender === true) {
-		console.print(format(options.option_with_val_fmt, 'G', gettext("Gender"), user.gender));
+		console.print(format(options.option_with_val_fmt, 'G', options.text_gender || gettext("Gender"), user.gender));
 		keys += 'G';
 		console.add_hotspot('G');
 		console.newline();
 	}
 	if (system.settings & SYS_PWEDIT) {
-		console.print(format(options.option_with_date_fmt, 'P', gettext("Password"), gettext("last changed")
+		console.print(format(options.option_with_date_fmt, 'P', options.text_password || gettext("Password"), gettext("last changed")
 			, system.datestr(user.security.password_date)));
 		keys += 'P';
 		console.add_hotspot('P');
 		console.newline();
 	}
 	if (ssh_support) {
-		console.print(format(options.option_with_date_fmt, 'S', gettext("SSH Keys"), gettext("last changed")
+		console.print(format(options.option_with_date_fmt, 'S', options.text_ssh_keys || gettext("SSH Keys"), gettext("last changed")
 			, system.datestr(file_date(prompts.ssh_keys_filename(user)))));
 		keys += 'S';
 		console.add_hotspot('S');
 		console.newline();	
 	}
-	console.print(format(options.option_with_date_fmt, 'M', gettext("Sign-off"), gettext("last changed")
+	console.print(format(options.option_with_date_fmt, 'M', options.text_signature || gettext("Signature"), gettext("last changed")
 		, system.datestr(file_date(prompts.sig_filename(user)))));
 	keys += 'M';
 	console.add_hotspot('M');
