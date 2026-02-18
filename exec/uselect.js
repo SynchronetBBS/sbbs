ba75961a265a4bede4d610c9c520bbb65491bf69
Rob Swindell (on Debian Linux) <rob@synchro.net>
2026-02-18 01:23:13 -0800

    Fix the 'Q'uit value (supposed to be -1, not the default number)
    Give it the modopts treatment since I'm pretty sure Nelgin was going to ask
    for it
    
    But really, I think this shold be redone as a lightbar menu, RIP menu, etc.

--- exec/uselect.js
+++ exec/uselect.js
@@ -2,6 +2,16 @@
 // for Synchronet v3.21+
 // Set SCFG->System->Loadable Modules->Select Item to "uselect"
 
+// Customizable via modopts/uselect options:
+// - header_fmt
+// - default_item_fmt
+// - nondefault_item_fmt
+// - prompt_fmt
+
+var options = load("modopts.js", "uselect");
+if (!options)
+	options = {};
+
 var dflt = Number(argv[0]);
 
 function digits(n)
@@ -10,19 +20,18 @@ function digits(n)
         return 1;
     return 1 + digits(Math.floor(n / 10));
 }
-console.print(format(bbs.text(bbs.text.SelectItemHdr), console.uselect_title));
+console.print(format(options.header_fmt || bbs.text(bbs.text.SelectItemHdr), console.uselect_title));
 
 var items = console.uselect_items;
 var udflt = 1;
 for(var i = 0; i < items.length; ++i ) {
-	var ch = '*';
-	if (items[i].num == dflt)
-		udflt = i + 1;
-	else
-		ch = ' ';
 	var hotspot = i + 1;
-	console.print(format("\x01U%c\x01v%*s(\x01V%d\x01v) \x01U%s"
-		, ch
+	var fmt = options.default_item_fmt || "\x01U*\x01v%*s(\x01V%d\x01v) \x01U%s";
+	if (items[i].num == dflt)
+		udflt = hotspot;
+	else
+		fmt = options.nondefault_item_fmt || "\x01U \x01v%*s(\x01V%d\x01v) \x01U%s";
+	console.print(format(fmt
 		, digits(hotspot) - digits(items.length), ""
 		, hotspot, items[i].name));
 	if(digits(hotspot) < digits(items.length))
@@ -31,10 +40,10 @@ for(var i = 0; i < items.length; ++i ) {
 	console.newline();
 }
 
-console.mnemonics(format(bbs.text(bbs.text.SelectItemWhich), udflt));
+console.mnemonics(format(options.prompt_fmt || bbs.text(bbs.text.SelectItemWhich), udflt));
 var choice = console.getnum(items.length);
 if (choice > 0)
 	choice = items[choice - 1].num;
-else
+else if (choice == 0)
 	choice = dflt;
 exit(choice);
