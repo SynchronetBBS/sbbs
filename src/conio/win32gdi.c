3f4843aa8dfe8b72cb231faa9a3fbd1c9ce5c855
Deucе <shurd@sasktel.net>
2026-02-23 11:45:23 -0500

    Set an explicit buffer size for keyboard pipe
    This may be what's causing hangs on Windows 10 for ticket 214.
    
    While we're here, fix some warnings.

--- src/conio/win32gdi.c
+++ src/conio/win32gdi.c
@@ -1262,14 +1262,14 @@ gdi_get_window_info(int *width, int *height, int *xpos, int *ypos)
 static volatile long PipeSerialNumber;
 BOOL CreateOverlappedPipe(LPHANDLE rp, LPHANDLE wp)
 {
-	UCHAR PipeNameBuffer[ MAX_PATH ];
+	char PipeNameBuffer[ MAX_PATH ];
 	sprintf( PipeNameBuffer,
-           "\\\\.\\Pipe\\RemoteExeAnon.%08x.%08x",
+           "\\\\.\\Pipe\\LOCAL\\SyncTERMKeyboard.%08lx.%08lx",
            GetCurrentProcessId(),
            InterlockedIncrement(&PipeSerialNumber)
          );
 	*rp = CreateNamedPipeA(PipeNameBuffer, PIPE_ACCESS_INBOUND | FILE_FLAG_OVERLAPPED, PIPE_TYPE_BYTE | PIPE_WAIT,
-	    1, 0, 0, 120 * 1000, NULL);
+	    1, 1024, 1024, 120 * 1000, NULL);
 	*wp = CreateFileA(PipeNameBuffer, GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	return TRUE;
 }
