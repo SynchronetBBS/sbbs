SynchronetBBS/sbbs

commit 556225fa71602778741c82bc89c38b7d3d970b7c

Don't pass smbmsg_t (680 bytes) by value
So this started with addressing CID 174264: Big parameter passed by value (PASS_BY_VALUE), but then I fell down the constification rabbit hole. :-)

src/sbbs3/writemsg.cpp
@@ -1845,57 +1845,58 @@ bool sbbs_t::movemsg(smbmsg_t* msg, uint subnum)
 	return(true);
 }
 
-ushort sbbs_t::chmsgattr(smbmsg_t msg)
+ushort sbbs_t::chmsgattr(const smbmsg_t* msg)
 {
 	int ch;
+	uint16_t attr = msg->hdr.attr;
 
 	while(online && !(sys_status&SS_ABORT)) {
 		CRLF;
-		show_msgattr(&msg);
+		show_msgattr(msg);
 		menu("msgattr");
 		ch=getkey(K_UPPER);
 		if(ch)
 			bprintf("%c\r\n",ch);
 		switch(ch) {
 			case 'P':
-				msg.hdr.attr^=MSG_PRIVATE;
+				attr^=MSG_PRIVATE;
 				break;
 			case 'S':
-				msg.hdr.attr^=MSG_SPAM;
+				attr^=MSG_SPAM;
 				break;
 			case 'R':
-				msg.hdr.attr^=MSG_READ;
+				attr^=MSG_READ;
 				break;
 			case 'K':
-				msg.hdr.attr^=MSG_KILLREAD;
+				attr^=MSG_KILLREAD;
 				break;
 			case 'A':
-				msg.hdr.attr^=MSG_ANONYMOUS;
+				attr^=MSG_ANONYMOUS;
 				break;
 			case 'N':   /* Non-purgeable */
-				msg.hdr.attr^=MSG_PERMANENT;
+				attr^=MSG_PERMANENT;
 				break;
 			case 'M':
-				msg.hdr.attr^=MSG_MODERATED;
+				attr^=MSG_MODERATED;
 				break;
 			case 'V':
-				msg.hdr.attr^=MSG_VALIDATED;
+				attr^=MSG_VALIDATED;
 				break;
 			case 'D':
-				msg.hdr.attr^=MSG_DELETE;
+				attr^=MSG_DELETE;
 				break;
 			case 'L':
-				msg.hdr.attr^=MSG_LOCKED;
+				attr^=MSG_LOCKED;
 				break;
 			case 'C':
-				msg.hdr.attr^=MSG_NOREPLY;
+				attr^=MSG_NOREPLY;
 				break;
 			case 'E':
-				msg.hdr.attr^=MSG_REPLIED;
+				attr^=MSG_REPLIED;
 				break;
 			default:
-				return(msg.hdr.attr); 
+				return(attr); 
 		} 
 	}
-	return(msg.hdr.attr);
+	return(attr);
 }

src/sbbs3/getmsg.cpp
@@ -75,7 +75,7 @@ int sbbs_t::loadmsg(smbmsg_t *msg, ulong number)
 }
 
 /* Synchronized with atcode()! */
-void sbbs_t::show_msgattr(smbmsg_t* msg)
+void sbbs_t::show_msgattr(const smbmsg_t* msg)
 {
 	uint16_t attr = msg->hdr.attr;
 	uint16_t poll = attr&MSG_POLL_VOTE_MASK;
@@ -163,7 +163,7 @@ const char* sbbs_t::msghdr_field(const smbmsg_t* msg, const char* str, char* buf
 /****************************************************************************/
 /* Displays a message header to the screen                                  */
 /****************************************************************************/
-void sbbs_t::show_msghdr(smb_t* smb, smbmsg_t* msg, const char* subject, const char* from, const char* to)
+void sbbs_t::show_msghdr(smb_t* smb, const smbmsg_t* msg, const char* subject, const char* from, const char* to)
 {
 	char	str[MAX_PATH+1];
 	char	age[64];

src/sbbs3/js_bbs.cpp
@@ -3948,7 +3948,7 @@ js_change_msg_attr(JSContext *cx, uintN argc, jsval *arglist)
 		return JS_TRUE;
 
 	rc=JS_SUSPENDREQUEST(cx);
-	int32 attr = sbbs->chmsgattr(*msg);
+	int32 attr = sbbs->chmsgattr(msg);
 	JS_RESUMEREQUEST(cx, rc);
 
 	JS_SET_RVAL(cx, arglist, INT_TO_JSVAL(attr));

src/sbbs3/readmail.cpp
@@ -503,7 +503,7 @@ void sbbs_t::readmail(uint usernumber, int which, long lm_mode)
 				done=1;
 				break;
 			case 'C':   /* Change attributes of last piece */
-				i=chmsgattr(msg);
+				i=chmsgattr(&msg);
 				if(msg.hdr.attr==i)
 					break;
 				if(msg.total_hfields)

src/sbbs3/readmsgs.cpp
@@ -1214,7 +1214,7 @@ int sbbs_t::scanposts(uint subnum, long mode, const char *find)
 								: matchuser(&cfg,msg.from,FALSE));
 							break;
 						case 'C':   /* Change message attributes */
-							i=chmsgattr(msg);
+							i=chmsgattr(&msg);
 							if(msg.hdr.attr==i)
 								break;
 							if(msg.total_hfields)

src/sbbs3/sbbs.h
@@ -614,7 +614,7 @@ public:
 
 	csi_t	main_csi;		/* Main Command Shell Image */
 
-	smbmsg_t*	current_msg;	/* For message header @-codes */
+	const smbmsg_t*	current_msg;	/* For message header @-codes */
 	const char*	current_msg_subj;
 	const char*	current_msg_from;
 	const char*	current_msg_to;
@@ -745,7 +745,7 @@ public:
 	void	removeline(char *str, char *str2, char num, char skip);
 	ulong	msgeditor(char *buf, const char *top, char *title);
 	bool	editfile(char *path, bool msg=false);
-	ushort	chmsgattr(smbmsg_t);
+	ushort	chmsgattr(const smbmsg_t*);
 	bool	quotemsg(smb_t*, smbmsg_t*, bool tails = false);
 	bool	editmsg(smb_t*, smbmsg_t*);
 	void	editor_inf(int xeditnum, const char *to, const char* from, const char *subj, long mode
@@ -767,8 +767,8 @@ public:
 
 	/* getmsg.cpp */
 	int		loadmsg(smbmsg_t *msg, ulong number);
-	void	show_msgattr(smbmsg_t*);
-	void	show_msghdr(smb_t*, smbmsg_t*, const char *subj = NULL, const char* from = NULL, const char* to = NULL);
+	void	show_msgattr(const smbmsg_t*);
+	void	show_msghdr(smb_t*, const smbmsg_t*, const char *subj = NULL, const char* from = NULL, const char* to = NULL);
 	bool	show_msg(smb_t*, smbmsg_t*, long p_mode = 0, post_t* post = NULL);
 	bool	msgtotxt(smb_t*, smbmsg_t*, const char *fname, bool header = true, ulong gettxt_mode = GETMSGTXT_ALL);
 	const char* msghdr_text(const smbmsg_t*, uint index);

src/smblib/smblib.h
@@ -243,8 +243,8 @@ SMBEXPORT char*		smb_hfieldtype(uint16_t type);
 SMBEXPORT uint16_t	smb_hfieldtypelookup(const char*);
 SMBEXPORT char*		smb_dfieldtype(uint16_t type);
 SMBEXPORT char*		smb_faddrtoa(fidoaddr_t* addr, char* outstr);
-SMBEXPORT char*		smb_netaddr(net_t* net);
-SMBEXPORT char*		smb_netaddrstr(net_t* net, char* fidoaddr_buf);
+SMBEXPORT char*		smb_netaddr(const net_t* net);
+SMBEXPORT char*		smb_netaddrstr(const net_t* net, char* fidoaddr_buf);
 SMBEXPORT char*		smb_nettype(enum smb_net_type);
 SMBEXPORT char*		smb_zonestr(int16_t zone, char* outstr);
 SMBEXPORT char*		smb_msgattrstr(int16_t attr, char* outstr, size_t maxlen);

src/smblib/smbstr.c
@@ -309,7 +309,7 @@ fidoaddr_t smb_atofaddr(const fidoaddr_t* sys_addr, const char *str)
 /* Returns ASCIIZ representation of network address (net_t)					*/
 /* NOT THREAD-SAFE!															*/
 /****************************************************************************/
-char* smb_netaddr(net_t* net)
+char* smb_netaddr(const net_t* net)
 {
 	return(smb_netaddrstr(net, NULL));
 }
@@ -317,7 +317,7 @@ char* smb_netaddr(net_t* net)
 /****************************************************************************/
 /* Copies ASCIIZ representation of network address (net_t) into buf			*/
 /****************************************************************************/
-char* smb_netaddrstr(net_t* net, char* fidoaddr_buf)
+char* smb_netaddrstr(const net_t* net, char* fidoaddr_buf)
 {
 	if(net->type==NET_FIDO)
 		return(smb_faddrtoa((fidoaddr_t*)net->addr,fidoaddr_buf));

commit 9abbcf31c3e92cc111f9189c835483fd9c94b52c

Display changed attributes in change msg attribute function
This appears to have been broken since commit 556225fa7 (4 years ago,
addressing a Coverity complaint <sigh>).

When changing (toggling) the attributes of a message, the new/changed
attributes were supposed to be shown, but were not. Exiting did save those
attributes correctly, but the UI was very non-intuitive because of this bug.

src/sbbs3/writemsg.cpp
@@ -1936,58 +1936,60 @@ bool sbbs_t::movemsg(smbmsg_t* msg, int subnum)
 	return true;
 }
 
-ushort sbbs_t::chmsgattr(const smbmsg_t* msg)
+ushort sbbs_t::chmsgattr(const smbmsg_t* org_msg)
 {
 	int      ch;
-	uint16_t attr = msg->hdr.attr;
+	smbmsg_t msg = *org_msg;
 
 	while (online && !(sys_status & SS_ABORT)) {
 		term->newline();
-		show_msgattr(msg);
+		show_msgattr(&msg);
 		menu("msgattr");
 		ch = getkey(K_UPPER);
+		if (sys_status & SS_ABORT)
+			break;
 		if (ch)
 			bprintf("%c\r\n", ch);
 		switch (ch) {
 			case 'P':
-				attr ^= MSG_PRIVATE;
+				msg.hdr.attr ^= MSG_PRIVATE;
 				break;
 			case 'S':
-				attr ^= MSG_SPAM;
+				msg.hdr.attr ^= MSG_SPAM;
 				break;
 			case 'R':
-				attr ^= MSG_READ;
+				msg.hdr.attr ^= MSG_READ;
 				break;
 			case 'K':
-				attr ^= MSG_KILLREAD;
+				msg.hdr.attr ^= MSG_KILLREAD;
 				break;
 			case 'A':
-				attr ^= MSG_ANONYMOUS;
+				msg.hdr.attr ^= MSG_ANONYMOUS;
 				break;
 			case 'N':   /* Non-purgeable */
-				attr ^= MSG_PERMANENT;
+				msg.hdr.attr ^= MSG_PERMANENT;
 				break;
 			case 'M':
-				attr ^= MSG_MODERATED;
+				msg.hdr.attr ^= MSG_MODERATED;
 				break;
 			case 'V':
-				attr ^= MSG_VALIDATED;
+				msg.hdr.attr ^= MSG_VALIDATED;
 				break;
 			case 'D':
-				attr ^= MSG_DELETE;
+				msg.hdr.attr ^= MSG_DELETE;
 				break;
 			case 'L':
-				attr ^= MSG_LOCKED;
+				msg.hdr.attr ^= MSG_LOCKED;
 				break;
 			case 'C':
-				attr ^= MSG_NOREPLY;
+				msg.hdr.attr ^= MSG_NOREPLY;
 				break;
 			case 'E':
-				attr ^= MSG_REPLIED;
+				msg.hdr.attr ^= MSG_REPLIED;
 				break;
 			default:
-				return attr;
+				return msg.hdr.attr;
 		}
 	}
-	return attr;
+	return org_msg->hdr.attr;
 }