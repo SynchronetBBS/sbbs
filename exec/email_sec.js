SynchronetBBS/sbbs

commit 1dac6acd2215db3e569eed33d40bf3fce8dac446

Port Reneclone (Renegade Clone) shell from Baja to JavaScript
This shell (unlike the PCBoard shell) really benefitted from reuse of the
table-based menu approach used in default.js. So I moved the menu loop
from there to shell_lib.js, for reuse in renegade.js.

Also moved some things from chat_sec.js and email_sec.js to shell_lib.js for
more reuse/commonality.

We could do more to have the Renegade shell work like a Renegade system, but
I think this hits the main points and resolves some weird key bindings of the
old shell ('E' for batch transfer menu?) and inconsistencies.

I tried to explore some stock Renegade systems online today but none of them
have guest accounts and almost all of them have very restricted access for new
users (usually can't get out of the main menu).

The menu files were updated using PabloDraw and saved as .msg. Using @-code
instead of whatever Ctrl-A magic that was to hide the sysop menu option.

This is another step toward resolving issue #902.

exec/email_sec.js
@@ -5,6 +5,7 @@
 
 require("sbbsdefs.js", "WM_NONE");
 require("userdefs.js", "USER_EXPERT");
+var shell = load({}, "shell_lib.js");
 var userprops = bbs.mods.userprops;
 if(!userprops)
 	userprops = load(bbs.mods.userprops = {}, "userprops.js");
@@ -40,53 +41,15 @@ while(bbs.online) {
 			}
 			break;
 		case 'F':	// Send Feedback
-			bbs.email(/* user # */1, bbs.text(bbs.text.ReFeedback));
+			shell.send_feedback();
 			break;
 		case 'A':	// Send file attachment
 			wm_mode = WM_FILE;
 		case 'S':	// Send Mail
-			console.putmsg(bbs.text(bbs.text.Email));
-			var name = console.getstr(40, K_TRIM);
-			if(!name)
-				break;
-			if(name.indexOf('@') > 0) {
-				bbs.netmail(name);
-				break;
-			}
-			var number = bbs.finduser(name);
-			if(console.aborted)
-				break;
-			if(!number)
-				number = system.matchuser(name);
-			if(!number && (msg_area.settings&MM_REALNAME))
-				number = system.matchuserdata(U_NAME, name);
-			if(number)
-				bbs.email(number, wm_mode);
-			else
-				console.putmsg(bbs.text(bbs.text.UnknownUser));
+			shell.send_email();
 			break;
 		case 'N':	// Send NetMail
-			var netmail = msg_area.fido_netmail_settings | msg_area.inet_netmail_settings;
-			console.crlf();
-			if((netmail&NMAIL_FILE) && !console.noyes("Attach a file"))
-				wm_mode = WM_FILE;
-			if(console.aborted)
-				break;
-			console.putmsg(bbs.text(bbs.text.EnterNetMailAddress));
-			var addr_list = userprops.get(ini_section, "address", []) || [];
-			var addr = console.getstr(256, K_LINE | K_TRIM, addr_list);
-			if(!addr || console.aborted)
-				break;
-			if(bbs.netmail(addr.split(','), wm_mode)) {
-				var addr_idx = addr_list.indexOf(addr);
-				if(addr_idx >= 0)
-					addr_list.splice(addr_idx, 1);
-				addr_list.unshift(addr);
-				if(addr_list.length > NetmailAddressHistoryLength)
-					addr_list.length = NetmailAddressHistoryLength;
-				userprops.set(ini_section, "address", addr_list);
-				userprops.set(ini_section, "localtime", new Date().toString());
-			}
+			shell.send_netmail();
 			break;
 		default:
 			exit(0);

exec/load/shell_lib.js
@@ -575,4 +575,159 @@ function download_user_files()
 	}
 }
 
+// From email_sec.js
+function send_email()
+{
+	console.putmsg(bbs.text(bbs.text.Email));
+	var name = console.getstr(40, K_TRIM);
+	if(!name)
+		return false;
+	if(name.indexOf('@') > 0)
+		return bbs.netmail(name);
+	var number = bbs.finduser(name);
+	if(console.aborted)
+		return false;
+	if(!number)
+		number = system.matchuser(name);
+	if(!number && (msg_area.settings&MM_REALNAME))
+		number = system.matchuserdata(U_NAME, name);
+	if(number)
+		return bbs.email(number, WM_NONE);
+	console.putmsg(bbs.text(bbs.text.UnknownUser));
+	return false;
+}
+
+// From email_sec.js
+function send_netmail()
+{
+	var userprops = bbs.mods.userprops || load(bbs.mods.userprops = {}, "userprops.js");
+	var netmail = msg_area.fido_netmail_settings | msg_area.inet_netmail_settings;
+	const ini_section = "netmail sent";
+	console.crlf();
+	var wm_mode = WM_NONE;
+	if((netmail&NMAIL_FILE) && !console.noyes("Attach a file"))
+		wm_mode = WM_FILE;
+	if(console.aborted)
+		return false;
+	console.putmsg(bbs.text(bbs.text.EnterNetMailAddress));
+	var addr_list = userprops.get(ini_section, "address", []) || [];
+	var addr = console.getstr(256, K_LINE | K_TRIM, addr_list);
+	if(!addr || console.aborted)
+		return false;
+	if(bbs.netmail(addr.split(','), wm_mode)) {
+		var addr_idx = addr_list.indexOf(addr);
+		if(addr_idx >= 0)
+			addr_list.splice(addr_idx, 1);
+		addr_list.unshift(addr);
+		if(addr_list.length > NetmailAddressHistoryLength)
+			addr_list.length = NetmailAddressHistoryLength;
+		userprops.set(ini_section, "address", addr_list);
+		userprops.set(ini_section, "localtime", new Date().toString());
+		return true;
+	}
+	return false;
+}
+
+function send_feedback()
+{
+	return bbs.email(/* user # */1, bbs.text(bbs.text.ReFeedback));
+}
+
+function page_sysop()
+{
+	if(!bbs.page_sysop()
+		&& !deny(format(bbs.text(bbs.text.ChatWithGuruInsteadQ), system.guru || "The Guru")))
+		bbs.page_guru();
+}
+
+// From default.js
+function menu_loop()
+{
+	var last_str_cmd = "";
+
+	// The menu-display/command-prompt loop
+	while(bbs.online && !js.terminated) {
+		if(!(user.settings & USER_EXPERT)) {
+			if (menu.cls)
+				console.clear();
+			bbs.menu(menu.file);
+		}
+		if (menu.node_action !== undefined)
+			bbs.node_action = menu.node_action;
+		bbs.nodesync();
+		eval(menu.eval);
+		console.newline();
+		console.aborted = false;
+		console.putmsg(menu.prompt, P_SAVEATR);
+		var cmd = console.getkey(K_UPPER);
+		if(cmd > ' ')
+			console.print(cmd);
+		if(cmd == ';') {
+			cmd = console.getstr(100, K_LINEWRAP);
+			if(cmd == '!')
+				cmd = last_str_cmd;
+			load({}, "str_cmds.js", cmd);
+			last_str_cmd = cmd;
+			continue;
+		}
+		if(cmd == '/') {
+			cmd = console.getkey(K_UPPER);
+			console.print(cmd);
+			if(cmd >= '1' && cmd <= '9') {
+				menu.slash_num_input(cmd);
+				continue;
+			}
+			cmd = '/' + cmd;
+		}
+		if(cmd >= '1' && cmd <= '9') {
+			menu.num_input(cmd);
+			continue;
+		}
+		if(cmd > ' ') {
+			bbs.log_key(cmd, /* comma: */true);
+		}
+		if(menu.nav[cmd]) {
+			if(menu.nav[cmd].eval)
+				eval(menu.nav[cmd].eval);
+			continue;
+		}
+		console.newline();
+		console.line_counter = 0;
+		if(cmd == help_key) {
+			if(user.settings & USER_EXPERT) {
+				if (menu.cls)
+					console.clear();
+				bbs.menu(menu.file);
+			}
+			continue;
+		}
+		var menu_cmd = menu.command[cmd];
+		if(!menu_cmd) {
+			console.print("\r\n\x01c\x01h" + gettext("Unrecognized command."));
+			if(user.settings & USER_EXPERT)
+				console.print("  " + gettext("Hit") + " '\x01i" + help_key + "\x01n\x01c\x01h' " + gettext("for a menu."));
+			console.print("  " + gettext("Type \x01y;help\x01c for more commands."));
+			console.newline();
+			continue;
+		}
+		if(menu_cmd.ars === undefined || bbs.compare_ars(menu_cmd.ars)) {
+			if(menu_cmd.msg)
+				console.print(menu_cmd.msg);
+			if(menu_cmd.eval)
+				eval(menu_cmd.eval);
+			if(menu_cmd.exec) {
+				var script = system.mods_dir + menu_cmd.exec;
+				if(!file_exists(script))
+					script = system.exec_dir + menu_cmd.exec;
+				if(menu_cmd.args)
+					js.exec.apply(null, [script, {}].concat(menu_cmd.args));
+				else
+					js.exec(script, {});
+			}
+		}
+		else if(menu_cmd.err)
+			console.print(menu_cmd.err);
+	}
+}
+
 this;

exec/chat_sec.js
@@ -4,6 +4,7 @@
 
 require("sbbsdefs.js", 'USER_EXPERT');
 require("nodedefs.js", 'NODE_CHAT');
+var shell = load({}, "shell_lib.js");
 
 // Over-ride these default values by creating/modifying the [chat] section in your ctrl/modopts.ini file
 var options = load("modopts.js", "chat");
@@ -139,9 +140,7 @@ while(bbs.online && !console.aborted) {
 			bbs.private_chat();
 			break;
 		case 'C':
-			if(!bbs.page_sysop()
-				&& !deny(format(bbs.text(bbs.text.ChatWithGuruInsteadQ), system.guru || "The Guru")))
-				bbs.page_guru();
+			shell.page_sysop();
 			break;
 		case 'T':
 			bbs.page_guru();

exec/pcboard.js
@@ -82,8 +82,7 @@ while(bbs.online && !js.terminated) {
 			bbs.temp_xfer();
 			continue;
 		case 'C':
-			bbs.email(/* user # */1, bbs.text(bbs.text.ReFeedback));
-			console.newline();
+			shell.send_feedback();
 			continue;
 		case 'Y':
 			bbs.scan_subs(SCAN_TOYOU);

exec/renegade.js
@@ -1,11 +1,10 @@
-// Default/Classic Synchronet Command Shell
-// replaces default.src/bin
+// Renegade Clone command shell
 
-// @format.tab-size 4
+// @format.tab-size 4, @format.use-tabs true
 
 "use strict";
 
-require("sbbsdefs.js", "K_UPPER");
+require("sbbsdefs.js", "SYS_RA_EMU");
 require("userdefs.js", "UFLAG_T");
 require("nodedefs.js", "NODE_MAIN");
 require("key_defs.js", "KEY_UP");
@@ -13,75 +12,109 @@ require("gettext.js", "gettext");
 load("termsetup.js");
 var shell = load({}, "shell_lib.js");
 
-system.settings &= ~SYS_RA_EMU; // Use (R)e-read and (A)uto-reply keys
+system.settings |= SYS_RA_EMU;  // Swap Msg ReRead/Reply commands
 
 shell.help_key = '?';
 // If user has unlimited time, display time-used rather than time-remaining
 const time_code = user.security.exemptions & UFLAG_T ? "@TUSED@" : "@TLEFT@";
+const time_prefix = user.security.exemptions & UFLAG_T ? gettext("Used") : gettext("Left");
 
 shell.main_menu = {
-	file: "main",
+	cls: true,
+	file: "renegade/main",
 	eval: 'bbs.main_cmds++',
 	node_action: NODE_MAIN,
-	prompt: gettext("\x01-\x01c\xfe \x01b\x01h", "shell_prompt_begin")
-		+ gettext("Main")
-		+ gettext(" \x01n\x01c\xfe \x01h", "shell_prompt_middle")
-		+ time_code
-		+ gettext(" \x01n\x01c[\x01h@GN@\x01n\x01c] @GRP@\x01\\ [\x01h@SN@\x01n\x01c] @SUB@: \x01n", "shell_main_prompt_end"),
-	num_input: shell.get_sub_num,
-	slash_num_input: shell.get_grp_num,
+	prompt: "\x01n\x01b\x01h" + gettext("Time") + " " + time_prefix + ": [\x01c" + time_code + "\x01b] "
+		+ "\x01b\x01h(\x01c" + shell.help_key + "\x01b=\x01c"
+		+ gettext("Help") + "\x01b)\r\n"
+		+ "\x01c" + gettext("Main") + " \x01b" + gettext("Menu") + " \x01m: \x01n",
 	command: {
 	 'A': { eval: 'bbs.auto_msg()' },
 	'/A': { exec: 'avatar_chooser.js'
 			,ars: 'ANSI and not GUEST'
 			,err: '\r\n' + gettext("Sorry, only regular users with ANSI terminals can do that.") + '\r\n' },
-	 'B': { eval: 'bbs.scan_subs(SCAN_BACK)'
-			,msg: '\r\n\x01c\x01h' + gettext("Browse/New Message Scan") + '\r\n' },
+	 'B': { eval: 'bbs.exec_xtrn("sbbslist")' },
 	 'C': { eval: 'bbs.chat_sec()' },
-	 'D': { eval: 'bbs.user_config(); exit()' },
-	 'E': { exec: 'email_sec.js' },
-	 'F': { eval: 'bbs.scan_subs(SCAN_FIND)'
-			,msg: '\r\n\x01c\x01h' + gettext("Find Text in Messages") + '\r\n' },
-	'/F': { eval: 'bbs.scan_subs(SCAN_FIND, /* all */true)' },
-	 'G': { eval: 'bbs.text_sec()' },
+	 'P': { eval: 'bbs.user_config(); exit()' },
+	 'S': { eval: 'bbs.text_sec()' },
 	 'I': { eval: 'main_info()' },
-	 'J': { eval: 'select_msg_area()' },
-	 'L': { eval: 'bbs.list_msgs()' },
+	 'Y': { eval: 'bbs.user_info()' },
+     'L': { eval: 'bbs.list_logons()' },
+    '/L': { eval: 'bbs.list_nodes()' },
+	 '#': { eval: 'bbs.list_nodes()' },
+	 'N': { eval: 'send_feedback()' },
+	 'O': { eval: 'bbs.xtrn_sec()' },
+	 'G': { eval: 'logoff(/* fast: */false)' },
+	'/G': { eval: 'logoff(/* fast: */true)' },
+	 'U': { eval: 'list_users()' },
+	 'V': { exec: 'scanpolls.js', args: ['all'] },
+	 'W': { eval: 'bbs.whos_online()' },
+	 'X': { eval: 'user.settings ^= USER_EXPERT' },
+	 '$': { eval: 'bbs.time_bank()' },
+	 '!': { eval: 'bbs.qwk_sec()' },
+	 '*': { eval: 'bbs.menu("sysmain")'
+			,ars: 'SYSOP or EXEMPT Q or I or N' },
+	},
+	nav: {
+	'\r': { },
+	 'E': { eval: 'menu = email_menu' },
+ 	 'M': { eval: 'menu = message_menu' },
+	 'F': { eval: 'enter_file_section(); menu = file_menu' },
+	},
+};
+
+shell.message_menu = {
+	cls: true,
+	file: "renegade/message",
+	eval: 'bbs.main_cmds++',
+	node_action: NODE_RMSG,
+	prompt: "\x01n\x01b\x01h[\x01n\x01c@GN@\x01b\x01h][\x01n\x01c@SN@\x01b\x01h] \x01n\x01c- \x01h@GRP@ @SUB@\r\n"
+		+ "\x01n\x01b\x01h" + gettext("Time") + " " + time_prefix + ": [\x01c" + time_code + "\x01b] "
+		+ "\x01b\x01h(\x01c" + shell.help_key + "\x01b=\x01c"
+		+ gettext("Help") + "\x01b)\r\n"
+		+ "\x01c" + gettext("Message") + " \x01b" + gettext("Menu") + " \x01m: \x01n",
+	num_input: shell.get_sub_num,
+	slash_num_input: shell.get_grp_num,
+	command: {
+	 'A': { eval: 'select_msg_area()' },
+     'J': { eval: 'select_msg_area()' },
+     'C': { eval: 'bbs.chat_sec()' },
 	'/L': { eval: 'bbs.list_nodes()' },
-	 'M': { eval: 'bbs.time_bank()' },
 	'/M': { exec: 'postmeme.js' },
 	 'N': { eval: 'bbs.scan_subs(SCAN_NEW)'
 			,msg: '\r\n\x01c\x01h' + gettext("New Message Scan") + '\r\n' },
 	'/N': { eval: 'bbs.scan_subs(SCAN_NEW, /* all */true)' },
-	 'O': { eval: 'logoff(/* fast: */false)' },
-	'/O': { eval: 'logoff(/* fast: */true)' },
+	 'G': { eval: 'logoff(/* fast: */false)' },
+	'/G': { eval: 'logoff(/* fast: */true)' },
 	 'P': { eval: 'bbs.post_msg()' },
 	'/P': { exec: 'postpoll.js' },
-	 'Q': { eval: 'bbs.qwk_sec()' },
+	 '!': { eval: 'bbs.qwk_sec()' },
 	 'R': { eval: 'bbs.scan_msgs()' },
-	 'S': { eval: 'bbs.scan_subs(SCAN_TOYOU)'
+	 'S': { eval: 'bbs.scan_subs(SCAN_FIND)'
+			,msg: '\r\n\x01c\x01h' + gettext("Find Text in Messages") + '\r\n' },
+	'/S': { eval: 'bbs.scan_subs(SCAN_FIND, /* all */true)' },
+	 'Y': { eval: 'bbs.scan_subs(SCAN_TOYOU)'
 			,msg: '\r\n\x01c\x01h' + gettext("Scan for Messages Posted to You") + '\r\n' },
-	'/S': { eval: 'bbs.scan_subs(SCAN_TOYOU, /* all */true)' },
-	 'U': { eval: 'list_users()' },
-	'/U': { eval: 'bbs.list_users(UL_ALL)' },
+	'/Y': { eval: 'bbs.scan_subs(SCAN_TOYOU, /* all */true)' },
+     'U': { eval: 'bbs.list_users(UL_SUB)' },
+	'/U': { eval: 'list_users()' },
 	 'V': { exec: 'scanpolls.js' },
 	'/V': { exec: 'scanpolls.js', args: ['all'] },
 	 'W': { eval: 'bbs.whos_online()' },
-	 'X': { eval: 'bbs.xtrn_sec()' },
-	 'Z': { eval: 'bbs.scan_subs(SCAN_NEW | SCAN_CONT)'
-			,msg: '\r\n\x01c\x01h' + gettext("Continuous New Message Scan") + '\r\n' },
-	'/Z': { eval: 'bbs.scan_subs(SCAN_NEW | SCAN_CONT, /* all */true)' },
-	 '*': { eval: 'show_subs(bbs.curgrp)' },
-	'/*': { eval: 'show_grps()' },
+	 'X': { eval: 'user.settings ^= USER_EXPERT' },
+	 '$': { eval: 'bbs.time_bank()' },
 	 '&': { exec: 'msgscancfg.js' },
-	 '!': { eval: 'bbs.menu("sysmain")'
+	 'Z': { exec: 'msgscancfg.js' },
+	 '*': { eval: 'bbs.menu("sysmain")'
 			,ars: 'SYSOP or EXEMPT Q or I or N' },
 	 '#': {  msg: '\r\n\x01c\x01h' + gettext("Type the actual number, not the symbol.") + '\r\n' },
 	'/#': {  msg: '\r\n\x01c\x01h' + gettext("Type the actual number, not the symbol.") + '\r\n' },
 	},
 	nav: {
 	'\r': { },
-	 'T': { eval: 'enter_file_section(); menu = file_menu' },
+	 'E': { eval: 'menu = email_menu' },
+	 'F': { eval: 'enter_file_section(); menu = file_menu' },
+	 'Q': { eval: 'menu = main_menu' },
 	 '>': { eval: 'sub_up()' },
 	 '}': { eval: 'sub_up()' },
 	 ')': { eval: 'sub_up()' },
@@ -97,23 +130,26 @@ shell.main_menu = {
 };
 
 // Can't do these statically through initialization:
-shell.main_menu.nav[KEY_UP] = { eval: 'sub_up()' };
-shell.main_menu.nav[KEY_DOWN] = { eval: 'sub_down()' };
-shell.main_menu.nav[KEY_RIGHT] = { eval: 'grp_up()' };
-shell.main_menu.nav[KEY_LEFT] = { eval: 'grp_down()' };
+shell.message_menu.nav[KEY_UP] = { eval: 'sub_up()' };
+shell.message_menu.nav[KEY_DOWN] = { eval: 'sub_down()' };
+shell.message_menu.nav[KEY_RIGHT] = { eval: 'grp_up()' };
+shell.message_menu.nav[KEY_LEFT] = { eval: 'grp_down()' };
 
 shell.file_menu = {
-	file: "transfer",
+	cls: true,
+	file: "renegade/transfer",
 	eval: 'bbs.file_cmds++',
 	node_action: NODE_XFER,
-	prompt: gettext("\x01-\x01c\xfe \x01b\x01h", "shell_prompt_begin")
-		+ gettext("File")
-		+ gettext(" \x01n\x01c\xfe \x01h", "shell_prompt_middle")
-		+ time_code
-		+ gettext(" \x01n\x01c(\x01h@LN@\x01n\x01c) @LIB@\x01\\ (\x01h@DN@\x01n\x01c) @DIR@: \x01n", "shell_file_prompt_end"),
+	prompt: "\x01n\x01b\x01h[\x01n\x01c@LN@\x01b\x01h][\x01n\x01c@DN@\x01b\x01h] \x01n\x01c- \x01h@LIB@ @DIR@\r\n"
+		+ "\x01b\x01h" + gettext("Time") + " " + time_prefix + ": [\x01c" + time_code + "\x01b] "
+		+ "\x01b\x01h(\x01c" + shell.help_key + "\x01b=\x01c"
+		+ gettext("Help") + "\x01b)\r\n"
+		+ "\x01c" + gettext("File") + " \x01b" + gettext("Menu") + " \x01m: \x01n",
 	num_input: shell.get_dir_num,
 	slash_num_input: shell.get_lib_num,
 	command: {
+	 'A': { eval: 'select_file_area()' },
+	 'J': { eval: 'select_file_area()' },
 	 'B': { eval: 'bbs.batch_menu()' },
 	 'C': { eval: 'bbs.chat_sec()' },
 	 'D': { eval: 'download_files()'
@@ -127,8 +163,8 @@ shell.file_menu = {
 	 'F': { eval: 'bbs.scan_dirs(FL_FINDDESC);'
 			,msg: '\r\n\x01c\x01h' + gettext("Find Text in File Descriptions (no wildcards)") + '\r\n' },
 	'/F': { eval: 'bbs.scan_dirs(FL_FINDDESC, /* all: */true);' },
-	 'I': { eval: 'file_info()' },
-	 'J': { eval: 'select_file_area()' },
+	 'G': { eval: 'logoff(/* fast: */false)' },
+	'/G': { eval: 'logoff(/* fast: */true)' },
 	 'L': { eval: 'list_files()' },
 	'/L': { eval: 'bbs.list_nodes()' },
 	 'N': { eval: 'bbs.scan_dirs(FL_ULTIME)'
@@ -136,6 +172,8 @@ shell.file_menu = {
 	'/N': { eval: 'bbs.scan_dirs(FL_ULTIME, /* all */true)' },
 	 'O': { eval: 'logoff(/* fast: */false)' },
 	'/O': { eval: 'logoff(/* fast: */true)' },
+	 '&': { exec: 'filescancfg.js' },
+	 'P': { exec: 'filescancfg.js' },
 	 'R': { eval: 'view_file_info(FI_REMOVE)'
 			,msg: '\r\n\x01c\x01h' + gettext("Remove/Edit File(s)") + '\r\n' },
 	 'S': { eval: 'bbs.scan_dirs(FL_NO_HDR)'
@@ -149,18 +187,19 @@ shell.file_menu = {
 	 'V': { eval: 'view_files()'
 			,msg: '\r\n\x01c\x01h' + gettext("View File(s)") + '\r\n' },
 	 'W': { eval: 'bbs.whos_online()' },
+	 'X': { eval: 'user.settings ^= USER_EXPERT' },
+	 'Y': { eval: 'bbs.user_info()' },
 	 'Z': { eval: 'upload_sysop_file()'
 			,msg: '\r\n\x01c\x01h' + gettext("Upload File to Sysop") + '\r\n' },
-	 '*': { eval: 'show_dirs(bbs.curlib)' },
-	'/*': { eval: 'show_libs()' },
-	 '&': { exec: 'filescancfg.js' },
-	 '!': { eval: 'bbs.menu("sysxfer")'
+	 '*': { eval: 'bbs.menu("sysxfer")'
 			,ars: 'SYSOP' },
+	 '$': { eval: 'bbs.time_bank()' },
 	 '#': {  msg: '\r\n\x01c\x01h' + gettext("Type the actual number, not the symbol.") + '\r\n' },
 	'/#': {  msg: '\r\n\x01c\x01h' + gettext("Type the actual number, not the symbol.") + '\r\n' },
 	},
 	nav: {
 	'\r': { },
+	 'M': { eval: 'menu = message_menu' },
 	 'Q': { eval: 'menu = main_menu' },
 	 '>': { eval: 'dir_up()' },
 	 '}': { eval: 'dir_up()' },
@@ -181,6 +220,28 @@ shell.file_menu.nav[KEY_UP] = { eval: 'dir_up()' };
 shell.file_menu.nav[KEY_DOWN] = { eval: 'dir_down()' };
 shell.file_menu.nav[KEY_RIGHT] = { eval: 'lib_up()' };
 shell.file_menu.nav[KEY_LEFT] = { eval: 'lib_down()' };
+
+shell.email_menu = {
+	cls: true,
+	file: "renegade/email",
+	prompt: "\x01n\x01b\x01h" + gettext("Time") + " " + time_prefix + ": [\x01c" + time_code + "\x01b] "
+		+ "\x01b\x01h(\x01c" + shell.help_key + "\x01b=\x01c"
+		+ gettext("Help") + "\x01b)\r\n"
+		+ "\x01c" + gettext("Email") + " \x01b" + gettext("Menu") + " \x01m: \x01n",
+	command: {
+		'R': { eval: 'bbs.read_mail(MAIL_YOUR, user.number)' },
+		'E': { eval: 'send_email()' },
+		'F': { eval: 'send_feedback()' },
+		'V': { eval: 'bbs.read_mail(MAIL_SENT, user.number)' },
+		'N': { eval: 'send_netmail()' },
+		'G': { eval: 'logoff(/* fast: */false)' },
+	   '/G': { eval: 'logoff(/* fast: */true)' },
+	},
+	nav: {
+	'\r': { },
+	 'Q': { eval: 'menu = main_menu' },
+	},
+};
 
 shell.menu = shell.main_menu;
 shell.menu_loop();

commit db2c939aeb67b58a350aa7eb40f6bc9f18876633

Allow the write-message (WM_*) mode flags to be passed to shell lib email funcs
Fixes the 'A' (email attachment) command from the email menu.

broken in commit 1dac6acd2215db3e5

Thanks Nelgin for the head's up!

exec/email_sec.js
@@ -41,10 +41,10 @@ while(bbs.online) {
 		case 'A':	// Send file attachment
 			wm_mode = WM_FILE;
 		case 'S':	// Send Mail
-			shell.send_email();
+			shell.send_email(wm_mode);
 			break;
 		case 'N':	// Send NetMail
-			shell.send_netmail();
+			shell.send_netmail(wm_mode);
 			break;
 		default:
 			exit(0);

exec/load/shell_lib.js
@@ -576,7 +576,7 @@ function download_user_files()
 }
 
 // From email_sec.js
-function send_email()
+function send_email(wm_mode)
 {
 	console.putmsg(bbs.text(bbs.text.Email));
 	var name = console.getstr(40, K_TRIM);
@@ -592,22 +592,22 @@ function send_email()
 	if(!number && (msg_area.settings&MM_REALNAME))
 		number = system.matchuserdata(U_NAME, name);
 	if(number)
-		return bbs.email(number, WM_NONE);
+		return bbs.email(number, wm_mode || WM_NONE);
 	console.putmsg(bbs.text(bbs.text.UnknownUser));
 	return false;
 }
 
 // From email_sec.js
 var NetmailAddressHistoryLength = 10;
-function send_netmail()
+function send_netmail(wm_mode)
 {
 	var userprops = bbs.mods.userprops || load(bbs.mods.userprops = {}, "userprops.js");
 	var netmail = msg_area.fido_netmail_settings | msg_area.inet_netmail_settings;
 	const ini_section = "netmail sent";
 	console.crlf();
-	var wm_mode = WM_NONE;
+	wm_mode = wm_mode || WM_NONE;
 	if((netmail&NMAIL_FILE) && !console.noyes(gettext("Attach a file")))
-		wm_mode = WM_FILE;
+		wm_mode |= WM_FILE;
 	if(console.aborted)
 		return false;
 	console.putmsg(bbs.text(bbs.text.EnterNetMailAddress));
@@ -629,9 +629,9 @@ function send_netmail()
 	return false;
 }
 
-function send_feedback()
+function send_feedback(wm_mode)
 {
-	return bbs.email(/* user # */1, bbs.text(bbs.text.ReFeedback));
+	return bbs.email(/* user # */1, wm_mode || WM_NONE, bbs.text(bbs.text.ReFeedback));
 }
 
 function page_sysop()