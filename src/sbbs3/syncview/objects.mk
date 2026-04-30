434fb05e1c22b0790b2d5af8f6f2d73e5d06d6b7
Deucе <shurd@sasktel.net>
2026-04-30 13:54:24 -0400

    Fix SyncView build
    Needs the new ansi_filter.o

--- src/sbbs3/syncview/objects.mk
+++ src/sbbs3/syncview/objects.mk
@@ -1,4 +1,5 @@
 OBJS	=	\
+			$(MTOBJODIR)$(DIRSEP)ansi_filter$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)ripper$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)sauce$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)syncview$(OFILE) \
