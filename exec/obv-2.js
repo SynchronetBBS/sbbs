4bced7dba6b8bbdee2f6a70f17f47e29b7795b9d
tmccaff57 <tmccaff57@gmail.com>
2026-05-25 21:40:06 -0400

    OBV/2 Clone Shell Converted from Baja to JavaScript
    A couple of things, I removed the $ donation and added an X expert toggle switch. I also added to the file menu some switches that never worked in the original, like library down and up. Also, in files menu, the K config listing was never in code and I added it.

---
+++ exec/obv-2.js
@@ -0,0 +1,238 @@
+// OBV/2 Command Shell 
+// Pure OBV/2 behavior, using shell_lib.js engine
+// @format.tab-size 4, @format.use-tabs true
+
+"use strict";
+
+require("sbbsdefs.js", "SYS_RA_EMU");
+require("userdefs.js", "USER_EXPERT");
+require("nodedefs.js", "NODE_MAIN");
+require("key_defs.js", "KEY_UP");
+require("gettext.js", "gettext");
+load("termsetup.js");
+
+var shell = load({}, "shell_lib.js");
+
+system.settings |= SYS_RA_EMU;  // Swap Msg ReRead/Reply commands
+
+shell.help_key = '?';
+
+// Force OBV/2 behavior: Cold Keys always ON
+user.settings |= USER_COLDKEYS;
+
+shell.main_menu = {
+    cls: true,
+    file: "obv-2/main",
+    eval: 'bbs.main_cmds++',
+    node_action: NODE_MAIN,
+   prompt: "\x01n\x01gMain \x01cþ\x01h@TLEFT@\x01n\x01cþ " 
+	    + "\x01h\x01kC\x01n\x01go\x01hm\x01nm\x01hand: \x01n",
+    command: {
+        'A': { eval: 'bbs.auto_msg()' },
+        'B': { eval: 'bbs.exec_xtrn("sbbslist")' },
+        'C': { eval: 'bbs.page_sysop()' },
+        'D': { eval: 'bbs.list_logons()' },
+        'F': { eval: 'send_feedback()' },
+        'G': { eval: 'logoff(/* fast: */false)' },
+       '/G': { eval: 'logoff(/* fast: */true)' },
+        'H': { eval: 'bbs.whos_online()' },
+        'I': { eval: 'bbs.chat_sec()' },
+        'K': { eval: 'bbs.user_config(); exit()' },
+        '+': { eval: 'bbs.user_config(); exit()' },
+        'L': { eval: 'bbs.list_users(UL_ALL)' },
+        'P': { eval: 'bbs.xtrn_sec()' },
+        'S': { eval: 'bbs.sys_info();' },
+        'U': { eval: 'bbs.time_bank()' },
+        'W': { eval: 'bbs.exec("?logonlist")' },
+        'Y': { eval: 'bbs.user_info()' },
+        'X': { eval: 'user.settings ^= USER_EXPERT' },
+        'Z': { eval: 'bbs.reinit_msg_ptrs()' }
+    },
+    nav: {
+        '\r': { },
+        'E': { eval: 'menu = email_menu' },
+        'M': { eval: 'menu = message_menu' },
+        'Q': { eval: 'menu = quick_menu' },
+        'T': { eval: 'enter_file_section(); menu = file_menu' }
+    }
+};
+
+shell.message_menu = {
+    cls: true,
+    file: "obv-2/message",
+    node_action: NODE_RMSG,
+    prompt: "\x01n\x01gMessage \x01cþ \x01h@TLEFT@\x01n\x01cþ " 
+        + "\x01c\x01h[@GN@] @GRP@ [@SN@] @SUB@: \x01n",
+    command: {
+        'A': { eval: 'console.clear();'
+               + 'select_msg_area();' },
+        'B': { eval: 'console.clear();'
+               + 'show_subs(bbs.curgrp)' },
+        '*': { eval: 'console.clear();'
+               + 'show_subs(bbs.curgrp);' },
+        'C': { eval: 'bbs.cfg_msg_scan(SCAN_NEW)' },
+        'G': { eval: 'logoff(/* fast: */false)' },
+       '/G': { eval: 'logoff(/* fast: */true)' },
+        'J': { eval: 'console.clear();'
+               + 'select_msg_area();' },
+        'N': { eval: 'bbs.scan_msgs()' },
+        'O': { eval: 'bbs.menu_dir = "obv-2";'
+               + 'bbs.qwk_sec();'
+               + 'bbs.menu_dir = "";' },
+        'P': { eval: 'bbs.post_msg()' },
+        'R': { eval: 'bbs.scan_msgs()' },
+        'Y': { eval: 'bbs.scan_subs(SCAN_TOYOU)' },
+        'Z': { exec: 'msgscancfg.js' },
+		'+': { eval: 'sub_up()' },
+        '-': { eval: 'sub_down()' },
+        '>': { eval: 'grp_up()' },
+        '<': { eval: 'grp_down()' },
+    },
+    nav: {
+        '\r': { },
+        'Q': { eval: 'menu = main_menu' },
+        'T': { eval: 'enter_file_section(); menu = file_menu' },
+        '+': { eval: 'sub_up()' },
+        '-': { eval: 'sub_down()' },
+        '>': { eval: 'grp_up()' },
+        '<': { eval: 'grp_down()' },
+        '*': { eval: 'show_subs(bbs.curgrp)' },
+        '/*': { eval: 'show_grps()' }
+    }
+};
+
+// Arrow key navigation
+shell.message_menu.nav[KEY_UP] = { eval: 'sub_up()' };
+shell.message_menu.nav[KEY_DOWN] = { eval: 'sub_down()' };
+shell.message_menu.nav[KEY_RIGHT] = { eval: 'grp_up()' };
+shell.message_menu.nav[KEY_LEFT] = { eval: 'grp_down()' };
+
+shell.file_menu = {
+    cls: true,
+    file: "obv-2/files",
+    eval: 'bbs.file_cmds++',
+    node_action: NODE_XFER,
+   prompt: "\x01n\x01gFile \x01cþ \x01h@TLEFT@ \x01n\x01cþ " 
+        + "\x01h\x01c(@LN@) @LIB@ (@DN@) @DIR@: ",
+    num_input: shell.get_dir_num,
+    slash_num_input: shell.get_lib_num,
+    command: {
+        'A': { eval: 'console.clear();'
+               + 'select_file_area();' },
+        'B': { eval: 'bbs.batch_menu()' },
+        'C': { exec: 'filescancfg.js' },
+       '/D': { eval: 'download_files()',
+            msg: '\r\n\x01c\x01h' + gettext("Download File(s)") + '\r\n',
+            ars: 'REST NOT D' },
+		'E': { eval: 'view_file_info(FI_REMOVE)'
+			,msg: '\r\n\x01c\x01h' + gettext("Remove/Edit File(s)") + '\r\n' },	
+        'G': { eval: 'logoff(/* fast: */false)' },
+       '/G': { eval: 'logoff(/* fast: */true)' },
+		'K': { eval: "console.clear();" + "console.print('\\x01n\\x01h\\x01cFile Configuration Listing\\r\\n');" +
+            "console.print('-----------------------------------\\r\\n');" +
+			"console.print('Current Library : ' + bbs.curlib + ' - ' "
+			+ "+ file_area.lib_list[bbs.curlib].description + '\\r\\n');" +
+			"console.print('Current Dir     : ' + bbs.curdir + ' - ' "
+			+ "+ file_area.lib_list[bbs.curlib].dir_list[bbs.curdir].description + '\\r\\n');" +
+			"console.print('New-scan Date   : ' + system.timestr(user.new_file_time) + '\\r\\n');" +
+            "console.print('Batch Queue     : ' + bbs.batch_size + ' file(s)\\r\\n');" +
+            "console.print('Protocol        : ' + user.protocol + '\\r\\n');" +
+            "console.print('UL Today        : ' + user.ulb + ' bytes\\r\\n');" +
+            "console.print('DL Today        : ' + user.dlb + ' bytes\\r\\n');" +
+            "console.crlf();"
+		},
+        'L': { eval: 'list_files()' },
+        'N': {
+            eval: 'bbs.scan_dirs(FL_ULTIME)',
+            msg: '\r\n\x01c\x01h' + gettext("New File Scan") + '\r\n'
+        },
+        'R': { eval: 'bbs.batch_menu()' },       
+        'S': {
+            eval: 'bbs.scan_dirs(FL_NO_HDR)',
+            msg: '\r\n\x01c\x01h' + gettext("Search for Filename(s)") + '\r\n'
+        },
+        'T': { eval: 'bbs.temp_xfer()' },
+        'U': {
+            eval: 'upload_file()',
+            msg: '\r\n\x01c\x01h' + gettext("Upload File") + '\r\n'
+        },
+	  '/U': { eval: 'upload_user_file()'
+			,msg: '\r\n\x01c\x01h' + gettext("Upload File to User") + '\r\n' },
+        'V': {
+            eval: 'view_files()',
+            msg: '\r\n\x01c\x01h' + gettext("View File(s)") + '\r\n'
+        },
+        'W': {
+            eval: 'view_file_info(FI_INFO)',
+            msg: '\r\n\x01c\x01h' + gettext("List Extended File Information") + '\r\n' },
+        'X': { eval: "if (file_exists(system.text_dir + 'jsyswant.txt')) {"
+			   + "console.crlf();"
+               + "bbs.printfile(system.text_dir + 'jsyswant.txt');"
+               + "} else {"
+               + "console.crlf();"
+               + "console.print('\\x01n\\x01hNo particular files are being sought.\\r\\n');"
+               + "console.crlf();"
+               + "}" },
+	},
+    nav: {
+        '\r': { },
+        'M': { eval: 'menu = message_menu' },
+        'Q': { eval: 'menu = main_menu' },
+        '+': { eval: 'dir_up()' },
+        '-': { eval: 'dir_down()' },
+		'*': { eval: 'show_dirs(bbs.curlib)' }, 
+	   '/*': { eval: 'show_libs()' },
+       '/+': { eval: 'lib_up()' },
+       '/-': { eval: 'lib_down()' },
+    }
+};
+
+// Arrow key navigation
+shell.file_menu.nav[KEY_UP] = { eval: 'dir_up()' };
+shell.file_menu.nav[KEY_DOWN] = { eval: 'dir_down()' };
+shell.file_menu.nav[KEY_RIGHT] = { eval: 'lib_up()' };
+shell.file_menu.nav[KEY_LEFT] = { eval: 'lib_down()' };
+
+shell.email_menu = {
+    cls: true,
+    file: "obv-2/email",
+    prompt: "\x01h\x01kEmail \x01n\x01gþ\x01h@TLEFT@\x01n\x01gþ "
+        + "\x01h\x01kC\x01n\x01go\x01hm\x01nm\x01hand: \x01n",
+    command: {
+        'F': { eval: 'send_email()' },
+        'R': { eval: 'bbs.read_mail(MAIL_YOUR, user.number)' },
+        'S': { eval: 'send_email()' },
+        'V': { eval: 'bbs.read_mail(MAIL_SENT, user.number)' }
+    },
+    nav: {
+        '\r': { },
+        'Q': { eval: 'menu = main_menu' }
+    }
+};
+
+shell.quick_menu = {
+    cls: true,
+    file: "obv-2/quick",
+    prompt: "\x01n\x01gQuick \x01cþ\x01h@TLEFT@\x01n\x01cþ "
+		+ "\x01h\x01kC\x01n\x01go\x01hm\x01nm\x01hand: \x01n",
+    command: {
+        'C': { eval: 'bbs.page_sysop()' },
+        'E': { eval: 'menu = email_menu' },
+        'F': { eval: 'send_feedback()' },
+        'G': { eval: 'logoff(/* fast: */false)' },
+       '/G': { eval: 'logoff(/* fast: */true)' },
+        'M': { eval: 'menu = message_menu' },
+        'Q': { eval: 'menu = main_menu' },
+        'T': { eval: 'menu = file_menu' }
+    },
+    nav: {
+        '\r': { },
+        'E': { eval: 'menu = email_menu' },
+        'M': { eval: 'menu = message_menu' },
+        'Q': { eval: 'menu = main_menu' },
+        'T': { eval: 'menu = file_menu' }
+    }
+};
+
+shell.menu = shell.main_menu;
+shell.menu_loop();
