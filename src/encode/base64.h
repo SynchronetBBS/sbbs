7b116541087e0fb0b661238117b62a39d696cfca
Deucе <shurd@sasktel.net>
2026-09-06 20:49:31 -0400

    Rewrite Base64 header under LGPL
    Port XPDev 60f43aa88259c1d955d893599887d27d8846f732 while retaining gen_defs.h for the monorepo legacy compiler and non-CMake build paths.

--- src/encode/base64.h
+++ src/encode/base64.h
@@ -1,36 +1,25 @@
-/* Base64 encoding/decoding exported function prototypes */
+/* SPDX-License-Identifier: LGPL-2.0-or-later */
 
-/****************************************************************************
- * @format.tab-size 4		(Plain Text/Source Code File Header)			*
- * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
- *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
- *																			*
- * This program is free software; you can redistribute it and/or			*
- * modify it under the terms of the GNU General Public License				*
- * as published by the Free Software Foundation; either version 2			*
- * of the License, or (at your option) any later version.					*
- * See the GNU General Public License for more details: gpl.txt or			*
- * http://www.fsf.org/copyleft/gpl.html										*
- *																			*
- * For Synchronet coding style and modification guidelines, see				*
- * http://www.synchro.net/source.html										*
- *																			*
- * Note: If this box doesn't appear square, then you need to fix your tabs.	*
- ****************************************************************************/
+/*
+ * Public interface for the Base64 implementation in base64.c.
+ *
+ * This header is licensed under the same GNU Lesser General Public License
+ * terms as the implementation. It replaces an older header whose GPL notice
+ * was added in error and did not reflect the license of base64.c.
+ */
 
-#ifndef _base64_h_
-#define _base64_h_
+#ifndef XPDEV_ENCODE_BASE64_H
+#define XPDEV_ENCODE_BASE64_H
 
 #include "gen_defs.h" // ssize_t
 
 #if defined(_WIN32) && (defined(B64_IMPORTS) || defined(B64_EXPORTS))
 	#if defined(B64_IMPORTS)
-		#define B64EXPORT   __declspec(dllimport)
+		#define B64EXPORT __declspec(dllimport)
 	#else
-		#define B64EXPORT   __declspec(dllexport)
+		#define B64EXPORT __declspec(dllexport)
 	#endif
-#else   /* !_WIN32 */
+#else
 	#define B64EXPORT
 #endif
 
@@ -38,11 +27,11 @@
 extern "C" {
 #endif
 
-B64EXPORT ssize_t b64_encode(char *target, size_t tlen, const char *source, size_t slen);
-B64EXPORT ssize_t b64_decode(char *target, size_t tlen, const char *source, size_t slen);
+B64EXPORT ssize_t b64_encode(char* target, size_t target_size, const char* source, size_t source_length);
+B64EXPORT ssize_t b64_decode(char* target, size_t target_size, const char* source, size_t source_length);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* Don't add anything after this line */
+#endif /* XPDEV_ENCODE_BASE64_H */
